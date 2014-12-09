9ddc9232b763989989349a7197f3fc12bee1f48a
devin <devin@wordimpress.com>
2014-12-09 11:13:34 -0800

    Base form custom fields setup, ready to start building in templating system

--- assets/js/admin-scripts.js
+++ assets/js/admin-scripts.js
@@ -3,7 +3,33 @@
  *
  */
 
-jQuery( document ).ready( function ( $ ) {
+jQuery.noConflict();
+(function ( $ ) {
+
+//--------------------------------------
+// Single Forms
+//--------------------------------------
+
+	//Default Radio Button: Allow only one to be checked
+	var handle_default_radio = function () {
+		"use strict";
+		var default_radio = $( '#_give_donation_levels_repeat input[type=radio]' );
+		default_radio.prop( 'checked', false );
+
+		default_radio.on( 'change', function () {
+			default_radio.not( this ).prop( 'checked', false );
+		} );
+
+	};
 
 
-} );
+	//On DOM Ready
+	$( function () {
+
+		// More code using $ as alias to jQuery
+		handle_default_radio();
+
+	} );
+
+
+})( jQuery );
--- includes/admin/forms/metabox.php
+++ includes/admin/forms/metabox.php
@@ -32,57 +32,81 @@ function cmb2_sample_metaboxes( array $meta_boxes ) {
 	/**
 	 * Repeatable Field Groups
 	 */
-	$meta_boxes['form_field_options'] = array(
-		'id'           => 'form_field_options',
-		'title'        => __( 'Create a New Donation Form', 'give' ),
-		'object_types' => array( 'give_forms', ),
-		'fields'       => array(
-
-			//Donation levels: Header
-			array(
-				'id'   => $prefix . 'levels_header',
-				'type' => 'levels_repeater_header',
-			),
-			//Donation Levels: Repeatable CMB2 Group
-			array(
-				'id'          => $prefix . 'donation_levels',
-				'type'        => 'group',
-				'description' => __( '', 'give' ),
-				'options'     => array(
-					'add_button'    => __( 'Add Level', 'give' ),
-					'remove_button' => __( 'Remove Level', 'give' ),
-					'sortable'      => true, // beta
+	$meta_boxes['form_field_options'] = apply_filters( 'give_form_field_options', array(
+			'id'           => 'form_field_options',
+			'title'        => __( 'Create a New Donation Form', 'give' ),
+			'object_types' => array( 'give_forms', ),
+			'context'      => 'normal',
+			'priority'     => 'high', //Show above Content WYSIWYG
+			'fields'       => array(
+				//Donation levels: Header
+				array(
+					'id'   => $prefix . 'levels_header',
+					'type' => 'levels_repeater_header',
 				),
-				// Fields array works the same, except id's only need to be unique for this group. Prefix is not needed.
-				'fields'      => array(
-					array(
-						'name' => __( 'Amount', 'give' ),
-						'id'   => $prefix . 'amount',
-						'type' => 'text_money',
+				//Donation Levels: Repeatable CMB2 Group
+				array(
+					'id'      => $prefix . 'donation_levels',
+					'type'    => 'group',
+					'options' => array(
+						'add_button'    => __( 'Add Level', 'give' ),
+						'remove_button' => __( 'Remove Level', 'give' ),
+						'sortable'      => true, // beta
 					),
-					array(
-						'name'       => __( 'Text', 'give' ),
-						'id'         => $prefix . 'text',
-						'type'       => 'text',
-						'attributes' => array(
-							'placeholder' => 'Donation Level',
-							'rows'        => 3,
-							'required'    => 'required',
+					// Fields array works the same, except id's only need to be unique for this group. Prefix is not needed.
+					'fields'  => array(
+						array(
+							'name' => __( 'Amount', 'give' ),
+							'id'   => $prefix . 'amount',
+							'type' => 'text_money',
+						),
+						array(
+							'name'       => __( 'Text', 'give' ),
+							'id'         => $prefix . 'text',
+							'type'       => 'text',
+							'attributes' => array(
+								'placeholder' => 'Donation Level',
+								'rows'        => 3,
+								'required'    => 'required',
+							),
+						),
+						array(
+							'name'    => __( 'Default', 'give' ),
+							'id'      => $prefix . 'default',
+							'type'    => 'radio_inline',
+							'options' => array(
+								'default' => __( 'Default', 'cmb' ),
+							),
 						),
 					),
-					array(
-						'name'    => __( 'Default', 'give' ),
-						'id'      => $prefix . 'default',
-						'type'    => 'radio_inline',
-						'options' => array(
-							'default' => __( 'Default', 'cmb' ),
-						),
+				),
+				//Display Style
+				array(
+					'name'        => __( 'Display Style', 'give' ),
+					'description' => __( 'Set how the donations levels will display on the form.', 'give' ),
+					'id'          => $prefix . 'display_style',
+					'type'        => 'radio_inline',
+					'default'     => 'buttons',
+					'options'     => array(
+						'buttons'  => __( 'Buttons', 'cmb' ),
+						'radios'   => __( 'Radios', 'cmb' ),
+						'dropdown' => __( 'Dropdown', 'cmb' ),
+					),
+				),
+				//Custom Amount
+				array(
+					'name'        => __( 'Custom Amount', 'give' ),
+					'description' => __( 'Do you want the user to be able to input their own donation amount?', 'give' ),
+					'id'          => $prefix . 'custom_amount',
+					'type'        => 'radio_inline',
+					'default'     => 'no',
+					'options'     => array(
+						'yes' => __( 'Yes', 'cmb' ),
+						'no'  => __( 'No', 'cmb' ),
 					),
 				),
 			),
-
-
-		),
+		)
 	);
 
 	return $meta_boxes;
@@ -98,10 +122,10 @@ function give_cmb_render_levels_repeater_header() {
 
 	<div class="table-container">
 		<div class="table-row">
-			<div class="table-cell col-amount"><?php _e('Amount', 'give'); ?></div>
-			<div class="table-cell col-text"><?php _e('Text', 'give'); ?></div>
-			<div class="table-cell col-default"><?php _e('Default', 'give'); ?></div>
-			<div class="table-cell col-sort"><?php _e('Sort', 'give'); ?></div>
+			<div class="table-cell col-amount"><?php _e( 'Amount', 'give' ); ?></div>
+			<div class="table-cell col-text"><?php _e( 'Text', 'give' ); ?></div>
+			<div class="table-cell col-default"><?php _e( 'Default', 'give' ); ?></div>
+			<div class="table-cell col-sort"><?php _e( 'Sort', 'give' ); ?></div>
 		</div>
 	</div>
 
