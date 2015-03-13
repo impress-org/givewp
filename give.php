654dfed93bdcc5efdbf3f2c7338146cd323f11fe
devin <devin@wordimpress.com>
2015-03-13 16:58:35 -0700

    Version bump to get this branch rolling

--- give.php
+++ give.php
@@ -5,7 +5,7 @@
  * Description: The most robust, flexible, and intuitive way to accept donations on WordPress.
  * Author: WordImpress
  * Author URI: http://wordimpress.com
- * Version: 0.7 beta
+ * Version: 0.8 beta
  * Text Domain: give
  * Domain Path: /languages
  *
