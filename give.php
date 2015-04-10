3e3e16687a0ba50659277e01d92eea889db549a5
devin <devin@wordimpress.com>
2015-04-10 14:58:00 -0700

    Removed unnecessary contextual help files until we decide how we are going to approach this with the plugin

--- give.php
+++ give.php
@@ -254,14 +254,12 @@ if ( ! class_exists( 'Give' ) ) : /**
 
 				require_once GIVE_PLUGIN_DIR . 'includes/admin/payments/actions.php';
 				require_once GIVE_PLUGIN_DIR . 'includes/admin/payments/payments-history.php';
-				require_once GIVE_PLUGIN_DIR . 'includes/admin/payments/contextual-help.php';
 
 				require_once GIVE_PLUGIN_DIR . 'includes/admin/forms/functions.php';
 				require_once GIVE_PLUGIN_DIR . 'includes/admin/forms/metabox.php';
 				require_once GIVE_PLUGIN_DIR . 'includes/admin/forms/dashboard-columns.php';
 				require_once GIVE_PLUGIN_DIR . 'includes/admin/forms/shortcode.php';
 
-				require_once GIVE_PLUGIN_DIR . 'includes/admin/reporting/contextual-help.php';
 				require_once GIVE_PLUGIN_DIR . 'includes/admin/reporting/reports.php';
 				require_once GIVE_PLUGIN_DIR . 'includes/admin/reporting/pdf-reports.php';
 				require_once GIVE_PLUGIN_DIR . 'includes/admin/reporting/class-give-graph.php';
