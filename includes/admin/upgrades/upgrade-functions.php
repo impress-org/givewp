183cdad5bd0b55213f4d9150973d9620b03b308f
devin <devin@wordimpress.com>
2015-06-16 10:43:02 -0700

    Removed unnecessary upgrade function

--- includes/admin/upgrades/upgrade-functions.php
+++ includes/admin/upgrades/upgrade-functions.php
@@ -44,13 +44,6 @@ function give_show_upgrade_notices() {
 	//		);
 	//	}
 
-	if ( version_compare( $give_version, '1.0', '<' ) ) {
-		printf(
-			'<div class="updated"><p>' . __( 'Give needs to upgrade the transaction logs database, click <a href="%s">here</a> to start the upgrade.', 'give' ) . '</p></div>',
-			esc_url( admin_url( 'index.php?page=give-upgrades&give-upgrade=upgrade_payments_logs_db' ) )
-		);
-	}
-
 	if ( version_compare( $give_version, '1.0', '<' ) || ! give_has_upgrade_completed( 'upgrade_donor_payments_association' ) ) {
 		printf(
 			'<div class="updated"><p>' . __( 'Give needs to upgrade the donor database, click <a href="%s">here</a> to start the upgrade.', 'give' ) . '</p></div>',
@@ -167,110 +160,6 @@ function give_get_completed_upgrades() {
 
 }
 
-
-/**
- * Fixes the give_log meta
- *
- * @since 1.0
- * @return void
- */
-function give_v1_upgrade_payments_logs_db() {
-
-	global $wpdb;
-
-	if ( ! current_user_can( 'manage_give_settings' ) ) {
-		wp_die( __( 'You do not have permission to do Give upgrades', 'give' ), __( 'Error', 'give' ), array( 'response' => 403 ) );
-	}
-	ignore_user_abort( true );
-
-	if ( ! give_is_func_disabled( 'set_time_limit' ) && ! ini_get( 'safe_mode' ) ) {
-		@set_time_limit( 0 );
-	}
-	$step   = isset( $_GET['step'] ) ? absint( $_GET['step'] ) : 1;
-	$number = 25;
-	$offset = $step == 1 ? 0 : ( $step - 1 ) * $number;
-	if ( 1 === $step ) {
-		// Check if we have any variable price products on the first step
-		$sql          = "SELECT ID FROM $wpdb->posts p LEFT JOIN $wpdb->postmeta m ON p.ID = m.post_id WHERE m.meta_key = '_variable_pricing' AND m.meta_value = 1 LIMIT 1";
-		$has_variable = $wpdb->get_col( $sql );
-		if ( empty( $has_variable ) ) {
-			// We had no variable priced forms, so go ahead and just complete
-			update_option( 'give_version', preg_replace( '/[^0-9.].*/', '', GIVE_VERSION ) );
-			delete_option( 'give_doing_upgrade' );
-			wp_redirect( admin_url( 'index.php?page=give-about' ) );
-			exit;
-		}
-	}
-	$payment_ids = $wpdb->get_col( $wpdb->prepare( "SELECT ID FROM $wpdb->posts WHERE post_type = 'give_payment' ORDER BY post_date DESC LIMIT %d,%d;", $offset, $number ) );
-	if ( ! empty( $payment_ids ) ) {
-		foreach ( $payment_ids as $payment_id ) {
-			$payment_meta = give_get_payment_meta( $payment_id );
-			$variable_donation = array();
-			// Don't care if the form is a single price id
-			if ( ! isset( $payment_meta['price_id'] ) || empty( $payment_meta['price_id'] ) ) {
-				continue;
-			}
-			$variable_donation[] = array(
-				'id'       => $payment_meta['form_id'],
-				'price_id' => $payment_meta['price_id']
-			);
-
-			$variable_donation_ids = array_unique( wp_list_pluck( $variable_donation, 'id' ) );
-			$unique_donation_ids   = implode( ',', $variable_donation_ids );
-			if ( empty( $unique_donation_ids ) ) {
-				continue; // If there were no downloads, just fees, move along
-			}
-			// Get all Log Ids where the post parent is in the set of download IDs we found in the cart meta
-			$logs        = $wpdb->get_results( "SELECT m.post_id AS log_id, p.post_parent AS give_form_id FROM $wpdb->postmeta m LEFT JOIN $wpdb->posts p ON m.post_id = p.ID WHERE meta_key = '_give_log_payment_id' AND meta_value = $payment_id AND p.post_parent IN ($unique_donation_ids)", ARRAY_A );
-			$mapped_logs = array();
-
-			// Go through each cart item
-			foreach ( $variable_donation as $cart_item ) {
-				// Itterate through the logs we found attached to this payment
-				foreach ( $logs as $key => $log ) {
-					// If this Log ID is associated with this donation ID give it the price_id
-					if ( (int) $log['give_form_id'] === (int) $cart_item['id'] ) {
-						$mapped_logs[ $log['log_id'] ] = $cart_item['price_id'];
-						// Remove this Donation/Log ID from the list, for multipurchase compatibility
-						unset( $logs[ $key ] );
-						// These aren't the logs we're looking for. Move Along, Move Along.
-						break;
-					}
-				}
-			}
-			if ( ! empty( $mapped_logs ) ) {
-				$update = "UPDATE $wpdb->postmeta SET meta_value = ";
-				$case   = "CASE post_id ";
-				foreach ( $mapped_logs as $post_id => $value ) {
-					$case .= "WHEN $post_id THEN $value ";
-				}
-				$case .= "END ";
-				$log_ids = implode( ',', array_keys( $mapped_logs ) );
-				$where   = "WHERE post_id IN ($log_ids) AND meta_key = '_give_log_price_id'";
-				$sql     = $update . $case . $where;
-				// Execute our query to update this payment
-				$wpdb->query( $sql );
-			}
-		}
-		// More Payments found so upgrade them
-		$step ++;
-		$redirect = add_query_arg( array(
-			'page'         => 'give-upgrades',
-			'give-upgrade' => 'upgrade_payments_logs_db',
-			'step'         => $step
-		), admin_url( 'index.php' ) );
-		wp_redirect( $redirect );
-		exit;
-	} else {
-		// No more payments found, finish up
-		update_option( 'give_version', preg_replace( '/[^0-9.].*/', '', GIVE_VERSION ) );
-		delete_option( 'give_doing_upgrade' );
-		wp_redirect( admin_url( 'index.php?page=give-about' ) );
-		exit;
-	}
-}
-
-add_action( 'give_upgrade_payments_logs_db', 'give_v1_upgrade_payments_logs_db' );
 
 /**
  * Run the upgrade for the customers to find all payment attachments
