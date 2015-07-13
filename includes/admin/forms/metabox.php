79876bf187b09ea198cdd809d63e5bd26630a391
devin <devin@wordimpress.com>
2015-07-13 09:23:41 -0700

    Improved field description to resolve #158

--- includes/admin/forms/metabox.php
+++ includes/admin/forms/metabox.php
@@ -147,7 +147,7 @@ function give_single_forms_cmb2_metaboxes( array $meta_boxes ) {
 				),
 				array(
 					'name'        => __( 'Custom Amount Text', 'give' ),
-					'description' => __( 'This text appears as a label next to the custom amount field for single level forms. For multi-level forms the text will appear as it\'s own level (ie button, radio, or select option). Leave this field blank to prevent it from displaying within your form.', 'give' ),
+					'description' => __( 'This text appears as a label next to the custom amount field for single level forms. For multi-level forms the text will appear as it\'s own level (ie button, radio, or select option). Add your own message or leave this field blank to prevent it from displaying within your form.', 'give' ),
 					'id'          => $prefix . 'custom_amount_text',
 					'type'        => 'text',
 					'row_classes' => 'give-subfield',
@@ -368,7 +368,7 @@ function give_cmb_render_levels_repeater_header() {
 		</div>
 	</div>
 
-<?php
+	<?php
 }
 
 
