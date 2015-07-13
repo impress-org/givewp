da8c7e203663f0eae4a742624e95aa3bd23ccae0
devin <devin@wordimpress.com>
2015-07-12 20:05:12 -0700

    New Customer Class tests

--- tests/tests-customers.php
+++ tests/tests-customers.php
@@ -11,31 +11,42 @@ class Give_Tests_Customers extends WP_UnitTestCase {
 
 	protected $_customer_id = null;
 
+	/**
+	 * Set it up
+	 */
 	public function setUp() {
 		parent::setUp();
 
+		//Create a Donation Form
 		$this->_post_id = $this->factory->post->create( array(
 			'post_title'  => 'Test Form',
 			'post_type'   => 'give_forms',
 			'post_status' => 'publish'
 		) );
 
-		$_variable_pricing = array(
+		$_multi_level_donations = array(
 			array(
-				'name'   => 'Simple',
-				'amount' => 20
+				'_give_id'     => array( 'level_id' => '1' ),
+				'_give_amount' => '10.00',
+				'_give_text'   => 'Basic Level'
 			),
 			array(
-				'name'   => 'Advanced',
-				'amount' => 100
-			)
+				'_give_id'     => array( 'level_id' => '2' ),
+				'_give_amount' => '20.00',
+				'_give_text'   => 'Intermediate Level'
+			),
+			array(
+				'_give_id'     => array( 'level_id' => '3' ),
+				'_give_amount' => '40.00',
+				'_give_text'   => 'Advanced Level'
+			),
 		);
 
 		$meta = array(
 			'give_price'               => '0.00',
-			'_variable_pricing'        => 1,
+			'_give_price_option'       => 'multi',
 			'_give_price_options_mode' => 'on',
-			'give_variable_prices'     => array_values( $_variable_pricing ),
+			'_give_donation_levels'    => array_values( $_multi_level_donations ),
 			'give_product_notes'       => 'Donation Notes',
 			'_give_product_type'       => 'default'
 		);
@@ -43,7 +54,7 @@ class Give_Tests_Customers extends WP_UnitTestCase {
 			update_post_meta( $this->_post_id, $key, $value );
 		}
 
-		/** Generate some donations */
+		//Generate Donations
 		$this->_user_id = $this->factory->user->create( array( 'role' => 'administrator' ) );
 		$user           = get_userdata( $this->_user_id );
 
@@ -63,24 +74,24 @@ class Give_Tests_Customers extends WP_UnitTestCase {
 			)
 		);
 
-		$price = '100.00';
-
 		$total = 0;
 
-		$prices     = get_post_meta( $donation_details[0]['id'], 'give_variable_prices', true );
+		$prices     = get_post_meta( $donation_details[0]['id'], '_give_donation_levels', true );
 		$item_price = $prices[1]['_give_amount'];
 
 		$total += $item_price;
 
 		$purchase_data = array(
-			'price'        => number_format( (float) $total, 2 ),
-			'date'         => date( 'Y-m-d H:i:s', strtotime( '-1 day' ) ),
-			'purchase_key' => strtolower( md5( uniqid() ) ),
-			'user_email'   => $user_info['email'],
-			'user_info'    => $user_info,
-			'currency'     => 'USD',
-			'status'       => 'pending',
-			'tax'          => '0.00'
+			'price'           => number_format( (float) $total, 2 ),
+			'give_form_title' => get_the_title( $this->_post_id ),
+			'give_form_id'    => $this->_post_id,
+			'date'            => date( 'Y-m-d H:i:s', strtotime( '-1 day' ) ),
+			'purchase_key'    => strtolower( md5( uniqid() ) ),
+			'user_email'      => $user_info['email'],
+			'user_info'       => $user_info,
+			'currency'        => 'USD',
+			'status'          => 'pending',
+			'gateway'         => 'manual'
 		);
 
 		$_SERVER['REMOTE_ADDR'] = '10.0.0.0';
@@ -185,17 +196,17 @@ class Give_Tests_Customers extends WP_UnitTestCase {
 
 		$customer = new Give_Customer( 'testadmin@domain.com' );
 
-		$this->assertEquals( '100', $customer->purchase_value );
+		$this->assertEquals( '20', $customer->purchase_value );
 		$this->assertEquals( '1', $customer->purchase_count );
 
 		$customer->increase_purchase_count();
 		$customer->increase_value( 10 );
 
-		$this->assertEquals( '110', $customer->purchase_value );
+		$this->assertEquals( '30', $customer->purchase_value );
 		$this->assertEquals( '2', $customer->purchase_count );
 
 		$this->assertEquals( give_count_purchases_of_customer( $this->_user_id ), '2' );
-		$this->assertEquals( give_purchase_total_of_user( $this->_user_id ), '110' );
+		$this->assertEquals( give_purchase_total_of_user( $this->_user_id ), '30' );
 
 		// Make sure we hit the false conditions
 		$this->assertFalse( $customer->increase_purchase_count( - 1 ) );
@@ -210,11 +221,11 @@ class Give_Tests_Customers extends WP_UnitTestCase {
 		$customer->decrease_purchase_count();
 		$customer->decrease_value( 10 );
 
-		$this->assertEquals( $customer->purchase_value, '90' );
+		$this->assertEquals( $customer->purchase_value, '10' );
 		$this->assertEquals( $customer->purchase_count, '0' );
 
 		$this->assertEquals( give_count_purchases_of_customer( $this->_user_id ), '0' );
-		$this->assertEquals( give_purchase_total_of_user( $this->_user_id ), '90' );
+		$this->assertEquals( give_purchase_total_of_user( $this->_user_id ), '10' );
 
 		// Make sure we hit the false conditions
 		$this->assertFalse( $customer->decrease_purchase_count( - 1 ) );
@@ -272,23 +283,14 @@ class Give_Tests_Customers extends WP_UnitTestCase {
 
 	}
 
-	public function test_users_purchased_product() {
+	public function test_give_get_users_completed_donations() {
 
-		$out2 = give_get_users_purchased_products( $this->_user_id );
+		$out2 = give_get_users_completed_donations( $this->_user_id );
 
 		$this->assertInternalType( 'array', $out2 );
 		$this->assertEquals( 1, count( $out2 ) );
 		$this->assertInternalType( 'object', $out2[0] );
-		$this->assertEquals( $out2[0]->post_type, 'download' );
-
-	}
-
-	public function test_has_user_purchased() {
-
-		$this->assertTrue( give_has_user_purchased( $this->_user_id, array( $this->_post_id ), 1 ) );
-		$this->assertFalse( give_has_user_purchased( $this->_user_id, array( 888 ), 1 ) );
-		$this->assertFalse( give_has_user_purchased( 0, $this->_post_id ) );
-		$this->assertFalse( give_has_user_purchased( 0, 888 ) );
+		$this->assertEquals( $out2[0]->post_type, 'give_forms' );
 
 	}
 
@@ -303,15 +305,15 @@ class Give_Tests_Customers extends WP_UnitTestCase {
 
 	}
 
-	public function test_get_purchase_total_of_user() {
+	public function test_get_donation_total_of_user() {
 
-		$purchase_total = give_purchase_total_of_user( $this->_user_id );
+		$donation_total = give_purchase_total_of_user( $this->_user_id );
 
-		$this->assertEquals( 100, $purchase_total );
+		$this->assertEquals( 20, $donation_total );
 	}
 
 	public function test_validate_username() {
-		$this->assertTrue( give_validate_username( 'easydigitaldownloads' ) );
+		$this->assertTrue( give_validate_username( 'giveuser' ) );
 		$this->assertFalse( give_validate_username( 'give12345$%&+-!@£%^&()(*&^%$£@!' ) );
 	}
 }
