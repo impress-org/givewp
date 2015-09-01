5f982bb9c95c489770232168a60cfdf10a6c9b32
Matt Cromwell <reachme@mattcromwell.com>
2015-09-01 10:31:46 -0700

    Updated readme.txt and updated version number

--- give.php
+++ give.php
@@ -5,7 +5,7 @@
  * Description: The most robust, flexible, and intuitive way to accept donations on WordPress.
  * Author: WordImpress
  * Author URI: http://wordimpress.com
- * Version: 1.1
+ * Version: 1.2
  * Text Domain: give
  * Domain Path: /languages
  *
