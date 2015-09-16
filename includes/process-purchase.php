7609786e3fbf021f629831d50cc10d549b4a3378
devin <devin@wordimpress.com>
2015-09-15 18:52:41 -0700

    *Added addition 'give_pre_process_purchase' hook *Code readability

--- includes/process-purchase.php
+++ includes/process-purchase.php
@@ -25,6 +25,8 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 function give_process_purchase_form() {
 
+	do_action( 'give_pre_process_purchase' );
+
 	// Validate the form $_POST data
 	$valid_data = give_purchase_form_validate_fields();
 
@@ -92,7 +94,11 @@ function give_process_purchase_form() {
 	}
 
 	// Allow the purchase data to be modified before it is sent to the gateway
-	$purchase_data = apply_filters( 'give_purchase_data_before_gateway', $purchase_data, $valid_data );
+	$purchase_data = apply_filters(
+		'give_purchase_data_before_gateway',
+		$purchase_data,
+		$valid_data
+	);
 
 	// Setup the data we're storing in the purchase session
 	$session_data = $purchase_data;
