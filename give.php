f0e9c137f5faa8b4c9f46a94db14eb0ce19fbea5
devin <devin@wordimpress.com>
2015-10-01 13:11:41 -0700

    Version bump

--- give.php
+++ give.php
@@ -5,7 +5,7 @@
  * Description: The most robust, flexible, and intuitive way to accept donations on WordPress.
  * Author: WordImpress
  * Author URI: http://wordimpress.com
- * Version: 1.3.0.2
+ * Version: 1.3.0.3
  * Text Domain: give
  * Domain Path: /languages
  *
