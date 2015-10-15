201ee22cb47f9f77f14dad2bad66805498d1af42
devin <devin@wordimpress.com>
2015-10-15 12:15:39 -0700

    Take into account the new form id so pages with multiple forms on page load appropriate gateway based on form ID #337

--- includes/gateways/functions.php
+++ includes/gateways/functions.php
@@ -239,7 +239,7 @@ function give_send_to_gateway( $gateway, $payment_data ) {
  */
 function give_get_chosen_gateway( $form_id ) {
 	$gateways        = give_get_enabled_payment_gateways();
-	$chosen          = isset( $_REQUEST['payment-mode'] ) ? $_REQUEST['payment-mode'] : give_get_default_gateway( $form_id );
+	$chosen          = (isset( $_REQUEST['payment-mode'] ) && isset( $_REQUEST['form-id'] ) && $form_id == $_REQUEST['form-id']) ? $_REQUEST['payment-mode'] : give_get_default_gateway( $form_id );
 	$enabled_gateway = '';
 
 	if ( $chosen ) {
