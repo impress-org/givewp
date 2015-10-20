816a6df5cc8e1fab77d38daf429c65e665012bb7
Matt Cromwell <reachme@mattcromwell.com>
2015-10-20 15:53:04 -0700

    Regarding issue #332 swapped out "p" tags for "div's". Added very subtle layout styles to keep the login form looking consistent.

--- templates/shortcode-login.php
+++ templates/shortcode-login.php
@@ -11,26 +11,25 @@ if ( ! is_user_logged_in() ) :
 		<fieldset>
 			<legend><?php _e( 'Log into Your Account', 'give' ); ?></legend>
 			<?php do_action( 'give_login_fields_before' ); ?>
-			<p>
+			<div class="give-login-username give-login">
 				<label for="give_user_login"><?php _e( 'Username', 'give' ); ?></label>
 				<input name="give_user_login" id="give_user_login" class="give-required give-input" type="text" title="<?php _e( 'Username', 'give' ); ?>" />
-			</p>
-
-			<p>
+			</div>
+			<div class="give-login-password give-login">
 				<label for="give_user_pass"><?php _e( 'Password', 'give' ); ?></label>
 				<input name="give_user_pass" id="give_user_pass" class="give-password give-required give-input" type="password" />
-			</p>
+			</div>
 
-			<p>
+			<div class="give-login-submit give-login">
 				<input type="hidden" name="give_redirect" value="<?php echo esc_url( $give_login_redirect ); ?>" />
 				<input type="hidden" name="give_login_nonce" value="<?php echo wp_create_nonce( 'give-login-nonce' ); ?>" />
 				<input type="hidden" name="give_action" value="user_login" />
 				<input id="give_login_submit" type="submit" class="give_submit" value="<?php _e( 'Log In', 'give' ); ?>" />
-			</p>
+			</div>
 
-			<p class="give-lost-password">
+			<div class="give-lost-password give-login">
 				<a href="<?php echo wp_lostpassword_url(); ?>" title="<?php _e( 'Lost Password', 'give' ); ?>"><?php _e( 'Lost Password?', 'give' ); ?></a>
-			</p>
+			</div>
 			<?php do_action( 'give_login_fields_after' ); ?>
 		</fieldset>
 	</form>
