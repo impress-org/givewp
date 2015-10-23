d717945b1b89a1a1f8f613d0a6a06c4836d21881
devin <devin@wordimpress.com>
2015-10-23 11:12:03 -0700

    internationalized string

--- includes/forms/template.php
+++ includes/forms/template.php
@@ -381,8 +381,8 @@ function give_output_levels( $form_id ) {
 
 		case 'dropdown':
 
-			$output .='<label for="give-donation-level" class="give-hidden">Choose your Donation Amount:</label>';
-			$output .='<select id="give-donation-level-' . $form_id . '" class="give-select give-select-level">';
+			$output .= '<label for="give-donation-level" class="give-hidden">' . __( 'Choose your Donation Amount', 'give' ) . ':</label>';
+			$output .= '<select id="give-donation-level-' . $form_id . '" class="give-select give-select-level">';
 
 			//first loop through prices
 			foreach ( $prices as $price ) {
