d985992c253d953fe69e3e844dad69a18d69606b
devin <devin@wordimpress.com>
2015-12-10 12:55:26 -0800

    New `give_can_view_receipt` function

--- includes/misc-functions.php
+++ includes/misc-functions.php
@@ -676,117 +676,166 @@ add_filter( 'nav_menu_meta_box_object', 'modify_nav_menu_meta_box_object' );
 
 
 /**
- *
  * Array_column backup usage
  *
  * @desctipiont: This file is part of the array_column library
- * @since: 1.3.0.1
+ * @since      : 1.3.0.1
  *
- * @copyright Copyright (c) Ben Ramsey (http://benramsey.com)
- * @license http://opensource.org/licenses/MIT MIT
+ * @copyright  Copyright (c) Ben Ramsey (http://benramsey.com)
+ * @license    http://opensource.org/licenses/MIT MIT
  */
 
-if (!function_exists('array_column')) {
-    /**
-     * Returns the values from a single column of the input array, identified by
-     * the $columnKey.
-     *
-     * Optionally, you may provide an $indexKey to index the values in the returned
-     * array by the values from the $indexKey column in the input array.
-     *
-     * @param array $input A multi-dimensional array (record set) from which to pull
-     *                     a column of values.
-     * @param mixed $columnKey The column of values to return. This value may be the
-     *                         integer key of the column you wish to retrieve, or it
-     *                         may be the string key name for an associative array.
-     * @param mixed $indexKey (Optional.) The column to use as the index/keys for
-     *                        the returned array. This value may be the integer key
-     *                        of the column, or it may be the string key name.
-     * @return array
-     */
-    function array_column($input = null, $columnKey = null, $indexKey = null)
-    {
-        // Using func_get_args() in order to check for proper number of
-        // parameters and trigger errors exactly as the built-in array_column()
-        // does in PHP 5.5.
-        $argc = func_num_args();
-        $params = func_get_args();
+if ( ! function_exists( 'array_column' ) ) {
+	/**
+	 * Returns the values from a single column of the input array, identified by
+	 * the $columnKey.
+	 *
+	 * Optionally, you may provide an $indexKey to index the values in the returned
+	 * array by the values from the $indexKey column in the input array.
+	 *
+	 * @param array $input     A multi-dimensional array (record set) from which to pull
+	 *                         a column of values.
+	 * @param mixed $columnKey The column of values to return. This value may be the
+	 *                         integer key of the column you wish to retrieve, or it
+	 *                         may be the string key name for an associative array.
+	 * @param mixed $indexKey  (Optional.) The column to use as the index/keys for
+	 *                         the returned array. This value may be the integer key
+	 *                         of the column, or it may be the string key name.
+	 *
+	 * @return array
+	 */
+	function array_column( $input = null, $columnKey = null, $indexKey = null ) {
+		// Using func_get_args() in order to check for proper number of
+		// parameters and trigger errors exactly as the built-in array_column()
+		// does in PHP 5.5.
+		$argc   = func_num_args();
+		$params = func_get_args();
 
-        if ($argc < 2) {
-            trigger_error("array_column() expects at least 2 parameters, {$argc} given", E_USER_WARNING);
-            return null;
-        }
+		if ( $argc < 2 ) {
+			trigger_error( "array_column() expects at least 2 parameters, {$argc} given", E_USER_WARNING );
 
-        if (!is_array($params[0])) {
-            trigger_error(
-                'array_column() expects parameter 1 to be array, ' . gettype($params[0]) . ' given',
-                E_USER_WARNING
-            );
-            return null;
-        }
+			return null;
+		}
 
-        if (!is_int($params[1])
-            && !is_float($params[1])
-            && !is_string($params[1])
-            && $params[1] !== null
-            && !(is_object($params[1]) && method_exists($params[1], '__toString'))
-        ) {
-            trigger_error('array_column(): The column key should be either a string or an integer', E_USER_WARNING);
-            return false;
-        }
+		if ( ! is_array( $params[0] ) ) {
+			trigger_error(
+				'array_column() expects parameter 1 to be array, ' . gettype( $params[0] ) . ' given',
+				E_USER_WARNING
+			);
 
-        if (isset($params[2])
-            && !is_int($params[2])
-            && !is_float($params[2])
-            && !is_string($params[2])
-            && !(is_object($params[2]) && method_exists($params[2], '__toString'))
-        ) {
-            trigger_error('array_column(): The index key should be either a string or an integer', E_USER_WARNING);
-            return false;
-        }
+			return null;
+		}
 
-        $paramsInput = $params[0];
-        $paramsColumnKey = ($params[1] !== null) ? (string) $params[1] : null;
+		if ( ! is_int( $params[1] )
+		     && ! is_float( $params[1] )
+		     && ! is_string( $params[1] )
+		     && $params[1] !== null
+		     && ! ( is_object( $params[1] ) && method_exists( $params[1], '__toString' ) )
+		) {
+			trigger_error( 'array_column(): The column key should be either a string or an integer', E_USER_WARNING );
 
-        $paramsIndexKey = null;
-        if (isset($params[2])) {
-            if (is_float($params[2]) || is_int($params[2])) {
-                $paramsIndexKey = (int) $params[2];
-            } else {
-                $paramsIndexKey = (string) $params[2];
-            }
-        }
+			return false;
+		}
 
-        $resultArray = array();
+		if ( isset( $params[2] )
+		     && ! is_int( $params[2] )
+		     && ! is_float( $params[2] )
+		     && ! is_string( $params[2] )
+		     && ! ( is_object( $params[2] ) && method_exists( $params[2], '__toString' ) )
+		) {
+			trigger_error( 'array_column(): The index key should be either a string or an integer', E_USER_WARNING );
 
-        foreach ($paramsInput as $row) {
-            $key = $value = null;
-            $keySet = $valueSet = false;
+			return false;
+		}
 
-            if ($paramsIndexKey !== null && array_key_exists($paramsIndexKey, $row)) {
-                $keySet = true;
-                $key = (string) $row[$paramsIndexKey];
-            }
+		$paramsInput     = $params[0];
+		$paramsColumnKey = ( $params[1] !== null ) ? (string) $params[1] : null;
 
-            if ($paramsColumnKey === null) {
-                $valueSet = true;
-                $value = $row;
-            } elseif (is_array($row) && array_key_exists($paramsColumnKey, $row)) {
-                $valueSet = true;
-                $value = $row[$paramsColumnKey];
-            }
+		$paramsIndexKey = null;
+		if ( isset( $params[2] ) ) {
+			if ( is_float( $params[2] ) || is_int( $params[2] ) ) {
+				$paramsIndexKey = (int) $params[2];
+			} else {
+				$paramsIndexKey = (string) $params[2];
+			}
+		}
 
-            if ($valueSet) {
-                if ($keySet) {
-                    $resultArray[$key] = $value;
-                } else {
-                    $resultArray[] = $value;
-                }
-            }
+		$resultArray = array();
 
-        }
+		foreach ( $paramsInput as $row ) {
+			$key    = $value = null;
+			$keySet = $valueSet = false;
 
-        return $resultArray;
-    }
+			if ( $paramsIndexKey !== null && array_key_exists( $paramsIndexKey, $row ) ) {
+				$keySet = true;
+				$key    = (string) $row[ $paramsIndexKey ];
+			}
+
+			if ( $paramsColumnKey === null ) {
+				$valueSet = true;
+				$value    = $row;
+			} elseif ( is_array( $row ) && array_key_exists( $paramsColumnKey, $row ) ) {
+				$valueSet = true;
+				$value    = $row[ $paramsColumnKey ];
+			}
+
+			if ( $valueSet ) {
+				if ( $keySet ) {
+					$resultArray[ $key ] = $value;
+				} else {
+					$resultArray[] = $value;
+				}
+			}
+
+		}
+
+		return $resultArray;
+	}
+
+}
+
+/**
+ * Determines the receipt visibility status
+ *
+ * @since 1.3.2
+ *
+ * @param string $payment_key
+ *
+ * @return bool Whether the receipt is visible or not.
+ */
+function give_can_view_receipt( $payment_key = '' ) {
+
+	$return = false;
+
+	if ( empty( $payment_key ) ) {
+		return $return;
+	}
+
+	global $give_receipt_args;
+
+	$give_receipt_args['id'] = give_get_purchase_id_by_key( $payment_key );
+
+	$user_id = (int) give_get_payment_user_id( $give_receipt_args['id'] );
+
+	$payment_meta = give_get_payment_meta( $give_receipt_args['id'] );
+
+	if ( is_user_logged_in() ) {
+		if ( $user_id === (int) get_current_user_id() ) {
+			$return = true;
+		} elseif ( wp_get_current_user()->user_email === give_get_payment_user_email( $give_receipt_args['id'] ) ) {
+			$return = true;
+		} elseif ( current_user_can( 'view_give_sensitive_data' ) ) {
+			$return = true;
+		}
+	}
+
+	$session = give_get_purchase_session();
+	if ( ! empty( $session ) && ! is_user_logged_in() ) {
+		if ( $session['purchase_key'] === $payment_meta['key'] ) {
+			$return = true;
+		}
+	}
+
+	return (bool) apply_filters( 'give_can_view_receipt', $return, $payment_key );
 
 }
