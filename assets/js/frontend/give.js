ba25ab576effbaf498f78bc6f1e0fd85bd0acbee
Devin Walker <devin@wordimpress.com>
2016-03-23 11:14:39 -0700

    Adjusted modal z-index values to be above increased z-index in #524
    Also adjusted the multi-level select field to be display block so modal button displays on separate line

--- assets/js/frontend/give.js
+++ assets/js/frontend/give.js
@@ -11,114 +11,114 @@
 
 var give_scripts;
 
-jQuery( function ( $ ) {
+jQuery(function ($) {
 
-	var doc = $( document );
+    var doc = $(document);
 
-	//Float Labels
-	var options = {
-		exclude     : ['#give-amount, .give-select-level, .multiselect, .give-repeater-table input, input[type="url"]'],
-		customEvent : give_fl_custom_events
-	};
-	$( '.float-labels-enabled' ).floatlabels( options );
+    //Float Labels
+    var options = {
+        exclude: ['#give-amount, .give-select-level, .multiselect, .give-repeater-table input, input[type="url"]'],
+        customEvent: give_fl_custom_events
+    };
+    $('.float-labels-enabled').floatlabels(options);
 
-	doc.on( 'give_gateway_loaded', function ( ev, response, form_id ) {
+    doc.on('give_gateway_loaded', function (ev, response, form_id) {
 
-		var form = $( 'form#' + form_id );
+        var form = $('form#' + form_id);
 
-		if ( form.hasClass( 'float-labels-enabled' ) ) {
-			form.floatlabels( options );
-		}
-	} );
+        if (form.hasClass('float-labels-enabled')) {
+            form.floatlabels(options);
+        }
+    });
 
-	doc.on( 'give_checkout_billing_address_updated', function ( ev, response, form_id ) {
+    doc.on('give_checkout_billing_address_updated', function (ev, response, form_id) {
 
-		var form  = $( 'form#' + form_id );
+        var form = $('form#' + form_id);
 
-		if ( form.hasClass( 'float-labels-enabled' ) ) {
+        if (form.hasClass('float-labels-enabled')) {
 
-			var wrap  = form.find( '#give-card-state-wrap' );
-			var el    = wrap.find( '#card_state' );
-			var label = wrap.find( 'label[for="card_state"]' );
+            var wrap = form.find('#give-card-state-wrap');
+            var el = wrap.find('#card_state');
+            var label = wrap.find('label[for="card_state"]');
 
-			label = label.length ? label.text().replace( /[*:]/g, '' ).trim() : '';
+            label = label.length ? label.text().replace(/[*:]/g, '').trim() : '';
 
-			if ( 'nostates' === response ) {
-				// fix input
-				el.attr( 'placeholder', label ).parent().removeClass( 'styled select' );
-			} else {
-				// fix select
-				el.children().first().text( label );
-				el.parent().addClass( 'styled select' );
-			}
+            if ('nostates' === response) {
+                // fix input
+                el.attr('placeholder', label).parent().removeClass('styled select');
+            } else {
+                // fix select
+                el.children().first().text(label);
+                el.parent().addClass('styled select');
+            }
 
-			el.parent().removeClass( 'is-active' );
+            el.parent().removeClass('is-active');
 
-			form.floatlabels( options );
-		}
-	} );
+            form.floatlabels(options);
+        }
+    });
 
-	// Reveal Btn which displays the checkout content
-	doc.on( 'click', '.give-btn-reveal', function ( e ) {
-		e.preventDefault();
-		var this_button = $( this );
-		var this_form = $( this ).parents( 'form' );
-		this_button.hide();
-		this_form.find( '#give-payment-mode-select, #give_purchase_form_wrap' ).slideDown();
-		return false;
-	} );
+    // Reveal Btn which displays the checkout content
+    doc.on('click', '.give-btn-reveal', function (e) {
+        e.preventDefault();
+        var this_button = $(this);
+        var this_form = $(this).parents('form');
+        this_button.hide();
+        this_form.find('#give-payment-mode-select, #give_purchase_form_wrap').slideDown();
+        return false;
+    });
 
-	// Modal with Magnific
-	doc.on( 'click', '.give-btn-modal', function ( e ) {
-		e.preventDefault();
-		var this_form_wrap = $( this ).parents( 'div.give-form-wrap' );
-		var this_form = this_form_wrap.find( 'form.give-form' );
-		var this_amount_field = this_form.find( '#give-amount' );
-		var this_amount = this_amount_field.val();
-		//Check to ensure our amount is greater than 0
+    // Modal with Magnific
+    doc.on('click', '.give-btn-modal', function (e) {
+        e.preventDefault();
+        var this_form_wrap = $(this).parents('div.give-form-wrap');
+        var this_form = this_form_wrap.find('form.give-form');
+        var this_amount_field = this_form.find('#give-amount');
+        var this_amount = this_amount_field.val();
+        //Check to ensure our amount is greater than 0
 
-		//Does this number have a value
-		if ( !this_amount || this_amount <= 0 ) {
-			this_amount_field.focus();
-			return false;
-		}
+        //Does this number have a value
+        if (!this_amount || this_amount <= 0) {
+            this_amount_field.focus();
+            return false;
+        }
 
-		//Alls well, open popup!
-		$.magnificPopup.open( {
-			mainClass   : 'give-modal',
-			items       : {
-				src : this_form,
-				type: 'inline'
-			},
-			callbacks   : {
-				open : function () {
-					// Will fire when this exact popup is opened
-					// this - is Magnific Popup object
-					if ( $( '.mfp-content' ).outerWidth() >= 500 ) {
-						$( '.mfp-content' ).addClass( 'give-responsive-mfp-content' );
-					}
-					//Hide all form elements besides the ones required for payment
-					this_form.children().not( '#give_purchase_form_wrap, #give-payment-mode-select, .mfp-close' ).hide();
+        //Alls well, open popup!
+        $.magnificPopup.open({
+            mainClass: 'give-modal',
+            items: {
+                src: this_form,
+                type: 'inline'
+            },
+            callbacks: {
+                open: function () {
+                    // Will fire when this exact popup is opened
+                    // this - is Magnific Popup object
+                    if ($('.mfp-content').outerWidth() >= 500) {
+                        $('.mfp-content').addClass('give-responsive-mfp-content');
+                    }
+                    //Hide all form elements besides the ones required for payment
+                    this_form.children().not('#give_purchase_form_wrap, #give-payment-mode-select, .mfp-close').hide();
 
-				},
-				close: function () {
-					//Remove popup class
-					this_form.removeClass( 'mfp-hide' );
-					//Show all fields again
-					this_form.children().not( '#give_purchase_form_wrap, #give-payment-mode-select, .mfp-close' ).show();
-				}
-			}
-		} );
-	} );
+                },
+                close: function () {
+                    //Remove popup class
+                    this_form.removeClass('mfp-hide');
+                    //Show all fields again
+                    this_form.children().not('#give_purchase_form_wrap, #give-payment-mode-select, .mfp-close').show();
+                }
+            }
+        });
+    });
 
-} );
+});
 
 /**
  * Floating Labels Custom Events
  * @param el
  */
-function give_fl_custom_events( el ) {
-	if ( el.hasClass( 'card-number' ) ) {
-		el.after( '<span class="off card-type"/>' );
-	}
+function give_fl_custom_events(el) {
+    if (el.hasClass('card-number')) {
+        el.after('<span class="off card-type"/>');
+    }
 }
