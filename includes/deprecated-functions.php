beae3d6bba9d1b37d895932f7e67f02efe00cd95
Devin Walker <devin@wordimpress.com>
2016-04-07 21:18:52 -0700

    Deprecate give_no_guest_checkout in favor of give_logged_in_only()

--- includes/deprecated-functions.php
+++ includes/deprecated-functions.php
@@ -15,3 +15,26 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
+
+
+/**
+ * Checks if Guest checkout is enabled for a particular donation form
+ *
+ * @since 1.0
+ * @deprecated 1.4.1
+ * @global    $give_options
+ *
+ * @param int $form_id
+ *
+ * @return bool $ret True if guest checkout is enabled, false otherwise
+ */
+function give_no_guest_checkout( $form_id ) {
+
+	$backtrace = debug_backtrace();
+
+	_give_deprecated_function( __FUNCTION__, '1.4.1', null, $backtrace );
+
+	$ret = get_post_meta( $form_id, '_give_logged_in_only', true );
+
+	return (bool) apply_filters( 'give_no_guest_checkout', $ret );
+}
--- includes/process-purchase.php
+++ includes/process-purchase.php
@@ -499,11 +499,8 @@ function give_purchase_form_validate_new_user() {
 			// All the checks have run and it's good to go
 			$valid_user_data['user_login'] = $user_login;
 		}
-	} else {
-		if ( give_no_guest_checkout( $form_id ) ) {
-
-			give_set_error( 'registration_required', esc_html__( 'You must register or login to complete your donation', 'give' ) );
-		}
+	} elseif ( give_logged_in_only( $form_id ) ) {
+		give_set_error( 'registration_required', esc_html__( 'You must register or login to complete your donation', 'give' ) );
 	}
 
 	// Check if we have an email to verify
@@ -763,7 +760,7 @@ function give_get_purchase_form_user( $valid_data = array() ) {
 	}
 
 	// Check guest checkout
-	if ( false === $user && false === give_no_guest_checkout( $_POST['give-form-id'] ) ) {
+	if ( false === $user && false === give_logged_in_only( $_POST['give-form-id'] ) ) {
 		// Set user
 		$user = $valid_data['guest_user_data'];
 	}
