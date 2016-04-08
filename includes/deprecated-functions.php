deeb765d790fc6b616dc77a3a86b8cde7b2342a3
Devin Walker <devin@wordimpress.com>
2016-04-07 20:45:12 -0700

    New deprecated functions file in preparation to deprecate give_can_checkout

---
+++ includes/deprecated-functions.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Deprecated Functions
+ *
+ * @description: All functions that have been deprecated.
+ *
+ * @package     Give
+ * @subpackage  Deprecated
+ * @copyright   Copyright (c) 2016, WordImpress
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       1.4.1
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
