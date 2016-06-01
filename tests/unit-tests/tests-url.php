5796ad390be92e3f2408c722d676e24b87f76144
Devin Walker <devin@wordimpress.com>
2016-06-01 14:54:09 -0700

    Fixed unit test

--- tests/unit-tests/tests-url.php
+++ tests/unit-tests/tests-url.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @group give_url
  */
@@ -13,7 +14,7 @@ class Tests_URL extends Give_Unit_Test_Case {
 
 	public function test_ajax_url() {
 		$_SERVER['SERVER_PORT'] = 80;
-		$_SERVER['HTTPS'] = 'off';
+		$_SERVER['HTTPS']       = 'off';
 
 		$this->assertEquals( give_get_ajax_url(), get_site_url( null, '/wp-admin/admin-ajax.php', 'http' ) );
 	}
@@ -21,6 +22,6 @@ class Tests_URL extends Give_Unit_Test_Case {
 	public function test_current_page_url() {
 		$_SERVER['SERVER_PORT'] = 80;
 		$_SERVER["SERVER_NAME"] = 'example.org';
-		$this->assertEquals( 'http://example.org/', give_get_current_page_url() );
+		$this->assertEquals( 'http://example.org', give_get_current_page_url() );
 	}
 }
