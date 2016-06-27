4bbeb3c7f2f6111d01aa8942c81b51801b563196
Devin Walker <devin@wordimpress.com>
2016-06-27 11:34:30 -0700

    Fixed error messages

--- includes/admin/reporting/tools/class-give-tools-recount-single-customer-stats.php
+++ includes/admin/reporting/tools/class-give-tools-recount-single-customer-stats.php
@@ -149,7 +149,7 @@ class Give_Tools_Recount_Single_Customer_Stats extends Give_Batch_Export {
 	public function process_step() {
 
 		if ( ! $this->can_export() ) {
-			wp_die( __( 'You do not have permission to modify this data.', 'give' ), __( 'Error', 'give' ), array( 'response' => 403 ) );
+			wp_die( __( 'You do not have permission to recount stats.', 'give' ), __( 'Error', 'give' ), array( 'response' => 403 ) );
 		}
 
 		$had_data = $this->get_data();
