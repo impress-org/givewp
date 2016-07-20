3bf9cb756ba6c1d3d61c628a23cb64cb338c6b17
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2016-07-20 14:54:16 +0530

    Auto select radio/button/dropdown variable level for multidonation form on bsis of amount

--- assets/js/frontend/give-checkout-global.js
+++ assets/js/frontend/give-checkout-global.js
@@ -370,13 +370,30 @@ jQuery(function ($) {
                 $('input[name="give-price-id"]', parent_form ).val( price_id );
             }
 
-            // Auto Select radio button if radio button is using fro variable prices.
-            if( parent_form.find('.give-radio-input').length ) {
-                parent_form.find('.give-radio-input').prop('checked', false); //Radio
-                parent_form.find('.give-radio-input[data-price-id="'+price_id+'"]').prop('checked', true); //Radio
+            // Auto select variable price items ( Radio/Button/Select ).
+            switch ( true ){
+
+                // Auto select radio button.
+                case ( !! parent_form.find('.give-radio-input').length ) :
+                    parent_form.find('.give-radio-input').prop('checked', false);
+                    parent_form.find('.give-radio-input[data-price-id="'+price_id+'"]').prop('checked', true);
+                    break;
+
+                // Set focus to price id button.
+                case ( !! parent_form.find('button.give-donation-level-btn').length ) :
+                    parent_form.find('button.give-donation-level-btn').blur();
+                    parent_form.find('button.give-donation-level-btn[data-price-id="'+price_id+'"]').focus();
+                    break;
+
+                // Auto select option.
+                case ( !! parent_form.find('select.give-select-level').length ) :
+                    parent_form.find('select.give-select-level option').prop( 'selected', false );
+                    parent_form.find('select.give-select-level option[data-price-id="'+price_id+'"]').prop( 'selected', true );
+                    break;
+
             }
         }
-
+        
         //This class is used for CSS purposes
         $(this).parent('.give-donation-amount').removeClass('give-custom-amount-focus-in');
 
