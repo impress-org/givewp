f0c9c4b44f4e521fdc48b6fbbc8824f035485bd9
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2016-08-02 07:39:38 +0530

    Add give_is_license_valid and give_is_license_expired filter

--- includes/class-give-license-handler.php
+++ includes/class-give-license-handler.php
@@ -521,7 +521,7 @@ if ( ! class_exists( 'Give_License' ) ) :
          * @return bool
          */
 		public function is_valid_license() {
-            if( ( is_object( $this->license_data ) && ! empty( $this->license_data ) && 'valid' === $this->license_data->license ) ) {
+            if( apply_filters( 'give_is_license_valid' , ( is_object( $this->license_data ) && ! empty( $this->license_data ) && 'valid' === $this->license_data->license ) ) ) {
                 return true;
             }
 
@@ -534,7 +534,7 @@ if ( ! class_exists( 'Give_License' ) ) :
          * @return bool
          */
         public function is_expired_license() {
-            if( ( ! $this->is_valid_license() && is_object( $this->license_data ) && ! empty( $this->license_data ) && ( 'expired' == $this->license_data->license ||  'expired' == $this->license_data->error ) ) ) {
+            if( apply_filters( 'give_is_license_expired', ( ! $this->is_valid_license() && is_object( $this->license_data ) && ! empty( $this->license_data ) && ( 'expired' == $this->license_data->license ||  'expired' == $this->license_data->error ) ) ) ) {
                 return true;
             }
 
