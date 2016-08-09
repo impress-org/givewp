1e70a6dbe974c50e5b63e110692133dc21e418bb
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2016-08-09 13:26:02 +0530

    Add feature to dismiss license notice by 24 hour

--- includes/admin/admin-actions.php
+++ includes/admin/admin-actions.php
@@ -52,7 +52,7 @@ function give_hide_subscription_notices() {
             : array();
 
         // Get notice id.
-        $notice_id = absint( $_GET['_give_hide_license_notices_permanently'] );
+        $notice_id = sanitize_text_field( $_GET['_give_hide_license_notices_permanently'] );
 
         if( ! in_array( $notice_id, $already_dismiss_notices ) ) {
             $already_dismiss_notices[] = $notice_id;
@@ -63,6 +63,29 @@ function give_hide_subscription_notices() {
 
         // Redirect user.
         wp_safe_redirect( remove_query_arg( '_give_hide_license_notices_permanently', $_SERVER['REQUEST_URI'] ) );
+        exit();
+    }
+
+    // Hide subscription notices shortly.
+    if ( ! empty( $_GET['_give_hide_license_notices_shortly'] ) ) {
+        global $current_user;
+
+        // Get notice id.
+        $notice_id = sanitize_text_field( $_GET['_give_hide_license_notices_shortly'] );
+
+        // Transient key name.
+        $transient_key = "_give_hide_license_notices_shortly_{$current_user->ID}_{$notice_id}";
+
+        if( get_transient( $transient_key ) ) {
+            return;
+        }
+
+
+        // Hide notice for 24 hours.
+        set_transient( $transient_key, true, 24 * HOUR_IN_SECONDS );
+
+        // Redirect user.
+        wp_safe_redirect( remove_query_arg( '_give_hide_license_notices_shortly', $_SERVER['REQUEST_URI'] ) );
         exit();
     }
 }
