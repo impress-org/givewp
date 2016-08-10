bb43fd2eda3ef6e9090abc98f6ee112c1f4b1b9f
Devin Walker <devin@wordimpress.com>
2016-08-10 14:29:37 -0700

    Code reformat

--- assets/js/admin/tinymce/mce-plugin.js
+++ assets/js/admin/tinymce/mce-plugin.js
@@ -1,13 +1,13 @@
-( function( tinymce ) {
+(function (tinymce) {
 
-	tinymce.PluginManager.add( 'give_shortcode', function( editor )
-	{
-		editor.addCommand( 'Give_Shortcode', function()
-		{
-			if( window.scForm ) {
-				window.scForm.open( editor.id );
-			}
-		});
-	});
+    tinymce.PluginManager.add('give_shortcode', function (editor) {
+        editor.addCommand('Give_Shortcode', function () {
 
-})( window.tinymce );
+            if (window.scForm) {
+
+                window.scForm.open(editor.id);
+            }
+        });
+    });
+
+})(window.tinymce);
