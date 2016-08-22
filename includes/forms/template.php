c7964a2d3ee9d40198fa33ed212cb97b9a7b4b39
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2016-08-22 17:31:44 +0530

    Move action hook give_purchase_form_user_info --> give_donation_form_user_info

--- includes/forms/template.php
+++ includes/forms/template.php
@@ -804,9 +804,16 @@ function give_user_info_fields( $form_id ) {
 		 *
 		 * @since 1.0
 		 *
-		 * @param int $form_id The form ID.
+		 * @deprecated 1.7 Use { @see 'give_donation_form_user_info' } instead.
 		 */
-		do_action( 'give_purchase_form_user_info', $form_id );
+		do_action_deprecated( 'give_purchase_form_user_info', array( $form_id ), '1.7', 'give_donation_form_user_info' );
+
+		/**
+		 * Fire after personal email field
+		 *
+		 * @since 1.7
+		 */
+		do_action( 'give_donation_form_user_info', $form_id );
 		?>
 	</fieldset>
 	<?php
