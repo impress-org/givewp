081d065ca0ca0947c196d976f5073ac62234eda4
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2016-08-22 14:13:25 +0530

    Move action hook give_purchase_form_after_cc_form --> give_donation_form_after_cc_form

--- includes/forms/template.php
+++ includes/forms/template.php
@@ -318,9 +318,16 @@ function give_show_purchase_form( $form_id ) {
 		 *
 		 * @since 1.0
 		 *
-		 * @param int $form_id The form ID.
+		 * @deprecated 1.7 Use {@see 'give_donation_form_after_cc_form'} instead.
 		 */
-		do_action( 'give_purchase_form_after_cc_form', $form_id );
+		do_action_deprecated( 'give_purchase_form_after_cc_form', array( $form_id ), '1.7', 'give_donation_form_after_cc_form' );
+
+		/**
+		 * Fire after credit card form fields render.
+		 *
+		 * @since 1.7
+		 */
+		do_action( 'give_donation_form_after_cc_form', $form_id );
 
 	} else {
 		/**
