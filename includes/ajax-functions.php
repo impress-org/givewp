298d0c653603be53432829b1a98fb4746b1bce86
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2016-08-22 17:55:39 +0530

    Move action hook give_purchase_form_register_login_fields --> give_donation_form_register_login_fields

--- includes/ajax-functions.php
+++ includes/ajax-functions.php
@@ -159,9 +159,16 @@ function give_load_checkout_fields() {
 	 *
 	 * @since 1.3.6
 	 *
-	 * @param int $form_id The form ID.
+	 * @deprecated 1.7 Use {@see 'give_donation_form_register_login_fields'} instead.
 	 */
-	do_action( 'give_purchase_form_register_login_fields', $form_id );
+	do_action_deprecated( 'give_purchase_form_register_login_fields', array( $form_id ), '1.7', 'give_donation_form_register_login_fields' );
+
+	/**
+	 * Fire to render registration/login form.
+	 *
+	 * @since 1.7
+	 */
+	do_action( 'give_donation_form_register_login_fields', $form_id );
 
 	$fields = ob_get_clean();
 
