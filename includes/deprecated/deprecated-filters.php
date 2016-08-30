bd57730cefe4b460c017de06871b48c5340f6546
Devin Walker <devin@wordimpress.com>
2016-08-30 11:52:27 -0700

    Fixed filter calls unto themselves caused by merge of #973

--- includes/deprecated/deprecated-filters.php
+++ includes/deprecated/deprecated-filters.php
@@ -13,9 +13,8 @@ $give_map_deprecated_filters = array(
 	'give_donation_from_address'                    => 'give_purchase_from_address',
 	'give_donation_labels'                          => 'give_payment_labels',
 	'give_donations_table_views'                    => 'give_payments_table_views',
-	'give_payments_table_columns'                   => 'give_payments_table_columns',
+	'give_donations_table_columns'                  => 'give_payments_table_columns',
 	'give_donations_table_sortable_columns'         => 'give_payments_table_sortable_columns',
-	'give_payments_table_column'                    => 'give_payments_table_column',
 	'give_donations_table_column'                   => 'give_payments_table_column',
 	'give_donation_row_actions'                     => 'give_payment_row_actions',
 	'give_donations_table_bulk_actions'             => 'give_payments_table_bulk_actions',
@@ -45,34 +44,43 @@ $give_map_deprecated_filters = array(
 	'give_recount_customer_donation_statuses'       => 'give_recount_customer_payment_statuses',
 	'give_customer_recount_should_process_donation' => 'give_customer_recount_should_process_payment',
 	'give_is_guest_donation'                        => 'give_is_guest_payment',
-
-
-
-
 );
 
 
 // Dynamic filters.
 switch ( true ) {
 	case ( ! empty( $_GET['payment-confirmation'] ) ) :
-		$give_map_deprecated_filters[ "give_donation_confirm_{$_GET['payment-confirmation']}" ] = "give_payment_confirm_{$_GET['payment-confirmation']}";
+		$give_map_deprecated_filters["give_donation_confirm_{$_GET['payment-confirmation']}"] = "give_payment_confirm_{$_GET['payment-confirmation']}";
 }
 
-foreach ( $give_map_deprecated_filters as $new => $old ){
+foreach ( $give_map_deprecated_filters as $new => $old ) {
 	add_filter( $new, 'give_deprecated_filter_mapping', 10, 4 );
 }
 
+/**
+ * Deprecated filter mapping.
+ * 
+ * @param        $data
+ * @param string $arg_1
+ * @param string $arg_2
+ * @param string $arg_3
+ *
+ * @return mixed|void
+ */
 function give_deprecated_filter_mapping( $data, $arg_1 = '', $arg_2 = '', $arg_3 = '' ) {
 	global $give_map_deprecated_filters;
 
 	$filter = current_filter();
 
-	if ( isset( $give_map_deprecated_filters[ $filter ] ) ){
+	if ( isset( $give_map_deprecated_filters[ $filter ] ) ) {
 		if ( has_filter( $give_map_deprecated_filters[ $filter ] ) ) {
+			echo '<pre>';
+			var_dump( $give_map_deprecated_filters[ $filter ] );
+			echo '</pre>';
 			$data = apply_filters( $give_map_deprecated_filters[ $filter ], $data, $arg_1, $arg_2, $arg_3 );
 
 			if ( ! defined( 'DOING_AJAX' ) ) {
-				_give_deprecated_function( 'The ' . $give_map_deprecated_filters[$filter] . ' filter', '1.7', $filter );
+				_give_deprecated_function( 'The ' . $give_map_deprecated_filters[ $filter ] . ' filter', '1.7', $filter );
 			}
 		}
 	}
