f75003718833e865675a5e19c3b38d91fa79c248
Rami Yushuvaev <r_a_m_i@hotmail.com>
2016-09-06 23:44:54 +0300

    Tests: add two more tests

--- tests/unit-tests/tests-post-types.php
+++ tests/unit-tests/tests-post-types.php
@@ -31,11 +31,12 @@ class Tests_Post_Types extends Give_Unit_Test_Case {
 		$this->assertEquals( 'Add Form', $wp_post_types['give_forms']->labels->add_new );
 		$this->assertEquals( 'Add New Donation Form', $wp_post_types['give_forms']->labels->add_new_item );
 		$this->assertEquals( 'Edit Donation Form', $wp_post_types['give_forms']->labels->edit_item );
+		$this->assertEquals( 'New Forms', $wp_post_types['give_forms']->labels->new_item );
+		$this->assertEquals( 'All Forms', $wp_post_types['give_forms']->labels->all_items );
 		$this->assertEquals( 'View Form', $wp_post_types['give_forms']->labels->view_item );
 		$this->assertEquals( 'Search Forms', $wp_post_types['give_forms']->labels->search_items );
 		$this->assertEquals( 'No forms found.', $wp_post_types['give_forms']->labels->not_found );
 		$this->assertEquals( 'No forms found in Trash.', $wp_post_types['give_forms']->labels->not_found_in_trash );
-		$this->assertEquals( 'All Forms', $wp_post_types['give_forms']->labels->all_items );
 		$this->assertEquals( 'Donations', $wp_post_types['give_forms']->labels->menu_name );
 		$this->assertEquals( 'Donation Form', $wp_post_types['give_forms']->labels->name_admin_bar );
 		$this->assertEquals( 1, $wp_post_types['give_forms']->publicly_queryable );
@@ -65,11 +66,12 @@ class Tests_Post_Types extends Give_Unit_Test_Case {
 		$this->assertEquals( 'Add New', $wp_post_types['give_payment']->labels->add_new );
 		$this->assertEquals( 'Add New Donation', $wp_post_types['give_payment']->labels->add_new_item );
 		$this->assertEquals( 'Edit Donation', $wp_post_types['give_payment']->labels->edit_item );
+		$this->assertEquals( 'New Donation', $wp_post_types['give_payment']->labels->new_item );
+		$this->assertEquals( 'All Donations', $wp_post_types['give_payment']->labels->all_items );
 		$this->assertEquals( 'View Donation', $wp_post_types['give_payment']->labels->view_item );
 		$this->assertEquals( 'Search Donations', $wp_post_types['give_payment']->labels->search_items );
 		$this->assertEquals( 'No donations found.', $wp_post_types['give_payment']->labels->not_found );
 		$this->assertEquals( 'No donations found in Trash.', $wp_post_types['give_payment']->labels->not_found_in_trash );
-		$this->assertEquals( 'All Donations', $wp_post_types['give_payment']->labels->all_items );
 		$this->assertEquals( 'Donations', $wp_post_types['give_payment']->labels->menu_name );
 		$this->assertEquals( 'Donation', $wp_post_types['give_payment']->labels->name_admin_bar );
 		$this->assertEquals( '', $wp_post_types['give_payment']->publicly_queryable );
