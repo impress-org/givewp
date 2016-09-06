d3d71a22a3fd83046d8a3178429d184f7b3716d2
Rami Yushuvaev <r_a_m_i@hotmail.com>
2016-09-07 01:43:49 +0300

    fix test

--- tests/unit-tests/tests-post-types.php
+++ tests/unit-tests/tests-post-types.php
@@ -31,7 +31,7 @@ class Tests_Post_Types extends Give_Unit_Test_Case {
 		$this->assertEquals( 'Add Form', $wp_post_types['give_forms']->labels->add_new );
 		$this->assertEquals( 'Add New Donation Form', $wp_post_types['give_forms']->labels->add_new_item );
 		$this->assertEquals( 'Edit Donation Form', $wp_post_types['give_forms']->labels->edit_item );
-		$this->assertEquals( 'New Forms', $wp_post_types['give_forms']->labels->new_item );
+		$this->assertEquals( 'New Form', $wp_post_types['give_forms']->labels->new_item );
 		$this->assertEquals( 'All Forms', $wp_post_types['give_forms']->labels->all_items );
 		$this->assertEquals( 'View Form', $wp_post_types['give_forms']->labels->view_item );
 		$this->assertEquals( 'Search Forms', $wp_post_types['give_forms']->labels->search_items );
