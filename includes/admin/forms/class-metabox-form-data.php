bc251a656de70200165b9230f2953d6d227c8ddc
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2016-09-14 15:07:06 +0530

    Add saving logic to form meta data

--- includes/admin/forms/class-metabox-form-data.php
+++ includes/admin/forms/class-metabox-form-data.php
@@ -40,14 +40,40 @@ class Give_MetaBox_Form_Data {
 		$this->metabox_id    = 'give-metabox-form-data';
 		$this->metabox_label = esc_html__( 'Donation Form Data', 'give' );
 
+		// Setup.
+		add_action( 'admin_init' , array( $this, 'setup' ) );
+
 		// Add metabox.
 		add_action( 'add_meta_boxes', array( $this, 'add_meta_box' ), 30 );
 
 		// Load required scripts.
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_script' ) );
+		
+		// Save form meta.
+		add_action( 'save_post_give_forms', array( $this, 'save' ), 10, 2 );
 
 		// cmb2 old setting loaders.
 		add_filter( 'give_metabox_form_data_settings', array( $this, 'cmb2_metabox_settings' ) );
+	}
+
+
+	/**
+	 * Setup metabox related data.
+	 */
+	function setup() {
+		$this->settings = $this->get_settings();
+	}
+
+
+	/**
+	 * Get metabox settings
+	 * @return mixed|void
+	 */
+	function get_settings() {
+		/**
+		 * Filter the metabox tabbed panel settings.
+		 */
+		return apply_filters( 'give_metabox_form_data_settings', array() );
 	}
 
 	/**
@@ -101,11 +127,6 @@ class Give_MetaBox_Form_Data {
 	 * @return mixed|void
 	 */
 	public function get_tabs() {
-		/**
-		 * Filter the metabox settings.
-		 */
-		$this->settings = apply_filters( 'give_metabox_form_data_settings', array() );
-
 		$tabs = array();
 
 		if( ! empty( $this->settings ) ) {
@@ -129,7 +150,7 @@ class Give_MetaBox_Form_Data {
 	public function output() {
 		// Bailout.
 		if( $form_data_tabs = $this->get_tabs() ) {
-			wp_nonce_field( 'give_save_form_meta', 'give_meta_nonce' );
+			wp_nonce_field( 'give_save_form_meta', 'give_form_meta_nonce' );
 			?>
 			<div class="panel-wrap form_data">
 				<ul class="form_data_tabs give-metabox-tabs">
@@ -158,13 +179,129 @@ class Give_MetaBox_Form_Data {
 	}
 
 	/**
-	 * CMB2 setting tab loader.
+	 * CMB2 settings loader.
 	 *
 	 * @return mixed|void
 	 */
 	function cmb2_metabox_settings() {
-		return apply_filters( 'cmb2_meta_boxes', array() );
+		$all_cmb2_settings = apply_filters( 'cmb2_meta_boxes', array() );
+		$give_forms_settings = $all_cmb2_settings;
 
+
+		// Filter settings: Use only give forms related settings.
+		foreach ( $all_cmb2_settings as $index => $setting ) {
+			if( ! in_array( 'give_forms', $setting['object_types'] ) ) {
+				unset( $give_forms_settings[$index] );
+			}
+		}
+		return $give_forms_settings;
+
+	}
+
+	/**
+	 * Check if we're saving, the trigger an action based on the post type.
+	 *
+	 * @param  int $post_id
+	 * @param  object $post
+	 */
+	public function save( $post_id, $post ) {
+
+		// $post_id and $post are required.
+		if ( empty( $post_id ) || empty( $post ) ) {
+			return;
+		}
+		
+		// Don't save meta boxes for revisions or autosaves.
+		if ( defined( 'DOING_AUTOSAVE' ) || is_int( wp_is_post_revision( $post ) ) || is_int( wp_is_post_autosave( $post ) ) ) {
+			return;
+		}
+
+		// Check the nonce.
+		if ( empty( $_POST['give_form_meta_nonce'] ) || ! wp_verify_nonce( $_POST['give_form_meta_nonce'], 'give_save_form_meta' ) ) {
+			return;
+		}
+
+		// Check the post being saved == the $post_id to prevent triggering this call for other save_post events.
+		if ( empty( $_POST['post_ID'] ) || $_POST['post_ID'] != $post_id ) {
+			return;
+		}
+
+		// Check user has permission to edit.
+		if ( ! current_user_can( 'edit_post', $post_id ) ) {
+			return;
+		}
+
+		// Fire action before saving form meta.
+		do_action( 'give_pre_process_give_forms_meta', $post_id, $post );
+
+		/**
+		 * Filter the meta key to save.
+		 * Third party addon developer can remove there meta keys from this array to handle saving data on there own.
+		 */
+		$form_meta_keys = apply_filters( 'give_process_form_meta_keys', $this->get_meta_keys_from_settings() );
+
+		// Save form meta data.
+		// @TODO: Sanitize data for group field
+		if( ! empty( $form_meta_keys ) ) {
+			foreach ( $form_meta_keys as $form_meta_key ) {
+				if( isset( $_POST[ $form_meta_key ] ) ) {
+					if( $field_type = $this->get_field_type( $form_meta_key ) ) {
+						switch ( $field_type ) {
+							case 'wysiwyg':
+								$form_meta_value = wp_kses_post( $_POST[ $form_meta_key ] );
+								update_post_meta( $post_id, $form_meta_key, $form_meta_value );
+								break;
+
+							default:
+								$form_meta_value = give_clean( $_POST[ $form_meta_key ] );
+								update_post_meta( $post_id, $form_meta_key, $form_meta_value );
+						}
+					}
+				}
+			}
+		}
+
+		// Fire action after saving form meta.
+		do_action( 'give_post_process_give_forms_meta', $post_id, $post );
+	}
+
+
+	/**
+	 * Get all setting field ids.
+	 * @return array
+	 */
+	private function get_meta_keys_from_settings() {
+		$meta_keys = array();
+		foreach ( $this->settings as $setting ) {
+			if( ! empty( $setting['fields'] ) ) {
+				foreach ( $setting['fields'] as $field ) {
+					$meta_keys[] = $field['id'];
+				}
+			}
+		}
+
+		return $meta_keys;
+	}
+
+
+	/**
+	 * Get field type.
+	 * @param $field_id
+	 * @return array|string
+	 */
+	function get_field_type( $field_id ) {
+		$field_type = '';
+
+		if( ! empty( $this->settings ) ) {
+			foreach ( $this->settings as $setting ) {
+				if( ! empty( $setting['fields'] ) ) {
+					foreach ( $setting['fields'] as $field ) {
+						$field_type[] = $field['type'];
+						return $field_type;
+					}
+				}
+			}
+		}
 	}
 }
 
