924e6d3e82c73119b45dc84aaed97d586addbc5b
Rami Yushuvaev <r_a_m_i@hotmail.com>
2016-09-21 00:36:10 +0300

    replace "plugin_action_links" filter with "plugin_action_links_{$plugin_file}" filter

--- includes/admin/plugins.php
+++ includes/admin/plugins.php
@@ -9,33 +9,33 @@
  * @since       1.4
  */
 
-
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-
 
 /**
  * Plugins row action links
  *
  * @since 1.4
  *
- * @param array $links already defined action links
- * @param string $file plugin file path and name being processed
+ * @param array $actions An array of plugin action links.
  *
- * @return array $links
+ * @return array An array of updated action links.
  */
-function give_plugin_action_links( $links, $file ) {
-	$settings_link = '<a href="' . admin_url( 'edit.php?post_type=give_forms&page=give-settings' ) . '">' . esc_html__( 'Settings', 'give' ) . '</a>';
-	if ( $file == 'give/give.php' ) {
-		array_unshift( $links, $settings_link );
-	}
+function give_plugin_action_links( $actions ) {
+	$new_actions = array(
+		'settings' => sprintf(
+			'<a href="%1$s">%2$s</a>',
+			admin_url( 'edit.php?post_type=give_forms&page=give-settings' ),
+			esc_html__( 'Settings', 'give' )
+		),
+	);
 
-	return $links;
+	return array_merge( $new_actions, $actions );
 }
 
-add_filter( 'plugin_action_links', 'give_plugin_action_links', 10, 2 );
+add_filter( 'plugin_action_links_' . GIVE_PLUGIN_BASENAME, 'give_plugin_action_links' );
 
 
 /**
