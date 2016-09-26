db10e4605d259c240be518aee71feb4c575934f2
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2016-09-26 17:32:31 +0530

    Update backward conpetibilit code for setting

--- includes/admin/settings/class-settings-cmb2-backward-compatibility.php
+++ includes/admin/settings/class-settings-cmb2-backward-compatibility.php
@@ -16,7 +16,7 @@ if( ! class_exists( 'Give_CMB2_Settings_Loader' ) ) :
 	 *
 	 * @since 1.8
 	 */
-	Class Give_CMB2_Settings_Loader extends Give_Settings_Page {
+	Class Give_CMB2_Settings_Loader {
 
 		/**
 		 * @since 1.8
@@ -26,15 +26,9 @@ if( ! class_exists( 'Give_CMB2_Settings_Loader' ) ) :
 
 		/**
 		 * @since 1.8
-		 * @var   string $current_tab Current setting tab.
-		 */
-		private $current_tab;
-
-		/**
-		 * @since 1.8
 		 * @var   string $current_tab Current setting section.
 		 */
-		private $current_section;
+		protected $current_tab;
 
 		/**
 		 * Give_CMB2_Settings_Loader constructor.
@@ -43,83 +37,63 @@ if( ! class_exists( 'Give_CMB2_Settings_Loader' ) ) :
 			// Get previous setting class object.
 			$this->prev_settings = new Give_Plugin_Settings();
 
-			// Get current tab/section
-			$this->current_tab     = empty( $_GET['tab'] ) ? 'general' : sanitize_title( $_GET['tab'] );
-			$this->current_section = empty( $_REQUEST['section'] ) ? ( current( array_keys( $this->get_sections() ) ) ) : sanitize_title( $_REQUEST['section'] );
+			// Get current tab.
+			$this->current_tab     = give_get_current_setting_tab();
 
+			// Filter Payment Gateways addon settings.
+			add_filter( 'give_get_settings_gateways', array( $this, 'get_filtered_addon_settings' ), 999999, 1 );
+			add_filter( 'give_get_sections_gateways', array( $this, 'get_filtered_addon_sections' ), 999999, 1 );
+		}
 
-			// Hide save button on api and system_info setting tab.
-			if( in_array( $this->current_tab, array( 'api', 'system_info' ) ) ) {
-				$GLOBALS['give_hide_save_button'] = true;
-			}
+		/**
+		 * Get section name from section title
+		 *
+		 * @since  1.8
+		 * @param  $field_name
+		 * @return string
+		 */
+		function get_section_name ( $field_name ) {
+			$section_name = explode( ' ', $field_name );
+			unset( $section_name[ count( $section_name ) - 1 ] );
 
-			// Filter & actions.
-			add_filter( 'give_settings_tabs_array', array( $this, 'add_settings_pages' ), 20 );
-			add_action( "give_sections_{$this->current_tab}_page", array( $this, 'output_sections' ) );
-			add_action( "give_settings_{$this->current_tab}_page", array( $this, 'output' ), 10 );
-			add_action( "give_settings_save_{$this->current_tab}", array( $this, 'save' ) );
+			// Output.
+			return strip_tags( implode( ' ', $section_name ) );
 		}
 
 
 		/**
-		 * Add all old setting pages to settings.
+		 * Get addon sections.
 		 *
 		 * @since  1.8
-		 * @param  array $pages Setting tabs.
-		 * @return array
+		 * @param  array $sections Array of setting fields (Optional).
+		 * @return mixed
 		 */
-		function add_settings_pages( $pages ) {
-			if( $old_settings = $this->prev_settings->give_get_settings_tabs() ) {
-				foreach ( $old_settings as $old_setting_name => $old_setting_label ) {
-					$pages[ $old_setting_name ] = $old_setting_label;
-				}
-
-				return $pages;
-			}
-		}
-
-		/**
-		 * Get sections.
-		 *
-		 * @since  1.8
-		 * @return array
-		 */
-		public function get_sections() {
-			$sections = array();
-
+		function get_filtered_addon_sections( $sections = array() ) {
+			// New sections.
+			$new_sections = array();
+			
 			if( ( $setting_fields = $this->prev_settings->give_settings( $this->current_tab ) ) && ! empty( $setting_fields['fields'] ) ) {
 				foreach ( $setting_fields['fields'] as $field ) {
+					// Section name.
+					$section_name = $this->get_section_name( $field['name'] );
+
+					// Bailout: Do not load section if it is already exist.
+					if( in_array( $section_name, $sections ) ) {
+						continue;
+					}
+
+					// Collect new sections from addons.
 					if( 'give_title' == $field['type'] ) {
-						$sections[ sanitize_title( $field['name'] ) ] = current( explode( ' ', $field['name'] ) );
+						$new_sections[ sanitize_title( $field['name'] ) ] = $section_name;
 					}
 				}
 			}
 
-			return apply_filters( 'give_get_sections_' . $this->id, $sections );
-		}
+			// Add new section.
+			$sections = array_merge( $sections, $new_sections );
 
-		/**
-		 * Output sections.
-		 *
-		 * @since  1.8
-		 * @return void
-		 */
-		public function output_sections() {
-			$sections = $this->get_sections();
-
-			if ( empty( $sections ) || 1 === sizeof( $sections ) ) {
-				return;
-			}
-
-			echo '<ul class="subsubsub">';
-
-			$array_keys = array_keys( $sections );
-
-			foreach ( $sections as $id => $label ) {
-				echo '<li><a href="' . admin_url( 'edit.php?post_type=give_forms&page=give-settings&tab=' . $this->current_tab . '&section=' . sanitize_title( $id ) ) . '" class="' . ( $this->current_section == $id  ? 'current' : '' ) . '">' . strip_tags( $label ) . '</a> ' . ( end( $array_keys ) == $id ? '' : '|' ) . ' </li>';
-			}
-
-			echo '</ul><br class="clear" />';
+			// Output.
+			return $sections;
 		}
 
 
@@ -127,149 +101,142 @@ if( ! class_exists( 'Give_CMB2_Settings_Loader' ) ) :
 		 * Get setting fields.
 		 *
 		 * @since  1.8
+		 * @param  array  $settings List of settings.
 		 * @return array
 		 */
-		function get_settings() {
+		function get_filtered_addon_settings( $settings ) {
 			global $wp_filter;
 
 			$new_setting_fields = array();
 
-			if( $setting_fields = $this->prev_settings->give_settings( $this->current_tab ) ){
+			if( ! empty( $settings ) ) {
+				// Store title field id.
+				$prev_title_field_id = '';
 
-				/**
-				 * give_settings can return array of all settings,
-				 * so make sure you always have array of single setting. otherwise leave.
-				 */
-				if( ! isset( $setting_fields['id'] ) ) {
-					return $new_setting_fields;
-				}
+				// Create new setting fields.
+				foreach ( $settings as $index => $field ) {
 
-				if( ! empty( $setting_fields['fields'] ) ) {
-					// Store title field id.
-					$prev_title_field_id = '';
+					$field['name'] = empty( $field['name'] ) ? '' : $field['name'];
+					$field['desc'] = empty( $field['desc'] ) ? '' : $field['desc'];
 
-					// Create new setting fields.
-					foreach ( $setting_fields['fields'] as $index => $field ) {
+					// Modify cmb2 setting fields.
+					switch ( $field['type'] ) {
+						case 'text' :
+						case 'file' :
+							$field['css'] = 'width:25em;';
+							break;
 
-						// Modify cmb2 setting fields.
-						switch ( $field['type'] ) {
-							case 'text' :
-							case 'file' :
-								$field['css'] = 'width:25em;';
-								break;
+						case 'text_small' :
+							$field['type'] = 'text';
+							break;
 
-							case 'text_small' :
-								$field['type'] = 'text';
-								break;
+						case 'text_email' :
+							$field['type'] = 'email';
+							$field['css'] = 'width:25em;';
+							break;
 
-							case 'text_email' :
-								$field['type'] = 'email';
-								$field['css'] = 'width:25em;';
-								break;
+						case 'radio_inline' :
+							$field['type']  = 'radio';
+							$field['class'] = 'give-radio-inline';
+							break;
 
-							case 'radio_inline' :
-								$field['type']  = 'radio';
-								$field['class'] = 'give-radio-inline';
-								break;
+						case 'give_title' :
+							$field['type'] = 'title';
+							break;
+					}
 
-							case 'give_title' :
-								$field['type'] = 'title';
-								break;
-						}
+					if( 'title' === $field['type'] ) {
 
-						if( 'title' === $field['type'] ) {
-
-							// If we do not have first element as title then these field will be skip from frontend
-							// because there are not belong to any section, so put all abandon fields under first section.
-							if( $index && empty( $prev_title_field_id )  ) {
-								array_unshift(
-									$new_setting_fields,
-									array(
-										'title' => $field['name'],
-										'type' => $field['type'],
-										'desc' => $field['desc'],
-										'id' => $field['id']
-									)
-								);
-
-								$prev_title_field_id = $field['id'];
-
-								continue;
-							} elseif ( $index ) {
-								// Section end.
-								$new_setting_fields[] = array(
-									'type' => 'sectionend',
-									'id'   => $prev_title_field_id
-								);
-							}
-
-							// Section start.
-							$new_setting_fields[] = array(
-								'title' => $field['name'],
-								'type' => $field['type'],
-								'desc' => $field['desc'],
-								'id' => $field['id']
+						// If we do not have first element as title then these field will be skip from frontend
+						// because there are not belong to any section, so put all abandon fields under first section.
+						if( $index && empty( $prev_title_field_id )  ) {
+							array_unshift(
+								$new_setting_fields,
+								array(
+									'title' => $field['name'],
+									'type' => $field['type'],
+									'desc' => $field['desc'],
+									'id' => $field['id']
+								)
 							);
 
 							$prev_title_field_id = $field['id'];
-						} else {
 
-							// setting fields
-							$new_setting_fields[] = $field;
+							continue;
+						} elseif ( $index ) {
+							// Section end.
+							$new_setting_fields[] = array(
+								'type' => 'sectionend',
+								'id'   => $prev_title_field_id
+							);
 						}
-					}
 
-					// Section end.
-					$new_setting_fields[] = array(
-						'type' => 'sectionend',
-						'id'   => $prev_title_field_id
-					);
-				}
+						// Section start.
+						$new_setting_fields[] = array(
+							'title' => $field['name'],
+							'type' => $field['type'],
+							'desc' => $field['desc'],
+							'id' => $field['id']
+						);
 
-				// Check if setting page has title section or not.
-				// If setting page does not have title section  then add title section to it and fix section end array id.
-				if( 'title' !== $new_setting_fields[0]['type'] ) {
-					array_unshift(
-						$new_setting_fields,
-						array(
-							'title' => $setting_fields['give_title'],
-							'type' => 'title',
-							'desc' => ! empty( $setting_fields['desc'] ) ? $setting_fields['desc'] : '',
-							'id' => $setting_fields['id']
-						)
-					);
+						$prev_title_field_id = $field['id'];
+					} else {
 
-					// Update id in section end array if does not contain.
-					if( empty( $new_setting_fields[count( $new_setting_fields ) - 1 ]['id'] ) ) {
-						$new_setting_fields[count( $new_setting_fields ) - 1 ]['id'] = $setting_fields['id'];
+						// setting fields
+						$new_setting_fields[] = $field;
 					}
 				}
 
-				// Return only section related settings.
-				if( $sections = $this->get_sections() ) {
-					$new_setting_fields = self::get_section_settiings( $new_setting_fields );
+				// Section end.
+				$new_setting_fields[] = array(
+					'type' => 'sectionend',
+					'id'   => $prev_title_field_id
+				);
+			}
+
+			// Check if setting page has title section or not.
+			// If setting page does not have title section  then add title section to it and fix section end array id.
+			if( 'title' !== $new_setting_fields[0]['type'] ) {
+				array_unshift(
+					$new_setting_fields,
+					array(
+						'title' => $settings['give_title'],
+						'type' => 'title',
+						'desc' => ! empty( $setting_fields['desc'] ) ? $setting_fields['desc'] : '',
+						'id' => $setting_fields['id']
+					)
+				);
+
+				// Update id in section end array if does not contain.
+				if( empty( $new_setting_fields[count( $new_setting_fields ) - 1 ]['id'] ) ) {
+					$new_setting_fields[count( $new_setting_fields ) - 1 ]['id'] = $setting_fields['id'];
+				}
+			}
+
+			// Return only section related settings.
+			if( $sections = $this->get_filtered_addon_sections() ) {
+				$new_setting_fields = $this->get_section_settings( $new_setting_fields );
+			}
+
+			// Third party plugin backward compatibility.
+			foreach ( $new_setting_fields  as $index => $field ) {
+				if( in_array( $field['type'], array( 'title', 'sectionend') ) ) {
+					continue;
 				}
 
-				// Third party plugin backward compatibility.
-				foreach ( $new_setting_fields  as $index => $field ) {
-					if( in_array( $field['type'], array( 'title', 'sectionend') ) ) {
-						continue;
-					}
+				$cmb2_filter_name = "cmb2_render_{$field['type']}";
 
-					$cmb2_filter_name = "cmb2_render_{$field['type']}";
+				if( ! empty( $wp_filter[ $cmb2_filter_name ] ) ) {
+					$cmb2_filter_arr = current( $wp_filter[ $cmb2_filter_name ] );
 
-					if( ! empty( $wp_filter[ $cmb2_filter_name ] ) ) {
-						$cmb2_filter_arr = current( $wp_filter[ $cmb2_filter_name ] );
-
-						if( ! empty( $cmb2_filter_arr ) ) {
-							$new_setting_fields[$index]['func'] = current( array_keys( $cmb2_filter_arr ) );
-							add_action( "give_admin_field_{$field['type']}", array( $this, 'addon_setting_field' ), 10, 2 );
-						}
+					if( ! empty( $cmb2_filter_arr ) ) {
+						$new_setting_fields[$index]['func'] = current( array_keys( $cmb2_filter_arr ) );
+						add_action( "give_admin_field_{$field['type']}", array( $this, 'addon_setting_field' ), 10, 2 );
 					}
 				}
 			}
 
-			return apply_filters( "give_get_settings_{$this->id}", $new_setting_fields );
+			return $new_setting_fields;
 		}
 
 
@@ -280,13 +247,15 @@ if( ! class_exists( 'Give_CMB2_Settings_Loader' ) ) :
 		 * @param $tab_settings
 		 * @return array
 		 */
-		function get_section_settiings( $tab_settings ) {
+		function get_section_settings( $tab_settings ) {
+			$current_section = give_get_current_setting_section();
+
 			$section_start = false;
 			$section_end   = false;
 			$section_only_setting_fields = array();
 
 			foreach ( $tab_settings as $field ) {
-				if( 'title' == $field['type'] &&  $this->current_section == sanitize_title( $field['title'] ) ) {
+				if( 'title' == $field['type'] && $current_section == sanitize_title( $field['title'] ) ) {
 					$section_start = true;
 				}
 
@@ -305,31 +274,7 @@ if( ! class_exists( 'Give_CMB2_Settings_Loader' ) ) :
 			// Remove title from setting, pevent it from render in setting tab.
 			$section_only_setting_fields[0]['title'] = '';
 
-			return apply_filters( "give_get_settings_{$this->current_tab}_{$this->current_section}", $section_only_setting_fields, $tab_settings );
-		}
-
-		/**
-		 * Output the settings.
-		 *
-		 * @since  1.8
-		 * @return void
-		 */
-		public function output() {
-			$settings = $this->get_settings();
-
-			Give_Admin_Settings::output_fields( $settings, 'give_settings' );
-		}
-
-		/**
-		 * Save settings.
-		 *
-		 * @since  1.8
-		 * @return void
-		 */
-		public function save() {
-			$settings = $this->get_settings();
-
-			Give_Admin_Settings::save_fields( $settings, 'give_settings' );
+			return apply_filters( "give_get_settings_{$this->current_tab}_{$current_section}", $section_only_setting_fields, $tab_settings );
 		}
 
 
@@ -374,4 +319,4 @@ if( ! class_exists( 'Give_CMB2_Settings_Loader' ) ) :
 	}
 endif;
 
-//return new Give_CMB2_Settings_Loader();
+new Give_CMB2_Settings_Loader();
