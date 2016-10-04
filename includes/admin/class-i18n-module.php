813f4a6dd9908c3b69a141db1b40297572fc1dba
Devin Walker <devin@wordimpress.com>
2016-10-04 12:24:20 -0700

    Removing a lot of the private properties that aren't needed - additionally, lots of style improvements and now using translate dashicon for banner #1021

--- includes/admin/class-i18n-module.php
+++ includes/admin/class-i18n-module.php
@@ -6,20 +6,6 @@
 class Give_i18n {
 
 	/**
-	 * Your translation site's logo.
-	 *
-	 * @var string
-	 */
-	private $glotpress_logo;
-
-	/**
-	 * Your translation site's name.
-	 *
-	 * @var string
-	 */
-	private $glotpress_name;
-
-	/**
 	 * Your translation site's URL.
 	 *
 	 * @var string
@@ -57,26 +43,6 @@ class Give_i18n {
 	 */
 	private $percent_translated;
 
-	/**
-	 * Project slug for the project on your translation site.
-	 *
-	 * @var string
-	 */
-	private $project_slug;
-
-	/**
-	 * URL to point to for registration links.
-	 *
-	 * @var string
-	 */
-	private $register_url;
-
-	/**
-	 * Your plugins textdomain
-	 *
-	 * @var string
-	 */
-	private $textdomain;
 
 	/**
 	 * Indicates whether there's a translation available at all.
@@ -95,7 +61,7 @@ class Give_i18n {
 	private $translation_loaded;
 
 	/**
-	 * Class constructor
+	 * Class constructor.
 	 *
 	 * @param array $args Contains the settings for the class.
 	 */
@@ -104,6 +70,7 @@ class Give_i18n {
 			return;
 		}
 
+		//This plugin is en_US native.
 		$this->locale = get_locale();
 		if ( 'en_US' === $this->locale ) {
 			return;
@@ -127,6 +94,7 @@ class Give_i18n {
 		foreach ( $args as $key => $arg ) {
 			$this->$key = $arg;
 		}
+
 	}
 
 	/**
@@ -137,11 +105,11 @@ class Give_i18n {
 	 * @return bool
 	 */
 	private function hide_promo() {
-		$hide_promo = get_transient( 'give_i18n_' . $this->project_slug . '_promo_hide' );
+		$hide_promo = get_transient( 'give_i18n_give_promo_hide' );
 		if ( ! $hide_promo ) {
 			if ( filter_input( INPUT_GET, 'remove_i18n_promo', FILTER_VALIDATE_INT ) === 1 ) {
 				// No expiration time, so this would normally not expire, but it wouldn't be copied to other sites etc.
-				set_transient( 'give_i18n_' . $this->project_slug . '_promo_hide', true );
+				set_transient( 'give_i18n_give_promo_hide', true );
 				$hide_promo = true;
 			}
 		}
@@ -167,7 +135,7 @@ class Give_i18n {
 			$message = __( 'You\'re using WordPress in a language we don\'t support yet. We\'d love for %2$s to be translated in that language too, but unfortunately, it isn\'t right now. You can change that! Register at %4$s to help translate it!', 'give' );
 		}
 
-		$registration_link = sprintf( '<a href="%1$s">%2$s</a>', esc_url( $this->register_url ), esc_html__( 'WordPress.org', 'give' ) );
+		$registration_link = sprintf( '<a href="%1$s">%2$s</a>', 'https://wordpress.org/support/register.php', esc_html__( 'WordPress.org', 'give' ) );
 		$message           = sprintf( $message, esc_html( $this->locale_name ), esc_html( 'Give' ), $this->percent_translated, $registration_link );
 
 		return $message;
@@ -177,25 +145,32 @@ class Give_i18n {
 	 * Outputs a promo box
 	 */
 	public function promo() {
-		$this->translation_details();
 
+		$this->translation_details();
 		$message = $this->promo_message();
 
-		if ( $message ) {
-			echo '<div id="give-i18n-notice" class="give-addon-alert updated" style="">';
-			//Logo linked to GlotPress.
-			if ( isset( $this->glotpress_logo ) && '' != $this->glotpress_logo ) {
-				echo '<a href="' . $this->register_url . '" class="alignleft" style="margin:0"><img style="margin: -6px 0 0;max-width: 180px;" src="' . $this->glotpress_logo . '" alt="' . __( 'WordPress.org', 'give' ) . '"/></a>';
-			}
-			//Container with Text.
-			echo '<div style="margin: 0 0 0 200px;">';
-			echo '<a href="' . esc_url( add_query_arg( array( 'remove_i18n_promo' => '1' ) ) ) . '" style="color:#333;text-decoration:none;font-weight:bold;font-size:16px;padding:1px 4px;" class="alignright"><span class="dashicons dashicons-dismiss"></span></a>';
-			echo '<h2 style="margin: 10px 0;">' . __( 'Help Translate Give', 'give' ) . '</h2>';
+		if ( $message ) { ?>
 
-			echo '<p>' . $message . '</p>';
-			echo '<p><a href="' . $this->register_url . '">' . __( 'Register now &raquo;', 'give' ) . '</a></p>';
-			echo '</div>';
-			echo '</div>';
+			<style>
+				/* Banner specific styles */
+
+			</style>
+			<div id="give-i18n-notice" class="give-addon-alert updated" style="">
+
+				<a href="https://wordpress.org/support/register.php" class="alignleft" style="margin:0" target="_blank"><span class="dashicons dashicons-translation" style="font-size: 110px; text-decoration: none;"></span></a>
+
+				<div style="margin: 0 0 0 132px;">
+					<a href="<?php echo esc_url( add_query_arg( array( 'remove_i18n_promo' => '1' ) ) ); ?>" style="color:#333;text-decoration:none;font-weight:bold;font-size:16px;padding:1px 4px;" class="alignright"><span class="dashicons dashicons-dismiss"></span></a>
+
+					<h2 style="margin: 10px 0;"><?php _e( 'Help Translate Give', 'give' ); ?></h2>
+
+					<p><?php echo $message; ?></p>
+					<p>
+						<a href="https://wordpress.org/support/register.php" target="_blank"><?php _e( 'Register now &raquo;', 'give' ); ?></a>
+					</p>
+				</div>
+			</div>
+			<?php
 		}
 	}
 
@@ -207,11 +182,11 @@ class Give_i18n {
 	 * @return object|null
 	 */
 	private function find_or_initialize_translation_details() {
-		$set = get_transient( 'give_i18n_' . $this->project_slug . '_' . $this->locale );
+		$set = get_transient( 'give_i18n_give_' . $this->locale );
 
 		if ( ! $set ) {
 			$set = $this->retrieve_translation_details();
-			set_transient( 'give_i18n_' . $this->project_slug . '_' . $this->locale, $set, DAY_IN_SECONDS );
+			set_transient( 'give_i18n_give_' . $this->locale, $set, DAY_IN_SECONDS );
 		}
 
 		return $set;
@@ -239,7 +214,7 @@ class Give_i18n {
 	 * @return object|null
 	 */
 	private function retrieve_translation_details() {
-		$api_url = trailingslashit( $this->glotpress_url ) . 'api/projects/' . $this->project_slug;
+		$api_url = trailingslashit( $this->glotpress_url ) . 'api/projects/give';
 
 		$resp = wp_remote_get( $api_url );
 		$body = wp_remote_retrieve_body( $resp );
@@ -281,11 +256,7 @@ class Give_i18n {
 
 $give_i18n = new Give_i18n(
 	array(
-		'project_slug'   => 'give',
-		'hook'           => 'admin_notices',
-		'glotpress_url'  => 'https://translate.wordpress.org/projects/wp-plugins/give',
-		'glotpress_name' => 'Translate Give',
-		'glotpress_logo' => GIVE_PLUGIN_URL . 'assets/images/give-logo-small.png',
-		'register_url '  => 'https://wordpress.org/support/register.php',
+		'hook'          => 'admin_notices',
+		'glotpress_url' => 'https://translate.wordpress.org/projects/wp-plugins/give',
 	)
 );
