ea91442bfef57d9345e14c8a73625aadb445f9ce
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2016-10-04 12:21:51 +0530

    Do not need upgrade for _give_logged_in_only, it is already tale care

--- includes/admin/upgrades/upgrade-functions.php
+++ includes/admin/upgrades/upgrade-functions.php
@@ -581,11 +581,6 @@ function give_v18_upgrades_form_metadata() {
 			$forms->the_post();
 
 			// Guest donation setting.
-			if( 'on' === get_post_meta( get_the_ID(), '_give_logged_in_only', true ) ) {
-				update_post_meta( get_the_ID(), '_give_logged_in_only', 'yes' );
-			}
-
-			// Guest donation setting.
 			if( 'on' === get_post_meta( get_the_ID(), '_give_offline_donation_enable_billing_fields_single', true ) ) {
 				update_post_meta( get_the_ID(), '_give_offline_donation_enable_billing_fields_single', 'enabled' );
 			}
