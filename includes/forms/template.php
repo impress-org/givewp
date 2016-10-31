a7ad0486b05f7e75604d682293679ef208594f3a
Rami Yushuvaev <r_a_m_i@hotmail.com>
2016-10-31 23:07:31 +0200

    Hide the newly added `<legend>` for Terms

--- includes/forms/template.php
+++ includes/forms/template.php
@@ -1418,7 +1418,7 @@ function give_terms_agreement( $form_id ) {
 
 	?>
 	<fieldset id="give_terms_agreement">
-		<legend class="give-terms-agreement-label"><?php echo apply_filters( 'give_terms_agreement_text', esc_html__( 'Terms', 'give' ) ); ?></legend>
+		<legend><?php echo apply_filters( 'give_terms_agreement_text', esc_html__( 'Terms', 'give' ) ); ?></legend>
 		<div id="give_terms" class="give_terms-<?php echo $form_id; ?>" style="display:none;">
 			<?php
 			/**
