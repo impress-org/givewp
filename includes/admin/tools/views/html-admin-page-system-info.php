688255c15d3b02d8c42b1f238c9a8c139ef00783
Kevin Hoffman <kevin@kevinwhoffman.com>
2016-11-07 17:54:50 -0500

    Split plugins into Add-ons, Active, Inactive, MU

--- includes/admin/tools/views/html-admin-page-system-info.php
+++ includes/admin/tools/views/html-admin-page-system-info.php
@@ -542,41 +542,132 @@ $give_options = give_get_settings();
 	</tbody>
 </table>
 
+<?php
+// Assemble Data for Active Give Add-ons, Active Plugins, and Inactive Plugins tables
+$all_plugins         = get_plugins();
+$active_addons       = array();
+$active_plugins      = array();
+$inactive_plugins    = array();
+$active_plugin_paths = (array) get_option( 'active_plugins', array() );
+
+if ( is_multisite() ) {
+	$network_activated_plugin_paths = array_keys( get_site_option( 'active_sitewide_plugins', array() ) );
+	$active_plugin_paths            = array_merge( $active_plugin_paths, $network_activated_plugin_paths );
+}
+
+foreach ( $all_plugins as $plugin_path => $plugin_data ) {
+	if ( in_array( $plugin_path, $active_plugin_paths ) ) {
+		$dirname = strtolower( dirname( $plugin_path ) );
+
+		// Check if plugin is a Give add-on.
+		if ( strstr( $dirname, 'give-' ) && strstr( $plugin_data['AuthorURI'], 'wordimpress.com' ) ) {
+			$active_addons[] = $plugin_data;
+			continue;
+		}
+
+		$active_plugins[] = $plugin_data;
+	} else {
+		$inactive_plugins[] = $plugin_data;
+	}
+}
+?>
+
 <table class="give-status-table widefat" cellspacing="0">
 	<thead>
-		<tr>
-			<th colspan="3" data-export-label="Active Plugins (<?php echo count( (array) get_option( 'active_plugins' ) ); ?>)"><h2><?php _e( 'Active Plugins', 'give' ); ?> (<?php echo count( (array) get_option( 'active_plugins' ) ); ?>)</h2></th>
-		</tr>
+	<tr>
+		<th colspan="3" data-export-label="Active Give Add-ons (<?php echo count( (array) $active_addons ); ?>)"><h2><?php _e( 'Active Give Add-ons', 'give' ); ?> (<?php echo count( (array) $active_addons ); ?>)</h2></th>
+	</tr>
 	</thead>
 	<tbody>
-	<?php
-	$active_plugins = (array) get_option( 'active_plugins', array() );
+		<?php
+		foreach ( $active_addons as $plugin_data ) {
+			$plugin_name = $plugin_data['Name'];
+			$author_name = $plugin_data['Author'];
 
-	if ( is_multisite() ) {
-		$network_activated_plugins = array_keys( get_site_option( 'active_sitewide_plugins', array() ) );
-		$active_plugins            = array_merge( $active_plugins, $network_activated_plugins );
-	}
-
-	foreach ( $active_plugins as $plugin ) {
-		$plugin_data    = @get_plugin_data( WP_PLUGIN_DIR . '/' . $plugin );
-
-		if ( ! empty( $plugin_data['Name'] ) ) {
-			// Link the plugin name to the plugin url if available.
-			$plugin_name = esc_html( $plugin_data['Name'] );
-
+			// Link the plugin name to the plugin URL if available.
 			if ( ! empty( $plugin_data['PluginURI'] ) ) {
 				$plugin_name = '<a href="' . esc_url( $plugin_data['PluginURI'] ) . '" title="' . esc_attr__( 'Visit plugin homepage' , 'give' ) . '">' . $plugin_name . '</a>';
 			}
+
+			// Link the author name to the author URL if available.
+			if ( ! empty( $plugin_data['AuthorURI'] ) ) {
+				$author_name = '<a href="' . esc_url( $plugin_data['AuthorURI'] ) . '" title="' . esc_attr__( 'Visit author homepage' , 'give' ) . '">' . $author_name . '</a>';
+			}
 			?>
 			<tr>
-				<td><?php echo $plugin_name; ?></td>
+				<td><?php echo wp_kses( $plugin_name, wp_kses_allowed_html( 'post' ) ); ?></td>
 				<td class="help">&nbsp;</td>
-				<td><?php echo sprintf( _x( 'by %s', 'by author', 'give' ), $plugin_data['Author'] ) . ' &ndash; ' . esc_html( $plugin_data['Version'] ); ?></td>
+				<td><?php echo sprintf( _x( 'by %s', 'by author', 'give' ), wp_kses( $author_name, wp_kses_allowed_html( 'post' ) ) ) . ' &ndash; ' . esc_html( $plugin_data['Version'] ); ?></td>
 			</tr>
-	<?php
+			<?php
 		}
-	}
-	?>
+		?>
+	</tbody>
+</table>
+
+<table class="give-status-table widefat" cellspacing="0">
+	<thead>
+		<tr>
+			<th colspan="3" data-export-label="Other Active Plugins (<?php echo count( (array) $active_plugins ); ?>)"><h2><?php _e( 'Other Active Plugins', 'give' ); ?> (<?php echo count( (array) $active_plugins ); ?>)</h2></th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php
+		foreach ( $active_plugins as $plugin_data ) {
+			$plugin_name = $plugin_data['Name'];
+			$author_name = $plugin_data['Author'];
+
+			// Link the plugin name to the plugin URL if available.
+			if ( ! empty( $plugin_data['PluginURI'] ) ) {
+				$plugin_name = '<a href="' . esc_url( $plugin_data['PluginURI'] ) . '" title="' . esc_attr__( 'Visit plugin homepage' , 'give' ) . '">' . $plugin_name . '</a>';
+			}
+
+			// Link the author name to the author URL if available.
+			if ( ! empty( $plugin_data['AuthorURI'] ) ) {
+				$author_name = '<a href="' . esc_url( $plugin_data['AuthorURI'] ) . '" title="' . esc_attr__( 'Visit author homepage' , 'give' ) . '">' . $author_name . '</a>';
+			}
+			?>
+			<tr>
+				<td><?php echo wp_kses( $plugin_name, wp_kses_allowed_html( 'post' ) ); ?></td>
+				<td class="help">&nbsp;</td>
+				<td><?php echo sprintf( _x( 'by %s', 'by author', 'give' ), wp_kses( $author_name, wp_kses_allowed_html( 'post' ) ) ) . ' &ndash; ' . esc_html( $plugin_data['Version'] ); ?></td>
+			</tr>
+		<?php
+		}
+		?>
+	</tbody>
+</table>
+
+<table class="give-status-table widefat" cellspacing="0">
+	<thead>
+		<tr>
+			<th colspan="3" data-export-label="Inactive Plugins (<?php echo count( (array) $inactive_plugins ); ?>)"><h2><?php _e( 'Inactive Plugins', 'give' ); ?> (<?php echo count( (array) $inactive_plugins ); ?>)</h2></th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php
+		foreach ( $inactive_plugins as $plugin_data ) {
+			$plugin_name = $plugin_data['Name'];
+			$author_name = $plugin_data['Author'];
+
+			// Link the plugin name to the plugin URL if available.
+			if ( ! empty( $plugin_data['PluginURI'] ) ) {
+				$plugin_name = '<a href="' . esc_url( $plugin_data['PluginURI'] ) . '" title="' . esc_attr__( 'Visit plugin homepage' , 'give' ) . '">' . $plugin_name . '</a>';
+			}
+
+			// Link the author name to the author URL if available.
+			if ( ! empty( $plugin_data['AuthorURI'] ) ) {
+				$author_name = '<a href="' . esc_url( $plugin_data['AuthorURI'] ) . '" title="' . esc_attr__( 'Visit author homepage' , 'give' ) . '">' . $author_name . '</a>';
+			}
+			?>
+			<tr>
+				<td><?php echo wp_kses( $plugin_name, wp_kses_allowed_html( 'post' ) ); ?></td>
+				<td class="help">&nbsp;</td>
+				<td><?php echo sprintf( _x( 'by %s', 'by author', 'give' ), wp_kses( $author_name, wp_kses_allowed_html( 'post' ) ) ) . ' &ndash; ' . esc_html( $plugin_data['Version'] ); ?></td>
+			</tr>
+			<?php
+		}
+		?>
 	</tbody>
 </table>
 
@@ -595,14 +686,14 @@ if ( ! empty( $active_mu_plugins ) ) {
 
 			foreach ( $active_mu_plugins as $mu_plugin_data ) {
 				if ( ! empty( $mu_plugin_data['Name'] ) ) {
-					// Link the plugin name to the plugin url if available.
+					// Link the plugin name to the plugin URL if available.
 					$plugin_name = esc_html( $mu_plugin_data['Name'] );
 
 					if ( ! empty( $mu_plugin_data['PluginURI'] ) ) {
 						$plugin_name = '<a href="' . esc_url( $mu_plugin_data['PluginURI'] ) . '" title="' . esc_attr__( 'Visit plugin homepage' , 'give' ) . '">' . $plugin_name . '</a>';
 					}
 
-					// Link the author name to the author url if available.
+					// Link the author name to the author URL if available.
 					$author_name = esc_html( $mu_plugin_data['Author'] );
 
 					if ( ! empty( $mu_plugin_data['AuthorURI'] ) ) {
