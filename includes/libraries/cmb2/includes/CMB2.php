4f1ca4fc4df8df8f3a0b667e549e9077e482759e
Devin Walker <devin@wordimpress.com>
2016-11-10 15:00:33 -0800

    Updated CMB2 to 2.2.3.1

--- includes/libraries/cmb2/includes/CMB2.php
+++ includes/libraries/cmb2/includes/CMB2.php
@@ -11,15 +11,17 @@
  * @property-read string $cmb_id
  * @property-read array $meta_box
  * @property-read array $updated
+ * @property-read bool  $has_columns
+ * @property-read array $tax_metaboxes_to_remove
  */
-class CMB2 {
+class CMB2 extends CMB2_Base {
 
 	/**
-	 * Current CMB2 instance ID
+	 * The object properties name.
 	 * @var   string
-	 * @since 2.0.0
+	 * @since 2.2.3
 	 */
-	protected $cmb_id = '';
+	protected $properties_name = 'meta_box';
 
 	/**
 	 * Metabox Config array
@@ -27,20 +29,6 @@ class CMB2 {
 	 * @since 0.9.0
 	 */
 	protected $meta_box = array();
-
-	/**
-	 * Object ID for metabox meta retrieving/saving
-	 * @var   mixed
-	 * @since 1.0.0
-	 */
-	protected $object_id = 0;
-
-	/**
-	 * Type of object being saved. (e.g., post, user, or comment)
-	 * @var   string
-	 * @since 1.0.0
-	 */
-	protected $object_type = 'post';
 
 	/**
 	 * Type of object registered for metabox. (e.g., post, user, or comment)
@@ -64,7 +52,6 @@ class CMB2 {
 	protected $mb_defaults = array(
 		'id'               => '',
 		'title'            => '',
-		'type'             => '',
 		'object_types'     => array(), // Post type
 		'context'          => 'normal',
 		'priority'         => 'high',
@@ -80,6 +67,7 @@ class CMB2 {
 		'taxonomies'       => array(),
 		'new_user_section' => 'add-new-user', // or 'add-existing-user'
 		'new_term_section' => true,
+		'show_in_rest'     => false,
 	);
 
 	/**
@@ -98,13 +86,6 @@ class CMB2 {
 
 	/**
 	 * Array of key => value data for saving. Likely $_POST data.
-	 * @var   array
-	 * @since 2.0.0
-	 */
-	public $data_to_save = array();
-
-	/**
-	 * Array of key => value data for saving. Likely $_POST data.
 	 * @var   string
 	 * @since 2.0.0
 	 */
@@ -118,26 +99,33 @@ class CMB2 {
 	protected $has_columns = false;
 
 	/**
+	 * If taxonomy field is requesting to remove_default, we store the taxonomy here.
+	 * @var   array
+	 * @since 2.2.3
+	 */
+	protected $tax_metaboxes_to_remove = array();
+
+	/**
 	 * Get started
 	 * @since 0.4.0
-	 * @param array   $meta_box  Metabox config array
+	 * @param array   $config    Metabox config array
 	 * @param integer $object_id Optional object id
 	 */
-	public function __construct( $meta_box, $object_id = 0 ) {
+	public function __construct( $config, $object_id = 0 ) {
 
-		if ( empty( $meta_box['id'] ) ) {
-			wp_die( __( 'Metabox configuration is required to have an ID parameter.', 'give' ) );
+		if ( empty( $config['id'] ) ) {
+			wp_die( esc_html__( 'Metabox configuration is required to have an ID parameter.', 'cmb2' ) );
 		}
 
-		$this->meta_box = wp_parse_args( $meta_box, $this->mb_defaults );
+		$this->meta_box = wp_parse_args( $config, $this->mb_defaults );
 		$this->meta_box['fields'] = array();
 
 		$this->object_id( $object_id );
 		$this->mb_object_type();
-		$this->cmb_id = $meta_box['id'];
+		$this->cmb_id = $config['id'];
 
-		if ( ! empty( $meta_box['fields'] ) && is_array( $meta_box['fields'] ) ) {
-			$this->add_fields( $meta_box['fields'] );
+		if ( ! empty( $config['fields'] ) && is_array( $config['fields'] ) ) {
+			$this->add_fields( $config['fields'] );
 		}
 
 		CMB2_Boxes::add( $this );
@@ -180,9 +168,9 @@ class CMB2 {
 		$object_type = $this->object_type( $object_type );
 		$object_id = $this->object_id( $object_id );
 
-		$this->nonce_field();
-
 		echo "\n<!-- Begin CMB2 Fields -->\n";
+
+		$this->nonce_field();
 
 		/**
 		 * Hook before form table begins
@@ -225,10 +213,8 @@ class CMB2 {
 
 		$classes = array( 'cmb2-wrap', 'form-table' );
 
-		$cb = $this->prop( 'classes_cb' );
-
 		// Use the callback to fetch classes.
-		if ( is_callable( $cb ) && ( $added_classes = call_user_func( $cb, $this ) ) ) {
+		if ( $added_classes = $this->get_param_callback_result( 'classes_cb' ) ) {
 			$added_classes = is_array( $added_classes ) ? $added_classes : array( $added_classes );
 			$classes = array_merge( $classes, $added_classes );
 		}
@@ -352,16 +338,13 @@ class CMB2 {
 
 		$desc            = $field_group->args( 'description' );
 		$label           = $field_group->args( 'name' );
-		$sortable        = $field_group->options( 'sortable' ) ? ' sortable' : ' non-sortable';
-		$repeat_class    = $field_group->args( 'repeatable' ) ? ' repeatable' : ' non-repeatable';
 		$group_val       = (array) $field_group->value();
-		$nrows           = count( $group_val );
-		$remove_disabled = $nrows <= 1 ? 'disabled="disabled" ' : '';
+		$remove_disabled = count( $group_val ) <= 1 ? 'disabled="disabled" ' : '';
 		$field_group->index = 0;
 
 		$field_group->peform_param_callback( 'before_group' );
 
-		echo '<div class="cmb-row cmb-repeat-group-wrap ', $field_group->row_classes(), '" data-fieldtype="group"><div class="cmb-td"><div id="', $field_group->id(), '_repeat" class="cmb-nested cmb-field-list cmb-repeatable-group', $sortable, $repeat_class, '" style="width:100%;">';
+		echo '<div class="cmb-row cmb-repeat-group-wrap ', $field_group->row_classes(), '" data-fieldtype="group"><div class="cmb-td"><div data-groupid="', $field_group->id(), '" id="', $field_group->id(), '_repeat" ', $this->group_wrap_attributes( $field_group ), '>';
 
 		if ( $desc || $label ) {
 			$class = $desc ? ' cmb-group-description' : '';
@@ -376,7 +359,6 @@ class CMB2 {
 		}
 
 		if ( ! empty( $group_val ) ) {
-
 			foreach ( $group_val as $group_key => $field_id ) {
 				$this->render_group_row( $field_group, $remove_disabled );
 				$field_group->index++;
@@ -397,6 +379,38 @@ class CMB2 {
 	}
 
 	/**
+	 * Get the group wrap attributes, which are passed through a filter.
+	 * @since  2.2.3
+	 * @param  CMB2_Field $field_group The group CMB2_Field object.
+	 * @return string                  The attributes string.
+	 */
+	public function group_wrap_attributes( $field_group ) {
+		$classes = 'cmb-nested cmb-field-list cmb-repeatable-group';
+		$classes .= $field_group->options( 'sortable' ) ? ' sortable' : ' non-sortable';
+		$classes .= $field_group->args( 'repeatable' ) ? ' repeatable' : ' non-repeatable';
+
+		$group_wrap_attributes = array(
+			'class' => $classes,
+			'style' => 'width:100%;',
+		);
+
+		/**
+		 * Allow for adding additional HTML attributes to a group wrapper.
+		 *
+		 * The attributes will be an array of key => value pairs for each attribute.
+		 *
+		 * @since 2.2.2
+		 *
+		 * @param string     $group_wrap_attributes Current attributes array.
+		 *
+		 * @param CMB2_Field $field_group           The group CMB2_Field object.
+		 */
+		$group_wrap_attributes = apply_filters( 'cmb2_group_wrap_attributes', $group_wrap_attributes, $field_group );
+
+		return CMB2_Utils::concat_attrs( $group_wrap_attributes );
+	}
+
+	/**
 	 * Render a repeatable group row
 	 * @since  1.0.2
 	 * @param  CMB2_Field $field_group  CMB2_Field group field object
@@ -411,11 +425,11 @@ class CMB2 {
 		<div class="postbox cmb-row cmb-repeatable-grouping', $closed_class, '" data-iterator="', $field_group->index, '">';
 
 			if ( $field_group->args( 'repeatable' ) ) {
-				echo '<button type="button" ', $remove_disabled, 'data-selector="', $field_group->id(), '_repeat" class="dashicons-before dashicons-no-alt cmb-remove-group-row"></button>';
+				echo '<button type="button" ', $remove_disabled, 'data-selector="', $field_group->id(), '_repeat" class="dashicons-before dashicons-no-alt cmb-remove-group-row" title="', esc_attr( $field_group->options( 'remove_button' ) ), '"></button>';
 			}
 
 			echo '
-			<div class="cmbhandle" title="' , __( 'Click to toggle', 'give' ), '"><br></div>
+			<div class="cmbhandle" title="' , esc_attr__( 'Click to toggle', 'cmb2' ), '"><br></div>
 			<h3 class="cmb-group-title cmbhandle-title"><span>', $field_group->replace_hash( $field_group->options( 'group_title' ) ), '</span></h3>
 
 			<div class="inside cmb-td cmb-nested cmb-field-list">';
@@ -454,7 +468,8 @@ class CMB2 {
 	/**
 	 * Add a hidden field to the list of hidden fields to be rendered later
 	 * @since 2.0.0
-	 * @param array  $field_args Array of field arguments to be passed to CMB2_Field
+	 * @param array           $field_args  Array of field arguments to be passed to CMB2_Field
+	 * @param CMB2_Field|null $field_group CMB2_Field group field object
 	 */
 	public function add_hidden_field( $field_args, $field_group = null ) {
 		if ( isset( $field_args['field_args'] ) ) {
@@ -464,7 +479,13 @@ class CMB2 {
 			$field = $this->get_new_field( $field_args, $field_group );
 		}
 
-		$this->hidden_fields[] = new CMB2_Types( $field );
+		$type = new CMB2_Types( $field );
+
+		if ( $field_group ) {
+			$type->iterator = $field_group->index;
+		}
+
+		$this->hidden_fields[] = $type;
 
 		return $field;
 	}
@@ -656,8 +677,8 @@ class CMB2 {
 	/**
 	 * Save a repeatable group
 	 * @since  1.x.x
-	 * @param  array $field_group CMB2_Field group field object
-	 * @return mixed              Return of CMB2_Field::update_data()
+	 * @param  CMB2_Field $field_group CMB2_Field group field object
+	 * @return mixed                   Return of CMB2_Field::update_data()
 	 */
 	public function save_group_field( $field_group ) {
 		$base_id = $field_group->id();
@@ -712,8 +733,8 @@ class CMB2 {
 					? $old[ $field_group->index ][ $sub_id ]
 					: false;
 
-				$is_updated = ( ! cmb2_utils()->isempty( $new_val ) && $new_val !== $old_val );
-				$is_removed = ( cmb2_utils()->isempty( $new_val ) && ! cmb2_utils()->isempty( $old_val ) );
+				$is_updated = ( ! CMB2_Utils::isempty( $new_val ) && $new_val !== $old_val );
+				$is_removed = ( CMB2_Utils::isempty( $new_val ) && ! CMB2_Utils::isempty( $old_val ) );
 
 				// Compare values and add to `$updated` array
 				if ( $is_updated || $is_removed ) {
@@ -725,10 +746,10 @@ class CMB2 {
 
 			}
 
-			$saved[ $field_group->index ] = cmb2_utils()->filter_empty( $saved[ $field_group->index ] );
+			$saved[ $field_group->index ] = CMB2_Utils::filter_empty( $saved[ $field_group->index ] );
 		}
 
-		$saved = cmb2_utils()->filter_empty( $saved );
+		$saved = CMB2_Utils::filter_empty( $saved );
 
 		return $field_group->update_data( $saved, true );
 	}
@@ -794,32 +815,18 @@ class CMB2 {
 			return $this->mb_object_type;
 		}
 
-		$registered_types = $this->prop( 'object_types' );
+		$registered_types = $this->box_types();
 
-		if ( ! $registered_types ) {
-			$this->mb_object_type = 'post';
-			return $this->mb_object_type;
-		}
-
-		$type = false;
-
-		// check if 'object_types' is a string
-		if ( is_string( $registered_types ) ) {
-			$type = $registered_types;
-		}
+		$type = '';
 
 		// if it's an array of one, extract it
-		elseif ( is_array( $registered_types ) && 1 === count( $registered_types ) ) {
+		if ( 1 === count( $registered_types ) ) {
 			$last = end( $registered_types );
 			if ( is_string( $last ) ) {
 				$type = $last;
 			}
-		} elseif ( is_array( $registered_types ) ) {
-			$page_type = $this->current_object_type();
-
-			if ( in_array( $page_type, $registered_types, true ) ) {
-				$type = $page_type;
-			}
+		} elseif ( ( $curr_type = $this->current_object_type() ) && in_array( $curr_type, $registered_types, true ) ) {
+			$type = $curr_type;
 		}
 
 		// Get our object type
@@ -837,6 +844,15 @@ class CMB2 {
 		}
 
 		return $this->mb_object_type;
+	}
+
+	/**
+	 * Gets the box 'object_types' array based on box settings.
+	 * @since  2.2.3
+	 * @return array Object types
+	 */
+	public function box_types() {
+		return CMB2_Utils::ensure_array( $this->prop( 'object_types' ), array( 'post' ) );
 	}
 
 	/**
@@ -924,11 +940,11 @@ class CMB2 {
 	 * Get a field object
 	 * @since  2.0.3
 	 * @param  string|array|CMB2_Field $field       Metabox field id or field config array or CMB2_Field object
-	 * @param  CMB2_Field              $field_group (optional) CMB2_Field object (group parent)
-	 * @return CMB2_Field|false CMB2_Field object (or false)
+	 * @param  CMB2_Field|null         $field_group (optional) CMB2_Field object (group parent)
+	 * @return CMB2_Field|false                     CMB2_Field object (or false)
 	 */
 	public function get_field( $field, $field_group = null ) {
-		if ( is_a( $field, 'CMB2_Field' ) ) {
+		if ( $field instanceof CMB2_Field ) {
 			return $field;
 		}
 
@@ -956,11 +972,11 @@ class CMB2 {
 	/**
 	 * Handles determining which type of arguments to pass to CMB2_Field
 	 * @since  2.0.7
-	 * @param  mixed  $field_id     Field (or group field) ID
-	 * @param  mixed  $field_args   Array of field arguments
-	 * @param  mixed  $sub_field_id Sub field ID (if field_group exists)
-	 * @param  mixed  $field_group  If a sub-field, will be the parent group CMB2_Field object
-	 * @return array                Array of CMB2_Field arguments
+	 * @param  mixed           $field_id     Field (or group field) ID
+	 * @param  mixed           $field_args   Array of field arguments
+	 * @param  mixed           $sub_field_id Sub field ID (if field_group exists)
+	 * @param  CMB2_Field|null $field_group  If a sub-field, will be the parent group CMB2_Field object
+	 * @return array                         Array of CMB2_Field arguments
 	 */
 	public function get_field_args( $field_id, $field_args, $sub_field_id, $field_group ) {
 
@@ -1046,7 +1062,7 @@ class CMB2 {
 	 * @since  2.0.0
 	 * @param  array  $field           Metabox field config array
 	 * @param  int    $position        (optional) Position of metabox. 1 for first, etc
-	 * @return mixed                   Field id or false
+	 * @return string|false            Field id or false
 	 */
 	public function add_field( array $field, $position = 0 ) {
 		if ( ! is_array( $field ) || ! array_key_exists( 'id', $field ) ) {
@@ -1059,14 +1075,11 @@ class CMB2 {
 		}
 
 		if ( isset( $field['column'] ) && false !== $field['column'] ) {
-			$this->has_columns = true;
+			$field = $this->define_field_column( $field );
+		}
 
-			$column = is_array( $field['column'] ) ? $field['column'] : array();
-
-			$field['column'] = wp_parse_args( $column, array(
-				'name'     => isset( $field['name'] ) ? $field['name'] : '',
-				'position' => false,
-			) );
+		if ( isset( $field['taxonomy'] ) && ! empty( $field['remove_default'] ) ) {
+			$this->tax_metaboxes_to_remove[ $field['taxonomy'] ] = $field['taxonomy'];
 		}
 
 		$this->_add_field_to_array(
@@ -1076,6 +1089,25 @@ class CMB2 {
 		);
 
 		return $field['id'];
+	}
+
+	/**
+	 * Defines a field's column if requesting to be show in admin columns.
+	 * @since  2.2.3
+	 * @param  array  $field Metabox field config array.
+	 * @return array         Modified metabox field config array.
+	 */
+	protected function define_field_column( array $field ) {
+		$this->has_columns = true;
+
+		$column = is_array( $field['column'] ) ? $field['column'] : array();
+
+		$field['column'] = wp_parse_args( $column, array(
+			'name'     => isset( $field['name'] ) ? $field['name'] : '',
+			'position' => false,
+		) );
+
+		return $field;
 	}
 
 	/**
@@ -1119,7 +1151,7 @@ class CMB2 {
 	 */
 	protected function _add_field_to_array( $field, &$fields, $position = 0 ) {
 		if ( $position ) {
-			cmb2_utils()->array_insert( $fields, array( $field['id'] => $field ), $position );
+			CMB2_Utils::array_insert( $fields, array( $field['id'] => $field ), $position );
 		} else {
 			$fields[ $field['id'] ] = $field;
 		}
@@ -1233,22 +1265,13 @@ class CMB2 {
 	}
 
 	/**
-	 * Determine whether this cmb object should show, based on the 'show_on_cb' callback.
-	 *
-	 * @since 2.0.9
-	 *
-	 * @return bool Whether this cmb should be shown.
+	 * Handles metabox property callbacks, and passes this $cmb object as property.
+	 * @since  2.2.3
+	 * @param  callable $cb The callback method/function/closure
+	 * @return mixed        Return of the callback function.
 	 */
-	public function should_show() {
-		// Default to showing this cmb
-		$show = true;
-
-		// Use the callback to determine showing the cmb, if it exists
-		if ( is_callable( $this->prop( 'show_on_cb' ) ) ) {
-			$show = (bool) call_user_func( $this->prop( 'show_on_cb' ), $this );
-		}
-
-		return $show;
+	protected function do_callback( $cb ) {
+		return call_user_func( $cb, $this );
 	}
 
 	/**
@@ -1281,15 +1304,12 @@ class CMB2 {
 	 */
 	public function __get( $field ) {
 		switch ( $field ) {
-			case 'cmb_id':
-			case 'meta_box':
 			case 'updated':
 			case 'has_columns':
+			case 'tax_metaboxes_to_remove':
 				return $this->{$field};
-			case 'object_id':
-				return $this->object_id();
 			default:
-				throw new Exception( 'Invalid ' . __CLASS__ . ' property: ' . $field );
+				return parent::__get( $field );
 		}
 	}
 
--- includes/libraries/cmb2/includes/CMB2_JS.php
+++ includes/libraries/cmb2/includes/CMB2_JS.php
@@ -50,6 +50,11 @@ class CMB2_JS {
 		// Filter required script dependencies
 		$dependencies = apply_filters( 'cmb2_script_dependencies', self::$dependencies );
 
+		// Only use minified files if SCRIPT_DEBUG is off
+		$debug = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG;
+
+		$min = $debug ? '' : '.min';
+
 		// if colorpicker
 		if ( ! is_admin() && isset( $dependencies['wp-color-picker'] ) ) {
 			self::colorpicker_frontend();
@@ -62,15 +67,20 @@ class CMB2_JS {
 
 		// if timepicker
 		if ( isset( $dependencies['jquery-ui-datetimepicker'] ) ) {
-			wp_register_script( 'jquery-ui-datetimepicker', cmb2_utils()->url( 'js/jquery-ui-timepicker-addon.min.js' ), array( 'jquery-ui-slider' ), CMB2_VERSION );
+			wp_register_script( 'jquery-ui-datetimepicker', CMB2_Utils::url( 'js/jquery-ui-timepicker-addon.min.js' ), array( 'jquery-ui-slider' ), CMB2_VERSION );
 		}
 
-		// Only use minified files if SCRIPT_DEBUG is off
-		$debug = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG;
-		$min   = $debug ? '' : '.min';
+		// if cmb2-wysiwyg
+		$enqueue_wysiwyg = isset( $dependencies['cmb2-wysiwyg'] ) && $debug;
+		unset( $dependencies['cmb2-wysiwyg'] );
 
-		// Register cmb JS
-		wp_enqueue_script( self::$handle, cmb2_utils()->url( "js/cmb2{$min}.js" ), $dependencies, CMB2_VERSION, true );
+		// Enqueue cmb JS
+		wp_enqueue_script( self::$handle, CMB2_Utils::url( "js/cmb2{$min}.js" ), $dependencies, CMB2_VERSION, true );
+
+		// if SCRIPT_DEBUG, we need to enqueue separately.
+		if ( $enqueue_wysiwyg ) {
+			wp_enqueue_script( 'cmb2-wysiwyg', CMB2_Utils::url( 'js/cmb2-wysiwyg.js' ), array( 'jquery', 'wp-util' ), CMB2_VERSION );
+		}
 
 		self::localize( $debug );
 	}
@@ -83,10 +93,10 @@ class CMB2_JS {
 		wp_register_script( 'iris', admin_url( 'js/iris.min.js' ), array( 'jquery-ui-draggable', 'jquery-ui-slider', 'jquery-touch-punch' ), CMB2_VERSION );
 		wp_register_script( 'wp-color-picker', admin_url( 'js/color-picker.min.js' ), array( 'iris' ), CMB2_VERSION );
 		wp_localize_script( 'wp-color-picker', 'wpColorPickerL10n', array(
-			'clear'         => __( 'Clear', 'give' ),
-			'defaultString' => __( 'Default', 'give' ),
-			'pick'          => __( 'Select Color', 'give' ),
-			'current'       => __( 'Current Color', 'give' ),
+			'clear'         => esc_html__( 'Clear', 'cmb2' ),
+			'defaultString' => esc_html__( 'Default', 'cmb2' ),
+			'pick'          => esc_html__( 'Select Color', 'cmb2' ),
+			'current'       => esc_html__( 'Current Color', 'cmb2' ),
 		) );
 	}
 
@@ -95,6 +105,12 @@ class CMB2_JS {
 	 * @since  2.0.7
 	 */
 	protected static function localize( $debug ) {
+		static $localized = false;
+		if ( $localized ) {
+			return;
+		}
+
+		$localized = true;
 		$l10n = array(
 			'ajax_nonce'       => wp_create_nonce( 'ajax_nonce' ),
 			'ajaxurl'          => admin_url( '/admin-ajax.php' ),
@@ -106,39 +122,39 @@ class CMB2_JS {
 				'date_picker'  => array(
 					'changeMonth'     => true,
 					'changeYear'      => true,
-					'dateFormat'      => _x( 'mm/dd/yy', 'Valid formatDate string for jquery-ui datepicker', 'give' ),
-					'dayNames'        => explode( ',', __( 'Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday', 'give' ) ),
-					'dayNamesMin'     => explode( ',', __( 'Su, Mo, Tu, We, Th, Fr, Sa', 'give' ) ),
-					'dayNamesShort'   => explode( ',', __( 'Sun, Mon, Tue, Wed, Thu, Fri, Sat', 'give' ) ),
-					'monthNames'      => explode( ',', __( 'January, February, March, April, May, June, July, August, September, October, November, December', 'give' ) ),
-					'monthNamesShort' => explode( ',', __( 'Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec', 'give' ) ),
-					'nextText'        => __( 'Next', 'give' ),
-					'prevText'        => __( 'Prev', 'give' ),
-					'currentText'     => __( 'Today', 'give' ),
-					'closeText'       => __( 'Done', 'give' ),
-					'clearText'       => __( 'Clear', 'give' ),
+					'dateFormat'      => _x( 'mm/dd/yy', 'Valid formatDate string for jquery-ui datepicker', 'cmb2' ),
+					'dayNames'        => explode( ',', esc_html__( 'Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday', 'cmb2' ) ),
+					'dayNamesMin'     => explode( ',', esc_html__( 'Su, Mo, Tu, We, Th, Fr, Sa', 'cmb2' ) ),
+					'dayNamesShort'   => explode( ',', esc_html__( 'Sun, Mon, Tue, Wed, Thu, Fri, Sat', 'cmb2' ) ),
+					'monthNames'      => explode( ',', esc_html__( 'January, February, March, April, May, June, July, August, September, October, November, December', 'cmb2' ) ),
+					'monthNamesShort' => explode( ',', esc_html__( 'Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec', 'cmb2' ) ),
+					'nextText'        => esc_html__( 'Next', 'cmb2' ),
+					'prevText'        => esc_html__( 'Prev', 'cmb2' ),
+					'currentText'     => esc_html__( 'Today', 'cmb2' ),
+					'closeText'       => esc_html__( 'Done', 'cmb2' ),
+					'clearText'       => esc_html__( 'Clear', 'cmb2' ),
 				),
 				'time_picker'  => array(
-					'timeOnlyTitle' => __( 'Choose Time', 'give' ),
-					'timeText'      => __( 'Time', 'give' ),
-					'hourText'      => __( 'Hour', 'give' ),
-					'minuteText'    => __( 'Minute', 'give' ),
-					'secondText'    => __( 'Second', 'give' ),
-					'currentText'   => __( 'Now', 'give' ),
-					'closeText'     => __( 'Done', 'give' ),
-					'timeFormat'    => _x( 'hh:mm TT', 'Valid formatting string, as per http://trentrichardson.com/examples/timepicker/', 'give' ),
+					'timeOnlyTitle' => esc_html__( 'Choose Time', 'cmb2' ),
+					'timeText'      => esc_html__( 'Time', 'cmb2' ),
+					'hourText'      => esc_html__( 'Hour', 'cmb2' ),
+					'minuteText'    => esc_html__( 'Minute', 'cmb2' ),
+					'secondText'    => esc_html__( 'Second', 'cmb2' ),
+					'currentText'   => esc_html__( 'Now', 'cmb2' ),
+					'closeText'     => esc_html__( 'Done', 'cmb2' ),
+					'timeFormat'    => _x( 'hh:mm TT', 'Valid formatting string, as per http://trentrichardson.com/examples/timepicker/', 'cmb2' ),
 					'controlType'   => 'select',
 					'stepMinute'    => 5,
 				),
 			),
 			'strings' => array(
-				'upload_file'  => __( 'Use this file', 'give' ),
-				'upload_files' => __( 'Use these files', 'give' ),
-				'remove_image' => __( 'Remove Image', 'give' ),
-				'remove_file'  => __( 'Remove', 'give' ),
-				'file'         => __( 'File:', 'give' ),
-				'download'     => __( 'Download', 'give' ),
-				'check_toggle' => __( 'Select / Deselect All', 'give' ),
+				'upload_file'  => esc_html__( 'Use this file', 'cmb2' ),
+				'upload_files' => esc_html__( 'Use these files', 'cmb2' ),
+				'remove_image' => esc_html__( 'Remove Image', 'cmb2' ),
+				'remove_file'  => esc_html__( 'Remove', 'cmb2' ),
+				'file'         => esc_html__( 'File:', 'cmb2' ),
+				'download'     => esc_html__( 'Download', 'cmb2' ),
+				'check_toggle' => esc_html__( 'Select / Deselect All', 'cmb2' ),
 			),
 		);
 
