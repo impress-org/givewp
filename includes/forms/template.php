ec6be154ce618daadc2e329d91cd00983d4af642
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2016-11-21 12:44:13 +0530

    update function to add class coditionally to donate button

--- includes/forms/template.php
+++ includes/forms/template.php
@@ -590,7 +590,7 @@ function give_display_checkout_button( $form_id, $args ) {
 		? $args['display_style']
 		: get_post_meta( $form_id, '_give_payment_display', true );
 
-	if( give_is_setting_enabled( $args['button_only'] ) ) {
+	if( (bool) ( isset( $args['display_style'] ) && 'button_only' === $args['display_style'] ) ) {
 		$display_option = 'modal';
 	}elseif ( $display_option === 'onpage' ) {
 		return '';
