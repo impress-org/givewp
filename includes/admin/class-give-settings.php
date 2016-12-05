b5c8abdcb01fb7e8706b26101fa7a964328a2b65
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2016-12-05 11:08:46 +0530

    Fix typo

--- includes/admin/class-give-settings.php
+++ includes/admin/class-give-settings.php
@@ -472,7 +472,7 @@ class Give_Plugin_Settings {
 
 						array(
 							'name'    => esc_html__( 'Featured Image Size', 'give' ),
-							'desc'    => esc_html__( 'The Featured Image is an image that is chosen as the representative image for a donation form. Some themes may have custom featured image sizes. Please select the size you would like to display for your single donation forms\' featured image.', 'give' ),
+							'desc'    => esc_html__( 'The Featured Image is an image that is chosen as the representative image for a donation form. Some themes may have custom featured image sizes. Please select the size you would like to display for your single donation form\'s featured image.', 'give' ),
 							'id'      => 'featured_image_size',
 							'type'    => 'select',
 							'default' => 'large',
