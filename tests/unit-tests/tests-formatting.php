1dc51c9691a6a085327bc18593daffb836cce76d
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2016-12-06 21:23:24 +0530

    Add test for give_get_cache_keyn function

--- tests/unit-tests/tests-formatting.php
+++ tests/unit-tests/tests-formatting.php
@@ -12,13 +12,37 @@ class Tests_Formatting extends Give_Unit_Test_Case {
 		parent::tearDown();
 	}
 
+
+	/**
+	 * Test give_get_cache_key function.
+	 * @since 1.8
+	 * @cover give_get_cache_key
+	 */
+	function test_give_get_cache_key() {
+		// Action.
+		$input_action = 'get_log_count';
+
+		// Basic query param.
+		$input_query_args = array(
+			'post_parent'    => 1024,
+			'post_type'      => 'give_log',
+			'posts_per_page' => - 1,
+			'post_status'    => 'publish',
+			'fields'         => 'ids',
+		);
+
+		$output = give_get_cache_key( $input_action, $input_query_args );
+
+		$this->assertEquals( 'give_cache_get_log_count_01f5c4012ed8142', $output );
+	}
+
 	/**
 	 * Test give_clean() - note this is a basic type test as WP core already.
 	 * has coverage for sanitized_text_field().
 	 *
 	 * @since 1.8
 	 */
-	public function test_wc_clean() {
+	public function test_give_clean() {
 		$this->assertEquals( 'cleaned', give_clean( '<script>alert();</script>cleaned' ) );
 	}
 
