1a0b926c90eaf265246abb76c7fe01a63ed5005e
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2016-12-16 16:48:40 +0530

    Update email nottification setting fields

--- includes/admin/emails/abstract-email-notification.php
+++ includes/admin/emails/abstract-email-notification.php
@@ -121,32 +121,31 @@ if ( ! class_exists( 'Give_Email_Notification' ) ) :
 
 			$setting_fields = array(
 				array(
-					'id'   => 'give_title_email_settings_2',
-					'type' => 'title',
+					'id'    => "give_title_email_settings_{$this->id}",
+					'type'  => 'title',
+					'title' => $this->label,
 				),
 				array(
-					'name' => esc_html__( 'Preview Email', 'give' ),
-					'desc' => esc_html__( 'Click the buttons to preview emails.', 'give' ),
-					'id'   => 'give_email_preview_buttons',
-					'type' => 'email_preview_buttons',
+					'name'    => esc_html__( 'Notification', 'give' ),
+					'desc'    => esc_html__( 'Choose option if you want to send email notification or not.', 'give' ),
+					'id'      => "{$this->id}_notification",
+					'type'    => 'radio_inline',
+					'default' => 'enabled',
+					'options' => array(
+						'enabled' => __( 'Enabled', 'give' ),
+						'disabled' => __( 'Disabled', 'give' ),
+					)
 				),
 				array(
-					'id'      => 'admin_notice_emails',
-					'name'    => esc_html__( 'Donation Notification Emails', 'give' ),
-					'desc'    => __( 'Enter the email address(es) that should receive a notification anytime a donation is made, please only enter <span class="give-underline">one email address per line</span> and <strong>not separated by commas</strong>.', 'give' ),
-					'type'    => 'textarea',
-					'default' => get_bloginfo( 'admin_email' ),
-				),
-				array(
-					'id'      => 'donation_subject',
-					'name'    => esc_html__( 'Donation Email Subject', 'give' ),
+					'id'      => "{$this->id}_email_subject",
+					'name'    => esc_html__( 'Email Subject', 'give' ),
 					'desc'    => esc_html__( 'Enter the subject line for the donation receipt email.', 'give' ),
 					'default' => esc_attr__( 'Donation Receipt', 'give' ),
 					'type'    => 'text',
 				),
 				array(
-					'id'      => 'donation_receipt',
-					'name'    => esc_html__( 'Donation Receipt', 'give' ),
+					'id'      => "{$this->id}_email_message",
+					'name'    => esc_html__( 'Email message', 'give' ),
 					'desc'    => sprintf(
 					/* translators: %s: emails tags list */
 						esc_html__( 'Enter the email that is sent to users after completing a successful donation. HTML is accepted. Available template tags: %s', 'give' ),
@@ -154,14 +153,77 @@ if ( ! class_exists( 'Give_Email_Notification' ) ) :
 					),
 					'type'    => 'wysiwyg',
 					'default' => give_get_default_donation_receipt_email(),
-				),
-				array(
-					'id'   => 'give_title_email_settings_2',
-					'type' => 'sectionend',
-				),
+				)
+			);
+
+
+			// Add extra setting field.
+			if( $extra_setting_field = $this->get_extra_setting_fields() ) {
+				$setting_fields = array_merge( $setting_fields, $extra_setting_field );
+			}
+
+			// Recipient field.
+			if( $this->has_recipient_field ) {
+				$setting_fields[] = $this->get_recipient_setting_field();
+			}
+
+			// Preview field.
+			if( $this->has_preview ) {
+				$setting_fields[] = $this->get_preview_setting_field();
+			}
+
+			// Add section end field.
+			$setting_fields[] = array(
+				'id'   => "give_title_email_settings_{$this->id}",
+				'type' => 'sectionend',
 			);
 
 			return $setting_fields;
+		}
+
+		/**
+		 * Get extra setting field.
+		 *
+		 * @since  1.8
+		 * @access public
+		 * @return array
+		 */
+		public function get_extra_setting_fields() {
+			return array();
+		}
+
+		/**
+		 * Get recipient setting field.
+		 *
+		 * @since  1.8
+		 * @access public
+		 * @return array
+		 */
+		function get_recipient_setting_field() {
+			return array(
+				'id'      => "{$this->id}_recipient",
+				'name'    => esc_html__( 'Donation Notification Emails', 'give' ),
+				'desc'    => __( 'Enter the email address(es) that should receive a notification anytime a donation is made, please only enter <span class="give-underline">one email address per line</span> and <strong>not separated by commas</strong>.', 'give' ),
+				'type'    => 'textarea',
+				'default' => get_bloginfo( 'admin_email' ),
+			);
+		}
+
+
+		/**
+		 * Get preview setting field.
+		 *
+		 * @since  1.8
+		 * @access public
+		 * @return array
+		 */
+		public function get_preview_setting_field(){
+			return array(
+				'name' => esc_html__( 'Preview Email', 'give' ),
+				'desc' => esc_html__( 'Click the buttons to preview emails.', 'give' ),
+				'id'   => "{$this->id}_preview_buttons",
+				'type' => 'email_preview_buttons',
+			);
 		}
 
 		/**
