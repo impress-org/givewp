37eacbd60fe87d25c38235ad9111ad35d8dc3b10
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2016-12-19 11:50:58 +0530

    Add default donation receipt message

--- includes/admin/emails/class-donation-receipt-email.php
+++ includes/admin/emails/class-donation-receipt-email.php
@@ -54,6 +54,40 @@ if ( ! class_exists( 'Give_Donation_Receipt_Email' ) ) :
 		public function get_default_email_subject() {
 			return esc_attr__( 'Donation Receipt', 'give' );
 		}
+
+
+		/**
+		 * Get default email message.
+		 *
+		 * @since  1.8
+		 * @access public
+		 * @return string
+		 */
+		public function get_default_email_message() {
+			$message = esc_html__( 'Dear', 'give' ) . " {name},\n\n";
+			$message .= esc_html__( 'Thank you for your donation. Your generosity is appreciated! Here are the details of your donation:', 'give' ) . "\n\n";
+			$message .= '<strong>' . esc_html__( 'Donor:', 'give' ) . '</strong> {fullname}' . "\n";
+			$message .= '<strong>' . esc_html__( 'Donation:', 'give' ) . '</strong> {donation}' . "\n";
+			$message .= '<strong>' . esc_html__( 'Donation Date:', 'give' ) . '</strong> {date}' . "\n";
+			$message .= '<strong>' . esc_html__( 'Amount:', 'give' ) . '</strong> {amount}' . "\n";
+			$message .= '<strong>' . esc_html__( 'Payment Method:', 'give' ) . '</strong> {payment_method}' . "\n";
+			$message .= '<strong>' . esc_html__( 'Payment ID:', 'give' ) . '</strong> {payment_id}' . "\n";
+			$message .= '<strong>' . esc_html__( 'Receipt ID:', 'give' ) . '</strong> {receipt_id}' . "\n\n";
+			$message .= '{receipt_link}' . "\n\n";
+			$message .= "\n\n";
+			$message .= esc_html__( 'Sincerely,', 'give' ) . "\n";
+			$message .= '{sitename}' . "\n";
+
+
+			/**
+			 * Filter the donation receipt email message
+			 *
+			 * @since 1.8
+			 *
+			 * @param string $message
+			 */
+			return apply_filters( 'give_default_donation_receipt_email', $message );
+		}
 	}
 
 endif; // End class_exists check
