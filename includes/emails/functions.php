dafad10eb741f6b99ced6f792863e14102099b29
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2017-01-06 09:00:52 +0530

    Update give_admin_notices_disabled fx

--- includes/emails/functions.php
+++ includes/emails/functions.php
@@ -273,7 +273,7 @@ function give_admin_notices_disabled( $payment_id = 0 ) {
 
 	return apply_filters(
 		'give_admin_notices_disabled',
-		! give_is_setting_enabled( give_get_option( 'admin_notices' ) ),
+		! give_is_setting_enabled( give_get_option( 'donation-receipt_notification' ) ),
 		$payment_id
 	);
 }
