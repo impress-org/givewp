f434b83dc5b97791dc6b36a37d0c7971ff6ef22b
Devin Walker <devin@wordimpress.com>
2017-04-10 13:34:36 -0700

    Versions updated and readme added for 1.8.6

--- give.php
+++ give.php
@@ -5,7 +5,7 @@
  * Description: The most robust, flexible, and intuitive way to accept donations on WordPress.
  * Author: WordImpress
  * Author URI: https://wordimpress.com
- * Version: 1.8.5
+ * Version: 1.8.6
  * Text Domain: give
  * Domain Path: /languages
  * GitHub Plugin URI: https://github.com/WordImpress/Give
@@ -260,7 +260,7 @@ if ( ! class_exists( 'Give' ) ) :
 
 			// Plugin version
 			if ( ! defined( 'GIVE_VERSION' ) ) {
-				define( 'GIVE_VERSION', '1.8.5' );
+				define( 'GIVE_VERSION', '1.8.6' );
 			}
 
 			// Plugin Folder Path
