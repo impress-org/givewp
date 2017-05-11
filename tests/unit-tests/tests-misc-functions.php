66512cb0bf1e62db4093805e53035136766a9824
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2017-05-11 21:40:20 +0530

    Add: phpunit tests

--- tests/unit-tests/tests-misc-functions.php
+++ tests/unit-tests/tests-misc-functions.php
@@ -32,6 +32,7 @@ class Tests_MISC_Functions extends Give_Unit_Test_Case {
 
 	/**
 	 * Data Provider
+	 *
 	 * @todo  Add more currencies for testing.
 	 *
 	 * @since 1.8.8
@@ -44,5 +45,37 @@ class Tests_MISC_Functions extends Give_Unit_Test_Case {
 			array( give_get_currency_name( 'TWD' ), __( 'Taiwan New Dollars', 'give' ) ),
 			array( give_get_currency_name( 'Wrong_Currency_Symbol' ), '' ),
 		);
+	}
+
+	/**
+	 * test for give post type meta related functions
+	 *
+	 * @since         1.8.8
+	 * @access        public
+	 *
+	 * @cover         give_get_meta
+	 * @cover         give_update_meta
+	 * @cover         give_delete_meta
+	 */
+	public function test_give_meta_helpers() {
+		$payment = Give_Helper_Payment::create_simple_payment();
+
+		$value = give_get_meta( $payment, 'testing_meta', true, 'TEST1' );
+		$this->assertEquals( 'TEST1', $value );
+
+		$status = give_update_meta( $payment, 'testing_meta', 'TEST' );
+		$this->assertEquals( true, (bool) $status );
+
+		$status = give_update_meta( $payment, 'testing_meta', 'TEST' );
+		$this->assertEquals( false, (bool) $status );
+
+		$value = give_get_meta( $payment, 'testing_meta', true );
+		$this->assertEquals( 'TEST', $value );
+
+		$status = give_delete_meta( $payment, 'testing_meta', 'TEST2' );
+		$this->assertEquals( false, $status );
+
+		$status = give_delete_meta( $payment, 'testing_meta' );
+		$this->assertEquals( true, $status );
 	}
 }
