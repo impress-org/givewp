33da01ccb36dcc86360584d59523e8d662ef0727
Devin Walker <sdtucson@gmail.com>
2017-05-31 16:27:43 -0700

    Don't use short brackets and renamed a few actions to be better reflective of their purpose

--- includes/deprecated/deprecated-actions.php
+++ includes/deprecated/deprecated-actions.php
@@ -15,7 +15,7 @@ foreach ( $give_map_deprecated_actions as $new => $old ) {
  */
 function give_deprecated_actions() {
 
-	$give_deprecated_actions = [
+	$give_deprecated_actions = array(
 		// New action hook                            Old action hook.
 		'give_donation_form_login_fields'          => 'give_purchase_form_login_fields',
 		'give_donation_form_register_login_fields' => 'give_purchase_form_register_login_fields',
@@ -23,7 +23,6 @@ function give_deprecated_actions() {
 		'give_donation_form_before_cc_form'        => 'give_purchase_form_before_cc_form',
 		'give_donation_form_after_cc_form'         => 'give_purchase_form_after_cc_form',
 		'give_donation_form_no_access'             => 'give_purchase_form_no_access',
-		'give_donation_form_bottom'                => 'give_purchase_form_bottom',
 		'give_donation_form_register_fields'       => 'give_purchase_form_register_fields',
 		'give_donation_form_after_user_info'       => 'give_purchase_form_after_user_info',
 		'give_donation_form_before_personal_info'  => 'give_purchase_form_before_personal_info',
@@ -39,7 +38,8 @@ function give_deprecated_actions() {
 		'give_donation_history_header_after'       => 'give_purchase_history_header_after',
 		'give_donation_history_row_start'          => 'give_purchase_history_row_start',
 		'give_donation_history_row_end'            => 'give_purchase_history_row_end',
-		'give_donation_form_top'                   => 'give_purchase_form_top',
+		'give_payment_form_top'                    => 'give_purchase_form_top',
+		'give_payment_form_bottom'                 => 'give_purchase_form_bottom',
 		'give_pre_process_donation'                => 'give_pre_process_purchase',
 		'give_complete_donation'                   => 'give_complete_purchase',
 		'give_ajax_donation_errors'                => 'give_ajax_checkout_errors',
@@ -80,8 +80,8 @@ function give_deprecated_actions() {
 		'give_donor_pre_set_primary_email'         => 'give_customer_pre_set_primary_email',
 		'give_donor_post_set_primary_email'        => 'give_customer_post_set_primary_email',
 		'give_donation_form_top'                   => 'give_checkout_form_top',
-		'give_donation_form_bottom'                   => 'give_checkout_form_bottom',
-	];
+		'give_donation_form_bottom'                => 'give_checkout_form_bottom',
+	);
 
 	return $give_deprecated_actions;
 }
