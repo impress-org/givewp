cabfaca76c2fe3eff85952acbf5e23af96f0f479
Devin Walker <sdtucson@gmail.com>
2017-06-02 16:44:51 -0700

    More testing

--- tests/unit-tests/tests-api.php
+++ tests/unit-tests/tests-api.php
@@ -505,7 +505,7 @@ class Tests_API extends Give_Unit_Test_Case {
 		$this->assertArrayHasKey( 'content', $out['forms'][0]['info'] );
 		$this->assertEquals( 'Post content 1', $out['forms'][0]['info']['content'] );
 		$this->assertArrayHasKey( 'thumbnail', $out['forms'][0]['info'] );
-		
+
 		$this->assertArrayHasKey( 'stats', $out['forms'][0] );
 		$this->assertArrayHasKey( 'total', $out['forms'][0]['stats'] );
 		$this->assertArrayHasKey( 'donations', $out['forms'][0]['stats']['total'] );
