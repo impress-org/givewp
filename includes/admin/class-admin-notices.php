688290218efcb0ef76c03faf5be5647aed82f57d
mehul0810 <mehul.gohil0810@gmail.com>
2017-06-06 08:51:23 +0530

    Fixed Message sprintf warning

--- includes/admin/class-admin-notices.php
+++ includes/admin/class-admin-notices.php
@@ -206,7 +206,7 @@ class Give_Notices {
 			$html .= '<p><strong>' . __( 'To which version should I update?', 'give' ) . '</strong></p>';
 			$html .= '<p>' . sprintf( __( 'You should update your PHP version to either 5.6 or to 7.0 or 7.1. On a normal WordPress site, switching to PHP 5.6 should never cause issues. We would however actually recommend you switch to PHP7. There are some plugins that are not ready for PHP7 though, so do some testing first. We have an article on how to test whether that\'s an option for you %1$shere%2$s. PHP7 is much faster than PHP 5.6. It\'s also the only PHP version still in active development and therefore the better option for your site in the long run.', 'give' ), '<a href="https://yoa.st/wg" target="_blank">', '</a>' ) . '</p>';
 			$html .= '<p><strong>' . __( 'Can\'t update? Ask your host!', 'give' ) . '</strong></p>';
-			$html .= '<p>' . sprintf( __( 'If you cannot upgrade your PHP version yourself, you can send an email to your host. If they don\'t want to upgrade your PHP version, we would suggest you switch hosts. Have a look at one of the recommended %3$sWordPress hosting partners%4$s.', 'give' ), sprintf( '<a href="%1$s" target="_blank">', esc_url( 'https://wordpress.org/hosting/' ) ), '</a>' ) . '</p>';
+			$html .= '<p>' . sprintf( __( 'If you cannot upgrade your PHP version yourself, you can send an email to your host. If they don\'t want to upgrade your PHP version, we would suggest you switch hosts. Have a look at one of the recommended %1$sWordPress hosting partners%2$s.', 'give' ), sprintf( '<a href="%1$s" target="_blank">', esc_url( 'https://wordpress.org/hosting/' ) ), '</a>' ) . '</p>';
 			$html .= '</div>';
 
 			echo apply_filters( 'give_outdated_php_version_notice_message', $html);
