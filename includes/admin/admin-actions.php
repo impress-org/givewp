70b0f123cf6fc902a5ea13587277b821ba5f6e77
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2017-06-07 12:09:00 +0530

    Update: add more give notices

--- includes/admin/admin-actions.php
+++ includes/admin/admin-actions.php
@@ -220,6 +220,7 @@ function _give_register_admin_notices() {
 		return;
 	}
 
+	// Ajax disabled notice.
 	if (
 		! give_test_ajax_works() &&
 		! get_user_meta( get_current_user_id(), '_give_admin_ajax_inaccessible_dismissed', true ) &&
@@ -238,6 +239,25 @@ function _give_register_admin_notices() {
 			'type'        => 'updated',
 			'description' => $notice_desc,
 			'show'        => true,
+		) );
+	}
+
+	// PHP version update notice
+	if ( function_exists( 'phpversion' ) && version_compare( GIVE_REQUIRED_PHP_VERSION, phpversion(), '>' ) ) {
+
+		$notice_desc = '<p><strong>' . __( 'Your site could be faster and more secure with a newer PHP version.', 'give' ) . '</strong></p>';
+		$notice_desc .= '<p>' . __( 'Hey, we\'ve noticed that you\'re running an outdated version of PHP. PHP is the programming language that WordPress and Give are built on. The version that is currently used for your site is no longer supported. Newer versions of PHP are both faster and more secure. In fact, your version of PHP no longer receives security updates, which is why we\'re sending you this notice.', 'give' ) . '</p>';
+		$notice_desc .= '<p>' . __( 'Hosts have the ability to update your PHP version, but sometimes they don\'t dare to do that because they\'re afraid they\'ll break your site.', 'give' ) . '</p>';
+		$notice_desc .= '<p><strong>' . __( 'To which version should I update?', 'give' ) . '</strong></p>';
+		$notice_desc .= '<p>' . sprintf( __( 'You should update your PHP version to either 5.6 or to 7.0 or 7.1. On a normal WordPress site, switching to PHP 5.6 should never cause issues. We would however actually recommend you switch to PHP7. There are some plugins that are not ready for PHP7 though, so do some testing first. We have an article on how to test whether that\'s an option for you %1$shere%2$s. PHP7 is much faster than PHP 5.6. It\'s also the only PHP version still in active development and therefore the better option for your site in the long run.', 'give' ), '<a href="https://givewp.com/documentation/core/requirements/" target="_blank">', '</a>' ) . '</p>';
+		$notice_desc .= '<p><strong>' . __( 'Can\'t update? Ask your host!', 'give' ) . '</strong></p>';
+		$notice_desc .= '<p>' . sprintf( __( 'If you cannot upgrade your PHP version yourself, you can send an email to your host. If they don\'t want to upgrade your PHP version, we would suggest you switch hosts. Have a look at one of the recommended %1$sWordPress hosting partners%2$s.', 'give' ), sprintf( '<a href="%1$s" target="_blank">', esc_url( 'https://wordpress.org/hosting/' ) ), '</a>' ) . '</p>';
+
+		Give()->notices->register_notice( array(
+			'id'          => 'give-donation-deleted',
+			'type'        => 'error',
+			'description' => $notice_desc,
+			'show'        => ! Give_Cache::get( '_give_hide_outdated_php_notices_shortly', true ),
 		) );
 	}
 
