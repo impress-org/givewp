bebc35b6e8ee24b43b1b02c1a0f3ff7991370bd3
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2017-06-07 11:42:20 +0530

    refactor: register admin notices

--- includes/admin/admin-actions.php
+++ includes/admin/admin-actions.php
@@ -208,3 +208,224 @@ function give_hide_outdated_php_notice() {
 }
 
 add_action( 'wp_ajax_give_hide_outdated_php_notice', 'give_hide_outdated_php_notice' );
+
+/**
+ * Register admin notices.
+ *
+ * @since 1.8.9
+ */
+function _give_register_admin_notices() {
+	// Bailout.
+	if( ! is_admin() ) {
+		return;
+	}
+
+	if (
+		! give_test_ajax_works() &&
+		! get_user_meta( get_current_user_id(), '_give_admin_ajax_inaccessible_dismissed', true ) &&
+		current_user_can( 'manage_give_settings' )
+	) {
+		$notice_desc = '<p>' . __( 'Your site appears to be blocking the WordPress ajax interface. This may cause issues with Give.', 'give' ) . '</p>';
+		$notice_desc .= '<p>' . sprintf( __( 'Please see <a href="%s" target="_blank">this reference</a> for possible solutions.', 'give' ), esc_url( 'http://docs.givewp.com/ajax-blocked' ) ) . '</p>';
+		$notice_desc .= sprintf(
+			'<p><a href=""></a></p>',
+			add_query_arg( array( 'give_action' => 'dismiss_notices', 'give_notice' => 'admin_ajax_inaccessible' ) ),
+			__( 'Dismiss Notice', 'give' )
+		);
+
+		Give()->notices->register_notice( array(
+			'id'          => 'give-donation-deleted',
+			'type'        => 'updated',
+			'description' => $notice_desc,
+			'show'        => true,
+		) );
+	}
+
+
+	// Bailout.
+	if ( empty( $_GET['give-message'] ) ) {
+		return;
+	}
+
+	// Donation reports errors.
+	if ( current_user_can( 'view_give_reports' ) ) {
+		switch ( $_GET['give-message'] ) {
+			case 'donation_deleted' :
+				Give()->notices->register_notice( array(
+					'id'          => 'give-donation-deleted',
+					'type'        => 'updated',
+					'description' => __( 'The donation has been deleted.', 'give' ),
+					'show'        => true,
+				) );
+				break;
+			case 'email_sent' :
+				Give()->notices->register_notice( array(
+					'id'          => 'give-payment-sent',
+					'type'        => 'updated',
+					'description' => __( 'The donation receipt has been resent.', 'give' ),
+					'show'        => true,
+				) );
+				break;
+			case 'refreshed-reports' :
+				Give()->notices->register_notice( array(
+					'id'          => 'give-refreshed-reports',
+					'type'        => 'updated',
+					'description' => __( 'The reports cache has been cleared.', 'give' ),
+					'show'        => true,
+				) );
+				break;
+			case 'donation-note-deleted' :
+				Give()->notices->register_notice( array(
+					'id'          => 'give-donation-note-deleted',
+					'type'        => 'updated',
+					'description' => __( 'The donation note has been deleted.', 'give' ),
+					'show'        => true,
+				) );
+				break;
+		}
+	}
+
+	// Give settings notices and errors.
+	if ( current_user_can( 'manage_give_settings' ) ) {
+		switch ( $_GET['give-message'] ) {
+			case 'settings-imported' :
+				Give()->notices->register_notice( array(
+					'id'          => 'give-settings-imported',
+					'type'        => 'updated',
+					'description' => __( 'The settings have been imported.', 'give' ),
+					'show'        => true,
+				) );
+				break;
+			case 'api-key-generated' :
+				Give()->notices->register_notice( array(
+					'id'          => 'give-api-key-generated',
+					'type'        => 'updated',
+					'description' => __( 'API keys have been generated.', 'give' ),
+					'show'        => true,
+				) );
+				break;
+			case 'api-key-exists' :
+				Give()->notices->register_notice( array(
+					'id'          => 'give-api-key-exists',
+					'type'        => 'updated',
+					'description' => __( 'The specified user already has API keys.', 'give' ),
+					'show'        => true,
+				) );
+				break;
+			case 'api-key-regenerated' :
+				Give()->notices->register_notice( array(
+					'id'          => 'give-api-key-regenerated',
+					'type'        => 'updated',
+					'description' => __( 'API keys have been regenerated.', 'give' ),
+					'show'        => true,
+				) );
+				break;
+			case 'api-key-revoked' :
+				Give()->notices->register_notice( array(
+					'id'          => 'give-api-key-revoked',
+					'type'        => 'updated',
+					'description' => __( 'API keys have been revoked.', 'give' ),
+					'show'        => true,
+				) );
+				break;
+			case 'sent-test-email' :
+				Give()->notices->register_notice( array(
+					'id'          => 'give-sent-test-email',
+					'type'        => 'updated',
+					'description' => __( 'The test email has been sent.', 'give' ),
+					'show'        => true,
+				) );
+				break;
+			case 'matched-success-failure-page':
+				Give()->notices->register_notice( array(
+					'id'          => 'give-matched-success-failure-page',
+					'type'        => 'updated',
+					'description' => __( 'You cannot set the success and failed pages to the same page', 'give' ),
+					'show'        => true,
+				) );
+				break;
+		}
+	}
+	// Payments errors.
+	if ( current_user_can( 'edit_give_payments' ) ) {
+		switch ( $_GET['give-message'] ) {
+			case 'note-added' :
+				Give()->notices->register_notice( array(
+					'id'          => 'give-note-added',
+					'type'        => 'updated',
+					'description' => __( 'The donation note has been added.', 'give' ),
+					'show'        => true,
+				) );
+				break;
+			case 'payment-updated' :
+				Give()->notices->register_notice( array(
+					'id'          => 'give-payment-updated',
+					'type'        => 'updated',
+					'description' => __( 'The donation has been updated.', 'give' ),
+					'show'        => true,
+				) );
+				break;
+		}
+	}
+
+	// Donor Notices.
+	if ( current_user_can( 'edit_give_payments' ) ) {
+		switch ( $_GET['give-message'] ) {
+			case 'donor-deleted' :
+				Give()->notices->register_notice( array(
+					'id'          => 'give-donor-deleted',
+					'type'        => 'updated',
+					'description' => __( 'The donor has been deleted.', 'give' ),
+					'show'        => true,
+				) );
+				break;
+
+			case 'email-added' :
+				Give()->notices->register_notice( array(
+					'id'          => 'give-donor-email-added',
+					'type'        => 'updated',
+					'description' => __( 'Donor email added.', 'give' ),
+					'show'        => true,
+				) );
+				break;
+
+			case 'email-removed' :
+				Give()->notices->register_notice( array(
+					'id'          => 'give-donor-email-removed',
+					'type'        => 'updated',
+					'description' => __( 'Donor email removed.', 'give' ),
+					'show'        => true,
+				) );
+				break;
+
+			case 'email-remove-failed' :
+				Give()->notices->register_notice( array(
+					'id'          => 'give-donor-email-remove-failed',
+					'type'        => 'updated',
+					'description' => __( 'Failed to remove donor email.', 'give' ),
+					'show'        => true,
+				) );
+				break;
+
+			case 'primary-email-updated' :
+				Give()->notices->register_notice( array(
+					'id'          => 'give-donor-primary-email-updated',
+					'type'        => 'updated',
+					'description' => __( 'Primary email updated for donor.', 'give' ),
+					'show'        => true,
+				) );
+				break;
+
+			case 'primary-email-failed' :
+				Give()->notices->register_notice( array(
+					'id'          => 'give-donor-primary-email-failed',
+					'type'        => 'updated',
+					'description' => __( 'Failed to set primary email.', 'give' ),
+					'show'        => true,
+				) );
+				break;
+		}
+	}
+}
+
+add_action( 'admin_notices', '_give_register_admin_notices', - 1 );
