c4ff24ca1d081e99dfaf54cb7aabd42ec0309ed2
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2017-06-12 10:03:40 +0530

    add: give_tooltip as default class

--- includes/class-give-tooltips.php
+++ includes/class-give-tooltips.php
@@ -101,7 +101,7 @@ class Give_Tooltips {
 
 		// Set classes.
 		$args['attributes']['class'] = ! empty( $args['attributes']['class'] ) ? $args['attributes']['class'] : '';
-		$args['attributes']['class'] .= " {$tooltip_pos[ $args['position'] ]}";
+		$args['attributes']['class'] .= "give-tooltip {$tooltip_pos[ $args['position'] ]}";
 		$args['attributes']['class'] .= ! empty( $args['status'] ) ? " {$tooltip_status[ $args['status'] ]}" : '';
 		$args['attributes']['class'] .= ! empty( $args['size'] ) ? " {$tooltip_size[ $args['size'] ]}" : '';
 		$args['attributes']['class'] .= $args['show_always'] ? ' hint--always' : '';
