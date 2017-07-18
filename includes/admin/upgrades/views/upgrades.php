cb0a078a66652b30e913a6f8c9a4aef574ac5a97
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2017-07-18 09:52:04 +0530

    Update: update page design

--- includes/admin/upgrades/views/upgrades.php
+++ includes/admin/upgrades/views/upgrades.php
@@ -27,7 +27,7 @@ $doing_upgrade_args = array(
 	'step'         => $step,
 	'total'        => $total,
 	'custom'       => $custom,
-	'steps'        => $steps
+	'steps'        => $steps,
 );
 update_option( 'give_doing_upgrade', $doing_upgrade_args );
 
@@ -38,47 +38,61 @@ if ( $step > $steps ) {
 
 $give_updates = Give_Updates::get_instance();
 ?>
-<div class="wrap" id="give-updates">
-	<h1><?php esc_html_e( 'Give - Upgrades', 'give' ); ?></h1>
-	<div class="panel-content">
-		<p>
-			<?php _e( 'You have updates. Please <a href="%s">create a backup</a> of your site before updating. It is important to always stay up-to-date with latest versions of Give core and it\'s add-ons. We regularly release new features, bug fixes, and enhancements. To update add-ons be sure your <a href="%s">license keys</a> are active.', 'give' ); ?>
-		</p>
-	</div>
-
-	<?php $update_counter = 1;?>
-
-	<?php $db_updates = $give_updates->get_db_update_count(); ?>
-	<?php if( ! empty( $db_updates ) ) : ?>
-		<?php $db_update_url = add_query_arg( array( 'type' => 'database' ) ); ?>
-		<div id="give-db-updates">
-			<h2><?php _e( 'Database Updates', 'give' ); ?></h2>
-			<div class="panel-content">
-				<p>
-					<?php echo sprintf( __( 'Give needs to update the database. <a href="%s">Update now ></a>', 'give' ), $db_update_url ); ?>
-				</p>
-			</div>
-			<div id="poststuff" style="display: none">
-				<div class="postbox">
-					<h2 class="hndle"><span><?php echo sprintf( __( 'Step 1 of %s', 'give' ), $db_updates ); ?></span></h2>
-					<div class="inside"></div>
-					<!-- .inside -->
-				</div><!-- .postbox -->
-			</div>
+<div class="wrap" id="poststuff">
+	<div id="give-updates">
+		<h1><?php esc_html_e( 'Give - Upgrades', 'give' ); ?></h1>
+		<div class="postbox-container">
+			<div class="postbox">
+				<div class="inside">
+					<div class="panel-content">
+						<h2><?php _e( 'You have updates. Please <a href="%s">create a backup</a> of your site before updating. It is important to always stay up-to-date with latest versions of Give core and it\'s add-ons. We regularly release new features, bug fixes, and enhancements. To update add-ons be sure your <a href="%s">license keys</a> are active.', 'give' ); ?></h2>
+					</div>
+				</div>
+				<!-- .inside -->
+			</div><!-- .postbox -->
 		</div>
-	<?php endif; ?>
 
-	<?php $plugin_updates = $give_updates->get_plugin_update_count(); ?>
-	<?php if( ! empty( $plugin_updates ) ) : ?>
-	<?php $plugin_update_url = add_query_arg( array( 's' => 'Give' ), admin_url( '/plugins.php' ) ); ?>
-	<div id="give-plugin-updates">
-		<h2><?php _e( 'Plugin Updates', 'give' ); ?></h2>
-		<div class="panel-content">
-			<p>
-				<?php echo sprintf( __( 'There are %s Give %s that need to be updated. <a href="%s">Update now ></a>', 'give' ), $plugin_updates, _n( 'addon', 'addons', $plugin_updates, 'give' ), $plugin_update_url ); ?>
-			</p>
-		</div>
+		<?php $update_counter = 1; ?>
+
+		<?php $db_updates = $give_updates->get_db_update_count(); ?>
+		<?php if ( ! empty( $db_updates ) ) : ?>
+			<?php $db_update_url = add_query_arg( array( 'type' => 'database' ) ); ?>
+			<div id="give-db-updates">
+				<div class="postbox-container">
+					<div class="postbox">
+						<h2 class="hndle"><?php _e( 'Database Updates', 'give' ); ?></h2>
+						<!--<h2 class="hndle"><span>-->
+						<?php //echo sprintf( __( 'Update 1 of %s', 'give' ), $db_updates ); ?><!--</span></h2>-->
+						<div class="inside">
+							<div class="panel-content">
+								<p><?php echo sprintf( __( 'Give needs to update the database. <a href="%s">Update now ></a>', 'give' ), $db_update_url ); ?></p>
+							</div>
+						</div>
+						<!-- .inside -->
+					</div><!-- .postbox -->
+				</div>
+			</div>
+		<?php endif; ?>
+
+		<?php $plugin_updates = $give_updates->get_plugin_update_count(); ?>
+		<?php if ( ! empty( $plugin_updates ) ) : ?>
+			<?php $plugin_update_url = add_query_arg( array( 's' => 'Give' ), admin_url( '/plugins.php' ) ); ?>
+			<div id="give-plugin-updates">
+				<div class="postbox-container">
+					<div class="postbox">
+						<h2 class="hndle"><?php _e( 'Plugin Updates', 'give' ); ?></h2>
+						<!--<h2 class="hndle"><span>-->
+						<?php //echo sprintf( __( 'Update 1 of %s', 'give' ), $db_updates ); ?><!--</span></h2>-->
+						<div class="inside">
+							<div class="panel-content">
+								<p><?php echo sprintf( __( 'There are %s Give %s that need to be updated. <a href="%s">Update now ></a>', 'give' ), $plugin_updates, _n( 'addon', 'addons', $plugin_updates, 'give' ), $plugin_update_url ); ?></p>
+							</div>
+						</div>
+						<!-- .inside -->
+					</div><!-- .postbox -->
+				</div>
+			</div>
+		<?php endif; ?>
+
 	</div>
-	<?php endif; ?>
-
 </div>
