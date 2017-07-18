efb5c750e6681438942afb494b1583e1ffd5d37e
Devin Walker <devin@wordimpress.com>
2017-07-18 10:33:55 +0530

    Refactor:  file names reporting -> reports and settings --> report

--- give.php
+++ give.php
@@ -455,9 +455,9 @@ if ( ! class_exists( 'Give' ) ) :
 				require_once GIVE_PLUGIN_DIR . 'includes/admin/tools/export/export-actions.php';
 				require_once GIVE_PLUGIN_DIR . 'includes/admin/tools/export/pdf-reports.php';
 
-				require_once GIVE_PLUGIN_DIR . 'includes/admin/reporting/reports.php';
-				require_once GIVE_PLUGIN_DIR . 'includes/admin/reporting/class-give-graph.php';
-				require_once GIVE_PLUGIN_DIR . 'includes/admin/reporting/graphing.php';
+				require_once GIVE_PLUGIN_DIR . 'includes/admin/reports/reports.php';
+				require_once GIVE_PLUGIN_DIR . 'includes/admin/reports/class-give-graph.php';
+				require_once GIVE_PLUGIN_DIR . 'includes/admin/reports/graphing.php';
 
 				require_once GIVE_PLUGIN_DIR . 'includes/admin/tools/logs/logs.php';
 
