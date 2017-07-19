2c5c8ac5159a037ddec5d0f16de645b0a189d0d7
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2017-07-19 11:47:53 +0530

    Add: load plugin info update template

---
+++ includes/admin/upgrades/views/plugins-update-section.php
@@ -0,0 +1,62 @@
+<?php
+/* @var Give_Updates $give_updates */
+$plugins = $give_updates->get_updates( 'plugin' );
+error_log( print_r( $plugins, true ) . "\n", 3, WP_CONTENT_DIR . '/debug_new.log' );
+if( empty( $plugins ) ) {
+	return;
+}
+
+ob_start();
+foreach ( $plugins as $plugin_data ) {
+	if ( 'active' != $plugin_data['Status'] ||  'add-on' != $plugin_data['Type'] ) {
+		continue;
+	}
+
+	$plugin_name = $plugin_data['Name'];
+	$author_name = $plugin_data['Author'];
+
+	// Link the plugin name to the plugin URL if available.
+	if ( ! empty( $plugin_data['PluginURI'] ) ) {
+		$plugin_name = sprintf(
+			'<a href="%s" title="%s">%s</a>(%s)',
+			esc_url( $plugin_data['PluginURI'] ),
+			esc_attr__( 'Visit plugin homepage' , 'give' ),
+			$plugin_name,
+			esc_html( $plugin_data['Version'] )
+		);
+	}
+
+	// Link the author name to the author URL if available.
+	if ( ! empty( $plugin_data['AuthorURI'] ) ) {
+		$author_name = sprintf(
+			'<a href="%s" title="%s">%s</a>',
+			esc_url( $plugin_data['AuthorURI'] ),
+			esc_attr__( 'Visit author homepage' , 'give' ),
+			$author_name
+		);
+	}
+	?>
+	<tr>
+		<td><?php echo wp_kses( $plugin_name, wp_kses_allowed_html( 'post' ) ); ?></td>
+		<td>
+			<?php
+			echo sprintf(
+				'<mark class="yes"><span class="dashicons dashicons-yes"></span></mark> %s',
+				( true === $plugin_data['License'] ? __( 'Licensed', 'give' ) : __( 'Unlicensed', 'give' ) )
+			);
+
+			echo sprintf(
+				' &ndash; %s &ndash; %s',
+				sprintf( _x( 'by %s', 'by author', 'give' ), wp_kses( $author_name, wp_kses_allowed_html( 'post' ) ) ),
+				sprintf( __( '( Latest Version: %s )' ), $plugin_data['update']->new_version )
+			);
+			?>
+		</td>
+	</tr>
+	<?php
+}
+echo sprintf(
+	'<table><tbody>%s</tbody></table>',
+	ob_get_clean()
+);
+?>
