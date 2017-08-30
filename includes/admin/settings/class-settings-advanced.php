5a58d1505ac4895a8645c2ffc006f2e3caf1635f
mehul0810 <mehul.gohil0810@gmail.com>
2017-08-30 11:48:16 +0530

    Removed unnecessary settings from advanced tab

--- includes/admin/settings/class-settings-advanced.php
+++ includes/admin/settings/class-settings-advanced.php
@@ -64,14 +64,6 @@ if ( ! class_exists( 'Give_Settings_Advanced' ) ) :
 							)
 						),
 						array(
-							'name'    => __( 'Default User Role', 'give' ),
-							'desc'    => __( 'Assign default user roles for donors when donors opt to register as a WP User.', 'give' ),
-							'id'      => 'donor_default_user_role',
-							'type'    => 'select',
-							'default' => 'subscriber',
-							'options' => give_get_user_roles()
-						),
-						array(
 							/* translators: %s: the_content */
 							'name'    => sprintf( __( '%s filter', 'give' ), '<code>the_content</code>' ),
 							/* translators: 1: https://codex.wordpress.org/Plugin_API/Filter_Reference/the_content 2: the_content */
