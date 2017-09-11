4a8a8d8d4353bd2ba0692fc75bf26db3002fc430
Devin Walker <sdtucson@gmail.com>
2017-09-11 15:14:22 -0700

    Improved verbiage

--- includes/forms/template.php
+++ includes/forms/template.php
@@ -1052,7 +1052,7 @@ function give_default_cc_address_fields( $form_id ) {
                     <span class="give-required-indicator <?php echo( array_key_exists( $selected_country, $states_not_required_country_list ) ? 'give-hidden' : '' ) ?> ">*</span>
 				<?php endif; ?>
                 <span class="give-tooltip give-icon give-icon-question"
-                      data-tooltip="<?php esc_attr_e( 'The state or province or county for your billing address.', 'give' ); ?>"></span>
+                      data-tooltip="<?php esc_attr_e( 'The state, province, or county for your billing address.', 'give' ); ?>"></span>
             </label>
 			<?php
 
