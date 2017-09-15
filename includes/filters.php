d1f67a1491b303eabffb068d862910d50f3a80c3
Jaydeep <jaydeep.ramii@gmail.com>
2017-09-15 19:33:16 +0530

    Build akismet args for spam check #673

--- includes/filters.php
+++ includes/filters.php
@@ -21,14 +21,14 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * @since  1.8
  *
- * @param  array  $old_settings Array of settings.
- * @param  array  $settings     Array of settings.
+ * @param  array $old_settings Array of settings.
+ * @param  array $settings     Array of settings.
  *
  * @return void
  */
 function give_set_settings_with_disable_prefix( $old_settings, $settings ) {
 	// Bailout.
-	if( ! function_exists( 'give_v18_renamed_core_settings' ) ) {
+	if ( ! function_exists( 'give_v18_renamed_core_settings' ) ) {
 		return;
 	}
 
@@ -65,4 +65,54 @@ function give_set_settings_with_disable_prefix( $old_settings, $settings ) {
 		update_option( 'give_settings', $settings );
 	}
 }
+
 add_action( 'update_option_give_settings', 'give_set_settings_with_disable_prefix', 10, 2 );
+
+/**
+ * Check spam through Akismet.
+ *
+ * @since 1.8.15
+ *
+ * @param $spam
+ *
+ * @return bool|mixed
+ */
+function give_akismet( $spam ) {
+
+	// Bail out, If spam.
+	if ( $spam ) {
+		return $spam;
+	}
+
+	// Bail out, if Akismet key not exist.
+	if ( ! give_check_akismet_key() ) {
+		return false;
+	}
+
+	// Build args array.
+	$args = array();
+
+	$args['comment_author']       = isset( $_POST['give_first'] ) ? strip_tags( trim( $_POST['give_first'] ) ) : '';
+	$args['comment_author_email'] = isset( $_POST['give_email'] ) ? $_POST['give_email'] : false;
+	$args['blog']                 = get_option( 'home' );
+	$args['blog_lang']            = get_locale();
+	$args['blog_charset']         = get_option( 'blog_charset' );
+	$args['user_ip']              = $_SERVER['REMOTE_ADDR'];
+	$args['user_agent']           = $_SERVER['HTTP_USER_AGENT'];
+	$args['referrer']             = $_SERVER['HTTP_REFERER'];
+	$args['comment_type']         = 'contact-form';
+
+	$ignore = array( 'HTTP_COOKIE', 'HTTP_COOKIE2', 'PHP_AUTH_PW' );
+
+	foreach ( $_SERVER as $key => $value ) {
+		if ( ! in_array( $key, (array) $ignore ) ) {
+			$args["$key"] = $value;
+		}
+	}
+
+	return give_akismet_spam_check( $args );
+
+}
+
+add_filter( 'give_spam', 'give_akismet' );
+
