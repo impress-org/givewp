4042a486f5e50fc082f8d1a649a865360cfd2292
Deepak Gupta <raftaar1191@gmail.com>
2017-09-15 16:10:29 +0530

    Rearrange address fields in donor backends #2037

--- includes/admin/donors/donors.php
+++ includes/admin/donors/donors.php
@@ -323,28 +323,27 @@ function give_donor_view( $donor ) {
 												<span class="info-item" data-key="country"><?php echo $address['country']; ?></span>
 												<span class="info-item" data-key="zip"><?php echo $address['zip']; ?></span>
 											</span>
-										<?php } ?>
+										<?php }
+										// For country
+										$selected_country = $address['country'];
+										$countries = give_get_country_list();
+
+										// For State
+										$selected_state = give_get_state();
+										$states         = give_get_states( $selected_country );
+										$selected_state = isset( $address['state'] ) ? $address['state'] : $selected_state;
+										?>
 										<span class="donor-address info-item edit-item">
-											<input class="info-item" type="text" data-key="line1" name="customerinfo[line1]" placeholder="<?php _e( 'Address 1', 'give' ); ?>" value="<?php echo $address['line1']; ?>" />
-											<input class="info-item" type="text" data-key="line2" name="customerinfo[line2]" placeholder="<?php _e( 'Address 2', 'give' ); ?>" value="<?php echo $address['line2']; ?>" />
-											<input class="info-item" type="text" data-key="city" name="customerinfo[city]" placeholder="<?php _e( 'City', 'give' ); ?>" value="<?php echo $address['city']; ?>" />
 											<select data-key="country" name="customerinfo[country]" id="billing_country" class="billing_country give-select edit-item">
 												<?php
-
-												$selected_country = $address['country'];
-
-												$countries = give_get_country_list();
 												foreach ( $countries as $country_code => $country ) {
 													echo '<option value="' . esc_attr( $country_code ) . '"' . selected( $country_code, $selected_country, false ) . '>' . $country . '</option>';
 												}
 												?>
 											</select>
+											<input class="info-item" type="text" data-key="line1" name="customerinfo[line1]" placeholder="<?php _e( 'Address 1', 'give' ); ?>" value="<?php echo $address['line1']; ?>" />
+											<input class="info-item" type="text" data-key="line2" name="customerinfo[line2]" placeholder="<?php _e( 'Address 2', 'give' ); ?>" value="<?php echo $address['line2']; ?>" />
 											<?php
-											$selected_state = give_get_state();
-											$states         = give_get_states( $selected_country );
-
-											$selected_state = isset( $address['state'] ) ? $address['state'] : $selected_state;
-
 											if ( ! empty( $states ) ) {
 												?>
 												<select data-key="state" name="customerinfo[state]" id="card_state" class="card_state give-select info-item">
@@ -361,6 +360,7 @@ function give_donor_view( $donor ) {
 												<?php
 											}
 											?>
+											<input class="info-item" type="text" data-key="city" name="customerinfo[city]" placeholder="<?php _e( 'City', 'give' ); ?>" value="<?php echo $address['city']; ?>" />
 											<input class="info-item" type="text" data-key="zip" name="customerinfo[zip]" placeholder="<?php _e( 'Zip / Postal Code', 'give' ); ?>" value="<?php echo $address['zip']; ?>" />
 										</span>
 
