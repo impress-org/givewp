683063d0cff52dccd92c7c0e710b474d3b1d4b66
Deepak Gupta <raftaar1191@gmail.com>
2017-09-18 13:05:20 +0530

    earrange address fields in dashboard donation sinel page #2037

--- assets/js/admin/admin-scripts.js
+++ assets/js/admin/admin-scripts.js
@@ -337,14 +337,21 @@ var give_setting_edit = false;
 				};
 				$.post(ajaxurl, data, function (response) {
 
+					// Show the states dropdown menu
+					$this.closest( '.column-container' ).find( '#give-order-address-state-wrap' ).removeClass( 'give-hidden' )
+
 					var state_wrap = $('#give-order-address-state-wrap');
-
 					state_wrap.find('*').not('.order-data-address-line').remove();
-
 					if (typeof ( response.states_found ) != undefined && true == response.states_found) {
 						state_wrap.append(response.data);
 						state_wrap.find('select').chosen();
 					} else {
+
+						if (typeof ( response.show_field ) != undefined && false == response.show_field ) {
+							// Hide the states dropdown menu
+							$this.closest( '.column-container' ).find( '#give-order-address-state-wrap' ).addClass( 'give-hidden' );
+						}
+
 						state_wrap.append('<input type="text" name="give-payment-address[0][state]" value="' + response.default_state + '" class="give-edit-toggles medium-text"/>');
 					}
 				});
