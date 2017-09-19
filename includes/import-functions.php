48214378417fb5303031f7376a79d6bbd773d703
Deepak Gupta <raftaar1191@gmail.com>
2017-09-19 14:56:34 +0530

    Add Import functions files #1966

---
+++ includes/import-functions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Import Functions
+ *
+ * @package     Give
+ * @subpackage  Functions
+ * @copyright   Copyright (c) 2016, WordImpress
+ * @license     https://opensource.org/licenses/gpl-license GNU Public License
+ * @since       1.8.14
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
