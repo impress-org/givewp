305179ae79c81e08cb699a91067e9c3fe2133b85
mehul0810 <mehul.gohil0810@gmail.com>
2017-09-19 16:34:44 +0530

    Improved Shortcode display area for better flexibility of attributes

--- templates/shortcode-receipt.php
+++ templates/shortcode-receipt.php
@@ -17,74 +17,75 @@ if ( empty( $payment ) ) {
 	return;
 }
 
-$donation_id    = $payment->ID;
-$form_id        = give_get_payment_meta( $donation_id, '_give_payment_form_id', true );
-$meta           = give_get_payment_meta( $donation_id );
-$donation       = give_get_payment_form_title( $meta );
-$user           = give_get_payment_meta_user_info( $donation_id );
-$email          = give_get_payment_user_email( $donation_id );
-$status         = $payment->post_status;
-$status_label   = give_get_payment_status( $payment, true );
+$donation_id  = $payment->ID;
+$form_id      = give_get_payment_meta( $donation_id, '_give_payment_form_id', true );
+$meta         = give_get_payment_meta( $donation_id );
+$donation     = give_get_payment_form_title( $meta );
+$user         = give_get_payment_meta_user_info( $donation_id );
+$email        = give_get_payment_user_email( $donation_id );
+$status       = $payment->post_status;
+$status_label = give_get_payment_status( $payment, true );
 
 /**
  * Generate Donation Receipt Arguments.
  *
- * Added donation receipt array to global variable $give_receipt_args to manage it from single variable
+ * Added donation receipt array to global variable $give_receipt_args to
+ * manage it from single variable
  *
  * @since 1.8.8
  */
 $give_receipt_args['donation_receipt']['donor'] = array(
-	'name'      => __( 'Donor', 'give' ),
-	'value'     => $user['first_name'] . ' ' . $user['last_name'],
-	'display'   => $give_receipt_args['donor'],
+	'name'    => __( 'Donor', 'give' ),
+	'value'   => $user['first_name'] . ' ' . $user['last_name'],
+	'display' => $give_receipt_args['donor'],
 );
 
 $give_receipt_args['donation_receipt']['date'] = array(
-	'name'      => __( 'Date', 'give' ),
-	'value'     => date_i18n( give_date_format(), strtotime( $meta['date'] ) ),
-	'display'   => $give_receipt_args['date'],
+	'name'    => __( 'Date', 'give' ),
+	'value'   => date_i18n( give_date_format(), strtotime( $meta['date'] ) ),
+	'display' => $give_receipt_args['date'],
 );
 
 $give_receipt_args['donation_receipt']['total_donation'] = array(
-	'name'      => __( 'Total Donation', 'give' ),
-	'value'     => give_payment_amount( $donation_id ),
-	'display'   => true,
+	'name'    => __( 'Total Donation', 'give' ),
+	'value'   => give_payment_amount( $donation_id ),
+	'display' => $give_receipt_args['price'],
 );
 
 $give_receipt_args['donation_receipt']['donation'] = array(
-	'name'      => __( 'Donation', 'give' ),
-	'value'     => $donation,
-	'display'   => true,
+	'name'    => __( 'Donation', 'give' ),
+	'value'   => $donation,
+	'display' => true,
 );
 
 $give_receipt_args['donation_receipt']['donation_status'] = array(
-	'name'      => __( 'Donation Status', 'give' ),
-	'value'     => esc_attr( $status ),
-	'display'   => $give_receipt_args['payment_status'],
+	'name'    => __( 'Donation Status', 'give' ),
+	'value'   => esc_attr( $status ),
+	'display' => $give_receipt_args['payment_status'],
 );
 
 $give_receipt_args['donation_receipt']['donation_id'] = array(
-	'name'      => __( 'Donation ID', 'give' ),
-	'value'     => $donation_id,
-	'display'   => ($give_receipt_args['payment_id'])?true:false,
+	'name'    => __( 'Donation ID', 'give' ),
+	'value'   => $donation_id,
+	'display' => $give_receipt_args['payment_id'] ? true : false,
 );
 
 $give_receipt_args['donation_receipt']['payment_details'] = array(
-	'name'      => __( 'Payment:', 'give' ),
-	'value'     => __( 'Details:', 'give' ),
-	'display'   => ($give_receipt_args['payment_id'])?false:true,
+	'name'    => __( 'Payment:', 'give' ),
+	'value'   => __( 'Details:', 'give' ),
+	'display' => $give_receipt_args['payment_id'] ? false : true,
 );
 
 $give_receipt_args['donation_receipt']['payment_key'] = array(
-	'name'      => __( 'Payment Key', 'give' ),
-	'value'     => get_post_meta( $donation_id, '_give_payment_purchase_key', true ),
-	'display'   => $give_receipt_args['payment_key'],
+	'name'    => __( 'Payment Key', 'give' ),
+	'value'   => get_post_meta( $donation_id, '_give_payment_purchase_key', true ),
+	'display' => $give_receipt_args['payment_key'],
 );
 
 $give_receipt_args['donation_receipt']['payment_method'] = array(
-	'name'      => __( 'Payment Method', 'give' ),
-	'value'     => give_get_gateway_checkout_label( give_get_payment_gateway( $donation_id ) ),
-	'display'   => true,
+	'name'    => __( 'Payment Method', 'give' ),
+	'value'   => give_get_gateway_checkout_label( give_get_payment_gateway( $donation_id ) ),
+	'display' => $give_receipt_args['payment_method'],
 );
 
 /**
@@ -172,80 +173,80 @@ if ( filter_var( $give_receipt_args['status_notice'], FILTER_VALIDATE_BOOLEAN ) 
 do_action( 'give_payment_receipt_before_table', $payment, $give_receipt_args );
 ?>
 
-	<table id="give_donation_receipt" class="give-table">
-		<thead>
-		<?php
-		/**
-		 * Fires in the payment receipt shortcode, before the receipt first header item.
-		 *
-		 * Allows you to add new <th> elements before the receipt first header item.
-		 *
-		 * @since 1.7
-		 *
-		 * @param object $payment           The payment object.
-		 * @param array  $give_receipt_args Receipt_argument.
-		 */
-		do_action( 'give_payment_receipt_header_before', $payment, $give_receipt_args );
-		?>
-		<tr>
-			<th scope="colgroup" colspan="2">
-				<span class="give-receipt-thead-text"><?php esc_html_e( 'Donation Receipt', 'give' ) ?></span>
-			</th>
-		</tr>
-		<?php
-		/**
-		 * Fires in the payment receipt shortcode, after the receipt last header item.
-		 *
-		 * Allows you to add new <th> elements after the receipt last header item.
-		 *
-		 * @since 1.7
-		 *
-		 * @param object $payment           The payment object.
-		 * @param array  $give_receipt_args Receipt_argument.
-		 */
-		do_action( 'give_payment_receipt_header_after', $payment, $give_receipt_args );
-		?>
-		</thead>
+<table id="give_donation_receipt" class="give-table">
+	<thead>
+	<?php
+	/**
+	 * Fires in the payment receipt shortcode, before the receipt first header item.
+	 *
+	 * Allows you to add new <th> elements before the receipt first header item.
+	 *
+	 * @since 1.7
+	 *
+	 * @param object $payment           The payment object.
+	 * @param array  $give_receipt_args Receipt_argument.
+	 */
+	do_action( 'give_payment_receipt_header_before', $payment, $give_receipt_args );
+	?>
+	<tr>
+		<th scope="colgroup" colspan="2">
+			<span class="give-receipt-thead-text"><?php esc_html_e( 'Donation Receipt', 'give' ) ?></span>
+		</th>
+	</tr>
+	<?php
+	/**
+	 * Fires in the payment receipt shortcode, after the receipt last header item.
+	 *
+	 * Allows you to add new <th> elements after the receipt last header item.
+	 *
+	 * @since 1.7
+	 *
+	 * @param object $payment           The payment object.
+	 * @param array  $give_receipt_args Receipt_argument.
+	 */
+	do_action( 'give_payment_receipt_header_after', $payment, $give_receipt_args );
+	?>
+	</thead>
 
-		<tbody>
-		<?php
-		/**
-		 * Fires in the payment receipt shortcode, before the receipt first item.
-		 *
-		 * Allows you to add new <td> elements before the receipt first item.
-		 *
-		 * @since 1.7
-		 *
-		 * @param object $payment           The payment object.
-		 * @param array  $give_receipt_args Receipt_argument.
-		 */
-		do_action( 'give_payment_receipt_before', $payment, $give_receipt_args );
-		?>
+	<tbody>
+	<?php
+	/**
+	 * Fires in the payment receipt shortcode, before the receipt first item.
+	 *
+	 * Allows you to add new <td> elements before the receipt first item.
+	 *
+	 * @since 1.7
+	 *
+	 * @param object $payment           The payment object.
+	 * @param array  $give_receipt_args Receipt_argument.
+	 */
+	do_action( 'give_payment_receipt_before', $payment, $give_receipt_args );
+	?>
 
-		<?php foreach ( $give_receipt_args['donation_receipt'] as $receipt_item ) { ?>
-			<?php if ( filter_var( $receipt_item['display'], FILTER_VALIDATE_BOOLEAN ) ) : ?>
+	<?php foreach ( $give_receipt_args['donation_receipt'] as $receipt_item ) { ?>
+		<?php if ( filter_var( $receipt_item['display'], FILTER_VALIDATE_BOOLEAN ) ) : ?>
 			<tr>
 				<td scope="row"><strong><?php echo $receipt_item['name']; ?></strong></td>
 				<td><?php echo $receipt_item['value']; ?></td>
 			</tr>
-			<?php endif; ?>
-		<?php } ?>
+		<?php endif; ?>
+	<?php } ?>
 
-		<?php
-		/**
-		 * Fires in the payment receipt shortcode, after the receipt last item.
-		 *
-		 * Allows you to add new <td> elements after the receipt last item.
-		 *
-		 * @since 1.7
-		 *
-		 * @param object $payment           The payment object.
-		 * @param array  $give_receipt_args Receipt_argument.
-		 */
-		do_action( 'give_payment_receipt_after', $payment, $give_receipt_args );
-		?>
-		</tbody>
-	</table>
+	<?php
+	/**
+	 * Fires in the payment receipt shortcode, after the receipt last item.
+	 *
+	 * Allows you to add new <td> elements after the receipt last item.
+	 *
+	 * @since 1.7
+	 *
+	 * @param object $payment           The payment object.
+	 * @param array  $give_receipt_args Receipt_argument.
+	 */
+	do_action( 'give_payment_receipt_after', $payment, $give_receipt_args );
+	?>
+	</tbody>
+</table>
 
 <?php
 /**
