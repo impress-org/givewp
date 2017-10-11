557448e1ecfc8b298267152deb1230034fa363da
mehul0810 <mehul.gohil0810@gmail.com>
2017-10-11 20:39:07 +0530

    Reduce number of parameters used in function

--- includes/admin/payments/class-payments-table.php
+++ includes/admin/payments/class-payments-table.php
@@ -413,7 +413,7 @@ class Give_Payment_History_Table extends WP_List_Table {
 
 			case 'amount' :
 				$amount = ! empty( $payment->total ) ? $payment->total : 0;
-				$value  = give_currency_filter( give_format_amount( $amount, array( 'sanitize' => false ), $payment->ID ), give_get_payment_currency_code( $payment->ID ) );
+				$value  = give_currency_filter( give_format_amount( $amount, array( 'sanitize' => false, 'donation_id' => $payment->ID ) ), give_get_payment_currency_code( $payment->ID ) );
 				$value  .= sprintf( '<br><small>%1$s %2$s</small>', __( 'via', 'give' ), give_get_gateway_admin_label( $payment->gateway ) );
 				break;
 
