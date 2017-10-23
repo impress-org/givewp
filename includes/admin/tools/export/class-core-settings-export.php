64d77c98f1e2979e57626aba532db1e30ed87bd9
Deepak Gupta <raftaar1191@gmail.com>
2017-10-23 12:59:13 +0530

    Change Version

--- includes/admin/tools/export/class-core-settings-export.php
+++ includes/admin/tools/export/class-core-settings-export.php
@@ -8,7 +8,7 @@
  * @subpackage  Admin/Reports
  * @copyright   Copyright (c) 2016, WordImpress
  * @license     https://opensource.org/licenses/gpl-license GNU Public License
- * @since       1.8.15
+ * @since       1.8.16
  */
 
 // Exit if accessed directly.
@@ -19,7 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Give_Core_Settings_Export Class
  *
- * @since 1.8.15
+ * @since 1.8.16
  */
 class Give_Core_Settings_Export extends Give_Export {
 
@@ -27,7 +27,7 @@ class Give_Core_Settings_Export extends Give_Export {
 	 * Our export type. Used for export-type specific filters/actions
 	 *
 	 * @var string
-	 * @since 1.8.15
+	 * @since 1.8.16
 	 */
 	public $export_type = 'core-settings';
 
@@ -35,7 +35,7 @@ class Give_Core_Settings_Export extends Give_Export {
 	 * Set the export headers
 	 *
 	 * @access public
-	 * @since  1.8.15
+	 * @since  1.8.16
 	 * @return void
 	 */
 	public function headers() {
@@ -55,7 +55,7 @@ class Give_Core_Settings_Export extends Give_Export {
 	 * Prints Give's core settings in JSON format
 	 *
 	 * @access public
-	 * @since 1.8.15
+	 * @since 1.8.16
 	 */
 	public function json_core_settings_export() {
 		echo wp_json_encode( get_option( 'give_settings' ) );
@@ -65,7 +65,7 @@ class Give_Core_Settings_Export extends Give_Export {
 	 * Get the Export Data
 	 *
 	 * @access public
-	 * @since  1.8.15
+	 * @since  1.8.16
 	 */
 	public function export() {
 		if ( ! $this->can_export() ) {
