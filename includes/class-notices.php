4be2b15e73b1b620bc145baaa0fe51ddcaa5a777
mehul0810 <mehul.gohil0810@gmail.com>
2017-11-08 17:40:33 +0530

    Added new fn to print give inline notices

--- includes/class-notices.php
+++ includes/class-notices.php
@@ -631,4 +631,48 @@ class Give_Notices {
 
 		echo $error;
 	}
+
+	/**
+	 * Print Inline Notice.
+	 *
+	 * @param string $message     Message Text.
+	 * @param array  $notice_args An array of notice arguments.
+	 *
+	 * @todo Implement render_admin_notices function within this function in future.
+	 *
+	 * @access public
+	 * @since  1.8.17
+	 *
+	 * @return string
+	 */
+	public function print_admin_notices( $message, $notice_args = array() ) {
+		// Bailout.
+		if ( empty( $message ) ) {
+			return '';
+		}
+
+		$defaults = array(
+			'echo'        => true,
+			'notice_type' => 'warning',
+			'dismissible' => true,
+		);
+		wp_parse_args( $notice_args, $defaults );
+
+		$output    = '';
+		$css_id    = 'give-inline-notice';
+		$css_class = 'notice-' . $notice_args['notice_type'] . ' give-notice notice inline ';
+		$css_class .= ( $notice_args['dismissible'] ) ? 'is-dismissible' : '';
+		$output    .= sprintf(
+			'<div id="%1$s" class="%2$s">%3$s</div>' . " \n",
+			$css_id,
+			$css_class,
+			$message
+		);
+
+		if( ! $notice_args['echo'] ) {
+			return $output;
+		}
+
+		echo $output;
+	}
 }
