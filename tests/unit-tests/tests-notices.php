99ccb230cc083033bb118630b8d00529650bd68e
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2017-11-13 09:50:25 +0530

    Refactor print_admin_notices unit tests

--- tests/unit-tests/tests-notices.php
+++ tests/unit-tests/tests-notices.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Class Tests_Notices
  */
@@ -16,54 +17,83 @@ class Tests_Notices extends Give_Unit_Test_Case {
 	/**
 	 * Tests for print_admin_notice()
 	 *
-	 * @since 1.8.17
+	 * @since        1.8.17
+	 *
+	 * @param array  $notice_args
+	 * @param string $expected
+	 *
+	 * @dataProvider print_admin_notices_provider
 	 */
-	public function test_print_admin_notices() {
+	public function test_print_admin_notices( $notice_args, $expected ) {
+		$this->assertEquals( $expected, Give()->notices->print_admin_notices( $notice_args ) );
+	}
 
-		// Check 1 - Info Notice.
-		$args = array(
-			'echo'        => false,
-			'notice_type' => 'info',
-			'dismissible' => false
+	/**
+	 * Data provider for  test_print_admin_notices
+	 *
+	 * @since 1.8.17
+	 * @return array
+	 */
+	public function print_admin_notices_provider() {
+		return array(
+			// Check 1 - Info Notice.
+			array(
+				array(
+					'description' => 'This is admin notice 1',
+					'id'          => 'give-inline-notice-1',
+					'echo'        => false,
+					'notice_type' => 'info',
+					'dismissible' => false,
+				),
+				'<div id="give-inline-notice-1" class="notice-info give-notice notice inline">This is admin notice 1</div>',
+			),
+
+			// Check 2 - Error Notice.
+			array(
+				array(
+					'description' => 'This is admin notice 2',
+					'id'          => 'give-inline-notice-2',
+					'echo'        => false,
+					'dismissible' => false,
+				),
+				'<div id="give-inline-notice-2" class="notice-warning give-notice notice inline">This is admin notice 2</div>',
+			),
+
+			// Check 3 - Default Warning Notice.
+			array(
+				array(
+					'description' => 'This is admin notice 3',
+					'id'          => 'give-inline-notice-3',
+					'echo'        => false,
+					'dismissible' => false,
+				),
+				'<div id="give-inline-notice-3" class="notice-warning give-notice notice inline">This is admin notice 3</div>',
+			),
+
+			// Check 4 - Success Notice.
+			array(
+				array(
+					'description' => 'This is admin notice 4',
+					'id'          => 'give-inline-notice-4',
+					'echo'        => false,
+					'notice_type' => 'updated',
+					'dismissible' => false,
+				),
+				'<div id="give-inline-notice-4" class="notice-updated give-notice notice inline">This is admin notice 4</div>',
+			),
+
+			// Check 5 - Dismissible Success Notice.
+			array(
+				array(
+					'description' => 'This is admin notice 5',
+					'id'          => 'give-inline-notice-5',
+					'echo'        => false,
+					'notice_type' => 'updated',
+					'dismissible' => true,
+				),
+				'<div id="give-inline-notice-5" class="notice-updated give-notice notice inline is-dismissible">This is admin notice 5</div>',
+			),
 		);
-		$notice = Give()->notices->print_admin_notices( 'This is admin notice', $args );
-		$this->assertEquals( '<div id="give-inline-notice" class="notice-info give-notice notice inline ">This is admin notice</div>', trim($notice) );
-
-		// Check 2 - Error Notice.
-		$args = array(
-			'echo'        => false,
-			'notice_type' => 'error',
-			'dismissible' => false
-		);
-		$notice = Give()->notices->print_admin_notices( 'This is admin notice', $args );
-		$this->assertEquals( '<div id="give-inline-notice" class="notice-error give-notice notice inline ">This is admin notice</div>', trim($notice) );
-
-		// Check 3 - Default Warning Notice.
-		$args = array(
-			'echo'        => false,
-			'dismissible' => false
-		);
-		$notice = Give()->notices->print_admin_notices( 'This is admin notice', $args );
-		$this->assertEquals( '<div id="give-inline-notice" class="notice-warning give-notice notice inline ">This is admin notice</div>', trim($notice) );
-
-		// Check 4 - Success Notice.
-		$args = array(
-			'echo'        => false,
-			'notice_type' => 'updated',
-			'dismissible' => false
-		);
-		$notice = Give()->notices->print_admin_notices( 'This is admin notice', $args );
-		$this->assertEquals( '<div id="give-inline-notice" class="notice-updated give-notice notice inline ">This is admin notice</div>', trim($notice) );
-
-		// Check 5 - Dismissible Success Notice.
-		$args = array(
-			'echo'        => false,
-			'notice_type' => 'updated',
-			'dismissible' => true
-		);
-		$notice = Give()->notices->print_admin_notices( 'This is admin notice', $args );
-		$this->assertEquals( '<div id="give-inline-notice" class="notice-updated give-notice notice inline is-dismissible">This is admin notice</div>', trim($notice) );
-
 	}
 
 }
