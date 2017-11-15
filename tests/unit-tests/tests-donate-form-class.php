7f1c8e3fa0ea1da3dc66e99a977d51c9e2e0f9c9
Deepak Gupta <raftaar1191@gmail.com>
2017-11-16 01:20:25 +0530

    Add PHPUnit #2337

--- tests/unit-tests/tests-donate-form-class.php
+++ tests/unit-tests/tests-donate-form-class.php
@@ -97,4 +97,38 @@ class Tests_Donate_Form_Class extends Give_Unit_Test_Case {
 		$this->assertEquals( 5000, $simple_form->get_goal() );
 	}
 
+	/**
+	 * Test get form wrap classes
+	 *
+	 * @covers Give_Donate_Form::get_form_wrap_classes
+	 */
+	public function test_get_form_wrap_classes() {
+		$simple_form = new Give_Donate_Form( $this->_simple_form->ID );
+		$donation_form_close = $simple_form->is_close_donation_form();
+
+		// Testing donation form is closed class
+		$class = $simple_form->get_form_wrap_classes( array() );
+		if ( true === $donation_form_close ) {
+			$this->assertContains('give-form-closed', $class);
+		} else {
+			$this->assertNotContains('give-form-closed', $class);
+		}
+
+		// Check the model display type class that is pass
+		$class = $simple_form->get_form_wrap_classes( array( 'display_style' => 'modal' ) );
+		$this->assertContains('give-display-modal', $class);
+
+		// Checking the button display type class that is pass through it
+		$class = $simple_form->get_form_wrap_classes( array( 'display_style' => 'button' ) );
+		$this->assertContains('give-display-button', $class);
+		$this->assertContains('give-display-button-only', $class);
+
+		// Update the DB and  checking if get_form_wrap_classes is fetching the proper class from the give meta.
+		$payment_display = give_get_meta( $this->_simple_form->ID, '_give_payment_display', true );
+		give_update_meta( $this->_simple_form->ID, '_give_payment_display', 'modal' );
+		$class = $simple_form->get_form_wrap_classes( array() );
+		$this->assertContains('give-display-modal', $class);
+		$payment_display = give_update_meta( $this->_simple_form->ID, '_give_payment_display', $payment_display );
+	}
+
 }
