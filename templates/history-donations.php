fce1e8d65ce805c7c2fa7633ae3245e2f367348c
mehul0810 <mehul.gohil0810@gmail.com>
2017-11-22 17:01:27 +0530

    added new conditional to check for history access and show donations based on it.

--- templates/history-donations.php
+++ templates/history-donations.php
@@ -13,7 +13,10 @@ if ( is_user_logged_in() ) {
 } elseif ( Give()->email_access->token_exists ) {
 	// Email Access Token?
 	$donations = give_get_users_donations( 0, 20, true, 'any' );
-} elseif ( Give()->session->get_session_expiration() !== false ) {
+} elseif (
+	false !== Give()->session->get_session_expiration() ||
+	true === give_get_history_session()
+) {
 	// Session active?
 	$email     = Give()->session->get( 'give_email' );
 	$donations = give_get_users_donations( $email, give_get_non_login_users_donations(), true, 'any' );
@@ -37,80 +40,80 @@ if ( $donations ) : ?>
 	<div class="give_user_history_main" >
 		<div class="give_user_history_notice"></div>
 		<table id="give_user_history" class="give-table">
-		<thead>
-		<tr class="give-donation-row">
-			<?php
-			/**
-			 * Fires in current user donation history table, before the header row start.
-			 *
-			 * Allows you to add new <th> elements to the header, before other headers in the row.
-			 *
-			 * @since 1.7
-			 */
-			do_action( 'give_donation_history_header_before' );
-
-			foreach ( $donation_history_args as $index => $value ) {
-				if ( filter_var( $donation_history_args[ $index ], FILTER_VALIDATE_BOOLEAN ) ) :
-					echo sprintf(
-						'<th scope="col" class="give-donation-%1$s>">%2$s</th>',
-						$index,
-						$table_headings[ $index ]
-					);
-				endif;
-			}
-
-			/**
-			 * Fires in current user donation history table, after the header row ends.
-			 *
-			 * Allows you to add new <th> elements to the header, after other headers in the row.
-			 *
-			 * @since 1.7
-			 */
-			do_action( 'give_donation_history_header_after' );
-			?>
-		</tr>
-		</thead>
-		<?php foreach ( $donations as $post ) :
-			setup_postdata( $post );
-			$donation_data = give_get_payment_meta( $post->ID ); ?>
+			<thead>
 			<tr class="give-donation-row">
 				<?php
 				/**
-				 * Fires in current user donation history table, before the row statrs.
+				 * Fires in current user donation history table, before the header row start.
 				 *
-				 * Allows you to add new <td> elements to the row, before other elements in the row.
+				 * Allows you to add new <th> elements to the header, before other headers in the row.
 				 *
 				 * @since 1.7
-				 *
-				 * @param int   $post_id       The ID of the post.
-				 * @param mixed $donation_data Payment meta data.
 				 */
-				do_action( 'give_donation_history_row_start', $post->ID, $donation_data );
+				do_action( 'give_donation_history_header_before' );
 
-				if ( filter_var( $donation_history_args['id'], FILTER_VALIDATE_BOOLEAN ) ) :
-					echo sprintf(
-						'<td class="give-donation-id">#%1$s</td>',
-						give_get_payment_number( $post->ID )
-					);
-				endif;
+				foreach ( $donation_history_args as $index => $value ) {
+					if ( filter_var( $donation_history_args[ $index ], FILTER_VALIDATE_BOOLEAN ) ) :
+						echo sprintf(
+							'<th scope="col" class="give-donation-%1$s>">%2$s</th>',
+							$index,
+							$table_headings[ $index ]
+						);
+					endif;
+				}
 
-				if ( filter_var( $donation_history_args['date'], FILTER_VALIDATE_BOOLEAN ) ) :
-					echo sprintf(
-						'<td class="give-donation-date">%1$s</td>',
-						date_i18n( give_date_format(), strtotime( get_post_field( 'post_date', $post->ID ) ) )
-					);
-				endif;
-
-				if ( filter_var( $donation_history_args['donor'], FILTER_VALIDATE_BOOLEAN ) ) :
-					echo sprintf(
-						'<td class="give-donation-donor">%1$s</td>',
-						give_get_donor_name_by( $post->ID )
-					);
-				endif;
+				/**
+				 * Fires in current user donation history table, after the header row ends.
+				 *
+				 * Allows you to add new <th> elements to the header, after other headers in the row.
+				 *
+				 * @since 1.7
+				 */
+				do_action( 'give_donation_history_header_after' );
 				?>
+			</tr>
+			</thead>
+			<?php foreach ( $donations as $post ) :
+				setup_postdata( $post );
+				$donation_data = give_get_payment_meta( $post->ID ); ?>
+				<tr class="give-donation-row">
+					<?php
+					/**
+					 * Fires in current user donation history table, before the row statrs.
+					 *
+					 * Allows you to add new <td> elements to the row, before other elements in the row.
+					 *
+					 * @since 1.7
+					 *
+					 * @param int   $post_id       The ID of the post.
+					 * @param mixed $donation_data Payment meta data.
+					 */
+					do_action( 'give_donation_history_row_start', $post->ID, $donation_data );
 
-				<?php if ( filter_var( $donation_history_args['amount'], FILTER_VALIDATE_BOOLEAN ) ) : ?>
-					<td class="give-donation-amount">
+					if ( filter_var( $donation_history_args['id'], FILTER_VALIDATE_BOOLEAN ) ) :
+						echo sprintf(
+							'<td class="give-donation-id">#%1$s</td>',
+							give_get_payment_number( $post->ID )
+						);
+					endif;
+
+					if ( filter_var( $donation_history_args['date'], FILTER_VALIDATE_BOOLEAN ) ) :
+						echo sprintf(
+							'<td class="give-donation-date">%1$s</td>',
+							date_i18n( give_date_format(), strtotime( get_post_field( 'post_date', $post->ID ) ) )
+						);
+					endif;
+
+					if ( filter_var( $donation_history_args['donor'], FILTER_VALIDATE_BOOLEAN ) ) :
+						echo sprintf(
+							'<td class="give-donation-donor">%1$s</td>',
+							give_get_donor_name_by( $post->ID )
+						);
+					endif;
+					?>
+
+					<?php if ( filter_var( $donation_history_args['amount'], FILTER_VALIDATE_BOOLEAN ) ) : ?>
+						<td class="give-donation-amount">
 						<span class="give-donation-amount">
 							<?php
 							$currency_code   = give_get_payment_currency_code( $post->ID );
@@ -135,72 +138,72 @@ if ( $donations ) : ?>
 							echo apply_filters( 'give_donation_history_row_amount', $donation_amount, $post->ID );
 							?>
 						</span>
-					</td>
-				<?php endif; ?>
+						</td>
+					<?php endif; ?>
 
-				<?php
-				if ( filter_var( $donation_history_args['status'], FILTER_VALIDATE_BOOLEAN ) ) :
-					echo sprintf(
-						'<td class="give-donation-status">%1$s</td>',
-						give_get_payment_status( $post, true )
-					);
-				endif;
-
-				if ( filter_var( $donation_history_args['payment_method'], FILTER_VALIDATE_BOOLEAN ) ) :
-					echo sprintf(
-						'<td class="give-donation-payment-method">%1$s</td>',
-						give_get_gateway_checkout_label( give_get_payment_gateway( $post->ID ) )
-					);
-				endif;
-				?>
-				<td class="give-donation-details">
 					<?php
-					// Display View Receipt or.
-					if ( 'publish' !== $post->post_status && 'subscription' !== $post->post_status ) :
+					if ( filter_var( $donation_history_args['status'], FILTER_VALIDATE_BOOLEAN ) ) :
 						echo sprintf(
-							'<a href="%1$s"><span class="give-donation-status %2$s">%3$s</span></a>',
-							esc_url(
-								add_query_arg(
-									'payment_key',
-									give_get_payment_key( $post->ID ),
-									give_get_history_page_uri()
-								)
-							),
-							$post->post_status,
-							__( 'View', 'give' ) . ' ' . give_get_payment_status( $post, true ) . ' &raquo;'
+							'<td class="give-donation-status">%1$s</td>',
+							give_get_payment_status( $post, true )
 						);
+					endif;
 
-					else :
+					if ( filter_var( $donation_history_args['payment_method'], FILTER_VALIDATE_BOOLEAN ) ) :
 						echo sprintf(
-							'<a href="%1$s">%2$s</a>',
-							esc_url(
-								add_query_arg(
-									'payment_key',
-									give_get_payment_key( $post->ID ),
-									give_get_history_page_uri()
-								)
-							),
-							__( 'View Receipt &raquo;', 'give' )
+							'<td class="give-donation-payment-method">%1$s</td>',
+							give_get_gateway_checkout_label( give_get_payment_gateway( $post->ID ) )
 						);
-
 					endif;
 					?>
-				</td>
-				<?php
-				/**
-				 * Fires in current user donation history table, after the row ends.
-				 *
-				 * Allows you to add new <td> elements to the row, after other elements in the row.
-				 *
-				 * @since 1.7
-				 *
-				 * @param int   $post_id       The ID of the post.
-				 * @param mixed $donation_data Payment meta data.
-				 */
-				do_action( 'give_donation_history_row_end', $post->ID, $donation_data );
-				?>
-			</tr>
-		<?php endforeach; ?>
+					<td class="give-donation-details">
+						<?php
+						// Display View Receipt or.
+						if ( 'publish' !== $post->post_status && 'subscription' !== $post->post_status ) :
+							echo sprintf(
+								'<a href="%1$s"><span class="give-donation-status %2$s">%3$s</span></a>',
+								esc_url(
+									add_query_arg(
+										'payment_key',
+										give_get_payment_key( $post->ID ),
+										give_get_history_page_uri()
+									)
+								),
+								$post->post_status,
+								__( 'View', 'give' ) . ' ' . give_get_payment_status( $post, true ) . ' &raquo;'
+							);
+
+						else :
+							echo sprintf(
+								'<a href="%1$s">%2$s</a>',
+								esc_url(
+									add_query_arg(
+										'payment_key',
+										give_get_payment_key( $post->ID ),
+										give_get_history_page_uri()
+									)
+								),
+								__( 'View Receipt &raquo;', 'give' )
+							);
+
+						endif;
+						?>
+					</td>
+					<?php
+					/**
+					 * Fires in current user donation history table, after the row ends.
+					 *
+					 * Allows you to add new <td> elements to the row, after other elements in the row.
+					 *
+					 * @since 1.7
+					 *
+					 * @param int   $post_id       The ID of the post.
+					 * @param mixed $donation_data Payment meta data.
+					 */
+					do_action( 'give_donation_history_row_end', $post->ID, $donation_data );
+					?>
+				</tr>
+			<?php endforeach; ?>
 
 			<?php
 			/**
@@ -212,7 +215,7 @@ if ( $donations ) : ?>
 			 */
 			do_action( 'give_donation_history_table_end' );
 			?>
-	</table>
+		</table>
 		<div id="give-donation-history-pagination" class="give_pagination navigation">
 			<?php
 			$big = 999999;
