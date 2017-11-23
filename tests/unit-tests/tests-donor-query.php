83062524e5e901a57c77d50fba86afe2721049d2
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2017-11-23 11:43:28 +0530

    Update table name in broken phpunit test

--- tests/unit-tests/tests-donor-query.php
+++ tests/unit-tests/tests-donor-query.php
@@ -62,15 +62,15 @@ class Tests_Give_Donors_Query extends Give_Unit_Test_Case {
 			 */
 			array(
 				array(),
-				'SELECT wptests_give_customers.* FROM wptests_give_customers WHERE 1=1 ORDER BY wptests_give_customers.id+0 DESC LIMIT 0,20;',
+				'SELECT wptests_give_donors.* FROM wptests_give_donors WHERE 1=1 ORDER BY wptests_give_donors.id+0 DESC LIMIT 0,20;',
 			),
 			array(
 				array( 'number' => 2 ),
-				'SELECT wptests_give_customers.* FROM wptests_give_customers WHERE 1=1 ORDER BY wptests_give_customers.id+0 DESC LIMIT 0,2;',
+				'SELECT wptests_give_donors.* FROM wptests_give_donors WHERE 1=1 ORDER BY wptests_give_donors.id+0 DESC LIMIT 0,2;',
 			),
 			array(
 				array( 'number' => - 1 ),
-				'SELECT wptests_give_customers.* FROM wptests_give_customers WHERE 1=1 ORDER BY wptests_give_customers.id+0 DESC LIMIT 0,999999999999;',
+				'SELECT wptests_give_donors.* FROM wptests_give_donors WHERE 1=1 ORDER BY wptests_give_donors.id+0 DESC LIMIT 0,999999999999;',
 			),
 
 			/**
@@ -78,47 +78,47 @@ class Tests_Give_Donors_Query extends Give_Unit_Test_Case {
 			 */
 			array(
 				array( 's' => 'name:Devin' ),
-				'SELECT wptests_give_customers.* FROM wptests_give_customers WHERE 1=1 AND wptests_give_customers.name LIKE \'%Devin%\' ORDER BY wptests_give_customers.id+0 DESC LIMIT 0,20;',
+				'SELECT wptests_give_donors.* FROM wptests_give_donors WHERE 1=1 AND wptests_give_donors.name LIKE \'%Devin%\' ORDER BY wptests_give_donors.id+0 DESC LIMIT 0,20;',
 			),
 			array(
 				array( 's' => 'note:Devin' ),
-				'SELECT wptests_give_customers.* FROM wptests_give_customers WHERE 1=1 AND wptests_give_customers.notes LIKE \'%Devin%\' ORDER BY wptests_give_customers.id+0 DESC LIMIT 0,20;',
+				'SELECT wptests_give_donors.* FROM wptests_give_donors WHERE 1=1 AND wptests_give_donors.notes LIKE \'%Devin%\' ORDER BY wptests_give_donors.id+0 DESC LIMIT 0,20;',
 			),
 			array(
 				array( 'email' => 'devin@givewp.com' ),
-				'SELECT wptests_give_customers.* FROM wptests_give_customers WHERE 1=1 AND wptests_give_customers.email = \'devin@givewp.com\' ORDER BY wptests_give_customers.id+0 DESC LIMIT 0,20;',
+				'SELECT wptests_give_donors.* FROM wptests_give_donors WHERE 1=1 AND wptests_give_donors.email = \'devin@givewp.com\' ORDER BY wptests_give_donors.id+0 DESC LIMIT 0,20;',
 			),
 			array(
 				array( 'email' => array( 'devin@givewp.com', 'matt@givewp.com' ) ),
-				'SELECT wptests_give_customers.* FROM wptests_give_customers WHERE 1=1 AND wptests_give_customers.email IN( \'devin@givewp.com\', \'matt@givewp.com\' ) ORDER BY wptests_give_customers.id+0 DESC LIMIT 0,20;',
+				'SELECT wptests_give_donors.* FROM wptests_give_donors WHERE 1=1 AND wptests_give_donors.email IN( \'devin@givewp.com\', \'matt@givewp.com\' ) ORDER BY wptests_give_donors.id+0 DESC LIMIT 0,20;',
 			),
 			array(
 				array( 'donor' => 1 ),
-				'SELECT wptests_give_customers.* FROM wptests_give_customers WHERE 1=1 AND wptests_give_customers.id IN( 1 ) ORDER BY wptests_give_customers.id+0 DESC LIMIT 0,20;',
+				'SELECT wptests_give_donors.* FROM wptests_give_donors WHERE 1=1 AND wptests_give_donors.id IN( 1 ) ORDER BY wptests_give_donors.id+0 DESC LIMIT 0,20;',
 			),
 			array(
 				array( 'donor' => array( 1, 2 ) ),
-				'SELECT wptests_give_customers.* FROM wptests_give_customers WHERE 1=1 AND wptests_give_customers.id IN( 1,2 ) ORDER BY wptests_give_customers.id+0 DESC LIMIT 0,20;',
+				'SELECT wptests_give_donors.* FROM wptests_give_donors WHERE 1=1 AND wptests_give_donors.id IN( 1,2 ) ORDER BY wptests_give_donors.id+0 DESC LIMIT 0,20;',
 			),
 			array(
 				array( 'donor' => '1,2' ),
-				'SELECT wptests_give_customers.* FROM wptests_give_customers WHERE 1=1 AND wptests_give_customers.id IN( 1,2 ) ORDER BY wptests_give_customers.id+0 DESC LIMIT 0,20;',
+				'SELECT wptests_give_donors.* FROM wptests_give_donors WHERE 1=1 AND wptests_give_donors.id IN( 1,2 ) ORDER BY wptests_give_donors.id+0 DESC LIMIT 0,20;',
 			),
 			array(
 				array( 'user' => 1 ),
-				'SELECT wptests_give_customers.* FROM wptests_give_customers WHERE 1=1 AND wptests_give_customers.user_id IN( 1 ) ORDER BY wptests_give_customers.id+0 DESC LIMIT 0,20;',
+				'SELECT wptests_give_donors.* FROM wptests_give_donors WHERE 1=1 AND wptests_give_donors.user_id IN( 1 ) ORDER BY wptests_give_donors.id+0 DESC LIMIT 0,20;',
 			),
 			array(
 				array( 'user' => array( 1, 2 ) ),
-				'SELECT wptests_give_customers.* FROM wptests_give_customers WHERE 1=1 AND wptests_give_customers.user_id IN( 1,2 ) ORDER BY wptests_give_customers.id+0 DESC LIMIT 0,20;',
+				'SELECT wptests_give_donors.* FROM wptests_give_donors WHERE 1=1 AND wptests_give_donors.user_id IN( 1,2 ) ORDER BY wptests_give_donors.id+0 DESC LIMIT 0,20;',
 			),
 			array(
 				array( 'user' => '1, 2' ),
-				'SELECT wptests_give_customers.* FROM wptests_give_customers WHERE 1=1 AND wptests_give_customers.user_id IN( 1,2 ) ORDER BY wptests_give_customers.id+0 DESC LIMIT 0,20;',
+				'SELECT wptests_give_donors.* FROM wptests_give_donors WHERE 1=1 AND wptests_give_donors.user_id IN( 1,2 ) ORDER BY wptests_give_donors.id+0 DESC LIMIT 0,20;',
 			),
 			array(
 				array( 'date_query' => 'year=2012&monthnum=12&day=12' ),
-				'SELECT wptests_give_customers.* FROM wptests_give_customers WHERE 1=1  AND ( ( YEAR( wptests_give_customers.date_created ) = 2012 AND MONTH( wptests_give_customers.date_created ) = 12 AND DAYOFMONTH( wptests_give_customers.date_created ) = 12 ) ) ORDER BY wptests_give_customers.id+0 DESC LIMIT 0,20;',
+				'SELECT wptests_give_donors.* FROM wptests_give_donors WHERE 1=1  AND ( ( YEAR( wptests_give_donors.date_created ) = 2012 AND MONTH( wptests_give_donors.date_created ) = 12 AND DAYOFMONTH( wptests_give_donors.date_created ) = 12 ) ) ORDER BY wptests_give_donors.id+0 DESC LIMIT 0,20;',
 			),
 		);
 	}
