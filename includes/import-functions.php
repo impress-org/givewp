55373dbeeaf0dcbe19e159973df26c7a1df1d802
Deepak Gupta <raftaar1191@gmail.com>
2017-11-29 03:50:54 +0530

    Remove lastname check as it's not compulsory #2420

--- includes/import-functions.php
+++ includes/import-functions.php
@@ -267,9 +267,10 @@ function give_import_get_user_from_csv( $data, $import_setting = array() ) {
 		if ( empty( $donor_data->id ) ) {
 			$donor_data = get_user_by( 'email', $data['email'] );
 
-			if ( empty( $donor_data->ID ) && ! empty( $data['first_name'] ) && ! empty( $data['last_name'] ) && isset( $import_setting['create_user'] ) && 1 === absint( $import_setting['create_user'] ) ) {
-				$give_role  = (array) give_get_option( 'donor_default_user_role', get_option( 'default_role', ( ( $give_donor = wp_roles()->is_role( 'give_donor' ) ) && ! empty( $give_donor ) ? 'give_donor' : 'subscriber' ) ) );
-				$donor_args = array(
+			if ( empty( $donor_data->ID ) && ! empty( $data['first_name'] ) && isset( $import_setting['create_user'] ) && 1 === absint( $import_setting['create_user'] ) ) {
+				$data['last_name'] = ( ! empty( $data['last_name'] ) ? $data['last_name'] : '' );
+				$give_role         = (array) give_get_option( 'donor_default_user_role', get_option( 'default_role', ( ( $give_donor = wp_roles()->is_role( 'give_donor' ) ) && ! empty( $give_donor ) ? 'give_donor' : 'subscriber' ) ) );
+				$donor_args        = array(
 					'user_login'      => $data['email'],
 					'user_email'      => $data['email'],
 					'user_registered' => date( 'Y-m-d H:i:s' ),
