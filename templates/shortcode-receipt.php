88589f3535454fd780da01e154e59b7c23aeb7cf
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2017-12-08 13:46:29 +0530

    Fix #2481

--- templates/shortcode-receipt.php
+++ templates/shortcode-receipt.php
@@ -48,7 +48,7 @@ $give_receipt_args['donation_receipt']['date'] = array(
 
 $give_receipt_args['donation_receipt']['total_donation'] = array(
 	'name'    => __( 'Total Donation', 'give' ),
-	'value'   => give_donation_amount( $donation_id, 'receipt' ),
+	'value'   => give_donation_amount( $donation_id ),
 	'display' => $give_receipt_args['price'],
 );
 
