13af5070935da4c388432acad76dd911b2c4df01
mehul0810 <mehul.gohil0810@gmail.com>
2017-12-12 09:55:28 +0530

    Simplified check in admin donation list page for better working for set as well as multi level donations

--- includes/payments/functions.php
+++ includes/payments/functions.php
@@ -1858,26 +1858,21 @@ function give_get_payment_form_title( $payment_meta, $only_level = false, $separ
 		$form_title = '';
 	}
 
-	// If multi-level, append to the form title.
-	if ( give_has_variable_prices( $form_id ) ) {
-
-		// Only add separator if there is a form title.
-		if ( ! empty( $form_title ) ) {
-			$form_title .= ' ' . $separator . ' ';
-		}
-
-		$form_title .= '<span class="donation-level-text-wrap">';
-
-		if ( 'custom' === $price_id ) {
-			$custom_amount_text = give_get_meta( $form_id, '_give_custom_amount_text', true );
-			$form_title         .= ! empty( $custom_amount_text ) ? $custom_amount_text : __( 'Custom Amount', 'give' );
-		} else {
-			$form_title .= give_get_price_option_name( $form_id, $price_id );
-		}
-
-		$form_title .= '</span>';
-
+	// Only add separator if there is a form title.
+	if ( ! empty( $form_title ) ) {
+		$form_title .= ' ' . $separator . ' ';
 	}
+
+	$form_title .= '<span class="donation-level-text-wrap">';
+
+	if ( 'custom' === $price_id ) {
+		$custom_amount_text = give_get_meta( $form_id, '_give_custom_amount_text', true );
+		$form_title         .= ! empty( $custom_amount_text ) ? $custom_amount_text : __( 'Custom Amount', 'give' );
+	} elseif ( give_has_variable_prices( $form_id ) ) {
+		$form_title .= give_get_price_option_name( $form_id, $price_id );
+	}
+
+	$form_title .= '</span>';
 
 	return apply_filters( 'give_get_payment_form_title', $form_title, $payment_meta );
 
