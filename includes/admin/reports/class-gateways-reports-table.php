2acc2a6eef5d3bd7ae747bec54063cd3ee0864a8
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2017-12-12 15:42:36 +0530

    Add donation list filter links on gateways report

--- includes/admin/reports/class-gateways-reports-table.php
+++ includes/admin/reports/class-gateways-reports-table.php
@@ -70,13 +70,16 @@ class Give_Gateway_Reports_Table extends WP_List_Table {
 				$status = 'publish';
 				
 			case 'pending_sales':
+				$status = 'pending';
+
+			case 'total_sales':
 				$value = $item[ $column_name ];
-				$status = ! empty( $status ) ? $status : 'pending';
+				$status = ! empty( $status ) ? "status={$status}" : '';
 
 				if ( $item[ $column_name ] ) {
 					$value = sprintf(
 						'<a href="%1$s" target="_blank">%2$s</a>',
-						admin_url( "edit.php?post_type=give_forms&page=give-payment-history&status={$status}&gateway={$item['ID']}" ),
+						admin_url( "edit.php?post_type=give_forms&page=give-payment-history&{$status}&gateway={$item['ID']}" ),
 						$item[ $column_name ]
 					);
 				}
