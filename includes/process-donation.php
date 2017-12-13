312a24cf99dbeb28d9d0c6d833cc221963dff0a1
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2017-12-13 08:31:14 +0530

    Rename fn

--- includes/process-donation.php
+++ includes/process-donation.php
@@ -1189,7 +1189,7 @@ function give_donation_form_validate_cc_zip( $zip = 0, $country_code = '' ) {
 
 
 /**
- * Auto set correct donation level id on basis of amount.
+ * Validate donation amount and auto set correct donation level id on basis of amount.
  *
  * Note: If amount does not match to donation level amount then level id will be auto select to first match level id on basis of amount.
  *
@@ -1198,7 +1198,7 @@ function give_donation_form_validate_cc_zip( $zip = 0, $country_code = '' ) {
  *
  * @return bool
  */
-function give_validate_donation_custom_amount( $valid_data, $data ) {
+function give_validate_donation_amount( $valid_data, $data ) {
 	/* @var Give_Donate_Form $form */
 	$form = new Give_Donate_Form( $data['give-form-id'] );
 
@@ -1255,4 +1255,4 @@ function give_validate_donation_custom_amount( $valid_data, $data ) {
 	return ( $donation_level_matched ? true : false );
 }
 
-add_action( 'give_checkout_error_checks', 'give_validate_donation_custom_amount', 10, 2 );
+add_action( 'give_checkout_error_checks', 'give_validate_donation_amount', 10, 2 );
