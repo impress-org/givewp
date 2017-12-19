6a21447286dc47ef748af88b5f2bb87ffe215b13
raftaar1191 <raftaar1191@gmail.com>
2017-12-19 19:51:25 +0530

    Filter and action add

--- includes/admin/payments/class-payments-table.php
+++ includes/admin/payments/class-payments-table.php
@@ -326,6 +326,16 @@ class Give_Payment_History_Table extends WP_List_Table {
 			),
 		);
 
+		/**
+		 * Remove Query from Args of the URL that are being pass to Donation Status.
+		 *
+		 * @since 1.8.18
+		 */
+		$args = (array) apply_filters( 'give_payments_table_status_remove_query_arg', array( 'paged', '_wpnonce', '_wp_http_referer' ) );
+
+		// Build URL.
+		$staus_url = remove_query_arg( $args );
+
 		foreach ( $tabs as $key => $tab ) {
 			$count_key = $tab[0];
 			$name      = $tab[1];
@@ -342,17 +352,9 @@ class Give_Payment_History_Table extends WP_List_Table {
 			 * @since 1.8.12
 			 */
 			if ( 'all' === $key || $key === $current || apply_filters( 'give_payments_table_show_all_status', 0 < $count, $key, $count ) ) {
-				/**
-				 * Remove Query from Args of the URL that are being pass to Donation Status.
-				 *
-				 * @since 1.8.18
-				 */
-				$args = (array) apply_filters( 'give_payments_table_status_remove_query_arg', array( 'paged', '_wpnonce', '_wp_http_referer' ) );
 
-				// Build URL.
-				$staus_url = remove_query_arg( $args );
 				$staus_url = 'all' === $key ?
-					add_query_arg( array( 'status' => false ), $staus_url ) :
+					add_query_arg( array( 'status' => false ), apply_filters( 'give_payments_table_status_all_query_arg', $staus_url ) ) :
 					add_query_arg( array( 'status' => $key ), $staus_url );
 
 				$views[ $key ] = sprintf(
