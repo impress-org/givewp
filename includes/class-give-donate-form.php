f4558211f4d91c097cd29480ee7dc63959d55db1
mehul0810 <mehul.gohil0810@gmail.com>
2018-01-04 17:36:19 +0530

    Fix - Strange Behaviour due to comparison with earnings instead of sales

--- includes/class-give-donate-form.php
+++ includes/class-give-donate-form.php
@@ -1084,7 +1084,7 @@ class Give_Donate_Form {
 			(
 			give_is_setting_enabled( give_get_meta( $this->ID, '_give_goal_option', true ) ) )
 			&& give_is_setting_enabled( give_get_meta( $this->ID, '_give_close_form_when_goal_achieved', true ) )
-			&& ( $this->get_goal() <= $this->get_earnings()
+			&& ( $this->get_goal() <= $this->get_sales()
 			),
 			$this->ID
 		);
