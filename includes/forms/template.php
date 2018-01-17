fa5796b52d4eb6cc8a92886bf07f5ed630d3e8db
raftaar1191 <raftaar1191@gmail.com>
2018-01-17 16:29:40 +0530

    Add class give-disable which return false and add some css  and run gulp #2664

--- includes/forms/template.php
+++ includes/forms/template.php
@@ -1117,12 +1117,12 @@ function give_get_register_fields( $form_id ) {
 					if( ! give_is_setting_enabled( $is_guest_checkout ) ) {
 						echo Give()->tooltips->render(
 							array(
-								'tag_content' => '<input type="checkbox" name="give_create_account" id="give-create-account-" class="give-input" checked disabled /><input type="hidden" name="give_create_account" value="on" />',
+								'tag_content' => '<input type="checkbox" name="give_create_account" value="on" id="give-create-account-" class="give-input give-disabled" checked />',
 								'label' => __( 'Registration is required to donate.', 'give' ),
 							) );
 					} else {
 						?>
-						<input type="checkbox" name="give_create_account" id="give-create-account-<?php echo $form_id; ?>" class="give-input" />
+						<input type="checkbox" name="give_create_account" value="on" id="give-create-account-<?php echo $form_id; ?>" class="give-input" />
 						<?php
 					}
 					?>
