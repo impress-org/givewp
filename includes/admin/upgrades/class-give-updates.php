8bb125072337f6627e593be139c2b185f1b5c8b7
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2018-01-23 05:49:52 +0530

    Update give_doing_update when update update batch pass2

--- includes/admin/upgrades/class-give-updates.php
+++ includes/admin/upgrades/class-give-updates.php
@@ -459,8 +459,9 @@ class Give_Updates {
 			update_option( $batch->key, $batch->data );
 
 			// Reset update info.
-			$doing_upgrade_args = get_option( 'give_doing_upgrade' );
-			$doing_upgrade_args['heading'] = sprintf( 'Update %s of %s', $give_updates->update, get_option( 'give_db_update_count' ) );
+			$doing_upgrade_args                     = get_option( 'give_doing_upgrade' );
+			$doing_upgrade_args['update']           = $give_updates->update;
+			$doing_upgrade_args['heading']          = sprintf( 'Update %s of %s', $give_updates->update, get_option( 'give_db_update_count' ) );
 			$doing_upgrade_args['total_percentage'] = $this->get_db_update_processing_percentage();
 			update_option( 'give_doing_upgrade', $doing_upgrade_args );
 
