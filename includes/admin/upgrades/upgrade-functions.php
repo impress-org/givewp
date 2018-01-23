91948c4cc78db3d382304208b7b1714f7db1c580
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2018-01-23 09:03:51 +0530

    Do not run 2.0.1 automatic update on fresh install

--- includes/admin/upgrades/upgrade-functions.php
+++ includes/admin/upgrades/upgrade-functions.php
@@ -79,8 +79,12 @@ function give_do_automatic_upgrades() {
 			$did_upgrade = true;
 
 		case version_compare( $give_version, '2.0.1', '<' ) :
-			give_v201_create_tables();
-			Give_Updates::get_instance()->__health_background_update( Give_Updates::get_instance() );
+			// Do nothing on fresh install.
+			if( ! doing_action( 'give_upgrades' ) ) {
+				give_v201_create_tables();
+				Give_Updates::get_instance()->__health_background_update( Give_Updates::get_instance() );
+			}
+
 			$did_upgrade = true;
 	}
 
