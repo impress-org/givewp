aac124cdfce11eb086efcb14a48429bb6212aeaf
raftaar1191 <raftaar1191@gmail.com>
2018-01-31 00:10:18 +0530

    Add DB upgrade #2250

--- includes/admin/upgrades/upgrade-functions.php
+++ includes/admin/upgrades/upgrade-functions.php
@@ -314,9 +314,60 @@ function give_show_upgrade_notices( $give_updates ) {
 
 		)
 	);
+
+	// v2.0.2 Upgrades
+	$give_updates->register(
+		array(
+			'id'       => 'v202_add_form_goal_meta',
+			'version'  => '2.0.2',
+			'callback' => 'give_v202_add_form_goal_meta_callback',
+			'depend'   => array(
+				'v20_upgrades_form_metadata',
+			),
+		)
+	);
 }
 
 add_action( 'give_register_updates', 'give_show_upgrade_notices' );
+
+/**
+ * Update Goal Meta when Dontion form is closed.
+ *
+ * @since 2.0.2
+ */
+function give_v202_add_form_goal_meta_callback() {
+	/* @var Give_Updates $give_updates */
+	$give_updates = Give_Updates::get_instance();
+
+	// form query
+	$forms = new WP_Query( array(
+			'paged'          => $give_updates->step,
+			'status'         => 'any',
+			'order'          => 'ASC',
+			'post_type'      => 'give_forms',
+			'posts_per_page' => 20,
+		)
+	);
+
+	if ( $forms->have_posts() ) {
+		$give_updates->set_percentage( $forms->found_posts, ( $give_updates->step * 20 ) );
+
+		while ( $forms->have_posts() ) {
+			$forms->the_post();
+
+			$form_id = get_the_ID();
+			$form    = new Give_Donate_Form( $form_id );
+			$form->set_goal_closed_meta();
+
+		}// End while().
+
+		wp_reset_postdata();
+
+	} else {
+		// No more forms found, finish up.
+		give_set_upgrade_complete( 'v202_add_form_goal_meta' );
+	}
+}
 
 /**
  * Triggers all upgrade functions
