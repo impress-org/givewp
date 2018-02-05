1b01caf7ca9d78fbae985411da0a89f9d852fcd0
Sidsector9 <siddharth.thevaril@gmail.com>
2018-02-05 17:05:59 +0530

    issue-1414 Add popup form for donation grid shortcode

--- includes/admin/shortcodes/shortcode-give-donation-grid.php
+++ includes/admin/shortcodes/shortcode-give-donation-grid.php
@@ -28,6 +28,67 @@ class Give_Shortcode_Donation_Grid extends Give_Shortcode_Generator {
 
 		parent::__construct( 'donation_grid' );
 	}
+
+	/**
+	 * Define the shortcode attribute fields
+	 *
+	 * @return array
+	 */
+	public function define_fields() {
+
+		return array(
+			array(
+				'type' => 'container',
+				'html' => sprintf( '<p class="strong margin-top">%s</p>', esc_html__( 'Optional settings', 'give' ) ),
+			),
+			array(
+				'type'    => 'listbox',
+				'name'    => 'columns',
+				'label'   => esc_attr__( 'Columns:', 'give' ),
+				'tooltip' => esc_attr__( 'Sets the number of donations per row.', 'give' ),
+				'options' => array(
+					'1' => esc_html__( '1', 'give' ),
+					'2' => esc_html__( '2', 'give' ),
+					'3' => esc_html__( '3', 'give' ),
+					'4' => esc_html__( '4', 'give' ),
+					'5' => esc_html__( '5', 'give' ),
+				),
+				'required'    => array(
+					'alert' => esc_html__( 'You must select the number of donations per row!', 'give' ),
+				),
+			),
+			array(
+				'type'    => 'listbox',
+				'name'    => 'show_goal',
+				'label'   => esc_attr__( 'Show Goal:', 'give' ),
+				'tooltip' => esc_attr__( 'Do you want to display the goal\'s progress bar?', 'give' ),
+				'options' => array(
+					'true'  => esc_html__( 'Show', 'give' ),
+					'false' => esc_html__( 'Hide', 'give' ),
+				),
+			),
+			array(
+				'type'    => 'listbox',
+				'name'    => 'show_excerpt',
+				'label'   => esc_attr__( 'Show Excerpt:', 'give' ),
+				'tooltip' => esc_attr__( 'Do you want to display the excerpt?', 'give' ),
+				'options' => array(
+					'true'  => esc_html__( 'Show', 'give' ),
+					'false' => esc_html__( 'Hide', 'give' ),
+				),
+			),
+			array(
+				'type'    => 'listbox',
+				'name'    => 'show_featured_image',
+				'label'   => esc_attr__( 'Show Featured Image:', 'give' ),
+				'tooltip' => esc_attr__( 'Do you want to display the featured image?', 'give' ),
+				'options' => array(
+					'true'  => esc_html__( 'Show', 'give' ),
+					'false' => esc_html__( 'Hide', 'give' ),
+				),
+			),
+		);
+	}
 }
 
 new Give_Shortcode_Donation_Grid();
