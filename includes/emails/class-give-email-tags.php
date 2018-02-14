e606f6f56d85aa97683cc78b643bfb857f201a90
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2018-02-14 16:50:13 +0530

    fix(email): replace only first occurrence of meta type

--- includes/emails/class-give-email-tags.php
+++ includes/emails/class-give-email-tags.php
@@ -1406,7 +1406,7 @@ function __give_render_metadata_email_tag( $content, $tag_args ) {
 
 			$meta_tag  = str_replace( array( '{', 'meta_', '}' ), '', $meta_tag );
 			$type      = current( explode( '_', $meta_tag, 2 ) );
-			$meta_name = str_replace( "{$type}_", '', $meta_tag );
+			$meta_name = preg_replace( "/^{$type}_/", '', $meta_tag );
 
 			switch ( $type ) {
 				case 'donation':
