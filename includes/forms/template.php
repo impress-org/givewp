cbca8c6a7385fbc6e051c24ca67aed64e66c0c76
raftaar1191 <raftaar1191@gmail.com>
2018-02-23 11:59:52 +0530

    fix(donation): Change Minimum Amount to 1.0 if empty  #2819

--- includes/forms/template.php
+++ includes/forms/template.php
@@ -486,11 +486,10 @@ function give_output_levels( $form_id ) {
 	$display_style      = give_get_meta( $form_id, '_give_display_style', true );
 	$custom_amount      = give_get_meta( $form_id, '_give_custom_amount', true );
 	$custom_amount_text = give_get_meta( $form_id, '_give_custom_amount_text', true );
-	$minimum_amount     = give_get_meta( $form_id, '_give_custom_amount_minimum', true, 'custom' );
 
-	if ( 'custom' !== $minimum_amount ) {
-		$minimum_amount = give_format_amount( $minimum_amount, array( 'sanitize' => false ) );
-	}
+	$minimum_amount     = give_get_meta( $form_id, '_give_custom_amount_minimum', true, '1.00' );
+	$minimum_amount = give_format_amount( $minimum_amount, array( 'sanitize' => false ) );
+
 
 	if ( empty( $custom_amount_text ) ) {
 		$custom_amount_text = esc_html__( 'Give a Custom Amount', 'give' );
