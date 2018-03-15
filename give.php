d4b27499fc94a71127f39e055dc704f2e57827cd
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2018-03-15 17:37:39 +0530

    feat(donation): add custom table for sequential donation feature

--- give.php
+++ give.php
@@ -25,15 +25,17 @@
  *
  * A Tribute to Open Source:
  *
- * "Open source software is software that can be freely used, changed, and shared (in modified or unmodified form) by anyone. Open
- * source software is made by many people, and distributed under licenses that comply with the Open Source Definition."
+ * "Open source software is software that can be freely used, changed, and shared (in modified or unmodified form) by
+ * anyone. Open source software is made by many people, and distributed under licenses that comply with the Open Source
+ * Definition."
  *
  * -- The Open Source Initiative
  *
- * Give is a tribute to the spirit and philosophy of Open Source. We at WordImpress gladly embrace the Open Source philosophy both
- * in how Give itself was developed, and how we hope to see others build more from our code base.
+ * Give is a tribute to the spirit and philosophy of Open Source. We at WordImpress gladly embrace the Open Source
+ * philosophy both in how Give itself was developed, and how we hope to see others build more from our code base.
  *
- * Give would not have been possible without the tireless efforts of WordPress and the surrounding Open Source projects and their talented developers. Thank you all for your contribution to WordPress.
+ * Give would not have been possible without the tireless efforts of WordPress and the surrounding Open Source projects
+ * and their talented developers. Thank you all for your contribution to WordPress.
  *
  * - The WordImpress Team
  */
@@ -145,6 +147,16 @@ if ( ! class_exists( 'Give' ) ) :
 		 * @var    Give_DB_Donor_Meta object
 		 */
 		public $donor_meta;
+
+		/**
+		 * Give Sequential Donation DB Object
+		 *
+		 * @since  2.1.0
+		 * @access public
+		 *
+		 * @var    Give_DB_Sequential_Donations object
+		 */
+		public $sequential_donation_db;
 
 		/**
 		 * Give API Object
@@ -311,25 +323,26 @@ if ( ! class_exists( 'Give' ) ) :
 			// Set up localization.
 			$this->load_textdomain();
 
-			$this->roles               = new Give_Roles();
-			$this->api                 = new Give_API();
-			$this->give_settings       = new Give_Admin_Settings();
-			$this->session             = new Give_Session();
-			$this->html                = new Give_HTML_Elements();
-			$this->emails              = new Give_Emails();
-			$this->email_tags          = new Give_Email_Template_Tags();
-			$this->donors              = new Give_DB_Donors();
-			$this->donor_meta          = new Give_DB_Donor_Meta();
-			$this->template_loader     = new Give_Template_Loader();
-			$this->email_access        = new Give_Email_Access();
-			$this->tooltips            = new Give_Tooltips();
-			$this->notices             = new Give_Notices();
-			$this->payment_meta        = new Give_DB_Payment_Meta();
-			$this->logs                = new Give_Logging();
-			$this->form_meta           = new Give_DB_Form_Meta();
-			$this->async_process       = new Give_Async_Process();
-			$this->scripts             = new Give_Scripts();
-			$this->seq_donation_number = Give_Seq_Donation_Number::get_instance();
+			$this->roles                  = new Give_Roles();
+			$this->api                    = new Give_API();
+			$this->give_settings          = new Give_Admin_Settings();
+			$this->session                = new Give_Session();
+			$this->html                   = new Give_HTML_Elements();
+			$this->emails                 = new Give_Emails();
+			$this->email_tags             = new Give_Email_Template_Tags();
+			$this->donors                 = new Give_DB_Donors();
+			$this->donor_meta             = new Give_DB_Donor_Meta();
+			$this->template_loader        = new Give_Template_Loader();
+			$this->email_access           = new Give_Email_Access();
+			$this->tooltips               = new Give_Tooltips();
+			$this->notices                = new Give_Notices();
+			$this->payment_meta           = new Give_DB_Payment_Meta();
+			$this->logs                   = new Give_Logging();
+			$this->form_meta              = new Give_DB_Form_Meta();
+			$this->sequential_donation_db = new Give_DB_Sequential_Donations();
+			$this->async_process          = new Give_Async_Process();
+			$this->scripts                = new Give_Scripts();
+			$this->seq_donation_number    = Give_Seq_Donation_Number::get_instance();
 
 			/**
 			 * Fire the action after Give core loads.
@@ -471,6 +484,7 @@ if ( ! class_exists( 'Give' ) ) :
 			require_once GIVE_PLUGIN_DIR . 'includes/class-give-email-access.php';
 			require_once GIVE_PLUGIN_DIR . 'includes/class-give-db-payment-meta.php';
 			require_once GIVE_PLUGIN_DIR . 'includes/class-give-db-form-meta.php';
+			require_once GIVE_PLUGIN_DIR . 'includes/class-give-db-sequential-donations.php';
 
 			require_once GIVE_PLUGIN_DIR . 'includes/country-functions.php';
 			require_once GIVE_PLUGIN_DIR . 'includes/template-functions.php';
---
+++ includes/class-give-db-sequential-donations.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * Sequential Donation DB
+ *
+ * @package     Give
+ * @subpackage  Classes/Give_DB_Sequential_Donations
+ * @copyright   Copyright (c) 2018, WordImpress
+ * @license     https://opensource.org/licenses/gpl-license GNU Public License
+ * @since       2.1.0
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Give_DB_Sequential_Donations Class
+ *
+ * This class is for interacting with the log database table.
+ *
+ * @since 2.1.0
+ */
+class Give_DB_Sequential_Donations extends Give_DB {
+
+	/**
+	 * Give_DB_Sequential_Donations constructor.
+	 *
+	 * Set up the Give DB Donor class.
+	 *
+	 * @since  2.1.0
+	 * @access public
+	 */
+	public function __construct() {
+		/* @var WPDB $wpdb */
+		global $wpdb;
+
+		$this->table_name  = $wpdb->prefix . 'give_sequential_donations';
+		$this->primary_key = 'id';
+		$this->version     = '1.0';
+
+		// Install table.
+		$this->register_table();
+
+		parent::__construct();
+	}
+
+	/**
+	 * Get columns and formats
+	 *
+	 * @since  2.1.0
+	 * @access public
+	 *
+	 * @return array  Columns and formats.
+	 */
+	public function get_columns() {
+		return array(
+			'id'         => '%d',
+			'payment_id' => '%s',
+		);
+	}
+
+	/**
+	 * Get default column values
+	 *
+	 * @since  2.1.0
+	 * @access public
+	 *
+	 * @return array  Default column values.
+	 */
+	public function get_column_defaults() {
+		return array(
+			'id'         => 0,
+			'payment_id' => '',
+		);
+	}
+
+
+	/**
+	 * Create the table
+	 *
+	 * @since  2.1.0
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public function create_table() {
+		global $wpdb;
+		$charset_collate = $wpdb->get_charset_collate();
+
+		$sql = "CREATE TABLE {$this->table_name} (
+        id bigint(20) NOT NULL AUTO_INCREMENT,
+        payment_id bigint(20) NOT NULL,
+        PRIMARY KEY  (id)
+        ) {$charset_collate};";
+
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		dbDelta( $sql );
+
+		update_option( $this->table_name . '_db_version', $this->version );
+	}
+}
