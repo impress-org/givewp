e4d73dccd67a05050d94edb21c931662daf69d39
Siddharth <siddharth.thevaril@gmail.com>
2018-03-21 19:22:33 +0530

    feat(modal-api): Add wrapper functions for filters and updated CSS

--- assets/src/js/frontend/give-popup.js
+++ assets/src/js/frontend/give-popup.js
@@ -1,10 +1,32 @@
+/**
+ * # All popups are an instance of GiveModal class
+ *
+ * # The minimum syntax required to instantiate a popup
+ * depending on the source are of 2 types:
+ *
+ * # `new GiveModal()` should be the first method and `popup()` should be the last method.
+ *
+ * -- 1. From an HTML entity:
+ *    `new GiveModal( '#test-popup' ).popup()`
+ *
+ * -- 2. Dynamically generated content:
+ *    `new GiveModal().customContent({ title: 'Some title', content: 'Some Content' }).popup()`
+ *
+ * # Set the type of popup by chaining one of the following suitable methods:
+ * -- 1. success()
+ * -- 2. notice()
+ * -- 3. warning()
+ * -- 4. error()
+ *
+ * # Chain the `addButton()` to add more buttons to the popup
+ */
+
 const GiveModal = ( function( $ ) {
 
 	/**
 	 * Contructor function.
 	 */
 	function GiveModal( selector ) {
-
 
 		// class or ID of the element.
 		this.selector = selector;
@@ -16,7 +38,11 @@ const GiveModal = ( function( $ ) {
 				src: this.selector,
 				type: 'inline',
 			},
-			popupType: null,
+			focus: '#give-popup-close-button',
+			removalDelay: 300,
+			fixedContentPos: true,
+			fixedBgPos: true,
+			alignTop: true,
 		};
 
 		/**
@@ -29,11 +55,13 @@ const GiveModal = ( function( $ ) {
 			/**
 			 * This function enables you to add a filter.
 			 *
-			 * @param name     Name of the filter hook.
-			 * @param function Callback function to filter the input
+			 * @param string   name   Name of the filter hook.
+			 * @param function filter Callback function to filter the input
 			 */
 			add: function( name, filter ) {
 				( this.filter_obj[ name ] || ( this.filter_obj[ name ] = [] ) ).push( filter );
+
+				// return that;
 			},
 
 
@@ -68,8 +96,7 @@ const GiveModal = ( function( $ ) {
 	 * Adds necessary classes for an error popup.
 	 */
 	GiveModal.prototype.error = function() {
-		this.config.mainClass = this.config.mainClass || 'give-error-popup';
-		this.config.popupType = 'Error';
+		this.config.mainClass = this.config.mainClass || 'popup-fade-slide give-error-popup';
 
 		return this;
 	}
@@ -79,8 +106,7 @@ const GiveModal = ( function( $ ) {
 	 * Adds necessary classes for a warning popup.
 	 */
 	GiveModal.prototype.warning = function() {
-		this.config.mainClass = this.config.mainClass || 'give-warning-popup';
-		this.config.popupType = 'Warning';
+		this.config.mainClass = this.config.mainClass || 'popup-fade-slide give-warning-popup';
 
 		return this;
 	}
@@ -90,8 +116,7 @@ const GiveModal = ( function( $ ) {
 	 * Adds necessary classes for a notice popup.
 	 */
 	GiveModal.prototype.notice = function() {
-		this.config.mainClass = this.config.mainClass || 'give-notice-popup';
-		this.config.popupType = 'Notice';
+		this.config.mainClass = this.config.mainClass || 'popup-fade-slide give-notice-popup';
 
 		return this;
 	}
@@ -101,8 +126,7 @@ const GiveModal = ( function( $ ) {
 	 * Adds necessary classes for a success popup.
 	 */
 	GiveModal.prototype.success = function() {
-		this.config.mainClass = this.config.mainClass || 'give-success-popup';
-		this.config.popupType = 'Success';
+		this.config.mainClass = this.config.mainClass || 'popup-fade-slide give-success-popup';
 
 		return this;
 	}
@@ -113,23 +137,10 @@ const GiveModal = ( function( $ ) {
 	 *
 	 * @param string id         ID of the button.
 	 * @param string buttonType Type of the button: primary|secondary.
-	 * @param string textNode   Button Text.
-	 */
-	GiveModal.prototype.appendButton = function( id, buttonType, textNode ) {
-		this.filter.add( 'filter_notice_fields', val => { return `${ val } <button id="${ id }" class="give-button give-button-${ buttonType }">${ textNode }</button>` } );
-
-		return this;
-	}
-
-	/**
-	 * Replaces the 'OK' button with a custom button.
-	 *
-	 * @param string id         ID of the button.
-	 * @param string buttonType Type of the button: primary|secondary.
-	 * @param string textNode   Button Text.
+	 * @param string textNode   Button Text. 
 	 */
 	GiveModal.prototype.addButton = function( id, buttonType, textNode ) {
-		this.filter.add( 'filter_notice_fields', val => { return `<button id="${ id }" class="give-button give-button-${ buttonType }">${ textNode }</button>` } );
+		this.filter.add( 'filter_notice_fields', val => { return `${ val } <button id="${ id }" class="give-button give-button-${ buttonType }">${ textNode }</button>` } );
 
 		return this;
 	}
@@ -145,11 +156,42 @@ const GiveModal = ( function( $ ) {
 
 	/**
 	 * Add custom popup content.
+	 *
+	 * @param string popupData.title   Title of the popup.
+	 * @param string popupData.content Content of the popup.
 	 */
-	GiveModal.prototype.customContent = function( content ) {
-		this.config.items.src = `<div class="white-popup">${ content }</div>`;
+	GiveModal.prototype.customContent = function( popupData = {} ) {
+		let title   = !! popupData.title && `<h2>${ popupData.title }</h2>`,
+		    content = !! popupData.content && popupData.content;
+		this.config.items.src = `<div class="white-popup zoom-animation">${ !! title ? title : '' }${ !! content ? content : '' }</div>`;
 
 		return this;
+	}
+
+
+	/**
+	 * Wrapper function to add a filter.
+	 *
+	 * @param string   name   Name of the filter hook.
+	 * @param function filter Callback function to filter the input
+	 */
+	GiveModal.prototype.addFilter = function( name, filter ) {
+		this.filter.add( name, filter );
+
+		return this;
+	}
+
+
+	/**
+	 * Wrapper function to apply a filter.
+	 *
+	 * @param string name  Name of the filter hook.
+	 * @param mixed  value Value to filter.
+	 */
+	GiveModal.prototype.applyFilter = function( name, value ) {
+		let data = this.filter.apply( name, value );
+
+		return data;
 	}
 
 
@@ -175,15 +217,22 @@ const GiveModal = ( function( $ ) {
 				 */
 				open: function() {
 
-					// Reference to $( '.mfp-content' )
+
+					// Reference to MagnificPopup object.
 					reference.callback = this;
+
+					// Reference to $( '.white-popup' )
 					reference.callback.content = $( this.content );
 
-					null !== reference.root.config.popupType && reference.callback.content.prepend( `<div class="give-popup-notice-type">${ reference.root.config.popupType }</div>` );
+					let html = reference.callback.content.html();
+					reference.callback.content.html( '' );
+					reference.callback.content.append( `<div class="white-popup-child-container">${ html }</div>` );
+
+					let cancelTextNode = reference.root.applyFilter( 'cancel_text_node', 'Close' );
 
 					let fields = '<div class="give-appended-controls">';
 					fields += '<div class="give-popup-buttons-wrap">';
-					fields += reference.root.filter.apply( 'filter_notice_fields', '<button class="give-button give-button-secondary popup-close-button">Close</button>' );
+					fields += reference.root.applyFilter( 'filter_notice_fields', `<button id="give-popup-close-button" class="give-button give-button-secondary give-popup-close-button">${ cancelTextNode }</button>` );
 					fields += '</div>';
 					fields += '</div>';
 
@@ -197,6 +246,7 @@ const GiveModal = ( function( $ ) {
 				 */
 				close: function() {
 					reference.callback.content.find( '.give-appended-controls' ).remove();
+					reference.root.filter.filter_obj = {};
 				},
 			}
 		});
@@ -210,9 +260,11 @@ const GiveModal = ( function( $ ) {
 		 * Event handler to close the popup after
 		 * clicking the close button.
 		 */
-		this.doc.on( 'click', '.popup-close-button', function() {
+		this.doc.on( 'click', '#give-popup-close-button', function() {
 			reference.root.close();
 		})
+
+		return this;
 	}
 
 	return GiveModal;
