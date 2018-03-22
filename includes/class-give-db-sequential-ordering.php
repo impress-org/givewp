10695d0745d9433bc82219cf433ab465c1e135af
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2018-03-22 16:52:37 +0530

    fix(sequential-donation): set default starting number

--- includes/class-give-db-sequential-ordering.php
+++ includes/class-give-db-sequential-ordering.php
@@ -111,9 +111,12 @@ class Give_DB_Sequential_Ordering extends Give_DB {
 		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
 		dbDelta( $sql );
 
-		if( ! empty( $payment_ID ) ) {
-			$payment_ID = $payment_ID + 1;
-			$wpdb->query("ALTER TABLE {$this->table_name} AUTO_INCREMENT={$payment_ID};");
+		if ( ! empty( $payment_ID ) ) {
+			$auto_increment = $payment_ID + 1;
+			$wpdb->query( "ALTER TABLE {$this->table_name} AUTO_INCREMENT={$auto_increment};" );
+			give_update_option( 'sequential-ordering_number', $auto_increment );
+		} else {
+			give_update_option( 'sequential-ordering_number', 1 );
 		}
 
 		update_option( $this->table_name . '_db_version', $this->version );
