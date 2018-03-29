01194ebbb00b100e035872b60a8364eb0f31e900
Devin Walker <sdtucson@gmail.com>
2018-03-28 23:32:04 -0700

    refactor(donor-wall): remove test code

--- templates/shortcode-donor-grid.php
+++ templates/shortcode-donor-grid.php
@@ -15,9 +15,6 @@ $donor = new Give_Donor( $donor->id );
 $give_settings = $args[1]; // Give settings.
 $atts          = $args[2]; // Shortcode attributes.
 
-echo '<pre>';
-var_dump($donor->get_last_donation());
-echo '</pre>';
 ?>
 
 <div class="give-donor">
@@ -25,7 +22,7 @@ echo '</pre>';
 		<?php
 		// Maybe display the Avatar.
 		if ( true === $atts['show_avatar'] ) : ?>
-			<div class="give-donor__image"><?php echo get_avatar( $donor->email, $atts['avatar_size'] ); ?></div>
+			<?php give_get_donor_avatar($donor); ?>
 		<?php endif; ?>
 
 		<div class="give-donor__details">
@@ -33,12 +30,18 @@ echo '</pre>';
 			<span class="give-donor__total">
 				<?php
 				// If not filtered by form ID then display total donations
-				echo give_currency_filter( give_format_amount( $donor->purchase_value, array( 'sanitize' => false ) ) );
+				echo give_currency_filter( give_format_amount( $donor->purchase_value, array( 'sanitize' => false, 'decimal' => false ) ) );
 
 				// Else filtered by form ID, only display donations made for this form.
 				?>
 			</span>
-			<span class="give-donor__timestamp"><?php ?></span>
+			<span class="give-donor__timestamp">
+				<?php
+				// If not filtered by form ID then display the "Donor Since" text.
+
+				// If filtered by form ID then display the last donation date.
+				echo  $donor->get_last_donation_date( true ); ?>
+			</span>
 		</div>
 	</div>
 	<div class="give-donor__content">
