f987f6f4b4d50c95ae7ba9727acc2d8236f591ed
raftaar1191 <raftaar1191@gmail.com>
2018-04-02 20:32:31 +0530

    feat(admin-tool): change export donor csv name #2964

--- includes/admin/tools/export/class-batch-export-donors.php
+++ includes/admin/tools/export/class-batch-export-donors.php
@@ -64,6 +64,48 @@ class Give_Batch_Donors_Export extends Give_Batch_Export {
 	private $query_id = '';
 
 	/**
+	 * Give_Batch_Export constructor.
+	 *
+	 * @since 2.0.7
+	 *
+	 * @param int $_step
+	 */
+	public function __construct( $_step = 1 ) {
+
+		parent::__construct( $_step );
+
+		// Filter to change the filename.
+		add_filter( 'give_export_filename', array( $this, 'give_export_filename' ), 10, 2 );
+	}
+
+	/**
+	 * Function to change the filename
+	 *
+	 * @since 2.0.7
+	 *
+	 * @param $filename
+	 * @param $export_type
+	 *
+	 * @return string
+	 */
+	function give_export_filename( $filename, $export_type ) {
+
+
+		if ( $this->export_type !== $export_type ) {
+			return $filename;
+		}
+
+		if ( ! empty( (int) $_GET['forms'] ) ) {
+			$slug     = get_post_field( 'post_name', get_post( absint( $_GET['forms'] ) ) );
+			$filename = 'give-export-donors-' . $slug . '-' . date( 'm-d-Y' );
+		} else {
+			$filename = 'give-export-donors-all-forms-' . date( 'm-d-Y' );
+		}
+
+		return $filename;
+	}
+
+	/**
 	 * Set the properties specific to the donors export.
 	 *
 	 * @since 1.5
@@ -87,9 +129,7 @@ class Give_Batch_Donors_Export extends Give_Batch_Export {
 		}
 
 		$this->price_id = ! empty( $request['give_price_option'] ) && 'all' !== $request['give_price_option'] ? absint( $request['give_price_option'] ) : null;
-
 	}
-
 
 	/**
 	 * Cache donor ids.
