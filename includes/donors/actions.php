8bc23211cb30bb9cbad7f29edce32a56a90bd97e
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2018-04-10 18:18:18 +0530

    fix(admin-donation): update donor comment status when change donation status

--- includes/donors/actions.php
+++ includes/donors/actions.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Insert donation comment
+ * Insert donor comment to donation.
  *
  * @since 2.1.0
  *
@@ -9,7 +9,7 @@
  *
  * @return bool
  */
-function __give_insert_donor_comment( $donation_id, $donation_data ) {
+function __give_insert_donor_donation_comment( $donation_id, $donation_data ) {
 	if ( empty( $_POST['give_comment'] ) ) {
 		return false;
 	}
@@ -21,5 +21,26 @@ function __give_insert_donor_comment( $donation_id, $donation_data ) {
 	);
 }
 
-add_action( 'give_insert_payment', '__give_insert_donor_comment', 10, 2 );
+add_action( 'give_insert_payment', '__give_insert_donor_donation_comment', 10, 2 );
 
+
+/**
+ * Update donor comment status when donation status update
+ *
+ * @since 2.1.0
+ *
+ * @param $donation_id
+ * @param $status
+ */
+function __give_update_donor_donation_comment_status( $donation_id, $status ) {
+	$approve = absint( 'publish' === $status );
+
+	/* @var WP_Comment $note */
+	$donor_comment = give_get_donor_donation_comment( $donation_id, give_get_payment_donor_id( $donation_id ) );
+
+	if( $donor_comment instanceof WP_Comment ) {
+		wp_set_comment_status( $donor_comment->comment_ID, (string) $approve );
+	}
+}
+
+add_action( 'give_update_payment_status', '__give_update_donor_donation_comment_status', 10, 2 );
