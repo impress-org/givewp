0ddc17b2ae4ab2c0e044fbb12b65b4a57f6e00cf
raftaar1191 <raftaar1191@gmail.com>
2018-04-12 11:34:28 +0530

    fix(phpunit): add csv sample file and importer donation class file #2961

---
+++ tests/unit-tests/importer/tests-donations.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Meta
+ * @package Give\Tests\Importer
+ */
+
+
+class WC_Tests_Give_Import_Donations extends Give_Unit_Test_Case {
+	/**
+	 * Test CSV file path.
+	 *
+	 * @var string
+	 */
+	protected $csv_file = '';
+
+	/**
+	 * Set it up.
+	 */
+	function setUp() {
+		parent::setUp();
+	}
+
+	/**
+	 * Tear it down.
+	 */
+	public function tearDown() {
+		parent::tearDown();
+	}
+
+	public function test_shoudl_get_fail() {
+		
+	}
+}
