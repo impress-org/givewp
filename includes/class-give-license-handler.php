fa4ca3af03896bfe593211be179215ffb98b10a6
Devin Walker <sdtucson@gmail.com>
2018-05-02 12:15:02 -0700

    fix: php 7.2 warning for using sizeof on object rather than array

--- includes/class-give-license-handler.php
+++ includes/class-give-license-handler.php
@@ -161,7 +161,7 @@ if ( ! class_exists( 'Give_License' ) ) :
 
 			// Add Setting for Give Add-on activation status.
 			$is_addon_activated = get_option( 'give_is_addon_activated' );
-			if ( ! $is_addon_activated && is_object( $this ) && sizeof( $this ) > 0 ) {
+			if ( ! $is_addon_activated && is_object( $this ) ) {
 				update_option( 'give_is_addon_activated', true );
 				Give_Cache::set( 'give_cache_hide_license_notice_after_activation', true, DAY_IN_SECONDS );
 			}
