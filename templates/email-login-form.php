eb6b8394ccf532bc776ebf064f395c7e83a406ea
raftaar1191 <raftaar1191@gmail.com>
2018-05-08 12:20:05 +0530

    feat(shortcode): remove hooks

--- templates/email-login-form.php
+++ templates/email-login-form.php
@@ -91,16 +91,6 @@ Give()->notices->render_frontend_notices();
                 <input type="hidden" name="give_ip" class="give_ip" value=""/>
 			<?php endif; ?>
 
-
-			<?php
-			/**
-			 * Add custom fields inside Email access form.
-			 *
-			 * @since 2.1.3
-			 */
-			do_action( 'give_email_access_before_form_end' );
-			?>
-
             <input type="submit" class="give-submit" value="<?php _e( 'Verify Email', 'give' ); ?>"/>
         </form>
     </div>
