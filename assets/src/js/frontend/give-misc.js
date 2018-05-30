a8b27c032f7f2e577938dbf0b033d70763c67ec2
raftaar1191 <raftaar1191@gmail.com>
2018-05-30 15:26:37 +0530

    fix(donation): fix state fields issues

--- assets/src/js/frontend/give-misc.js
+++ assets/src/js/frontend/give-misc.js
@@ -218,16 +218,16 @@ window.give_open_form_modal = function ( $form_wrap, $form ) {
 	} );
 };
 
-
 /**
  * Floating Labels Custom Events
  */
+var give_float_labels;
 window.give_fl_trigger = function() {
 	if ( give_float_labels instanceof FloatLabels ) {
 		give_float_labels.rebuild();
 	}
 	else {
-		var give_float_labels = new FloatLabels( '.float-labels-enabled', {
+		give_float_labels = new FloatLabels( '.float-labels-enabled', {
 			exclude: '#give-amount, .give-select-level, [multiple]',
 			prioritize: 'placeholder',
 			prefix: 'give-fl-',
