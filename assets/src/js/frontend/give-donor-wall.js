fd67530abdfcdc297348cd81db7153d1627c2c75
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2018-06-07 17:47:52 +0530

    style(code): improve code formatting

--- assets/src/js/frontend/give-donor-wall.js
+++ assets/src/js/frontend/give-donor-wall.js
@@ -1,16 +1,24 @@
-window.addEventListener('load', function () {
-	let readMoreLinks = document.querySelectorAll('.give-donor__read-more');
+window.addEventListener(
+	'load', function () {
+		let readMoreLinks = document.querySelectorAll( '.give-donor__read-more' );
 
-	readMoreLinks.forEach(function (readMoreLink) {
-		readMoreLink.addEventListener('click', function () {
-			jQuery.magnificPopup.open({
-				items: {
-					src: this.parentNode.parentNode.parentNode.parentNode.getElementsByClassName('give-donor__comment')[0].innerHTML,
-					type: 'inline'
-				},
-				mainClass: 'give-modal',
-				closeOnBgClick: false
-			})
-		});
-	});
-}, false);
+		readMoreLinks.forEach(
+			function (readMoreLink) {
+				readMoreLink.addEventListener(
+					'click', function () {
+						jQuery.magnificPopup.open(
+							{
+								items: {
+									src: this.parentNode.parentNode.parentNode.parentNode.getElementsByClassName( 'give-donor__comment' )[0].innerHTML,
+									type: 'inline'
+								},
+								mainClass: 'give-modal',
+								closeOnBgClick: false
+							}
+						)
+					}
+				);
+			}
+		);
+	}, false
+);
