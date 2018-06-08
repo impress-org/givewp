29ff2b8a770520eaa7a2e66d3994a0ebc0ec2572
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2018-06-08 17:06:26 +0530

    fix(donor-wall): refactor js

--- assets/src/js/frontend/give-donor-wall.js
+++ assets/src/js/frontend/give-donor-wall.js
@@ -1,26 +1,58 @@
-window.addEventListener(
-	'load', function () {
-		let readMoreLinks = document.querySelectorAll( '.give-donor__read-more' );
+/**
+ * This class handles donor wall shortcode related features
+ *
+ * @since 2.2.0
+ *
+ */
+class GiveDonorWall{
+	constructor(){
+		window.addEventListener(
+			'load', function () {
+				let readMoreLinks = GiveDonorWall.readMoreLinks();
 
-		if( readMoreLinks.length ) {
-			readMoreLinks.forEach(
-				function (readMoreLink) {
-					readMoreLink.addEventListener(
-						'click', function () {
-							jQuery.magnificPopup.open(
-								{
-									items: {
-										src: this.parentNode.parentNode.parentNode.parentNode.getElementsByClassName( 'give-donor__comment' )[0].innerHTML,
-										type: 'inline',
-									},
-									mainClass: 'give-modal give-donor-wall-modal',
-									closeOnBgClick: false,
-								}
-							)
+				if( readMoreLinks.length ) {
+					readMoreLinks.forEach(
+						function (readMoreLink) {
+							GiveDonorWall.readMoreBtnEvent( readMoreLink );
 						}
 					);
 				}
-			);
-		}
-	}, false
-);
+			}, false
+		);
+	}
+
+	/**
+	 * Get all read more links
+	 *
+	 * @since  2.2.0
+	 */
+	static readMoreLinks(){
+		return document.querySelectorAll( '.give-donor__read-more' );
+	}
+
+	/**
+	 * Add click event to read more link
+	 *
+	 * @since  2.2.0
+	 *
+	 * @param {object} el
+	 */
+	static readMoreBtnEvent( el ){
+		el.addEventListener(
+			'click', function () {
+				jQuery.magnificPopup.open(
+					{
+						items: {
+							src: this.parentNode.parentNode.parentNode.parentNode.getElementsByClassName( 'give-donor__comment' )[0].innerHTML,
+							type: 'inline',
+						},
+						mainClass: 'give-modal give-donor-wall-modal',
+						closeOnBgClick: false,
+					}
+				)
+			}
+		);
+	}
+}
+
+let giveDonorWall = new GiveDonorWall();
