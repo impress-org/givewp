579f8df23a033f987be21c99f333406ae17c344b
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2018-06-19 10:33:48 +0530

    style(admin-form): improve code formatting

--- includes/admin/forms/class-give-clone-forms.php
+++ includes/admin/forms/class-give-clone-forms.php
@@ -22,10 +22,10 @@ if ( ! class_exists( 'Give_Clone_Forms' ) ) {
 		public function __construct() {
 
 			// Add the 'Clone Form' to Row Actions.
-			add_filter( 'post_row_actions', array( $this, 'add_clone_form_row_action' ), 10, 2 );
+			add_filter( 'post_row_actions', array( $this, 'row_action' ), 10, 2 );
 
 			// Run admin_action hook.
-			add_action( 'admin_action_give_clone_form', array( $this, 'clone_the_form' ) );
+			add_action( 'admin_action_give_clone_form', array( $this, 'handler' ) );
 		}
 
 
@@ -39,10 +39,10 @@ if ( ! class_exists( 'Give_Clone_Forms' ) ) {
 		 *
 		 * @return array
 		 */
-		public function add_clone_form_row_action( $actions, $post ) {
+		public function row_action( $actions, $post ) {
 
 			// @codingStandardsIgnoreStart
-			
+
 			if ( isset( $_GET['post_type'] ) && 'give_forms' === give_clean( $_GET['post_type'] ) ) { // WPCS: input var ok.
 				if ( current_user_can( 'edit_posts' ) ) {
 					$actions['clone_form'] = sprintf(
@@ -72,104 +72,143 @@ if ( ! class_exists( 'Give_Clone_Forms' ) ) {
 		 *
 		 * @return void
 		 */
-		public function clone_the_form() {
-
-			// Global $wpdb object.
-			global $wpdb;
-
+		public function handler() {
+			// Validate action.
 			// @codingStandardsIgnoreStart
-
-			if ( ! isset( $_REQUEST['form_id'] ) ) {
+			if (
+				! isset( $_REQUEST['form_id'] )
+				|| ! isset( $_REQUEST['action'] )
+				|| ( 'give_clone_form' !== $_REQUEST['action'] )
+			) {
 				wp_die( esc_html__( 'Form ID not found in the query string', 'give' ) );
-			}
 
-			$nonce        = give_clean( $_REQUEST['_wpnonce'] );
-			$form_id      = give_clean( $_REQUEST['form_id'] );
-			$post         = get_post( $form_id );
-			$current_user = wp_get_current_user();
-			$post_author  = $current_user->ID;
+			} elseif ( ! wp_verify_nonce( give_clean( $_REQUEST['_wpnonce'] ), 'give-clone-form' ) ) {
 
-			// @codingStandardsIgnoreEnd
-
-			// Verify nonce.
-			if ( ! wp_verify_nonce( $nonce, 'give-clone-form' ) ) {
 				wp_die( esc_html__( 'Nonce verification failed', 'give' ) );
 			}
+			// @codingStandardsIgnoreEnd
 
-			if ( isset( $post ) && null !== $post ) {
+			$form_id      = give_clean( $_REQUEST['form_id'] ); // @codingStandardsIgnoreLine
+			$post_data         = get_post( $form_id );
+			$current_user = wp_get_current_user();
+			$error_notice = sprintf(
+				/* translators: %s: Form ID */
+				esc_html__( 'Cloning failed. Form with ID %s does not exist.', 'give' ),
+				absint( $form_id )
+			);
+
+			if ( isset( $post_data ) && null !== $post_data ) {
 
 				$args = array(
-					'comment_status' => $post->comment_status,
-					'ping_status'    => $post->ping_status,
-					'post_author'    => $post_author,
-					'post_content'   => $post->post_content,
-					'post_excerpt'   => $post->post_excerpt,
-					'post_name'      => $post->post_name,
-					'post_parent'    => $post->post_parent,
-					'post_password'  => $post->post_password,
+					'comment_status' => $post_data->comment_status,
+					'ping_status'    => $post_data->ping_status,
+					'post_author'    => $current_user->ID,
+					'post_content'   => $post_data->post_content,
+					'post_excerpt'   => $post_data->post_excerpt,
+					'post_name'      => $post_data->post_name,
+					'post_parent'    => $post_data->post_parent,
+					'post_password'  => $post_data->post_password,
 					'post_status'    => 'draft',
-					'post_title'     => $post->post_title,
-					'post_type'      => $post->post_type,
-					'to_ping'        => $post->to_ping,
-					'menu_order'     => $post->menu_order,
+					'post_title'     => $post_data->post_title,
+					'post_type'      => $post_data->post_type,
+					'to_ping'        => $post_data->to_ping,
+					'menu_order'     => $post_data->menu_order,
 				);
 
 				// Get the ID of the cloned post.
 				$clone_form_id = wp_insert_post( $args );
 
-				// Get the taxonomies of the post type `give_forms`.
-				$taxonomies = get_object_taxonomies( $post->post_type );
+				$this->clone_taxonomies( $clone_form_id, $post_data );
+				$this->clone_meta_data( $clone_form_id, $post_data );
 
-				foreach ( $taxonomies as $taxonomy ) {
-
-					$post_terms = wp_get_object_terms(
-						$form_id,
-						$taxonomy,
-						array(
-							'fields' => 'slugs',
+				if ( ! is_wp_error( $clone_form_id ) ) {
+					// Redirect to the cloned form editor page.
+					wp_safe_redirect(
+						add_query_arg(
+							array(
+								'action' => 'edit',
+								'post'   => $clone_form_id,
+							),
+							admin_url( 'post.php' )
 						)
 					);
-
-					wp_set_object_terms( $clone_form_id, $post_terms, $taxonomy, false );
+				} else {
+					wp_die( $error_notice ); // @codingStandardsIgnoreLine
 				}
-
-				// Clone the metadata of the form.
-				$post_meta_query = $wpdb->prepare( "SELECT meta_key, meta_value FROM {$wpdb->formmeta} WHERE form_id=%s", $form_id );
-
-				$post_meta_data = $wpdb->get_results( $post_meta_query ); // WPCS: db call ok. WPCS: cache ok. WPCS: unprepared SQL OK.
-
-				if ( ! empty( $post_meta_data ) ) {
-
-					$clone_query = "INSERT INTO {$wpdb->formmeta} (form_id, meta_key, meta_value) ";
-
-					foreach ( $post_meta_data as $meta_data ) {
-						$meta_key             = $meta_data->meta_key;
-						$meta_value           = $meta_data->meta_value;
-						$clone_query_select[] = $wpdb->prepare( 'SELECT %s, %s, %s', $clone_form_id, $meta_key, $meta_value );
-					}
-
-					$clone_query .= implode( ' UNION ALL ', $clone_query_select );
-
-					$wpdb->query( $clone_query ); // WPCS: db call ok. WPCS: cache ok. WPCS: unprepared SQL OK.
-				}
-
-				// Redirect to the cloned form editor page.
-				wp_safe_redirect(
-					add_query_arg(
-						array(
-							'action' => 'edit',
-							'post'   => $clone_form_id,
-						),
-						admin_url( 'post.php' )
-					)
-				);
 
 				exit;
 
 			} else {
 
-				/* translators: %s: Form ID */
-				wp_die( sprintf( esc_html__( 'Cloning failed. Form with ID %s does not exist.', 'give' ), esc_html( $form_id ) ) );
+				wp_die( $error_notice ); // @codingStandardsIgnoreLine
+			}
+		}
+
+
+		/**
+		 * Clone taxonomies
+		 *
+		 * @since  2.2.0
+		 * @access private
+		 *
+		 * @param int     $new_form_id New form ID.
+		 * @param WP_Post $old_form    Old form object.
+		 */
+		private function clone_taxonomies( $new_form_id, $old_form ) {
+			// Get the taxonomies of the post type `give_forms`.
+			$taxonomies = get_object_taxonomies( $old_form->post_type );
+
+			foreach ( $taxonomies as $taxonomy ) {
+
+				$post_terms = wp_get_object_terms(
+					$old_form->ID,
+					$taxonomy,
+					array(
+						'fields' => 'slugs',
+					)
+				);
+
+				wp_set_object_terms(
+					$new_form_id,
+					$post_terms,
+					$taxonomy,
+					false
+				);
+			}
+		}
+
+
+		/**
+		 * Clone meta data
+		 *
+		 * @since  2.2.0
+		 * @access private
+		 *
+		 * @param int     $new_form_id New Form ID.
+		 * @param WP_Post $old_form    Old form object.
+		 */
+		private function clone_meta_data( $new_form_id, $old_form ) {
+			global $wpdb;
+
+			// Clone the metadata of the form.
+			$post_meta_query = $wpdb->prepare( "SELECT meta_key, meta_value FROM {$wpdb->formmeta} WHERE form_id=%s", $old_form->ID );
+
+			$post_meta_data = $wpdb->get_results( $post_meta_query ); // WPCS: db call ok. WPCS: cache ok. WPCS: unprepared SQL OK.
+
+			if ( ! empty( $post_meta_data ) ) {
+
+				$clone_query        = "INSERT INTO {$wpdb->formmeta} (form_id, meta_key, meta_value) ";
+				$clone_query_select = array();
+
+				foreach ( $post_meta_data as $meta_data ) {
+					$meta_key             = $meta_data->meta_key;
+					$meta_value           = $meta_data->meta_value;
+					$clone_query_select[] = $wpdb->prepare( 'SELECT %s, %s, %s', $new_form_id, $meta_key, $meta_value );
+				}
+
+				$clone_query .= implode( ' UNION ALL ', $clone_query_select );
+
+				$wpdb->query( $clone_query ); // WPCS: db call ok. WPCS: cache ok. WPCS: unprepared SQL OK.
 			}
 		}
 	}
