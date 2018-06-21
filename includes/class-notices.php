56d11310dfb3ed1e664306f813e9690fa8597062
Siddharth <siddharth.thevaril@gmail.com>
2018-06-21 16:15:24 +0530

    fix(donation): Fix error notices on form display modal and button

--- includes/class-notices.php
+++ includes/class-notices.php
@@ -57,7 +57,7 @@ class Give_Notices {
 		add_action( 'give_dismiss_notices', array( $this, 'dismiss_notices' ) );
 
 		add_action( 'give_frontend_notices', array( $this, 'render_frontend_notices' ), 999 );
-		add_action( 'give_pre_form', array( $this, 'render_frontend_notices' ), 11 );
+		add_action( 'init', array( $this, 'load_actions_for_notices' ) );
 		add_action( 'give_ajax_donation_errors', array( $this, 'render_frontend_notices' ) );
 
 		/**
@@ -281,6 +281,28 @@ class Give_Notices {
 			self::print_frontend_errors( $errors );
 
 			give_clear_errors();
+		}
+	}
+
+	/**
+	 * Renders notices for different actions depending on
+	 * the type of form display option.
+	 *
+	 * @since 2.2
+	 *
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public function load_actions_for_notices() {
+		$request_form_id = isset( $_REQUEST['form-id'] ) ? absint( $_REQUEST['form-id'] ) : 0;
+
+		$display_option = give_get_meta( $request_form_id, '_give_payment_display', true );
+
+		if ( 'modal' === $display_option ) {
+			add_action( 'give_payment_mode_top', array( $this, 'render_frontend_notices' ) );
+		} else {
+			add_action( 'give_pre_form', array( $this, 'render_frontend_notices' ), 11 );
 		}
 	}
 
