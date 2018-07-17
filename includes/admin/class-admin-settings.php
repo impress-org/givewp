7a0a6569e40b8ea59ec9640366d1840b99253a24
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2018-07-17 16:10:38 +0530

    style(code): improve code formatting

--- includes/admin/class-admin-settings.php
+++ includes/admin/class-admin-settings.php
@@ -487,24 +487,25 @@ if ( ! class_exists( 'Give_Admin_Settings' ) ) :
 						$option_value = self::get_option( $option_name, $value['id'], $value['default'] );
 
 						?>
-					<tr valign="top" <?php echo ! empty( $value['wrapper_class'] ) ? 'class="' . $value['wrapper_class'] . '"' : '' ?>>
-						<th scope="row" class="titledesc">
-							<label
-									for="<?php echo esc_attr( $value['id'] ); ?>"><?php echo self::get_field_title( $value ); ?></label>
-						</th>
-						<td class="give-forminp give-forminp-<?php echo sanitize_title( $value['type'] ) ?>">
-									<textarea
-											name="<?php echo esc_attr( $value['id'] ); ?>"
-											id="<?php echo esc_attr( $value['id'] ); ?>"
-											style="<?php echo esc_attr( $value['css'] ); ?>"
-											class="<?php echo esc_attr( $value['class'] ); ?>"
-											rows="10"
-											cols="60"
-										<?php echo implode( ' ', $custom_attributes ); ?>
-									><?php echo esc_textarea( $option_value ); ?></textarea>
-							<?php echo $description; ?>
-						</td>
-						</tr><?php
+						<tr valign="top" <?php echo ! empty( $value['wrapper_class'] ) ? 'class="' . $value['wrapper_class'] . '"' : '' ?>>
+							<th scope="row" class="titledesc">
+								<label
+										for="<?php echo esc_attr( $value['id'] ); ?>"><?php echo self::get_field_title( $value ); ?></label>
+							</th>
+							<td class="give-forminp give-forminp-<?php echo sanitize_title( $value['type'] ) ?>">
+										<textarea
+												name="<?php echo esc_attr( $value['id'] ); ?>"
+												id="<?php echo esc_attr( $value['id'] ); ?>"
+												style="<?php echo esc_attr( $value['css'] ); ?>"
+												class="<?php echo esc_attr( $value['class'] ); ?>"
+												rows="10"
+												cols="60"
+											<?php echo implode( ' ', $custom_attributes ); ?>
+										><?php echo esc_textarea( $option_value ); ?></textarea>
+								<?php echo $description; ?>
+							</td>
+						</tr>
+						<?php
 						break;
 
 					// Select boxes.
