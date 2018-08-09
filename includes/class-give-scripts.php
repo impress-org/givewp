260f49598a1cf55c75a8307ca282f5c0e11eac5e
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2018-08-09 15:26:11 +0530

    fix(scripts): do not load public script in backend

--- includes/class-give-scripts.php
+++ includes/class-give-scripts.php
@@ -59,6 +59,7 @@ class Give_Scripts {
 			add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_styles' ) );
 			add_action( 'enqueue_block_editor_assets', array( $this, 'gutenberg_admin_scripts' ) );
 			add_action( 'admin_head', array( $this, 'global_admin_head' ) );
+
 		} else {
 			add_action( 'wp_enqueue_scripts', array( $this, 'public_enqueue_styles' ) );
 			add_action( 'wp_enqueue_scripts', array( $this, 'public_enqueue_scripts' ) );
