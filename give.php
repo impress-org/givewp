dc14d10b25a016cab49e3933ac3d8221ec4a4f11
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2018-08-28 22:36:58 +0530

    fix(compat): remove github plugin updater support
    fixes #3635

--- give.php
+++ give.php
@@ -8,7 +8,6 @@
  * Version: 2.2.3
  * Text Domain: give
  * Domain Path: /languages
- * GitHub Plugin URI: https://github.com/WordImpress/Give
  *
  * Give is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
