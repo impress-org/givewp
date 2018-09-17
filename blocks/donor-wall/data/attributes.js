2e2186e0183c0d636388e736c6e15f3474f80ef6
Siddharth <siddharth.thevaril@gmail.com>
2018-09-17 12:45:15 +0530

    feat: Add block attributes

---
+++ blocks/donor-wall/data/attributes.js
@@ -0,0 +1,32 @@
+/**
+ * Block Attributes
+*/
+
+const blockAttributes = {
+	columns: {
+		type: 'string',
+		default: '4',
+	},
+	showAvatar: {
+		type: 'boolean',
+		default: true,
+	},
+	showName: {
+		type: 'boolean',
+		default: true,
+	},
+	showTotal: {
+		type: 'boolean',
+		default: true,
+	},
+	showDate: {
+		type: 'boolean',
+		default: true,
+	},
+	showComments: {
+		type: 'boolean',
+		default: true,
+	},
+};
+
+export default blockAttributes;
