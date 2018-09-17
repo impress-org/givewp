56400d954a71570ed7449a65390161799302af03
Siddharth <siddharth.thevaril@gmail.com>
2018-09-17 13:13:36 +0530

    feat: Add options to donor wall options

---
+++ blocks/donor-wall/data/options.js
@@ -0,0 +1,19 @@
+/**
+ * WordPress dependencies
+ */
+const { __ } = wp.i18n;
+
+/**
+ * Options data for various form selects
+ */
+const giveDonorWallOptions = {};
+
+// Form Display Styles
+giveDonorWallOptions.columns = [
+	{ value: '1', label: '1' },
+	{ value: '2', label: '2' },
+	{ value: '3', label: '3' },
+	{ value: '4', label: '4' },
+];
+
+export default giveDonorWallOptions;
