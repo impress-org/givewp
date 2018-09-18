6d48ebcbcf7dc0915cba1a2597c4f5a7deb39c99
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2018-09-18 11:06:38 +0530

    refactor: remove unnecessary trenary condition

--- includes/ajax-functions.php
+++ includes/ajax-functions.php
@@ -264,9 +264,6 @@ function give_ajax_get_states_field() {
 		}
 	}
 
-	// Get the country list that does not require city.
-	$city_require = array_key_exists( $country, give_city_not_required_country_list() ) ? false : true;
-
 	$response = array(
 		'success'        => true,
 		'states_found'   => $states_found,
@@ -275,7 +272,7 @@ function give_ajax_get_states_field() {
 		'states_require' => $states_require,
 		'data'           => $data,
 		'default_state'  => $default_state,
-		'city_require'   => $city_require,
+		'city_require'   => ! array_key_exists( $country, give_city_not_required_country_list() ),
 	);
 	wp_send_json( $response );
 }
