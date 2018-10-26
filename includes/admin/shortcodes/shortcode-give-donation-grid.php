2949dc2c8c709613455d3f961131e4a7a23cdf98
kakshak <kakshak.kalariya@multidots.com>
2018-10-26 15:09:22 +0530

    style: code formatting

--- includes/admin/shortcodes/shortcode-give-donation-grid.php
+++ includes/admin/shortcodes/shortcode-give-donation-grid.php
@@ -46,14 +46,14 @@ class Give_Shortcode_Donation_Grid extends Give_Shortcode_Generator {
 				'name'        => 'ids',
 				'label'       => esc_attr__( 'Form IDs:', 'give' ),
 				'tooltip'     => esc_attr__( 'Enter a comma-separated list of form IDs. If empty, all published forms are displayed.', 'give' ),
-				'placeholder' => esc_html__( 'All Forms', 'give' )
+				'placeholder' => esc_html__( 'All Forms', 'give' ),
 			),
 			array(
 				'type'        => 'textbox',
 				'name'        => 'exclude',
 				'label'       => esc_attr__( 'Excluded Form IDs:', 'give' ),
 				'tooltip'     => esc_attr__( 'Enter a comma-separated list of form IDs to exclude those from the grid.', 'give' ),
-				'placeholder' => esc_html__( 'Excluded Forms', 'give' )
+				'placeholder' => esc_html__( 'Excluded Forms', 'give' ),
 			),
 			array(
 				'type'        => 'listbox',
@@ -90,45 +90,45 @@ class Give_Shortcode_Donation_Grid extends Give_Shortcode_Generator {
 					'3' => esc_html__( '3', 'give' ),
 					'4' => esc_html__( '4', 'give' ),
 				),
-				'placeholder' => esc_html__( 'Best Fit', 'give' )
+				'placeholder' => esc_html__( 'Best Fit', 'give' ),
 			),
 			array(
-				'type'    => 'listbox',
-				'name'    => 'show_goal',
-				'label'   => esc_attr__( 'Show Goal:', 'give' ),
-				'tooltip' => __( 'Do you want to display the goal\'s progress bar?', 'give' ),
-				'options' => array(
+				'type'        => 'listbox',
+				'name'        => 'show_goal',
+				'label'       => esc_attr__( 'Show Goal:', 'give' ),
+				'tooltip'     => __( 'Do you want to display the goal\'s progress bar?', 'give' ),
+				'options'     => array(
 					'false' => esc_html__( 'Hide', 'give' ),
 				),
-				'placeholder' => esc_html__( 'Show', 'give' )
+				'placeholder' => esc_html__( 'Show', 'give' ),
 			),
 			array(
-				'type'    => 'listbox',
-				'name'    => 'show_excerpt',
-				'label'   => esc_attr__( 'Show Excerpt:', 'give' ),
-				'tooltip' => esc_attr__( 'Do you want to display the excerpt?', 'give' ),
-				'options' => array(
+				'type'        => 'listbox',
+				'name'        => 'show_excerpt',
+				'label'       => esc_attr__( 'Show Excerpt:', 'give' ),
+				'tooltip'     => esc_attr__( 'Do you want to display the excerpt?', 'give' ),
+				'options'     => array(
 					'false' => esc_html__( 'Hide', 'give' ),
 				),
-				'placeholder' => esc_html__( 'Show', 'give' )
+				'placeholder' => esc_html__( 'Show', 'give' ),
 			),
 			array(
-				'type'    => 'listbox',
-				'name'    => 'show_featured_image',
-				'label'   => esc_attr__( 'Show Featured Image:', 'give' ),
-				'tooltip' => esc_attr__( 'Do you want to display the featured image?', 'give' ),
-				'options' => array(
+				'type'        => 'listbox',
+				'name'        => 'show_featured_image',
+				'label'       => esc_attr__( 'Show Featured Image:', 'give' ),
+				'tooltip'     => esc_attr__( 'Do you want to display the featured image?', 'give' ),
+				'options'     => array(
 					'false' => esc_html__( 'Hide', 'give' ),
 				),
-				'placeholder' => esc_html__( 'Show', 'give' )
+				'placeholder' => esc_html__( 'Show', 'give' ),
 			),
 			array(
-				'type'    => 'listbox',
-				'name'    => 'display_style',
-				'label'   => esc_attr__( 'Display Style:', 'give' ),
-				'tooltip' => esc_attr__( 'Show form as modal window or redirect to a new page?', 'give' ),
-				'options' => array(
-					'redirect'     => esc_html__( 'Redirect', 'give' ),
+				'type'        => 'listbox',
+				'name'        => 'display_style',
+				'label'       => esc_attr__( 'Display Style:', 'give' ),
+				'tooltip'     => esc_attr__( 'Show form as modal window or redirect to a new page?', 'give' ),
+				'options'     => array(
+					'redirect' => esc_html__( 'Redirect', 'give' ),
 				),
 				'placeholder' => esc_html__( 'Modal', 'give' ),
 			),
