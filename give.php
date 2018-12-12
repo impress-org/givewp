b36308a03eacb0df7239290c41b10291930084f8
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2018-12-12 18:51:41 +0530

    fix: do not load textdomain multiple times
    we are are loading it in init function

--- give.php
+++ give.php
@@ -331,9 +331,6 @@ if ( ! class_exists( 'Give' ) ) :
 		private function init_hooks() {
 			register_activation_hook( GIVE_PLUGIN_FILE, 'give_install' );
 			add_action( 'plugins_loaded', array( $this, 'init' ), 0 );
-
-			// Set up localization on init Hook.
-			add_action( 'init', array( $this, 'load_textdomain' ), 0 );
 		}
 
 
