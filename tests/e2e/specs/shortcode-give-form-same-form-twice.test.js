efa0bb99cdd894841675b265451b52ca9c773e28
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2018-12-13 19:30:24 +0530

    chore: update wait time to 2 sec

--- tests/e2e/specs/shortcode-give-form-same-form-twice.test.js
+++ tests/e2e/specs/shortcode-give-form-same-form-twice.test.js
@@ -433,7 +433,7 @@ describe( 'Display Option: All fields', () => {
 	}, 100000 )
 
 	it ( 'EXISTENCE: verify donation confirmation of the second form', async () => {
-		await page.waitFor( 10000 ); // Wait for receipt to load by ajax.
+		await page.waitFor( 2000 ); // Wait for receipt to load by ajax.
 		await expect( page ).toMatch( 'Payment Complete: Thank you for your donation.' )
 		await expect( page ).toMatch( 'Mr. Creed Bratton' )
 		await expect( page ).toMatch( '$30.00' )
