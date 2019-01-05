06f1f200160427cc8a6f0ff42b6b5a3d3f57e5de
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2019-01-05 22:27:39 +0530

    chore: upgrade webpack to version 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,38 @@
-const path = require( 'path' );
-const webpack = require( 'webpack' );
-const CopyWebpackPlugin = require( 'copy-webpack-plugin' );
-const ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
-const BrowserSyncPlugin = require( 'browser-sync-webpack-plugin' );
-const ImageminPlugin = require( 'imagemin-webpack-plugin' ).default;
-const CleanWebpackPlugin = require( 'clean-webpack-plugin' );
-const WebpackRTLPlugin = require( 'webpack-rtl-plugin' );
-const wpPot = require( 'wp-pot' );
+/* globals process, __dirname, module  */
 
-const inProduction = ( 'production' === process.env.NODE_ENV );
+/**
+ * External dependencies
+ */
+const path                 = require('path');
+const webpack              = require('webpack');
+const CopyWebpackPlugin    = require('copy-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const BrowserSyncPlugin    = require('browser-sync-webpack-plugin');
+const ImageminPlugin       = require('imagemin-webpack-plugin').default;
+const CleanWebpackPlugin   = require('clean-webpack-plugin');
+const WebpackRTLPlugin     = require('webpack-rtl-plugin');
+const wpPot                = require('wp-pot');
+
+const inProduction = ('production' === process.env.NODE_ENV);
+const mode         = inProduction ? 'production' : 'development';
 
 const config = {
+	mode,
+
+	entry: {
+		give: ['./assets/src/css/frontend/give-frontend.scss', './assets/src/js/frontend/give.js'],
+		admin: ['./assets/src/css/admin/give-admin.scss', './assets/src/js/admin/admin.js'],
+		'babel-polyfill': '@babel/polyfill',
+		gutenberg: './blocks/load.js',
+		'admin-shortcode-button': ['./assets/src/css/admin/shortcodes.scss'],
+		'admin-shortcodes': './includes/admin/shortcodes/admin-shortcodes.js',
+		'plugin-deactivation-survey': ['./assets/src/css/admin/plugin-deactivation-survey.scss', './assets/src/js/admin/plugin-deactivation-survey.js'],
+	},
+	output: {
+		path: path.join(__dirname, './assets/dist/'),
+		filename: 'js/[name].js',
+	},
+
 	// Ensure modules like magnific know jQuery is external (loaded via WP).
 	externals: {
 		$: 'jQuery',
@@ -25,28 +47,40 @@ const config = {
 			{
 				test: /\.js$/,
 				exclude: /node_modules/,
-				loaders: [
-					'babel-loader',
-				],
+				loader: 'babel-loader',
 			},
 
 			// Expose accounting.js for plugin usage.
 			{
-				test: require.resolve( 'accounting' ),
-				loader: 'expose-loader?accounting',
+				test: require.resolve('accounting'),
+				use: [
+					{
+						loader: 'expose-loader',
+						options: 'accounting',
+					}
+				],
 			},
 
 			// Create RTL styles.
 			{
 				test: /\.css$/,
-				loader: ExtractTextPlugin.extract( 'style-loader' ),
+				use: [
+					MiniCSSExtractPlugin.loader,
+					{
+						loader: 'style-loader',
+						options: {
+							sourceMap: true,
+						},
+					}
+				],
 			},
 
 			// SASS to CSS.
 			{
 				test: /\.scss$/,
-				use: ExtractTextPlugin.extract( {
-					use: [ {
+				use: [
+					MiniCSSExtractPlugin.loader,
+					{
 						loader: 'css-loader',
 						options: {
 							sourceMap: true,
@@ -60,10 +94,9 @@ const config = {
 						loader: 'sass-loader',
 						options: {
 							sourceMap: true,
-							outputStyle: ( inProduction ? 'compressed' : 'nested' ),
+							outputStyle: (inProduction ? 'compressed' : 'nested'),
 						},
-					} ],
-				} ),
+					}],
 			},
 
 			// Font files.
@@ -100,78 +133,54 @@ const config = {
 	plugins: [
 
 		// Removes the "dist" folder before building.
-		new CleanWebpackPlugin( [ 'assets/dist' ] ),
+		new CleanWebpackPlugin(['assets/dist']),
 
-		new ExtractTextPlugin( 'css/[name].css' ),
+		new MiniCSSExtractPlugin({
+			filename: "css/[name].css"
+		}),
 
 		// Create RTL css.
-		new WebpackRTLPlugin(),
+		new WebpackRTLPlugin({
+			suffix: '-rtl',
+			minify: 'production' === mode ? {safe: true} : false,
+		}),
 
 		// Copy images and SVGs
-		new CopyWebpackPlugin( [ { from: 'assets/src/images', to: 'images' } ] ),
+		new CopyWebpackPlugin([{from: 'assets/src/images', to: 'images'}]),
 
 		// Minify images.
 		// Must go after CopyWebpackPlugin above: https://github.com/Klathmon/imagemin-webpack-plugin#example-usage
-		new ImageminPlugin( { test: /\.(jpe?g|png|gif|svg)$/i } ),
-
-		// Setup browser sync. Note: don't use ".local" TLD as it will be very slow. We recommending using ".test".
-		new BrowserSyncPlugin( {
-			files: [
-				'**/*.php',
-			],
-			host: 'localhost',
-			port: 3000,
-			proxy: 'give.test',
-		} ),
+		new ImageminPlugin({test: /\.(jpe?g|png|gif|svg)$/i}),
 	],
 };
 
-module.exports = [
-	Object.assign( {
-		entry: {
-			give: [ './assets/src/css/frontend/give-frontend.scss', './assets/src/js/frontend/give.js' ],
-			admin: [ './assets/src/css/admin/give-admin.scss', './assets/src/js/admin/admin.js' ],
-		},
-		output: {
-			path: path.join( __dirname, './assets/dist/' ),
-			filename: 'js/[name].js',
-			library: 'Give',
-			libraryTarget: 'umd',
-		},
-	}, config ),
-	Object.assign( {
-		entry: {
-			'babel-polyfill': 'babel-polyfill',
-			gutenberg: './blocks/load.js',
-			'admin-shortcode-button': [ './assets/src/css/admin/shortcodes.scss' ],
-			'admin-shortcodes': './includes/admin/shortcodes/admin-shortcodes.js',
-			'plugin-deactivation-survey': [ './assets/src/css/admin/plugin-deactivation-survey.scss', './assets/src/js/admin/plugin-deactivation-survey.js' ],
-		},
-
-		// Tell webpack where to output.
-		output: {
-			path: path.resolve( __dirname, './assets/dist/' ),
-			filename: 'js/[name].js',
-		},
-	}, config ),
-];
-
-// inProd?
-if ( inProduction ) {
+if (inProduction) {
 	// POT file.
-	wpPot( {
+	wpPot({
 		package: 'Give',
 		domain: 'give',
 		destFile: 'languages/give.pot',
 		relativeTo: './',
 		bugReport: 'https://github.com/impress-org/give/issues/new',
 		team: 'GiveWP <info@givewp.com>',
-	} );
+	});
 
-	// Uglify JS.
-	config.plugins.push( new webpack.optimize.UglifyJsPlugin( { sourceMap: true } ) );
+	// Uglify JS
+	config.plugins.push(new webpack.optimize.UglifyJsPlugin({sourceMap: true}));
 
-	// Minify CSS.
-	config.plugins.push( new webpack.LoaderOptionsPlugin( { minimize: true } ) );
-
+	// Minify JS
+	config.plugins.push(new webpack.LoaderOptionsPlugin({minimize: true}));
+} else {
+	// Setup browser sync. Note: don't use ".local" TLD as it will be very slow. We recommending using ".test".
+	config.plugins.push(new BrowserSyncPlugin({
+		files: [
+			'**/*.php',
+		],
+		host: 'localhost',
+		port: 3000,
+		proxy: 'give.test',
+	}));
 }
+
+module.exports = config;
+
