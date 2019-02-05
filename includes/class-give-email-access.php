244827cb5a4ffbb05a96b62c8087af273724865e
mehul0810 <mehul.gohil0810@gmail.com>
2019-02-05 16:59:49 +0530

    fix: add new fn to fetch token value

--- includes/class-give-email-access.php
+++ includes/class-give-email-access.php
@@ -208,7 +208,27 @@ class Give_Email_Access {
 	public function send_email( $donor_id, $email ) {
 		return apply_filters( 'give_email-access_email_notification', $donor_id, $email );
 	}
-
+	
+	/**
+	 * This function is used to fetch the token value from query string or cookies based on availability.
+	 *
+	 * @since  2.4.1
+	 * @access public
+	 *
+	 * @return string
+	 */
+	public function get_token() {
+		
+		$token = isset( $_GET['give_nl'] ) ? give_clean( $_GET['give_nl'] ) : '';
+		
+		// Check for cookie.
+		if ( empty( $token ) ) {
+			$token = isset( $_COOKIE['give_nl'] ) ? give_clean( $_COOKIE['give_nl'] ) : '';
+		}
+		
+		return $token;
+	}
+	
 	/**
 	 * Has the user authenticated?
 	 *
@@ -219,12 +239,7 @@ class Give_Email_Access {
 	 */
 	public function check_for_token() {
 
-		$token = isset( $_GET['give_nl'] ) ? give_clean( $_GET['give_nl'] ) : '';
-
-		// Check for cookie.
-		if ( empty( $token ) ) {
-			$token = isset( $_COOKIE['give_nl'] ) ? give_clean( $_COOKIE['give_nl'] ) : '';
-		}
+		$token = $this->get_token();
 
 		// Must have a token.
 		if ( ! empty( $token ) ) {
