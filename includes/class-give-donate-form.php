69b74b26cafeba4c6bcb043c794c248cd719c878
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2019-02-08 10:10:41 +0530

    fix: use WP_Post::get_instance to prevent frequent query

--- includes/class-give-donate-form.php
+++ includes/class-give-donate-form.php
@@ -349,14 +349,7 @@ class Give_Donate_Form {
 	 * @param  array    $_args Arguments passed.
 	 */
 	public function __construct( $_id = false, $_args = array() ) {
-		$result = new Give_Forms_Query(
-			array(
-				'output' => '',
-				'p'      => $_id,
-			)
-		);
-
-		$donation_form = current( $result->get_forms() );
+		$donation_form = WP_Post::get_instance( $_id );
 
 		return $this->setup_donation_form( $donation_form );
 	}
@@ -384,6 +377,8 @@ class Give_Donate_Form {
 		if ( 'give_forms' !== $donation_form->post_type ) {
 			return false;
 		}
+
+		Give_Forms_Query::update_meta_cache( array( $donation_form->ID ) );
 
 		foreach ( $donation_form as $key => $value ) {
 
