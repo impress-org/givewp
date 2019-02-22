6650a03ece749c352bd10a450aa53046d2f4e406
mehul0810 <mehul.gohil0810@gmail.com>
2019-02-22 11:39:44 +0530

    feat: prepare basic customer class

---
+++ includes/gateways/stripe/includes/class-give-stripe-customer.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Give - Stripe Core Customer
+ *
+ * @since 2.5.0
+ *
+ * @package    Give
+ * @subpackage Stripe Core
+ * @copyright  Copyright (c) 2019, GiveWP
+ * @license    https://opensource.org/licenses/gpl-license GNU Public License
+ */
+
+// Exit, if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
