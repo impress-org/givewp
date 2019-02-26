8c1a1197f92d4660870645a6b5dfbb8fd855d73a
mehul0810 <mehul.gohil0810@gmail.com>
2019-02-26 08:39:14 +0530

    fix: add blank admin actions and filters file

---
+++ includes/gateways/stripe/includes/admin/admin-actions.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Give - Stripe Core Admin Actions
+ *
+ * @since 2.5.0
+ *
+ * @package    Give
+ * @subpackage Stripe Core
+ * @copyright  Copyright (c) 2019, GiveWP
+ * @license    https://opensource.org/licenses/gpl-license GNU Public License
+ */
+
+// Exit, if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
