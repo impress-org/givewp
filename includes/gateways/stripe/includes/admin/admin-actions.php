b90c62d589b08c9a8fa2e3f5b321ab98416d7175
mehul0810 <mehul.gohil0810@gmail.com>
2019-02-26 19:23:23 +0530

    feat: add fn to process webhook automation

--- includes/gateways/stripe/includes/admin/admin-actions.php
+++ includes/gateways/stripe/includes/admin/admin-actions.php
@@ -15,3 +15,52 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
+/**
+ * This function is used as an AJAX callback to the click event of "Sync Again" button.
+ *
+ * @since 2.5.0
+ *
+ * @return void|array
+ */
+function give_stripe_check_webhook_status_callback() {
+
+    // Set defaults.
+    $data = array(
+        'live_webhooks_setup'    => false,
+        'sandbox_webhooks_setup' => false,
+    );
+
+    $give_stripe_webhook = new Give_Stripe_Webhooks();
+    $webhook_id          = give_stripe_get_webhook_id();
+
+    if ( ! empty( $webhook_id ) ) {
+
+        // Get webhook details of an existing one.
+        $webhook_details = $give_stripe_webhook->retrieve( $webhook_id );
+
+		// Set WebHook details to DB.
+        if ( ! empty( $webhook_details->id ) ) {
+            $give_stripe_webhook->set_data_to_db( $webhook_details->id );
+        }
+    }
+
+    // Recreate Webhook, if the details in DB mismatch with Stripe.
+    if ( empty( $webhook_details->id ) ) {
+
+        // Get webhook details after creating one.
+        $webhook_details = $give_stripe_webhook->create();
+    }
+
+	if ( ! empty( $webhook_details->id ) ) {
+        if ( give_is_test_mode() ) {
+            $data['sandbox_webhooks_setup'] = true;
+        } else {
+            $data['live_webhooks_setup'] = true;
+        }
+    }
+
+	wp_send_json_success( $data );
+
+    give_die();
+}
+add_action( 'wp_ajax_give_stripe_check_webhook_status', 'give_stripe_check_webhook_status_callback' );
