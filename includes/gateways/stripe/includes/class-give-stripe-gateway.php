66e14cc2592497497b4233ac1de05c391e56d8b0
mehul0810 <mehul.gohil0810@gmail.com>
2019-03-04 15:35:15 +0530

    fix: load stripe php sdk file

--- includes/gateways/stripe/includes/class-give-stripe-gateway.php
+++ includes/gateways/stripe/includes/class-give-stripe-gateway.php
@@ -68,6 +68,8 @@ if ( ! class_exists( 'Give_Stripe_Gateway' ) ) {
 				return false;
 			}
 
+			require_once GIVE_PLUGIN_DIR . 'vendor/autoload.php';
+
 			// Set secret key received from Stripe.
 			$this->secret_key = give_stripe_get_secret_key();
 
