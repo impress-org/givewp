f447ed1e6afef182b4c4232c705e30b1a6144efe
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2019-03-12 22:54:44 +0530

    chore: run frontend test for all git branch

--- tests/e2e/bin/install.sh
+++ tests/e2e/bin/install.sh
@@ -4,21 +4,16 @@ echo "Give Log: current branch is ${TRAVIS_BRANCH}";
 echo "${TRAVIS_PHP_VERSION:0:3}";
 echo "${TRAVIS_EVENT_TYPE}"
 
-if [[ ${TRAVIS_PHP_VERSION:0:3} != "5.3" ]] && [ "${TRAVIS_BRANCH}" == 'master' ]; then
-	echo 'Give Log: setup and run frontend tests';
+echo 'Give Log: setup and run frontend tests';
 
-	until $(curl --output /dev/null --silent --head --fail http://localhost:8004); do printf '.'; sleep 5; done;
-	cd ~/wordpress_data/wp-content/plugins
-	git clone -b ${TRAVIS_BRANCH} --single-branch https://github.com/impress-org/give.git
-	cd ~/wordpress_data/wp-content/plugins/give/
-	docker exec give_wordpress_1 wp plugin activate give
-	composer install
-	rm -rf ./node_modules package-lock.json
-	npm cache clean --force
-	npm install
-	npm run dev
-	npm run test
-
-else
-	echo 'Give Log: Stop frontend tests from running on branches other than master';
-fi
+until $(curl --output /dev/null --silent --head --fail http://localhost:8004); do printf '.'; sleep 5; done;
+cd ~/wordpress_data/wp-content/plugins
+git clone -b ${TRAVIS_BRANCH} --single-branch https://github.com/impress-org/give.git
+cd ~/wordpress_data/wp-content/plugins/give/
+docker exec give_wordpress_1 wp plugin activate give
+composer install
+rm -rf ./node_modules package-lock.json
+npm cache clean --force
+npm install
+npm run dev
+npm run test
