e1c00e68050596533921a442478bc7c7dc21f531
mehul0810 <mehul.gohil0810@gmail.com>
2019-04-11 20:52:56 +0530

    fix: show notices based on the error

--- includes/gateways/stripe/includes/actions.php
+++ includes/gateways/stripe/includes/actions.php
@@ -56,98 +56,126 @@ function give_stripe_credit_card_form( $form_id, $args, $echo = true ) {
 			<?php esc_attr_e( 'Credit Card Info', 'give' ); ?>
 		</legend>
 
-		<?php if ( is_ssl() ) : ?>
+		<?php
+		if ( is_ssl() ) {
+			?>
 			<div id="give_secure_site_wrapper">
 				<span class="give-icon padlock"></span>
 				<span>
 					<?php esc_attr_e( 'This is a secure SSL encrypted payment.', 'give' ); ?>
 				</span>
 			</div>
-		<?php endif; ?>
-		<?php
-		if ( 'single' === $stripe_cc_field_format ) {
+			<?php
+		}
 
-			// Display the stripe container which can be occupied by Stripe for CC fields.
-			echo '<div id="give-stripe-single-cc-fields-' . esc_html( $id_prefix ) . '" class="give-stripe-single-cc-field-wrap"></div>';
-
-		} elseif ( 'multi' === $stripe_cc_field_format ) {
+		if (
+				(
+					! is_ssl() &&
+					! give_is_test_mode()
+				) ||
+				(
+					empty( give_stripe_get_publishable_key() ) ||
+					empty( give_stripe_get_secret_key() )
+				)
+			) {
 			?>
-			<div id="give-card-number-wrap" class="form-row form-row-two-thirds form-row-responsive give-stripe-cc-field-wrap">
-				<div>
-					<label for="give-card-number-field-<?php echo esc_html( $id_prefix ); ?>" class="give-label">
-						<?php esc_attr_e( 'Card Number', 'give' ); ?>
-						<span class="give-required-indicator">*</span>
-						<span class="give-tooltip give-icon give-icon-question"
-							data-tooltip="<?php esc_attr_e( 'The (typically) 16 digits on the front of your credit card.', 'give' ); ?>"></span>
-						<span class="card-type"></span>
-					</label>
-					<div id="give-card-number-field-<?php echo esc_html( $id_prefix ); ?>" class="input empty give-stripe-cc-field give-stripe-card-number-field"></div>
-				</div>
-			</div>
-
-			<div id="give-card-cvc-wrap" class="form-row form-row-one-third form-row-responsive give-stripe-cc-field-wrap">
-				<div>
-					<label for="give-card-cvc-field-<?php echo esc_html( $id_prefix ); ?>" class="give-label">
-						<?php esc_attr_e( 'CVC', 'give' ); ?>
-						<span class="give-required-indicator">*</span>
-						<span class="give-tooltip give-icon give-icon-question"
-							data-tooltip="<?php esc_attr_e( 'The 3 digit (back) or 4 digit (front) value on your card.', 'give' ); ?>"></span>
-					</label>
-					<div id="give-card-cvc-field-<?php echo esc_html( $id_prefix ); ?>" class="input empty give-stripe-cc-field give-stripe-card-cvc-field"></div>
-				</div>
-			</div>
-
-			<div id="give-card-name-wrap" class="form-row form-row-two-thirds form-row-responsive">
-				<label for="card_name" class="give-label">
-					<?php esc_attr_e( 'Cardholder Name', 'give' ); ?>
-					<span class="give-required-indicator">*</span>
-					<span class="give-tooltip give-icon give-icon-question"
-						data-tooltip="<?php esc_attr_e( 'The name of the credit card account holder.', 'give' ); ?>"></span>
-				</label>
-				<input
-					type="text"
-					autocomplete="off"
-					id="card_name"
-					name="card_name"
-					class="card-name give-input required"
-					placeholder="<?php esc_attr_e( 'Cardholder Name', 'give' ); ?>"
-				/>
-			</div>
-
-			<?php do_action( 'give_before_cc_expiration' ); ?>
-
-			<div id="give-card-expiration-wrap" class="card-expiration form-row form-row-one-third form-row-responsive give-stripe-cc-field-wrap">
-				<div>
-					<label for="give-card-expiration-field-<?php echo esc_html( $id_prefix ); ?>" class="give-label">
-						<?php esc_attr_e( 'Expiration', 'give' ); ?>
-						<span class="give-required-indicator">*</span>
-						<span class="give-tooltip give-icon give-icon-question"
-							data-tooltip="<?php esc_attr_e( 'The date your credit card expires, typically on the front of the card.', 'give' ); ?>"></span>
-					</label>
-
-					<div id="give-card-expiration-field-<?php echo esc_html( $id_prefix ); ?>" class="input empty give-stripe-cc-field give-stripe-card-expiration-field"></div>
-				</div>
+			<div class="give_error give_warning">
+				<p>
+					<?php
+					echo sprintf(
+						'<strong>%1$s</strong> %2$s',
+						esc_html__( 'Notice:', 'give' ),
+						esc_html__( 'Credit card fields are disabled because Stripe is either not connected or your site is not running securely over HTTPS.', 'give' )
+					);
+					?>
+				</p>
 			</div>
 			<?php
-		} // End if().
+		} else {
+			if ( 'single' === $stripe_cc_field_format ) {
 
-		/**
-		 * This action hook is used to display content after the Credit Card expiration field.
-		 *
-		 * Note: Kept this hook as it is.
-		 *
-		 * @param int   $form_id Donation Form ID.
-		 * @param array $args    List of additional arguments.
-		 */
-		do_action( 'give_after_cc_expiration', $form_id, $args );
+				// Display the stripe container which can be occupied by Stripe for CC fields.
+				echo '<div id="give-stripe-single-cc-fields-' . esc_html( $id_prefix ) . '" class="give-stripe-single-cc-field-wrap"></div>';
 
-		/**
-		 * This action hook is used to display content after the Credit Card expiration field.
-		 *
-		 * @param int   $form_id Donation Form ID.
-		 * @param array $args    List of additional arguments.
-		 */
-		do_action( 'give_stripe_after_cc_expiration', $form_id, $args );
+			} elseif ( 'multi' === $stripe_cc_field_format ) {
+				?>
+				<div id="give-card-number-wrap" class="form-row form-row-two-thirds form-row-responsive give-stripe-cc-field-wrap">
+					<div>
+						<label for="give-card-number-field-<?php echo esc_html( $id_prefix ); ?>" class="give-label">
+							<?php esc_attr_e( 'Card Number', 'give' ); ?>
+							<span class="give-required-indicator">*</span>
+							<span class="give-tooltip give-icon give-icon-question"
+								data-tooltip="<?php esc_attr_e( 'The (typically) 16 digits on the front of your credit card.', 'give' ); ?>"></span>
+							<span class="card-type"></span>
+						</label>
+						<div id="give-card-number-field-<?php echo esc_html( $id_prefix ); ?>" class="input empty give-stripe-cc-field give-stripe-card-number-field"></div>
+					</div>
+				</div>
+
+				<div id="give-card-cvc-wrap" class="form-row form-row-one-third form-row-responsive give-stripe-cc-field-wrap">
+					<div>
+						<label for="give-card-cvc-field-<?php echo esc_html( $id_prefix ); ?>" class="give-label">
+							<?php esc_attr_e( 'CVC', 'give' ); ?>
+							<span class="give-required-indicator">*</span>
+							<span class="give-tooltip give-icon give-icon-question"
+								data-tooltip="<?php esc_attr_e( 'The 3 digit (back) or 4 digit (front) value on your card.', 'give' ); ?>"></span>
+						</label>
+						<div id="give-card-cvc-field-<?php echo esc_html( $id_prefix ); ?>" class="input empty give-stripe-cc-field give-stripe-card-cvc-field"></div>
+					</div>
+				</div>
+
+				<div id="give-card-name-wrap" class="form-row form-row-two-thirds form-row-responsive">
+					<label for="card_name" class="give-label">
+						<?php esc_attr_e( 'Cardholder Name', 'give' ); ?>
+						<span class="give-required-indicator">*</span>
+						<span class="give-tooltip give-icon give-icon-question"
+							data-tooltip="<?php esc_attr_e( 'The name of the credit card account holder.', 'give' ); ?>"></span>
+					</label>
+					<input
+						type="text"
+						autocomplete="off"
+						id="card_name"
+						name="card_name"
+						class="card-name give-input required"
+						placeholder="<?php esc_attr_e( 'Cardholder Name', 'give' ); ?>"
+					/>
+				</div>
+
+				<?php do_action( 'give_before_cc_expiration' ); ?>
+
+				<div id="give-card-expiration-wrap" class="card-expiration form-row form-row-one-third form-row-responsive give-stripe-cc-field-wrap">
+					<div>
+						<label for="give-card-expiration-field-<?php echo esc_html( $id_prefix ); ?>" class="give-label">
+							<?php esc_attr_e( 'Expiration', 'give' ); ?>
+							<span class="give-required-indicator">*</span>
+							<span class="give-tooltip give-icon give-icon-question"
+								data-tooltip="<?php esc_attr_e( 'The date your credit card expires, typically on the front of the card.', 'give' ); ?>"></span>
+						</label>
+
+						<div id="give-card-expiration-field-<?php echo esc_html( $id_prefix ); ?>" class="input empty give-stripe-cc-field give-stripe-card-expiration-field"></div>
+					</div>
+				</div>
+				<?php
+			} // End if().
+
+			/**
+			 * This action hook is used to display content after the Credit Card expiration field.
+			 *
+			 * Note: Kept this hook as it is.
+			 *
+			 * @param int   $form_id Donation Form ID.
+			 * @param array $args    List of additional arguments.
+			 */
+			do_action( 'give_after_cc_expiration', $form_id, $args );
+
+			/**
+			 * This action hook is used to display content after the Credit Card expiration field.
+			 *
+			 * @param int   $form_id Donation Form ID.
+			 * @param array $args    List of additional arguments.
+			 */
+			do_action( 'give_stripe_after_cc_expiration', $form_id, $args );
+		}
 		?>
     </fieldset>
 	<?php
