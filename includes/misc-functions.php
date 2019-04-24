b1070ade208745665e9ea0cd50323febf4610f1e
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2019-04-24 15:40:23 +0530

    refactor: remove unused fn

--- includes/misc-functions.php
+++ includes/misc-functions.php
@@ -914,7 +914,6 @@ function give_get_plugins() {
 			// Plugin is a Give-addon.
 			$plugins[ $plugin_path ]['Type'] = 'add-on';
 
-			/* @var stdClass $license_active */
 			$license_active = Give_License::get_license_by_item_name( Give_License::license_dashed_name( $plugin_data['Name'] ) );
 
 			// Does a valid license exist?
@@ -1872,49 +1871,6 @@ function give_is_donor_comment_field_enabled( $form_id ) {
 
 	return false;
 
-}
-
-/**
- * Get add-on user meta value information
- * Note: only for internal use.
- *
- * @since 2.1.0
- *
- * @param string $license_id
- *
- * @return array
- */
-function __give_get_active_license_info( $license_id ) {
-	global $wpdb;
-	$option_name = "{$license_id}_license_active";
-	$data        = array();
-
-	if ( ! isset( $GLOBALS['give_active_licenses_info'] ) ) {
-		$GLOBALS['give_active_licenses_info'] = array();
-
-		$licenses_info = $wpdb->get_results(
-			"
-			SELECT option_name, option_value
-			FROM {$wpdb->options}
-			WHERE option_name LIKE '%_license_active%'
-			AND option_name LIKE '%give_%'
-			",
-			ARRAY_A
-		);
-
-		if ( ! empty( $licenses_info ) ) {
-			$GLOBALS['give_active_licenses_info'] = array_combine(
-				wp_list_pluck( $licenses_info, 'option_name' ),
-				wp_list_pluck( $licenses_info, 'option_value' )
-			);
-		}
-	}
-
-	if ( in_array( $option_name, array_keys( $GLOBALS['give_active_licenses_info'] ) ) ) {
-		$data = maybe_unserialize( $GLOBALS['give_active_licenses_info'][ $option_name ] );
-	}
-
-	return $data;
 }
 
 /**
