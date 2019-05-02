ed2c30a3db87fe9b379e2ace32642489ea0e5cba
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2019-05-02 16:55:31 +0530

    fix: place license and add-on update field side by side

--- includes/admin/settings/class-settings-license.php
+++ includes/admin/settings/class-settings-license.php
@@ -123,58 +123,64 @@ if ( ! class_exists( 'Give_Settings_License' ) ) :
 		public function output() {
 			ob_start();
 			?>
-			<div id="give-addon-uploader-wrap" ondragover="event.preventDefault()">
-				<div id="give-addon-uploader-inner">
-					<?php if ( 'direct' !== get_filesystem_method() ) : ?>
-						<div class="give-notice notice notice-error inline">
-							<p>
-								<?php
-								echo sprintf(
-									__( 'Sorry, you can not upload plugin from here because we do not have direct access to file system. Please <a href="%1$s" target="_blank">click here</a> to upload Give Add-on.', 'give' ),
-									admin_url( 'plugin-install.php?tab=upload' )
-								);
-								?>
-							</p>
-						</div>
-					<?php else: ?>
-						<div class="give-notices"></div>
-						<div class="give-form-wrap">
-							<?php _e( '<h1>Drop files here </br>or</h1>', 'give' ); ?>
-							<form method="post" enctype="multipart/form-data" class="give-upload-form" action="/">
-								<?php wp_nonce_field( 'give-upload-addon', '_give_upload_addon' ); ?>
-								<input type="file" name="addon" value="<?php _e( 'Select File', 'give' ); ?>">
+			<div class="give-grid-row">
+				<div class="give-grid-col-6">
+					<div id="give-license-activator-wrap">
+						<div id="give-license-activator-inner">
+							<div class="give-notices"></div>
+							<form method="post" action="">
+								<?php wp_nonce_field( 'give-license-activator-nonce', 'give_license_activator_nonce' ); ?>
+								<label for="give-license-activator" class="screen-reader-text"><?php _e( 'Activate License', 'give' ); ?></label>
+								<input id="give-license-activator" type="text" name="give_license_key" placeholder="<?php _e( 'Enter a valid license key', 'give' ) ?>">
+								<input
+									data-activate="<?php _e( 'Activate License', 'give' ); ?>"
+									data-activating="<?php _e( 'Verifying License...', 'give' ); ?>"
+									value="<?php _e( 'Activate License', 'give' ); ?>"
+									type="submit"
+									class="button"
+									disabled
+								>
 							</form>
 						</div>
-						<div class="give-activate-addon-wrap" style="display: none">
-							<button
-								class="give-activate-addon-btn button-primary"
-								data-activate="<?php _e( 'Activate Addon', 'give' ); ?>"
-								data-activating="<?php _e( 'Activateing Addon...', 'give' ); ?>"
-							><?php _e( 'Activate Addon', 'give' ); ?></button>
+
+						<p class="give-field-description"><?php _e( 'Enter a license key above to unlock your GiveWP add-ons. You can access your licenses anytime from the My Account section on the GiveWP website.' ); ?></p>
+					</div>
+				</div>
+
+				<div class="give-grid-col-6">
+					<div id="give-addon-uploader-wrap" ondragover="event.preventDefault()">
+						<div id="give-addon-uploader-inner">
+							<?php if ( 'direct' !== get_filesystem_method() ) : ?>
+								<div class="give-notice notice notice-error inline">
+									<p>
+										<?php
+										echo sprintf(
+											__( 'Sorry, you can not upload plugin from here because we do not have direct access to file system. Please <a href="%1$s" target="_blank">click here</a> to upload Give Add-on.', 'give' ),
+											admin_url( 'plugin-install.php?tab=upload' )
+										);
+										?>
+									</p>
+								</div>
+							<?php else: ?>
+								<div class="give-notices"></div>
+								<div class="give-form-wrap">
+									<?php _e( '<h1>Drop files here </br>or</h1>', 'give' ); ?>
+									<form method="post" enctype="multipart/form-data" class="give-upload-form" action="/">
+										<?php wp_nonce_field( 'give-upload-addon', '_give_upload_addon' ); ?>
+										<input type="file" name="addon" value="<?php _e( 'Select File', 'give' ); ?>">
+									</form>
+								</div>
+								<div class="give-activate-addon-wrap" style="display: none">
+									<button
+										class="give-activate-addon-btn button-primary"
+										data-activate="<?php _e( 'Activate Addon', 'give' ); ?>"
+										data-activating="<?php _e( 'Activateing Addon...', 'give' ); ?>"
+									><?php _e( 'Activate Addon', 'give' ); ?></button>
+								</div>
+							<?php endif; ?>
 						</div>
-					<?php endif; ?>
+					</div>
 				</div>
-			</div>
-
-			<div id="give-license-activator-wrap">
-				<div id="give-license-activator-inner">
-					<div class="give-notices"></div>
-					<form method="post" action="">
-						<?php wp_nonce_field( 'give-license-activator-nonce', 'give_license_activator_nonce' ); ?>
-						<label for="give-license-activator" class="screen-reader-text"><?php _e( 'Activate License', 'give' ); ?></label>
-						<input id="give-license-activator" type="text" name="give_license_key" placeholder="<?php _e( 'Enter a valid license key', 'give' ) ?>">
-						<input
-							data-activate="<?php _e( 'Activate License', 'give' ); ?>"
-							data-activating="<?php _e( 'Verifying License...', 'give' ); ?>"
-							value="<?php _e( 'Activate License', 'give' ); ?>"
-							type="submit"
-							class="button"
-							disabled
-						>
-					</form>
-				</div>
-
-				<p class="give-field-description"><?php _e( 'Enter a license key above to unlock your GiveWP add-ons. You can access your licenses anytime from the My Account section on the GiveWP website.' ); ?></p>
 			</div>
 
 			<h2><?php _e( 'License and Downloads', 'give' ); ?></h2>
