0b5c6e41439b6ec9d37c8d09dd1bf1993a78f03f
mehul0810 <mehul.gohil0810@gmail.com>
2019-05-10 16:37:17 +0530

    fix: relocate the process payment back to stripe class

--- includes/gateways/stripe/includes/payment-methods/class-give-stripe-card.php
+++ includes/gateways/stripe/includes/payment-methods/class-give-stripe-card.php
@@ -38,8 +38,6 @@ if ( ! class_exists( 'Give_Stripe_Card' ) ) {
 			$this->id = 'stripe';
 
 			parent::__construct();
-
-			add_action( 'init', array( $this, 'listen_stripe_3dsecure_payment' ) );
 		}
 
 		/**
@@ -147,7 +145,137 @@ if ( ! class_exists( 'Give_Stripe_Card' ) ) {
 				return;
 			}
 
-			give_stripe_process_payment( $donation_data, $this );
+			// Make sure we don't have any left over errors present.
+			give_clear_errors();
+
+			$payment_method_id = ! empty( $donation_data['post_data']['give_stripe_payment_method'] )
+				? $donation_data['post_data']['give_stripe_payment_method']
+				: $this->check_for_source( $donation_data );
+
+			// Any errors?
+			$errors = give_get_errors();
+
+			// No errors, proceed.
+			if ( ! $errors ) {
+
+				$form_id          = ! empty( $donation_data['post_data']['give-form-id'] ) ? intval( $donation_data['post_data']['give-form-id'] ) : 0;
+				$price_id         = ! empty( $donation_data['post_data']['give-price-id'] ) ? $donation_data['post_data']['give-price-id'] : 0;
+				$donor_email      = ! empty( $donation_data['post_data']['give_email'] ) ? $donation_data['post_data']['give_email'] : 0;
+				$donation_summary = give_payment_gateway_donation_summary( $donation_data, false );
+
+				// Get an existing Stripe customer or create a new Stripe Customer and attach the source to customer.
+				$give_stripe_customer = new Give_Stripe_Customer( $donor_email, $payment_method_id );
+				$stripe_customer      = $give_stripe_customer->customer_data;
+				$stripe_customer_id   = $give_stripe_customer->get_id();
+
+				// We have a Stripe customer, charge them.
+				if ( $stripe_customer_id ) {
+
+					// Proceed to get stripe source/payment method details.
+					$payment_method    = $give_stripe_customer->attached_payment_method;
+					$payment_method_id = $payment_method->id;
+
+					// Setup the payment details.
+					$payment_data = array(
+						'price'           => $donation_data['price'],
+						'give_form_title' => $donation_data['post_data']['give-form-title'],
+						'give_form_id'    => $form_id,
+						'give_price_id'   => $price_id,
+						'date'            => $donation_data['date'],
+						'user_email'      => $donation_data['user_email'],
+						'purchase_key'    => $donation_data['purchase_key'],
+						'currency'        => give_get_currency( $form_id ),
+						'user_info'       => $donation_data['user_info'],
+						'status'          => 'pending',
+						'gateway'         => $this->id,
+					);
+
+					// Record the pending payment in Give.
+					$donation_id = give_insert_payment( $payment_data );
+
+					// Assign required data to array of donation data for future reference.
+					$donation_data['donation_id'] = $donation_id;
+					$donation_data['description'] = $donation_summary;
+					$donation_data['source_id']   = $payment_method_id;
+
+					// Save Stripe Customer ID to Donation note, Donor and Donation for future reference.
+					give_insert_payment_note( $donation_id, 'Stripe Customer ID: ' . $stripe_customer_id );
+					$this->save_stripe_customer_id( $stripe_customer_id, $donation_id );
+					give_update_meta( $donation_id, '_give_stripe_customer_id', $stripe_customer_id );
+
+					// Save Source ID to donation note and DB.
+					give_insert_payment_note( $donation_id, 'Stripe Source/Payment Method ID: ' . $payment_method_id );
+					give_update_meta( $donation_id, '_give_stripe_source_id', $payment_method_id );
+
+					// Save donation summary to donation.
+					give_update_meta( $donation_id, '_give_stripe_donation_summary', $donation_summary );
+
+
+					if ( give_stripe_is_checkout_enabled() ) {
+
+						// Process charge w/ support for preapproval.
+						$charge = $this->process_charge( $donation_data, $stripe_customer_id );
+
+						// Verify the Stripe payment.
+						$this->verify_payment( $donation_id, $stripe_customer_id, $charge );
+					} else {
+
+						/**
+						 * This filter hook is used to update the payment intent arguments.
+						 *
+						 * @since 2.5.0
+						 */
+						$intent_args = apply_filters(
+							'give_stripe_create_intent_args',
+							array(
+								'amount'               => $this->format_amount( $donation_data['price'] ),
+								'currency'             => give_get_currency( $form_id ),
+								'payment_method_types' => [ 'card' ],
+								'statement_descriptor' => give_stripe_get_statement_descriptor(),
+								'receipt_email'        => $donation_data['user_email'],
+								'description'          => give_payment_gateway_donation_summary( $donation_data ),
+								'metadata'             => $this->prepare_metadata( $donation_id ),
+								'customer'             => $stripe_customer_id,
+								'payment_method'       => $payment_method_id,
+								'confirm'              => true,
+								'return_url'           => give_get_success_page_uri(),
+							)
+						);
+						$intent     = $this->payment_intent->create( $intent_args );
+
+						// Save Payment Intent Client Secret to donation note and DB.
+						give_insert_payment_note( $donation_id, 'Stripe Payment Intent Client Secret: ' . $intent->client_secret );
+						give_update_meta( $donation_id, '_give_stripe_payment_intent_client_secret', $intent->client_secret );
+
+						// Set Payment Intent ID as transaction ID for the donation.
+						give_set_payment_transaction_id( $donation_id, $intent->id );
+						give_insert_payment_note( $donation_id, 'Stripe Charge/Payment Intent ID: ' . $intent->id );
+
+						// Process additional steps for SCA or 3D secure.
+						give_stripe_process_additional_authentication( $donation_id, $intent );
+
+						// Send them to success page.
+						give_send_to_success_page();
+
+					}
+				} else {
+
+					// No customer, failed.
+					give_record_gateway_error(
+						__( 'Stripe Customer Creation Failed', 'give' ),
+						sprintf(
+							/* translators: %s Donation Data */
+							__( 'Customer creation failed while processing the donation. Details: %s', 'give' ),
+							wp_json_encode( $donation_data )
+						)
+					);
+					give_set_error( 'stripe_error', __( 'The Stripe Gateway returned an error while processing the donation.', 'give' ) );
+					give_send_back_to_checkout( '?payment-mode=' . give_clean( $_POST['payment-mode'] ) );
+
+				} // End if().
+			} else {
+				give_send_back_to_checkout( '?payment-mode=' . give_clean( $_POST['payment-mode'] ) );
+			} // End if().
 		}
 
 		/**
