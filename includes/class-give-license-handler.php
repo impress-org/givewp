1ed6c96db73379c97fe4b20ded5ba6334cd1b02f
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2019-05-23 12:09:44 +0530

    chore: convert url to staging for testing

--- includes/class-give-license-handler.php
+++ includes/class-give-license-handler.php
@@ -154,7 +154,7 @@ if ( ! class_exists( 'Give_License' ) ) :
 		 *
 		 * @var null|string
 		 */
-		private static $account_url = 'https://givewp.com/my-account/';
+		private static $account_url = 'http://staging.givewp.com/my-account/';
 
 		/**
 		 * Checkout URL
@@ -164,7 +164,7 @@ if ( ! class_exists( 'Give_License' ) ) :
 		 *
 		 * @var null|string
 		 */
-		private static $checkout_url = 'https://givewp.com/checkout/';
+		private static $checkout_url = 'http://staging.givewp.com/checkout/';
 
 		/**
 		 * Class Constructor
