97d75f9f0de19b2b2c837ee0b8b00edda23eb89f
Devin Walker <sdtucson@gmail.com>
2019-05-22 22:57:20 -0700

    refactor: use staging urls for testing

--- includes/class-give-license-handler.php
+++ includes/class-give-license-handler.php
@@ -124,7 +124,7 @@ if ( ! class_exists( 'Give_License' ) ) :
 		 *
 		 * @var    string
 		 */
-		private static $site_url = 'https://givewp.com/';
+		private static $site_url = 'http://staging.givewp.com/';
 
 		/**
 		 * API URL
@@ -134,7 +134,7 @@ if ( ! class_exists( 'Give_License' ) ) :
 		 *
 		 * @var    string
 		 */
-		private $api_url = 'https://givewp.com/edd-sl-api/';
+		private $api_url = 'http://staging.givewp.com/edd-sl-api/';
 
 		/**
 		 * array of licensed addons
