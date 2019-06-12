4239f0e22aeeec40f412667f5aa97b642e337c1d
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2019-06-12 21:21:48 +0530

    chore: code improvement
    1. use give_clean instead of core sanitization fn
    2. start-date will be in Y-,-d format,so no need to reformat

--- includes/admin/payments/class-payments-table.php
+++ includes/admin/payments/class-payments-table.php
@@ -918,10 +918,10 @@ class Give_Payment_History_Table extends WP_List_Table {
 		$day        = isset( $_GET['day'] ) ? $_GET['day'] : null;
 		$search     = isset( $_GET['s'] ) ? sanitize_text_field( $_GET['s'] ) : null;
 		$start_date = ! empty ( $_GET['start-date'] )
-			? date('Y-m-d', strtotime( give_clean( $_GET['start-date'] ) ) )
+			? give_clean( $_GET['start-date'] )
 			: date( 'Y-m-d', 0 );
 		$end_date   = ! empty( $_GET['end-date'] )
-			? sanitize_text_field( $_GET['end-date'] )
+			? give_clean( $_GET['end-date'] )
 			: date( 'Y-m-d', current_time( 'timestamp' ) );
 		$form_id    = ! empty( $_GET['form_id'] ) ? absint( $_GET['form_id'] ) : null;
 		$gateway    = ! empty( $_GET['gateway'] ) ? give_clean( $_GET['gateway'] ) : null;
