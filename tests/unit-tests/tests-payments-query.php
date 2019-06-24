34852e90a39d7a93ad52730daf9b6d862d4cc51e
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2019-06-24 18:11:17 +0530

    chore: add basic test class for Give_Payments_Query

---
+++ tests/unit-tests/tests-payments-query.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Class Test_Payments_Query
+ */
+class Test_Payments_Query extends Give_Unit_Test_Case {
+	/**
+	 * Set it up
+	 *
+	 * @since 2.5.1
+	 */
+	public function setUp() {
+		parent::setUp();
+	}
+
+	/**
+	 * Tear it Down
+	 * @since 2.5.1
+	 */
+	public function tearDown() {
+		parent::tearDown();
+	}
+}
