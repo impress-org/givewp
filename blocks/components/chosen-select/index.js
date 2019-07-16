fccfe72fa588ffb8c2489dc182c5183526dbbc42
mehul0810 <mehul.gohil0810@gmail.com>
2019-07-16 15:31:18 +0530

    fix: add necessary changes to chosen component

--- blocks/components/chosen-select/index.js
+++ blocks/components/chosen-select/index.js
@@ -2,6 +2,7 @@
  * Wordpress dependencies
  */
 const { Component } = wp.element;
+const { BaseControl } = wp.components;
 
 /**
  * Render ChosenSelect Control
@@ -31,7 +32,7 @@ class ChosenSelect extends Component {
 	componentDidMount() {
 		this.$el = jQuery( this.el );
 		this.$input = this.$el.chosen( {
-			reset_search_field_on_update: false,
+			width: '100%',
 		} ).data( 'chosen' );
 
 		this.handleChange = this.handleChange.bind( this );
@@ -49,34 +50,37 @@ class ChosenSelect extends Component {
 		this.props.onChange( e.target.value );
 	}
 
-	componentDidUpdate( prevProps ) {
-		if ( prevProps.options !== this.props.options ) {
-			this.$input.search_field.autocomplete( {
-				source: function( request, response ) {
-					const data = {
-						action: 'give_block_donation_form_search_results',
-						search: request.term,
-					};
+	componentDidUpdate() {
+		const $searchField = jQuery( '.chosen-base-control' ).closest( '.chosen-container' ).find( '.chosen-search-input' );
+		this.$input.search_field.autocomplete( {
+			source: function( request, response ) {
+				const data = {
+					action: 'give_block_donation_form_search_results',
+					search: request.term,
+				};
 
-					jQuery.post( ajaxurl, data, ( responseData ) => {
-						jQuery( '.give-block-chosen-select' ).empty();
-						responseData = JSON.parse( responseData );
+				jQuery.post( ajaxurl, data, ( responseData ) => {
+					jQuery( '.give-block-chosen-select' ).empty();
+					responseData = JSON.parse( responseData );
+
+					if ( responseData.length > 0 ) {
 						response( jQuery.map( responseData, function( item ) {
 							jQuery( '.give-block-chosen-select' ).append( '<option value="' + item.id + '">' + item.name + '</option>' );
 						} ) );
 						jQuery( '.give-block-chosen-select' ).trigger( 'chosen:updated' );
-					} );
-				},
-			} );
-			this.$input.search_field.val( this.search );
-			// this.$el.trigger( 'chosen:updated' );
-		}
+
+						$searchField.prop( 'disabled', false );
+						$searchField.val( request.term );
+					}
+				} );
+			},
+		} );
 	}
 
 	render() {
 		return (
-			<div>
-				<select className="chosen-select give-select give-select-chosen give-block-chosen-select" ref={ el => this.el = el }>
+			<BaseControl className="chosen-base-control">
+				<select className="give-select give-select-chosen give-block-chosen-select" ref={ el => this.el = el }>
 					{ this.props.options.map( ( option, index ) =>
 						<option
 							key={ `${ option.label }-${ option.value }-${ index }` }
@@ -86,7 +90,7 @@ class ChosenSelect extends Component {
 						</option>
 					) }
 				</select>
-			</div>
+			</BaseControl>
 		);
 	}
 }
