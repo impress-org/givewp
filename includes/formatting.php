19da93f76d7401fd60162646df08eab0af725d1a
Mehul Gohil <mehul.gohil0810@gmail.com>
2019-08-02 10:43:39 +0530

    fix: update code for human format amount fn

--- includes/formatting.php
+++ includes/formatting.php
@@ -501,13 +501,6 @@ function give_format_amount( $amount, $args = array() ) {
  * @return string  formatted amount number with large number names.
  */
 function give_human_format_large_amount( $amount, $args = array() ) {
-	// Sanitize amount.
-	$sanitize_amount = give_maybe_sanitize_amount( $amount );
-
-	// Bailout.
-	if ( ! floatval( $sanitize_amount ) ) {
-		return '0';
-	};
 
 	// Set default currency;
 	if ( empty( $args['currency'] ) ) {
@@ -515,10 +508,13 @@ function give_human_format_large_amount( $amount, $args = array() ) {
 	}
 
 	// Get thousand separator.
-	$thousands_sep = give_get_price_thousand_separator();
+	$thousands_sep = give_get_price_thousand_separator( $args['currency'] );
 
 	// Explode amount to calculate name of large numbers.
 	$amount_array = explode( $thousands_sep, $amount );
+
+	// Unformat currency for calculation purpose.
+	$unformat_amount = implode( '', $amount_array );
 
 	// Calculate amount parts count.
 	$amount_count_parts = count( $amount_array );
@@ -530,25 +526,25 @@ function give_human_format_large_amount( $amount, $args = array() ) {
 		case 'INR':
 			// Calculate large number formatted amount.
 			if ( 4 < $amount_count_parts ) {
-				$human_format_amount = sprintf( esc_html__( '%s arab', 'give' ), round( ( $sanitize_amount / 1000000000 ), 2 ) );
+				$human_format_amount = sprintf( esc_html__( '%s arab', 'give' ), round( ( $unformat_amount / 1000000000 ), 2 ) );
 			} elseif ( 3 < $amount_count_parts ) {
-				$human_format_amount = sprintf( esc_html__( '%s crore', 'give' ), round( ( $sanitize_amount / 10000000 ), 2 ) );
+				$human_format_amount = sprintf( esc_html__( '%s crore', 'give' ), round( ( $unformat_amount / 10000000 ), 2 ) );
 			} elseif ( 2 < $amount_count_parts ) {
-				$human_format_amount = sprintf( esc_html__( '%s lakh', 'give' ), round( ( $sanitize_amount / 100000 ), 2 ) );
+				$human_format_amount = sprintf( esc_html__( '%s lakh', 'give' ), round( ( $unformat_amount / 100000 ), 2 ) );
 			}
 			break;
 		default:
 			// Calculate large number formatted amount.
 			if ( 4 < $amount_count_parts ) {
-				$human_format_amount = sprintf( esc_html__( '%s trillion', 'give' ), round( ( $sanitize_amount / 1000000000000 ), 2 ) );
+				$human_format_amount = sprintf( esc_html__( '%s trillion', 'give' ), round( ( $unformat_amount / 1000000000000 ), 2 ) );
 			} elseif ( 3 < $amount_count_parts ) {
-				$human_format_amount = sprintf( esc_html__( '%s billion', 'give' ), round( ( $sanitize_amount / 1000000000 ), 2 ) );
+				$human_format_amount = sprintf( esc_html__( '%s billion', 'give' ), round( ( $unformat_amount / 1000000000 ), 2 ) );
 			} elseif ( 2 < $amount_count_parts ) {
-				$human_format_amount = sprintf( esc_html__( '%s million', 'give' ), round( ( $sanitize_amount / 1000000 ), 2 ) );
+				$human_format_amount = sprintf( esc_html__( '%s million', 'give' ), round( ( $unformat_amount / 1000000 ), 2 ) );
 			}
 	}
 
-	return apply_filters( 'give_human_format_large_amount', $human_format_amount, $amount, $sanitize_amount );
+	return apply_filters( 'give_human_format_large_amount', $human_format_amount, $amount, $unformat_amount );
 }
 
 /**
