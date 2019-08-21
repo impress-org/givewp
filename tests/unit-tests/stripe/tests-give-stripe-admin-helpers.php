fcae461ac213b393f5542ef95da82b8d7f6f53d3
Mehul Gohil <mehul.gohil0810@gmail.com>
2019-08-21 20:24:46 +0530

    feat: update admin helper unit tests

--- tests/unit-tests/stripe/tests-give-stripe-admin-helpers.php
+++ tests/unit-tests/stripe/tests-give-stripe-admin-helpers.php
@@ -7,6 +7,30 @@
 class Tests_Give_Stripe_Admin_Helpers extends Give_Unit_Test_Case {
 
 	/**
+	 * List of enabled gateways.
+	 *
+	 * @since  2.5.5
+	 * @access public
+	 *
+	 * @var $gateways
+	 */
+	public $gateways;
+
+	/**
+	 * Setup required variables.
+	 *
+	 * @since  2.5.5
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public function setUp() {
+		parent::setUp();
+
+		$this->gateways = give_get_option( 'gateways', array() );
+	}
+
+	/**
 	 * Unit test for function give_stripe_is_connected();
 	 *
 	 * @since 2.5.0
@@ -62,10 +86,6 @@ class Tests_Give_Stripe_Admin_Helpers extends Give_Unit_Test_Case {
 	 */
 	public function test_give_stripe_is_any_payment_method_active() {
 
-		// Default setup will have Stripe active.
-		$give_settings = give_get_settings();
-		$gateways      = $give_settings['gateways'];
-
 		/**
 		 * Case 1: By default Stripe CC is enabled, so this fn will return true.
 		 */
@@ -73,17 +93,10 @@ class Tests_Give_Stripe_Admin_Helpers extends Give_Unit_Test_Case {
 		$this->assertTrue( $is_stripe_active );
 
 		/**
-		 * Case 2: Ensure Stripe is not active when all the Stripe supported payment methods are disabled.
+		 * Case 2: Ensure Stripe is active when Stripe ACH is enabled.
 		 */
-		unset( $gateways['stripe'] );
-		give_update_option( 'gateways', $gateways );
-		$is_stripe_active = give_stripe_is_any_payment_method_active();
-		$this->assertFalse( $is_stripe_active );
-
-		/**
-		 * Case 3: Ensure Stripe is active when Stripe ACH is enabled.
-		 */
-		$gateways = array_merge( $gateways, array(
+		unset( $this->gateways['stripe'] );
+		$gateways = array_merge( $this->gateways, array(
 				'stripe_ach' => 1,
 			)
 		);
@@ -92,10 +105,10 @@ class Tests_Give_Stripe_Admin_Helpers extends Give_Unit_Test_Case {
 		$this->assertTrue( $is_stripe_active );
 
 		/**
-		 * Case 4: Ensure Stripe is active when Stripe iDEAL is enabled.
+		 * Case 3: Ensure Stripe is active when Stripe iDEAL is enabled.
 		 */
-		unset( $gateways['stripe_ach'] );
-		$gateways = array_merge( $gateways, array(
+		unset( $this->gateways['stripe'] );
+		$gateways = array_merge( $this->gateways, array(
 				'stripe_ideal' => 1,
 			)
 		);
@@ -104,10 +117,10 @@ class Tests_Give_Stripe_Admin_Helpers extends Give_Unit_Test_Case {
 		$this->assertTrue( $is_stripe_active );
 
 		/**
-		 * Case 5: Ensure Stripe is active when Stripe Google Pay is enabled.
+		 * Case 4: Ensure Stripe is active when Stripe Google Pay is enabled.
 		 */
-		unset( $gateways['stripe_ideal'] );
-		$gateways = array_merge( $gateways, array(
+		unset( $this->gateways['stripe'] );
+		$gateways = array_merge( $this->gateways, array(
 				'stripe_google_pay' => 1,
 			)
 		);
@@ -116,15 +129,23 @@ class Tests_Give_Stripe_Admin_Helpers extends Give_Unit_Test_Case {
 		$this->assertTrue( $is_stripe_active );
 
 		/**
-		 * Case 6: Ensure Stripe is active when Stripe Apple Pay is enabled.
+		 * Case 5: Ensure Stripe is active when Stripe Apple Pay is enabled.
 		 */
-		unset( $gateways['stripe_google_pay'] );
-		$gateways = array_merge( $gateways, array(
+		unset( $this->gateways['stripe'] );
+		$gateways = array_merge( $this->gateways, array(
 				'stripe_apple_pay' => 1,
 			)
 		);
 		give_update_option( 'gateways', $gateways );
 		$is_stripe_active = give_stripe_is_any_payment_method_active();
 		$this->assertTrue( $is_stripe_active );
+
+		/**
+		 * Case 6: Ensure Stripe is not active when all the Stripe supported payment methods are disabled.
+		 */
+		unset( $this->gateways['stripe'] );
+		give_update_option( 'gateways', $this->gateways );
+		$is_stripe_active = give_stripe_is_any_payment_method_active();
+		$this->assertFalse( $is_stripe_active );
 	}
 }
