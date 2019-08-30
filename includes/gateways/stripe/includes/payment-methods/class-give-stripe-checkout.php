8900c8561caccae7445e0d5dbbcd8a5368932dc7
Mehul Gohil <mehul.gohil0810@gmail.com>
2019-08-30 17:40:55 +0530

    fix: add processing donation overlay on redirect to checkout fn

--- includes/gateways/stripe/includes/payment-methods/class-give-stripe-checkout.php
+++ includes/gateways/stripe/includes/payment-methods/class-give-stripe-checkout.php
@@ -283,10 +283,26 @@ if ( ! class_exists( 'Give_Stripe_Checkout' ) ) {
 				return;
 			}
 			?>
+			<div id="give-stripe-checkout-processing"></div>
 			<script>
                 const stripe = Stripe( '<?php echo $publishable_key; ?>', {
                     'stripeAccount': '<?php echo $stripe_account_id; ?>'
                 } );
+                const processingHtml = document.querySelector( '#give-stripe-checkout-processing');
+
+                // Show Processing Donation Overlay.
+                processingHtml.setAttribute( 'class', 'stripe-checkout-process' );
+                processingHtml.style.background = '#FFFFFF';
+                processingHtml.style.opacity = '0.75';
+                processingHtml.style.position = 'fixed';
+                processingHtml.style.top = '0';
+                processingHtml.style.left = '0';
+                processingHtml.style.bottom = '0';
+                processingHtml.style.right = '0';
+                processingHtml.style.zIndex = '2147483646';
+                processingHtml.innerHTML = '<div class="give-stripe-checkout-processing-container" style="position: absolute;top: 50%;left: 50%;width: 300px; margin-left: -150px; text-align:center;"><div style="display:inline-block;"><span class="give-loading-animation" style="color: #333;height:26px;width:26px;font-size:26px; margin:0; "></span><span style="color:#333; font-size: 18px; margin:0 0 0 10px;">' + give_stripe_vars.checkout_processing_text + '</span></div></div>';
+
+                // Redirect donor to Checkout page.
                 stripe.redirectToCheckout({
                     // Make the id field from the Checkout Session creation API response
                     // available to this file, so you can provide it as parameter here
