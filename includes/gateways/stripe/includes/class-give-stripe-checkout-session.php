79e27d75b3ede9d211bb9d7b35a740017cdd4e0d
Mehul Gohil <mehul.gohil0810@gmail.com>
2019-09-02 18:21:51 +0530

    feat: add fn to retrieve the checkout session

--- includes/gateways/stripe/includes/class-give-stripe-checkout-session.php
+++ includes/gateways/stripe/includes/class-give-stripe-checkout-session.php
@@ -63,4 +63,44 @@ class Give_Stripe_Checkout_Session {
 
 		return false;
 	}
+
+	/**
+	 * This function is used to retrieve the Checkout session by using Checkout session ID.
+	 *
+	 * @param int $id Checkout Session ID.
+	 *
+	 * @since  2.5.5
+	 * @access public
+	 *
+	 * @return \Stripe\Checkout\Session|bool
+	 */
+	public function retrieve( $id ) {
+
+		try {
+
+			// Process Checkout session.
+			$session = \Stripe\Checkout\Session::retrieve(
+				$id,
+				give_stripe_get_connected_account_options()
+			);
+
+			// Return Checkout Session Object.
+			return $session;
+
+		} catch ( Exception $e ) {
+
+			give_record_gateway_error(
+				__( 'Stripe Error', 'give' ),
+				sprintf(
+					/* translators: %s Exception Message Body */
+					__( 'The Stripe Gateway returned an error while retrieving the Checkout Session. Details: %s', 'give' ),
+					$e
+				)
+			);
+			give_set_error( 'stripe_error', __( 'An occurred while processing the donation with the gateway. Please try your donation again.', 'give' ) );
+			give_send_back_to_checkout( '?payment-mode=' . give_clean( $_GET['payment-mode'] ) );
+		}
+
+		return false;
+	}
 }
