8d40d8a2de3cdd89b4a407b74a918cd80e072037
Mehul Gohil <mehul.gohil0810@gmail.com>
2019-09-02 18:16:37 +0530

    feat: add separate file to handle and create checkout session

---
+++ includes/gateways/stripe/includes/class-give-stripe-checkout-session.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Give - Stripe | Checkout Session API
+ *
+ * @package    Give
+ * @subpackage Stripe Core
+ * @copyright  Copyright (c) 2019, GiveWP
+ * @license    https://opensource.org/licenses/gpl-license GNU Public License
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Class Give_Stripe_Checkout_Session.
+ *
+ * @since 2.5.5
+ */
+class Give_Stripe_Checkout_Session {
+
+	/**
+	 * This function is used to create a new Checkout session.
+	 *
+	 * @param array $data List of arguments to create Checkout session.
+	 *
+	 * @since  2.5.5
+	 * @access public
+	 *
+	 * @return \Stripe\Checkout\Session|bool
+	 */
+	public function create( $args ) {
+
+		try {
+			/**
+			 * This filter will be used to modify create checkout arguments.
+			 *
+			 * @since 2.5.5
+			 */
+			$args = apply_filters( 'give_stripe_create_checkout_session_args', $args );
+
+			// Process Checkout session.
+			$session = \Stripe\Checkout\Session::create(
+				$args,
+				give_stripe_get_connected_account_options()
+			);
+
+			return $session;
+		} catch ( Exception $e ) {
+
+			give_record_gateway_error(
+				__( 'Stripe Error', 'give' ),
+				sprintf(
+					/* translators: %s Exception Message Body */
+					__( 'The Stripe Gateway returned an error while creating the Checkout Session. Details: %s', 'give' ),
+					$e
+				)
+			);
+			give_set_error( 'stripe_error', __( 'An occurred while processing the donation with the gateway. Please try your donation again.', 'give' ) );
+			give_send_back_to_checkout( '?payment-mode=' . give_clean( $_GET['payment-mode'] ) );
+		}
+
+		return false;
+	}
+}
