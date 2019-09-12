2b9ccf62239821bc41209ed9a4ca4bf8b0f28f80
Mehul Gohil <mehul.gohil0810@gmail.com>
2019-09-12 14:00:30 +0530

    fix: add new fn and use it for stripe localize vard

--- includes/gateways/stripe/includes/give-stripe-helpers.php
+++ includes/gateways/stripe/includes/give-stripe-helpers.php
@@ -1123,3 +1123,61 @@ function give_stripe_prepare_metadata( $donation_id = 0 ) {
 
 	return $args;
 }
+
+/**
+ * This function will be used to get localize variables to be passed into any of the scripts.
+ *
+ * @since 2.5.5
+ *
+ * @return array
+ */
+function give_stripe_get_localize_vars() {
+
+	// Get publishable key.
+	$publishable_key = give_stripe_get_publishable_key();
+
+	// Checkout options.
+	// @TODO: convert checkboxes to radios.
+	$zip_option      = give_is_setting_enabled( give_get_option( 'stripe_checkout_zip_verify' ) );
+	$remember_option = give_is_setting_enabled( give_get_option( 'stripe_checkout_remember_me' ) );
+
+	$stripe_card_update = false;
+	$get_data           = give_clean( filter_input_array( INPUT_GET ) );
+
+	if ( isset( $get_data['action'] ) &&
+		'update' === $get_data['action'] &&
+		isset( $get_data['subscription_id'] ) &&
+		is_numeric( $get_data['subscription_id'] )
+	) {
+		$stripe_card_update = true;
+	}
+
+	return apply_filters( 'give_stripe_get_localize_vars', array(
+		'zero_based_currency'          => give_is_zero_based_currency(),
+		'zero_based_currencies_list'   => give_get_zero_based_currencies(),
+		'sitename'                     => give_get_option( 'stripe_checkout_name' ),
+		'publishable_key'              => $publishable_key,
+		'checkout_image'               => give_get_option( 'stripe_checkout_image' ),
+		'checkout_address'             => give_get_option( 'stripe_collect_billing' ),
+		'checkout_processing_text'     => give_get_option( 'stripe_checkout_processing_text', __( 'Donation Processing...', 'give' ) ),
+		'zipcode_option'               => $zip_option,
+		'remember_option'              => $remember_option,
+		'give_version'                 => get_option( 'give_version' ),
+		'cc_fields_format'             => give_get_option( 'stripe_cc_fields_format', 'multi' ),
+		'card_number_placeholder_text' => __( 'Card Number', 'give' ),
+		'card_cvc_placeholder_text'    => __( 'CVC', 'give' ),
+		'donate_button_text'           => __( 'Donate Now', 'give' ),
+		'element_font_styles'          => give_stripe_get_element_font_styles(),
+		'element_base_styles'          => give_stripe_get_element_base_styles(),
+		'element_complete_styles'      => give_stripe_get_element_complete_styles(),
+		'element_empty_styles'         => give_stripe_get_element_empty_styles(),
+		'element_invalid_styles'       => give_stripe_get_element_invalid_styles(),
+		'float_labels'                 => give_is_float_labels_enabled( array(
+			'form_id' => get_the_ID(),
+		) ),
+		'base_country'                 => give_get_option( 'base_country' ),
+		'stripe_card_update'           => $stripe_card_update,
+		'stripe_account_id'            => give_stripe_is_connected() ? give_get_option( 'give_stripe_user_id' ) : false,
+		'preferred_locale'             => give_stripe_get_preferred_locale(),
+	) );
+}
