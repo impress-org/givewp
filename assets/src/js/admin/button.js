901af05002a90217ea3a7855e46ad9809815d2d5
Henry Holtgeerts <henryholtgeerts@gmail.com>
2019-12-04 10:14:45 -0500

    fix: retain span in shortcode button on update
    -Prevent button.js from removing span tag from inside button if span has class "give-button-text"
    -Adjust width styling for shortcode button to hide text at a larger screen size

--- assets/src/js/admin/button.js
+++ assets/src/js/admin/button.js
@@ -21,7 +21,8 @@ class GiveButton {
 
 	setBusyState() {
 		const busyIcon      = '<span class="dashicons dashicons-marker"></span>';
-		this.root.innerHTML = `${busyIcon} ${this.buttonText}`;
+		const text = this.root.querySelector('.give-button-text') ? `<span class="${this.buttonText}"></span>` : this.buttonText;
+		this.root.innerHTML = `${busyIcon} ${text}`;
 		this.disable();
 	}
 
@@ -35,11 +36,12 @@ class GiveButton {
 
 	updateIcon( className, position = 'before' ) {
 		const icon = `<span class="${className}"></span>`;
-
+		const text = this.root.querySelector('.give-button-text') ? `<span class="give-button-text">${this.buttonText}</span>` : this.buttonText;
+		
 		if ( 'after' === position ) {
-			this.root.innerHTML = `${this.buttonText} ${icon}`;
+			this.root.innerHTML = `${text} ${icon}`;
 		} else {
-			this.root.innerHTML = `${icon} ${this.buttonText}`;
+			this.root.innerHTML = `${icon} ${text}`;
 		}
 
 		this.iconClassName = className;
