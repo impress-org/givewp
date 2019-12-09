a90e49b0e70ab6c73610d0220e689b498ade39c3
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2019-12-10 00:33:05 +0530

    chore: send donor register email if has valid user

--- includes/admin/emails/class-donor-register-email.php
+++ includes/admin/emails/class-donor-register-email.php
@@ -107,6 +107,12 @@ if ( ! class_exists( 'Give_Donor_Register_Email' ) ) :
 		 * @access public
 		 */
 		public function setup_email_notification( $user_id, $user_data ) {
+			if ( ! is_int( $user_id ) || ! $user_id ) {
+				wp_die( esc_html__( 'Cheatin&#8217; uh?', 'give' ), esc_html__( 'Error', 'give' ), array(
+					'response' => 400,
+				) );
+			}
+
 			$this->setup_email_data();
 
 			$this->recipient_email = $user_data['email'];
