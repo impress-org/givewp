f361b53163719ea73b29c9f535309e722d75a95d
Henry Holtgeerts <henryholtgeerts@gmail.com>
2019-12-13 14:07:30 -0500

    fix: allow App component to render
    Bring back the App component, despite all the errors it throws

--- assets/src/js/admin/reports/index.js
+++ assets/src/js/admin/reports/index.js
@@ -1,9 +1,11 @@
-//import App from './app/index.js'
+import App from './app/index.js'
 import { HashRouter as Router } from "react-router-dom";
 import React from 'react';
 import ReactDOM from 'react-dom'
 
 ReactDOM.render(
-    <h1>Prevent App</h1>,
+    <Router>
+        <App/>
+    </Router>,
     document.getElementById('reports-app')
 );
