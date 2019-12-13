e9187bac5547b38b7ebc173a0d078ecb79b0becc
Henry Holtgeerts <henryholtgeerts@gmail.com>
2019-12-13 14:12:16 -0500

    fix: remove giveReportsData references from Tabs component

--- assets/src/js/admin/reports/components/tabs/index.js
+++ assets/src/js/admin/reports/components/tabs/index.js
@@ -1,16 +1,11 @@
 import Tab from '../tab'
 
 const Tabs = (props) => {
-    const links = Object.values(props.pages).map((page, index) => {
-        if (page.show_in_menu === true) {
-            return (
-                <Tab to={page.path} key={index}>{page.title}</Tab>
-            )
-        }
-    })
     return (
         <div className='nav-tab-wrapper give-nav-tab-wrapper'>
-            {links}
+            <Tab to='/'>
+                Overview
+            </Tab>
         </div>
     )
 }
