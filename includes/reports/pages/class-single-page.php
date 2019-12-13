bda16eac190f96610172c18273ebd074038982f7
Henry Holtgeerts <henryholtgeerts@gmail.com>
2019-12-13 12:35:32 -0500

    refactor: remove Card class
    Removed Card class and all references to Card class

--- includes/reports/pages/class-single-page.php
+++ includes/reports/pages/class-single-page.php
@@ -16,48 +16,11 @@ class Single_Page extends Page {
 
     public function __construct() {
 
-        require_once GIVE_PLUGIN_DIR . 'includes/reports/cards/class-card.php';
         require_once GIVE_PLUGIN_DIR . 'includes/reports/pages/class-page.php';
 
         $this->title = 'Single';
         $this->show_in_menu = false;
         $this->path = '/single';
-        $this->cards = [
-            'donations_for_period' => new Card([
-                'title' => 'Donations For Period',
-                'type' => 'line',
-                'width' => 12,
-                'props' => [
-                    [
-                        'label' => 'Total Raised',
-                        'data' => ''
-                    ],
-                    [
-                        'label' => 'Total Donors',
-                        'data' => ''
-                    ],
-                    [
-                        'label' => 'Average Donation',
-                        'data' => ''
-                    ],
-                    [
-                        'label' => 'Total Refunded',
-                        'data' => ''
-                    ]
-                ]
-            ]),
-            'campaign_performance' => new Card([
-                'title' => 'Campaign Performance',
-                'type' => 'doughnut',
-                'width' => 6,
-                'props' => ''
-            ]),
-            'payment_statuses' => new Card([
-                'title' => 'Payment Statuses',
-                'type' => 'bar',
-                'width' => 6,
-                'props' => ''
-            ]),
-        ];
+        $this->cards = [];
     }
 }
