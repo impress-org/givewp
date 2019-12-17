0a7496d105d97720193971acb6b21a703acad453
Henry Holtgeerts <henryholtgeerts@gmail.com>
2019-12-16 18:48:34 -0800

    feature: set chart styles from color palette

--- assets/src/js/admin/reports/components/chart/utils/index.js
+++ assets/src/js/admin/reports/components/chart/utils/index.js
@@ -1,15 +1,15 @@
-export function formatData (data) {
+export function formatData (type, data) {
 
     const formattedLabels = data.labels
 
     const formattedDatasets = data.datasets.map((dataset) => {
-        //const styles = createStyles(props)
+        const styles = createStyles(type, dataset.data)
         const formatted = {
             label: dataset.label,
             data: dataset.data,
-            backgroundColor: ['rgba(105, 184, 104, 0.21)'],
-            borderColor: ['rgba(105, 184, 104, 1)'],
-            borderWidth: 3
+            backgroundColor: styles.backgroundColor,
+            borderColor: styles.borderColor,
+            borderWidth: styles.borderWidth
         }
         return formatted
     })
@@ -22,18 +22,34 @@ export function formatData (data) {
     return formattedData
 }
 
-function createStyles (props) {
+function createStyles (type, data) {
+
+    const palette = [
+        '#69B868',
+        '#F49420',
+        '#D75A4B',
+        '#556E79',
+        '#9EA3A8'
+    ]
+
+    const area = ['#69B86844']
+    const line = ['#69B868']
+
+    const backgroundColor = type === 'line' ? area : palette
+    const borderColor = type === 'line' ? line : palette
+    const borderWidth = type === 'line' ? 3 : 0
+
     const styles = {
-        backgroundColor: ['rgba(105, 184, 104, 0.21)'],
-        borderColor: ['rgba(105, 184, 104, 1)'],
-        borderWidth: 3
+        backgroundColor,
+        borderColor,
+        borderWidth
     }
 
     return styles
 }
 
 export function createConfig (type, data) {
-    const formattedData = formatData(data)
+    const formattedData = formatData(type, data)
     const config = {
         type: type,
         data: formattedData,
