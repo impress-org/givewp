b2a1c2ed335529ae45a224efd695a3ccf7bcdfca
Henry Holtgeerts <henryholtgeerts@gmail.com>
2019-12-18 10:08:09 -0800

    fix: improve donot item styles

--- assets/src/js/admin/reports/components/donor-item/index.js
+++ assets/src/js/admin/reports/components/donor-item/index.js
@@ -2,26 +2,29 @@ const DonorItem = ({image, name, email, count, total}) => {
     
     const itemStyle = {
         height: '72px',
-        display: 'flex'
+        display: 'flex',
+        alignItems: 'center'
     }
 
     const imageStyle = {
         height: '36px',
         width: '36px',
+        background: '#CCCCCC',
         borderRadius: '50%',
-        objectFit: 'cover'
+        objectFit: 'cover',
+        overflow: 'hidden'
     }
 
     return (
         <div style={itemStyle}>
-            <img src={image}/>
-            <div style={{flex: 1}}>
-                <p><strong>{name}</strong></p>
-                <p>{email}</p>
+            <img src={image} style={imageStyle}/>
+            <div style={{flex: 1, marginLeft: '16px'}}>
+                <p style={{margin: 0}}><strong>{name}</strong></p>
+                <p style={{margin: 0}}>{email}</p>
             </div>
-            <div>
-                <p>{count}</p>
-                <p>{total}</p>
+            <div style={{textAlign: 'right'}}>
+                <p style={{margin: 0}}>{count}</p>
+                <p style={{margin: 0}}>{total}</p>
             </div>
         </div>
     )
