2ee75840dc0c2bfb30535d12e35a04f4d6bb92c1
Henry Holtgeerts <henryholtgeerts@gmail.com>
2019-12-17 16:11:43 -0800

    feature: log event on list scroll

--- assets/src/js/admin/reports/components/list/index.js
+++ assets/src/js/admin/reports/components/list/index.js
@@ -1,6 +1,27 @@
-const List = (props) => {
+import { useEffect } from 'react'
+
+const List = ({onApproachScrolleEnd, children}) => {
+
+    const list = createRef()
+
+    useEffect(() => {
+
+        function checkScroll (evt) {
+            console.log('check scroll!', evt)
+        }
+        
+        list.current.addEventListener('scroll', checkScroll)
+    
+        return function cleanup () {
+            list.current.removeEventListener('scroll', checkScroll)
+        }
+
+    }, [])
+
     return (
-        <h1>List goes here!</h1>
+        <div ref={list}>
+            {children}
+        </div>
     )
 }
 export default List
