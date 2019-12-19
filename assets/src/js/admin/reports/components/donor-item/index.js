309f24af3ae8ca9c4703e1aa5ad6098f60f8d60a
Henry Holtgeerts <henryholtgeerts@gmail.com>
2019-12-19 10:54:59 -0800

    refactor: move donor item styles to scss

--- assets/src/js/admin/reports/components/donor-item/index.js
+++ assets/src/js/admin/reports/components/donor-item/index.js
@@ -1,28 +1,15 @@
-const DonorItem = ({image, name, email, count, total}) => {
-    
-    const itemStyle = {
-        height: '72px',
-        display: 'flex',
-        alignItems: 'center'
-    }
+import './style.scss'
 
-    const imageStyle = {
-        height: '36px',
-        width: '36px',
-        background: '#CCCCCC',
-        borderRadius: '50%',
-        objectFit: 'cover',
-        overflow: 'hidden'
-    }
+const DonorItem = ({image, name, email, count, total}) => {
 
     return (
-        <div style={itemStyle}>
-            <img src={image} style={imageStyle}/>
-            <div style={{flex: 1, marginLeft: '16px'}}>
+        <div class='donor-item'>
+            <img src={image} />
+            <div>
                 <p style={{margin: 0}}><strong>{name}</strong></p>
                 <p style={{margin: 0}}>{email}</p>
             </div>
-            <div style={{textAlign: 'right'}}>
+            <div>
                 <p style={{margin: 0}}>{count}</p>
                 <p style={{margin: 0}}>{total}</p>
             </div>
