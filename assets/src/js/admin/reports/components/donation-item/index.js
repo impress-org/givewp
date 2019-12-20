fbd30c9efc3c09ff0490c518f98d5636241f878c
Henry Holtgeerts <henryholtgeerts@gmail.com>
2019-12-19 18:16:09 -0800

    feature: style DonationItem component

--- assets/src/js/admin/reports/components/donation-item/index.js
+++ assets/src/js/admin/reports/components/donation-item/index.js
@@ -6,21 +6,21 @@ const DonationItem = ({status, amount, time, donor, source}) => {
     const elapsed = '3 hours ago'
 
     return (
-        <div>
-            <div>
+        <div className='donation-item'>
+            <div className='icon'>
                 {icon}
             </div>
-            <div>
+            <div className='info'>
                 <p>
-                    <span>{amount}</span>
-                    <span>{status}</span>
-                    <span>{elapsed}</span>
+                    <span className='amount'>{amount}</span> 
+                    <span className='status'>{status}</span> 
+                    <span className='elapsed'>{elapsed}</span>
                 </p>
                 <p>
                     {donor.name}<strong>(#{donor.id})</strong>
                 </p>
                 <p>
-                    <span>{source}</span>
+                    <span className='source'>{source}</span>
                 </p>
             </div>
         </div>
