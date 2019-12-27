251e1999dbba63fc6e462f5910e6d8bb50d1d2cf
Henry Holtgeerts <henryholtgeerts@gmail.com>
2019-12-27 11:12:33 -0800

    feature: scaffold MiniChart component

---
+++ assets/src/js/admin/reports/components/mini-chart/index.js
@@ -0,0 +1,22 @@
+import Chart from '../chart'
+
+const MiniChart = ({title, type, data}) => {
+
+    const amount = '$200.00'
+    const percentage = -5
+
+    return (
+        <div className='givewp-mini-chart'>
+            <div className='header'>
+                <div className='title'>{title}</div>
+                <div className='percentage'>{percentage}</div>
+            </div>
+            <div className='content'>
+                <div className='amount'>{amount}</div>
+                <Chart type={type} data={data} aspectRatio={1} />
+            </div>
+        </div>
+    )
+}
+
+export default MiniChart
