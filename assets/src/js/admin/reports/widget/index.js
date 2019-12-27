e4cbb3884806ec06660077b28bf76f35a3ac1e6c
Henry Holtgeerts <henryholtgeerts@gmail.com>
2019-12-27 15:27:14 -0800

    feature: add dummy line chart to widget

--- assets/src/js/admin/reports/widget/index.js
+++ assets/src/js/admin/reports/widget/index.js
@@ -1,16 +1,29 @@
 import Grid from '../components/grid'
 import Card from '../components/card'
+import Chart from '../components/chart'
 
 const Widget = () => {
     return (
         <Grid>
             <Card width={12}>
-                <h1>Testing</h1>
+                <Chart
+                    type='line'
+                    aspectRatio={0.4}
+                    data={{
+                        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Jun', 'Jul'],
+                        datasets: [
+                            {
+                                label: 'Donations',
+                                data: [4, 5, 3, 7, 5, 6]
+                            },
+                        ]
+                    }}
+                />
             </Card>
-            <Card width={6}></Card>
-            <Card width={6}></Card>
-            <Card width={6}></Card>
-            <Card width={6}></Card>
+            <Card width={6}>Mini Chart here</Card>
+            <Card width={6}>Mini chart here</Card>
+            <Card width={6}>Mini chart here</Card>
+            <Card width={6}>Mini chart here</Card>
         </Grid>
     )
 }
