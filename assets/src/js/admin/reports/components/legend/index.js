59f7fac3c5c7d0730d150962e9b48ebce70fe5e6
Henry Holtgeerts <henryholtgeerts@gmail.com>
2019-12-30 10:04:13 -0800

    feature: add logic to calculate percent

--- assets/src/js/admin/reports/components/legend/index.js
+++ assets/src/js/admin/reports/components/legend/index.js
@@ -3,14 +3,17 @@ import { getColor } from './utils'
 
 const Legend = ({data}) => {
 
+    const total = data.datasets[0].data.reduce((a, b) => a + b)
+
     const items = data.labels.map((label, index) => {
         const color = getColor(index)
+        const percent = Math.round( ( data.datasets[0].data[index] / total ) * 100 ) + '%'
         return (
             <div className='item'>
                 <div className='bar' style={{background: color}}></div>
                 <p className='label'>{label}</p>
                 <p className='data'>{data.datasets[0].data[index]}</p>
-                <p className='percent'>40%</p>
+                <p className='percent'>{percent}</p>
             </div>
         )
     })
