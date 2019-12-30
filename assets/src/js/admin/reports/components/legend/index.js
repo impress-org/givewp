d3b62640931d10a6713454f46c47a50e6dff92ab
Henry Holtgeerts <henryholtgeerts@gmail.com>
2019-12-30 12:33:18 -0800

    chore: add inline documentaiton for Legend

--- assets/src/js/admin/reports/components/legend/index.js
+++ assets/src/js/admin/reports/components/legend/index.js
@@ -1,20 +1,33 @@
-import './style.scss'
-import { getColor } from './utils'
+// Dependencies
 import PropTypes from 'prop-types'
+
+// Utilities
+import { getColor } from './utils'
+
+// Styles
+import './style.scss'
 
 const Legend = ({data, chartRef}) => {
 
+    // Calculate total value of points in dataset (used to find percentages)
     const total = data.datasets[0].data.reduce((a, b) => a + b)
 
+    // Hide a given datapoint in the chart and the coresponding legend item
     const hideData = (evt, index, value) => {
         evt.target.closest('.item').classList.toggle('inactive')
         chartRef.data.datasets[0].data[index] = chartRef.data.datasets[0].data[index] === value ? null : value
         chartRef.update()
     }
 
+    // Map data labels to build legend items
     const items = data.labels.map((label, index) => {
+
+        // Use ulitity function to match item color to chart color
         const color = getColor(index)
+
+        // Prepare percent to display with legend item
         const percent = Math.round( ( data.datasets[0].data[index] / total ) * 100 ) + '%'
+
         return (
             <div className='item' onClick={(evt) => hideData(evt, index, data.datasets[0].data[index])}>
                 <div className='bar' style={{background: color}}></div>
