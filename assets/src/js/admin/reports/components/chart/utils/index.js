6d4485954236772d9e46ca7957fb662fea4dd823
Henry Holtgeerts <henryholtgeerts@gmail.com>
2019-12-30 12:41:51 -0800

    chore: add inline documentaiton for Chart utils

--- assets/src/js/admin/reports/components/chart/utils/index.js
+++ assets/src/js/admin/reports/components/chart/utils/index.js
@@ -1,9 +1,13 @@
+// Format data from Reports API for ChartJS
 export function formatData (type, data) {
 
-    const formattedLabels = data.labels
+    const formattedLabels = data.labels.slice(0)
 
     const formattedDatasets = data.datasets.map((dataset, index) => {
+
+        // Setup styles
         const styles = createStyles(type, dataset.data, index)
+
         const formatted = {
             label: (' ' + dataset.label).slice(1),
             data: dataset.data.slice(0),
@@ -22,6 +26,8 @@ export function formatData (type, data) {
     return formattedData
 }
 
+// Create chart styles from predifined pallette, 
+// depending on chart type
 function createStyles (type, data, index) {
 
     const palette = [
@@ -38,6 +44,7 @@ function createStyles (type, data, index) {
         borderWidth: 0
     }
 
+    // Handle special styles needed for 'line' and 'doughnut' charts
     switch (type) {
         case 'line':
             styles.backgroundColor = [
@@ -56,6 +63,7 @@ function createStyles (type, data, index) {
     return styles
 }
 
+// Return config object for ChartJS
 export function createConfig (type, data) {
     const formattedData = formatData(type, data)
     let config = {
@@ -71,6 +79,7 @@ export function createConfig (type, data) {
         }
     }
 
+    // Setup yAxes to begin at zero if chart is 'line' or 'bar'
     if (type === 'line' || type === 'bar') {
         config.options.scales = {
             yAxes: [{
