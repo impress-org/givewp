6ca0f28d7af9ff797b773d0dec7d73a4e3271d3e
Henry Holtgeerts <henryholtgeerts@gmail.com>
2019-12-30 12:20:08 -0800

    feature: define Chart prop types

--- assets/src/js/admin/reports/components/chart/index.js
+++ assets/src/js/admin/reports/components/chart/index.js
@@ -1,6 +1,7 @@
 import ChartJS from 'chart.js'
 import { useEffect, useState, createRef } from 'react'
 import { createConfig, calcHeight } from './utils'
+import PropTypes from 'prop-types'
 
 import Legend from '../legend'
 
@@ -31,4 +32,16 @@ const Chart = ({type, aspectRatio, data, showLegend}) => {
         </div>
     )
 }
+
+Chart.propTypes = {
+    // Chart type (see https://www.chartjs.org/docs/2.8.0/charts/)
+    type: PropTypes.string.isRequired,
+    // Aspect ratio used to display chart (number:1)
+    aspectRatio: PropTypes.number.isRequired
+    // Data object provided by Reports API
+    data: PropTypes.object.isRequired,
+    // Whether or not to display a legend for Chart (default to true)
+    showLegend: PropTypes.bool
+}
+
 export default Chart
