49e077b24349669d7789dc3a0944b8847c476358
Henry Holtgeerts <henryholtgeerts@gmail.com>
2019-12-31 12:41:21 -0800

    chore: cleanup Tab component

--- assets/src/js/admin/reports/components/tab/index.js
+++ assets/src/js/admin/reports/components/tab/index.js
@@ -1,13 +1,19 @@
 import { Link, useRouteMatch } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
-const Tab = ({to, exact, children}) => {
+const Tab = ({to, children}) => {
+
     const match = useRouteMatch({
         path: to,
         exact: true
     })
+
     const classList = match ? 'nav-tab nav-tab-active' : 'nav-tab'
+
     return (
-        <Link to={to} exact={exact} className={classList}>{children}</Link>
+        <Link to={to} className={classList}>{children}</Link>
     )
+
 }
+
 export default Tab
