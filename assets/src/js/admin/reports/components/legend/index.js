1eb2e3c59d80a9950b8ddc5d0ad98240b72fa148
Henry Holtgeerts <henryholtgeerts@gmail.com>
2019-12-30 16:16:37 -0800

    fix: use useState in Legend to avoid directly manipulating the DOM

--- assets/src/js/admin/reports/components/legend/index.js
+++ assets/src/js/admin/reports/components/legend/index.js
@@ -1,5 +1,6 @@
 // Dependencies
 import PropTypes from 'prop-types'
+import { useState } from 'react'
 
 // Utilities
 import { getColor } from './utils'
@@ -12,15 +13,31 @@ const Legend = ({data, chartRef}) => {
     // Calculate total value of points in dataset (used to find percentages)
     const total = data.datasets[0].data.reduce((a, b) => a + b)
 
+    const [inactiveItems, setInactiveItems] = useState([])
+
     // Hide a given datapoint in the chart and the coresponding legend item
-    const hideData = (evt, index, value) => {
-        evt.target.closest('.item').classList.toggle('inactive')
-        chartRef.data.datasets[0].data[index] = chartRef.data.datasets[0].data[index] === value ? null : value
+    const hideData = (index, value) => {
+        let inactive = inactiveItems.slice(0)
+        console.log('hide data!')
+        if (chartRef.data.datasets[0].data[index] === value) {
+            chartRef.data.datasets[0].data[index] = null
+            inactive.push(index)
+            setInactiveItems(inactive)
+        } else {
+            chartRef.data.datasets[0].data[index] = value
+            const foundIndex = inactive.indexOf(index)
+            inactive.splice(foundIndex, 1)
+            setInactiveItems(inactive)
+        }
+        //chartRef.data.datasets[0].data[index] = chartRef.data.datasets[0].data[index] === value ? null : value
         chartRef.update()
     }
 
     // Map data labels to build legend items
     const items = data.labels.map((label, index) => {
+
+        // Prepare status class for item
+        const status = inactiveItems.includes(index) ? ' inactive' : ''
 
         // Use ulitity function to match item color to chart color
         const color = getColor(index)
@@ -29,7 +46,7 @@ const Legend = ({data, chartRef}) => {
         const percent = Math.round( ( data.datasets[0].data[index] / total ) * 100 ) + '%'
 
         return (
-            <div className='item' onClick={(evt) => hideData(evt, index, data.datasets[0].data[index])}>
+            <div className={'item' + status} key={index} onClick={() => hideData(index, data.datasets[0].data[index])}>
                 <div className='bar' style={{background: color}}></div>
                 <p className='label'>{label}</p>
                 <p className='data'>{data.datasets[0].data[index]}</p>
