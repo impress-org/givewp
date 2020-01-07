a57e58095275b1ce00220c7cf1f65981f6486c8a
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-01-07 12:49:26 -0500

    refactor: remove extra boilerplate

--- src/Reports/API.php
+++ src/Reports/API.php
@@ -35,34 +35,12 @@ class API {
 			'callback' => [__CLASS__, 'handle_report_callback'],
 		));
 
-		register_rest_route( 'givewp/v3', '/reports/page=(?P<page>[a-zA-Z0-9-]+)/', array(
-			'methods' => 'GET',
-			'callback' => [__CLASS__, 'handle_page_callback'],
-		));
-
-		register_rest_route( 'givewp/v3', '/reports/single=(?P<page>[a-zA-Z0-9-]+)/', array(
-			'methods' => 'GET',
-			'callback' => [__CLASS__, 'handle_single_callback'],
-		));
-
 	}
 
 	//Return response for report API request
 	protected function handle_report_callback (WP_REST_Request $request) {
 		$report = self::$reports[$request['report']];
 		return $report->handle_api_callback($request);
-	}
-
-	//Return response for page API request
-	protected function handle_page_callback (WP_REST_Request $request) {
-		$page = self::$pages[$request['page']];
-		return $page->handle_api_callback($request);
-	}
-
-	//Return response for single API request
-	protected function handle_single_callback (WP_REST_Request $request) {
-		$page = self::$pages['single'];
-		return $page->handle_api_callback($request);
 	}
 
 }
