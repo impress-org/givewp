0c3e02f065e7fe51d312b59fee646057e580d8dd
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-01-13 16:30:34 -0500

    feature: scaffold FormPerformance endpoint

---
+++ src/API/Endpoints/Reports/FormPerformance.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Reports base endpoint
+ *
+ * @package Give
+ */
+
+namespace Give\API\Endpoints\Reports;
+
+class FormPerformance extends Endpoint {
+
+	public function __construct() {
+		$this->endpoint = 'form-performance';
+	}
+
+	public function get_report($request) {
+
+		// Setup args for give_count_payments
+		$args = [
+			'start-date' => $request['start'],
+			'end-date' => $request['end']
+		];
+
+		// Use give_count_payments logic to get payments
+		$payments = give_count_payments( $args );
+
+		// Add caching logic here...
+
+		return new \WP_REST_Response([
+			'data' => [
+				'labels' => [
+					'Completed',
+					'Pending',
+					'Refunded',
+					'Abandoned'
+				],
+				'datasets' => [
+					[
+						'label' => 'Payments',
+						'data' => [
+							$payments->completed,
+							$payments->pending,
+							$payments->refunded,
+							$payments->abandoned
+						]
+					]
+				],
+			]
+		]);
+	}
+}
