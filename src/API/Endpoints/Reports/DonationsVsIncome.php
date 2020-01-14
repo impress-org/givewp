6765f20375426147c193d4c93c5ff3eac0ec16a5
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-01-14 17:05:33 -0500

    feature: implement Give_Payment_Stats to source donations

--- src/API/Endpoints/Reports/DonationsVsIncome.php
+++ src/API/Endpoints/Reports/DonationsVsIncome.php
@@ -16,30 +16,82 @@ class DonationsVsIncome extends Endpoint {
 
 	public function get_report($request) {
 
+		$start = date_create($request['start']);
+		$end = date_create($request['end']);
+		$diff = date_diff($start, $end);
+
+		$data = [];
+
+		switch(true) {
+			case ($diff->days > 400):
+				$data = $this->get_data($start, $end, 'P1Y');
+				break;
+			case ($diff->days > 120):
+				$data = $this->get_data($start, $end, 'P1M');
+				break;
+			case ($diff->days > 30):
+				$data = $this->get_data($start, $end, 'P7D');
+				break;
+			case ($diff->days > 10):
+				$data = $this->get_data($start, $end, 'P3D');
+				break;
+			case ($diff->days > 2):
+				$data = $this->get_data($start, $end, 'P1D');
+				break;
+			case ($diff->days > 0):
+				$data = $this->get_data($start, $end, 'PT1H');
+				break;
+		}
+
 		// Add caching logic here...
 
 		return new \WP_REST_Response([
-			'data' => [
-				'donations' => $donations,
-				'income' => $income,
-				'labels' => [
-					'Completed',
-					'Pending',
-					'Refunded',
-					'Abandoned'
-				],
-				'datasets' => [
-					[
-						'label' => 'Payments',
-						'data' => [
-							$payments->completed,
-							$payments->pending,
-							$payments->refunded,
-							$payments->abandoned
-						]
-					]
-				],
-			]
+			'data' => $data
 		]);
+	}
+
+	public function get_data($start, $end, $interval) {
+
+		$stats = new \Give_Payment_Stats();
+
+		$labels = [];
+		$donations = [];
+		$income = [];
+		$periods = [];
+
+		$dateInterval = new \DateInterval($interval);
+		while ( $start < $end ) {
+
+			$periodStart = $start->format('Y-m-d H:i:s');
+			$periodEnd = $end->format('Y-m-d H:i:s');
+
+			$donationsForPeriod = $stats->get_sales( 0, $periodStart, $periodEnd );
+			$incomeForPeriod = $stats->get_earnings( 0, $periodS, $periodEnd );
+
+			array_push($donations, $donationsForPeriod);
+			array_push($income, $incomeForPeriod);
+			array_push($labels, 'label!');
+			array_push($periods, $periodStart);
+
+			date_add($start, $dateInterval);
+		}
+
+		$data = [
+			'periods' => $periods,
+			'labels' => $labels,
+			'datasets' => [
+				[
+					'label' => 'Donations',
+					'data' => $donations
+				],
+				[
+					'label' => 'Income',
+					'data' => $income
+				]
+			]
+		];
+
+		return $data;
+
 	}
 }
