4d29067acaa406cf9bd7863162c1534c2c1863ce
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-01-15 11:41:59 -0500

    fix: set periodEnd based on provided interval

--- src/API/Endpoints/Reports/DonationsVsIncome.php
+++ src/API/Endpoints/Reports/DonationsVsIncome.php
@@ -35,10 +35,13 @@ class DonationsVsIncome extends Endpoint {
 			case ($diff->days > 10):
 				$data = $this->get_data($start, $end, 'P3D');
 				break;
-			case ($diff->days > 2):
+			case ($diff->days > 4):
 				$data = $this->get_data($start, $end, 'P1D');
 				break;
-			case ($diff->days > 0):
+			case ($diff->days > 1):
+				$data = $this->get_data($start, $end, 'PT6H');
+				break;
+			case ($diff->days >= 0):
 				$data = $this->get_data($start, $end, 'PT1H');
 				break;
 		}
@@ -63,14 +66,19 @@ class DonationsVsIncome extends Endpoint {
 		while ( $start < $end ) {
 
 			$periodStart = $start->format('Y-m-d H:i:s');
-			$periodEnd = $end->format('Y-m-d H:i:s');
+
+			// Add interval to get period end
+			$periodEnd = clone $start;
+			date_add($periodEnd, $dateInterval);
+
+			$periodEnd = $periodEnd->format('Y-m-d H:i:s');
 
 			$donationsForPeriod = $stats->get_sales( 0, $periodStart, $periodEnd );
 			$incomeForPeriod = $stats->get_earnings( 0, $periodS, $periodEnd );
 
 			array_push($donations, $donationsForPeriod);
 			array_push($income, $incomeForPeriod);
-			array_push($labels, 'label!');
+			array_push($labels, $periodEnd);
 			array_push($periods, $periodStart);
 
 			date_add($start, $dateInterval);
