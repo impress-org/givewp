787fc5d481beb8aa16a4196c0032a83d47d5c4c9
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-01-15 16:04:27 -0500

    feature: scaffold RESTList HOC

---
+++ assets/src/js/admin/reports/components/rest-list/index.js
@@ -0,0 +1,80 @@
+// Vendor dependencies
+import axios from 'axios'
+import { useState, useEffect, Fragment } from 'react'
+import PropTypes from 'prop-types'
+
+// Components
+import List from '../list'
+import DonorItem from '../donor-item'
+import LocationItem from '../location-item'
+import DonationItem from '../donation-item'
+
+// Store-related dependencies
+import { useStoreValue } from '../../app/store'
+
+const RESTList = ({endpoint}) => {
+
+	// Use period from store
+	const [{ period }, dispatch] = useStoreValue()
+
+	// Use state to hold data fetched from API
+	const [fetched, setFetched] = useState(null)
+
+	// Fetch new data and update Chart when period changes
+	useEffect(() => {
+		axios.get(wpApiSettings.root + 'give-api/v2/reports/' + endpoint, {
+			params: {
+				start: period.startDate.format('YYYY-MM-DD'),
+				end: period.startDate.format('YYYY-MM-DD')
+			},
+			headers: {
+				'X-WP-Nonce': wpApiSettings.nonce
+			}
+		})
+		.then(function (response) {
+			console.log(response)
+			setFetched(response.data.data)
+		})
+	}, [period, endpoint])
+
+	const items = fetched.map((item, index) => {
+		switch (item.type) {
+			case 'donor':
+				return <DonorItem name={item.name} />
+			case 'donation':
+				return <DonationItem name={item.name} />
+			case 'location':
+				return <LocationItem name={item.name} />
+		}
+	})
+
+	return (
+		<Fragment>
+			{fetched && (
+				<List>
+					{items}
+				</List>
+			)}
+		</Fragment>
+	)
+}
+
+RESTChart.propTypes = {
+	// Chart type (ex: line)
+	type: PropTypes.string.isRequired,
+	// Chart aspect ratio
+	aspectRatio: PropTypes.number,
+	// API endpoint where data is fetched (ex: 'payment-statuses')
+	endpoint: PropTypes.string.isRequired,
+	// Display Chart with Legend
+	showLegend: PropTypes.bool
+}
+
+RESTChart.defaultProps = {
+	type: null,
+	aspectRatio: 0.6,
+	endpoint: null,
+	showLegend: false
+}
+
+export default RESTChart
