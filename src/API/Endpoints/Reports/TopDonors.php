1e4f16f72f11a33810f93aaa0a5ca552d43654c8
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-01-17 11:25:19 -0500

    feature: add donor overview link

--- src/API/Endpoints/Reports/TopDonors.php
+++ src/API/Endpoints/Reports/TopDonors.php
@@ -17,7 +17,7 @@ class TopDonors extends Endpoint {
 	public function get_report( $request ) {
 
 		$args = [
-			'number'     => 10,
+			'number'     => 25,
 			'paged'      => 1,
 			'orderby'    => 'purchase_value',
 			'order'      => 'DESC',
@@ -32,14 +32,15 @@ class TopDonors extends Endpoint {
 
 		foreach ( $donors as $donor ) {
 
-			$avatar = give_validate_gravatar( $donor->email ) ? get_avatar( $donor->email, 60 ) : null;
-			$total  = give_currency_filter( give_format_amount( $donor->purchase_value, array( 'sanitize' => false ) ), [ 'decode_currency' => true ] );
-
+			$avatar     = give_validate_gravatar( $donor->email ) ? get_avatar( $donor->email, 60 ) : null;
+			$total      = give_currency_filter( give_format_amount( $donor->purchase_value, array( 'sanitize' => false ) ), [ 'decode_currency' => true ] );
+			$url        = admin_url( 'edit.php?post_type=give_forms&page=give-donors&view=overview&id=' . absint( $donor->id ) );
 			$countLabel = $donor->purchase_count > 1 ? esc_html__( 'Donations', 'give' ) : esc_html__( 'Donation', 'give' );
 
 			$item = [
 				'type'  => 'donor',
 				'name'  => $donor->name,
+				'url'   => $url,
 				'count' => "{$donor->purchase_count} {$countLabel}",
 				'total' => $total,
 				'image' => $avatar,
