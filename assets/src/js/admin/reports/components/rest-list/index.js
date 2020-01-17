88aff26d3f193ef6d3346e1208f91e4f02c0ee54
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-01-17 09:44:25 -0500

    hotfix: check that start/end dates are defined before querying API

--- assets/src/js/admin/reports/components/rest-list/index.js
+++ assets/src/js/admin/reports/components/rest-list/index.js
@@ -1,93 +1,93 @@
 // Vendor dependencies
-import axios from 'axios'
-import { useState, useEffect, Fragment } from 'react'
-import PropTypes from 'prop-types'
+import axios from 'axios';
+import { useState, useEffect, Fragment } from 'react';
+import PropTypes from 'prop-types';
 
 // Components
-import List from '../list'
-import DonorItem from '../donor-item'
-import LocationItem from '../location-item'
-import DonationItem from '../donation-item'
+import List from '../list';
+import DonorItem from '../donor-item';
+import LocationItem from '../location-item';
+import DonationItem from '../donation-item';
 
 // Store-related dependencies
-import { useStoreValue } from '../../app/store'
+import { useStoreValue } from '../../app/store';
 
-const RESTList = ({endpoint}) => {
-
+const RESTList = ( { endpoint } ) => {
 	// Use period from store
-	const [{ period }, dispatch] = useStoreValue()
+	const [ { period } ] = useStoreValue();
 
 	// Use state to hold data fetched from API
-	const [fetched, setFetched] = useState(null)
+	const [ fetched, setFetched ] = useState( null );
 
 	// Fetch new data and update List when period changes
-	useEffect(() => {
-		axios.get(wpApiSettings.root + 'give-api/v2/reports/' + endpoint, {
-			params: {
-				start: period.startDate.format('YYYY-MM-DD'),
-				end: period.startDate.format('YYYY-MM-DD')
-			},
-			headers: {
-				'X-WP-Nonce': wpApiSettings.nonce
-			}
-		})
-		.then(function (response) {
-			console.log(response)
-			setFetched(response.data.data)
-		})
-	}, [period, endpoint])
+	useEffect( () => {
+		if ( period.startDate && period.endDate ) {
+			axios.get( wpApiSettings.root + 'give-api/v2/reports/' + endpoint, {
+				params: {
+					start: period.startDate.format( 'YYYY-MM-DD' ),
+					end: period.startDate.format( 'YYYY-MM-DD' ),
+				},
+				headers: {
+					'X-WP-Nonce': wpApiSettings.nonce,
+				},
+			} )
+				.then( function( response ) {
+					setFetched( response.data.data );
+				} );
+		}
+	}, [ period, endpoint ] );
 
-	const items = fetched ? fetched.map((item, index) => {
-		switch (item.type) {
+	const items = fetched ? fetched.map( ( item, index ) => {
+		switch ( item.type ) {
 			case 'donor':
 				return <DonorItem
-							image={item.image}
-							name={item.name}
-							email={item.email}
-							count={item.count}
-							total={item.total}
-							key={index}
-						/>
+					image={ item.image }
+					name={ item.name }
+					email={ item.email }
+					count={ item.count }
+					total={ item.total }
+					key={ index }
+				/>;
 			case 'donation':
 				return <DonationItem
-							status={item.status}
-							amount={item.amount}
-							time={item.time}
-							donor={item.donor}
-							source={item.source}
-							key={index}
-						/>
+					status={ item.status }
+					amount={ item.amount }
+					time={ item.time }
+					donor={ item.donor }
+					source={ item.source }
+					key={ index }
+				/>;
 			case 'location':
 				return <LocationItem
-							city={item.city}
-							state={item.state}
-							country={item.country}
-							flag={item.flag}
-							count={item.count}
-							total={item.total}
-							key={index}
-						/>
+					city={ item.city }
+					state={ item.state }
+					country={ item.country }
+					flag={ item.flag }
+					count={ item.count }
+					total={ item.total }
+					key={ index }
+				/>;
 		}
-	}) : null
+	} ) : null;
 
 	return (
 		<Fragment>
-			{fetched && (
+			{ fetched && (
 				<List>
-					{items}
+					{ items }
 				</List>
-			)}
+			) }
 		</Fragment>
-	)
-}
+	);
+};
 
 RESTList.propTypes = {
 	// API endpoint where data is fetched (ex: 'payment-statuses')
-	endpoint: PropTypes.string.isRequired
-}
+	endpoint: PropTypes.string.isRequired,
+};
 
 RESTList.defaultProps = {
 	endpoint: null,
-}
+};
 
-export default RESTList
+export default RESTList;
