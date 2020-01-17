34f31150dc46aca24489581ce9a37a51dd46c085
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-01-18 02:02:36 +0530

    chore: add initial templates to elegent theme

--- templates/form-styles/elegent/index.php
+++ templates/form-styles/elegent/index.php
@@ -1,2 +1,9 @@
-<?php
-echo 'you are watching embed form';
+<div id="give-form-<?php echo $form->ID; ?>-wrap" class="<?php echo $form->get_form_wrap_classes( $args ); ?>">
+	<?php
+	if ( $form->is_close_donation_form() ) {
+		include 'close-form-notice.php';
+	} else {
+		include 'form.php';
+	}
+	?>
+</div><!--end #give-form-<?php echo absint( $form->ID ); ?>-->
