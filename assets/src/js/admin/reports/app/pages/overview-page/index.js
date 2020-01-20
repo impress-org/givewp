81bdb8effab2026bf0ac5ccaee73f67637181a58
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-01-20 13:01:13 -0500

    feature: scaffold RESTMiniChart HOC

--- assets/src/js/admin/reports/app/pages/overview-page/index.js
+++ assets/src/js/admin/reports/app/pages/overview-page/index.js
@@ -2,231 +2,221 @@
 // Pages use the Grid component to establish a
 // 12 column grid for content to exist in
 
-import Grid from '../../../components/grid'
-import Card from '../../../components/card'
-import Chart from '../../../components/chart'
-import RESTChart from '../../../components/rest-chart'
-import MiniChart from '../../../components/mini-chart'
-import List from '../../../components/list'
-import RESTList from '../../../components/rest-list'
-import LocationItem from '../../../components/location-item'
-import DonationItem from '../../../components/donation-item'
-import DonorItem from '../../../components/donor-item'
+import Grid from '../../../components/grid';
+import Card from '../../../components/card';
+import RESTChart from '../../../components/rest-chart';
+import MiniChart from '../../../components/mini-chart';
+import RESTMiniChart from '../../../components/rest-mini-chart';
+import List from '../../../components/list';
+import RESTList from '../../../components/rest-list';
+import LocationItem from '../../../components/location-item';
+import DonationItem from '../../../components/donation-item';
 const { __ } = wp.i18n;
 
 const OverviewPage = () => {
-    return (
-        <Grid>
-			<Card title={__('Donations vs Income', 'give')} width={12}>
+	return (
+		<Grid>
+			<Card title={ __( 'Donations vs Income', 'give' ) } width={ 12 }>
 				<RESTChart
-                    type='line'
-                    aspectRatio={0.4}
-                    endpoint='donations-vs-income'
-                    showLegend={false}
-                />
-            </Card>
-            <Card width={3}>
-                <MiniChart
-                    title='Mini Doughnut'
-                    type='doughnut'
-                    data={{
-                        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Jun', 'Jul'],
-                        datasets: [
-                            {
-                                label: 'Donations',
-                                data: [400.00, 532.00, 333.00, 72.56, 300.00, 422.22]
-                            }
-                        ]
-                    }}
-                />
-            </Card>
-            <Card width={3}>
-                <MiniChart
-                    title='Mini Line'
-                    type='line'
-                    data={{
-                        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Jun', 'Jul'],
-                        datasets: [
-                            {
-                                label: 'Donations',
-                                data: [400.00, 532.00, 333.00, 72.56, 390.23, 350.00]
-                            }
-                        ]
-                    }}
-                />
-            </Card>
-            <Card width={3}>
-                <MiniChart
-                    title='Mini Pie'
-                    type='pie'
-                    data={{
-                        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Jun', 'Jul'],
-                        datasets: [
-                            {
-                                label: 'Donations',
-                                data: [400.00, 532.00, 333.00, 72.56, 300.00, 450.30]
-                            }
-                        ]
-                    }}
-                />
-            </Card>
-            <Card width={3}>
-                <MiniChart
-                    title='Mini Bar'
-                    type='bar'
-                    data={{
-                        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Jun', 'Jul'],
-                        datasets: [
-                            {
-                                label: 'Donations',
-                                data: [400.00, 532.00, 333.00, 72.56, 300.00, 450.00]
-                            }
-                        ]
-                    }}
-                />
-            </Card>
-            <Card title={__('Payment Methods', 'give')} width={4}>
+					type="line"
+					aspectRatio={ 0.4 }
+					endpoint="donations-vs-income"
+					showLegend={ false }
+				/>
+			</Card>
+			<Card width={ 3 }>
+				<RESTMiniChart
+					title="Income"
+					endpoint="income-over-time"
+				/>
+			</Card>
+			<Card width={ 3 }>
+				<MiniChart
+					title="Mini Line"
+					type="line"
+					data={ {
+						labels: [ 'Jan', 'Feb', 'Mar', 'Apr', 'Jun', 'Jul' ],
+						datasets: [
+							{
+								label: 'Donations',
+								data: [ 400.00, 532.00, 333.00, 72.56, 390.23, 350.00 ],
+							},
+						],
+					} }
+				/>
+			</Card>
+			<Card width={ 3 }>
+				<MiniChart
+					title="Mini Pie"
+					type="pie"
+					data={ {
+						labels: [ 'Jan', 'Feb', 'Mar', 'Apr', 'Jun', 'Jul' ],
+						datasets: [
+							{
+								label: 'Donations',
+								data: [ 400.00, 532.00, 333.00, 72.56, 300.00, 450.30 ],
+							},
+						],
+					} }
+				/>
+			</Card>
+			<Card width={ 3 }>
+				<MiniChart
+					title="Mini Bar"
+					type="bar"
+					data={ {
+						labels: [ 'Jan', 'Feb', 'Mar', 'Apr', 'Jun', 'Jul' ],
+						datasets: [
+							{
+								label: 'Donations',
+								data: [ 400.00, 532.00, 333.00, 72.56, 300.00, 450.00 ],
+							},
+						],
+					} }
+				/>
+			</Card>
+			<Card title={ __( 'Payment Methods', 'give' ) } width={ 4 }>
 				<RESTChart
-                    type='doughnut'
-                    aspectRatio={0.6}
-                    endpoint='payment-methods'
-                    showLegend={true}
-                />
-            </Card>
-            <Card title={__('Payment Statuses', 'give')} width={4}>
+					type="doughnut"
+					aspectRatio={ 0.6 }
+					endpoint="payment-methods"
+					showLegend={ true }
+				/>
+			</Card>
+			<Card title={ __( 'Payment Statuses', 'give' ) } width={ 4 }>
 				<RESTChart
-                    type='bar'
-                    aspectRatio={1.2}
-                    endpoint='payment-statuses'
-                    showLegend={false}
-                />
-            </Card>
-            <Card title={__('Form Performance (All Time)', 'give')} width={4}>
-                <RESTChart
-                    type='pie'
-                    aspectRatio={0.6}
-                    endpoint='form-performance'
-                    showLegend={true}
-                />
-            </Card>
-            <Card title={__('Donor List', 'give')} width={4}>
-				<RESTList endpoint='top-donors' />
-            </Card>
-            <Card title={__('Location List', 'give')} width={4}>
-                <List onScrollEnd={() => alert('reached end!')}>
-                    <LocationItem
-                        city='Anacorts'
-                        state='Washington'
-                        country='United States'
-                        flag='flag.png'
-                        count='4 Donations'
-                        total='$345.00'
-                    />
-                    <LocationItem
-                        city='Anacorts'
-                        state='Washington'
-                        country='United States'
-                        flag='flag.png'
-                        count='4 Donations'
-                        total='$345.00'
-                    />
-                    <LocationItem
-                        city='Anacorts'
-                        state='Washington'
-                        country='United States'
-                        flag='flag.png'
-                        count='4 Donations'
-                        total='$345.00'
-                    />
-                    <LocationItem
-                        city='Anacorts'
-                        state='Washington'
-                        country='United States'
-                        flag='flag.png'
-                        count='4 Donations'
-                        total='$345.00'
-                    />
-                    <LocationItem
-                        city='Anacorts'
-                        state='Washington'
-                        country='United States'
-                        flag='flag.png'
-                        count='4 Donations'
-                        total='$345.00'
-                    />
-                    <LocationItem
-                        city='Anacorts'
-                        state='Washington'
-                        country='United States'
-                        flag='flag.png'
-                        count='4 Donations'
-                        total='$345.00'
-                    />
-                    <LocationItem
-                        city='Anacorts'
-                        state='Washington'
-                        country='United States'
-                        flag='flag.png'
-                        count='4 Donations'
-                        total='$345.00'
-                    />
-                    <LocationItem
-                        city='Anacorts'
-                        state='Washington'
-                        country='United States'
-                        flag='flag.png'
-                        count='4 Donations'
-                        total='$345.00'
-                    />
-                 </List>
-            </Card>
-            <Card title={__('Donation List', 'give')} width={4}>
-                <List onScrollEnd={() => alert('reached end of list!')}>
-                    <DonationItem
-                        status='completed'
-                        amount='$50.00'
-                        time='2013-02-08 09:30'
-                        donor={{name: 'Test Name', id: 456}}
-                        source='Save the Whales'
-                    />
-                    <DonationItem
-                        status='completed'
-                        amount='$50.00'
-                        time='2013-02-08 09:30'
-                        donor={{name: 'Test Name', id: 456}}
-                        source='Save the Whales'
-                    />
-                    <DonationItem
-                        status='abandoned'
-                        amount='$50.00'
-                        time='2013-02-08 09:30'
-                        donor={{name: 'Test Name', id: 456}}
-                        source='Save the Whales'
-                    />
-                    <DonationItem
-                        status='refunded'
-                        amount='$50.00'
-                        time='2013-02-08 09:30'
-                        donor={{name: 'Test Name', id: 456}}
-                        source='Save the Whales'
-                    />
-                    <DonationItem
-                        status='completed'
-                        amount='$50.00'
-                        time='2013-02-08 09:30'
-                        donor={{name: 'Test Name', id: 456}}
-                        source='Save the Whales'
-                    />
-                    <DonationItem
-                        status='completed'
-                        amount='$50.00'
-                        time='2013-02-08 09:30'
-                        donor={{name: 'Test Name', id: 456}}
-                        source='Save the Whales'
-                    />
-                </List>
-            </Card>
-        </Grid>
-    )
-}
-export default OverviewPage
+					type="bar"
+					aspectRatio={ 1.2 }
+					endpoint="payment-statuses"
+					showLegend={ false }
+				/>
+			</Card>
+			<Card title={ __( 'Form Performance (All Time)', 'give' ) } width={ 4 }>
+				<RESTChart
+					type="pie"
+					aspectRatio={ 0.6 }
+					endpoint="form-performance"
+					showLegend={ true }
+				/>
+			</Card>
+			<Card title={ __( 'Donor List', 'give' ) } width={ 4 }>
+				<RESTList endpoint="top-donors" />
+			</Card>
+			<Card title={ __( 'Location List', 'give' ) } width={ 4 }>
+				<List>
+					<LocationItem
+						city="Anacorts"
+						state="Washington"
+						country="United States"
+						flag="flag.png"
+						count="4 Donations"
+						total="$345.00"
+					/>
+					<LocationItem
+						city="Anacorts"
+						state="Washington"
+						country="United States"
+						flag="flag.png"
+						count="4 Donations"
+						total="$345.00"
+					/>
+					<LocationItem
+						city="Anacorts"
+						state="Washington"
+						country="United States"
+						flag="flag.png"
+						count="4 Donations"
+						total="$345.00"
+					/>
+					<LocationItem
+						city="Anacorts"
+						state="Washington"
+						country="United States"
+						flag="flag.png"
+						count="4 Donations"
+						total="$345.00"
+					/>
+					<LocationItem
+						city="Anacorts"
+						state="Washington"
+						country="United States"
+						flag="flag.png"
+						count="4 Donations"
+						total="$345.00"
+					/>
+					<LocationItem
+						city="Anacorts"
+						state="Washington"
+						country="United States"
+						flag="flag.png"
+						count="4 Donations"
+						total="$345.00"
+					/>
+					<LocationItem
+						city="Anacorts"
+						state="Washington"
+						country="United States"
+						flag="flag.png"
+						count="4 Donations"
+						total="$345.00"
+					/>
+					<LocationItem
+						city="Anacorts"
+						state="Washington"
+						country="United States"
+						flag="flag.png"
+						count="4 Donations"
+						total="$345.00"
+					/>
+				</List>
+			</Card>
+			<Card title={ __( 'Donation List', 'give' ) } width={ 4 }>
+				<List>
+					<DonationItem
+						status="completed"
+						amount="$50.00"
+						time="2013-02-08 09:30"
+						donor={ { name: 'Test Name', id: 456 } }
+						source="Save the Whales"
+					/>
+					<DonationItem
+						status="completed"
+						amount="$50.00"
+						time="2013-02-08 09:30"
+						donor={ { name: 'Test Name', id: 456 } }
+						source="Save the Whales"
+					/>
+					<DonationItem
+						status="abandoned"
+						amount="$50.00"
+						time="2013-02-08 09:30"
+						donor={ { name: 'Test Name', id: 456 } }
+						source="Save the Whales"
+					/>
+					<DonationItem
+						status="refunded"
+						amount="$50.00"
+						time="2013-02-08 09:30"
+						donor={ { name: 'Test Name', id: 456 } }
+						source="Save the Whales"
+					/>
+					<DonationItem
+						status="completed"
+						amount="$50.00"
+						time="2013-02-08 09:30"
+						donor={ { name: 'Test Name', id: 456 } }
+						source="Save the Whales"
+					/>
+					<DonationItem
+						status="completed"
+						amount="$50.00"
+						time="2013-02-08 09:30"
+						donor={ { name: 'Test Name', id: 456 } }
+						source="Save the Whales"
+					/>
+				</List>
+			</Card>
+		</Grid>
+	);
+};
+export default OverviewPage;
---
+++ assets/src/js/admin/reports/components/rest-mini-chart/index.js
@@ -0,0 +1,61 @@
+// Vendor dependencies
+import axios from 'axios';
+import { useState, useEffect, Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+// Components
+import MiniChart from '../mini-chart';
+
+// Store-related dependencies
+import { useStoreValue } from '../../app/store';
+
+const RESTMiniChart = ( { title, endpoint } ) => {
+	// Use period from store
+	const [ { period } ] = useStoreValue();
+
+	// Use state to hold data fetched from API
+	const [ fetched, setFetched ] = useState( null );
+
+	// Fetch new data and update Chart when period changes
+	useEffect( () => {
+		if ( period.startDate && period.endDate ) {
+			axios.get( wpApiSettings.root + 'give-api/v2/reports/' + endpoint, {
+				params: {
+					start: period.startDate.format( 'YYYY-MM-DD-HH' ),
+					end: period.endDate.format( 'YYYY-MM-DD-HH' ),
+				},
+				headers: {
+					'X-WP-Nonce': wpApiSettings.nonce,
+				},
+			} )
+				.then( function( response ) {
+					setFetched( response.data.data );
+				} );
+		}
+	}, [ period, endpoint ] );
+
+	return (
+		<Fragment>
+			{ fetched && (
+				<MiniChart
+					title={ title }
+					data={ fetched }
+				/>
+			) }
+		</Fragment>
+	);
+};
+
+RESTMiniChart.propTypes = {
+	// Mini Chart title
+	title: PropTypes.string.isRequired,
+	// API endpoint where data is fetched (ex: 'payment-statuses')
+	endpoint: PropTypes.string.isRequired,
+};
+
+RESTMiniChart.defaultProps = {
+	title: null,
+	endpoint: null,
+};
+
+export default RESTMiniChart;
