df568345a4f11e9fb6cc8d1208f693acd4645bf7
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-01-21 23:22:33 +0530

    refactor: remove goal from top of form

--- templates/form-styles/elegent/actions.php
+++ templates/form-styles/elegent/actions.php
@@ -1,5 +1,9 @@
 <?php
+remove_action( 'give_pre_form', 'give_show_goal_progress', 10 );
+
+add_action( 'give_pre_form', 'give_elegent_add_form_introduction_section' );
+
+
 function give_elegent_add_form_introduction_section() {
 	include 'sections/introduction.php';
 }
-add_action( 'give_pre_form', 'give_elegent_add_form_introduction_section' );
