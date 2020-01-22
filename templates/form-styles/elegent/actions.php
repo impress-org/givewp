581ab2774cf872720bba4d05a1db945efcb80a71
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-01-22 22:46:42 +0530

    refactor: make personal information section static

--- templates/form-styles/elegent/actions.php
+++ templates/form-styles/elegent/actions.php
@@ -33,6 +33,19 @@ function give_elegent_add_progress_bar_section( $form_id, $args, $form ) {
 }
 
 /**
+ * Setup common hooks in-favor of elegent form style
+ *
+ * @param int              $form_id
+ * @param array            $args
+ * @param Give_Donate_Form $form
+ */
+function give_elegent_setup_common_hooks( $form_id, $args, $form ) {
+	// Remove personal information from current position.
+	remove_action( 'give_donation_form_after_user_info', 'give_user_info_fields' );
+	remove_action( 'give_register_fields_before', 'give_user_info_fields' );
+}
+
+/**
  * Setup hooks in-favor of elegent form style
  *
  * @param int              $form_id
@@ -52,6 +65,7 @@ function give_elegent_setup_hooks( $form_id, $args, $form ) {
 	add_action( 'give_pre_form', 'give_elegent_add_form_introduction_section', 11, 3 );
 	add_action( 'give_pre_form', 'give_elegent_add_form_stats_section', 12, 3 );
 	add_action( 'give_pre_form', 'give_elegent_add_progress_bar_section', 13, 3 );
+	add_action( 'give_payment_mode_top', 'give_user_info_fields' );
 
 	/**
 	 * Remove actions
@@ -60,5 +74,22 @@ function give_elegent_setup_hooks( $form_id, $args, $form ) {
 	remove_action( 'give_pre_form', 'give_show_goal_progress', 10 );
 	// Hide title.
 	add_filter( 'give_form_title', '__return_empty_string' );
+
+	// Setup common hooks.
+	give_elegent_setup_common_hooks( $form_id, $args, $form );
 }
 add_action( 'give_pre_form_output', 'give_elegent_setup_hooks', 1, 3 );
+
+/**
+ * Setup hooks in-favor of elegent form style when donor changes payment gateway
+ *
+ * @param int $form_id
+ */
+function give_elegent_setup_hooks_on_ajax( $form_id ) {
+	give_elegent_setup_common_hooks( $form_id, array(), new Give_Donate_Form( $form_id ) );
+}
+add_action( 'wp_ajax_give_load_gateway', 'give_elegent_setup_hooks_on_ajax', 9 );
+add_action( 'wp_ajax_no_privgive_load_gateway', 'give_elegent_setup_hooks_on_ajax, 9' );
+
+
+// @todo: add remove hooks on basis on form style or mode embed or onpage
