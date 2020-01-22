36e8a5cc4225e9ba22821a3bcab3a121bf0d177a
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-01-22 10:37:28 -0500

    feature: add RESTMiniChart to widget

--- assets/src/js/admin/reports/widget/index.js
+++ assets/src/js/admin/reports/widget/index.js
@@ -1,86 +1,61 @@
 // Reports admin dashboard widget
 
 // Components
-import Grid from '../components/grid'
-import Card from '../components/card'
-import Chart from '../components/chart'
-import MiniChart from '../components/mini-chart'
+import Grid from '../components/grid';
+import Card from '../components/card';
+import Chart from '../components/chart';
+import RESTMiniChart from '../components/rest-mini-chart';
+
+// Dependencies
+const { __ } = wp.i18n;
 
 const Widget = () => {
-    return (
-        <Grid gap='12px'>
-            <Card width={12}>
-                <Chart
-                    type='line'
-                    aspectRatio={0.4}
-                    data={{
-                        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Jun', 'Jul'],
-                        datasets: [
-                            {
-                                label: 'Donations',
-                                data: [4, 5, 3, 7, 5, 6]
-                            },
-                        ]
-                    }}
-                />
-            </Card>
-            <Card width={6}>
-                <MiniChart
-                    title='Avg Donations'
-                    data={{
-                        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Jun', 'Jul'],
-                        datasets: [
-                            {
-                                label: 'Donations',
-                                data: [4, 5, 3, 7, 5, 6]
-                            },
-                        ]
-                    }}
-                />
-            </Card>
-            <Card width={6}>
-                <MiniChart
-                    title='Avg Donations'
-                    data={{
-                        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Jun', 'Jul'],
-                        datasets: [
-                            {
-                                label: 'Donations',
-                                data: [4, 5, 3, 7, 5, 6]
-                            },
-                        ]
-                    }}
-                />
-            </Card>
-            <Card width={6}>
-                <MiniChart
-                    title='Avg Donations'
-                    data={{
-                        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Jun', 'Jul'],
-                        datasets: [
-                            {
-                                label: 'Donations',
-                                data: [4, 5, 3, 7, 5, 6]
-                            },
-                        ]
-                    }}
-                />
-            </Card>
-            <Card width={6}>
-                <MiniChart
-                    title='Avg Donations'
-                    data={{
-                        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Jun', 'Jul'],
-                        datasets: [
-                            {
-                                label: 'Donations',
-                                data: [4, 5, 3, 7, 5, 6]
-                            },
-                        ]
-                    }}
-                />
-            </Card>
-        </Grid>
-    )
-}
-export default Widget
+	return (
+		<Grid gap="12px">
+			<Card width={ 12 }>
+				<Chart
+					type="line"
+					aspectRatio={ 0.4 }
+					data={ {
+						labels: [ 'Jan', 'Feb', 'Mar', 'Apr', 'Jun', 'Jul' ],
+						datasets: [
+							{
+								label: 'Donations',
+								data: [ 4, 5, 3, 7, 5, 6 ],
+							},
+						],
+					} }
+				/>
+			</Card>
+			<Card width={ 6 }>
+				<RESTMiniChart
+					title={ __( 'Total Income', 'give' ) }
+					highlight="total"
+					endpoint="income-over-time"
+				/>
+			</Card>
+			<Card width={ 6 }>
+				<RESTMiniChart
+					title={ __( 'Total Income', 'give' ) }
+					highlight="total"
+					endpoint="income-over-time"
+				/>
+			</Card>
+			<Card width={ 6 }>
+				<RESTMiniChart
+					title={ __( 'Total Income', 'give' ) }
+					highlight="total"
+					endpoint="income-over-time"
+				/>
+			</Card>
+			<Card width={ 6 }>
+				<RESTMiniChart
+					title={ __( 'Total Income', 'give' ) }
+					highlight="total"
+					endpoint="income-over-time"
+				/>
+			</Card>
+		</Grid>
+	);
+};
+export default Widget;
