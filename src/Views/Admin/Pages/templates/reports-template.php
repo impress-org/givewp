eae61635f636f405523bce37d98185df83851415
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-01-29 10:44:28 -0500

    fix: echo translated strings

--- src/Views/Admin/Pages/templates/reports-template.php
+++ src/Views/Admin/Pages/templates/reports-template.php
@@ -6,35 +6,35 @@
 	<div class="wrap give-settings-page">
 		<div class="give-settings-header">
 			<h1 class="wp-heading-inline">
-				<?php __( 'Reports', 'give' ); ?>
+				<?php _e( 'Reports', 'give' ); ?>
 			</h1>
 			<div class="givewp-period-selector">
 				<div class="group">
 					<button>
-						<?php __( 'Day', 'give' ); ?>
+						<?php _e( 'Day', 'give' ); ?>
 					</button>
 					<button class="selected">
-						<?php __( 'Week', 'give' ); ?>
+						<?php _e( 'Week', 'give' ); ?>
 					</button>
 					<button>
-						<?php __( 'Month', 'give' ); ?>
+						<?php _e( 'Month', 'give' ); ?>
 					</button>
 					<button>
-						<?php __( 'Year', 'give' ); ?>
+						<?php _e( 'Year', 'give' ); ?>
 					</button>
 				</div>
 			</div>
 		</div>
 		<div class="nav-tab-wrapper give-nav-tab-wrapper" style="height: auto; overflow: visible;">
 			<a class="nav-tab nav-tab-active" href="#/">
-				<?php __( 'Overview', 'give' ); ?>
+				<?php _e( 'Overview', 'give' ); ?>
 			</a>
 			<a class="nav-tab" href="http://givewp.local/wp-admin/edit.php?post_type=give_forms&amp;page=give-reports">
-				<?php __( 'Legacy Reports Page', 'give' ); ?>
+				<?php _e( 'Legacy Reports Page', 'give' ); ?>
 			</a>
 		</div>
 		<div class="givewp-loading-notice">
-			<h2><?php __( 'Loading', 'give' ); ?></h2>
+			<h2><?php _e( 'Loading', 'give' ); ?></h2>
 		</div>
 	</div>
 </div>
