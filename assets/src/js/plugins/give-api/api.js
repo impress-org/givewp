c9cfe771ad09281ea64f9abf3639e078376f4f15
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-02-06 18:11:54 +0530

    chore: add helper fn to delete param from url

--- assets/src/js/plugins/give-api/api.js
+++ assets/src/js/plugins/give-api/api.js
@@ -294,6 +294,34 @@ const Give = {
 
 			return true;
 		},
+
+		/**
+		 * Remove parameter from url
+		 *
+		 * @since 2.7
+		 * @param {string} url
+		 * @param {string} parameter
+		 * @return {string|*}
+		 */
+		removeURLParameter: function removeURLParameter( url, parameter ) {
+			//prefer to use l.search if you have a location/link object
+			const urlparts = url.split( '?' );
+			if ( urlparts.length >= 2 ) {
+				const prefix = encodeURIComponent( parameter ) + '=';
+				const pars = urlparts[ 1 ].split( /[&;]/g );
+
+				//reverse iteration as may be destructive
+				for ( let i = pars.length; i-- > 0; ) {
+					//idiom for string.startsWith
+					if ( pars[ i ].lastIndexOf( prefix, 0 ) !== -1 ) {
+						pars.splice( i, 1 );
+					}
+				}
+
+				return urlparts[ 0 ] + ( pars.length > 0 ? '?' + pars.join( '&' ) : '' );
+			}
+			return url;
+		},
 	},
 
 	/**
