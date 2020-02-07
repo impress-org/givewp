1cf5159348152752af77ad3e2424d51c7d85dafe
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-02-07 15:34:01 -0500

    fix: revamp TotalRefunds to use new Chart data shape

--- src/API/Endpoints/Reports/TotalRefunds.php
+++ src/API/Endpoints/Reports/TotalRefunds.php
@@ -34,32 +34,12 @@ class TotalRefunds extends Endpoint {
 		$data = [];
 
 		switch ( true ) {
-			case ( $diff->days > 900 ):
-				$data = $this->get_data( $start, $end, 'P1Y', 'Y' );
-				break;
-			case ( $diff->days > 700 ):
-				$data = $this->get_data( $start, $end, 'P6M', 'F Y' );
-				break;
-			case ( $diff->days > 400 ):
-				$data = $this->get_data( $start, $end, 'P3M', 'F Y' );
-				break;
-			case ( $diff->days > 120 ):
-				$data = $this->get_data( $start, $end, 'P1M', 'M Y' );
-				break;
-			case ( $diff->days > 30 ):
-				$data = $this->get_data( $start, $end, 'P7D', 'M jS' );
-				break;
-			case ( $diff->days > 10 ):
-				$data = $this->get_data( $start, $end, 'P3D', 'M jS' );
-				break;
-			case ( $diff->days > 4 ):
-				$data = $this->get_data( $start, $end, 'P1D', 'l' );
-				break;
 			case ( $diff->days > 1 ):
-				$data = $this->get_data( $start, $end, 'P1D', 'D ga' );
+				$interval = round( $diff->days / 12 );
+				$data     = $this->get_data( $start, $end, 'P' . $interval . 'D' );
 				break;
 			case ( $diff->days >= 0 ):
-				$data = $this->get_data( $start, $end, 'PT1H', 'D ga' );
+				$data = $this->get_data( $start, $end, 'PT1H' );
 				break;
 		}
 
@@ -73,7 +53,7 @@ class TotalRefunds extends Endpoint {
 		);
 	}
 
-	public function get_data( $start, $end, $interval, $format ) {
+	public function get_data( $start, $end, $interval ) {
 
 		$allTimeStartStr = $this->get_all_time_start();
 
@@ -82,29 +62,39 @@ class TotalRefunds extends Endpoint {
 		$startStr = $start->format( 'Y-m-d H:i:s' );
 		$endStr   = $end->format( 'Y-m-d H:i:s' );
 
-		// Determine the start date of the previous period (used to calculate trend)
-		$prev    = date_sub( date_create( $startStr ), date_diff( $start, $end ) );
-		$prevStr = $prev->format( 'Y-m-d H:i:s' );
-
-		$labels  = [];
-		$refunds = [];
+		$tooltips = [];
+		$income   = [];
 
 		$dateInterval = new \DateInterval( $interval );
+
 		while ( $start < $end ) {
 
-			$periodStart = $start->format( 'Y-m-d H:i:s' );
+			$periodStart = clone $start;
+			$periodEnd   = clone $start;
 
-			// Add interval to get period end
-			$periodEnd = clone $start;
+			// Add interval to set up period end
 			date_add( $periodEnd, $dateInterval );
 
-			$label     = $periodEnd->format( $format );
-			$periodEnd = $periodEnd->format( 'Y-m-d H:i:s' );
+			$endStr = $periodEnd->format( 'Y-m-d H:i:s' );
 
-			$refundsForPeriod = $stats->get_sales( 0, $startStr, $periodEnd, 'refunded' );
+			$refundsForPeriod = $stats->get_sales( 0, $startStr, $endStr, 'refunded' );
 
-			$refunds[] = $refundsForPeriod;
-			$labels[]  = $label;
+			$refunds[] = [
+				'y' => $refundsForPeriod,
+				'x' => $endStr,
+			];
+
+			if ( $interval == 'PT1H' ) {
+				$periodLabel = $periodStart->format( 'D ga' ) . ' - ' . $periodEnd->format( 'D ga' );
+			} else {
+				$periodLabel = $periodStart->format( 'M j, Y' ) . ' - ' . $periodEnd->format( 'M j, Y' );
+			}
+
+			$tooltips[] = [
+				'title'  => $refundsForPeriod,
+				'body'   => __( 'Total Refunds', 'give' ),
+				'footer' => $periodLabel,
+			];
 
 			date_add( $start, $dateInterval );
 		}
@@ -119,11 +109,10 @@ class TotalRefunds extends Endpoint {
 
 		// Create data objec to be returned, with total highlighted
 		$data = [
-			'labels'   => $labels,
 			'datasets' => [
 				[
-					'label'     => __( 'Refunds', 'give' ),
 					'data'      => $refunds,
+					'tooltips'  => $tooltips,
 					'trend'     => $trend,
 					'highlight' => $totalForPeriod,
 				],
