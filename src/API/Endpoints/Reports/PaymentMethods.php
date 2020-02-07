3735dcaff247160715a63c7662df7bd61e7ff062
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-02-07 15:35:39 -0500

    fix: update PaymentMethods endpoint to use new Chart data shape

--- src/API/Endpoints/Reports/PaymentMethods.php
+++ src/API/Endpoints/Reports/PaymentMethods.php
@@ -14,32 +14,59 @@ class PaymentMethods extends Endpoint {
 		$this->endpoint = 'payment-methods';
 	}
 
-	public function get_report($request) {
-
-		$labels = [];
-		$data = [];
+	public function get_report( $request ) {
 
 		// Use give_count_payments logic to get payments
 		$gateways = give_get_payment_gateways();
-		$stats = new \Give_Payment_Stats();
+		$stats    = new \Give_Payment_Stats();
+
+		$gatewaysArr = [];
 
 		foreach ( $gateways as $gateway_id => $gateway ) {
-			$donations = $stats->get_earnings( 0, date($request['start']), date($request['end']), $gateway_id );
-			array_push($labels, $gateway['admin_label']);
-			array_push($data, $donations);
+			$gatewaysArr[] = [
+				'admin_label' => $gateway['admin_label'],
+				'count'       => $stats->get_sales( 0, date( $request['start'] ), date( $request['end'] ), $gateway_id ),
+				'amount'      => $stats->get_earnings( 0, date( $request['start'] ), date( $request['end'] ), $gateway_id ),
+			];
+		}
+		$sorted = usort( $gatewaysArr, [ $this, 'compare_gateways' ] );
+
+		$labels   = [];
+		$data     = [];
+		$tooltips = [];
+
+		if ( $sorted == true ) {
+			$gatewaysArr = array_slice( $gatewaysArr, 0, 5 );
+			foreach ( $gatewaysArr as $gateway ) {
+				$labels[]   = $gateway['admin_label'];
+				$data[]     = $gateway['amount'];
+				$tooltips[] = [
+					'title'  => give_currency_filter( give_format_amount( $gateway['amount'] ), [ 'decode_currency' => true ] ),
+					'body'   => $gateway['count'] . ' ' . __( 'Payments', 'give' ),
+					'footer' => $gateway['admin_label'],
+				];
+			}
 		}
 
-		// Add caching logic here...
-
-		return new \WP_REST_Response([
-			'data' => [
-				'labels' => $labels,
-				'datasets' => [
-					[
-						'data' => $data
-					]
+		return new \WP_REST_Response(
+			[
+				'data' => [
+					'labels'   => $labels,
+					'datasets' => [
+						[
+							'data'     => $data,
+							'tooltips' => $tooltips,
+						],
+					],
 				],
 			]
-		]);
+		);
+	}
+
+	public function compare_gateways( $a, $b ) {
+		if ( $a['amount'] == $b['amount'] ) {
+				return 0;
+		}
+		return ( $a['amount'] > $b['amount'] ) ? -1 : 1;
 	}
 }
