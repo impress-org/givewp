8683ceeb898ee666b474fb3eb51bd36ef3d1d76a
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-02-11 11:30:46 -0500

    feature: add "See All Time" button to not found notice

--- assets/src/js/admin/reports/components/not-found-notice/index.js
+++ assets/src/js/admin/reports/components/not-found-notice/index.js
@@ -1,13 +1,34 @@
 // Dependencies
 const { __ } = wp.i18n;
 
+// Store-related dependencies
+import { useStoreValue } from '../../store';
+
 // Styles
 import './style.scss';
 
 const NotFoundNotice = () => {
+	// Get 'period' object from the store
+	const [ {}, dispatch ] = useStoreValue();
+
+	// Dispatch SET_RANGE action
+	const setRange = ( range ) => {
+		dispatch( {
+			type: 'SET_RANGE',
+			payload: {
+				range,
+			},
+		} );
+	};
+
 	return (
 		<div className="givewp-not-found-notice">
 			<h2>{ __( 'No donations were found for this period.', 'give' ) }</h2>
+			<button
+				onClick={ () => setRange( 'alltime' ) }
+				className="givewp-not-found-notice-button">
+				{ __( 'See All Time Donations', 'give' ) }
+			</button>
 		</div>
 	);
 };
