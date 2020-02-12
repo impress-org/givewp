480bf3e2202c2f28f50b278ed94b505d7b8ae09b
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-02-12 18:17:46 -0500

    feature: add RESTTable component

---
+++ assets/src/js/admin/reports/components/rest-table/index.js
@@ -0,0 +1,96 @@
+// Vendor dependencies
+import axios from 'axios';
+import { useState, useEffect, Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+// Components
+import Table from '../table';
+import LoadingOverlay from '../loading-overlay';
+import NotFoundOverlay from '../not-found-overlay';
+
+// Utilities
+import { getSkeletonLabels, getSkeletonRows, getLabels, getRows } from './utils';
+
+// Store-related dependencies
+import { useStoreValue } from '../../store';
+
+const RESTTable = ( { title, endpoint } ) => {
+	// Use period from store
+	const [ { period, donationsFound, pageLoaded } ] = useStoreValue();
+
+	// Use state to hold data fetched from API
+	const [ fetched, setFetched ] = useState( null );
+
+	const [ dataFound, setDataFound ] = useState( true );
+
+	const [ loaded, setLoaded ] = useState( false );
+
+	// Fetch new data and update List when period changes
+	useEffect( () => {
+		if ( period.startDate && period.endDate ) {
+			setLoaded( false );
+			axios.get( wpApiSettings.root + 'give-api/v2/reports/' + endpoint, {
+				params: {
+					start: period.startDate.format( 'YYYY-MM-DD-HH' ),
+					end: period.endDate.format( 'YYYY-MM-DD-HH' ),
+				},
+				headers: {
+					'X-WP-Nonce': wpApiSettings.nonce,
+				},
+			} )
+				.then( function( response ) {
+					setFetched( response.data.data );
+					const found = response.data.data.length > 0 ? true : false;
+					setDataFound( found );
+					setLoaded( true );
+				} );
+		}
+	}, [ period, endpoint ] );
+
+	const ready = fetched && donationsFound && pageLoaded ? true : false;
+
+	let labels;
+	let rows;
+
+	if ( ready ) {
+		labels = getLabels( fetched );
+		rows = getRows( fetched );
+	} else {
+		labels = getSkeletonLabels( fetched );
+		rows = getSkeletonRows( fetched );
+	}
+
+	let overlay;
+	switch ( true ) {
+		case loaded === false: {
+			overlay = <LoadingOverlay />;
+			break;
+		}
+		case dataFound === false: {
+			overlay = <NotFoundOverlay />;
+			break;
+		}
+	}
+
+	return (
+		<Fragment>
+			{ overlay }
+			<Table
+				title={ title }
+				labels={ labels }
+				rows={ rows }
+			/>
+		</Fragment>
+	);
+};
+
+RESTTable.propTypes = {
+	// API endpoint where data is fetched (ex: 'payment-statuses')
+	endpoint: PropTypes.string.isRequired,
+};
+
+RESTTable.defaultProps = {
+	endpoint: null,
+};
+
+export default RESTTable;
---
+++ assets/src/js/admin/reports/components/rest-table/utils/index.js
@@ -0,0 +1,80 @@
+export function getSkeletonLabels() {
+	const labels = [
+		'Date',
+		'Donors',
+		'Donations',
+		'Refunds',
+		'Net',
+	];
+	return labels;
+}
+
+export function getSkeletonRows() {
+	const rows = [
+		[
+			'Feb 1',
+			'12',
+			'33',
+			'$400.00',
+			'2',
+			'$383.00',
+		],
+		[
+			'Feb 1',
+			'12',
+			'33',
+			'$400.00',
+			'2',
+			'$383.00',
+		],
+		[
+			'Feb 1',
+			'12',
+			'33',
+			'$400.00',
+			'2',
+			'$383.00',
+		],
+		[
+			'Feb 1',
+			'12',
+			'33',
+			'$400.00',
+			'2',
+			'$383.00',
+		],
+		[
+			'Feb 1',
+			'12',
+			'33',
+			'$400.00',
+			'2',
+			'$383.00',
+		],
+		[
+			'Feb 1',
+			'12',
+			'33',
+			'$400.00',
+			'2',
+			'$383.00',
+		],
+		[
+			'Feb 1',
+			'12',
+			'33',
+			'$400.00',
+			'2',
+			'$383.00',
+		],
+	];
+	return rows;
+}
+
+export function getLabels() {
+
+}
+
+export function getRows() {
+
+}
