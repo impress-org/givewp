7e42fd3f381121da820c14fafee006185f6238b7
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-02-12 18:13:14 -0500

    feature: scaffold table component

--- assets/src/js/admin/reports/components/table/index.js
+++ assets/src/js/admin/reports/components/table/index.js
@@ -1,6 +1,45 @@
-const Table = (props) => {
-    return (
-        <h1>Table goes here!</h1>
-    )
-}
-export default Table
+import { Fragment } from 'react';
+
+import './style.scss';
+
+const Table = ( { title, labels, rows } ) => {
+	const labelEls = labels.map( ( label, index ) => {
+		return (
+			<div className="givewp-table-label" key={ index }>
+				{ label }
+			</div>
+		);
+	} );
+
+	const rowEls = rows.map( ( row, index ) => {
+		const itemEls = row.map( ( item, key ) => {
+			return (
+				<div className="givewp-table-row-item" key={ key }>
+					{ item }
+				</div>
+			);
+		} );
+
+		return (
+			<div className="givewp-table-row" key={ index }>
+				{ itemEls }
+			</div>
+		);
+	} );
+
+	return (
+		<Fragment>
+			{ title && ( <div className="givewp-table-title">
+				{ title }
+			</div> ) }
+			<div className="givewp-table">
+				<div className="givewp-table-header">
+					{ labelEls }
+				</div>
+				{ rowEls }
+			</div>
+		</Fragment>
+	);
+};
+
+export default Table;
