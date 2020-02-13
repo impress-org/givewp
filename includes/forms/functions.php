928c62073101ea4ddc2cb364e81cb4b331a0a6a6
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-02-13 12:58:59 +0530

    chore: pass currency to give_maybe_sanitize_amount fn

--- includes/forms/functions.php
+++ includes/forms/functions.php
@@ -903,10 +903,21 @@ function give_get_price_option_amount( $form_id = 0, $price_id = 0 ) {
 		if ( isset( $price['_give_id']['level_id'] ) && $price['_give_id']['level_id'] == $price_id ) {
 			$amount = isset( $price['_give_amount'] ) ? $price['_give_amount'] : 0.00;
 			break;
-		};
+		}
 	}
 
-	return apply_filters( 'give_get_price_option_amount', give_maybe_sanitize_amount( $amount ), $form_id, $price_id );
+
+	/**
+	 * Filter the price amount
+	 *
+	 * @since 1.0
+	 */
+	return apply_filters(
+		'give_get_price_option_amount',
+		give_maybe_sanitize_amount( $amount, array( 'currency' => give_get_currency( $form_id ) ) ),
+		$form_id,
+		$price_id
+	);
 }
 
 /**
