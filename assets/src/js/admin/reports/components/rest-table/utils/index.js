f70026a79a4348b7ed6caf05415ad5b173a913a7
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-02-17 09:50:12 -0500

    feature: implement RESTTable utilities

--- assets/src/js/admin/reports/components/rest-table/utils/index.js
+++ assets/src/js/admin/reports/components/rest-table/utils/index.js
@@ -71,10 +71,14 @@ export function getSkeletonRows() {
 	return rows;
 }
 
-export function getLabels() {
-
+export function getLabels( data ) {
+	const labels = Object.keys( data[ 0 ] );
+	return labels;
 }
 
-export function getRows() {
-
+export function getRows( data ) {
+	const rows = data.map( ( row ) => {
+		return Object.values( row );
+	} );
+	return rows;
 }
