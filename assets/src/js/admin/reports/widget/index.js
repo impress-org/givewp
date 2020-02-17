71d11ebbe2afa657d1afc2717993f05ddf76a770
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-02-17 09:52:05 -0500

    feature: add MiniPeriodSelector to dashboard widget

--- assets/src/js/admin/reports/widget/index.js
+++ assets/src/js/admin/reports/widget/index.js
@@ -1,11 +1,12 @@
 // Reports admin dashboard widget
 
 // Dependencies
-import { Fragment } from 'react';
 const { __ } = wp.i18n;
 
 // Store-related dependencies
 import { useStoreValue } from '../store';
+
+import './style.scss';
 
 // Components
 import Grid from '../components/grid';
@@ -14,23 +15,24 @@ import RESTChart from '../components/rest-chart';
 import RESTMiniChart from '../components/rest-mini-chart';
 import NotFoundNotice from '../components/not-found-notice';
 import LoadingNotice from '../components/loading-notice';
+import MiniPeriodSelector from '../components/mini-period-selector';
 
 const Widget = () => {
 	const [ { donationsFound, pageLoaded } ] = useStoreValue();
-	const showGrid = donationsFound && pageLoaded ? true : false;
 
 	return (
-		<Fragment>
+		<div className="givewp-reports-widget-container">
 			{ donationsFound === false && (
-				<NotFoundNotice />
+				<NotFoundNotice range={ 'month' } />
 			) }
 			{ pageLoaded === false && (
 				<LoadingNotice />
 			) }
-			<Grid gap="12px" visible={ showGrid }>
+			<Grid gap="12px" visible={ pageLoaded }>
 				<Card width={ 12 }>
 					<RESTChart
 						title={ __( 'All Time Income', 'give' ) }
+						headerEls={ <MiniPeriodSelector /> }
 						type="line"
 						aspectRatio={ 0.8 }
 						endpoint="income"
@@ -62,7 +64,7 @@ const Widget = () => {
 					/>
 				</Card>
 			</Grid>
-		</Fragment>
+		</div>
 	);
 };
 export default Widget;
