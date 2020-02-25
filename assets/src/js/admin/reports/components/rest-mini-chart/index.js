75e83b1768569b7bddd724305ffdc558525dbe90
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-02-25 13:03:05 -0500

    fix: implement useReportsAPI hook in RESTMiniChart

--- assets/src/js/admin/reports/components/rest-mini-chart/index.js
+++ assets/src/js/admin/reports/components/rest-mini-chart/index.js
@@ -1,6 +1,5 @@
 // Vendor dependencies
-import axios from 'axios';
-import { useState, useEffect, Fragment } from 'react';
+import { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 // Components
@@ -8,56 +7,15 @@ import MiniChart from '../mini-chart';
 import LoadingOverlay from '../loading-overlay';
 
 // Store-related dependencies
-import { useStoreValue } from '../../store';
+import { useReportsAPI } from '../../utils';
 
 const RESTMiniChart = ( { title, endpoint } ) => {
 	// Use period from store
-	const [ { period } ] = useStoreValue();
-
-	// Use state to hold data fetched from API
-	const [ fetched, setFetched ] = useState( null );
-
-	// Use state to handle loaded status
-	const [ loaded, setLoaded ] = useState( true );
-
-	const [ querying, setQuerying ] = useState( false );
-
-	const CancelToken = axios.CancelToken;
-	const source = CancelToken.source();
-
-	// Fetch new data and update Chart when period changes
-	useEffect( () => {
-		if ( period.startDate && period.endDate ) {
-			if ( querying === true ) {
-				source.cancel( 'Operation canceled by the user.' );
-			}
-
-			setQuerying( true );
-			setLoaded( false );
-
-			axios.get( wpApiSettings.root + 'give-api/v2/reports/' + endpoint, {
-				params: {
-					start: period.startDate.format( 'YYYY-MM-DD-HH' ),
-					end: period.endDate.format( 'YYYY-MM-DD-HH' ),
-				},
-				headers: {
-					'X-WP-Nonce': wpApiSettings.nonce,
-				},
-			} )
-				.then( function( response ) {
-					setQuerying( false );
-					setFetched( response.data.data );
-					setLoaded( true );
-				} )
-				.catch( function() {
-					setQuerying( false );
-				} );
-		}
-	}, [ period, endpoint ] );
+	const [ fetched, querying ] = useReportsAPI( endpoint );
 
 	return (
 		<Fragment>
-			{ ! loaded && (
+			{ querying && (
 				<LoadingOverlay />
 			) }
 			{ fetched && (
