87948ba4b95241d7579efcd3cbed33b53fe4c119
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-02-25 09:45:42 -0500

    fix: centrally locate MiniPeriodSelector actions

--- assets/src/js/admin/reports/components/mini-period-selector/index.js
+++ assets/src/js/admin/reports/components/mini-period-selector/index.js
@@ -1,6 +1,6 @@
 // Store-related dependencies
 import { useStoreValue } from '../../store';
-
+import { setRange } from '../../store/actions';
 import './style.scss';
 const { __ } = wp.i18n;
 
@@ -8,22 +8,12 @@ const MiniPeriodSelector = () => {
 	// Get 'period' object from the store
 	const [ { period }, dispatch ] = useStoreValue();
 
-	// Dispatch SET_RANGE action
-	const setRange = ( range ) => {
-		dispatch( {
-			type: 'SET_RANGE',
-			payload: {
-				range,
-			},
-		} );
-	};
-
 	return (
 		<div className="givewp-mini-period-selector">
 			<div className="group">
-				<button className={ period.range === 'day' ? 'selected' : null } onClick={ () => setRange( 'day' ) }>{ __( 'Day', 'give' ) }</button>
-				<button className={ period.range === 'week' ? 'selected' : null } onClick={ () => setRange( 'week' ) }>{ __( 'Week', 'give' ) }</button>
-				<button className={ period.range === 'month' ? 'selected' : null } onClick={ () => setRange( 'month' ) }>{ __( 'Month', 'give' ) }</button>
+				<button className={ period.range === 'day' ? 'selected' : null } onClick={ () => dispatch( setRange( 'day' ) ) }>{ __( 'Day', 'give' ) }</button>
+				<button className={ period.range === 'week' ? 'selected' : null } onClick={ () => dispatch( setRange( 'week' ) ) }>{ __( 'Week', 'give' ) }</button>
+				<button className={ period.range === 'month' ? 'selected' : null } onClick={ () => dispatch( setRange( 'month' ) ) }>{ __( 'Month', 'give' ) }</button>
 			</div>
 		</div>
 	);
