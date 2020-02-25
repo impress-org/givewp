fc6d1645d90c6183e061c78d4533bfb3ddbfa904
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-02-25 16:45:21 -0500

    fix: ensure trend indicator arrow points correct direction

--- assets/src/js/admin/reports/components/mini-chart/index.js
+++ assets/src/js/admin/reports/components/mini-chart/index.js
@@ -9,26 +9,26 @@ import './style.scss';
 
 const MiniChart = ( { title, data } ) => {
 	//SVG up/positive icon
-	const up = <div className="up"><svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+	const up = <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
 		<path fillRule="evenodd" clipRule="evenodd" d="M7 13C10.3137 13 13 10.3137 13 7C13 3.68629 10.3137 1 7 1C3.68629 1 1 3.68629 1 7C1 10.3137 3.68629 13 7 13Z" fill="#69B868" fillOpacity="0.3" />
-		<mask id="mask0" mask-type="alpha" maskUnits="userSpaceOnUse" x="3" y="4" width="8" height="5">
+		<mask id="maskUp" mask-type="alpha" maskUnits="userSpaceOnUse" x="3" y="4" width="8" height="5">
 			<path fillRule="evenodd" clipRule="evenodd" d="M7 4.6665L3.5 8.1665L4.3225 8.989L7 6.31734L9.6775 8.989L10.5 8.1665L7 4.6665Z" fill="white" />
 		</mask>
-		<g mask="url(#mask0)">
+		<g mask="url(#maskUp)">
 			<rect x="-7.5835" y="-7.5835" width="29.1667" height="29.1667" fill="#69B868" />
 		</g>
-	</svg></div>;
+	</svg>;
 
 	//SVG down/negative icon
-	const down = <div className="down"><svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+	const down = <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
 		<path fillRule="evenodd" clipRule="evenodd" d="M7 13C10.3137 13 13 10.3137 13 7C13 3.68629 10.3137 1 7 1C3.68629 1 1 3.68629 1 7C1 10.3137 3.68629 13 7 13Z" fill="#D75A4B" fillOpacity="0.3" />
-		<mask id="mask0" mask-type="alpha" maskUnits="userSpaceOnUse" x="3" y="5" width="8" height="5">
+		<mask id="maskDown" mask-type="alpha" maskUnits="userSpaceOnUse" x="3" y="5" width="8" height="5">
 			<path fillRule="evenodd" clipRule="evenodd" d="M9.6775 5.01074L7 7.68241L4.3225 5.01074L3.5 5.83324L7 9.33324L10.5 5.83324L9.6775 5.01074Z" fill="white" />
 		</mask>
-		<g mask="url(#mask0)">
+		<g mask="url(#maskDown)">
 			<rect x="-7.5835" y="-7.5835" width="29.1667" height="29.1667" fill="#D75A4B" />
 		</g>
-	</svg></div>;
+	</svg>;
 
 	const [ highlightValue, setHighlightValue ] = useState( null );
 	const [ trend, setTrend ] = useState( null );
