78ec39d4cbbdf49cddbf0a1ba996fda23516f68f
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-02-28 18:19:49 -0500

    hotfix: scaffold additional sample data

--- assets/src/js/admin/reports/utils/sample.js
+++ assets/src/js/admin/reports/utils/sample.js
@@ -11,7 +11,27 @@ export const getSampleData = ( endpoint ) => {
 							},
 							{
 								y: 88,
+								x: '2001-01-02',
+							},
+							{
+								y: 88,
+								x: '2001-01-03',
+							},
+							{
+								y: 88,
+								x: '2001-01-04',
+							},
+							{
+								y: 88,
+								x: '2001-01-05',
+							},
+							{
+								y: 88,
 								x: '2001-01-06',
+							},
+							{
+								y: 88,
+								x: '2001-01-07',
 							},
 						],
 						tooltips: [
@@ -21,9 +41,342 @@ export const getSampleData = ( endpoint ) => {
 								footer: 'Jan 1',
 							},
 							{
+								title: '$10.00',
+								body: '12 Donors',
+								footer: 'Jan 2',
+							},
+							{
+								title: '$10.00',
+								body: '12 Donors',
+								footer: 'Jan 3',
+							},
+							{
+								title: '$10.00',
+								body: '12 Donors',
+								footer: 'Jan 4',
+							},
+							{
+								title: '$10.00',
+								body: '12 Donors',
+								footer: 'Jan 5',
+							},
+							{
 								title: '$20.00',
 								body: '3 Donors',
+								footer: 'Jan 6',
+							},
+							{
+								title: '$20.00',
+								body: '3 Donors',
+								footer: 'Jan 7',
+							},
+						],
+					},
+				],
+			};
+		case 'total-income':
+			return {
+				datasets: [
+					{
+						trend: -5,
+						highlight: '$150.00',
+						info: 'VS previous 7 days',
+						data: [
+							{
+								y: 20,
+								x: '2001-01-01',
+							},
+							{
+								y: 88,
+								x: '2001-01-02',
+							},
+							{
+								y: 88,
+								x: '2001-01-03',
+							},
+							{
+								y: 88,
+								x: '2001-01-04',
+							},
+							{
+								y: 88,
+								x: '2001-01-05',
+							},
+							{
+								y: 88,
+								x: '2001-01-06',
+							},
+							{
+								y: 88,
+								x: '2001-01-07',
+							},
+						],
+						tooltips: [
+							{
+								title: '$10.00',
+								body: '12 Donors',
+								footer: 'Jan 1',
+							},
+							{
+								title: '$10.00',
+								body: '12 Donors',
 								footer: 'Jan 2',
+							},
+							{
+								title: '$10.00',
+								body: '12 Donors',
+								footer: 'Jan 3',
+							},
+							{
+								title: '$10.00',
+								body: '12 Donors',
+								footer: 'Jan 4',
+							},
+							{
+								title: '$10.00',
+								body: '12 Donors',
+								footer: 'Jan 5',
+							},
+							{
+								title: '$20.00',
+								body: '3 Donors',
+								footer: 'Jan 6',
+							},
+							{
+								title: '$20.00',
+								body: '3 Donors',
+								footer: 'Jan 7',
+							},
+						],
+					},
+				],
+			};
+		case 'total-donors':
+			return {
+				datasets: [
+					{
+						trend: -5,
+						highlight: '4',
+						info: 'VS previous 7 days',
+						data: [
+							{
+								y: 20,
+								x: '2001-01-01',
+							},
+							{
+								y: 88,
+								x: '2001-01-02',
+							},
+							{
+								y: 88,
+								x: '2001-01-03',
+							},
+							{
+								y: 88,
+								x: '2001-01-04',
+							},
+							{
+								y: 88,
+								x: '2001-01-05',
+							},
+							{
+								y: 88,
+								x: '2001-01-06',
+							},
+							{
+								y: 88,
+								x: '2001-01-07',
+							},
+						],
+						tooltips: [
+							{
+								title: '$10.00',
+								body: '12 Donors',
+								footer: 'Jan 1',
+							},
+							{
+								title: '$10.00',
+								body: '12 Donors',
+								footer: 'Jan 2',
+							},
+							{
+								title: '$10.00',
+								body: '12 Donors',
+								footer: 'Jan 3',
+							},
+							{
+								title: '$10.00',
+								body: '12 Donors',
+								footer: 'Jan 4',
+							},
+							{
+								title: '$10.00',
+								body: '12 Donors',
+								footer: 'Jan 5',
+							},
+							{
+								title: '$20.00',
+								body: '3 Donors',
+								footer: 'Jan 6',
+							},
+							{
+								title: '$20.00',
+								body: '3 Donors',
+								footer: 'Jan 7',
+							},
+						],
+					},
+				],
+			};
+		case 'average-donation':
+			return {
+				datasets: [
+					{
+						trend: -5,
+						highlight: '$88.00',
+						info: 'VS previous 7 days',
+						data: [
+							{
+								y: 20,
+								x: '2001-01-01',
+							},
+							{
+								y: 88,
+								x: '2001-01-02',
+							},
+							{
+								y: 88,
+								x: '2001-01-03',
+							},
+							{
+								y: 88,
+								x: '2001-01-04',
+							},
+							{
+								y: 88,
+								x: '2001-01-05',
+							},
+							{
+								y: 88,
+								x: '2001-01-06',
+							},
+							{
+								y: 88,
+								x: '2001-01-07',
+							},
+						],
+						tooltips: [
+							{
+								title: '$10.00',
+								body: '12 Donors',
+								footer: 'Jan 1',
+							},
+							{
+								title: '$10.00',
+								body: '12 Donors',
+								footer: 'Jan 2',
+							},
+							{
+								title: '$10.00',
+								body: '12 Donors',
+								footer: 'Jan 3',
+							},
+							{
+								title: '$10.00',
+								body: '12 Donors',
+								footer: 'Jan 4',
+							},
+							{
+								title: '$10.00',
+								body: '12 Donors',
+								footer: 'Jan 5',
+							},
+							{
+								title: '$20.00',
+								body: '3 Donors',
+								footer: 'Jan 6',
+							},
+							{
+								title: '$20.00',
+								body: '3 Donors',
+								footer: 'Jan 7',
+							},
+						],
+					},
+				],
+			};
+		case 'total-refunds':
+			return {
+				datasets: [
+					{
+						trend: -5,
+						highlight: '9',
+						info: 'VS previous 7 days',
+						data: [
+							{
+								y: 20,
+								x: '2001-01-01',
+							},
+							{
+								y: 88,
+								x: '2001-01-02',
+							},
+							{
+								y: 88,
+								x: '2001-01-03',
+							},
+							{
+								y: 88,
+								x: '2001-01-04',
+							},
+							{
+								y: 88,
+								x: '2001-01-05',
+							},
+							{
+								y: 88,
+								x: '2001-01-06',
+							},
+							{
+								y: 88,
+								x: '2001-01-07',
+							},
+						],
+						tooltips: [
+							{
+								title: '$10.00',
+								body: '12 Donors',
+								footer: 'Jan 1',
+							},
+							{
+								title: '$10.00',
+								body: '12 Donors',
+								footer: 'Jan 2',
+							},
+							{
+								title: '$10.00',
+								body: '12 Donors',
+								footer: 'Jan 3',
+							},
+							{
+								title: '$10.00',
+								body: '12 Donors',
+								footer: 'Jan 4',
+							},
+							{
+								title: '$10.00',
+								body: '12 Donors',
+								footer: 'Jan 5',
+							},
+							{
+								title: '$20.00',
+								body: '3 Donors',
+								footer: 'Jan 6',
+							},
+							{
+								title: '$20.00',
+								body: '3 Donors',
+								footer: 'Jan 7',
 							},
 						],
 					},
