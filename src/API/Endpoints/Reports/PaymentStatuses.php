7ff53e9a6bcfea1d4ec93974fdce1bf8f72edf64
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-03-03 14:25:06 -0600

    fix: add failed and canelled payments to payment statuses chart

--- src/API/Endpoints/Reports/PaymentStatuses.php
+++ src/API/Endpoints/Reports/PaymentStatuses.php
@@ -47,10 +47,12 @@ class PaymentStatuses extends Endpoint {
 								$payments->pending,
 								$payments->refunded,
 								$payments->abandoned,
+								$payments->cancelled,
+								$payments->failed,
 							],
 							'tooltips' => [
 								[
-									'title'  => $payments->publish . ' ' . __( 'Payments', 'give' ),
+									'title'  => $payments->publish + $payments->give_subscription . ' ' . __( 'Payments', 'give' ),
 									'body'   => __( 'Completed', 'give' ),
 									'footer' => '',
 								],
@@ -67,6 +69,16 @@ class PaymentStatuses extends Endpoint {
 								[
 									'title'  => $payments->abandoned . ' ' . __( 'Payments', 'give' ),
 									'body'   => __( 'Abandoned', 'give' ),
+									'footer' => '',
+								],
+								[
+									'title'  => $payments->cancelled . ' ' . __( 'Payments', 'give' ),
+									'body'   => __( 'Cancelled', 'give' ),
+									'footer' => '',
+								],
+								[
+									'title'  => $payments->failed . ' ' . __( 'Payments', 'give' ),
+									'body'   => __( 'Failed', 'give' ),
 									'footer' => '',
 								],
 							],
