67c2f2ca5d17bde94ba5f0048426069dcf9ccc65
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-03-03 16:05:36 -0600

    fix: improve AverageDonation inline documentation

--- src/API/Endpoints/Reports/AverageDonation.php
+++ src/API/Endpoints/Reports/AverageDonation.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Income over time endpoint
+ * Average donation endpoint
  *
  * @package Give
  */
@@ -12,10 +12,17 @@ class AverageDonation extends Endpoint {
 
 	protected $payments;
 
+	/* Initialize endpoint and setup endpoint variable */
 	public function __construct() {
 		$this->endpoint = 'average-donation';
 	}
 
+	/**
+	 * Check for cached reports, and return WP_REST_Response with report data and Give status
+	 *
+	 * @param array $request Sanitized request parameters
+	 * @return \WP_REST_Response
+	 */
 	public function get_report( $request ) {
 
 		// Check if a cached version exists
@@ -31,9 +38,10 @@ class AverageDonation extends Endpoint {
 
 		$start = date_create( $request['start'] );
 		$end   = date_create( $request['end'] );
-		$diff  = date_diff( $start, $end );
 
+		// Setup intervalStr, to be passed to get_data() method
 		$intervalStr = 'P1D';
+		$diff        = date_diff( $start, $end );
 		switch ( true ) {
 			case ( $diff->days > 12 ):
 				$interval    = round( $diff->days / 12 );
@@ -50,12 +58,16 @@ class AverageDonation extends Endpoint {
 				break;
 		}
 
+		// Get datasets and tooltips for period, given an interval string
 		$data = $this->get_data( $start, $end, $intervalStr );
 
 		// Cache the report data
 		$result = $this->cache_report( $request, $data );
+
+		// Get Give status (either 'donations_found' or 'no_donations_found')
 		$status = $this->get_give_status();
 
+		// Return WP REST Response with data and Give status
 		return new \WP_REST_Response(
 			[
 				'data'   => $data,
@@ -64,62 +76,82 @@ class AverageDonation extends Endpoint {
 		);
 	}
 
+	/**
+	 * Get report datasets and tooltips, give the requested start, end and intervals
+	 *
+	 * @param DateTime $start The start of the requested period
+	 * @param DateTime $end The end of the requested period
+	 * @param string   $intervalStr The desired intervals to break requested period into
+	 */
 	public function get_data( $start, $end, $intervalStr ) {
 
+		// Setup payments for period
 		$this->payments = $this->get_payments( $start->format( 'Y-m-d' ), $end->format( 'Y-m-d' ) );
 
-		$income   = [];
-		$tooltips = [];
+		// Setup $donations and $tooltips arrays, to be filled by while loop
+		$donations = [];
+		$tooltips  = [];
 
+		// Setup DateInterval, used to advance while loop
 		$interval = new \DateInterval( $intervalStr );
 
+		// Setup $periodStart and $periodEnd, used to track while loop
 		$periodStart = clone $start;
 		$periodEnd   = clone $start;
 
 		// Subtract interval to set up period start
 		date_sub( $periodStart, $interval );
 
+		// Loop through intervals until end of period is reached
 		while ( $periodStart < $end ) {
 
+			// Get average donation for period
 			$averageForPeriod = $this->get_average_donation( $periodStart->format( 'Y-m-d H:i:s' ), $periodEnd->format( 'Y-m-d H:i:s' ) );
 
+			// Use special label if interval is only one hour
 			if ( $intervalStr == 'PT1H' ) {
 				$periodLabel = $periodStart->format( 'D ga' ) . ' - ' . $periodEnd->format( 'D ga' );
 			} else {
 				$periodLabel = $periodStart->format( 'M j, Y' ) . ' - ' . $periodEnd->format( 'M j, Y' );
 			}
 
-			$income[] = [
+			// Add datapoint to $donations array
+			$donations[] = [
 				'x' => $periodEnd->format( 'Y-m-d H:i:s' ),
 				'y' => $averageForPeriod,
 			];
 
+			// Add associated tooltips to $tooltips array
 			$tooltips[] = [
 				'title'  => give_currency_filter( give_format_amount( $averageForPeriod ), [ 'decode_currency' => true ] ),
 				'body'   => __( 'Avg Donation', 'give' ),
 				'footer' => $periodLabel,
 			];
 
-			// Add interval to set up next period
+			// Add interval to advance while loop
 			date_add( $periodStart, $interval );
 			date_add( $periodEnd, $interval );
 		}
 
-		$averageIncomeForPeriod = $this->get_average_donation( $start->format( 'Y-m-d H:i:s' ), $end->format( 'Y-m-d H:i:s' ) );
-		$trend                  = $this->get_trend( $start, $end, $income );
+		// Get average donation for entire requested period
+		$averageDonationForPeriod = $this->get_average_donation( $start->format( 'Y-m-d H:i:s' ), $end->format( 'Y-m-d H:i:s' ) );
 
+		// Get trend in average donations for entire requested period
+		$trend = $this->get_trend( $start, $end );
+
+		// Setup MiniChart info tooltip string
 		$diff = date_diff( $start, $end );
 		$info = $diff->days > 1 ? __( 'VS previous', 'give' ) . ' ' . $diff->days . ' ' . __( 'days', 'give' ) : __( 'VS previous day', 'give' );
 
-		// Create data objec to be returned, with 'highlights' object containing total and average figures to display
+		// Create data array to be returned
 		$data = [
 			'datasets' => [
 				[
-					'data'      => $income,
+					'data'      => $donations,
 					'tooltips'  => $tooltips,
 					'trend'     => $trend,
 					'info'      => $info,
-					'highlight' => give_currency_filter( give_format_amount( $averageIncomeForPeriod ), [ 'decode_currency' => true ] ),
+					'highlight' => give_currency_filter( give_format_amount( $averageDonationForPeriod ), [ 'decode_currency' => true ] ),
 				],
 			],
 		];
@@ -128,7 +160,15 @@ class AverageDonation extends Endpoint {
 
 	}
 
-	public function get_trend( $start, $end, $income ) {
+	/**
+	 * Get trend in average donations for period
+	 *
+	 * @param DateTime $start Start of period
+	 * @param DateTime $end End of period
+	 *
+	 * @return int
+	 */
+	public function get_trend( $start, $end ) {
 
 		$interval = $start->diff( $end );
 
@@ -159,6 +199,14 @@ class AverageDonation extends Endpoint {
 		return $trend;
 	}
 
+	/**
+	 * Get average donation for a give period, within payments array fetched during get_data()
+	 *
+	 * @param string $startStr String representation of period start date
+	 * @param string $endStr String representation of period end date
+	 *
+	 * @return int
+	 */
 	public function get_average_donation( $startStr, $endStr ) {
 
 		$earnings     = 0;
@@ -178,6 +226,14 @@ class AverageDonation extends Endpoint {
 		return $average;
 	}
 
+	/**
+	 * Get average donation for a give period, outside of payments array fetched during get_data()
+	 *
+	 * @param string $startStr String representation of period start date
+	 * @param string $endStr String representation of period end date
+	 *
+	 * @return int
+	 */
 	public function get_prev_average_donation( $startStr, $endStr ) {
 
 		$stats = new \Give_Payment_Stats();
