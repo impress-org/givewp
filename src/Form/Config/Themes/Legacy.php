f0d6549244db9179b81dd64b8e6ace58d4c63c34
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-03-05 00:13:07 +0530

    chore: add dummay field options

--- src/Form/Config/Themes/Legacy.php
+++ src/Form/Config/Themes/Legacy.php
@@ -17,6 +17,39 @@ return [
 						'placeholder' => __( 'Campaign Heading', 'give' ),
 					),
 				),
+				array(
+					'id'         => 'subheading',
+					'name'       => __( 'Sub Heading', 'give' ),
+					'desc'       => __( 'Set campaign sub heading.', 'give' ),
+					'type'       => 'text',
+					'attributes' => array(
+						'placeholder' => __( 'Campaign Sub Heading', 'give' ),
+					),
+				),
+			],
+		],
+		'thank-you'    => [
+			'name'   => __( 'Thank You', 'give' ),
+			'desc'   => __( 'Step description will show up here if any', 'give' ),
+			'fields' => [
+				array(
+					'id'         => 'heading',
+					'name'       => __( 'Heading', 'give' ),
+					'desc'       => __( 'Set campaign heading.', 'give' ),
+					'type'       => 'text',
+					'attributes' => array(
+						'placeholder' => __( 'Campaign Heading', 'give' ),
+					),
+				),
+				array(
+					'id'         => 'subheading',
+					'name'       => __( 'Sub Heading', 'give' ),
+					'desc'       => __( 'Set campaign sub heading.', 'give' ),
+					'type'       => 'text',
+					'attributes' => array(
+						'placeholder' => __( 'Campaign Sub Heading', 'give' ),
+					),
+				),
 			],
 		],
 	],
