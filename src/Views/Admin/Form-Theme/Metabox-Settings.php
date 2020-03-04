aa3801c6b0e4bf474c120381dbe9208ffbd2833c
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-03-04 17:11:08 +0530

    refactor: move notice to theme introduction tab

--- src/Views/Admin/Form-Theme/Metabox-Settings.php
+++ src/Views/Admin/Form-Theme/Metabox-Settings.php
@@ -27,11 +27,12 @@
 			<?php _e( 'What is a Form Theme', 'give' ); ?>
 		</strong>
 		<p><?php _e( 'In GiveWP, a form theme is a collection of templates and stylesheets used to define then appearance and display of a donation form on your website. Each one comes with a different design, layout and feature. All you need to do is choose the one that suits your taste and requirements for your cause.Compatibility with add-ons and third party plugins depend on the theme chosen. Be sure to test your donation form before going live to ensure smooth sailing!', 'give' ); ?></p>
-	</div>
-	<div class="give-notice notice notice-success inline">
-		<p>
-			<?php _e( 'More themes are coming soon! Let us know what you want to see next', 'give' ); ?>
-			<button class="button"><?php _e( 'Take the Survey', 'give' ); ?></button>
-		</p>
+
+		<div class="give-notice notice notice-success inline">
+			<p>
+				<?php _e( 'More themes are coming soon! Let us know what you want to see next', 'give' ); ?>
+				<button class="button"><?php _e( 'Take the Survey', 'give' ); ?></button>
+			</p>
+		</div>
 	</div>
 </div>
