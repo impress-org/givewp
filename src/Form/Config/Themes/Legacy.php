8fe84e1cdc5b6a71e66fb4d0deb1f19ca1926103
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-03-04 22:56:03 +0530

    chore: add dummy settings

--- src/Form/Config/Themes/Legacy.php
+++ src/Form/Config/Themes/Legacy.php
@@ -3,5 +3,21 @@ return [
 	'id'      => 'legacy',
 	'name'    => __( 'Legacy - Standard Form', 'give' ),
 	'image'   => '',
-	'options' => [],
+	'options' => [
+		'introduction' => [
+			'name'   => __( 'Introduction', 'give' ),
+			'desc'   => __( 'Step description will show up here if any', 'give' ),
+			'fields' => [
+				array(
+					'id'         => 'heading',
+					'name'       => __( 'Heading', 'give' ),
+					'desc'       => __( 'Set campaign heading.', 'give' ),
+					'type'       => 'text',
+					'attributes' => array(
+						'placeholder' => __( 'Campaign Heading', 'give' ),
+					),
+				),
+			],
+		],
+	],
 ];
--- src/Form/Config/Themes/Sequola.php
+++ src/Form/Config/Themes/Sequola.php
@@ -3,5 +3,21 @@ return [
 	'id'      => 'sequola',
 	'name'    => __( 'Sequola - Multi-Step Form', 'give' ),
 	'image'   => '',
-	'options' => [],
+	'options' => [
+		'introduction' => [
+			'name'   => __( 'Introduction', 'give' ),
+			'desc'   => __( 'Step description will show up here if any', 'give' ),
+			'fields' => [
+				array(
+					'id'         => 'heading',
+					'name'       => __( 'Heading', 'give' ),
+					'desc'       => __( 'Set campaign heading.', 'give' ),
+					'type'       => 'text',
+					'attributes' => array(
+						'placeholder' => __( 'Campaign Heading', 'give' ),
+					),
+				),
+			],
+		],
+	],
 ];
