a2b3dc9d537c07fa98450903759514ef3e141f78
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-03-05 16:36:17 +0530

    chore: use array short tags

--- src/Form/Config/Themes/Legacy.php
+++ src/Form/Config/Themes/Legacy.php
@@ -8,48 +8,48 @@ return [
 			'name'   => __( 'Introduction', 'give' ),
 			'desc'   => __( 'Step description will show up here if any', 'give' ),
 			'fields' => [
-				array(
+				[
 					'id'         => 'heading',
 					'name'       => __( 'Heading', 'give' ),
 					'desc'       => __( 'Set campaign heading.', 'give' ),
 					'type'       => 'text',
-					'attributes' => array(
+					'attributes' => [
 						'placeholder' => __( 'Campaign Heading', 'give' ),
-					),
-				),
-				array(
+					],
+				],
+				[
 					'id'         => 'subheading',
 					'name'       => __( 'Sub Heading', 'give' ),
 					'desc'       => __( 'Set campaign sub heading.', 'give' ),
 					'type'       => 'text',
-					'attributes' => array(
+					'attributes' => [
 						'placeholder' => __( 'Campaign Sub Heading', 'give' ),
-					),
-				),
+					],
+				],
 			],
 		],
 		'thank-you'    => [
 			'name'   => __( 'Thank You', 'give' ),
 			'desc'   => __( 'Step description will show up here if any', 'give' ),
 			'fields' => [
-				array(
+				[
 					'id'         => 'heading',
 					'name'       => __( 'Heading', 'give' ),
 					'desc'       => __( 'Set campaign heading.', 'give' ),
 					'type'       => 'text',
-					'attributes' => array(
+					'attributes' => [
 						'placeholder' => __( 'Campaign Heading', 'give' ),
-					),
-				),
-				array(
+					],
+				],
+				[
 					'id'         => 'subheading',
 					'name'       => __( 'Sub Heading', 'give' ),
 					'desc'       => __( 'Set campaign sub heading.', 'give' ),
 					'type'       => 'text',
-					'attributes' => array(
+					'attributes' => [
 						'placeholder' => __( 'Campaign Sub Heading', 'give' ),
-					),
-				),
+					],
+				],
 			],
 		],
 	],
--- src/Form/Config/Themes/Sequola.php
+++ src/Form/Config/Themes/Sequola.php
@@ -8,66 +8,48 @@ return [
 			'name'   => __( 'Introduction', 'give' ),
 			'desc'   => __( 'Step description will show up here if any', 'give' ),
 			'fields' => [
-				array(
+				[
 					'id'         => 'heading',
 					'name'       => __( 'Heading', 'give' ),
 					'desc'       => __( 'Set campaign heading.', 'give' ),
 					'type'       => 'text',
-					'attributes' => array(
+					'attributes' => [
 						'placeholder' => __( 'Campaign Heading', 'give' ),
-					),
-				),
-				array(
+					],
+				],
+				[
 					'id'         => 'subheading',
 					'name'       => __( 'Sub Heading', 'give' ),
 					'desc'       => __( 'Set campaign sub heading.', 'give' ),
 					'type'       => 'text',
-					'attributes' => array(
+					'attributes' => [
 						'placeholder' => __( 'Campaign Sub Heading', 'give' ),
-					),
-				),
-				array(
-					'id'            =>  'emails',
-					'type'          => 'group',
-					'options'       => array(
-						'add_button'    => __( 'Add Email', 'give' ),
-						'header_title'  => __( 'Admin Email', 'give' ),
-						'remove_button' => '<span class="dashicons dashicons-no"></span>',
-					),
-					// Fields array works the same, except id's only need to be unique for this group.
-					// Prefix is not needed.
-					'fields'        => array(
-						array(
-							'name' => __( 'ID', 'give' ),
-							'id'   => 'id',
-							'type' => 'text',
-						)
-					)
-				)
+					],
+				],
 			],
 		],
 		'thank-you'    => [
 			'name'   => __( 'Thank You', 'give' ),
 			'desc'   => __( 'Step description will show up here if any', 'give' ),
 			'fields' => [
-				array(
+				[
 					'id'         => 'heading',
 					'name'       => __( 'Heading', 'give' ),
 					'desc'       => __( 'Set campaign heading.', 'give' ),
 					'type'       => 'text',
-					'attributes' => array(
+					'attributes' => [
 						'placeholder' => __( 'Campaign Heading', 'give' ),
-					),
-				),
-				array(
+					],
+				],
+				[
 					'id'         => 'subheading',
 					'name'       => __( 'Sub Heading', 'give' ),
 					'desc'       => __( 'Set campaign sub heading.', 'give' ),
 					'type'       => 'text',
-					'attributes' => array(
+					'attributes' => [
 						'placeholder' => __( 'Campaign Sub Heading', 'give' ),
-					),
-				),
+					],
+				],
 			],
 		],
 	],
