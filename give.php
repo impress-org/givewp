19146a799b9ad69cd5a3b38c74d0415db4fcd82c
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-03-06 21:29:11 +0530

    chore: remove unnecessary file load

--- give.php
+++ give.php
@@ -579,8 +579,6 @@ if ( ! class_exists( 'Give' ) ) :
 			require_once GIVE_PLUGIN_DIR . 'includes/forms/route.php';
 			require_once GIVE_PLUGIN_DIR . 'templates/form-styles/elegent/actions.php';
 			require_once GIVE_PLUGIN_DIR . 'templates/form-styles/elegent/filters.php';
-			require_once GIVE_PLUGIN_DIR . 'src/Form/Helpers/Theme.php';
-			require_once GIVE_PLUGIN_DIR . 'src/Form/Helpers/Themes.php';
 
 			require_once GIVE_PLUGIN_DIR . 'includes/process-donation.php';
 			require_once GIVE_PLUGIN_DIR . 'includes/payments/backward-compatibility.php';
