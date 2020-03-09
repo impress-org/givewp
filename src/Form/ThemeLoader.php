555a1795ba0727fa820c1126a9ab99b81e1b7035
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-03-09 17:12:16 +0530

    chore: add basisc class for form them loading

---
+++ src/Form/ThemeLoader.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * Handle Theme Loading Handler
+ *
+ * @package Give
+ * @since 2.7.0
+ */
+
+namespace Give\Form;
+
+use function Give\Helpers\Form\Theme\get as getThemeSettings;
+use function Give\Helpers\Form\Theme\getActiveID;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * ThemeLoader class.
+ *
+ * @since 2.7.0
+ */
+class ThemeLoader {
+	/**
+	 * Saved form theme settings
+	 *
+	 * @var array
+	 */
+	private $themeSettings = [];
+
+	/**
+	 * Form theme config.
+	 *
+	 * @var array
+	 */
+	private $themeConfig = [];
+
+	/**
+	 * Activate form theme id.
+	 *
+	 * @var string
+	 */
+	private $activeThemeID;
+
+	/**
+	 * Form ID.
+	 *
+	 * @var string
+	 */
+	private $formID;
+
+	/**
+	 * Form Theme loading handler
+	 *
+	 * @param int $formID
+	 */
+	public function __construct( $formID = 0 ) {
+		global $post;
+
+		$this->formID = $formID ?: $post->ID;
+
+		$this->activeThemeID = getActiveID( $this->formID );
+		$this->themeSettings = getThemeSettings( $this->formID );
+		$this->themeConfig   = Give()->themes->getTheme( $this->activeThemeID );
+	}
+
+
+	/**
+	 * Initialize form theme
+	 */
+	public function init() {
+		$entryFilePath = $this->getThemePath() . 'functions.php';
+
+		// Exit.
+		if ( ! file_exists( $entryFilePath ) ) {
+			return;
+		}
+
+		require_once $entryFilePath;
+	}
+
+
+	/**
+	 * Get form theme path
+	 *
+	 * @return string
+	 */
+	private function getThemePath() {
+		return $this->themeConfig['entry'];
+	}
+}
