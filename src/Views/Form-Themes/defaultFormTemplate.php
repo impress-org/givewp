594aa20bf0fe7ba8389e180a9007bd8d806d6eae
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-03-09 18:52:48 +0530

    chore: add theme loader logic

--- src/Views/Form-Themes/defaultFormTemplate.php
+++ src/Views/Form-Themes/defaultFormTemplate.php
@@ -1,13 +1,17 @@
 <?php
 use Give\Form\ThemeLoader;
+
+global $post;
+
+
+// Load form theme.
+$themeLoader = new ThemeLoader( $post->ID );
+$themeLoader->init();
+
+$atts = array( 'display_style' => 'onpage' );
 ?>
 <!DOCTYPE html>
 <html lang="en" class="give-form-styles" style="margin-top: 0 !important;">
-	<?php
-	global $post;
-
-	$atts = array( 'display_style' => 'onpage' );
-	?>
 	<head>
 		<meta charset="utf-8">
 		<title><?php echo esc_html( $post->post_title ); ?></title>
@@ -21,10 +25,6 @@ use Give\Form\ThemeLoader;
 	</head>
 	<body>
 		<?php
-		// Load form theme.
-		$themeLoader = new ThemeLoader( $post->ID );
-		$themeLoader->init();
-
 		// Fetch the Give Form.
 		ob_start();
 		give_get_donation_form( array_map( 'give_clean', wp_parse_args( $_SERVER['QUERY_STRING'] ) ) );
