344966371d2074624b99b3b2abfa660eb3a42815
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-03-09 19:26:04 +0530

    chore: load core stylesheet for theme

--- src/Views/Form-Themes/Sequoia/scripts.php
+++ src/Views/Form-Themes/Sequoia/scripts.php
@@ -4,20 +4,19 @@ use function Give\Helpers\Script\getLocalizedScript;
 use function Give\Helpers\Script\getScripTag;
 use function Give\Helpers\Script\getStyleTag;
 
-add_action( 'give_embed_head', array( $this, 'give_ft_sequoia_page_styles' ) );
-add_action( 'give_embed_footer', array( $this, 'give_ft_sequoia_page_scripts' ) );
+add_action( 'give_embed_head', 'give_ft_sequoia_page_styles' );
+add_action( 'give_embed_footer', 'give_ft_sequoia_page_scripts' );
 
 
 /**
  * Load form theme style
  */
 function give_ft_sequoia_page_styles() {
-	// echo getStyleTag( $this->get_frontend_stylesheet_uri() );
-
+	echo getStyleTag( Give()->scripts->get_frontend_stylesheet_uri() );
 	echo getStyleTag( GIVE_PLUGIN_URL . 'assets/dist/css/give-elegent-theme.css' );
 	echo getStyleTag( 'https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap' );
 
-	echo getLocalizedScript( 'give_global_vars', $this->get_public_data() );
+	echo getLocalizedScript( 'give_global_vars', Give()->scripts->get_public_data() );
 }
 
 
@@ -53,5 +52,5 @@ function give_ft_sequoia_page_scripts() {
 	echo getScripTag( GIVE_PLUGIN_URL . 'assets/dist/js/give.js' );
 	echo getScripTag( GIVE_PLUGIN_URL . 'assets/dist/js/give-elegent-theme.js' );
 
-	$this->stripe_frontend_scripts();
+	Give()->scripts->stripe_frontend_scripts();
 }
