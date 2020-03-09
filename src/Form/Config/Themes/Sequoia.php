87ffe2f9c3e049deb0f3bc85c43dfd8dbf1d4c09
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-03-09 18:18:25 -0400

    fix: improve sequoia options

--- src/Form/Config/Themes/Sequoia.php
+++ src/Form/Config/Themes/Sequoia.php
@@ -9,22 +9,34 @@ return [
 			'desc'   => __( 'Step description will show up here if any', 'give' ),
 			'fields' => [
 				[
-					'id'         => 'heading',
-					'name'       => __( 'Heading', 'give' ),
-					'desc'       => __( 'Set campaign heading.', 'give' ),
+					'id'         => 'intro_headline',
+					'name'       => __( 'Headline', 'give' ),
+					'desc'       => __( 'Do you want to customize the headline for this form? We recommend keeping it to no more than 8 words as a best practive. If no title is provided the fallback will be your form’s post title.', 'give' ),
 					'type'       => 'text',
 					'attributes' => [
 						'placeholder' => __( 'Campaign Heading', 'give' ),
 					],
 				],
 				[
-					'id'         => 'subheading',
-					'name'       => __( 'Sub Heading', 'give' ),
-					'desc'       => __( 'Set campaign sub heading.', 'give' ),
-					'type'       => 'text',
+					'id'         => 'intro_description',
+					'name'       => __( 'Description', 'give' ),
+					'desc'       => __( 'Do you want to customize the description for this form? The description displays below the headline. We recommend keeping it to 1-2 short sentences. If no description is provided the fallback will be your form’s excerpt.', 'give' ),
+					'type'       => 'textarea',
 					'attributes' => [
-						'placeholder' => __( 'Campaign Sub Heading', 'give' ),
+						'placeholder' => __( 'Help provide education, care, and community development. It couldn’t happen without you.', 'give' ),
 					],
+				],
+				[
+					'id'   => 'intro_image',
+					'name' => __( 'Image', 'give' ),
+					'desc' => __( 'Upload an eye-catching image that reflects your cause. The image is required and if none is provided the featured image will be a the fallback. If none is set you will see a placeholder image displayed on the form. For best results use an image that’s 600x400 pixels.', 'give' ),
+					'type' => 'upload',
+				],
+				[
+					'id'   => 'intro_primary_color',
+					'name' => __( 'Primary Color', 'give' ),
+					'desc' => __( 'The primary color is used through the Form Theme for various elements including buttons, line breaks, and focus and hover elements. Set a color that reflects your brand or main featured image for best results.', 'give' ),
+					'type' => 'color',
 				],
 			],
 		],
@@ -33,14 +45,13 @@ return [
 			'desc'   => __( 'Step description will show up here if any', 'give' ),
 			'fields' => [
 				[
-					'id'            => 'sequoia_reveal_label',
-					'name'          => __( 'Continue Button', 'give' ),
-					'desc'          => __( 'The button label for displaying the additional payment fields.', 'give' ),
-					'type'          => 'text_small',
-					'attributes'    => [
+					'id'         => 'sequoia_reveal_label',
+					'name'       => __( 'Continue Button', 'give' ),
+					'desc'       => __( 'The button label for displaying the additional payment fields.', 'give' ),
+					'type'       => 'text_small',
+					'attributes' => [
 						'placeholder' => __( 'Donate Now', 'give' ),
 					],
-					'wrapper_class' => 'give-hidden',
 				],
 			],
 		],
@@ -168,22 +179,28 @@ return [
 			'desc'   => __( 'Step description will show up here if any', 'give' ),
 			'fields' => [
 				[
-					'id'         => 'heading',
-					'name'       => __( 'Heading', 'give' ),
-					'desc'       => __( 'Set campaign heading.', 'give' ),
+					'id'         => 'thank_you_headline',
+					'name'       => __( 'Headline', 'give' ),
+					'desc'       => __( 'This message should be short and sweet. Make the donor feel good about their donation so they continue to give in the future. This text is required and you may use any of the available template tags within this message.', 'give' ),
 					'type'       => 'text',
 					'attributes' => [
-						'placeholder' => __( 'Campaign Heading', 'give' ),
+						'placeholder' => __( 'A great big thank you!', 'give' ),
 					],
 				],
 				[
-					'id'         => 'subheading',
-					'name'       => __( 'Sub Heading', 'give' ),
-					'desc'       => __( 'Set campaign sub heading.', 'give' ),
-					'type'       => 'text',
+					'id'         => 'thank_you_description',
+					'name'       => __( 'Description', 'give' ),
+					'desc'       => __( 'The description is displayed directly below the main headline and should be 1-2 sentences for best performance. You may use any of the available template tags within this message.', 'give' ),
+					'type'       => 'textarea',
 					'attributes' => [
-						'placeholder' => __( 'Campaign Sub Heading', 'give' ),
+						'placeholder' => __( '{name}, you contribution means a lot and will be put to good use making a difference. We’ve sent your donation receipt to {donor_email}. ', 'give' ),
 					],
+				],
+				[
+					'id'   => 'thank_you_image',
+					'name' => __( 'Image', 'give' ),
+					'desc' => __( 'Upload an eye-catching image that reflects your cause. The image is required and if none is provided the featured image will be a the fallback. If none is set you will see a placeholder image displayed on the form. For best results use an image that’s 600x400 pixels.', 'give' ),
+					'type' => 'upload',
 				],
 			],
 		],
