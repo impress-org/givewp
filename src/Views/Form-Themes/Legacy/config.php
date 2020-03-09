ad8ae5ef8e96d26ace176d670bbefc2e0274d0bd
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-03-09 11:12:57 +0530

    chore: move config files to there theme dir

---
+++ src/Views/Form-Themes/Legacy/config.php
@@ -0,0 +1,57 @@
+<?php
+return [
+	'id'      => 'legacy',
+	'name'    => __( 'Legacy - Standard Form', 'give' ),
+	'image'   => '',
+	'entry'   => GIVE_PLUGIN_DIR . 'src/Views/Form-Themes/Sequoia',
+	'options' => [
+		'introduction' => [
+			'name'   => __( 'Introduction', 'give' ),
+			'desc'   => __( 'Step description will show up here if any', 'give' ),
+			'fields' => [
+				[
+					'id'         => 'heading',
+					'name'       => __( 'Heading', 'give' ),
+					'desc'       => __( 'Set campaign heading.', 'give' ),
+					'type'       => 'text',
+					'attributes' => [
+						'placeholder' => __( 'Campaign Heading', 'give' ),
+					],
+				],
+				[
+					'id'         => 'subheading',
+					'name'       => __( 'Sub Heading', 'give' ),
+					'desc'       => __( 'Set campaign sub heading.', 'give' ),
+					'type'       => 'text',
+					'attributes' => [
+						'placeholder' => __( 'Campaign Sub Heading', 'give' ),
+					],
+				],
+			],
+		],
+		'thank-you'    => [
+			'name'   => __( 'Thank You', 'give' ),
+			'desc'   => __( 'Step description will show up here if any', 'give' ),
+			'fields' => [
+				[
+					'id'         => 'heading',
+					'name'       => __( 'Heading', 'give' ),
+					'desc'       => __( 'Set campaign heading.', 'give' ),
+					'type'       => 'text',
+					'attributes' => [
+						'placeholder' => __( 'Campaign Heading', 'give' ),
+					],
+				],
+				[
+					'id'         => 'subheading',
+					'name'       => __( 'Sub Heading', 'give' ),
+					'desc'       => __( 'Set campaign sub heading.', 'give' ),
+					'type'       => 'text',
+					'attributes' => [
+						'placeholder' => __( 'Campaign Sub Heading', 'give' ),
+					],
+				],
+			],
+		],
+	],
+];
