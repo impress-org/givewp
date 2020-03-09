af0bdb4bfed2cb752dc5d97229ba0b88e5e9cf86
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-03-09 17:17:40 +0530

    chore: init theme load

--- src/Views/Form-Themes/defaultFormTemplate.php
+++ src/Views/Form-Themes/defaultFormTemplate.php
@@ -1,7 +1,11 @@
+<?php
+use Give\Form\ThemeLoader;
+?>
 <!DOCTYPE html>
 <html lang="en" class="give-form-styles" style="margin-top: 0 !important;">
 	<?php
 	global $post;
+
 	$atts = array( 'display_style' => 'onpage' );
 	?>
 	<head>
@@ -17,6 +21,10 @@
 	</head>
 	<body>
 		<?php
+		// Load form theme.
+		$themeLoader = new ThemeLoader( $post->ID );
+		$themeLoader->init();
+
 		// Fetch the Give Form.
 		ob_start();
 		give_get_donation_form( array_map( 'give_clean', wp_parse_args( $_SERVER['QUERY_STRING'] ) ) );
