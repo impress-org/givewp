3177c7959810d10142764d707e719e68ae154bd7
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-03-14 00:33:57 +0530

    chore: load Sequoia theme in footer

--- src/Views/Form/Themes/Sequoia/Scripts.php
+++ src/Views/Form/Themes/Sequoia/Scripts.php
@@ -19,9 +19,7 @@ class Scripts {
 	public function init() {
 		wp_enqueue_style( 'give-google-font-montserrat', 'https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap', array(), GIVE_VERSION );
 		wp_enqueue_style( 'give-sequoia-theme-css', GIVE_PLUGIN_URL . 'assets/dist/css/give-sequoia-theme.css', array( 'give-styles' ), GIVE_VERSION );
-
-		Give_Scripts::register_script( 'give-sequoia-theme-js', GIVE_PLUGIN_URL . 'assets/dist/js/give-sequoia-theme.js', array( 'give' ), GIVE_VERSION );
-		wp_enqueue_script( 'give-sequoia-theme-js' );
+		wp_enqueue_script( 'give-sequoia-theme-js', GIVE_PLUGIN_URL . 'assets/dist/js/give-sequoia-theme.js', array( 'give' ), GIVE_VERSION, true );
 	}
 }
 
