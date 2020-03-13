b17e0b4fe84f46c5224f896573618d7afb313dee
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-03-13 18:17:54 +0530

    chore: add basic interfaces

---
+++ src/Form/Theme/Scriptable.php
@@ -0,0 +1,19 @@
+<?php
+namespace Give\Form\Theme;
+
+/**
+ * Interface Scriptable
+ *
+ * @since 2.7.0
+ * @package Give\Form\Theme
+ */
+interface Scriptable {
+
+	/**
+	 * Load scripts and styles
+	 *
+	 * @since 2.7.0
+	 * @return mixed
+	 */
+	public function loadScripts();
+}
---
+++ src/Form/Theme/Themeable.php
@@ -0,0 +1,11 @@
+<?php
+namespace Give\Form\Theme;
+
+/**
+ * Interface Themeable
+ *
+ * @since 2.7.0
+ * @package Give\Form\Theme
+ */
+interface Themeable extends Hookable, Scriptable {
+}
