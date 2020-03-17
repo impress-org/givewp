1a8da0cbcac3502c6538569f59a84c43d917318d
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-03-17 15:44:21 +0530

    chore: add basic field types

--- src/FormAPI/Field.php
+++ src/FormAPI/Field.php
@@ -4,92 +4,80 @@ namespace Give\FormAPI;
 use InvalidArgumentException;
 
 class Field {
-
 	/**
-	 * Field id
+	 * Get field object.
 	 *
 	 * @since 2.7.0
-	 * @var string
-	 */
-	public $id;
-
-	/**
-	 * Field name
-	 *
-	 * @since 2.7.0
-	 * @var string
-	 */
-	public $name;
-
-	/**
-	 * Field description
-	 *
-	 * @since 2.7.0
-	 * @var string
-	 */
-	public $desc;
-
-	/**
-	 * Field type
-	 *
-	 * @since 2.7.0
-	 * @var string
-	 */
-	public $type;
-
-	/**
-	 * Field arguments
-	 *
-	 * @since 2.7.0
-	 * @var array
-	 */
-	private $array;
-
-
-	/**
 	 * @param array $array
 	 *
-	 * @return static
+	 * @return Form\Field
 	 */
 	public static function fromArray( $array ) {
 		$field = new static();
-
 		$field->validate( $array );
 
-		$field->id   = $array['id'];
-		$field->name = $array['name'];
-		$field->type = $array['type'];
-		$field->desc = isset( $array['desc'] ) ? $array['desc'] : null;
-
-		$field->array = $array;
-
-		return $field;
+		return $field->parse( $array );
 	}
 
+
+	/**
+	 * Parse field argument and return object
+	 *
+	 * @since 2.7.0
+	 * @param array $array
+	 * @return Form\Field
+	 */
+	private function parse( $array ) {
+		/**
+		 * Field type can contain multiple words join with underscore.
+		 * First word will give actual type for field and other will be modifier.
+		 * For example text_small where text is actual field type and small is a modifier.
+		 */
+		$type = $array['type'];
+
+		/* @var Form\Field $class */
+		$class = 'Give\FormAPI\Form\\' . $this->getFieldClassName( $type );
+
+		if ( ! class_exists( $class ) ) {
+			throw new InvalidArgumentException( __( "{$type} field type is not supported by field API.", 'give' ) );
+		}
+
+		return $class::fromArray( $array );
+	}
+
+	/**
+	 * Get field class name.
+	 *
+	 * @since 2.7.0
+	 * @param $type
+	 *
+	 * @return string
+	 */
+	private function getFieldClassName( $type ) {
+		$type = ucwords( $type );
+
+		if ( false !== strpos( $type, '_' ) ) {
+			$type = current( explode( '_', $type, 2 ) );
+		}
+
+		return $type;
+	}
 
 	/**
 	 * Validate field arguments
 	 *
 	 * @since 2.7.0
-	 * @param $array
+	 *
+	 * @param array $array
+	 *
+	 * @throws InvalidArgumentException
 	 */
 	private function validate( $array ) {
 		$required = array( 'id', 'name', 'type' );
+		$array    = array_filter( $array ); // Remove empty values.
 
 		if ( array_diff( $required, array_keys( $array ) ) ) {
-			throw new InvalidArgumentException( __( 'To create a field object, please provide id, name and type.', 'give' ) );
+			throw new InvalidArgumentException( __( 'To create a TextField object, please provide valid id, name and type.', 'give' ) );
 		}
-	}
-
-
-	/**
-	 * Get form metabox api compatible arguments
-	 * Note: for internal use only nd can be remove in future with any backward compatibility.
-	 *
-	 * @since 2.7.0
-	 * @return array
-	 */
-	public function getFormMetaboxFieldArguments() {
-		return $this->array;
 	}
 }
