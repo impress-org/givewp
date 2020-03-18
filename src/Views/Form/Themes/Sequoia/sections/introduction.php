6cead343a7204232887609070cc32aae6bf974ae
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-03-18 11:28:02 -0400

    fix: add image container markup

--- src/Views/Form/Themes/Sequoia/sections/introduction.php
+++ src/Views/Form/Themes/Sequoia/sections/introduction.php
@@ -10,12 +10,14 @@ $image       = $theme_options['introduction']['image'];
 ?>
 
 <div class="give-section introduction">
-	<div class="heading">
-		<h2 class="give-form-title"><?php echo $headline; ?></h2>
-	</div>
+	<h2>
+		<?php echo $headline; ?>
+	</h2>
 	<div class="seperator"></div>
-	<div class="subheading text">
-		<p><?php echo $description; ?></p>
+	<p>
+		<?php echo $description; ?>
+	</p>
+	<div class="image-container">
+		<img src="<?php echo $image; ?>" />
 	</div>
-	<img src="<?php echo $image; ?>" />
 </div>
