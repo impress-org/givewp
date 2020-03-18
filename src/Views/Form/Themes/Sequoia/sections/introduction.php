97e5b77fff4964889b86125cbaf0369223a3d35d
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-03-18 11:30:12 -0400

    feature: set seperator color from primary color option

--- src/Views/Form/Themes/Sequoia/sections/introduction.php
+++ src/Views/Form/Themes/Sequoia/sections/introduction.php
@@ -7,13 +7,14 @@ $theme_options = give_get_meta( $form->ID, '_give_sequoia_form_theme_settings', 
 $headline    = $theme_options['introduction']['headline'];
 $description = $theme_options['introduction']['description'];
 $image       = $theme_options['introduction']['image'];
+$color       = $theme_options['introduction']['primary_color'];
 ?>
 
 <div class="give-section introduction">
 	<h2>
 		<?php echo $headline; ?>
 	</h2>
-	<div class="seperator"></div>
+	<div class="seperator" style="background: <?php echo $color; ?>"></div>
 	<p>
 		<?php echo $description; ?>
 	</p>
