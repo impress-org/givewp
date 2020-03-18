bb2f1d8e2cd259651ab869ed3bfc192c87312b5c
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-03-18 10:08:31 -0400

    feature: change goal values/labels based on format

--- src/Views/Form/Themes/Sequoia/sections/form-income-stats.php
+++ src/Views/Form/Themes/Sequoia/sections/form-income-stats.php
@@ -1,44 +1,72 @@
-<div class="give-section form-stats">
-	<div class="amount-raised">
-		<div class="number"><?php echo give_currency_filter(
-			give_format_amount(
-				$form->get_earnings(),
-				array(
-					'sanitize' => false,
-					'decimal'  => false,
-				)
-			)
-		); ?></div>
-		<div class="text">raised</div>
-	</div>
-	<div class="donation-count">
-		<div class="number">
-		<?php
-		echo give_format_amount(
-			$form->get_sales(),
+<?php
+
+	$goal_stats = give_goal_progress_stats( $form_id );
+	echo '<h1>' . $goal_stats['format'] . '</h1>';
+
+	// Setup default raised value
+	$raised = give_currency_filter(
+		give_format_amount(
+			$form->get_earnings(),
 			array(
 				'sanitize' => false,
 				'decimal'  => false,
 			)
-		);
-		?>
+		)
+	);
+
+	// Setup default count value
+	$count = $form->get_sales();
+
+	// Setup default count label
+	$countLabel = __( 'donations', 'give' );
+
+	// Setup default goal value
+	$goal = give_currency_filter(
+		give_format_amount(
+			$form->get_goal(),
+			array(
+				'sanitize' => false,
+				'decimal'  => false,
+			)
+		)
+	);
+
+	// Change values and labels based on goal format
+	switch ( $goal_stats['format'] ) {
+		case 'percentage': {
+			$raised = "{$goal_stats['progress']}%";
+			break;
+		}
+		case 'donation': {
+			$count = $goal_stats['actual'];
+			$goal  = $goal_stats['goal'];
+			break;
+		}
+		case 'donors': {
+			$count      = $goal_stats['actual'];
+			$countLabel = __( 'donors', 'give' );
+			$goal       = $goal_stats['goal'];
+			break;
+		}
+	}
+	?>
+<div class="give-section form-stats">
+	<div class="raised">
+		<div class="number">
+			<?php echo $raised; ?>
 		</div>
-		<div class="text">donations</div>
+		<div class="text"><?php _e( 'raised', 'give' ); ?></div>
+	</div>
+	<div class="count">
+		<div class="number">
+			<?php echo $count; ?>
+		</div>
+		<div class="text"><?php echo $countLabel; ?></div>
 	</div>
 	<?php if ( $form->has_goal() ) : ?>
 		<div class="goal">
 			<div class="number">
-			<?php
-			echo give_currency_filter(
-				give_format_amount(
-					$form->get_goal(),
-					array(
-						'sanitize' => false,
-						'decimal'  => false,
-					)
-				)
-			);
-			?>
+			<?php echo $goal; ?>
 			</div>
 			<div class="text">goal</div>
 		</div>
