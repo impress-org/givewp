63db2038efc9c5ba0bf2fe168e4e8a2b8b7b788b
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-03-18 09:31:12 -0400

    feature: use custom goal color for gradient

--- src/Views/Form/Themes/Sequoia/sections/progress-bar.php
+++ src/Views/Form/Themes/Sequoia/sections/progress-bar.php
@@ -5,7 +5,7 @@
 
 	$style = "width:{$goal_stats['progress']}%;";
 	if ( ! empty( $color ) ) {
-		$style .= ";background-color:{$color};";
+		$style .= ";background: linear-gradient(180deg, {$color} 0%, {$color} 100%), linear-gradient(180deg, #fff 0%, #ccc 100%); background-blend-mode: multiply;";
 	}
 	?>
 	<div class="give-section progress-bar">
