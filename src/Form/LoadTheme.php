965ab69fb3f6b4ded10c33df1a828a67b7830e3e
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-03-19 23:39:01 +0530

    chore: update doc block

--- src/Form/LoadTheme.php
+++ src/Form/LoadTheme.php
@@ -43,8 +43,8 @@ class LoadTheme {
 	 * setup form template
 	 *
 	 * @since 2.7.0
-	 * @param string $formTemplate
-	 * @param int    $formId
+	 * @param string $formTemplate Form template id.
+	 * @param int    $formId Form Id. Default value: check explanation in src/Helpers/Form/Utils.php:103
 	 */
 	private function setUpTemplate( $formTemplate, $formId = null ) {
 		$formID = (int) ( $formId ?: getFormId() );
