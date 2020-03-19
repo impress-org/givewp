d42d0c82444369ea46f72658d0c57e1fd65d2fba
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-03-20 01:23:04 +0530

    chore: remove intermediate button in embed mode

--- src/Views/Form/Themes/Sequoia/Actions.php
+++ src/Views/Form/Themes/Sequoia/Actions.php
@@ -83,6 +83,9 @@ class Actions {
 		// Remove goal.
 		remove_action( 'give_pre_form', 'give_show_goal_progress', 10 );
 
+		// Remove intermediate continue button which appear when display style set to other then onpage.
+		remove_action( 'give_after_donation_levels', 'give_display_checkout_button', 10 );
+
 		// Hide title.
 		add_filter( 'give_form_title', '__return_empty_string' );
 
