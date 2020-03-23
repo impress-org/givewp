92b3bb46e50649b81fa5d6068c367baec39ba5f9
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-03-23 22:20:13 +0530

    chore: use iframe object from scope

--- assets/src/js/plugins/form-theme/utils.js
+++ assets/src/js/plugins/form-theme/utils.js
@@ -18,17 +18,25 @@ export const initializeIframeResize = function( iframe ) {
 			heightCalculationMethod: 'documentElementOffset',
 			widthCalculationMethod: 'documentElementOffset',
 			onMessage: function( messageData ) {
-				const iframe = messageData.iframe;
-
 				switch ( messageData.message ) {
 					case 'giveEmbedFormContentLoaded':
-						iframe.parentElement.classList.remove( 'give-loader-type-img' );
+						let parent = iframe.parentElement;
+						if ( iframe.parentElement.classList.contains( 'modal-content' ) ) {
+							parent = parent.parentElement.parentElement;
+						}
+
+						parent.classList.remove( 'give-loader-type-img' );
 						iframe.style.visibility = 'visible';
 
 						break;
 				}
 			},
 			onScroll: ( { x, y } ) => {
+				// No need to auto scroll if form loaded in modal.
+				if ( iframe.parentElement.classList.contains( 'modal-content' ) ) {
+					return false;
+				}
+
 				jQuery( 'html, body' ).animate( { scrollTop: y, scrollLeft: x } );
 
 				return false;
