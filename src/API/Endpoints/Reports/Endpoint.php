a7fb4036d29bff4ea03121a46537e50b222e0ad2
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-03-24 11:59:00 +0530

    fix: return whole data as result

--- src/API/Endpoints/Reports/Endpoint.php
+++ src/API/Endpoints/Reports/Endpoint.php
@@ -92,11 +92,7 @@ abstract class Endpoint {
 		$cached_report = $this->get_cached_report( $request );
 		if ( $cached_report !== null ) {
 			// Bail and return the cached version
-			return new WP_REST_Response(
-				array(
-					'data' => $cached_report,
-				)
-			);
+			return new WP_REST_Response( $cached_report );
 		}
 
 		$this->setupProperties( $request );
