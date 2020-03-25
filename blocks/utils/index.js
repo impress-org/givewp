9b0f9cf796c7b8015e61ae550271b2d09c24c2dc
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-03-25 21:25:26 +0530

    chore: add getFormOptions helper fn

--- blocks/utils/index.js
+++ blocks/utils/index.js
@@ -1,7 +1,41 @@
 /**
+ * External depedencies
+ */
+import { isUndefined } from 'lodash';
+
+/**
  * getSiteUrl from API root
  * @returns {string} siteurl
  */
 export function getSiteUrl() {
 	return wpApiSettings.root.replace( '/wp-json/', '' );
+}
+
+/**
+ * Convert forms object in option
+ *
+ * @since 2.7.0
+ *
+ * @param {object} forms
+ *
+ * @return {[]}
+ */
+export function getFormOptions( forms ) {
+	let formOptions = [];
+
+	if ( ! isUndefined( forms ) ) {
+		formOptions = forms.map(
+			( { id, title: { rendered: title } } ) => {
+				return {
+					value: id,
+					label: title === '' ? `${ id } : ${ __( 'No form title' ) }` : title,
+				};
+			}
+		);
+	}
+
+	// Add Default option
+	formOptions.unshift( { value: '0', label: __( '-- Select Form --' ) } );
+
+	return formOptions;
 }
