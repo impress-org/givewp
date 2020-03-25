7c0325168b2e29298d6428f5414a909d8732b3e4
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-03-25 17:14:31 +0530

    chore: update h2 to h3

--- blocks/components/blank-slate/index.js
+++ blocks/components/blank-slate/index.js
@@ -26,7 +26,7 @@ const GiveBlankSlate = ( props ) => {
 
 	const blockLoaded = (
 		<div className="block-loaded">
-			{ !! title && ( <h2 className="give-blank-slate__heading">{ title }</h2> ) }
+			{ !! title && ( <h3 className="give-blank-slate__heading">{ title }</h3> ) }
 			{ !! description && ( <p className="give-blank-slate__message">{ description }</p> ) }
 			{ children }
 			{ !! helpLink && ( <GiveHelpLink /> ) }
