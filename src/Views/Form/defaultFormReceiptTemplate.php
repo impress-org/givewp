f1be3ed7e152753df4631d03b8256badd5220058
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-03-27 02:34:36 +0530

    chore: use helper fn to get give_receipt shortcode with attributes

--- src/Views/Form/defaultFormReceiptTemplate.php
+++ src/Views/Form/defaultFormReceiptTemplate.php
@@ -1,9 +1,12 @@
+<?php
+
+use function Give\Helpers\Frontend\getReceiptShortcodeFromConfirmationPage;
+?>
 <!DOCTYPE html>
 <html lang="en" style="margin-top: 0 !important;">
 	<head>
 		<meta charset="utf-8">
 		<title><?php _e( 'Donation Receipt', 'give' ); ?></title>
-
 		<?php
 		/**
 		 * Fire the action hook in header
@@ -13,13 +16,10 @@
 	</head>
 	<body class="give-form-templates">
 		<?php
-		global $post;
-		// Fetch the Give Form.
-		ob_start();
-
-		echo apply_filters( 'the_content', $post->post_content );
-
-		echo ob_get_clean();
+		echo apply_filters(
+			'the_content',
+			getReceiptShortcodeFromConfirmationPage( 'give_receipt' )
+		);
 
 		/**
 		 * Fire the action hook in footer
