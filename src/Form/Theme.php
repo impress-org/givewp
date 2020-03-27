1dbcd3d57bb83e88ae8fff6d61993c1f0d296730
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-03-28 00:11:07 +0530

    chore: addfn to Theme to get success page url

--- src/Form/Theme.php
+++ src/Form/Theme.php
@@ -110,4 +110,14 @@ abstract class Theme {
 	public function getFailedTransactionPageURL() {
 		return give_get_failed_transaction_uri( '?giveDonationAction=failedDonation' );
 	}
+
+	/**
+	 * Get success page URL.
+	 *
+	 * @since 2.7.0
+	 * @return mixed
+	 */
+	public function getSuccessPageURL() {
+		return give_get_success_page_url( '?giveDonationAction=showReceipt' );
+	}
 }
