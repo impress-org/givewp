8cd86b0e086ea4ad94bbfb1168dca7fa09e7d3aa
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-04-03 18:37:31 +0530

    fix: already handle with legacy form setting handler

--- src/Views/Form/Themes/Sequoia/Sequoia.php
+++ src/Views/Form/Themes/Sequoia/Sequoia.php
@@ -12,6 +12,17 @@ use function Give\Helpers\Form\Theme\get as getThemeOptions;
  * @package Give\Form\Theme
  */
 class Sequoia extends Theme implements Hookable, Scriptable {
+	/**
+	 * Map form template settings to legacy form settings.
+	 *
+	 * @since 2.7.0
+	 * @var array
+	 */
+	private $mapToLegacySetting = [
+		'payment_information' => [
+			'checkout_label' => '_give_checkout_label',
+		],
+	];
 
 	/**
 	 * @inheritDoc
@@ -98,5 +109,12 @@ class Sequoia extends Theme implements Hookable, Scriptable {
 	 */
 	public function getOptionsConfig() {
 		return require 'optionConfig.php';
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getLegacySettingHandler() {
+		return new Theme\LegacyFormSettingCompatibility( $this->mapToLegacySetting );
 	}
 }
