d5f9e1d936a8e72e299152a286769a381ad1dce8
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-04-08 17:19:53 -0400

    feat: scaffold loading screen logic

--- src/Views/Form/Themes/Sequoia/views/form.php
+++ src/Views/Form/Themes/Sequoia/views/form.php
@@ -14,8 +14,54 @@ $formId = getFormId();
 		 */
 		do_action( 'give_embed_head' );
 		?>
+		<style>
+			#loader {
+				display: contents;
+				position: absolute;
+			}
+			#loader .give-embed-form {
+				height: 734px;
+			}
+			#form {
+				opacity: 0;
+				pointer-events: none;
+			}
+
+			.sequoia-loaded #loader {
+				display: none;
+			}
+
+			.sequoia-loaded #form {
+				opacity: 1;
+				pointer-events: auto;
+			}
+		</style>
+		<script>
+			document.addEventListener("DOMContentLoaded", function(event){
+				const interval = setInterval(function(){ 
+					const height = document.querySelector('.give-form-wrap').offsetHeight;
+					document.querySelector('#loader .give-embed-form').setAttribute('style' ,`height: ${height}px;`);
+				}, 50);
+
+				const timer = setTimeout(function(){
+					loaded();
+				}, 1000);
+
+				function loaded () {
+					clearTimeout(timer);
+					clearInterval(interval);
+					document.body.className += ' sequoia-loaded';
+				}
+			});
+		</script>
 	</head>
 	<body class="give-form-templates">
+		<div id="loader">
+			<div class="give-embed-form">
+				<h1>LOADING!!!!</h1>
+			</div>
+		</div>
+		<div id="form">
 		<?php
 
 		// Fetch the Give Form.
@@ -26,5 +72,6 @@ $formId = getFormId();
 		 */
 		do_action( 'give_embed_footer' );
 		?>
+		</div>
 	</body>
 </html>
