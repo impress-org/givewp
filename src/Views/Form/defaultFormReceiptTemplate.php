e479656f5cee26604878ace80ba37040476937b9
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-04-10 03:30:25 +0530

    chore: refactor: rename varibale and echo output

--- src/Views/Form/defaultFormReceiptTemplate.php
+++ src/Views/Form/defaultFormReceiptTemplate.php
@@ -5,10 +5,10 @@
  * @since 2.7.0
  */
 use function Give\Helpers\Frontend\getReceiptShortcodeFromConfirmationPage;
-use Give\IframeView;
+use Give\Views\IframeView;
 
-$tm = new IframeView();
+$iframeView = new IframeView();
 
-$tm->setTitle( __( 'Donation Receipt', 'give' ) )
+echo $iframeView->setTitle( __( 'Donation Receipt', 'give' ) )
    ->setBody( do_shortcode( getReceiptShortcodeFromConfirmationPage() ) )
    ->render();
--- src/Views/Form/defaultFormTemplate.php
+++ src/Views/Form/defaultFormTemplate.php
@@ -4,16 +4,16 @@
  *
  * @since 2.7.0
  */
-use Give\IframeView;
+use Give\Views\IframeView;
 use function Give\Helpers\Form\Template\Utils\Frontend\getFormId;
 
-$formId = getFormId();
-$tm     = new IframeView();
+$formId     = getFormId();
+$iframeView = new IframeView();
 
 // Fetch the Give Form.
 ob_start();
 give_get_donation_form( [ 'id' => $formId ] );
 
-$tm->setTitle( get_post_field( 'post_title', $formId ) )
+echo $iframeView->setTitle( get_post_field( 'post_title', $formId ) )
    ->setBody( ob_get_clean() )
    ->render();
