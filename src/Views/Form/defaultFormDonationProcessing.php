4565e78f500c8fbaa054def44683a91cb986a1ff
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-04-10 03:26:32 +0530

    chore: rename class

--- src/Views/Form/defaultFormDonationProcessing.php
+++ src/Views/Form/defaultFormDonationProcessing.php
@@ -4,9 +4,9 @@
  *
  * @since 2.7.0
  */
-use Give\TemplateSkinManager;
+use Give\IframeView;
 
-$tm = new TemplateSkinManager();
+$tm = new IframeView();
 
 $tm->setTitle( __( 'Donation Processing', 'give' ) )
 	->setBody( apply_filters( 'give_payment_confirm_' . give_clean( $_GET['payment-confirmation'] ), '' ) )
