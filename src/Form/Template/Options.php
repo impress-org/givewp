4317f768463bd90a8bf5f2e76edbdb1a8d4dcd83
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-04-10 19:36:47 +0530

    feat: add fuction to get checkout label field config

--- src/Form/Template/Options.php
+++ src/Form/Template/Options.php
@@ -36,4 +36,26 @@ class Options {
 
 		return $options;
 	}
+
+	/**
+	 * Return array configuration for checkout label setting field.
+	 *
+	 * Note: if you want to add an option in template to overwrite "Donate Now" button title then instead of define it manually in template options, developer can call this function.
+	 * This function help to maintain backward compatibility with legacy donation form renderer.
+	 *
+	 * @return array
+	 */
+	public static function getCheckoutLabelField() {
+		return [
+			'id'                 => 'checkout_label',
+			'name'               => __( 'Submit Button', 'give' ),
+			'desc'               => __( 'The button label for completing a donation.', 'give' ),
+			'type'               => 'text_medium',
+			'attributes'         => [
+				'placeholder' => __( 'Donate Now', 'give' ),
+			],
+			'default'            => __( 'Donate Now', 'give' ),
+			'mapToLegacySetting' => '_give_checkout_label',
+		];
+	}
 }
