ce8e32e9d9ae74cf29b16355b64645f5609e8f6b
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-04-14 10:41:24 -0400

    feature: add template receipt ajax handler

--- src/Form/LoadTemplate.php
+++ src/Form/LoadTemplate.php
@@ -105,6 +105,25 @@ class LoadTemplate {
 		add_action( 'give_embed_footer', 'wp_print_footer_scripts', 20 );
 		add_filter( 'give_form_wrap_classes', [ $this, 'editClassList' ], 999 );
 		add_action( 'give_hidden_fields_after', [ $this, 'addHiddenField' ] );
+
+		// Handle receipt screen template
+		add_action( 'wp_ajax_give_get_template_receipt', [ $this, 'handleReceiptAjax' ], 999999 );
+		add_action( 'wp_ajax_nopriv_give_get_template_receipt', [ $this, 'handleReceiptAjax' ], 999999 );
+	}
+
+	/**
+	 * Render sequoia receipt by ajax
+	 *
+	 * @since 2.2.0
+	 */
+	function handleReceiptAjax() {
+		$get_data = give_clean( filter_input_array( INPUT_GET ) );
+
+		ob_start();
+		include_once $this->template->getReceiptView();
+		$data = ob_get_clean();
+		wp_send_json( $data );
+		wp_die(); // All ajax handlers die when finished
 	}
 
 
