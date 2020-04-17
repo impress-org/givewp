02792b3991c590461baf304095a47eded5b3b2fa
Mehul Gohil <mehul.gohil0810@gmail.com>
2020-04-17 17:50:17 +0530

    fix: update disconnect url helper fn for admin

--- includes/gateways/stripe/includes/admin/admin-helpers.php
+++ includes/gateways/stripe/includes/admin/admin-helpers.php
@@ -167,26 +167,32 @@ function give_stripe_connect_button() {
 /**
  * Stripe Disconnect URL.
  *
- * @param string $account_id Stripe Account ID.
+ * @param string $account_id   Stripe Account ID.
+ * @param string $account_name Stripe Account Name.
  *
  * @since 2.5.0
  *
- * @return void
+ * @return string
  */
-function give_stripe_disconnect_url( $account_id = '' ) {
+function give_stripe_disconnect_url( $account_id = '', $account_name = '' ) {
+
+	$args = [
+		'stripe_action'  => 'disconnect',
+		'mode'           => give_is_test_mode() ? 'test' : 'live',
+		'stripe_user_id' => ! empty( $account_id ) ? $account_id : give_get_option( 'give_stripe_user_id' ),
+		'return_url'     => rawurlencode( admin_url( 'edit.php?post_type=give_forms&page=give-settings&tab=gateways&section=stripe-settings' ) ),
+	];
+
+	// Send Account Name.
+	if ( ! empty( $account_name ) ) {
+		$args['account_name'] = $account_name;
+	}
 
 	// Prepare Stripe Disconnect URL.
-	$link = add_query_arg(
-		array(
-			'stripe_action'  => 'disconnect',
-			'mode'           => give_is_test_mode() ? 'test' : 'live',
-			'stripe_user_id' => ! empty( $account_id ) ? $account_id : give_get_option( 'give_stripe_user_id' ),
-			'return_url'     => rawurlencode( admin_url( 'edit.php?post_type=give_forms&page=give-settings&tab=gateways&section=stripe-settings' ) ),
-		),
+	return add_query_arg(
+		$args,
 		esc_url_raw( 'https://connect.givewp.com/stripe/connect.php' )
 	);
-
-	echo esc_url( $link );
 }
 
 /**
