861c57876075ebda7bfd8fe1e0c5c864f19ce697
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-04-21 01:50:21 +0530

    chore: add Donation class

---
+++ src/Donation/Donation.php
@@ -0,0 +1,36 @@
+<?php
+namespace Give\Donation;
+
+use function Give\Helpers\Form\Template\Utils\Frontend\getPaymentId;
+
+/**
+ * Class Donation
+ *
+ * @package Give\Donation
+ */
+class Donation {
+	private $id;
+
+	/**
+	 * Donation constructor.
+	 *
+	 * @param int $id
+	 */
+	public function __construct( $id ) {
+		$this->id = absint( $id );
+	}
+
+
+	/**
+	 * Return true if donation is subscription.
+	 *
+	 * @since 2.7.0
+	 * @param int $donationId
+	 * @return bool
+	 */
+	public static function isSubscription( $donationId = null ) {
+		$donationId = $donationId ?: getPaymentId();
+
+		return 1 === Give()->payment_meta->get_meta( $donationId, '_give_subscription_payment', true );
+	}
+}
