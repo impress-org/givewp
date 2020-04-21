6fc0708646f3f4929c4daecb89c033d7394910d4
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-04-21 15:34:15 +0530

    fix: make subscription values dynamic on receipt

--- src/Views/Form/Templates/Sequoia/views/subscription-details.php
+++ src/Views/Form/Templates/Sequoia/views/subscription-details.php
@@ -2,13 +2,54 @@
 
 use Give\Addon\Recurring;
 use Give\Donation\Donation;
+use function Give\Helpers\Form\Template\Utils\Frontend\getPaymentId;
+
+$donationID = getPaymentId();
 
 // Exit if donation is not subscription.
-if ( ! Recurring::isActive() || ! Donation::isSubscription() ) {
+if ( ! Recurring::isActive() || ! Donation::isRecurring( $donationID ) ) {
 	return '';
 }
 
+$subscriptionDB = new Give_Subscriptions_DB();
+$subscriptionId = $subscriptionDB->get_column_by( 'id', 'parent_payment_id', $donationID );
+$subscription   = new Give_Subscription( $subscriptionId );
 
+$frequency = give_recurring_pretty_subscription_frequency(
+	$subscription->period,
+	$subscription->bill_times,
+	false,
+	$subscription->frequency ?: 1
+);
+
+$subscriptionInformation = sprintf(
+	'%1$s / %2$s',
+	give_currency_filter(
+		give_format_amount(
+			$subscription->recurring_amount,
+			[ 'donation_id' => $donationID ]
+		),
+		[ 'currency_code' => give_get_payment_currency_code( $donationID ) ]
+	),
+	$frequency
+);
+
+$editAmountLink = give_recurring_is_subscriptions_page() && $subscription->can_update_subscription() ?
+	sprintf(
+		'&nbsp;<strong><a href="%3$s" title="%2$s">%1$s</a></strong>',
+		__( 'Edit Amount', 'give' ),
+		__( 'Edit amount of subscription', 'give' ),
+		esc_url( $subscription->get_edit_subscription_url() )
+	) :
+	'';
+
+$status = give_recurring_get_pretty_subscription_status( $subscription->status );
+
+$renewalDate = ! empty( $subscription->expiration ) ?
+	date_i18n( get_option( 'date_format' ), strtotime( $subscription->expiration ) ) :
+	__( 'N/A', 'give' );
+
+$progress = get_times_billed_text( $subscription );
 ?>
 <div class="details">
 	<h3 class="headline"><?php _e( 'Subscription Details', 'give' ); ?></h3>
@@ -19,7 +60,13 @@ if ( ! Recurring::isActive() || ! Donation::isSubscription() ) {
 				<?php _e( 'Subscription', 'give' ); ?>
 			</div>
 			<div class="value">
-				<?php echo '$25 / Monthly <strong><a href="#" title="Edit amount of subscription">Edit</a></strong>'; ?>
+				<?php
+				printf(
+					'%1$s%2$s',
+					$subscriptionInformation,
+					$editAmountLink
+				);
+				?>
 			</div>
 		</div>
 		<div class="details-row">
@@ -27,7 +74,7 @@ if ( ! Recurring::isActive() || ! Donation::isSubscription() ) {
 				<?php _e( 'Status', 'give' ); ?>
 			</div>
 			<div class="value">
-				<?php echo 'Active'; ?>
+				<?php echo $status; ?>
 			</div>
 		</div>
 
@@ -36,7 +83,7 @@ if ( ! Recurring::isActive() || ! Donation::isSubscription() ) {
 				<?php _e( 'Renewal Date', 'give' ); ?>
 			</div>
 			<div class="value">
-				<?php echo 'May 20, 2020'; ?>
+				<?php echo $renewalDate; ?>
 			</div>
 		</div>
 
@@ -45,7 +92,7 @@ if ( ! Recurring::isActive() || ! Donation::isSubscription() ) {
 				<?php _e( 'Progress', 'give' ); ?>
 			</div>
 			<div class="value">
-				<?php echo '1 / Ongoing'; ?>
+				<?php echo $progress; ?>
 			</div>
 		</div>
 
