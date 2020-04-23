e6e099ebd1feb32e6921193d91d644d197ef2372
Mehul Gohil <mehul.gohil0810@gmail.com>
2020-04-23 15:15:35 +0530

    fix: improve stripe webhooks to handle multiple accounts

--- includes/gateways/stripe/includes/class-give-stripe-webhooks.php
+++ includes/gateways/stripe/includes/class-give-stripe-webhooks.php
@@ -63,16 +63,23 @@ if ( ! class_exists( 'Give_Stripe_Webhooks' ) ) {
 				return;
 			}
 
+			// Retrieve the request's body and parse it as JSON.
+			$body  = @file_get_contents( 'php://input' );
+			$event = json_decode( $body );
+
+			/**
+			 * Never remove this as it ensure the check for which Stripe account to use.
+			 * @Todo Utilize idempotency key on every Stripe request for better stability.
+			 */
+			$metadata    = (array) $event->data->object->metadata;
+			$donation_id = ! empty( $metadata['Donation Post ID'] ) ? $metadata['Donation Post ID'] : 0;
+			$form_id     = give_get_payment_form_id( $donation_id );
+
 			// Load Stripe SDK.
 			give_stripe_load_stripe_sdk();
 
 			// Set App Info, API Key, and API Version.
-			give_stripe_set_app_info();
-			$this->stripe_gateway->set_api_version();
-
-			// Retrieve the request's body and parse it as JSON.
-			$body  = @file_get_contents( 'php://input' );
-			$event = json_decode( $body );
+			give_stripe_set_app_info( $form_id );
 
 			$processed_event = $this->process( $event );
 
