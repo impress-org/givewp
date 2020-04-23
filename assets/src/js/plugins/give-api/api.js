cf362bbdcd242a671ca7ebe00f6d1ca068853984
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-04-23 11:00:57 -0400

    chore: format code

--- assets/src/js/plugins/give-api/api.js
+++ assets/src/js/plugins/give-api/api.js
@@ -335,17 +335,17 @@ const Give = {
 
 Give.share = {
 	fn: {
-		twitter: function twitter (url, text) {
+		twitter: function twitter( url, text ) {
 			const targetWindow = parent.window ? parent.window : window;
 			// Calculate new window position, based on parent window height/width
 			const top = targetWindow.innerHeight / 2 - 126;
 			const left = targetWindow.innerWidth / 2 - 280;
 			// Open new window with prompt for Twitter sharing
-			targetWindow.open(`https://twitter.com/intent/tweet?url=${url}&text=${text}`, 'newwindow', `width=560,height=253,top=${top},left=${left}`); 
+			targetWindow.open( `https://twitter.com/intent/tweet?url=${ url }&text=${ text }`, 'newwindow', `width=560,height=253,top=${ top },left=${ left }` );
 			return false;
-		}
-	}
-}
+		},
+	},
+};
 
 Give.notice = GiveNotice;
 Give.form = GiveForm;
