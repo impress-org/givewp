fd65c611c71c29d78d79670b9c3075897074d588
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-04-23 12:05:43 -0400

    chore: add documentaiton

--- assets/src/js/plugins/give-api/share.js
+++ assets/src/js/plugins/give-api/share.js
@@ -1,5 +1,12 @@
 export default {
 	fn: {
+
+		/**
+		 * Open a new window prompting user to Tweet
+		 * The tweet is pre-populated with supplied text, and url to share
+		 *
+		 * @since 2.7.0
+		 */
 		twitter: function twitter( url, text ) {
 			const targetWindow = parent.window ? parent.window : window;
 			// Calculate new window position, based on parent window height/width
@@ -8,6 +15,13 @@ export default {
 			// Open new window with prompt for Twitter sharing
 			targetWindow.open( `https://twitter.com/intent/tweet?url=${ url }&text=${ text }`, 'newwindow', `width=560,height=253,top=${ top },left=${ left }` );
 		},
+
+		/**
+		 * Open a new window prompting user to share on Facebook
+		 * The post is pre-populated with supplied url
+		 *
+		 * @since 2.7.0
+		 */
 		facebook: function facebook( url ) {
 			const targetWindow = parent.window ? parent.window : window;
 			// Calculate new window position, based on parent window height/width
