d2174035702504ce4317f98e30b586fb135e6b45
Mehul Gohil <mehul.gohil0810@gmail.com>
2020-04-23 10:54:20 +0530

    fix: improve test for secret and publishable key

--- tests/unit-tests/stripe/tests-give-stripe-helpers.php
+++ tests/unit-tests/stripe/tests-give-stripe-helpers.php
@@ -6,7 +6,6 @@
  */
 class Tests_Give_Stripe_Helpers extends Give_Unit_Test_Case {
 
-
 	/**
 	 * Unit test for function give_stripe_get_secret_key();
 	 *
@@ -16,14 +15,26 @@ class Tests_Give_Stripe_Helpers extends Give_Unit_Test_Case {
 	 */
 	public function test_give_stripe_get_secret_key() {
 
+		$accounts = [
+			'account_1' => [
+				'type'                 => 'connect',
+				'live_secret_key'      => 'sk_live_xxxxxxxx',
+				'live_publishable_key' => 'pk_live_xxxxxxxx',
+				'test_secret_key'      => 'sk_test_xxxxxxxx',
+				'test_publishable_key' => 'pk_test_xxxxxxxx',
+			],
+			'account_2' => [
+				'type'                 => 'manual',
+				'live_secret_key'      => 'sk_live_xxxxxxxx',
+				'live_publishable_key' => 'pk_live_xxxxxxxx',
+				'test_secret_key'      => 'sk_test_xxxxxxxx',
+				'test_publishable_key' => 'pk_test_xxxxxxxx',
+			],
+		];
+		give_update_option( '_give_stripe_get_all_accounts', $accounts );
+
 		// Set dummy default key.
-		give_update_option( '_give_stripe_default_account', [
-			'type'                 => 'connect',
-			'live_secret_key'      => 'sk_live_xxxxxxxx',
-			'live_publishable_key' => 'pk_live_xxxxxxxx',
-			'test_secret_key'      => 'sk_test_xxxxxxxx',
-			'test_publishable_key' => 'pk_test_xxxxxxxx',
-		] );
+		give_update_option( '_give_stripe_default_account', 'account_1' );
 
 		$this->assertStringStartsWith( 'sk_test_', give_stripe_get_secret_key() );
 
@@ -43,14 +54,26 @@ class Tests_Give_Stripe_Helpers extends Give_Unit_Test_Case {
 	 */
 	public function test_give_stripe_get_publishable_key() {
 
+		$accounts = [
+			'account_1' => [
+				'type'                 => 'connect',
+				'live_secret_key'      => 'sk_live_xxxxxxxx',
+				'live_publishable_key' => 'pk_live_xxxxxxxx',
+				'test_secret_key'      => 'sk_test_xxxxxxxx',
+				'test_publishable_key' => 'pk_test_xxxxxxxx',
+			],
+			'account_2' => [
+				'type'                 => 'manual',
+				'live_secret_key'      => 'sk_live_xxxxxxxx',
+				'live_publishable_key' => 'pk_live_xxxxxxxx',
+				'test_secret_key'      => 'sk_test_xxxxxxxx',
+				'test_publishable_key' => 'pk_test_xxxxxxxx',
+			],
+		];
+		give_update_option( '_give_stripe_get_all_accounts', $accounts );
+
 		// Set dummy default key.
-		give_update_option( '_give_stripe_default_account', [
-			'type'                 => 'connect',
-			'live_secret_key'      => 'sk_live_xxxxxxxx',
-			'live_publishable_key' => 'pk_live_xxxxxxxx',
-			'test_secret_key'      => 'sk_test_xxxxxxxx',
-			'test_publishable_key' => 'pk_test_xxxxxxxx',
-		] );
+		give_update_option( '_give_stripe_default_account', 'account_2' );
 
 		$this->assertStringStartsWith( 'pk_test_', give_stripe_get_publishable_key() );
 
