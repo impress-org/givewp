d3b0d881ac346ff0c360cf21b01359eeba7f6b02
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-05-05 16:49:39 -0400

    fix: perist border when input is selected

--- src/Views/Form/Templates/Sequoia/assets/js/form.js
+++ src/Views/Form/Templates/Sequoia/assets/js/form.js
@@ -178,6 +178,11 @@
 				$( testNotice ).clone().prependTo( '.give-section.payment' );
 				$( testNotice ).remove();
 
+				// Perist fee recovery input border when selected
+				$( '.give-fee-recovery-donors-choice > label' ).on( 'click', function() {
+					$( '.give-fee-recover-donors-choice' ).toggleClass( 'active' );
+				} );
+
 				// Show Sequoia loader on click/touchend
 				$( 'body.give-form-templates' ).on( 'click touchend', 'form.give-form input[name="give-purchase"].give-submit', function() {
 					//Override submit loader with Sequoia loader
