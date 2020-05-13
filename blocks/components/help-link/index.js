d8182498b90458eb14379b459f00cb146ea193b2
Devin Walker <sdtucson@gmail.com>
2020-05-12 19:57:18 -0700

    fix: resolve typo in block help

--- blocks/components/help-link/index.js
+++ blocks/components/help-link/index.js
@@ -10,7 +10,7 @@ const { __ } = wp.i18n;
 const GiveHelpLink = () => {
 	return (
 		<p className="give-blank-slate__help">
-			Need help ? Get started with <a href="http://docs.givewp.com/give101/" target="_blank" rel="noopener noreferrer">{ __( 'GiveWP 101' ) }</a>
+			Need help? Get started with <a href="http://docs.givewp.com/give101/" target="_blank" rel="noopener noreferrer">{ __( 'GiveWP 101' ) }</a>
 		</p>
 	);
 };
