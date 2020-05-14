a5291d1a77aef956783dc4eaaab2af6c478bded3
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-05-14 14:36:09 -0400

    feature: use primary color for checkmark

--- src/Views/Form/Templates/Sequoia/Sequoia.php
+++ src/Views/Form/Templates/Sequoia/Sequoia.php
@@ -109,12 +109,20 @@ class Sequoia extends Template implements Hookable, Scriptable {
 			.give-forgot-password a {
 				color: %1$s;
 			}
+			
 		',
 			$primaryColor
 		);
 		wp_add_inline_style( 'give-sequoia-template-css', $dynamicCss );
 
-		$rawColor            = trim( $primaryColor, '#' );
+		$rawColor        = trim( $primaryColor, '#' );
+		$registrationCss = "
+			.payment [id*='give-create-account-wrap-'] label::after {
+				background-image: url(\"data:image/svg+xml,%3Csvg width='15' height='11' viewBox='0 0 15 11' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M5.73047 10.7812C6.00391 11.0547 6.46875 11.0547 6.74219 10.7812L14.7812 2.74219C15.0547 2.46875 15.0547 2.00391 14.7812 1.73047L13.7969 0.746094C13.5234 0.472656 13.0859 0.472656 12.8125 0.746094L6.25 7.30859L3.16016 4.24609C2.88672 3.97266 2.44922 3.97266 2.17578 4.24609L1.19141 5.23047C0.917969 5.50391 0.917969 5.96875 1.19141 6.24219L5.73047 10.7812Z' fill='%23{$rawColor}'/%3E%3C/svg%3E%0A\");
+			}
+		";
+		wp_add_inline_style( 'give-sequoia-template-css', $registrationCss );
+
 		$recurringDynamicCss = "
 			.give-recurring-donors-choice:hover,
 			.give-recurring-donors-choice.active {
