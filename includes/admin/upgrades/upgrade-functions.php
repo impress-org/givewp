b97286535dd5ac4fd2c14a913132305e3f2e58fa
Mehul Gohil <mehul.gohil0810@gmail.com>
2020-05-22 19:55:19 +0530

    fix: updated code to resolve backward compatibility issues

--- includes/admin/upgrades/upgrade-functions.php
+++ includes/admin/upgrades/upgrade-functions.php
@@ -3645,7 +3645,6 @@ function give_v263_upgrades() {
 function give_v270_upgrades() {
 	$stripe_accounts = give_stripe_get_all_accounts();
 	$is_migrated     = get_option( '_give_stripe_data_migrated', false );
-//	give_update_option( '_give_stripe_get_all_accounts', [] );
 
 	// Process, only when there is no Stripe accounts stored.
 	if ( ! $stripe_accounts && ! $is_migrated ) {
@@ -3708,12 +3707,12 @@ function give_v270_upgrades() {
 		give_update_option( '_give_stripe_get_all_accounts', $stripe_accounts );
 
 		// Remove legacy settings.
-//		give_delete_option( 'live_secret_key' );
-//		give_delete_option( 'test_secret_key' );
-//		give_delete_option( 'live_publishable_key' );
-//		give_delete_option( 'test_secret_key' );
-//		give_delete_option( 'give_stripe_connected' );
-//		give_delete_option( 'give_stripe_user_id' );
+		give_delete_option( 'live_secret_key' );
+		give_delete_option( 'test_secret_key' );
+		give_delete_option( 'live_publishable_key' );
+		give_delete_option( 'test_secret_key' );
+		give_delete_option( 'give_stripe_connected' );
+		give_delete_option( 'give_stripe_user_id' );
 
 		// Set option to check that data is migrated or not.
 		update_option( '_give_stripe_data_migrated', true );
