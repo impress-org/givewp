d970a0e0981f1786b1481c93b9bb7004b63fb624
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-05-28 22:45:24 +0530

    chore: add isPluginActive helper function

--- src/Helpers/Utils.php
+++ src/Helpers/Utils.php
@@ -78,4 +78,29 @@ class Utils {
 	public static function removeDonationAction( $url ) {
 		return add_query_arg( [ 'giveDonationAction' => false ], $url );
 	}
+
+	/**
+	 * Determines whether a plugin is active.
+	 *
+	 * Only plugins installed in the plugins/ folder can be active.
+	 *
+	 * Plugins in the mu-plugins/ folder can't be "activated," so this function will
+	 * return false for those plugins.
+	 *
+	 * For more information on this and similar theme functions, check out
+	 * the {@link https://developer.wordpress.org/themes/basics/conditional-tags/
+	 * Conditional Tags} article in the Theme Developer Handbook.
+	 *
+	 * @since 2.7.0
+	 *
+	 * @param string $plugin Path to the plugin file relative to the plugins directory.
+	 * @return bool True, if in the active plugins list. False, not in the list.
+	 */
+	public static function isPluginActive( $plugin ) {
+		if ( ! function_exists( 'is_plugin_active' ) ) {
+			include_once ABSPATH . 'wp-admin/includes/plugin.php';
+		}
+
+		return is_plugin_active( $plugin );
+	}
 }
