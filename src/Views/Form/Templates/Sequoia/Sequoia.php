b08e55575c1aac77ebf669045f36215224e83a7e
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-05-29 22:20:52 +0530

    chore: update logic to print dynamic css

--- src/Views/Form/Templates/Sequoia/Sequoia.php
+++ src/Views/Form/Templates/Sequoia/Sequoia.php
@@ -110,24 +110,19 @@ class Sequoia extends Template implements Hookable, Scriptable {
 				color: %1$s;
 			}
 
-		',
+			',
 			$primaryColor
 		);
-		wp_add_inline_style( 'give-sequoia-template-css', $dynamicCss );
 
-		$rawColor        = trim( $primaryColor, '#' );
-		$registrationCss = "
+		$rawColor    = trim( $primaryColor, '#' );
+		$dynamicCss .= "
 			.payment [id*='give-create-account-wrap-'] label::after {
 				background-image: url(\"data:image/svg+xml,%3Csvg width='15' height='11' viewBox='0 0 15 11' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M5.73047 10.7812C6.00391 11.0547 6.46875 11.0547 6.74219 10.7812L14.7812 2.74219C15.0547 2.46875 15.0547 2.00391 14.7812 1.73047L13.7969 0.746094C13.5234 0.472656 13.0859 0.472656 12.8125 0.746094L6.25 7.30859L3.16016 4.24609C2.88672 3.97266 2.44922 3.97266 2.17578 4.24609L1.19141 5.23047C0.917969 5.50391 0.917969 5.96875 1.19141 6.24219L5.73047 10.7812Z' fill='%23{$rawColor}'/%3E%3C/svg%3E%0A\");
 			}
 		";
-		wp_add_inline_style( 'give-sequoia-template-css', $registrationCss );
-
-		// Setup addon dynamic CSS variable
-		$addonDynamicCss = null;
 
 		if ( Utils::isPluginActive( 'give-recurring/give-recurring.php' ) ) {
-			$addonDynamicCss .= "
+			$dynamicCss .= "
 				.give-recurring-donors-choice:hover,
 				.give-recurring-donors-choice.active {
 					border: 1px solid {$primaryColor};
@@ -139,7 +134,7 @@ class Sequoia extends Template implements Hookable, Scriptable {
 		}
 
 		if ( Utils::isPluginActive( 'give-fee-recovery/give-fee-recovery.php' ) ) {
-			$addonDynamicCss .= "
+			$dynamicCss .= "
 				.give-fee-recovery-donors-choice.give-fee-message:hover,
 				.give-fee-recovery-donors-choice.give-fee-message.active {
 					border: 1px solid {$primaryColor};
@@ -151,7 +146,7 @@ class Sequoia extends Template implements Hookable, Scriptable {
 		}
 
 		if ( Utils::isPluginActive( 'give-mailchimp/give-mailchimp.php' ) ) {
-			$addonDynamicCss .= "
+			$dynamicCss .= "
 				.give-mailchimp-fieldset:hover,
 				.give-mailchimp-fieldset.active {
 					border: 1px solid {$primaryColor} !important;
@@ -163,7 +158,7 @@ class Sequoia extends Template implements Hookable, Scriptable {
 		}
 
 		if ( Utils::isPluginActive( 'give-constant-contact/give-constant-contact.php' ) ) {
-			$addonDynamicCss .= "
+			$dynamicCss .= "
 				.give-constant-contact-fieldset:hover,
 				.give-constant-contact-fieldset.active {
 					border: 1px solid {$primaryColor} !important;
@@ -175,7 +170,7 @@ class Sequoia extends Template implements Hookable, Scriptable {
 		}
 
 		if ( Utils::isPluginActive( 'give-mailchimp/give-form-field-manager.php' ) ) {
-			$addonDynamicCss .= "
+			$dynamicCss .= "
 				.ffm-checkbox-field label.checked::after {
 					background-image: url(\"data:image/svg+xml,%3Csvg width='15' height='11' viewBox='0 0 15 11' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M5.73047 10.7812C6.00391 11.0547 6.46875 11.0547 6.74219 10.7812L14.7812 2.74219C15.0547 2.46875 15.0547 2.00391 14.7812 1.73047L13.7969 0.746094C13.5234 0.472656 13.0859 0.472656 12.8125 0.746094L6.25 7.30859L3.16016 4.24609C2.88672 3.97266 2.44922 3.97266 2.17578 4.24609L1.19141 5.23047C0.917969 5.50391 0.917969 5.96875 1.19141 6.24219L5.73047 10.7812Z' fill='%23{$rawColor}'/%3E%3C/svg%3E%0A\");
 				}
@@ -185,10 +180,7 @@ class Sequoia extends Template implements Hookable, Scriptable {
 			";
 		}
 
-		// Add dynamic CSS inline if addon dynamic css has been defined
-		if ( $addonDynamicCss ) {
-			wp_add_inline_style( 'give-sequoia-template-css', $addonDynamicCss );
-		}
+		wp_add_inline_style( 'give-sequoia-template-css', $dynamicCss );
 
 		wp_enqueue_script( 'give-sequoia-template-js', GIVE_PLUGIN_URL . 'assets/dist/js/give-sequoia-template.js', [ 'give' ], GIVE_VERSION, true );
 		wp_localize_script( 'give-sequoia-template-js', 'sequoiaTemplateOptions', $templateOptions );
