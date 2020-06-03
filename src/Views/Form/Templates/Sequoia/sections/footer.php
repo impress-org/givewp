cb693b25f36b6e5f921b6a4558bb4ac4fd07bacf
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-06-03 15:40:19 -0400

    fix: conditionally show secure donation notice

--- src/Views/Form/Templates/Sequoia/sections/footer.php
+++ src/Views/Form/Templates/Sequoia/sections/footer.php
@@ -6,7 +6,12 @@
 		<div class="step-tracker" data-step="2"></div>
 	</div>
 	<div class="secure-notice">
+	<?php if ( is_ssl() ) : ?>
 		<i class="fas fa-lock"></i>
 		<?php _e( 'Secure Donation', 'give' ); ?>
+	<?php else : ?>
+		<i class="fas fa-exclamation-triangle"></i>
+		<?php _e( 'Insecure Donation', 'give' ); ?>
+	<?php endif; ?>
 	</div>
 </div>
