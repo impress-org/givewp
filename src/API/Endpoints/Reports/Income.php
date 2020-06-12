2b05fd6f76fd204649e45d698809ac5616631819
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-06-12 11:26:51 -0700

    fix: use correct currency in income endpoint tooltips

--- src/API/Endpoints/Reports/Income.php
+++ src/API/Endpoints/Reports/Income.php
@@ -87,7 +87,13 @@ class Income extends Endpoint {
 			);
 
 			$tooltips[] = array(
-				'title'  => give_currency_filter( give_format_amount( $incomeForPeriod ), array( 'decode_currency' => true ) ),
+				'title'  => give_currency_filter(
+					give_format_amount( $incomeForPeriod ),
+					array(
+						'currency_code'   => $this->currency,
+						'decode_currency' => true,
+					)
+				),
 				'body'   => $donorsForPeriod . ' ' . __( 'Donors', 'give' ),
 				'footer' => $periodLabel,
 			);
