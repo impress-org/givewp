374b12e6f679195afa4111cd425b145209fb8c2c
Devin Walker <sdtucson@gmail.com>
2020-06-25 14:45:31 -0700

    refactor: remove unused code from old gravatar widget that never got used

--- give.php
+++ give.php
@@ -569,7 +569,6 @@ if ( ! class_exists( 'Give' ) ) :
 			require_once GIVE_PLUGIN_DIR . 'includes/class-give-logging.php';
 			require_once GIVE_PLUGIN_DIR . 'includes/class-give-comment.php';
 
-			require_once GIVE_PLUGIN_DIR . 'includes/class-give-donor-wall-widget.php';
 			require_once GIVE_PLUGIN_DIR . 'includes/forms/widget.php';
 			require_once GIVE_PLUGIN_DIR . 'includes/forms/class-give-forms-query.php';
 			require_once GIVE_PLUGIN_DIR . 'includes/forms/template.php';
