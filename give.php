8e63fe7fae6386bd3ff8d84a51416ddd69d6e6de
Devin Walker <sdtucson@gmail.com>
2020-06-30 13:02:55 -0700

    chore: add change log for 2.7.1 and fix typos in previous change logs

--- give.php
+++ give.php
@@ -5,7 +5,7 @@
  * Description: The most robust, flexible, and intuitive way to accept donations on WordPress.
  * Author: GiveWP
  * Author URI: https://givewp.com/
- * Version: 2.7.0
+ * Version: 2.7.1
  * Text Domain: give
  * Domain Path: /languages
  *
@@ -461,7 +461,7 @@ if ( ! class_exists( 'Give' ) ) :
 
 			// Plugin version.
 			if ( ! defined( 'GIVE_VERSION' ) ) {
-				define( 'GIVE_VERSION', '2.7.0' );
+				define( 'GIVE_VERSION', '2.7.1' );
 			}
 
 			// Plugin Root File.
