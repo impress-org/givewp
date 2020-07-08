5aa82588258711d98825d113200034d172749688
Michael Beil <michaelbeil@me.com>
2020-07-08 17:53:00 +0200

    update link to shortlink

--- src/Views/Form/Templates/Sequoia/optionConfig.php
+++ src/Views/Form/Templates/Sequoia/optionConfig.php
@@ -166,7 +166,7 @@ return [
 			[
 				'id'         => 'description',
 				'name'       => __( 'Description', 'give' ),
-				'desc'       => __( 'The description is displayed directly below the main headline and should be 1-2 sentences. You may use <a href="https://givewp.com/documentation/core/settings/emails/email-tags/?utm_source=GiveWPPlugin&utm_medium=admin&utm_campaign=Plugin_Links&utm_content=Email_Tags target="_blank">any of the available template tags</a> within this message.', 'give' ),
+				'desc'       => __( 'The description is displayed directly below the main headline and should be 1-2 sentences. You may use <a href="http://docs.givewp.com/email-tags target="_blank">any of the available template tags</a> within this message.', 'give' ),
 				'type'       => 'textarea',
 				'attributes' => [
 					'placeholder' => __( '{name}, you contribution means a lot and will be put to good use making a difference. We’ve sent your donation receipt to {donor_email}. ', 'give' ),
