829a462e32820e2b0fe8a8fecc648a78df3990c8
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-07-09 13:54:00 -0700

    feature: scaffold wizard react app

---
+++ assets/src/js/admin/onboarding-wizard/index.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+
+// Entry point for Onboarind Wizard app
+
+// Vendor dependencies
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Reports app
+import App from './app/index.js';
+
+ReactDOM.render(
+	<App />,
+	document.getElementById( 'onboarding-wizard-app' )
+);
