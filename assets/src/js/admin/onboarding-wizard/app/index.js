4950bb06de51b76e38c190b0a63e8d71156ee423
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-07-09 14:28:28 -0700

    feature: add skip button

--- assets/src/js/admin/onboarding-wizard/app/index.js
+++ assets/src/js/admin/onboarding-wizard/app/index.js
@@ -4,8 +4,20 @@ import './style.scss';
 
 const App = () => {
 	return (
-		<div className="give-onboarding-wizard">
-			<h1>Onboardng Wizard!</h1>
+		<div className="give-obw">
+			<div className="give-obw-step">
+				<h2>
+					Welcome To
+				</h2>
+				<div className="give-obw-logo">GiveWP</div>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in mi a leo convallis consequat. Sed ornare tellus vel justo porttitor, eu bibendum lorem vulputate.
+				</p>
+				<div className="give-obw-buttons">
+					<a className="give-obw-button give-obw-button--primary" href="#">Get Started</a>
+					<a className="give-obw-button" href={ window.giveOnboardingWizardData.setupUrl }>Not right now.</a>
+				</div>
+			</div>
 		</div>
 	);
 };
--- src/Views/Admin/Pages/OnboardingWizard.php
+++ src/Views/Admin/Pages/OnboardingWizard.php
@@ -14,7 +14,7 @@ class OnboardingWizard {
 
 	public function init() {
 		add_action( 'admin_menu', [ $this, 'add_page' ] );
-		add_action( 'admin_init', [ $this, 'render_page' ] );
+		add_action( 'admin_init', [ $this, 'setup_wizard' ] );
 		add_action( 'admin_enqueue_scripts', [ $this, 'enqueue_scripts' ] );
 	}
 
@@ -22,10 +22,19 @@ class OnboardingWizard {
 		add_dashboard_page( '', '', 'manage_options', 'give-onboarding-wizard', '' );
 	}
 
+	public function setup_wizard() {
+		if ( empty( $_GET['page'] ) || $this->slug !== $_GET['page'] ) { // WPCS: CSRF ok, input var ok.
+			return;
+		} else {
+			$this->render_page();
+		}
+	}
+
 	public function render_page() {
 		ob_start();
 		include_once GIVE_PLUGIN_DIR . 'src/Views/Admin/Pages/templates/onboarding-wizard-template.php';
 		exit;
+
 	}
 
 	public function enqueue_scripts() {
@@ -48,9 +57,18 @@ class OnboardingWizard {
 			'give-admin-onboarding-wizard-app',
 			'giveOnboardingWizardData',
 			[
+				'setupUrl'   => admin_url( '?page=give-getting-started' ),
 				'currencies' => array_keys( give_get_currencies_list() ),
 			]
 		);
+
+		wp_enqueue_style(
+			'give-google-font-montserrat',
+			'https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap',
+			[],
+			GIVE_VERSION
+		);
+
 	}
 
 }
