e01a134837ccc68e3762264427a8eadfc381b074
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-07-09 13:54:00 -0700

    feature: scaffold wizard react app

---
+++ assets/src/js/admin/onboarding-wizard/app/index.js
@@ -0,0 +1,12 @@
+// Onboarding Wizard App
+
+import './style.scss';
+
+const App = () => {
+	return (
+		<div className="give-onboarding-wizard">
+			<h1>Onboardng Wizard!</h1>
+		</div>
+	);
+};
+export default App;
