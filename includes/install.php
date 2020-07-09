2af6d6c90be2abe8006d81b53f5fcd74ea5eb8f1
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-07-09 14:54:33 -0700

    chore: improve documentation

--- includes/install.php
+++ includes/install.php
@@ -19,7 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * Runs on plugin install by setting up the post types, custom taxonomies, flushing rewrite rules to initiate the new
  * 'donations' slug and also creates the plugin and populates the settings fields for those plugin pages. After
- * successful install, the user is redirected to the Give Welcome screen.
+ * successful install, the user is redirected to the Give Onboarding Wizard.
  *
  * @since 1.0
  *
