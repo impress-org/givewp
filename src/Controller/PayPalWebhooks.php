0d8e99ff720292d56195bdb9786a2519e4920a97
Jason Adams <jason.the.adams@gmail.com>
2020-07-14 08:09:51 -0700

    feature: create Webhooks repository and use in controller

--- src/Controller/PayPalWebhooks.php
+++ src/Controller/PayPalWebhooks.php
@@ -3,9 +3,26 @@
 namespace Give\Controller;
 
 use Exception;
+use Give\PaymentGateways\PayPalCommerce\Repositories\Webhooks;
 use Give\PaymentGateways\PayPalCommerce\Webhooks\Listeners\EventListener;
 
 class PayPalWebhooks {
+	/**
+	 * @var Webhooks
+	 */
+	private $webhooksRepository;
+
+	/**
+	 * PayPalWebhooks constructor.
+	 *
+	 * @since 2.8.0
+	 *
+	 * TODO: Load dependencies via service container once introduced
+	 */
+	public function __construct() {
+		$this->webhooksRepository = new Webhooks();
+	}
+
 	/**
 	 * Handles all webhook event requests. First it verifies that authenticity of the event with
 	 * PayPal, and then it passes the event along to the appropriate listener to finish.
@@ -17,7 +34,7 @@ class PayPalWebhooks {
 	public function handle() {
 		$event = json_decode( file_get_contents( 'php://input' ), false );
 
-		if ( ! $this->verifyEventSignature( $event ) ) {
+		if ( ! $this->webhooksRepository->verifyEventSignature( $event, getallheaders() ) ) {
 			throw new Exception( 'Failed event verification' );
 		}
 
@@ -28,54 +45,6 @@ class PayPalWebhooks {
 		$handler = new $handlerClass();
 
 		$handler->processEvent( $event );
-	}
-
-	/**
-	 *
-	 *
-	 * @param object $event
-	 *
-	 * @return bool
-	 */
-	private function verifyEventSignature( $event ) {
-		$verificationUrl = give_is_test_mode()
-			? 'https://api.sandbox.paypal.com/v1/notifications/verify-webhook-signature'
-			: 'https://api.paypal.com/v1/notifications/verify-webhook-signature';
-
-		// TODO: Retrieve the actual token and webhook ID
-		$token     = 'abc';
-		$webhookId = '';
-
-		$headers = getallheaders();
-
-		$response = wp_remote_post(
-			$verificationUrl,
-			[
-				'headers' => [
-					'Content-Type'  => 'application/json',
-					'Authorization' => "Bearer $token",
-				],
-				'body'    => wp_json_encode(
-					[
-						'transmission_id'   => $headers['PAYPAL-TRANSMISSION-ID'],
-						'transmission_time' => $headers['PAYPAL-TRANSMISSION-TIME'],
-						'cert_url'          => $headers['PAYPAL-CERT-URL'],
-						'auth_algo'         => $headers['AYPAL-AUTH-ALGO'],
-						'transmission_sig'  => $headers['PAYPAL-TRANSMISSION-SIG'],
-						'webhook_id'        => $webhookId,
-						'webhook_event'     => $event,
-					]
-				),
-			]
-		);
-
-		if ( is_wp_error( $response ) ) {
-			return false;
-		}
-
-		$response = json_decode( $response['body'], false );
-
-		return $response->verification_status === 'SUCCESS';
 	}
 
 	/**
---
+++ src/PaymentGateways/PayPalCommerce/Repositories/Webhooks.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Give\PaymentGateways\PayPalCommerce\Repositories;
+
+class Webhooks {
+	/**
+	 * Verifies with PayPal that the given event is securely from PayPal and not some sneaking sneaker
+	 *
+	 * @see https://developer.paypal.com/docs/api/webhooks/v1/#verify-webhook-signature
+	 * @since 2.8.0
+	 *
+	 * @param array $headers The request headers
+	 * @param object $event The event to verify
+	 *
+	 * @return bool
+	 */
+	public function verifyEventSignature( $event, $headers ) {
+		$verificationUrl = give_is_test_mode()
+			? 'https://api.sandbox.paypal.com/v1/notifications/verify-webhook-signature'
+			: 'https://api.paypal.com/v1/notifications/verify-webhook-signature';
+
+		// TODO: Retrieve the actual token and webhook ID
+		$token     = 'abc';
+		$webhookId = '';
+
+		$response = wp_remote_post(
+			$verificationUrl,
+			[
+				'headers' => [
+					'Content-Type'  => 'application/json',
+					'Authorization' => "Bearer $token",
+				],
+				'body'    => wp_json_encode(
+					[
+						'transmission_id'   => $headers['PAYPAL-TRANSMISSION-ID'],
+						'transmission_time' => $headers['PAYPAL-TRANSMISSION-TIME'],
+						'cert_url'          => $headers['PAYPAL-CERT-URL'],
+						'auth_algo'         => $headers['AYPAL-AUTH-ALGO'],
+						'transmission_sig'  => $headers['PAYPAL-TRANSMISSION-SIG'],
+						'webhook_id'        => $webhookId,
+						'webhook_event'     => $event,
+					]
+				),
+			]
+		);
+
+		if ( is_wp_error( $response ) ) {
+			return false;
+		}
+
+		$response = json_decode( $response['body'], false );
+
+		return $response->verification_status === 'SUCCESS';
+	}
+
+	/**
+	 * Creates a webhook with the given event types registered.
+	 *
+	 * @see https://developer.paypal.com/docs/api/webhooks/v1/#webhooks_post
+	 * @since 2.8.0
+	 *
+	 * @param string $url
+	 * @param array $eventTypes a sequential array of events to register
+	 *
+	 * @return mixed
+	 */
+	public function createWebhook( $url, array $eventTypes ) {
+		$apiUrl = give_is_test_mode()
+			? 'https://api.sandbox.paypal.com/v1/notifications/webhooks'
+			: 'https://api.paypal.com/v1/notifications/webhooks';
+
+		// TODO: Retrieve the actual token and webhook ID
+		$token = 'abc';
+
+		$response = wp_remote_post(
+			$apiUrl,
+			[
+				'headers' => [
+					'Content-Type'  => 'application/json',
+					'Authorization' => "Bearer $token",
+				],
+				'body'    => wp_json_encode(
+					[
+						'url'         => $url,
+						'event_types' => array_map(
+							static function ( $eventType ) {
+								return [
+									'name' => $eventType,
+								];
+							},
+							$eventTypes
+						),
+					]
+				),
+			]
+		);
+
+		$response = json_decode( $response['body'], false );
+
+		return $response->id;
+	}
+}
