2b088ca726c43eaa1fa97546fda1177ddbb6d5a2
Kyle Johnson <kbjohnson90+github@gmail.com>
2020-07-14 11:22:40 -0400

    Remove excess character.

--- src/Views/Admin/Pages/templates/setup-page/stories/components.stories.js
+++ src/Views/Admin/Pages/templates/setup-page/stories/components.stories.js
@@ -34,7 +34,7 @@ export const Dismiss = () => Styles +
 		.replace( /{{\s*label\s*}}/gi, 'Dismiss Setup Screen' );
 
 export const SectionBasic = () => Styles + `
-	'<section>
+	<section>
 		<header>
 			<h2>Connect a payment gateway to begin accepting donations</h2>
 		</header>
