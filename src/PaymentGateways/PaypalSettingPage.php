6b3994e78feabdb282e7d5fcfc379cb5e7eb17a6
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-07-15 22:19:23 +0530

    fix: add logic to save client rest api credentials

--- src/PaymentGateways/PaypalSettingPage.php
+++ src/PaymentGateways/PaypalSettingPage.php
@@ -41,7 +41,8 @@ class PaypalSettingPage implements SettingPage {
 		add_action( 'give_get_groups_paypal', [ $this, 'getGroups' ] );
 		add_filter( 'give_get_settings_gateways', [ $this, 'registerPaypalSettings' ] );
 		add_filter( 'give_get_sections_gateways', [ $this, 'registerPaypalSettingSection' ] );
-		add_action( 'give-settings_start', [ $this, 'savePayPalMerchantDetails' ] );
+		// add_action( 'give-settings_start', [ $this, 'savePayPalMerchantDetails' ] );
+		add_action( 'admin_init', [ $this, 'savePayPalMerchantDetails' ] );
 
 		// Load custom setting fields.
 		$adminSettingFields = new PaymentGateways\PayPalCheckout\AdminSettingFields();
@@ -124,8 +125,8 @@ class PaypalSettingPage implements SettingPage {
 	public function savePayPalMerchantDetails() {
 		// Save PayPal merchant details only if admin redirected to PayPal connect setting page after completing onboarding process.
 		if (
-			! isset( $_GET['merchantIdInPayPal'] ) ||
-			! \Give_Admin_Settings::is_setting_page( 'gateways', 'paypal' )
+			! isset( $_GET['merchantIdInPayPal'] )
+			// ! \Give_Admin_Settings::is_setting_page( 'gateways', 'paypal' )
 		) {
 			return;
 		}
@@ -142,11 +143,49 @@ class PaypalSettingPage implements SettingPage {
 			'returnMessage',
 		];
 
-		$allAccounts = maybe_unserialize( give_update_option( 'paypalCheckoutAccounts', [] ) );
+		$allAccounts = (array) maybe_unserialize( give_update_option( 'paypalCheckoutAccounts', [] ) );
 		$paypalData  = array_intersect_key( $paypalGetData, array_flip( $allowedPayPalData ) );
 
 		$allAccounts[ $paypalData['merchantIdInPayPal'] ] = $paypalData;
 
 		give_update_option( 'paypalCheckoutAccounts', maybe_serialize( $allAccounts ) );
+
+		$this->saveSellerRestAPICredentials( $paypalData['merchantIdInPayPal'] );
+	}
+
+	/**
+	 * Save seller rest API credentials
+	 *
+	 * @param string $partnerMerchantId
+	 * @return void
+	 * @since 2.8.0
+	 */
+	private function saveSellerRestAPICredentials( $partnerMerchantId ) {
+		$tokenInfo      = get_option( 'give_paypal_checkout_seller_access_token', [ 'access_token' => '' ] );
+		$payPalResponse = wp_remote_retrieve_body(
+			wp_remote_get(
+				sprintf(
+					'https://api.sandbox.paypal.com/v1/customer/partners/%1$s/merchant-integrations/credentials/',
+					$partnerMerchantId
+				),
+				[
+					'headers' => [
+						'Authorization' => sprintf(
+							'Bearer %1$s',
+							$tokenInfo['access_token']
+						),
+						'Content-Type'  => 'application/json',
+					],
+				]
+			)
+		);
+
+		$mode      = give_is_test_mode() ? 'sandbox' : 'live';
+		$optionKey = 'give_paypal_checkout_seller_rest_api_credentials';
+
+		$optionValue          = get_option( $optionKey, [] );
+		$optionValue[ $mode ] = json_decode( $payPalResponse, true );
+
+		update_option( $optionKey, $optionValue );
 	}
 }
