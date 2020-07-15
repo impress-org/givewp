b9a605d86efb9dfa85aa66bec74b32c4e9c03787
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-07-15 22:37:31 +0530

    chore: rmeove paypal legacy tab menu

--- src/PaymentGateways/PaypalSettingPage.php
+++ src/PaymentGateways/PaypalSettingPage.php
@@ -68,7 +68,6 @@ class PaypalSettingPage implements SettingPage {
 	public function getSettings() {
 		$settings[ $this->paypalCheckout->getId() ] = $this->paypalCheckout->getOptions();
 		$settings[ $this->paypalStandard->getId() ] = $this->paypalStandard->getOptions();
-		$settings['paypal-legacy']                  = [];
 
 		return $settings;
 	}
@@ -83,7 +82,6 @@ class PaypalSettingPage implements SettingPage {
 		return [
 			$this->paypalCheckout->getId() => $this->paypalCheckout->getName(),
 			$this->paypalStandard->getId() => $this->paypalStandard->getName(),
-			'paypal-legacy'                => esc_html__( 'PayPal Legacy', 'give' ),
 		];
 	}
 
