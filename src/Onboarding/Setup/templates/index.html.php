fe123cb0eb69af14d66c3b5e99d5012e04b882eb
Kyle Johnson <kbjohnson90+github@gmail.com>
2020-07-21 13:14:57 -0400

    Implement Stripe Connect button on Setup Page.

--- src/Onboarding/Setup/templates/index.html.php
+++ src/Onboarding/Setup/templates/index.html.php
@@ -49,11 +49,23 @@
 						'row-item',
 						[
 							'class'       => 'stripe',
-							'icon'        => $this->image( 'stripe@2x.min.png' ),
+							'icon'        => $this->image( 'stripe-connect@2x.min.png' ),
 							'icon_alt'    => esc_html__( 'Stripe', 'give' ),
 							'title'       => esc_html__( 'Connect to Stripe', 'give' ),
 							'description' => esc_html__( 'Stripe is one of the most popular payment gateways, and for good reason! Receive one-time and Recurring Donations (add-on) using many of the most popular payment methods. Note: the FREE version of Stripe includes an additional 2% fee for processing one-time donations.', 'give' ),
-							'action'      => '<button><i class="fab fa-stripe-s"></i>&nbsp;&nbsp;Connect to Stripe</button>',
+							'action'      => sprintf(
+								'<a href="%s"><i class="fab fa-stripe-s"></i>&nbsp;&nbsp;Connect with Stripe</a>',
+								add_query_arg(
+									[
+										'stripe_action' => 'connect',
+										'mode'          => give_is_test_mode() ? 'test' : 'live',
+										'return_url'    => rawurlencode( admin_url( 'edit.php?post_type=give_forms&page=give-setup' ) ),
+										'website_url'   => get_bloginfo( 'url' ),
+										'give_stripe_connected' => '0',
+									],
+									esc_url_raw( 'https://connect.givewp.com/stripe/connect.php' )
+								)
+							),
 						]
 					),
 				],
