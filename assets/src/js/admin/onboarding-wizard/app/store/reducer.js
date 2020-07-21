4d692649e6a91b3f0548bb409ca7deeb7b5acd70
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-07-21 13:29:06 -0700

    feature: scaffold store logic

---
+++ assets/src/js/admin/onboarding-wizard/app/store/reducer.js
@@ -0,0 +1,17 @@
+import { getWindowData } from '../../utils';
+
+export const reducer = ( state, action ) => {
+	switch ( action.type ) {
+		case 'GO_TO_STEP':
+			if ( action.payload.step <= state.lastStep ) {
+				return {
+					...state,
+					currentStep: action.payload.step,
+				};
+			}
+			window.location.href = getWindowData( 'setupUrl' );
+			break;
+		default:
+			return state;
+	}
+};
