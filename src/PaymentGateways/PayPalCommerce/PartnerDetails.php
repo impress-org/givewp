af2efd17796c52935a1d56aab8c466651ffe4898
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-07-22 18:04:45 +0530

    chore: add class to access partner details

---
+++ src/PaymentGateways/PayPalCommerce/PartnerDetails.php
@@ -0,0 +1,19 @@
+<?php
+namespace Give\PaymentGateways\PayPalCommerce;
+
+/**
+ * Class PartnerDetails
+ * @package Give\PaymentGateways\PayPalCommerce
+ *
+ * @since 2.8.0
+ */
+class PartnerDetails {
+	/**
+	 * Api Url
+	 *
+	 * @since 2.8.0
+	 *
+	 * @var string
+	 */
+	public static $apiUrl = 'https://connect.givewp.com';
+}
