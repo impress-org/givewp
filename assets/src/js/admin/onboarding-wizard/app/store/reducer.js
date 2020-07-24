2f415946c67bbb074dc93194601e14af9dd08a82
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-07-24 12:30:15 -0700

    feature: add reducer cases for handling wizard inputs

--- assets/src/js/admin/onboarding-wizard/app/store/reducer.js
+++ assets/src/js/admin/onboarding-wizard/app/store/reducer.js
@@ -5,6 +5,48 @@ export const reducer = ( state, action ) => {
 				...state,
 				currentStep: action.payload.step,
 			};
+		case 'SET_USER_TYPE':
+			return {
+				...state,
+				configuration: { ...state.configuration,
+					userType: action.payload.type,
+				},
+			};
+		case 'SET_CAUSE_TYPE':
+			return {
+				...state,
+				configuration: { ...state.configuration,
+					causeType: action.payload.type,
+				},
+			};
+		case 'SET_COUNTRY':
+			return {
+				...state,
+				configuration: { ...state.configuration,
+					country: action.payload.country,
+				},
+			};
+		case 'SET_STATE':
+			return {
+				...state,
+				configuration: { ...state.configuration,
+					state: action.payload.state,
+				},
+			};
+		case 'SET_CURRENCY':
+			return {
+				...state,
+				configuration: { ...state.configuration,
+					currency: action.payload.currency,
+				},
+			};
+		case 'SET_FEATURE_NEEDS':
+			return {
+				...state,
+				configuration: { ...state.configuration,
+					featureNeeds: action.payload.featureNeeds,
+				},
+			};
 		default:
 			return state;
 	}
