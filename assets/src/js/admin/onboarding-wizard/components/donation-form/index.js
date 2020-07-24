8ecccb3a1c40090d1835a1c3f5254a7ac95da384
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-07-24 09:46:41 -0700

    fix: improve design implementation of donation form step

---
+++ assets/src/js/admin/onboarding-wizard/components/donation-form/index.js
@@ -0,0 +1,22 @@
+const DonationForm = () => {
+	const placeholderStyle = {
+		boxShadow: '0px 1.74499px 4.36246px rgba(0, 0, 0, 0.305862)',
+		borderRadius: '6px',
+		width: '480px',
+		height: '595px',
+		background: '#fff',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+		fontSize: '24px',
+		lineHeight: '32px',
+		textAlign: 'center',
+	};
+	return (
+		<div style={ placeholderStyle }>
+			This is a placeholder.
+		</div>
+	);
+};
+
+export default DonationForm;
