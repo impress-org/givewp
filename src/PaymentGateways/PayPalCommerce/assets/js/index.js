e68d27ea07c8b6012838cd04e8964633dafdc138
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-07-28 17:13:22 +0530

    fix: prevent form submissin if paypal commerce selected as gateway

--- src/PaymentGateways/PayPalCommerce/assets/js/index.js
+++ src/PaymentGateways/PayPalCommerce/assets/js/index.js
@@ -1,3 +1,5 @@
+/* globals jQuery */
+import DonationForm from './DonationForm';
 import SmartButtons from './paypal-smart-buttons';
 
 document.addEventListener( 'DOMContentLoaded', () => {
@@ -7,5 +9,17 @@ document.addEventListener( 'DOMContentLoaded', () => {
 		const $form = $formWrap.querySelector( '.give-form' );
 		const smartButtons = new SmartButtons( $form );
 		smartButtons.boot();
+	} );
+
+	// On form submit prevent submission for PayPal commerce.
+	// Form submission will be take care internally by smart buttons or advanced card fields.
+	jQuery( 'form.give-form' ).on( 'submit', e => {
+		if ( ! DonationForm.isPayPalCommerceSelected( jQuery( this ) ) || ! DonationForm.isDonationFormHtml5Valid( jQuery( this ).get( 0 ) ) ) {
+			return true;
+		}
+
+		e.preventDefault();
+
+		return false;
 	} );
 } );
