301b5f7fb56149d7641209aeaa96c59758592f2e
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-07-31 01:31:15 +0530

    chore: add todo

--- src/PaymentGateways/PayPalCommerce/assets/js/AdvancedCardFields.js
+++ src/PaymentGateways/PayPalCommerce/assets/js/AdvancedCardFields.js
@@ -33,6 +33,7 @@ class AdvancedCardFields extends PaymentMethod {
 			fields: this.getFields(),
 		} );
 
+		// @todo: add form submit event only if processing payment with advanced card fields.
 		this.jQueryForm.on( 'submit', { hostedCardFields }, onSubmitHandlerForDonationForm );
 	}
 
