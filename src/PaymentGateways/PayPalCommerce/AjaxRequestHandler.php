5f98776d22351ef4a0d06e045579b5d9f9cb5aaa
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-07-30 10:27:28 +0530

    chore: use getApiUrl fn

--- src/PaymentGateways/PayPalCommerce/AjaxRequestHandler.php
+++ src/PaymentGateways/PayPalCommerce/AjaxRequestHandler.php
@@ -60,7 +60,7 @@ class AjaxRequestHandler {
 
 		$payPalResponse = wp_remote_retrieve_body(
 			wp_remote_post(
-				$this->paypalClient->getEnvironment()->baseUrl() . '/v1/oauth2/token',
+				$this->paypalClient->getApiUrl( '/v1/oauth2/token' ),
 				[
 					'headers' => [
 						'Authorization' => sprintf(
