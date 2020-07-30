b4f40d0357a39b0b6013ebc596f330bc14ffa580
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-07-30 22:13:21 +0530

    refactor: remove repository related code from PayPalOrder model

--- src/PaymentGateways/PayPalCommerce/PayPalOrder.php
+++ src/PaymentGateways/PayPalCommerce/PayPalOrder.php
@@ -1,11 +1,8 @@
 <?php
 namespace Give\PaymentGateways\PayPalCommerce;
 
-use Exception;
 use Give\Helpers\ArrayDataSet;
 use InvalidArgumentException;
-use PayPalCheckoutSdk\Orders\OrdersCaptureRequest;
-use PayPalCheckoutSdk\Orders\OrdersCreateRequest;
 use stdClass;
 
 /**
@@ -97,103 +94,6 @@ class PayPalOrder {
 	public $payment;
 
 	/**
-	 * @since 2.8.0
-	 *
-	 * @var PayPalClient
-	 */
-	private $paypalClient;
-
-	/**
-	 * @since 2.8.0
-	 *
-	 * @var MerchantDetail
-	 */
-	private $merchantDetails;
-
-	/**
-	 * PayPalOrder constructor.
-	 *
-	 * @param  PayPalClient  $paypalClient
-	 * @param  MerchantDetail  $merchantDetails
-	 */
-	public function __construct( PayPalClient $paypalClient, MerchantDetail $merchantDetails ) {
-		$this->paypalClient    = $paypalClient;
-		$this->merchantDetails = $merchantDetails;
-	}
-
-	/**
-	 * Approve order.
-	 *
-	 * @since 2.8.0
-	 *
-	 * @param string $orderId
-	 *
-	 * @return string
-	 * @throws Exception
-	 */
-	public function approveOrder( $orderId ) {
-		$request = new OrdersCaptureRequest( $orderId );
-
-		try {
-			return $this->paypalClient->getHttpClient()->execute( $request )->result;
-		} catch ( Exception $ex ) {
-			throw $ex;
-		}
-	}
-
-	/**
-	 * Create order.
-	 *
-	 * @since 2.8.0
-	 *
-	 * @param array $array
-	 *
-	 * @return string
-	 * @throws Exception
-	 */
-	public function createOrder( $array ) {
-		$this->validateCreateOrderArguments( $array );
-
-		$request = new OrdersCreateRequest();
-		$request->payPalPartnerAttributionId( PartnerDetails::$attributionId );
-		$request->body = [
-			'intent'              => 'CAPTURE',
-			'purchase_units'      => [
-				[
-					'reference_id'        => get_post_field( 'post_name', $array['formId'] ),
-					'description'         => '',
-					'amount'              => [
-						'value'         => give_maybe_sanitize_amount( $array['donationAmount'] ),
-						'currency_code' => give_get_currency( $array['formId'] ),
-					],
-					'payee'               => [
-						'email_address' => $this->merchantDetails->merchantId,
-						'merchant_id'   => $this->merchantDetails->merchantIdInPayPal,
-					],
-					'payer'               => [
-						'given_name'    => $array['payer']['firstName'],
-						'surname'       => $array['payer']['lastName'],
-						'email_address' => $array['payer']['email'],
-					],
-					'payment_instruction' => [
-						'disbursement_mode' => 'INSTANT',
-					],
-				],
-			],
-			'application_context' => [
-				'shipping_preference' => 'NO_SHIPPING',
-				'user_action'         => 'PAY_NOW',
-			],
-		];
-
-		try {
-			return $this->paypalClient->getHttpClient()->execute( $request )->result->id;
-		} catch ( Exception $ex ) {
-			throw $ex;
-		}
-	}
-
-	/**
 	 * Create PayPalOrder object from given array.
 	 *
 	 * @since 2.8.0
@@ -237,7 +137,6 @@ class PayPalOrder {
 	 */
 	public function getPayment() {
 		return PayPalPayment::fromArray( (array) current( $this->purchaseUnit->payments->captures ) );
-
 	}
 
 	/**
@@ -254,23 +153,6 @@ class PayPalOrder {
 
 		if ( array_diff( $required, array_keys( $array ) ) ) {
 			throw new InvalidArgumentException( __( 'To create a PayPalOrder object, please provide valid id, intent, payer, create_time, update_time, status, links and purchase_units', 'give' ) );
-		}
-	}
-
-	/**
-	 * Validate argument given to create PayPal order.
-	 *
-	 * @since 2.8.0
-	 *
-	 * @param array $array
-	 * @throws InvalidArgumentException
-	 */
-	private function validateCreateOrderArguments( $array ) {
-		$required = [ 'formId', 'donationAmount', 'payer' ];
-		$array    = array_filter( $array ); // Remove empty values.
-
-		if ( array_diff( $required, array_keys( $array ) ) ) {
-			throw new InvalidArgumentException( __( 'To create a paypal order, please provide formId, donationAmount and payer', 'give' ) );
 		}
 	}
 }
