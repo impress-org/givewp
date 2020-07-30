b8137bb34fd6e8c0b4f766835fdda9e7b64bda56
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-07-31 01:24:52 +0530

    fix: remove extra slash from api url

--- src/PaymentGateways/PayPalCommerce/Repositories/MerchantDetails.php
+++ src/PaymentGateways/PayPalCommerce/Repositories/MerchantDetails.php
@@ -65,7 +65,7 @@ class MerchantDetails {
 		$merchant = give( MerchantDetail::class );
 		$response = wp_remote_retrieve_body(
 			wp_remote_post(
-				give( PayPalClient::class )->getApiUrl( '/v1/identity/generate-token' ),
+				give( PayPalClient::class )->getApiUrl( 'v1/identity/generate-token' ),
 				[
 					'headers' => [
 						'Accept'          => 'application/json',
