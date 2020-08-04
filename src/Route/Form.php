840ac527729c75bee8de7be70044b824bd6ad5a6
Kyle Johnson <kbjohnson90+github@gmail.com>
2020-08-04 14:57:57 -0400

    chore: Simplify getURL to only use query strings regardless of permalink setting.

--- src/Route/Form.php
+++ src/Route/Form.php
@@ -110,15 +110,13 @@ class Form {
 	 * @return string
 	 */
 	public function getURL( $form_id ) {
-		return get_option( 'permalink_structure' )
-			? home_url( "/{$this->base}/{$form_id}" )
-			: add_query_arg(
-				[
-					'give_form_id' => $form_id,
-					'url_prefix'   => $this->base,
-				],
-				home_url()
-			);
+		return add_query_arg(
+			[
+				'give_form_id' => $form_id,
+				'url_prefix'   => $this->base,
+			],
+			home_url()
+		);
 	}
 
 
