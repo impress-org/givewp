ba8df5d4b8265fb7ae3abd848fd0818f340c75be
Devin Walker <sdtucson@gmail.com>
2020-08-04 12:52:58 -0700

    chore: bump versions, modify changelog, add kyle to authors in readme

--- give.php
+++ give.php
@@ -5,7 +5,7 @@
  * Description: The most robust, flexible, and intuitive way to accept donations on WordPress.
  * Author: GiveWP
  * Author URI: https://givewp.com/
- * Version: 2.7.3
+ * Version: 2.7.4
  * Text Domain: give
  * Domain Path: /languages
  *
@@ -230,7 +230,7 @@ final class Give {
 	private function setup_constants() {
 		// Plugin version.
 		if ( ! defined( 'GIVE_VERSION' ) ) {
-			define( 'GIVE_VERSION', '2.7.3' );
+			define( 'GIVE_VERSION', '2.7.4' );
 		}
 
 		// Plugin Root File.
