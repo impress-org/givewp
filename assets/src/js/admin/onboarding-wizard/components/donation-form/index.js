b663aa4c3f45ca778277bffefad9e11dceed0c9a
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-08-05 10:31:54 -0700

    feature: add iframe to DonationFormPreview component

--- assets/src/js/admin/onboarding-wizard/components/donation-form/index.js
+++ assets/src/js/admin/onboarding-wizard/components/donation-form/index.js
@@ -1,21 +1,14 @@
+import { getWindowData } from '../../utils';
+
 const DonationForm = () => {
-	const placeholderStyle = {
-		boxShadow: '0px 1.74499px 4.36246px rgba(0, 0, 0, 0.305862)',
-		borderRadius: '6px',
-		width: '480px',
-		height: '595px',
-		background: '#fff',
-		display: 'flex',
-		alignItems: 'center',
-		justifyContent: 'center',
-		fontSize: '24px',
-		lineHeight: '32px',
-		textAlign: 'center',
+	const formPreviewUrl = getWindowData( 'formPreviewUrl' );
+	const iframeStyle = {
+		border: 'none',
+		width: '580px',
+		height: '800px',
 	};
 	return (
-		<div style={ placeholderStyle }>
-			This is a placeholder.
-		</div>
+		<iframe src={ formPreviewUrl } style={ iframeStyle } />
 	);
 };
 
