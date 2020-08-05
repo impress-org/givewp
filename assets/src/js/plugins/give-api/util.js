01899781e3f482a72ebc881f89e5e0da1e9d3dea
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-08-06 01:38:12 +0530

    chore: add helper function

--- assets/src/js/plugins/give-api/util.js
+++ assets/src/js/plugins/give-api/util.js
@@ -1,3 +1,20 @@
 export default {
-	fn: {},
+	fn: {
+		/**
+		 * Copy style.
+		 *
+		 * @since 2.8.0
+		 *
+		 * @param {object} sourceNode Source not selector
+		 * @param {object} targetNode Target not selector
+		 * @param {object} whileListedProperties List of style properties
+		 */
+		copyNodeStyle: function( sourceNode, targetNode, whileListedProperties ) {
+			const computedStyle = window.getComputedStyle( sourceNode );
+
+			if ( whileListedProperties ) {
+				whileListedProperties.forEach( key => targetNode.style.setProperty( key, computedStyle.getPropertyValue( key ), computedStyle.getPropertyPriority( key ) ) );
+			}
+		},
+	},
 };
