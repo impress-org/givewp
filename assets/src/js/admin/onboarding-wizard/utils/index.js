e3b2972b1306c81a5f4e85973e959248c94208ba
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-08-05 15:35:42 -0700

    chore: add doc block for kebab case util

--- assets/src/js/admin/onboarding-wizard/utils/index.js
+++ assets/src/js/admin/onboarding-wizard/utils/index.js
@@ -9,6 +9,13 @@ export const getWindowData = ( value ) => {
 	return data[ value ];
 };
 
+/**
+ * Returns string in Kebab Case (ex: kebab-case)
+ *
+ * @param {string} str String to be returned in Kebab Case
+ * @return {string} String returned in Kebab Case
+ * @since 2.8.0
+ */
 export const toKebabCase = ( str ) => {
 	return str.replace( ' / ', ' ' )
 		.replace( /([a-z])([A-Z])/g, '$1-$2' )
