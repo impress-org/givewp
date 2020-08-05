0326b11d9e17e43fe1b0458b6e119e6d1dc3a396
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-08-05 11:36:41 -0700

    feature: add loading state for donation form iframe

--- assets/src/js/admin/onboarding-wizard/components/donation-form/index.js
+++ assets/src/js/admin/onboarding-wizard/components/donation-form/index.js
@@ -1,14 +1,36 @@
+// Import vendor dependencies
+import { useState } from 'react';
+const { __ } = wp.i18n;
+
+// Import utilities
 import { getWindowData } from '../../utils';
+
+// Import components
+import ConfigurationIcon from '../icons/configuration';
+
+// Import styles
+import './style.scss';
 
 const DonationForm = () => {
 	const formPreviewUrl = getWindowData( 'formPreviewUrl' );
+	const [ iframeLoaded, setIframeLoaded ] = useState( false );
+
 	const iframeStyle = {
-		border: 'none',
-		width: '580px',
-		height: '800px',
+		opacity: iframeLoaded === false ? '0' : '1',
+	};
+	const messageStyle = {
+		opacity: iframeLoaded === false ? '1' : '0',
 	};
 	return (
-		<iframe src={ formPreviewUrl } style={ iframeStyle } />
+		<div className="give-obw-donation-form-preview">
+			<div className="give-obw-donation-form-preview__loading-message" style={ messageStyle }>
+				<ConfigurationIcon />
+				<h3>
+					{ __( 'Building Form Preview...', 'give' ) }
+				</h3>
+			</div>
+			<iframe onLoad={ () => setIframeLoaded( true ) } className="give-obw-donation-form-preview__iframe" src={ formPreviewUrl } style={ iframeStyle } />
+		</div>
 	);
 };
 
