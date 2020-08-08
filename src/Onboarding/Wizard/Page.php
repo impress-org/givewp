e5e9d5638cf48459e92b5390dc23734ca1bd7b10
Kyle Johnson <kbjohnson90+github@gmail.com>
2020-08-07 21:41:51 -0400

    Add page check before enqueing styles.

--- src/Onboarding/Wizard/Page.php
+++ src/Onboarding/Wizard/Page.php
@@ -80,6 +80,10 @@ class Page {
 	 **/
 	public function enqueue_scripts() {
 
+		if ( empty( $_GET['page'] ) || $this->slug !== $_GET['page'] ) { // WPCS: CSRF ok, input var ok.
+			return;
+		}
+
 		wp_enqueue_style(
 			'give-google-font-montserrat',
 			'https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap',
