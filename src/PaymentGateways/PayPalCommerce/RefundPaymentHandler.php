79f991b72330d0364c56981598adbf577d05084a
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-08-10 19:51:29 +0530

    fix: show paypal payment refund failure notice on redirect

--- src/PaymentGateways/PayPalCommerce/RefundPaymentHandler.php
+++ src/PaymentGateways/PayPalCommerce/RefundPaymentHandler.php
@@ -35,13 +35,52 @@ class RefundPaymentHandler {
 	 *
 	 * @since 2.8.0
 	 *
-	 * @param Give_Payment $donation
+	 * @param int $donationId
 	 *
 	 * @throws Exception
 	 */
-	public function refundPayment( Give_Payment $donation ) {
-		$payPalPaymentId = give_get_payment_transaction_id( $donation->ID );
+	public function refundPayment( $donationId ) {
+		$payPalPaymentId   = give_get_payment_transaction_id( $donationId );
+		$newDonationStatus = give_clean( $_POST['give-payment-status'] );
 
-		$this->ordersRepository->refundPayment( $payPalPaymentId );
+		if ( 'refunded' !== $newDonationStatus ) {
+			return;
+		}
+
+		try {
+			$this->ordersRepository->refundPayment( $payPalPaymentId );
+		} catch ( Exception $ex ) {
+			wp_safe_redirect( admin_url( "edit.php?post_type=give_forms&page=give-payment-history&view=view-payment-details&id={$donationId}&paypal-error=refund-failure" ) );
+			exit();
+		}
+	}
+
+
+	/**
+	 * show Paypal Commerce payment refund failure notice.
+	 *
+	 * @since 2.8.0
+	 */
+	public function showPaymentRefundFailureNotice() {
+		if ( ! isset( $_GET['paypal-error'] ) || 'refund-failure' !== $_GET['paypal-error'] ) {
+			return;
+		}
+
+		give( 'notices' )->register_notice(
+			[
+				'id'          => 'give-paypal-commerce-refund-failure',
+				'type'        => 'warning',
+				'show'        => true,
+				'description' => sprintf(
+					'<strong>%1$s</strong>&nbsp;%2$s&nbsp;%3$s<a href="%4$s" target="_blank">%5$s</a>%6$s',
+					esc_html__( 'PayPal Donations:', 'give' ),
+					esc_html__( 'We were unable to process refund.', 'give' ),
+					esc_html__( 'Please', 'give' ),
+					admin_url( 'edit.php?post_type=give_forms&page=give-tools&tab=logs' ),
+					esc_html__( ' check log ', 'give' ),
+					esc_html__( 'for detailed information.', 'give' )
+				),
+			]
+		);
 	}
 }
