49330caa2868e901d3ae87af103fc0548492ebaa
Jason Adams <jason.the.adams@gmail.com>
2020-08-10 17:00:54 -0700

    feature: retrieve payment from PayPal to finish refund

--- src/PaymentGateways/PayPalCommerce/Webhooks/Listeners/PaymentCaptureRefunded.php
+++ src/PaymentGateways/PayPalCommerce/Webhooks/Listeners/PaymentCaptureRefunded.php
@@ -2,7 +2,7 @@
 
 namespace Give\PaymentGateways\PayPalCommerce\Webhooks\Listeners;
 
-use Give\PaymentGateways\PayPalCommerce\Models\PayPalOrder;
+use Give\PaymentGateways\PayPalCommerce\Repositories\MerchantDetails;
 use Give\Repositories\PaymentsRepository;
 
 class PaymentCaptureRefunded implements EventListener {
@@ -14,21 +14,30 @@ class PaymentCaptureRefunded implements EventListener {
 	private $paymentsRepository;
 
 	/**
+	 * @var MerchantDetails
+	 */
+	private $merchantDetails;
+
+	/**
 	 * PaymentCaptureRefunded constructor.
 	 *
 	 * @since 2.8.0
 	 *
 	 * @param PaymentsRepository $paymentsRepository
+	 * @param MerchantDetails    $merchantDetails
 	 */
-	public function __construct( PaymentsRepository $paymentsRepository ) {
+	public function __construct( PaymentsRepository $paymentsRepository, MerchantDetails $merchantDetails ) {
 		$this->paymentsRepository = $paymentsRepository;
+		$this->merchantDetails    = $merchantDetails;
 	}
 
 	/**
 	 * @inheritDoc
 	 */
 	public function processEvent( $event ) {
-		$donation = $this->paymentsRepository->getDonationByPayment( $event->resource->id );
+		$paymentId = $this->getPaymentFromRefund( $event->resource );
+
+		$donation = $this->paymentsRepository->getDonationByPayment( $paymentId );
 
 		// If there's no matching donation then it's not tracked by GiveWP
 		if ( ! $donation ) {
@@ -44,5 +53,42 @@ class PaymentCaptureRefunded implements EventListener {
 		 * @since 2.8.0
 		 */
 		do_action( 'give_paypal_commerce_webhook_charge_refunded', $event, $donation );
+	}
+
+	/**
+	 * This uses the links property of the refund to retrieve the refunded Payment from PayPal
+	 *
+	 * @since 2.8.0
+	 *
+	 * @param object $refund
+	 *
+	 * @return string
+	 */
+	private function getPaymentFromRefund( $refund ) {
+		$link = current(
+			array_filter(
+				$refund->links,
+				function ( $link ) {
+					return $link->rel === 'up';
+				}
+			)
+		);
+
+		$accountDetails = $this->merchantDetails->getDetails();
+
+		$response = wp_remote_request(
+			$link->href,
+			[
+				'method'  => $link->method,
+				'headers' => [
+					'Content-Type'  => 'application/json',
+					'Authorization' => "Bearer $accountDetails->accessToken",
+				],
+			]
+		);
+
+		$response = json_decode( $response['body'], false );
+
+		return $response->id;
 	}
 }
