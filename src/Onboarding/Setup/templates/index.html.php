29f0fdbe55140a0e9188bc37ae14a0d667e7c04d
Kyle Johnson <kbjohnson90+github@gmail.com>
2020-08-11 14:55:56 -0400

    Update PayPal placeholder with link to settings page.

--- src/Onboarding/Setup/templates/index.html.php
+++ src/Onboarding/Setup/templates/index.html.php
@@ -50,11 +50,23 @@
 					$this->render_template(
 						'row-item',
 						[
+							'class'       => 'paypal',
 							'icon'        => $this->image( 'paypal@2x.min.png' ),
 							'icon_alt'    => esc_html__( 'PayPal', 'give' ),
 							'title'       => esc_html__( 'Connect to PayPal', 'give' ),
 							'description' => esc_html__( 'PayPal is synonymous with nonprofits and online charitable gifts. It’s been the go-to payment merchant in for many of the worlds top NGOs. Accept PayPal, Credit and Debit Cards, and more using PayPal’s Smart Buttons without any added platform fees.', 'give' ),
-							'action'      => '<img src="' . GIVE_PLUGIN_URL . 'assets/dist/images/setup-page/paypal.svg' . '" alt="Connect to PayPal" />',
+							'action'      => sprintf(
+								'<a href="%s"><i class="fab fa-paypal"></i>&nbsp;&nbsp;Connect to PayPal</a>',
+								add_query_arg(
+									[
+										'post_type' => 'give_forms',
+										'page'      => 'give-settings',
+										'tab'       => 'gateways',
+										'section'   => 'paypal-standard',
+									],
+									esc_url_raw( admin_url( 'edit.php' ) )
+								)
+							),
 						]
 					),
 					! \Give\Helpers\Gateways\Stripe::isAccountConfigured() ? $this->render_template(
