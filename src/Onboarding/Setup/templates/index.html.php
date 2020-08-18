7c9f20344470457546389d6b992347f552ce5a20
Kyle Johnson <kbjohnson90+github@gmail.com>
2020-08-18 19:59:12 -0400

    Update Setup Page Stripe colors.

--- src/Onboarding/Setup/templates/index.html.php
+++ src/Onboarding/Setup/templates/index.html.php
@@ -83,7 +83,7 @@
 							'class'       => ( $this->isStripeSetup() ) ? 'stripe setup-item-completed' : 'stripe',
 							'icon'        => ( $this->isStripeSetup() )
 											 ? $this->image( 'check-circle.min.png' )
-											 : $this->image( 'stripe-connect@2x.min.png' ),
+											 : $this->image( 'stripe@2x.min.png' ),
 							'icon_alt'    => esc_html__( 'Stripe', 'give' ),
 							'title'       => esc_html__( 'Connect to Stripe', 'give' ),
 							'description' => esc_html__( 'Stripe is one of the most popular payment gateways, and for good reason! Receive one-time and Recurring Donations (add-on) using many of the most popular payment methods. Note: the FREE version of Stripe includes an additional 2% fee for processing one-time donations.', 'give' ),
@@ -121,7 +121,7 @@
 						[
 							'id'          => 'stripeWebhooks',
 							'class'       => 'stripe stripe-webhooks',
-							'icon'        => $this->image( 'stripe-connect@2x.min.png' ),
+							'icon'        => $this->image( 'stripe@2x.min.png' ),
 							'icon_alt'    => esc_html__( 'Stripe', 'give' ),
 							'title'       => esc_html__( 'Please configure your Stripe webhook to finalize the setup.', 'give' ),
 							'description' => esc_html__( 'In order for Stripe to function properly, you must add a new Stripe webhook endpoint. To do this please visit the Webhooks Section of your Stripe Dashboard and click the Add endpoint button and paste the following URL: ', 'give' ) . '<br /><span id="stripeWebhooksCopyHandler" class="stripe-webhooks-url"><input disabled="disabled" id="stripeWebhooksCopy" value="' . add_query_arg( 'give-listener', 'stripe', site_url() ) . '" /> &nbsp; <i id="stripeWebhooksCopyIcon" class="fa fa-clipboard"></i></input>',
