0054cb50f8cba064e169cc901eb48a365c17b428
Kyle Johnson <kbjohnson90+github@gmail.com>
2020-08-18 11:17:18 -0400

    Improve Stripe Setup completed state.

--- src/Onboarding/Setup/templates/index.html.php
+++ src/Onboarding/Setup/templates/index.html.php
@@ -83,21 +83,23 @@
 							'class'       => ( $this->isStripeSetup() ) ? 'stripe setup-item-completed' : 'stripe',
 							'icon'        => ( $this->isStripeSetup() )
 											 ? $this->image( 'check-circle.min.png' )
-											 : $this->image( 'stripe@2x.min.png' ),
+											 : $this->image( 'stripe-connect@2x.min.png' ),
 							'icon_alt'    => esc_html__( 'Stripe', 'give' ),
 							'title'       => esc_html__( 'Connect to Stripe', 'give' ),
-							'description' => esc_html__( 'Stripe is one of the most popular payment gateways, and for good reason! Receive one-time and Recurring Donations (add-on) using many of the most popular payment methods. Note: the FREE version of Stripe includes an additional 2% fee for processing one-time donations. Remove the fee by installing and activating the premium Stripe add-on.', 'give' ),
-							'action'      => sprintf(
-								'<a href="%s"><i class="fab fa-stripe-s"></i>&nbsp;&nbsp;Connect with Stripe</a>',
-								add_query_arg(
-									[
-										'stripe_action' => 'connect',
-										'mode'          => give_is_test_mode() ? 'test' : 'live',
-										'return_url'    => rawurlencode( admin_url( 'edit.php?post_type=give_forms&page=give-setup' ) ),
-										'website_url'   => get_bloginfo( 'url' ),
-										'give_stripe_connected' => '0',
-									],
-									esc_url_raw( 'https://connect.givewp.com/stripe/connect.php' )
+							'description' => esc_html__( 'Stripe is one of the most popular payment gateways, and for good reason! Receive one-time and Recurring Donations (add-on) using many of the most popular payment methods. Note: the FREE version of Stripe includes an additional 2% fee for processing one-time donations.', 'give' ),
+							'action'      => ( $this->isStripeSetup() )
+								? sprintf(
+									'<a href="%s"><i class="fab fa-stripe-s"></i>&nbsp;&nbsp;%s</a>',
+									add_query_arg(
+										[
+											'post_type' => 'give_forms',
+											'page'      => 'give-settings',
+											'tab'       => 'gateways',
+											'section'   => 'stripe-settings',
+										],
+										esc_url_raw( admin_url( 'edit.php' ) )
+									),
+									__( 'Stripe Settings', 'give' )
 								)
 								: sprintf(
 									'<a href="%s"><i class="fab fa-stripe-s"></i>&nbsp;&nbsp;Connect with Stripe</a>',
