1774ed0219840192204288b83fb4895b1ad5cf0b
Kyle Johnson <kbjohnson90+github@gmail.com>
2020-08-19 13:29:47 -0400

    Update plugin link for settings, stripe accounts.

--- includes/gateways/stripe/includes/admin/class-give-stripe-admin-settings.php
+++ includes/gateways/stripe/includes/admin/class-give-stripe-admin-settings.php
@@ -752,9 +752,9 @@ if ( ! class_exists( 'Give_Stripe_Admin_Settings' ) ) {
 								<?php
 								echo sprintf(
 									__( 'NOTE: You are using the free Stripe payment gateway integration. This includes an additional 2%% fee for processing one-time donations. This fee is removed by activating the premium <a href="%1$s" target="_blank">Stripe add-on</a> and never applies to subscription donations made through the <a href="%2$s" target="_blank">Recurring Donations add-on</a>. <a href="%3$s" target="_blank">Learn More ></a>', 'give' ),
-									esc_url( 'https://givewp.com/addons/stripe-gateway/' ),
-									esc_url( 'https://givewp.com/addons/recurring-donations/' ),
-									esc_url( 'http://docs.givewp.com/addon-stripe' )
+									esc_url( 'http://docs.givewp.com/settings-stripe-addon' ),
+									esc_url( 'http://docs.givewp.com/settings-stripe-recurring' ),
+									esc_url( 'http://docs.givewp.com/settings-stripe-free' )
 								);
 								?>
 							</p>
