1721534fcc64282b4d6ca891fceae581dc0ebdbc
Kyle Johnson <kbjohnson90+github@gmail.com>
2020-08-20 14:22:59 -0400

    Add uncommited class.

--- src/Onboarding/Helpers/LocationList.php
+++ src/Onboarding/Helpers/LocationList.php
@@ -7,16 +7,15 @@ namespace Give\Onboarding\Helpers;
  * @since 2.8.0
  */
 class LocationList {
+    public static function getCountries() {
+        $countries = give_get_country_list();
+        unset( $countries[ '' ] );
+        return FormatList::fromKeyValue( $countries );
+    }
 
-	public static function getCountries() {
-		$countries = give_get_country_list();
-		unset( $countries[''] );
-		return FormatList::fromKeyValue( $countries );
-	}
-
-	public static function getStates( $country ) {
-		$states     = give_get_states( $country );
-		$states[''] = sprintf( '%s...', esc_html__( 'Select', 'give' ) );
-		return FormatList::fromKeyValue( $states );
-	}
+    public static function getStates( $country ) {
+        $states = give_get_states( $country );
+        $states[''] = sprintf( '%s...', esc_html__( 'Select', 'give' ) );
+        return FormatList::fromKeyValue( $states );
+    }
 }
