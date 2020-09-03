58642914ee09d0fa8c819492392adc490858a6da
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-09-03 11:30:46 -0400

    feature: use generic milestone component template (instead of block)

--- src/Milestones/templates/milestone.php
+++ src/Milestones/templates/milestone.php
@@ -3,9 +3,6 @@
  * Milestone block/shortcode template
  * Styles for this template are defined in 'blocks/milestone/common.scss'
  *
- * @param array $attributes[
- *  'title' => 'string',
- * ]
  */
 ?>
 
@@ -17,14 +14,25 @@
 	</div>
 	<div class="give-milestone__section">
 		<div class="give-milestone__title">
-			<?php echo $attributes['title']; ?>
+			<?php echo $this->getTitle(); ?>
 		</div>
 		<div class="give-milestone__progress">
 			<div class="give-milestone__progress-bar" style="width: 60%"></div>
 		</div>
 		<div class="give-milestone__context">
 			<span>
-				$600 of $1,000
+				<?php
+				echo give_currency_filter(
+					give_format_amount(
+						$this->getEarnings(),
+						[
+							'sanitize' => false,
+							'decimal'  => false,
+						]
+					)
+				);
+				?>
+				 of $1,000
 			</span>
 			<span>
 				15 Days to Go
