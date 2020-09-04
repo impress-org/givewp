ab6d24435a7b1c357e781b78d293ce913c19c20d
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-09-04 15:55:33 -0400

    chore: add docblocks

--- src/Milestones/resources/js/data/utils.js
+++ src/Milestones/resources/js/data/utils.js
@@ -1,6 +1,12 @@
 const { useSelect } = wp.data;
 const { __ } = wp.i18n;
 
+/**
+ * Get array of form options for a select control
+ *
+ * @since 2.9.0
+ * @return {array} Array of options for a select control
+ */
 export const useFormOptions = () => {
 	const formOptions = useSelect( ( select ) => {
 		const records = select( 'core' ).getEntityRecords( 'postType', 'give_forms' );
@@ -17,6 +23,12 @@ export const useFormOptions = () => {
 	return formOptions;
 };
 
+/**
+ * Get array of form tag options for a select control
+ *
+ * @since 2.9.0
+ * @return {array} Array of options for a select control
+ */
 export const useTagOptions = () => {
 	const tagOptions = useSelect( ( select ) => {
 		const records = select( 'core' ).getEntityRecords( 'taxonomy', 'give_forms_tag', { per_page: 100 } );
@@ -33,6 +45,12 @@ export const useTagOptions = () => {
 	return tagOptions;
 };
 
+/**
+ * Get array of form category options for a select control
+ *
+ * @since 2.9.0
+ * @return {array} Array of options for a select control
+ */
 export const useCategoryOptions = () => {
 	const categoryOptions = useSelect( ( select ) => {
 		const records = select( 'core' ).getEntityRecords( 'taxonomy', 'give_forms_category', { per_page: 100 } );
