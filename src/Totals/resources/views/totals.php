6edb15f4ac825a898f3c9ddb53b8e29eb1761324
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-09-15 12:18:30 -0400

    fix: improve markup and styles

--- src/Totals/resources/views/totals.php
+++ src/Totals/resources/views/totals.php
@@ -6,32 +6,26 @@
  */
 ?>
 
-<div class="give-total">
-	<div class="give-total__section">
-		<div class="give-total__message">
+<div class="give-totals">
+	<div class="give-totals__content">
+		<div class="give-totals__message">
 			<?php echo $this->getMessage(); ?>
 		</div>
 		<?php if ( ! empty( $this->getLinkUrl() ) && ! empty( $this->getLinkText() ) ) : ?>
-			<div class="give-total__link">
-				<a href="<?php echo $this->getLinkUrl(); ?>" target="<?php echo $this->getLinkTarget(); ?>"><?php echo $this->getLinkText(); ?></a>
-			</div>
+		<div class="give-totals__link">
+			<a href="<?php echo $this->getLinkUrl(); ?>" target="<?php echo $this->getLinkTarget(); ?>"><?php echo $this->getLinkText(); ?></a>
+		</div>
 		<?php endif; ?>
+	</div>
+	<div class="give-totals__goal">
 		<?php if ( ! empty( $this->getGoal() ) ) : ?>
-		<div class="give-total__progress">
+		<div class="give-totals__progress">
 			<?php $percent = ( $this->getTotal() / $this->getGoal() ) * 100; ?>
-			<div class="give-total__progress-bar" style="width: <?php echo $percent < 100 ? $percent : 100; ?>%"></div>
+			<div class="give-totals__progress-bar" style="width: <?php echo $percent < 100 ? $percent : 100; ?>%"></div>
+			<div class="give-totals__progress-text"> 
+				<?php echo sprintf( __( '%1$d of %2$s', 'give' ), $this->getFormattedTotal(), $this->getFormattedGoal() ); ?>
+			</div>
 		</div>
 		<?php endif; ?>
-		<div class="give-total__context">
-			<span> 
-				<?php echo $this->getFormattedTotal(); ?>
-				<?php
-				if ( ! empty( $this->getGoal() ) ) {
-					echo __( ' of ', 'give' );
-					echo $this->getFormattedGoal();
-				}
-				?>
-			</span>
-		</div>
 	</div>
 </div>
