ebf5f0f94161899dd957a308daed11ea39629263
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-09-17 10:48:48 -0400

    fix: rearrange order of block elements

--- src/Totals/resources/views/totals.php
+++ src/Totals/resources/views/totals.php
@@ -7,6 +7,20 @@
 ?>
 
 <div class="give-totals">
+	<?php if ( ! empty( $this->getGoal() ) ) : ?>
+	<div class="give-totals__goal">
+		<p class="give-totals__progress-text"> 
+			<?php
+				$total = $this->metric === 'revenue' ? $this->getFormattedTotal() : $this->getTotal();
+				echo sprintf( __( '%1$s of %2$s', 'give' ), $total, $this->getFormattedGoal() );
+			?>
+		</p>
+		<div class="give-totals__progress">
+			<?php $percent = ( $this->getTotal() / $this->getGoal() ) * 100; ?>
+			<div class="give-totals__progress-bar" style="width: <?php echo min( [ $percent, 100 ] ); ?>%; background: linear-gradient(180deg, <?php echo $this->getColor(); ?> 0%, <?php echo $this->getColor(); ?> 100%), linear-gradient(180deg, #fff 0%, #ccc 100%);"></div>
+		</div>
+	</div>
+	<?php endif; ?>
 	<?php if ( ! empty( $this->getMessage() ) || ! empty( $this->getLinkUrl() ) ) : ?>
 	<p class="give-totals__message">
 		<?php echo $this->getMessage(); ?>
@@ -14,19 +28,5 @@
 		<a href="<?php echo $this->getLinkUrl(); ?>" target="<?php echo $this->getLinkTarget(); ?>"><?php echo $this->getLinkText(); ?></a>
 		<?php endif; ?>
 	</p>
-	<?php endif; ?>
-	<?php if ( ! empty( $this->getGoal() ) ) : ?>
-	<div class="give-totals__goal">
-		<div class="give-totals__progress">
-			<?php $percent = ( $this->getTotal() / $this->getGoal() ) * 100; ?>
-			<div class="give-totals__progress-bar" style="width: <?php echo min( [ $percent, 100 ] ); ?>%; background: linear-gradient(180deg, <?php echo $this->getColor(); ?> 0%, <?php echo $this->getColor(); ?> 100%), linear-gradient(180deg, #fff 0%, #ccc 100%);"></div>
-		</div>
-		<p class="give-totals__progress-text"> 
-			<?php
-				$total = $this->metric === 'revenue' ? $this->getFormattedTotal() : $this->getTotal();
-				echo sprintf( __( '%1$s of %2$s', 'give' ), $total, $this->getFormattedGoal() );
-			?>
-		</p>
-	</div>
 	<?php endif; ?>
 </div>
