7f738fee116884f8c0ea05b5e8407c2467ceb655
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-09-24 12:49:53 +0530

    chore: add initial code for Revenue respository

---
+++ src/Revenue/Repositories/Revenue.php
@@ -0,0 +1,44 @@
+<?php
+namespace Give\Revenue\Repositories;
+
+use wpdb;
+
+/**
+ * Class Revenue
+ * @package Give\Revenue\Repositories
+ *
+ * Use this class to get data from "give_revenue" table.
+ *
+ * @since 2.9.0
+ */
+class Revenue {
+	/**
+	 * @var wpdb
+	 */
+	private $db;
+
+	/**
+	 * @var string
+	 */
+	private $tableName;
+
+	/**
+	 * Revenue constructor
+	 */
+	public function constructor() {
+		global $wpdb;
+
+		$this->db        = $wpdb;
+		$this->tableName = "{$this->db->prefix}give_revenue";
+	}
+
+	/**
+	 *
+	 */
+	public function create() {
+		return $this->db->insert(
+			$this->tableName,
+			[]
+		);
+	}
+}
