b206c96c05c370e6354e12fd69666de5db6abe45
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-09-24 23:52:09 +0530

    chore: add data migration class

---
+++ src/Revenue/Migrations/AddPastDonationToRevenueTable.php
@@ -0,0 +1,35 @@
+<?php
+namespace Give\Revenue\Migrations;
+
+use Give\Framework\Migrations\Contracts\Migration;
+
+/**
+ * Class AddPastDonationToRevenueTable
+ *
+ * use this table to migrated past donations data to revenue table.
+ *
+ * @package Give\Revenue\Migrations
+ *
+ * @since 2.9.0
+ */
+class AddPastDonationToRevenueTable extends Migration {
+
+	/**
+	 * @inheritdoc
+	 */
+	public function run() {}
+
+	/**
+	 * @inheritdoc
+	 */
+	public static function id() {
+		return 'add-past-donation-data-to-revenue-table';
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public static function timestamp() {
+		return strtotime( '2019-09-24' );
+	}
+}
