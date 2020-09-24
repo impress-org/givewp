1bd0bd8c21c5ba4599fe6b5e93a7a465e7c43e92
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-09-24 11:46:02 +0530

    chore: add initial code for revenue migration

--- src/Revenue/Migrations/AddDataInRevenueTableForPastDonations.php
+++ src/Revenue/Migrations/AddDataInRevenueTableForPastDonations.php
@@ -2,6 +2,8 @@
 namespace Give\Revenue\Migrations;
 
 use Give\Framework\Migrations\Contracts\Migration;
+use Give_Updates;
+use WP_Query;
 
 /**
  * Class AddDataInRevenueTableForPastDonations
@@ -13,7 +15,33 @@ class AddDataInRevenueTableForPastDonations extends Migration {
 	/**
 	 * @inheritdoc
 	 */
-	public function run() {}
+	public function run() {
+		$give_updates = Give_Updates::get_instance();
+
+		$donations = new WP_Query(
+			[
+				'paged'          => $give_updates->step,
+				'status'         => 'any',
+				'order'          => 'ASC',
+				'post_type'      => [ 'give_payment' ],
+				'posts_per_page' => 100,
+			]
+		);
+
+		if ( $donations->have_posts() ) {
+			$give_updates->set_percentage( $donations->found_posts, $give_updates->step * 100 );
+
+			while ( $donations->have_posts() ) {
+				$donations->the_post();
+				$donationId = get_the_ID();
+			}
+
+			wp_reset_postdata();
+		} else {
+			// Update Ran Successfully.
+			give_set_upgrade_complete( 'v270_store_stripe_account_for_donation' );
+		}
+	}
 
 	/**
 	 * @inheritdoc
