5836f51cee98d22cc758137346afbdc050d8e874
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-09-26 00:04:28 +0530

    fix: load paypal sdk from client side

--- assets/src/js/frontend/paypal-commerce/index.js
+++ assets/src/js/frontend/paypal-commerce/index.js
@@ -1,37 +1,41 @@
-/* globals jQuery */
+/* globals jQuery, givePayPalCommerce */
 import DonationForm from './DonationForm';
 import SmartButtons from './SmartButtons';
 import AdvancedCardFields from './AdvancedCardFields';
 import CustomCardFields from './CustomCardFields';
+import { loadScript } from '@paypal/paypal-js';
 
 document.addEventListener( 'DOMContentLoaded', () => {
-	const $formWraps = document.querySelectorAll( '.give-form-wrap' );
+	loadScript( givePayPalCommerce.payPalSdkQueryParameters )
+		.then( () => {
+			const $formWraps = document.querySelectorAll( '.give-form-wrap' );
 
-	if ( ! $formWraps.length ) {
-		return false;
-	}
-
-	$formWraps.forEach( $formWrap => {
-		const $form = $formWrap.querySelector( '.give-form' );
-		const smartButtons = new SmartButtons( $form );
-		const customCardFields = new CustomCardFields( $form );
-
-		smartButtons.boot();
-
-		// Boot CustomCardFields class before AdvancedCardFields because of internal dependencies.
-		if ( AdvancedCardFields.canShow() ) {
-			const advancedCardFields = new AdvancedCardFields( customCardFields );
-
-			customCardFields.boot();
-			advancedCardFields.boot();
-		} else {
-			if ( DonationForm.isPayPalCommerceSelected( jQuery( $form ) ) ) {
-				customCardFields.removeFields();
+			if ( ! $formWraps.length ) {
+				return false;
 			}
 
-			customCardFields.removeFieldsOnGatewayLoad();
-		}
-	} );
+			$formWraps.forEach( $formWrap => {
+				const $form = $formWrap.querySelector( '.give-form' );
+				const smartButtons = new SmartButtons( $form );
+				const customCardFields = new CustomCardFields( $form );
+
+				smartButtons.boot();
+
+				// Boot CustomCardFields class before AdvancedCardFields because of internal dependencies.
+				if ( AdvancedCardFields.canShow() ) {
+					const advancedCardFields = new AdvancedCardFields( customCardFields );
+
+					customCardFields.boot();
+					advancedCardFields.boot();
+				} else {
+					if ( DonationForm.isPayPalCommerceSelected( jQuery( $form ) ) ) {
+						customCardFields.removeFields();
+					}
+
+					customCardFields.removeFieldsOnGatewayLoad();
+				}
+			} );
+		} );
 
 	// On form submit prevent submission for PayPal commerce.
 	// Form submission will be take care internally by smart buttons or advanced card fields.
