6cc544355a317606ef40d8b1af5c34516cdb4b2e
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-10-02 13:09:48 +0530

    refactor: remove dynamic property, we will add it to script param on client side

--- src/PaymentGateways/PayPalCommerce/ScriptLoader.php
+++ src/PaymentGateways/PayPalCommerce/ScriptLoader.php
@@ -135,7 +135,6 @@ EOT;
 			'components'                  => 'hosted-fields,buttons',
 			'locale'                      => get_locale(),
 			'disable-funding'             => 'credit',
-			'vault'                       => true,
 			'data-partner-attribution-id' => give( 'PAYPAL_COMMERCE_ATTRIBUTION_ID' ),
 			'data-client-token'           => $this->merchantRepository->getClientToken(),
 		];
