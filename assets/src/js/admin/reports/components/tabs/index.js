7bad4adbc9aa274ec4f7adfce16d8658c0db449e
alaca <ante.laca@gmail.com>
2020-10-03 07:43:33 +0200

    feature: extend reports menu with wp.hooks

--- assets/src/js/admin/reports/components/tabs/index.js
+++ assets/src/js/admin/reports/components/tabs/index.js
@@ -1,5 +1,6 @@
 // Dependencies
 const { __ } = wp.i18n;
+const { applyFilters } = wp.hooks;
 import { getWindowData } from '../../utils';
 
 // Components
@@ -8,11 +9,22 @@ import Tab from '../tab';
 const Tabs = () => {
 	const url = getWindowData( 'legacyReportsUrl' );
 
+	const renderItems = () => {
+		const items = applyFilters( 'givewp-reports-page-menu-items', [] );
+
+		if ( Array.isArray( items ) && items.length ) {
+			return items
+				.filter( ( item ) => item.hasOwnProperty( 'href' ) && item.hasOwnProperty( 'text' ) )
+				.map( ( item, i ) => <a key={ i } className="nav-tab" href={ item.href }>{ item.text }</a> );
+		}
+	};
+
 	return (
 		<div className="nav-tab-wrapper give-nav-tab-wrapper">
 			<Tab to="/">
-                Overview
+				{ __( 'Overview', 'give' ) }
 			</Tab>
+			{ renderItems() }
 			<a className="nav-tab" href={ url }>{ __( 'Legacy Reports', 'give' ) }</a>
 		</div>
 	);
