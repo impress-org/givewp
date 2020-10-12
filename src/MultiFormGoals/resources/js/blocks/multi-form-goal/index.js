e34aa756cef87f7d399a5a57e0a2f510edac15a5
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-10-12 10:43:44 -0400

    feature: set initial focus to progress bar in multi-form goal blocks

--- src/MultiFormGoals/resources/js/blocks/multi-form-goal/index.js
+++ src/MultiFormGoals/resources/js/blocks/multi-form-goal/index.js
@@ -4,6 +4,8 @@
 const { __ } = wp.i18n;
 const { registerBlockType } = wp.blocks;
 const { InnerBlocks } = wp.blockEditor;
+const { useEffect } = wp.element;
+const { select, dispatch } = wp.data;
 
 /**
  * Internal dependencies
@@ -47,7 +49,20 @@ export default registerBlockType( 'give/multi-form-goal', {
 			'wide',
 		],
 	},
-	edit: () => {
+	edit: ( { isSelected, clientId } ) => {
+		// When adding a new Multi-Form Goal block, select the inner Progress Bar block by default
+		useEffect( () => {
+			if ( isSelected ) {
+				selectProgressBar();
+			}
+		}, [] );
+
+		const selectProgressBar = () => {
+			const parentBlock = select( 'core/editor' ).getBlocksByClientId( clientId )[ 0 ];
+			const progressBarBlock = parentBlock.innerBlocks[ parentBlock.innerBlocks.length - 1 ];
+			dispatch( 'core/block-editor' ).selectBlock( progressBarBlock.clientId );
+		};
+
 		return (
 			<div className="give-multi-form-goal-block">
 				<InnerBlocks
