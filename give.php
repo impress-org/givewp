3b61d063dd44c917347ca3dc764e87e9f0fc933f
Kevin Hoffman <kevin@kevinwhoffman.com>
2020-10-13 23:38:12 -0400

    chore: prepare release 2.9.0-beta.1

--- give.php
+++ give.php
@@ -5,7 +5,7 @@
  * Description: The most robust, flexible, and intuitive way to accept donations on WordPress.
  * Author: GiveWP
  * Author URI: https://givewp.com/
- * Version: 2.9.0-alpha.2
+ * Version: 2.9.0-beta.1
  * Text Domain: give
  * Domain Path: /languages
  *
