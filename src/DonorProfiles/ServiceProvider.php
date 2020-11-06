3cdb2c3aaed796cc323c1b99a717d81b5d7ee6c7
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-11-06 12:27:04 -0500

    feature: register boilerplate route

--- src/DonorProfiles/ServiceProvider.php
+++ src/DonorProfiles/ServiceProvider.php
@@ -7,6 +7,7 @@ use Give\Helpers\Hooks;
 use Give\DonorProfiles\Shortcode as DonorProfileShortcode;
 use Give\DonorProfiles\Block as DonorProfileBlock;
 use Give\DonorProfiles\Model as DonorProfile;
+use Give\DonorProfiles\Routes\DonationHistoryRoute;
 
 class ServiceProvider implements ServiceProviderInterface {
 
@@ -15,7 +16,10 @@ class ServiceProvider implements ServiceProviderInterface {
 	 */
 	public function register() {
 		give()->singleton( DonorProfile::class );
+
 		give()->singleton( DonorProfileShortcode::class );
+
+		give()->bind( DonationHistoryRoute::class );
 
 		if ( function_exists( 'register_block_type' ) ) {
 			give()->singleton( DonorProfileBlock::class );
@@ -28,6 +32,7 @@ class ServiceProvider implements ServiceProviderInterface {
 	public function boot() {
 		Hooks::addAction( 'init', DonorProfileShortcode::class, 'addShortcode' );
 		Hooks::addAction( 'wp_enqueue_scripts', DonorProfileShortcode::class, 'loadFrontendAssets' );
+		Hooks::addAction( 'rest_api_init', DonationHistoryRoute::class, 'registerRoute' );
 
 		if ( function_exists( 'register_block_type' ) ) {
 			Hooks::addAction( 'init', DonorProfileBlock::class, 'addBlock' );
