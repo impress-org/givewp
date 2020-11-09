fb94b686a6694e92c74400cff4369b6159ad61c8
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-11-09 14:52:24 -0500

    feature: use Montserrat font

--- src/DonorProfiles/Model.php
+++ src/DonorProfiles/Model.php
@@ -70,9 +70,16 @@ class Model {
 		);
 
 		wp_enqueue_style(
+			'give-google-font-montserrat',
+			'https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap',
+			[],
+			null
+		);
+
+		wp_enqueue_style(
 			'give-donor-profiles-app',
 			GIVE_PLUGIN_URL . 'assets/dist/css/donor-profiles-app.css',
-			[],
+			[ 'give-google-font-montserrat' ],
 			GIVE_VERSION
 		);
 	}
--- src/DonorProfiles/resources/js/app/components/rest-donation-table/donation-row/index.js
+++ src/DonorProfiles/resources/js/app/components/rest-donation-table/donation-row/index.js
@@ -3,7 +3,6 @@ const DonationRow = () => {
 		<div className="give-donor-profile-table__row">
 			<div className="give-donor-profile-table__column">
 				<div className="give-donor-profile-table__donation-amount">$2,124.40</div>
-				<div className="give-donor-profile-table__donation-id">Id: 4</div>
 			</div>
 			<div className="give-donor-profile-table__column">
 				Save the Whales
@@ -16,6 +15,9 @@ const DonationRow = () => {
 				<div className="give-donor-profile-table__donation-status">
 					Renewal
 				</div>
+			</div>
+			<div className="give-donor-profile-table__pill">
+				<div className="give-donor-profile-table__donation-id">ID: 4</div>
 				<div className="give-donor-profile-table__donation-receipt">
 					<a>View Receipt</a>
 				</div>
