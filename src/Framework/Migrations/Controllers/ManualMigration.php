bf184b7b60c68b17e502e77bf3f3e8e261a3a221
Jason Adams <jason.the.adams@gmail.com>
2020-11-10 18:11:36 -0800

    feature: finish support for clearing updates

--- src/Framework/Migrations/Controllers/ManualMigration.php
+++ src/Framework/Migrations/Controllers/ManualMigration.php
@@ -2,6 +2,10 @@
 
 namespace Give\Framework\Migrations\Controllers;
 
+use Exception;
+use Give\Framework\Migrations\Actions\ClearCompletedUpgrade;
+use Give\Framework\Migrations\Actions\ManuallyRunMigration;
+use Give\Framework\Migrations\Contracts\Migration;
 use Give\Framework\Migrations\MigrationsRegister;
 use Give_Admin_Settings;
 
@@ -39,7 +43,9 @@ class ManualMigration {
 			$migrationToClear = $_GET['give-clear-migration'];
 		}
 
-		if ( ( isset( $migrationToRun ) || isset( $migrationToClear ) ) && current_user_can( 'manage_options' ) ) {
+		$hasMigration = isset( $migrationToRun ) || isset( $migrationToClear );
+
+		if ( $hasMigration && ! current_user_can( 'manage_options' ) ) {
 			Give_Admin_Settings::add_error(
 				'invalid-migration-permissions',
 				__( 'You do not have the permissions to manually run or clear migrations', 'give' )
@@ -49,13 +55,64 @@ class ManualMigration {
 		}
 
 		if ( isset( $migrationToRun ) ) {
-			if ( ! $this->migrationsRegister->hasMigration( $migrationToRun ) ) {
-				Give_Admin_Settings::add_error(
-					'invalid-migration-id',
-					__( "There is no migration with the ID: $migrationToRun", 'give' )
-				);
-				return;
-			}
+			$this->runMigration( $migrationToRun );
+		}
+
+		if ( isset( $migrationToClear ) ) {
+			$this->clearMigration( $migrationToClear );
+		}
+
+		if ( $hasMigration ) {
+			$uriDetails = parse_url( $_SERVER['REQUEST_URI'] );
+			parse_str( $uriDetails['query'], $queryData );
+
+			unset( $queryData['give-run-migration'], $queryData['give-clear-migration'] );
+
+			wp_safe_redirect( $uriDetails['path'] . '?' . http_build_query( $queryData ) );
+		}
+	}
+
+	/**
+	 * Runs the given automatic migration
+	 *
+	 * @since 2.9.2
+	 *
+	 * @param string $migrationId
+	 */
+	private function runMigration( $migrationId ) {
+		if ( ! $this->migrationsRegister->hasMigration( $migrationId ) ) {
+			Give_Admin_Settings::add_error(
+				'invalid-migration-id',
+				__( "There is no migration with the ID: $migrationId", 'give' )
+			);
+
+			return;
+		}
+
+		/** @var Migration $migration */
+		$migration = give( $this->migrationsRegister->getMigration( $migrationId ) );
+
+		/** @var ManuallyRunMigration $manualRunner */
+		$manualRunner = give( ManuallyRunMigration::class );
+
+		$manualRunner( $migration );
+	}
+
+	/**
+	 * Clears the manual migration so it may be run again
+	 *
+	 * @since 2.9.2
+	 *
+	 * @param string $migrationToClear
+	 */
+	private function clearMigration( $migrationToClear ) {
+		/** @var ClearCompletedUpgrade $clearUpgrade */
+		$clearUpgrade = give( ClearCompletedUpgrade::class );
+
+		try {
+			$clearUpgrade( $migrationToClear );
+		} catch ( Exception $exception ) {
+			Give_Admin_Settings::add_error( 'clear-migration-failed', "Unable to reset migration. Error: {$exception->getMessage()}" );
 		}
 	}
 }
