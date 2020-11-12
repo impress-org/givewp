4361016d69e5ccabcb3f088c9bab13d007f806ee
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-11-12 09:31:13 -0500

    fix: reduce font load

--- src/DonorProfiles/Model.php
+++ src/DonorProfiles/Model.php
@@ -71,7 +71,7 @@ class Model {
 
 		wp_enqueue_style(
 			'give-google-font-montserrat',
-			'https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap',
+			'https://fonts.googleapis.com/css?family=Montserrat:500,500i,600,600i,700,700i&display=swap',
 			[],
 			null
 		);
