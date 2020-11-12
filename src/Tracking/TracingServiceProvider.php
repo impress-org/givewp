874dfb2748689d565d85d907bfd83f620c05ec90
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-11-12 23:48:21 +0530

    chore: add tracking service provider

---
+++ src/Tracking/TracingServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+namespace Give\Tracking;
+
+use Give\ServiceProviders\ServiceProvider;
+
+/**
+ * Class TracingServiceProvider
+ * @package Give\Tracking
+ *
+ * @since 2.10.0
+ */
+class TracingServiceProvider implements ServiceProvider {
+	/**
+	 * @inheritdoc
+	 */
+	public function register() {}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function boot() {}
+}
