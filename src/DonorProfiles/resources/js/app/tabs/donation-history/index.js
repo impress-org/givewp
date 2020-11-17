831447e862f137f92eb69c5ba9ef58803c2f2a1a
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-11-17 12:46:56 -0500

    fix: extract external data logic into utility

--- src/DonorProfiles/resources/js/app/tabs/donation-history/index.js
+++ src/DonorProfiles/resources/js/app/tabs/donation-history/index.js
@@ -1,30 +1,12 @@
-import axios from 'axios';
 const { __ } = wp.i18n;
 
 // Internal dependencies
 import Content from './content';
 import DashboardContent from './dashboard-content';
-import { store } from './store';
-import { getAPIRoot, getAPINonce } from '../../utils';
-import { setDonations, setQuerying, setCount, setRevenue, setAverage } from './store/actions';
+import { fetchDonationsDataFromAPI } from './utils';
 
 export const registerDonationHistoryTab = () => {
-	const { dispatch } = store;
-
-	dispatch( setQuerying( true ) );
-	axios.get( getAPIRoot() + 'give-api/v2/donor-profile/donations', {
-		headers: {
-			'X-WP-Nonce': getAPINonce(),
-		},
-	} )
-		.then( ( response ) => response.data )
-		.then( ( data ) => {
-			dispatch( setDonations( data.donations ) );
-			dispatch( setCount( data.count ) );
-			dispatch( setRevenue( data.revenue ) );
-			dispatch( setAverage( data.average ) );
-			dispatch( setQuerying( false ) );
-		} );
+	fetchDonationsDataFromAPI();
 
 	window.giveDonorProfile.utils.registerTab( {
 		label: __( 'Donation History', 'give' ),
