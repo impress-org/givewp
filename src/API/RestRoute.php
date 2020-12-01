5ce355da9c4e02b67fc3398413e6695d5ec81522
Kyle Johnson <kbjohnson90+github@gmail.com>
2020-12-01 10:37:47 -0500

    chore: Add backwards compatible RestRoute interface.

---
+++ src/API/RestRoute.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Give\API;
+
+/**
+ * A backwards compatibility layer for add-ons.
+ * Supports the relocated RestRoute interface.
+ */
+interface RestRoute extends \Give\Framework\RestRoute {}
