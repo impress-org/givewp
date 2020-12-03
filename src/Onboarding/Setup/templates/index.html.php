4dfe01c2378dc462c0df3f91c22a55acb184441e
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2020-12-03 17:02:17 +0530

    chore: add usage tracking notice to setup page

--- src/Onboarding/Setup/templates/index.html.php
+++ src/Onboarding/Setup/templates/index.html.php
@@ -121,6 +121,38 @@
 		);
 		?>
 
+	<!-- Anonymous usage tracking opt-in notice -->
+	<?php
+	echo $this->render_template(
+		'section',
+		[
+			'contents' => $this->render_template(
+				'row-item',
+				[
+					'icon'        => $this->image( 'hands-in.svg' ),
+					'class'       => 'anonymous-usage-tracking',
+					'icon_alt'    => esc_html__( 'Anonymous usage tracking icon', 'give' ),
+					'title'       => esc_html__( 'Help us improve yor fundraising experience', 'give' ),
+					'description' => sprintf(
+						'%1$s<br><br><a href="https://givewp.com" class="learn-more-link">%2$s</a>',
+						esc_html__( 'You can contribute to improve GiveWP. the Give Team uses non-sensitive data to improve donation from conversion rates, increase average donation amounts, and streamline the fundraising experience. We never share this information with anyone and we never spam', 'give' ),
+						esc_html__( 'Learn more about how GiveWP respects your privacy while improving the plugin >', 'give' )
+					),
+					'action'      => sprintf(
+						'<a class="button" href="%1$s">%2$s</a><div class="sub-links"><a href="%3$s">%4$s</a><a href="%5$s">%6$s</a></div>',
+						add_query_arg( [ 'give_action' => 'opt_in_into_tracking' ] ),
+						esc_html__( 'Opt-in', 'give' ),
+						add_query_arg( [ 'give_action' => 'hide_opt_in_notice_shortly' ] ),
+						esc_html__( 'Not Right Now', 'give' ),
+						add_query_arg( [ 'give_action' => 'hide_opt_in_notice_permanently' ] ),
+						esc_html__( 'Dismiss Forever', 'give' ),
+					),
+				]
+			),
+		]
+	);
+	?>
+
 	<!-- Resources -->
 	<?php
 		echo $this->render_template(
