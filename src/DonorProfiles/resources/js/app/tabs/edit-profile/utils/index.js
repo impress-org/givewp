845b6361b4037568948fd15d85c51c5928cf6d42
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-12-04 15:11:28 -0500

    chore: add inline documentation

--- src/DonorProfiles/resources/js/app/tabs/edit-profile/utils/index.js
+++ src/DonorProfiles/resources/js/app/tabs/edit-profile/utils/index.js
@@ -14,12 +14,19 @@ export const updateProfileWithAPI = async( {
 	avatarFile,
 	id,
 } ) => {
+	/**
+	 * If a new avatar file is defined, upload it and use the returned
+	 * media ID to be stored as donor meta
+	 */
 	const { profile } = store.getState();
 	let { avatarId } = profile;
 	if ( avatarFile ) {
 		avatarId = await uploadAvatarWithAPI( avatarFile );
 	}
 
+	/**
+	 * Pass new profile data to the Profile REST endpoint
+	 */
 	const { dispatch } = store;
 	axios.post( getAPIRoot() + 'give-api/v2/donor-profile/profile', {
 		data: JSON.stringify( {
@@ -40,14 +47,20 @@ export const updateProfileWithAPI = async( {
 	} )
 		.then( ( response ) => response.data )
 		.then( ( responseData ) => {
+			/**
+			 * Once updated, update the store's representation of
+			 * the donor's profile data
+			 */
 			dispatch( setProfile( responseData.profile ) );
 		} );
 };
 
 export const uploadAvatarWithAPI = ( file ) => {
+	// Prepare a FormData object with the file to be past to the 'media' REST endpoint
 	const formData = new window.FormData();
 	formData.append( 'file', file );
 
+	// Upload the new file, and return the resolved Promise with new media ID
 	return axios.post( getAPIRoot() + 'wp/v2/media', formData, {
 		headers: {
 			'X-WP-Nonce': getAPINonce(),
