b62dfb081b081bc938ef677e8eb13f5756f0a834
Henry Holtgeerts <henryholtgeerts@gmail.com>
2021-01-26 13:22:15 -0500

    fix: prevent donations query when not logged in

--- src/DonorProfiles/resources/js/app/tabs/donation-history/utils/index.js
+++ src/DonorProfiles/resources/js/app/tabs/donation-history/utils/index.js
@@ -1,23 +1,26 @@
 import axios from 'axios';
 import { store } from '../store';
-import { getAPIRoot, getAPINonce } from '../../../utils';
+import { getAPIRoot, getAPINonce, getWindowData } from '../../../utils';
 import { setDonations, setQuerying, setCount, setRevenue, setAverage } from '../store/actions';
 
 export const fetchDonationsDataFromAPI = () => {
 	const { dispatch } = store;
+	const loggedIn = Number( getWindowData( 'id' ) ) !== 0 ? true : false;
 
-	dispatch( setQuerying( true ) );
-	axios.get( getAPIRoot() + 'give-api/v2/donor-profile/donations', {
-		headers: {
-			'X-WP-Nonce': getAPINonce(),
-		},
-	} )
-		.then( ( response ) => response.data )
-		.then( ( data ) => {
-			dispatch( setDonations( data.donations ) );
-			dispatch( setCount( data.count ) );
-			dispatch( setRevenue( data.revenue ) );
-			dispatch( setAverage( data.average ) );
-			dispatch( setQuerying( false ) );
-		} );
+	if ( loggedIn ) {
+		dispatch( setQuerying( true ) );
+		axios.get( getAPIRoot() + 'give-api/v2/donor-profile/donations', {
+			headers: {
+				'X-WP-Nonce': getAPINonce(),
+			},
+		} )
+			.then( ( response ) => response.data )
+			.then( ( data ) => {
+				dispatch( setDonations( data.donations ) );
+				dispatch( setCount( data.count ) );
+				dispatch( setRevenue( data.revenue ) );
+				dispatch( setAverage( data.average ) );
+				dispatch( setQuerying( false ) );
+			} );
+	}
 };
