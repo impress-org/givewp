37a2cf044f27b894b92096418a9b0f05eddf20c0
Kyle Johnson <kbjohnson90+github@gmail.com>
2021-01-28 12:49:28 -0500

    feature: Add checkbox field type.

--- src/Framework/FieldsAPI/FormField/FieldTypes.php
+++ src/Framework/FieldsAPI/FormField/FieldTypes.php
@@ -5,7 +5,9 @@ namespace Give\Framework\FieldsAPI\FormField;
 class FieldTypes {
     const TYPE_TEXT = 'text';
     const TYPE_TEXTAREA = 'textarea';
+    const TYPE_CHECKBOX = 'checkbox';
 
     const TYPE_RADIO = 'radio';
     const TYPE_SELECT = 'select';
+
 }
