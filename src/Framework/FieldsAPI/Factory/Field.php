6c9cca66fe4825e85567e0e703831e2d1d9a06ae
Kyle Johnson <kbjohnson90+github@gmail.com>
2021-01-28 12:56:52 -0500

    refactor: Remove concrete factory methods.

--- src/Framework/FieldsAPI/Factory/Field.php
+++ src/Framework/FieldsAPI/Factory/Field.php
@@ -21,16 +21,4 @@ class Field {
     protected static function make( $type, $name ) {
         return new FormField( $type, $name );
     }
-
-    public static function text( $name ) {
-        return self::make( FieldTypes::TYPE_TEXT, $name );
-    }
-
-    public static function select( $name ) {
-        return self::make( FieldTypes::TYPE_SELECT, $name );
-    }
-    
-    public static function textarea( $name ) {
-        return self::make( FieldTypes::TYPE_TEXTAREA, $name );
-    }
 }
