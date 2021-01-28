f1feb3628ca053ee14371fdf80b38fbf7d2d9d73
Kyle Johnson <kbjohnson90+github@gmail.com>
2021-01-28 12:54:39 -0500

    chore: Add test for field factory checkbox field.

--- tests/unit/tests/Framework/FieldsAPI/Factory/FieldTest.php
+++ tests/unit/tests/Framework/FieldsAPI/Factory/FieldTest.php
@@ -20,6 +20,11 @@ final class FieldTest extends TestCase {
         $this->assertEquals( 'textarea', $field->getType() );
     }
 
+    public function testMakeCheckbox() {
+        $field = Field::checkbox( 'my-checkbox-field' );
+        $this->assertEquals( 'checkbox', $field->getType() );
+    }
+
     public function testTypeNotSupported() {
         $this->expectException(TypeNotSupported::class);
         Field::custom( 'my-custom-field-type' );
