d2db0e73f16721fd66c4bb5ec91b44f46f85b341
alaca <ante.laca@gmail.com>
2021-01-29 14:03:55 +0100

    feature: update DeleteOldLogTables migration

--- src/Log/Migrations/DeleteOldLogTables.php
+++ src/Log/Migrations/DeleteOldLogTables.php
@@ -2,11 +2,31 @@
 
 namespace Give\Log\Migrations;
 
+use Give\Framework\Database\DB;
 use Give\Framework\Migrations\Contracts\Migration;
 
+/**
+ * Class DeleteOldLogTables
+ * @package Give\Log\Migrations
+ */
 class DeleteOldLogTables extends Migration {
+	/**
+	 * @return string
+	 */
+	public static function id() {
+		return 'delete_old_log_tables';
+	}
+
+	/**
+	 * @return int
+	 */
+	public static function timestamp() {
+		return strtotime( '2021-01-28 14:00' );
+	}
 
 	public function run() {
-		// migration logic
+		global $wpdb;
+
+		DB::query( "DROP TABLE {$wpdb->give_logs}, {$wpdb->give_logmeta};" );
 	}
 }
