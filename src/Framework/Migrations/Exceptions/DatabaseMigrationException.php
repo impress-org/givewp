71087fb201be0db5e25812aa973b4c42063478f7
alaca <ante.laca@gmail.com>
2021-01-29 13:56:00 +0100

    feature: add fromException method

--- src/Framework/Migrations/Exceptions/DatabaseMigrationException.php
+++ src/Framework/Migrations/Exceptions/DatabaseMigrationException.php
@@ -2,10 +2,16 @@
 
 namespace Give\Framework\Migrations\Exceptions;
 
+use Exception;
+use Give\Framework\Database\Exceptions\DatabaseQueryException;
+
 /**
  * Class DatabaseMigrationException
  *
  * Represents an exception that occurred when executing a migration within the database
  */
-class DatabaseMigrationException extends \Exception {
+class DatabaseMigrationException extends Exception {
+	public static function fromException( DatabaseQueryException $exception, $message ) {
+		return new Exception( $message . PHP_EOL . print_r( $exception, true ) );
+	}
 }
