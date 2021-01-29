44d4a855eff0b94e4668267828434f56f43d536f
alaca <ante.laca@gmail.com>
2021-01-29 14:07:02 +0100

    refactor: update MigrateExistingLogs class

--- src/Log/Migrations/MigrateExistingLogs.php
+++ src/Log/Migrations/MigrateExistingLogs.php
@@ -5,8 +5,25 @@ namespace Give\Log\Migrations;
 use Give\Framework\Migrations\Contracts\Migration;
 
 class MigrateExistingLogs extends Migration {
+	/**
+	 * @return string
+	 */
+	public static function id() {
+		return 'migrate_existing_logs';
+	}
+
+	/**
+	 * @return int
+	 */
+	public static function timestamp() {
+		return strtotime( '2021-01-28 13:00' );
+	}
 
 	public function run() {
 		// migration logic
+	}
+
+	public function check() {
+
 	}
 }
