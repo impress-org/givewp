0265df8096c241182fc4c08c650635c9872b67b8
alaca <ante.laca@gmail.com>
2021-01-29 14:06:22 +0100

    refactor: remove test code

--- src/Log/LogServiceProvider.php
+++ src/Log/LogServiceProvider.php
@@ -2,10 +2,9 @@
 
 namespace Give\Log;
 
-use Give\Log\ValueObjects\LogCategory;
 use Give\ServiceProviders\ServiceProvider;
 use Give\Framework\Migrations\MigrationsRegister;
-use Give\Log\Migrations\CreateNewLogTables;
+use Give\Log\Migrations\CreateNewLogTable;
 use Give\Log\Migrations\MigrateExistingLogs;
 use Give\Log\Migrations\DeleteOldLogTables;
 
@@ -33,36 +32,10 @@ class LogServiceProvider implements ServiceProvider {
 	public function boot() {
 		give( MigrationsRegister::class )->addMigrations(
 			[
-				CreateNewLogTables::class,
-			//              MigrateExistingLogs::class,
-			//              DeleteOldLogTables::class,
+				CreateNewLogTable::class,
+				MigrateExistingLogs::class,
+				DeleteOldLogTables::class,
 			]
 		);
-
-		// Test it
-		if ( CreateNewLogTables::check() ) {
-			$this->testLogs();
-		}
-
-	}
-
-	/**
-	 * IGNORE - just for testing
-	 */
-	public function testLogs() {
-
-		Log::success( 'Its working' );
-
-		Log::error(
-			'Hey man, this is an error!',
-			[
-				'category' => LogCategory::PAYMENT,
-				'source'   => 'Stripe add-on',
-			]
-		);
-
-		Log::migration( CreateNewLogTables::class )->success( 'Worked like a charm', self::class );
-
-		Log::migration( CreateNewLogTables::class )->notice( 'Hmmm', self::class, [ 'info' => "Something isn't quite right" ] );
 	}
 }
