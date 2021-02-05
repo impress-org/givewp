7e503f760785be73cc20a7f4c5623a5a319e16e6
alaca <ante.laca@gmail.com>
2021-02-05 18:00:08 +0100

    feature:  export label and pagination component

--- src/Views/Components/index.js
+++ src/Views/Components/index.js
@@ -3,3 +3,5 @@ export { default as Card } from './Card';
 export { default as Table } from './Table';
 export { default as LoadingOverlay } from './LoadingOverlay';
 export { default as Spinner } from './Spinner';
+export { default as Label } from './Label';
+export { default as Pagination } from './Pagination';
