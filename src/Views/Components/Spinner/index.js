4735c446929bdb5afb309b1cb071fe3df88f36f7
alaca <ante.laca@gmail.com>
2021-02-05 17:58:58 +0100

    feature: add spinner sizes

--- src/Views/Components/Spinner/index.js
+++ src/Views/Components/Spinner/index.js
@@ -1,9 +1,28 @@
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+
 import styles from './style.module.scss';
 
-const Spinner = () => {
+const Spinner = ( { size } ) => {
+	const spinnerClasses = classNames( {
+		[	styles.spinner ]: true,
+		[ styles.large ]: size === 'large',
+		[ styles.medium ]: size === 'medium',
+		[ styles.small ]: size === 'small',
+	} );
+
 	return (
-		<div className={ styles.spinner }> </div>
+		<div className={ spinnerClasses }> </div>
 	);
+};
+
+Spinner.propTypes = {
+	// Spinner size [small, medium, large ]
+	size: PropTypes.string,
+};
+
+Spinner.defaultProps = {
+	size: 'small',
 };
 
 export default Spinner;
