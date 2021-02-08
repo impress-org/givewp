a9b21050df1267c644891a469e043b3bb4873520
alaca <ante.laca@gmail.com>
2021-02-08 18:55:52 +0100

    feature: add new components

--- src/Views/Components/index.js
+++ src/Views/Components/index.js
@@ -5,3 +5,7 @@ export { default as LoadingOverlay } from './LoadingOverlay';
 export { default as Spinner } from './Spinner';
 export { default as Label } from './Label';
 export { default as Pagination } from './Pagination';
+export { default as Select } from './Select';
+export { default as LoadingNotice } from './LoadingNotice';
+export { default as Button } from './Button';
+export { default as Input } from './Input';
