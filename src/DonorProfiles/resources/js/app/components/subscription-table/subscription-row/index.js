2d46194fb1b3dc5553291efeb66eef299b2b5ca2
Henry Holtgeerts <henryholtgeerts@gmail.com>
2021-02-09 10:14:24 -0500

    feature: conditionally show management options

--- src/DonorProfiles/resources/js/app/components/subscription-table/subscription-row/index.js
+++ src/DonorProfiles/resources/js/app/components/subscription-table/subscription-row/index.js
@@ -4,41 +4,45 @@ const { __ } = wp.i18n;
 
 const SubscriptionRow = ( { subscription } ) => {
 	const id = subscription[ 0 ];
-	const { amount, frequency, progress, renewalDate, status, form } = subscription[ 1 ];
+	const { payment, form, gateway } = subscription[ 1 ];
 
 	return (
 		<div className="give-donor-profile-table__row">
 			<div className="give-donor-profile-table__column">
-				<div className="give-donor-profile-table__donation-amount">{ amount } / { frequency }</div>
+				<div className="give-donor-profile-table__donation-amount">{ payment.amount } / { payment.frequency }</div>
 				{ form.title }
 			</div>
 			<div className="give-donor-profile-table__column">
 				<div className="give-donor-profile-table__donation-status">
-					{ status }
+					{ payment.status }
 				</div>
 			</div>
 			<div className="give-donor-profile-table__column">
-				{ renewalDate }
+				{ payment.renewalDate }
 			</div>
 			<div className="give-donor-profile-table__column">
-				{ progress }
+				{ payment.progress }
 			</div>
 			<div className="give-donor-profile-table__pill">
-				<div className="give-donor-profile-table__donation-receipt">
-					<Link to={ `/recurring-donations/update-method/${ id }` }>
-						{ __( 'Update Payment Method', 'give' ) } <FontAwesomeIcon icon="arrow-right" />
-					</Link>
-				</div>
+				{ gateway.can_update && (
+					<div className="give-donor-profile-table__donation-receipt">
+						<Link to={ `/recurring-donations/update-method/${ id }` }>
+							{ __( 'Update Payment Method', 'give' ) } <FontAwesomeIcon icon="arrow-right" />
+						</Link>
+					</div>
+				) }
 				<div className="give-donor-profile-table__donation-receipt">
 					<Link to={ `/recurring-donations/receipt/${ id }` }>
 						{ __( 'View Receipt', 'give' ) } <FontAwesomeIcon icon="arrow-right" />
 					</Link>
 				</div>
-				<div className="give-donor-profile-table__donation-receipt">
-					<Link to={ `/recurring-donations/cancel/${ id }` }>
-						{ __( 'Cancel Subscription', 'give' ) }
-					</Link>
-				</div>
+				{ gateway.can_cancel && (
+					<div className="give-donor-profile-table__donation-receipt">
+						<Link to={ `/recurring-donations/cancel/${ id }` }>
+							{ __( 'Cancel Subscription', 'give' ) }
+						</Link>
+					</div>
+				) }
 			</div>
 		</div>
 	);
