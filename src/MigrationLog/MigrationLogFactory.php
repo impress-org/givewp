0e13cf1a7adcd2c4d49bf3f4decb68e871619bd0
alaca <ante.laca@gmail.com>
2021-02-10 19:38:36 +0100

    refactor: add error attribute

--- src/MigrationLog/MigrationLogFactory.php
+++ src/MigrationLog/MigrationLogFactory.php
@@ -12,9 +12,18 @@ use Give\Framework\Migrations\Contracts\Migration;
  * @since 2.10.0
  */
 class MigrationLogFactory {
-
-	public function make( $id, $status = '', $lastRun = null ) {
-		return new MigrationLogModel( $id, $status, $lastRun );
+	/**
+	 * Make MigrationModel instance
+	 *
+	 * @param string $id
+	 * @param string  $status
+	 * @param mixed|null  $error
+	 * @param string|null  $lastRun
+	 *
+	 * @return MigrationLogModel
+	 */
+	public function make( $id, $status = '', $error = null, $lastRun = null ) {
+		return new MigrationLogModel( $id, $status, $error, $lastRun );
 	}
 
 	/**
