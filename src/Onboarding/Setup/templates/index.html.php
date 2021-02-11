c67d16dea9b51af4c74557adbf626b805e7d4b71
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2021-02-11 22:10:24 +0530

    doc: add action hook doc

--- src/Onboarding/Setup/templates/index.html.php
+++ src/Onboarding/Setup/templates/index.html.php
@@ -12,7 +12,14 @@
 	</div>
 	<?php endif; ?>
 
-	<?php do_action( 'give_setup_page_before_sections' ); ?>
+	<?php
+	/**
+	 * Use this action to perform task before section loads.
+	 *
+	 * @since 2.10.0
+	 */
+	do_action( 'give_setup_page_before_sections' );
+	?>
 
 	<!-- Configuration -->
 	<?php
