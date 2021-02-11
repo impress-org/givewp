2966dfa6bb4f5ee5ddfb89115c920f122c29d73a
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2021-02-11 08:59:37 +0530

    feature: add traits

---
+++ src/Tracking/Traits/HasDonations.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Give\Tracking\Traits;
+
+use Give\Helpers\ArrayDataSet;
+use Give\Tracking\TrackRoutine;
+
+/**
+ * Trait HasDonations
+ * @package Give\Tracking\Traits
+ *
+ * @since 2.10.0
+ */
+trait HasDonations {
+
+	/**
+	 * Return donation ids after last tracked request date.
+	 *
+	 * @sicne 2.10.0
+	 * @return array
+	 */
+	private function getNewDonationIdsSinceLastRequest() {
+		global $wpdb;
+
+		$statues = ArrayDataSet::getStringSeparatedByCommaEnclosedWithSingleQuote(
+			[
+				'publish', // One time donation
+				'give_subscription', // Renewal
+			]
+		);
+		$time    = date( 'Y-m-d H:i:s', get_option( TrackRoutine::LAST_REQUEST_OPTION_NAME, time() ) );
+
+		return $wpdb->get_col(
+			"
+				SELECT ID
+				FROM {$wpdb->posts}
+				WHERE post_date_gmt >= '{$time}'
+				AND post_status IN ({$statues})
+				AND post_type='give_payment'
+				"
+		);
+	}
+}
