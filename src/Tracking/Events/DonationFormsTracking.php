15aed16c95fd2d1a950dc819269900f8e75990f7
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2021-02-11 08:59:15 +0530

    feature: add tracking event for donation forms

---
+++ src/Tracking/Events/DonationFormsTracking.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Give\Tracking\Events;
+
+use Give\Tracking\Contracts\TrackEvent;
+use Give\Tracking\Track;
+use Give\Tracking\TrackingData\DonationFormsData;
+
+/**
+ * Class DonationFormsTracking
+ * @package Give\Tracking\Events
+ *
+ * @since 2.10.0
+ */
+class DonationFormsTracking extends TrackEvent
+{/**
+ * @var string
+ */
+	protected $trackId = 'donation-form-updated';
+
+	/**
+	 * GivePluginSettingsTracking constructor.
+	 *
+	 * @param  Track  $track
+	 * @param  DonationFormsData  $themeData
+	 *
+	 * @since 2.10.0
+	 */
+	public function __construct( Track $track, DonationFormsData $themeData ) {
+		parent::__construct( $track, $themeData );
+	}
+
+}
