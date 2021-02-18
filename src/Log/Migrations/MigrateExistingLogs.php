b3f9c61878d2bb3fe5e411f2539ceacba90e3648
alaca <ante.laca@gmail.com>
2021-02-18 17:10:40 +0100

    refactor: remove logging as migration runner will handle that

--- src/Log/Migrations/MigrateExistingLogs.php
+++ src/Log/Migrations/MigrateExistingLogs.php
@@ -101,10 +101,6 @@ class MigrateExistingLogs extends Migration {
 						$context
 					)->save();
 				} catch ( \Exception $exception ) {
-					// Log migration error
-					Log::migration( self::class )
-					   ->error( 'Log migration failed', 'MigrateExistingLogs Migration', [ 'exception' => $exception ] );
-
 					$give_updates->__pause_db_update( true );
 					update_option( 'give_upgrade_error', 1, false );
 				}
