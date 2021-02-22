a7eef55341980843bfb172d0703957beb961cebc
Henry Holtgeerts <henryholtgeerts@gmail.com>
2021-02-22 11:49:09 -0500

    fix: redirect annual receipt link in parent window

--- src/DonorProfiles/resources/js/app/components/annual-receipt-table/annual-receipt-row/index.js
+++ src/DonorProfiles/resources/js/app/components/annual-receipt-table/annual-receipt-row/index.js
@@ -4,6 +4,11 @@ const { __ } = wp.i18n;
 const AnnualReceiptRow = ( { annualReceipt } ) => {
 	const { year, amount, count, statementUrl } = annualReceipt[ 1 ];
 
+	const handleHrefClick = ( e ) => {
+		e.preventDefault();
+		window.parent.location = e.target.href;
+	};
+
 	return (
 		<div className="give-donor-profile-table__row">
 			<div className="give-donor-profile-table__column">
@@ -16,7 +21,7 @@ const AnnualReceiptRow = ( { annualReceipt } ) => {
 				{ count }
 			</div>
 			<div className="give-donor-profile-table__column">
-				<a href={ statementUrl }>
+				<a href={ statementUrl } onClick={ ( e ) => handleHrefClick( e ) }>
 					{ __( 'View Receipt', 'give' ) } <FontAwesomeIcon icon="arrow-right" />
 				</a>
 			</div>
