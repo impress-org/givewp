4b58e230dc56635a6fa107cdb8cba3217e665192
Kyle B. Johnson <kyle.johnson@hey.com>
2021-02-23 12:12:41 -0500

    enhancement: Add support for arbritrary attributes.

---
+++ tests/unit/tests/Framework/FieldsAPI/FormField/FieldAttributesTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Give\Framework\FieldsAPI\FormField;
+
+final class FieldAttributesTest extends TestCase {
+
+    public function testHasAttributes() {
+        $field = new FormField( 'text', 'my-text-field' );
+        $field->setAttributes( $attributes = [
+            'foo' => 'bar',
+        ]);
+        $this->assertEquals( $attributes, $field->getAttributes() );
+    }
+}
