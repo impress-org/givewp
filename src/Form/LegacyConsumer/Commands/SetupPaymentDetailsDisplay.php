1f7a2155e6647c5b32294ad1d107e3825fc71c48
Kyle B. Johnson <kyle.johnson@hey.com>
2021-02-25 10:04:08 -0500

    refactor: Use a CommandFactory to maintain context in nested callbacks.

--- src/Form/LegacyConsumer/Commands/SetupPaymentDetailsDisplay.php
+++ src/Form/LegacyConsumer/Commands/SetupPaymentDetailsDisplay.php
@@ -5,33 +5,41 @@ namespace Give\Form\LegacyConsumer\Commands;
 use Give\Framework\FieldsAPI\FieldCollection;
 use Give\Form\LegacyConsumer\FieldView;
 
-class SetupPaymentDetailsDisplay implements HookCommandInterface {
-	public function __invoke( $hook ) {
+class SetupPaymentDetailsDisplay {
+
+	public function __construct( $hook ) {
+		$this->hook = $hook;
+	}
+
+	public function __invoke() {
 		add_action(
 			'give_view_donation_details_billing_after',
-			function( $donationID ) use ( $hook ) {
-
-				$fieldCollection = new FieldCollection( 'root' );
-				do_action( "give_fields_$hook", $fieldCollection, get_the_ID() );
-
-				$fieldCollection->walk(
-					function( $field ) use ( $donationID ) {
-						?>
-					<div class="referral-data postbox" style="padding-bottom: 15px;">
-						<h3 class="hndle">
-								<?php echo $field->getLabel(); ?>
-						</h3>
-						<div class="inside">    
-							<p>   
-									<?php echo give_get_meta( $donationID, $field->getName(), true ); ?>
-							</p>
-						</div>
-					</div>
-						<?php
-					}
-				);
-			}
+			[ $this, 'process' ]
 		);
 	}
-}
 
+	public function process( $donationID ) {
+
+		$this->donationID = $donationID;
+
+		$fieldCollection = new FieldCollection( 'root' );
+		do_action( "give_fields_{$this->hook}", $fieldCollection, get_the_ID() );
+
+		$fieldCollection->walk( [ $this, 'render' ] );
+	}
+
+	public function render( $field ) {
+		?>
+		<div class="referral-data postbox" style="padding-bottom: 15px;">
+			<h3 class="hndle">
+				<?php echo $field->getLabel(); ?>
+			</h3>
+			<div class="inside">    
+				<p>   
+					<?php echo give_get_meta( $this->donationID, $field->getName(), true ); ?>
+				</p>
+			</div>
+		</div>
+		<?php
+	}
+}
