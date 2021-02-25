25bbce24da2dffa7c2a80711d67d50697faca096
Henry Holtgeerts <henryholtgeerts@gmail.com>
2021-02-25 09:37:57 -0500

    fix: require use confirmation to logout

--- src/DonorProfiles/resources/js/app/components/tab-menu/index.js
+++ src/DonorProfiles/resources/js/app/components/tab-menu/index.js
@@ -1,16 +1,18 @@
 // Store dependencies
+import { useState, Fragment } from 'react';
 import { useSelector } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const { __ } = wp.i18n;
 
 // Internal dependencies
 import TabLink from '../tab-link';
-
-import { logoutWithAPI } from './utils';
+import LogoutModal from '../logout-modal';
 
 import './style.scss';
 
 const TabMenu = () => {
+	const [ logoutModalOpen, setLogoutModalOpen ] = useState( false );
+
 	const tabsSelector = useSelector( state => state.tabs );
 	const tabLinks = Object.values( tabsSelector ).map( ( tab, index ) => {
 		return (
@@ -18,20 +20,18 @@ const TabMenu = () => {
 		);
 	} );
 
-	const handleLogout = async() => {
-		const { status } = await logoutWithAPI();
-		if ( status === 200 ) {
-			window.location.reload();
-		}
-	};
-
 	return (
-		<div className="give-donor-profile-tab-menu">
-			{ tabLinks }
-			<div className="give-donor-profile-logout give-donor-profile-tab-link" onClick={ () => handleLogout() }>
-				<FontAwesomeIcon icon="sign-out-alt" /> { __( 'Logout', 'give' ) }
+		<Fragment>
+			{ logoutModalOpen && (
+				<LogoutModal onRequestClose={ () => setLogoutModalOpen( false ) } />
+			) }
+			<div className="give-donor-profile-tab-menu">
+				{ tabLinks }
+				<div className="give-donor-profile-logout give-donor-profile-tab-link" onClick={ () => setLogoutModalOpen( true ) }>
+					<FontAwesomeIcon icon="sign-out-alt" /> { __( 'Logout', 'give' ) }
+				</div>
 			</div>
-		</div>
+		</Fragment>
 	);
 };
 export default TabMenu;
