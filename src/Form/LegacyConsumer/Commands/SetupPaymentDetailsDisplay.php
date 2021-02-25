f17d6048c3df15725d285e1c0a0ea16a788192fa
Kyle B. Johnson <kyle.johnson@hey.com>
2021-02-25 10:35:20 -0500

    chore: Add docblocks.

--- src/Form/LegacyConsumer/Commands/SetupPaymentDetailsDisplay.php
+++ src/Form/LegacyConsumer/Commands/SetupPaymentDetailsDisplay.php
@@ -2,15 +2,30 @@
 
 namespace Give\Form\LegacyConsumer\Commands;
 
+use Give\Framework\FieldsAPI\FormField;
 use Give\Framework\FieldsAPI\FieldCollection;
 use Give\Form\LegacyConsumer\FieldView;
 
+/**
+ * @unreleased
+ */
 class SetupPaymentDetailsDisplay {
 
+	/**
+	 * @unreleased
+	 *
+	 * @param string $hook
+	 */
 	public function __construct( $hook ) {
 		$this->hook = $hook;
 	}
 
+
+	/**
+	 * @unreleased
+	 *
+	 * @return void
+	 */
 	public function __invoke() {
 		add_action(
 			'give_view_donation_details_billing_after',
@@ -18,6 +33,13 @@ class SetupPaymentDetailsDisplay {
 		);
 	}
 
+	/**
+	 * @unreleased
+	 *
+	 * @param int $donationID
+	 *
+	 * @return void
+	 */
 	public function process( $donationID ) {
 
 		$this->donationID = $donationID;
@@ -28,7 +50,14 @@ class SetupPaymentDetailsDisplay {
 		$fieldCollection->walk( [ $this, 'render' ] );
 	}
 
-	public function render( $field ) {
+	/**
+	 * @unreleased
+	 *
+	 * @param FormField $field
+	 *
+	 * @return void
+	 */
+	public function render( FormField $field ) {
 		?>
 		<div class="referral-data postbox" style="padding-bottom: 15px;">
 			<h3 class="hndle">
