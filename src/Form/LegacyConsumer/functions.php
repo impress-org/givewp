2ffa880184031a753a419fcdc067fafe1d96339f
Jason Adams <jason.adams@impress.org>
2021-03-01 15:36:10 -0800

    feature: improve give_field IDE friendliness

--- src/Form/LegacyConsumer/functions.php
+++ src/Form/LegacyConsumer/functions.php
@@ -1,11 +1,14 @@
 <?php
 
+use Give\Framework\FieldsAPI\Factory\Field;
+use Give\Framework\FieldsAPI\FormField;
+
 /**
  * @param string $type
  * @param string $name
- * 
+ *
  * @return FormField
  */
 function give_field( $type, $name ) {
-    return \Give\Framework\FieldsAPI\Factory\Field::$type( $name );
+	return Field::$type( $name );
 }
