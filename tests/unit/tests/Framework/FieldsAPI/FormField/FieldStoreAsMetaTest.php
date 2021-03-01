56b295cf312eac566e086c319ecbfb7d7a4e26c0
Kyle B. Johnson <kyle.johnson@impress.org>
2021-03-01 16:05:29 -0500

    chore: Remove tests for removed functionality.

--- tests/unit/tests/Framework/FieldsAPI/FormField/FieldStoreAsMetaTest.php
+++ tests/unit/tests/Framework/FieldsAPI/FormField/FieldStoreAsMetaTest.php
@@ -21,21 +21,4 @@ final class FieldStoreAsMetaTest extends TestCase {
         $field->storeAsDonorMeta( false ); // Toggle off.
         $this->assertFalse( $field->shouldStoreAsDonorMeta() );
     }
-
-    public function testStoreAsDonationMeta() {
-        $field = new FormField( 'text', 'my-text-field' );
-        $field->storeAsDonationMeta();
-        $this->assertTrue( $field->shouldStoreAsDonationMeta() );
-    }
-
-    public function testNotStoreAsDonationMeta() {
-        $field = new FormField( 'text', 'my-text-field' );
-
-        // False by default.
-        $this->assertFalse( $field->shouldStoreAsDonationMeta() );
-
-        $field->storeAsDonationMeta(); // Toggle on.
-        $field->storeAsDonationMeta( false ); // Toggle off.
-        $this->assertFalse( $field->shouldStoreAsDonationMeta() );
-    }
 }
