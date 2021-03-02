6e72b515a348f3bfcede22e1ba6c2111c852148d
Kyle B. Johnson <kyle.johnson@impress.org>
2021-03-02 17:33:44 -0500

    chore: Revert unnecessary changes.

--- src/DonorProfiles/Routes/LoginRoute.php
+++ src/DonorProfiles/Routes/LoginRoute.php
@@ -11,8 +11,6 @@ use Give\API\RestRoute;
  */
 class LoginRoute implements RestRoute {
 
-	use Captcha\ProtectedRoute;
-
 	/** @var string */
 	protected $endpoint = 'donor-profile/login';
 
@@ -20,7 +18,6 @@ class LoginRoute implements RestRoute {
 	 * @inheritDoc
 	 */
 	public function registerRoute() {
-
 		register_rest_route(
 			'give-api/v2',
 			$this->endpoint,
@@ -31,20 +28,15 @@ class LoginRoute implements RestRoute {
 					'permission_callback' => '__return_true',
 				],
 				'args' => [
-					'login'                => [
+					'login'    => [
 						'type'              => 'string',
 						'required'          => true,
 						'sanitize_callback' => 'sanitize_text_field',
 					],
-					'password'             => [
+					'password' => [
 						'type'              => 'string',
 						'required'          => true,
 						'sanitize_callback' => 'sanitize_text_field',
-					],
-					'g-recaptcha-response' => [
-						'type'              => 'string',
-						'required'          => $this->isCaptchaEnabled(),
-						'validate_callback' => [ $this, 'validateRecaptcha' ],
 					],
 				],
 			]
