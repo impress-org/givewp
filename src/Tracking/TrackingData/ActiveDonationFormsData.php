eb4e00c91db24aa47dd984add34f5ac65048708c
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2021-03-03 22:11:26 +0530

    refacor: use revenue table to get form ids

--- src/Tracking/TrackingData/ActiveDonationFormsData.php
+++ src/Tracking/TrackingData/ActiveDonationFormsData.php
@@ -21,19 +21,13 @@ class ActiveDonationFormsData extends DonationFormsData {
 	protected function setFormIdsByDonationIds() {
 		global $wpdb;
 
-		$statues = DonationStatuses::getCompletedDonationsStatues( true );
-
 		$this->formIds = $wpdb->get_col(
 			"
-			SELECT DISTINCT dm.meta_value
-			FROM {$wpdb->donationmeta} as dm
-				INNER JOIN {$wpdb->posts} as p ON dm.donation_id = p.ID
-				INNER JOIN {$wpdb->donationmeta} as dm2 ON dm.donation_id = dm2.donation_id
-			WHERE p.post_status IN ({$statues})
-			  	AND p.post_type='give_payment'
-				AND dm2.meta_key='_give_payment_mode'
-				AND dm2.meta_value='live'
-				AND dm.meta_key='_give_payment_form_id'
+			SELECT DISTINCT r.form_id
+			FROM {$wpdb->give_revenue} as r
+				INNER JOIN {$wpdb->donationmeta} as dm ON r.donation_id = dm.donation_id
+			WHERE dm.meta_key='_give_payment_mode'
+				AND dm.meta_value='live'
 			"
 		);
 
