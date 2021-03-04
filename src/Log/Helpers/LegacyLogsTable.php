4862b9a3399680fb63cad268694cfd1ff52aba2e
alaca <ante.laca@gmail.com>
2021-03-04 19:31:30 +0100

    feature: add table helper

---
+++ src/Log/Helpers/LegacyLogsTable.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Give\Log\Helpers;
+
+/**
+ * Class LogsLegacyTable
+ * @package Give\Log\Helpers
+ *
+ * @since @unreleased
+ */
+class LegacyLogsTable {
+	/**
+	 * Check if legacy logs table exists
+	 *
+	 * @return bool
+	 */
+	public function exist() {
+		global $wpdb;
+
+		return (bool) $wpdb->get_var(
+			$wpdb->prepare( "SHOW TABLES LIKE '%s'", "{$wpdb->prefix}give_logs" )
+		);
+	}
+}
