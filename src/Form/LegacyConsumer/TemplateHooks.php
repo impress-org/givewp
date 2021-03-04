415afbdceeb9f7c9df2fd410013054611f44b797
Kyle B. Johnson <kyle.johnson@impress.org>
2021-03-04 11:00:07 -0500

    chore: Remove payment_mode_select template hook.
    The render location doesn't make sense for placing fields.

--- src/Form/LegacyConsumer/TemplateHooks.php
+++ src/Form/LegacyConsumer/TemplateHooks.php
@@ -3,42 +3,41 @@
 namespace Give\Form\LegacyConsumer;
 
 class TemplateHooks {
-    /**
-     * A "full-ish" list of available actions.
-     * @note The `give_` prefix has been removed for interoperability.
-     * @note Update to account for previously deprecated hooks.
-     * @link https://givewp.com/documentation/developers/how-to-create-custom-form-fields/
-     * @link https://givewp.com/add-content-donation-forms/
-     */
-    const TEMPLATE_HOOKS =  [
-        'before_donation_levels',
-        'after_donation_amount',
-        'after_donation_levels',
-        'payment_mode_select',
-        'payment_mode_top',
-        'payment_mode_before_gateways',
-        'payment_mode_after_gateways',
-        'payment_mode_after_gateways_wrap',
-        'payment_mode_bottom',
-        'donation_form',
-        'purchase_form_top',
-        'donation_form_register_login_fields',
-        'donation_form_before_cc_form',
-        'cc_form',
-        'before_cc_fields',
-        'before_cc_expiration',
-        'after_cc_expiration',
-        'after_cc_fields',
-        'donation_form_after_cc_form',
-        'purchase_form_bottom',
-    ];
+	/**
+	 * A "full-ish" list of available actions.
+	 * @note The `give_` prefix has been removed for interoperability.
+	 * @note Update to account for previously deprecated hooks.
+	 * @link https://givewp.com/documentation/developers/how-to-create-custom-form-fields/
+	 * @link https://givewp.com/add-content-donation-forms/
+	 */
+	const TEMPLATE_HOOKS = [
+		'before_donation_levels',
+		'after_donation_amount',
+		'after_donation_levels',
+		'payment_mode_top',
+		'payment_mode_before_gateways',
+		'payment_mode_after_gateways',
+		'payment_mode_after_gateways_wrap',
+		'payment_mode_bottom',
+		'donation_form',
+		'purchase_form_top',
+		'donation_form_register_login_fields',
+		'donation_form_before_cc_form',
+		'cc_form',
+		'before_cc_fields',
+		'before_cc_expiration',
+		'after_cc_expiration',
+		'after_cc_fields',
+		'donation_form_after_cc_form',
+		'purchase_form_bottom',
+	];
 
-    public function walk( callable $callback ) {
-        $hooks = $this->getHooks();
-        array_walk( $hooks, $callback );
-    }
+	public function walk( callable $callback ) {
+		$hooks = $this->getHooks();
+		array_walk( $hooks, $callback );
+	}
 
-    public function getHooks() {
-        return self::TEMPLATE_HOOKS;
-    }
+	public function getHooks() {
+		return self::TEMPLATE_HOOKS;
+	}
 }
