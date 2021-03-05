281550e3993e3eaff967c037a5300c5fba3f6fc5
Kyle B. Johnson <kyle.johnson@impress.org>
2021-03-05 09:37:02 -0500

    chore: Add test for receipt info.

---
+++ tests/unit/tests/DonorProfiles/Repositories/DonationsTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use Give\DonorProfiles\Repositories\Donations as DonationsRepository;
+
+final class DonationsTest extends Give_Unit_Test_Case {
+    
+    public function testReceiptInfoConatinsDonationData() {
+        $class = new ReflectionClass( DonationsRepository::class );
+        $getReceiptInfo = $class->getMethod( 'getReceiptInfo' );
+        $getReceiptInfo->setAccessible(true);
+
+        $payment = get_post(
+            Give_Helper_Payment::create_simple_payment()
+        );
+
+        $receiptInfo = $getReceiptInfo->invokeArgs( new DonationsRepository, [ $payment ]);
+        $receiptInfo = json_encode( $receiptInfo );
+
+        // Expected values provided by sample data.
+        $this->assertContains( '$20.00', $receiptInfo );
+        $this->assertContains( 'admin@example.org', $receiptInfo );
+    }
+}
