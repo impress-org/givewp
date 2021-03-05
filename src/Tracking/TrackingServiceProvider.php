6603234a7166a08962c6bc9ed8b6c7cf31a25179
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2021-03-05 10:25:25 +0530

    refactor: check tracking environment before addin tracking setting and onboarding logic

--- src/Tracking/TrackingServiceProvider.php
+++ src/Tracking/TrackingServiceProvider.php
@@ -36,14 +36,14 @@ class TrackingServiceProvider implements ServiceProvider {
 			Hooks::addAction( TrackJobScheduler::CRON_JOB_HOOK_NAME, TrackJob::class, 'send' );
 		}
 
-		if ( is_admin() ) {
+		if ( is_admin() && Track::checkEnvironment() ) {
 			Hooks::addFilter( 'give_get_settings_advanced', AdminSettings::class, 'addSettings' );
 			Hooks::addAction( 'give_opt_in_into_tracking', AdminActionHandler::class, 'optInToUsageTracking' );
 			Hooks::addAction( 'give_hide_opt_in_notice_shortly', AdminActionHandler::class, 'optOutFromUsageTracking' );
 			Hooks::addAction( 'give_hide_opt_in_notice_permanently', AdminActionHandler::class, 'optOutFromUsageTracking' );
 			Hooks::addAction( 'update_option_give_settings', AdminActionHandler::class, 'optInToUsageTrackingAdminGrantManually', 10, 2 );
-			Hooks::addAction( 'admin_notices', UsageTrackingOnBoarding::class, 'addNotice' );
 			Hooks::addAction( 'give_setup_page_before_sections', UsageTrackingOnBoarding::class, 'addNotice', 0 );
+			Hooks::addAction( 'admin_notices', UsageTrackingOnBoarding::class, 'addNotice' );
 		}
 	}
 
