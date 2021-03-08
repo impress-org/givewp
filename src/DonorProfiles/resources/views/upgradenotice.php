32568f087379dcb829b1f69b1ac12c9d189389cd
Henry Holtgeerts <henryholtgeerts@gmail.com>
2021-03-08 16:20:33 -0500

    fix: improve upgrade notice copy and styles

--- src/DonorProfiles/resources/views/upgradenotice.php
+++ src/DonorProfiles/resources/views/upgradenotice.php
@@ -11,25 +11,30 @@ $setupUrl = add_query_arg(
 
 <div class="notice give-donor-profile-upgrade-notice is-dismissible hidden" data-give-dismissible="upgrade-donor-profiles-notice-210" >
 	<div class="give-donor-profile-upgrade-notice__graphic">
-		Donor Profile Graphic
+		<img src="<?php echo GIVE_PLUGIN_URL . '/assets/dist/images/admin/donor-dashboard.svg'; ?>"/>
 	</div>
 	<div class="give-donor-profile-upgrade-notice__copy">
-		<div class="give-donor-profile-upgrade-notice__subtitle">
-			New in GiveWP 2.10
-		</div>
-		<div class="give-donor-profile-upgrade-notice__title">
-			Donor Profiles
+		<div class="give-donor-profile-upgrade-notice__row">
+			<div class="give-donor-profile-upgrade-notice__title">
+				Introducing the Donor Dashboard
+			</div>
+			<div class="give-donor-profile-upgrade-notice__badge">
+				<i class="fas fa-bell"></i> New in GiveWP 2.10.0
+			</div>
 		</div>
 		<div class="give-donor-profile-upgrade-notice__body">
-			Let donors update their information, review receipts, and mange subscriptions. All in one place.
+			The Donor Dashboard provides your donors with a one-stop location to manage all their giving history, profile, and more! Ready to get started? In order to use the new Donor Dashboard, GiveWP needs to create a new page on your site.
 		</div>
-		<a href="#">
-			Learn more on the GiveWP blog
-		</a>
+		<div class="give-donor-profile-upgrade-notice__row">
+			<a class="give-donor-profile-upgrade-notice__button" href="<?php echo $setupUrl; ?>">
+				Create Donor Dashboard Page
+			</a>
+			<div class="give-donor-profile-upgrade-notice__pill">
+				Want to know more? Learn more about the <a href="#">new Donor Dashboard <i class="fas fa-external-link-alt"></i></a>
+			</div>
+		</div>
 	</div>
 	<div class="give-donor-profile-upgrade-notice__cta">
-		<a class="give-donor-profile-upgrade-notice__button" href="<?php echo $setupUrl; ?>">
-			Setup Donor Profiles
-		</a>
+	
 	</div>
 </div>
