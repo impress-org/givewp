36037ff4fd86c5d44a0ba45bd883b337f7fbfc7c
alaca <ante.laca@gmail.com>
2021-03-10 09:53:51 +0100

    refactor: use `window.top` reference instead of `window`

--- src/DonorProfiles/resources/js/app/hooks/index.js
+++ src/DonorProfiles/resources/js/app/hooks/index.js
@@ -20,19 +20,19 @@ export const useWindowSize = () => {
 		function handleResize() {
 			// Set window width/height to state
 			setWindowSize( {
-				width: window.innerWidth,
-				height: window.innerHeight,
+				width: window.top.innerWidth,
+				height: window.top.innerHeight,
 			} );
 		}
 
 		// Add event listener
-		window.addEventListener( 'resize', handleResize );
+		window.top.addEventListener( 'resize', handleResize );
 
 		// Call handler right away so state gets updated with initial window size
 		handleResize();
 
 		// Remove event listener on cleanup
-		return () => window.removeEventListener( 'resize', handleResize );
+		return () => window.top.removeEventListener( 'resize', handleResize );
 	}, [] ); // Empty array ensures that effect is only run on mount
 
 	return windowSize;
