d68a55292442e50d45fb4be16dfc7b0cf25baf26
Henry Holtgeerts <henryholtgeerts@gmail.com>
2021-03-12 10:01:10 -0500

    fix: localize string

--- src/DonorProfiles/resources/views/successnotice.php
+++ src/DonorProfiles/resources/views/successnotice.php
@@ -7,5 +7,5 @@ $pageUrl = get_permalink( $pageId );
 ?>
 
 <div class="notice notice-success is-dismissible">
-	<p><?php printf( __( 'Success! Donor Dashboard page was created. You can <a href="%s">take a look at it here.</a>' ), $pageUrl ); ?></p>
+	<p><?php printf( __( 'Success! Donor Dashboard page was created. You can <a href="%s">take a look at it here.</a>', 'give' ), $pageUrl ); ?></p>
 </div>
