a9ea570d5f84eb391b03cc971cadc6f7cac51c7b
Kyle B. Johnson <kyle.johnson@impress.org>
2021-03-15 15:46:01 -0400

    chore: Add missing docblock tags.

--- src/Onboarding/SettingsRepositoryFactory.php
+++ src/Onboarding/SettingsRepositoryFactory.php
@@ -8,15 +8,22 @@ namespace Give\Onboarding;
 class SettingsRepositoryFactory {
 
 	/**
-	 * @param string $optionName
-	 *
 	 * @since 2.8.0
 	 * @unreleased Casts the option value as an array.
+	 *
+	 * @param string $optionName
+	 *
+	 * @return SettingsRepository
 	 */
 	public function make( $optionName ) {
 
 		$option = get_option( $optionName, [] );
 
+		/**
+		 * @param array $settings
+		 *
+		 * @return bool True if the value was updated, false otherwise.
+		 */
 		$persistCallback = function( $settings ) use ( $optionName ) {
 			return update_option( $optionName, $settings );
 		};
