249ae6d7e82e2369b022526c1df709575479a429
alaca <ante.laca@gmail.com>
2021-03-16 13:16:08 +0100

    refactor: update test

--- tests/e2e/integration/logs/logs_page_spec.js
+++ tests/e2e/integration/logs/logs_page_spec.js
@@ -7,8 +7,8 @@ describe( 'Test Logs page', function() {
 		cy.exec( 'wp-env run cli wp give flush-logs' );
 
 		// Seed logs
-		cy.exec( 'wp-env run cli "wp give test-logs --type=notice"' );
-		cy.exec( 'wp-env run cli "wp give test-logs --type=error"' );
+		cy.exec( 'wp-env run cli "wp give test-logs --type=notice --category=CypressCategory --count=5"' );
+		cy.exec( 'wp-env run cli "wp give test-logs --type=error --source=CypressSource"' );
 
 		cy.visit( baseURL + '/wp-admin/edit.php?post_type=give_forms&page=give-tools&tab=logs' );
 		// Logs table is visible
@@ -36,6 +36,30 @@ describe( 'Test Logs page', function() {
 		// Check sorted
 		cy.getByTest( 'logs-table' ).find( '.give-table-row div[class*=label]' ).should( 'satisfy', ( element ) => {
 			return element[ 0 ].className.indexOf( 'notice' ) !== -1;
+		} );
+
+		// Reset status sort
+		cy.getByTest( 'logs-status-dropdown' ).select( 'All statuses' );
+
+		// Sort by Category
+		cy.getByTest( 'logs-category-dropdown' ).select( 'CypressCategory' );
+		cy.getByTest( 'logs-category-dropdown' ).should( 'have.value', 'CypressCategory' );
+
+		// Check sorted
+		cy.getByTest( 'logs-table' ).find( '.give-table-row div[class*=item]:eq(1)' ).should( 'satisfy', ( element ) => {
+			return element[ 0 ].innerText === 'CypressCategory';
+		} );
+
+		// Reset category sort
+		cy.getByTest( 'logs-category-dropdown' ).select( 'All categories' );
+
+		// Sort by Source
+		cy.getByTest( 'logs-source-dropdown' ).select( 'CypressSource' );
+		cy.getByTest( 'logs-source-dropdown' ).should( 'have.value', 'CypressSource' );
+
+		// Check sorted
+		cy.getByTest( 'logs-table' ).find( '.give-table-row div[class*=item]:eq(2)' ).should( 'satisfy', ( element ) => {
+			return element[ 0 ].innerText === 'CypressSource';
 		} );
 
 		// Flush logs
