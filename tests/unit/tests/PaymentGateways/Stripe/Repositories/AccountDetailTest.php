4e4e4f722347e991aa33d1035d16f955d25c514c
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2021-03-17 21:17:29 +0530

    test: set db value correctely for test

--- tests/unit/tests/PaymentGateways/Stripe/Repositories/AccountDetailTest.php
+++ tests/unit/tests/PaymentGateways/Stripe/Repositories/AccountDetailTest.php
@@ -40,7 +40,7 @@ class AccountDetailTest extends TestCase{
 			]
 		);
 		give_update_option( '_give_stripe_default_account', $globalStripeAccountId );
-		give_update_meta( $this->form->get_ID(), 'give_stripe_per_form_accounts', false );
+		give_update_meta( $this->form->get_ID(), 'give_stripe_per_form_accounts', 'disabled' );
 
 		$this->assertSame( $globalStripeAccountId, $this->repository->getDonationFormStripeAccountId( $this->form->get_ID() )->accountId );
 	}
@@ -67,7 +67,7 @@ class AccountDetailTest extends TestCase{
 			]
 		);
 		give_update_option( '_give_stripe_default_account', $globalStripeAccountId );
-		give_update_meta( $this->form->get_ID(), 'give_stripe_per_form_accounts', true );
+		give_update_meta( $this->form->get_ID(), 'give_stripe_per_form_accounts', 'enabled' );
 		give_update_meta( $this->form->get_ID(), '_give_stripe_default_account', $manuallySelectedStripeAccountId );
 
 		$this->assertSame( $manuallySelectedStripeAccountId, $this->repository->getDonationFormStripeAccountId( $this->form->get_ID() )->accountId );
