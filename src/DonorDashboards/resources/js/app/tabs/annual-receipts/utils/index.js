87a2cf40de42166fe00ee5eca22b72de1ceaeb84
Henry Holtgeerts <henryholtgeerts@gmail.com>
2021-03-29 16:49:52 -0400

    fix: check that donor is logged in before fetching

--- src/DonorDashboards/resources/js/app/tabs/annual-receipts/utils/index.js
+++ src/DonorDashboards/resources/js/app/tabs/annual-receipts/utils/index.js
@@ -1,21 +1,25 @@
 import axios from 'axios';
 import { store } from '../store';
-import { getAPIRoot } from '../../../utils';
+import { getAPIRoot, isLoggedIn } from '../../../utils';
 import { setAnnualReceipts, setQuerying } from '../store/actions';
 
 export const fetchAnnualReceiptsFromAPI = () => {
-	const { dispatch } = store;
 
-	dispatch( setQuerying( true ) );
-	axios.post( getAPIRoot() + 'give-api/v2/donor-dashboard/annual-receipts', {},
-		{} )
-		.then( ( response ) => response.data )
-		.then( ( data ) => {
-			const { receipts } = data;
-			dispatch( setAnnualReceipts( receipts ) );
-			dispatch( setQuerying( false ) );
-		} )
-		.catch( () => {
-			dispatch( setQuerying( false ) );
-		} );
+	const { dispatch } = store;
+	const loggedIn = isLoggedIn();
+
+	if ( loggedIn ) {
+		dispatch( setQuerying( true ) );
+		axios.post( getAPIRoot() + 'give-api/v2/donor-dashboard/annual-receipts', {},
+			{} )
+			.then( ( response ) => response.data )
+			.then( ( data ) => {
+				const { receipts } = data;
+				dispatch( setAnnualReceipts( receipts ) );
+				dispatch( setQuerying( false ) );
+			} )
+			.catch( () => {
+				dispatch( setQuerying( false ) );
+			} );
+	}
 };
