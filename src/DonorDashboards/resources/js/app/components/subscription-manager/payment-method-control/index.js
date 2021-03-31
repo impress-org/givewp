e09a89e15597ef1655726e9c1d2e7ed8a6f37b33
Henry Holtgeerts <henryholtgeerts@gmail.com>
2021-03-31 18:59:21 -0400

    fix: only display card controls for supported gateways

--- src/DonorDashboards/resources/js/app/components/subscription-manager/payment-method-control/index.js
+++ src/DonorDashboards/resources/js/app/components/subscription-manager/payment-method-control/index.js
@@ -5,11 +5,19 @@ import './style.scss';
 
 const PaymentMethodControl = ( { gateway, label, onChange } ) => {
 	switch ( gateway ) {
-		case 'stripe': {
+		case 'stripe':
+		case 'stripe_apple_pay':
+		case 'stripe_becs':
+		case 'stripe_checkout':
+		case 'stripe_google_pay': {
 			return <StripeControl label={ label } onChange={ ( val ) => onChange( val ) } />;
 		}
-		default: {
+		case 'authorize':
+		case 'paypalpro': {
 			return <CardControl label={ label } onChange={ ( val ) => onChange( val ) } />;
+		}
+		default: {
+			return null;
 		}
 	}
 };
