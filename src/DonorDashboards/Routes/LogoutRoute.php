6679977c63bf2e071e4a98a111ed7ae156d200e7
Henry Holtgeerts <henryholtgeerts@gmail.com>
2021-04-20 09:41:12 -0400

    fix: conditionally handle logout for WP users

--- src/DonorDashboards/Routes/LogoutRoute.php
+++ src/DonorDashboards/Routes/LogoutRoute.php
@@ -43,27 +43,37 @@ class LogoutRoute implements RestRoute {
 	 */
 	public function handleRequest( WP_REST_Request $request ) {
 
-		// Prevent occurring of any custom action on wp_logout.
-		remove_all_actions( 'wp_logout' );
+		// Check if WP user is logged in
+		if ( get_current_user_id() !== 0 ) {
 
-		/**
-		 * Fires before processing user logout.
-		 *
-		 * @since 1.0
-		 */
-		do_action( 'give_before_user_logout' );
+			// Handle logout logic for WP users
 
-		// Logout user.
-		wp_logout();
+			// Prevent occurring of any custom action on wp_logout.
+			remove_all_actions( 'wp_logout' );
 
-		/**
-		 * Fires after processing user logout.
-		 *
-		 * @since 1.0
-		 */
-		do_action( 'give_after_user_logout' );
+			/**
+			 * Fires before processing user logout.
+			 *
+			 * @since 1.0
+			 */
+			do_action( 'give_before_user_logout' );
 
-		give()->session->destroy_session();
+			// Logout user (and destroys current Give Session, via hook registered in Give_Session class)
+			wp_logout();
+
+			/**
+			 * Fires after processing user logout.
+			 *
+			 * @since 1.0
+			 */
+			do_action( 'give_after_user_logout' );
+
+		} else {
+
+			// Destroy current Give Session
+			give()->session->destroy_session();
+
+		}
 
 		return new WP_REST_Response(
 			[
