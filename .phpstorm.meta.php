205b665f30e2d4e0e47c2e5d6a0f43ea7bad98fa
Scott Kingsley Clark <scott@skc.dev>
2021-04-26 22:01:40 -0500

    Update .phpstorm.meta.php

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace PHPSTORM_META {
-	// Allow PHP Storm Editor to resolve return types when calling give( Object_Type::class ) or give( `Object_type` )
+	// Allow PhpStorm IDE to resolve return types when calling give( Object_Type::class ) or give( `Object_Type` ).
 	override(
 		\give( 0 ),
 		map( [
