6d9c918430fece39736e05a957a44a4f4dfd8cfd
Kyle B. Johnson <kyle.johnson@impress.org>
2021-05-06 13:32:57 -0400

    chore: Add baseline test for minor amounts.

--- tests/unit/tests/ValueObjects/MoneyTest.php
+++ tests/unit/tests/ValueObjects/MoneyTest.php
@@ -5,7 +5,12 @@ use Give\ValueObjects\Money;
 
 final class MoneyTest extends TestCase {
 
-	public function testMinorAmountIsNotChanged() {
+	public function testMinorAmount() {
+		$money = Money::of( 250, 'USD' );
+		$this->assertEquals( 25000, $money->getMinorAmount() );
+	}
+
+	public function testMinorAmountFloat() {
 		$money = Money::of( 257.78, 'USD' );
 		$this->assertEquals( 25778, $money->getMinorAmount() );
 	}
