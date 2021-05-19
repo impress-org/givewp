1fcb963bcec9c4c506b4e30b7e821bfb021b84a2
Jason Adams <jason.adams@impress.org>
2021-05-19 16:20:49 -0700

    test: attempt to add testing with mocking

---
+++ tests/unit/tests/Framework/Exceptions/UncaughtExceptionLoggerTest.php
@@ -0,0 +1,41 @@
+<?php
+
+use Give\Framework\Exceptions\Contracts\LoggableException;
+use Give\Framework\Exceptions\UncaughtExceptionLogger;
+use Give\Log\Log;
+
+class UncaughtExceptionLoggerTest extends Give_Unit_Test_Case {
+	public function testShouldLogException() {
+		$logger = new UncaughtExceptionLogger();
+
+		$this->mock( Log::class, function(PHPUnit_Framework_MockObject_MockBuilder $builder) {
+			$mock = $builder->setMethods(['error'])
+				->getMock();
+
+			$mock->expects( $this->once() )
+					->method( 'error' )
+					->with( [ '', [] ] );
+
+			return $mock;
+		} );
+
+		$logger->handleException( new ExceptionLogged() );
+	}
+
+	public function testShouldNotLogException() {
+
+	}
+}
+
+class ExceptionLogged extends Exception implements LoggableException {
+	public function getLogMessage() {
+		return '';
+	}
+
+	public function getLogContext() {
+		return [];
+	}
+}
+
+class ExceptionNotLogged extends Exception {
+}
