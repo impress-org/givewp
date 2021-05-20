a34c1df7ff3486bc3b1750c886398e1f688f6d66
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2021-05-20 17:54:27 +0530

    fix: hide illing field when admin disconnect account

--- assets/src/js/admin/paypal-commerce/index.js
+++ assets/src/js/admin/paypal-commerce/index.js
@@ -63,7 +63,7 @@ window.addEventListener( 'DOMContentLoaded', function() {
 				`.trim(),
 				cancelBtnTitle: Give.fn.getGlobalVar( 'confirm' ),
 			},
-			closeOnBgClick: true,
+			closeOnBgClick: true
 		} ).render();
 
 		// Update URL in browser address without reloading the page.
@@ -165,6 +165,9 @@ window.addEventListener( 'DOMContentLoaded', function() {
 					connectionSettingContainer.classList.remove( 'give-hidden' );
 					disConnectionSettingContainer.classList.add( 'give-hidden' );
 					countryField.parentElement.parentElement.classList.remove( 'hide-with-position' );
+
+					let billingSettingContainer = document.querySelector('label[for=\'paypal_commerce_collect_billing_details\']');
+					billingSettingContainer.parentElement.parentElement.classList.add('give-hidden');
 
 					fetch( ajaxurl + '?action=give_paypal_commerce_disconnect_account' );
 				},
