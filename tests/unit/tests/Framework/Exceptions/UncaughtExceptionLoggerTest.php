67934ae7af01b0747af83504e867996114f7a4e7
Kyle B. Johnson <kyle.johnson@impress.org>
2021-05-20 11:54:06 -0400

    fix: Update mock expectation logic.

--- tests/unit/tests/Framework/Exceptions/UncaughtExceptionLoggerTest.php
+++ tests/unit/tests/Framework/Exceptions/UncaughtExceptionLoggerTest.php
@@ -8,16 +8,13 @@ class UncaughtExceptionLoggerTest extends Give_Unit_Test_Case {
 	public function testShouldLogException() {
 		$logger = new UncaughtExceptionLogger();
 
-		$this->mock( Log::class, function(PHPUnit_Framework_MockObject_MockBuilder $builder) {
-			$mock = $builder->setMethods(['error'])
-				->getMock();
-
-			$mock->expects( $this->once() )
-					->method( 'error' )
-					->with( [ '', [] ] );
-
-			return $mock;
+		$mock = $this->mock( Log::class, function(PHPUnit_Framework_MockObject_MockBuilder $builder) {
+			return $builder->setMethods(['error'])->getMock();
 		} );
+
+		$mock->expects( $this->once() )
+			->method( 'error' )
+			->with( '', [] );
 
 		$logger->handleException( new ExceptionLogged() );
 	}
