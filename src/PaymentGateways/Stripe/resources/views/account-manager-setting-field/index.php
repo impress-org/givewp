e4adf59c242cc34bead8e1dd407b1bc44f54d018
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2021-05-21 22:17:02 +0530

    refactor: divide setting field html to mutiple templates

---
+++ src/PaymentGateways/Stripe/resources/views/account-manager-setting-field/index.php
@@ -0,0 +1,6 @@
+<div id="give-stripe-account-manager-errors"></div>
+<?php include 'introduction.php'; ?>
+<div class="give-stripe-account-manager-container">
+	<?php include 'stripe-account-list.php'; ?>
+	<?php include 'add-new-stripe-account.php'; ?>
+</div>
