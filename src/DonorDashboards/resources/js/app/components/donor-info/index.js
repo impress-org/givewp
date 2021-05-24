0173db9c9388a1a19760f96fcc36fcf9ec23e077
Jason Adams <jason.adams@impress.org>
2021-05-24 15:01:25 -0700

    fix: remove extra parenthesis

--- src/DonorDashboards/resources/js/app/components/donor-info/index.js
+++ src/DonorDashboards/resources/js/app/components/donor-info/index.js
@@ -13,7 +13,7 @@ const DonorInfo = () => {
 			<div className="give-donor-dashboard-donor-info__avatar">
 				<div className="give-donor-dashboard-donor-info__avatar-container">
 					{ avatarUrl ? (
-						<img src={ avatarUrl } />
+						<img alt="Donor Picture" src={ avatarUrl } />
 					) : (
 						<span className="give-donor-dashboard-donor-info__avatar-initials">
 							{ initials ? initials : <FontAwesomeIcon icon="user" /> }
@@ -44,7 +44,7 @@ const DonorInfo = () => {
 				) }
 				{ sinceCreated && (
 					<div className="give-donor-dashboard-donor-info__detail">
-						<FontAwesomeIcon icon="heart" fixedWidth={ true } /> { sprintf( __( 'Donor for %s', 'give' ), sinceCreated ) ) }
+						<FontAwesomeIcon icon="heart" fixedWidth={ true } /> { sprintf( __( 'Donor for %s', 'give' ), sinceCreated ) }
 					</div>
 				) }
 			</div>
