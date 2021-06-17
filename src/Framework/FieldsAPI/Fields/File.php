12fcec94ca9ff0af58385ef5aa516312073f34db
Nathan Knowler <nathan@impress.org>
2021-06-17 16:17:32 -0500

    feature: add max size and allowed types to File

--- src/Framework/FieldsAPI/Fields/File.php
+++ src/Framework/FieldsAPI/Fields/File.php
@@ -28,6 +28,52 @@ class File implements Field, ValidatesRequired {
 	//use Concerns\ShowInReceipt;
 	//use Concerns\StoreAsMeta;
 
+	/** @var string */
 	protected $type = 'file';
 
+	/** @var int */
+	protected $maxSize = 1024;
+
+	/** @var string[] */
+	protected $allowedTypes = [ '*' ];
+
+	/**
+	 * Set the maximum file size.
+	 *
+	 * @param int $maxSize
+	 * @return $this
+	 */
+	public function maxSize( $maxSize ) {
+		$this->maxSize = $maxSize;
+		return $this;
+	}
+
+	/**
+	 * Access the maximum file size.
+	 *
+	 * @return int
+	 */
+	public function getMaxSize() {
+		return $this->maxSize;
+	}
+
+	/**
+	 * Set the allowed file types.
+	 *
+	 * @param string[] $allowedTypes
+	 * @return $this
+	 */
+	public function allowedTypes( $allowedTypes = [ '*' ] ) {
+		$this->allowedTypes = $allowedTypes;
+		return $this;
+	}
+
+	/**
+	 * Access the allowed file types.
+	 *
+	 * @return string[]
+	 */
+	public function getAllowedTypes() {
+		return $this->allowedTypes;
+	}
 }
