0e69ab4a2c721f56a9db230badbe4919db9a3c0c
Nathan Knowler <nathan@impress.org>
2021-06-17 15:41:31 -0500

    feature: add HTML field class

---
+++ src/Framework/FieldsAPI/Fields/Html.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Give\Framework\FieldsAPI\Fields;
+
+use Give\Framework\FieldsAPI\Fields\Contracts\Field;
+
+/**
+ * An HTML field.
+ *
+ * @unreleased
+ */
+class Html implements Field {
+
+	use Concerns\HasType;
+	use Concerns\MakeFieldWithName;
+	use Concerns\SerializeAsJson;
+
+	/** @var string  */
+	protected $type = 'html';
+
+	/** @var string */
+	protected $html;
+
+	/**
+	 * Set the HTML string.
+	 *
+	 * @param string $html
+	 *
+	 * @return $this
+	 */
+	public function html( $html ) {
+		$this->html = $html;
+		return $this;
+	}
+
+	/**
+	 * Access the HTML string.
+	 *
+	 * @return string
+	 */
+	public function getHtml() {
+		return $this->html;
+	}
+}
