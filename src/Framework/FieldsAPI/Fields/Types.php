298e5b0918a73e4a0a4515a2fdf4dd48ed966329
Nathan Knowler <nathan@impress.org>
2021-06-18 00:38:28 -0500

    refactor: use types from field classes

---
+++ src/Framework/FieldsAPI/Fields/Types.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Give\Framework\FieldsAPI\Fields;
+
+class Types {
+	const CHECKBOX = Checkbox::TYPE;
+	const DATE     = Date::TYPE;
+	const EMAIL    = Email::TYPE;
+	const FILE     = File::TYPE;
+	const HIDDEN   = Hidden::TYPE;
+	const HTML     = Html::TYPE;
+	const PHONE    = Phone::TYPE;
+	const RADIO    = Radio::TYPE;
+	const SELECT   = Select::TYPE;
+	const TEXT     = Text::TYPE;
+	const TEXTAREA = Textarea::TYPE;
+	const URL      = Url::TYPE;
+}
