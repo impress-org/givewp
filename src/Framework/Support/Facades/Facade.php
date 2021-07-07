34265233c6067b651dbbae8ce6ca213ef1fd6418
Jason Adams <jason.adams@impress.org>
2021-07-07 11:45:19 -0700

    doc: add Facade usage documentation

--- src/Framework/Support/Facades/Facade.php
+++ src/Framework/Support/Facades/Facade.php
@@ -7,6 +7,16 @@ use Give\Framework\Exceptions\Primitives\Exception;
 /**
  * Class Facade
  *
+ * This class provides a way of taking a normal instance class and creating a static facade out of it. It does it in
+ * such a way, though, that the facade is still mockable. It does this by instantiating the decorated class through the
+ * Give Service Container (SC). So by injecting a mock singleton of the decorated class in the SC, it can be mocked.
+ *
+ * To use this, simply make a new facade class which extends this once, then override the getFacadeClass and return the
+ * class to be decorated, for example: return MyClass::class;
+ *
+ * To help the IDE, take the methods from the decorated class and add them your class docblock. So if Repository had an
+ * insert method, you would add "@method static Model insert()" to the top.
+ *
  * @unreleased
  */
 class Facade {
