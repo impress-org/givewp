64d40f6e87d95af1e38e3bc729efc7f3df3d9065
Nathan Knowler <nathan@impress.org>
2021-07-07 14:45:41 -0500

    refactor: Form extends Group
    When serializing a form, we decided that it should not just be an array
    of the nodes that it contains and it should include other details like
    `name`, `type`, `label`, etc. So now Form extends a Group node type.

--- src/Framework/FieldsAPI/Form.php
+++ src/Framework/FieldsAPI/Form.php
@@ -2,48 +2,7 @@
 
 namespace Give\Framework\FieldsAPI;
 
-use Give\Framework\FieldsAPI\Contracts\Collection;
-use Give\Framework\FieldsAPI\Contracts\Node;
-
-class Form implements Node, Collection {
-
-	use Concerns\AppendNodes;
-	use Concerns\HasLabel;
-	use Concerns\HasNodes;
-	use Concerns\HasType;
-	use Concerns\InsertNode;
-	use Concerns\RemoveNode;
-	use Concerns\WalkNodes;
+class Form extends Group {
 
 	const TYPE = 'form';
-
-	/** @var string */
-	protected $name = 'root';
-
-	/**
-	 * @return string
-	 */
-	public function getName() {
-		return $this->name;
-	}
-
-	/**
-	 * Form constructor.
-	 *
-	 * @param Node ...$nodes
-	 */
-	public function __construct( Node ...$nodes ) {
-		$this->nodes = $nodes;
-	}
-
-	/**
-	 * Make the form with nodes.
-	 *
-	 * @param Node ...$nodes
-	 *
-	 * @return static
-	 */
-	public static function make( Node ...$nodes ) {
-		return new static( ...$nodes );
-	}
 }
