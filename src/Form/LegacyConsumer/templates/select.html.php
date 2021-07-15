d9a27e64d747e8d3c948bf22befe9330fac7078f
Jason Adams <jason.adams@impress.org>
2021-07-15 12:06:40 -0700

    feature: add default value to select and radio template

--- src/Form/LegacyConsumer/templates/select.html.php
+++ src/Form/LegacyConsumer/templates/select.html.php
@@ -1,4 +1,4 @@
-<?php /** @var Give\Framework\FieldsAPI\Fields\Select $field */ ?>
+<?php /** @var Give\Framework\FieldsAPI\Select $field */ ?>
 <select
 	name="give_<?php echo $field->getName(); ?>"
 	id="give-<?php echo $field->getName(); ?>"
@@ -9,8 +9,10 @@
 	<?php foreach ( $field->getOptions() as $option ) : ?>
 		<?php $label = $option->getLabel(); ?>
 		<?php $value = $option->getValue(); ?>
+		<?php $default = $field->getDefaultValue() === $option->getValue(); ?>
 	<option
 		<?php echo $label ? "value={$value}" : ''; ?>
+		<?php echo $default ? 'selected' : ''; ?>
 	>
 		<?php echo $label ?: $value; ?>
 	</option>
