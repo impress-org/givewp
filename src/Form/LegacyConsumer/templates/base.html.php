9f9cc103b3b02c628cd10480b9987dd18e15b6a2
Jason Adams <jason.adams@impress.org>
2021-07-15 12:07:26 -0700

    feature: add required and placeholder to label template

--- src/Form/LegacyConsumer/templates/base.html.php
+++ src/Form/LegacyConsumer/templates/base.html.php
@@ -1,8 +1,9 @@
-<?php /** @var $field */ ?>
+<?php /** @var Give\Framework\FieldsAPI\Field $field */ ?>
 <?php /** @var string $typeAttribute */ ?>
 <input
 	type="<?php echo $typeAttribute; ?>"
 	name="<?php echo $field->getName(); ?>"
+	placeholder="<?php echo $field->getLabel(); ?>"
 	id="give-<?php echo $field->getName(); ?>"
 	value="<?php echo $field->getDefaultValue(); ?>"
 	<?php echo $field->isRequired() ? 'required' : ''; ?>
--- src/Form/LegacyConsumer/templates/label-content.html.php
+++ src/Form/LegacyConsumer/templates/label-content.html.php
@@ -1,2 +1,5 @@
-<?php /** @var Give\Framework\FieldsAPI\Fields\Concerns\HasLabel $field */ ?>
+<?php /** @var Give\Framework\FieldsAPI\Field $field */ ?>
 <?php echo $field->getLabel(); ?>
+<?php if ( $field->isRequired() ) : ?>
+<span class="give-required-indicator"> *</span>
+<?php endif; ?>
