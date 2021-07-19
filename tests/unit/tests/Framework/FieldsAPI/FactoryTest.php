b3b8dea30762dfe09dbe11f746bb60903e7f4f37
Kyle B. Johnson <kyle.johnson@impress.org>
2021-07-19 16:09:17 -0400

    chore: Update factory test assertions.

--- tests/unit/tests/Framework/FieldsAPI/FactoryTest.php
+++ tests/unit/tests/Framework/FieldsAPI/FactoryTest.php
@@ -1,13 +1,14 @@
 <?php
 
 use Give\Framework\FieldsAPI\Factory;
-use Give_Unit_Test_Case;
+use Give\Framework\FieldsAPI\Text;
 
 class FactoryTest extends Give_Unit_Test_Case {
 
 	public function testMakeTextField() {
 		$field = Factory::text('my-text-field');
 
-		$this->assertEquals( 'text', $field::TYPE );
+		$this->assertInstanceOf(Text::class, $field);
+		$this->assertEquals('my-text-field', $field->getName());
 	}
 }
