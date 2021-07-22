145d4690590536966d77934c476c955bb012600b
Kyle B. Johnson <kyle.johnson@impress.org>
2021-07-22 14:44:54 -0400

    fix: Stripe HTML tags from button level text.

--- includes/forms/template.php
+++ includes/forms/template.php
@@ -550,7 +550,7 @@ function give_output_levels( $form_id ) {
 					$level_classes,
 					$formatted_amount,
 					array_key_exists( '_give_default', $price ) ? 1 : 0,
-					esc_attr( $level_text )
+					esc_attr( strip_tags( $level_text ) )
 				);
 			}
 
@@ -592,7 +592,7 @@ function give_output_levels( $form_id ) {
 					$formatted_amount,
 					( give_is_default_level_id( $price ) ? 'checked="checked"' : '' ),
 					array_key_exists( '_give_default', $price ) ? 1 : 0,
-					esc_html( $level_text )
+					esc_html( strip_tags( $level_text ) )
 				);
 			}
 
@@ -640,7 +640,7 @@ function give_output_levels( $form_id ) {
 					$formatted_amount,
 					( give_is_default_level_id( $price ) ? 'selected="selected"' : '' ),
 					array_key_exists( '_give_default', $price ) ? 1 : 0,
-					esc_html( $level_text )
+					esc_html( strip_tags( $level_text ) )
 				);
 			}
 
