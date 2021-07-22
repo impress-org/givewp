db5c7efb570f5cef5b94d9ed6a92d8c9bd8ce1b9
Kyle B. Johnson <kyle.johnson@impress.org>
2021-07-22 14:55:52 -0400

    chore: Update docblock.

--- includes/forms/template.php
+++ includes/forms/template.php
@@ -499,10 +499,11 @@ add_action( 'give_donation_form_top', 'give_output_donation_amount_top', 10, 2 )
 /**
  * Outputs the Donation Levels in various formats such as dropdown, radios, and buttons.
  *
- * @param int $form_id The form ID.
- *
- * @return string Donation levels.
  * @since  1.0
+ * @unreleased Stripe HTML tags from rendered level text https://github.com/impress-org/givewp/pull/5894
+ *
+ * @param int $form_id The form ID.
+ * @return string Donation levels.
  */
 function give_output_levels( $form_id ) {
 
