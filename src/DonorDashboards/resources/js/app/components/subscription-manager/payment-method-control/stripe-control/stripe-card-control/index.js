a222111593e7bd75904783011daa4f69682c7243
alaca <ante.laca@gmail.com>
2021-07-29 15:18:16 +0200

    refactor: update component to use ready state;

--- src/DonorDashboards/resources/js/app/components/subscription-manager/payment-method-control/stripe-control/stripe-card-control/index.js
+++ src/DonorDashboards/resources/js/app/components/subscription-manager/payment-method-control/stripe-control/stripe-card-control/index.js
@@ -1,46 +1,59 @@
-import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import { CardElement, useStripe } from '@stripe/react-stripe-js';
 import { useState, useEffect } from 'react';
 import './style.scss';
 
-const CardControl = ( { label, onChange, value } ) => {
+const CardControl = ( { label, onChange, onReady, value } ) => {
 	const stripe = useStripe();
-	const elements = useElements();
 
 	const [ focused, setFocused ] = useState( false );
 	const [ paymentMethodId, setPaymentMethodId ] = useState( value );
 
 	useEffect( () => {
 		if ( paymentMethodId ) {
+			handleReady( true );
 			onChange( {
 				give_stripe_payment_method: paymentMethodId,
 			} );
 		}
 	}, [ paymentMethodId ] );
 
-	const handleBlur = async() => {
-		setFocused( false );
-
-		if ( ! stripe || ! elements ) {
-			// Stripe.js has not loaded yet. Make sure to disable
-			// form submission until Stripe.js has loaded.
-			return;
+	const handleReady = ( ready ) => {
+		if ( typeof onReady === 'function' ) {
+			onReady( ready );
 		}
+	}
 
-		// Get a reference to a mounted CardElement. Elements knows how
-		// to find your CardElement because there can only ever be one of
-		// each type of element.
-		const cardElement = elements.getElement( CardElement );
+	const handleCard = ( cardElement ) => {
 
-		// Use your card Element with other Stripe.js APIs
-		const { error, paymentMethod } = await stripe.createPaymentMethod( {
-			type: 'card',
-			card: cardElement,
+		cardElement.on( 'focus', function(){
+			setFocused( true );
 		} );
 
-		if ( ! error ) {
-			setPaymentMethodId( paymentMethod.id );
-		}
-	};
+		cardElement.on( 'blur', async function() {
+			setFocused( false );
+
+			if ( cardElement._empty || cardElement._invalid ) {
+				return;
+			}
+
+			const { error, paymentMethod } = await stripe.createPaymentMethod( {
+				type: 'card',
+				card: cardElement,
+			} );
+
+			if ( ! error ) {
+				setPaymentMethodId( paymentMethod.id );
+			}
+		} );
+
+		cardElement.on( 'change', function( { empty } ) {
+			handleReady( empty );
+
+			if ( empty ) {
+				cardElement.clear();
+			}
+		} );
+	}
 
 	return (
 		<div className="give-donor-dashboard-stripe-card-control">
@@ -48,7 +61,7 @@ const CardControl = ( { label, onChange, value } ) => {
 			<div className={ focused ? 'give-donor-dashboard-stripe-card-control__input give-donor-dashboard-stripe-card-control__input--focused' : 'give-donor-dashboard-stripe-card-control__input' }>
 				<CardElement
 					style={ { base: { fontFamily: 'Montserrat' } } }
-					onFocus={ () => setFocused( true ) } onBlur={ () => handleBlur() }
+					onReady={ handleCard }
 				/>
 			</div>
 		</div>
