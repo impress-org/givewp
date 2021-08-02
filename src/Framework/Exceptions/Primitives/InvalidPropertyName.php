473d0f117f19da8b77c9e912e8c9e314551bae0a
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2021-08-02 19:59:17 +0530

    fix: extend custom exception class

--- src/Framework/Exceptions/Primitives/InvalidPropertyName.php
+++ src/Framework/Exceptions/Primitives/InvalidPropertyName.php
@@ -2,14 +2,11 @@
 
 namespace Give\Framework\Exceptions\Primitives;
 
-use Give\Framework\Exceptions\Contracts\LoggableException;
-use Give\Framework\Exceptions\Traits\Loggable;
-
 /**
  * Class InvalidPropertyName
  * @package Give\Framework\Exceptions\Primitives
+ *
  * @unreleased
  */
-class InvalidPropertyName extends \RuntimeException implements LoggableException {
-	use Loggable;
+class InvalidPropertyName extends Exception {
 }
