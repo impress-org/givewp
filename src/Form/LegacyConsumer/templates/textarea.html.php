b0cd1ed35061f0141b94d31cb5627b13326d6457
Nathan Knowler <nathan@impress.org>
2021-08-12 14:10:55 -0500

    fix: remove undefined var in textarea template

--- src/Form/LegacyConsumer/templates/textarea.html.php
+++ src/Form/LegacyConsumer/templates/textarea.html.php
@@ -1,7 +1,5 @@
 <?php /** @var Give\Framework\FieldsAPI\Textarea $field */ ?>
-<?php /** @var string $classAttribute */ ?>
 <textarea
-  class="<?php echo $classAttribute; ?>"
   name="give_<?php echo $field->getName(); ?>"
   id="give-<?php echo $field->getName(); ?>"
 	<?php if ( $field->isRequired() ) : ?>
