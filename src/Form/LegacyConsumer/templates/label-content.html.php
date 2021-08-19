f3b5b57546fa173f40c99646bec00c33778427d5
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2021-08-19 22:22:24 +0530

    fix: add help text support to radio button and label view

--- src/Form/LegacyConsumer/templates/label-content.html.php
+++ src/Form/LegacyConsumer/templates/label-content.html.php
@@ -6,3 +6,8 @@
 		<span class="screen-reader-text"><?php esc_html_e( 'Required', 'give' ); ?></span>
 	</span>
 <?php endif; ?>
+<?php
+echo ( $helpText = $field->getHelpText() ) ?
+	Give()->tooltips->render_help( $helpText ) :
+	'';
+?>
