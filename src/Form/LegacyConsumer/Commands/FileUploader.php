fd2e7b2f7c3ed1fbc0951b73989666f51274f75c
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2021-08-24 10:49:44 +0530

    feature: create seperate class to handle file upload

---
+++ src/Form/LegacyConsumer/Commands/FileUploader.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Give\Form\LegacyConsumer\Commands;
+
+/**
+ * @package Give\Form\LegacyConsumer\Commands
+ *
+ * @unreleased
+ */
+class FileUploader {
+	private $files;
+
+	/**
+	 * @unreleased
+	 */
+	public function __construct() {
+		$this->files = $_FILES;
+	}
+
+	/**
+	 * @unreleased
+	 */
+	public function __invoke() {
+		if ( ! function_exists( 'wp_handle_upload' ) ) {
+			require_once( ABSPATH . 'wp-admin/includes/file.php' );
+		}
+
+		$fileIds = [];
+		foreach ( $this->files as $file ) {
+			$upload = wp_handle_upload( $file, [ 'test_form' => false, ] );
+
+			if ( isset( $upload['url'] ) ) {
+				$path = $upload['url'];
+
+				// Check the type of file. We'll use this as the 'post_mime_type'.
+				$filetype = wp_check_filetype( basename( $path ), null );
+
+				// Prepare an array of post data for the attachment.
+				$attachment = [
+					'guid'           => $path,
+					'post_mime_type' => $filetype['type'],
+					'post_title'     => preg_replace( '/\.[^.]+$/', '', basename( $path ) ),
+					'post_content'   => '',
+					'post_status'    => 'inherit',
+				];
+
+				// Insert the attachment.
+				$attachmentId = wp_insert_attachment( $attachment, $path );
+
+				// Make sure that this file is included, as wp_generate_attachment_metadata() depends on it.
+				require_once( ABSPATH . 'wp-admin/includes/image.php' );
+
+				// Generate the metadata for the attachment, and update the database record.
+				$attachmentData = wp_generate_attachment_metadata( $attachmentId, $path );
+				wp_update_attachment_metadata( $attachmentId, $attachmentData );
+
+				$fileIds[] = $attachmentId;
+			}
+		}
+
+		return $fileIds;
+	}
+}
--- src/Form/LegacyConsumer/Commands/SetupFieldPersistence.php
+++ src/Form/LegacyConsumer/Commands/SetupFieldPersistence.php
@@ -45,10 +45,18 @@ class SetupFieldPersistence implements HookCommandInterface {
 	 */
 	public function process( Field $field ) {
 		if ( isset( $_POST[ $field->getName() ] ) ) {
-			$data = give_clean( $_POST[ $field->getName() ] );
-			$value = is_array( $data ) ?
-				implode( '|', array_values( array_filter( $data ) ) ):
-				$data;
+			switch ( $field->getType() ) {
+				case 'file':
+					$fileUploader = new FileUploader();
+					$value = $fileUploader();
+					break;
+
+				default:
+					$data = give_clean( $_POST[ $field->getName() ] );
+					$value = is_array( $data ) ?
+						implode( '|', array_values( array_filter( $data ) ) ):
+						$data;
+			}
 
 			if ( $field->shouldStoreAsDonorMeta() ) {
 				$donorID = give_get_payment_meta( $this->donationID, '_give_payment_donor_id' );
@@ -58,53 +66,5 @@ class SetupFieldPersistence implements HookCommandInterface {
 				give_update_payment_meta( $this->donationID, $field->getName(), $value );
 			}
 		}
-	}
-
-	/**
-	 * @unreleased
-	 *
-	 * @param File $field
-	 *
-	 * @return array
-	 */
-	private function saveFiles( $field ){
-		if ( ! function_exists( 'wp_handle_upload' ) ) {
-			require_once( ABSPATH . 'wp-admin/includes/file.php' );
-		}
-
-		$fileIds = [];
-		foreach ( $_FILES as $file ) {
-			$upload = wp_handle_upload( $file, [ 'test_form' => false, ] );
-
-			if ( isset( $upload['url'] ) ) {
-				$path = $upload['url'];
-
-				// Check the type of file. We'll use this as the 'post_mime_type'.
-				$filetype = wp_check_filetype( basename( $path ), null );
-
-				// Prepare an array of post data for the attachment.
-				$attachment = [
-					'guid'           => $path,
-					'post_mime_type' => $filetype['type'],
-					'post_title'     => preg_replace( '/\.[^.]+$/', '', basename( $path ) ),
-					'post_content'   => '',
-					'post_status'    => 'inherit',
-				];
-
-				// Insert the attachment.
-				$attachmentId = wp_insert_attachment( $attachment, $path );
-
-				// Make sure that this file is included, as wp_generate_attachment_metadata() depends on it.
-				require_once( ABSPATH . 'wp-admin/includes/image.php' );
-
-				// Generate the metadata for the attachment, and update the database record.
-				$attachmentData = wp_generate_attachment_metadata( $attachmentId, $path );
-				wp_update_attachment_metadata( $attachmentId, $attachmentData );
-
-				$fileIds[] = $attachmentId;
-			}
-		}
-
-		return $fileIds;
 	}
 }
