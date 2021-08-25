76d4fb0d4fe1778faad49808a2cbcd5a79a8e278
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2021-08-25 17:10:42 +0530

    fix: add array suffix to name

--- src/Form/LegacyConsumer/templates/file.html.php
+++ src/Form/LegacyConsumer/templates/file.html.php
@@ -1,7 +1,7 @@
 <?php /** @var Give\Framework\FieldsAPI\File $field */ ?>
 <input
 	type="file"
-	name="<?php echo $field->getName(); ?>"
+	name="<?php echo $field->getName(); ?><?php echo $field->getAllowMultiple() ? '[]' : ''; ?>"
 	id="give-<?php echo $field->getName(); ?>"
 	<?php echo $field->getAllowMultiple() ? 'multiple' : ''; ?>
 	<?php echo $field->isRequired() ? 'required' : ''; ?>
