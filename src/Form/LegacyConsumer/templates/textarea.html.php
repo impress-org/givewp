9125c3a8bead41bd139dc01bf91f54cb2d1b1681
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2021-08-26 12:35:02 +0530

    fix: define placeholder in textarea field template in legacy consumer

--- src/Form/LegacyConsumer/templates/textarea.html.php
+++ src/Form/LegacyConsumer/templates/textarea.html.php
@@ -2,6 +2,7 @@
 <textarea
 	name="<?php echo $field->getName(); ?>"
 	id="give-<?php echo $field->getName(); ?>"
+	placeholder="<?php echo $field->getPlaceholder() ?: ''; ?>"
 	<?php echo $field->isRequired() ? 'required' : ''; ?>
 	<?php echo $field->isReadOnly() ? 'readonly' : ''; ?>
 ></textarea>
