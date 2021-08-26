89a1981891421c2fab13ab3fa4f48399d8b129fe
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2021-08-26 11:49:01 +0530

    feature: add wp_editor field

---
+++ src/Framework/FieldsAPI/WpEditor.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Give\Framework\FieldsAPI;
+
+/**
+ * @unreleased
+ */
+class WpEditor extends Field {
+	use Concerns\HasEmailTag;
+	use Concerns\HasHelpText;
+	use Concerns\HasLabel;
+	use Concerns\ShowInReceipt;
+	use Concerns\StoreAsMeta;
+
+	const TYPE = 'wp_editor';
+
+	/**
+	 * @unreleased
+	 * @param string $name
+	 */
+	public function __construct( $name ) {
+		parent::__construct( $name );
+
+		$this->validationRules->rule( 'richTextEditorType', 'teeny' ); // support: teeny, rich ( without media and quick tags )
+	}
+
+	/**
+	 * @unreleased
+	 *
+	 * @param bool $richTextEditorType
+	 *
+	 * @return $this
+	 */
+	public function richTextEditorType( $richTextEditorType ){
+		$this->validationRules->rule( 'richTextEditorType', $richTextEditorType );
+		return $this;
+	}
+
+	/**
+	 * @unreleased
+	 *
+	 * @return $this
+	 */
+	public function getRichTextEditorType(){
+		return $this->validationRules->getRule( 'richTextEditorType' );
+	}
+}
