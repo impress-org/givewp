c17f39c7be557fa505e1848b3636db16acfddcef
Jason Adams <jason.adams@impress.org>
2021-08-26 14:14:58 -0700

    fix: only prevent submission when modal is displayed

--- src/PaymentGateways/resources/js/stripe-account-manager/customize-stripe-account-validation.js
+++ src/PaymentGateways/resources/js/stripe-account-manager/customize-stripe-account-validation.js
@@ -18,37 +18,35 @@ window.addEventListener('DOMContentLoaded', function () {
 	form.addEventListener('submit', event => {
 		const stripeSetting = document.querySelector('#stripe_form_account_options [name="give_stripe_per_form_accounts"]:checked');
 
-		// Do nothing if Stripe is not enabled
+		// Do nothing if Stripe gateawy is not active
 		if( ! stripeSetting ) {
 			return;
 		}
 
-		event.preventDefault();
+		// Do nothing if Stripe account is not connected
 		const isCustomizeStripeAccountOptionSelected = stripeSetting.value === 'enabled';
 		if (!isCustomizeStripeAccountOptionSelected) {
-			form.submit()
 			return;
 		}
 
+		// Do nothing if default Stripe account is used
 		const hasDefaultStripeAccount = !!document.querySelector('#stripe_form_account_options .give-stripe-account-manager-list-item.give-stripe-boxshadow-option-wrap__selected');
-
-		if (!hasDefaultStripeAccount) {
-			new Give.modal.GiveNoticeAlert({
-				type: 'warning',
-				modalContent: {
-					title: __('Select Default Stripe Account', 'give'),
-					desc: __('We notice that you want to accept donation with other then global default Stripe Account but you did not selected Stripe account.', 'give'),
-				},
-			}).render();
-
-			// Open Stripe settings.
-			if ('stripe_form_account_options' !== Give.fn.getParameterByName('give_tab')) {
-				document.querySelector('a[href="#stripe_form_account_options"]').click();
-			}
-
+		if ( hasDefaultStripeAccount ) {
 			return;
 		}
 
-		form.submit()
+		event.preventDefault();
+		new Give.modal.GiveNoticeAlert({
+			 type: 'warning',
+			 modalContent: {
+				 title: __('Select Default Stripe Account', 'give'),
+				 desc: __('We notice that you want to accept donation with other then global default Stripe Account but you did not selected Stripe account.', 'give'),
+			 },
+		 }).render();
+
+		 // Open Stripe settings.
+		 if ('stripe_form_account_options' !== Give.fn.getParameterByName('give_tab')) {
+			 document.querySelector('a[href="#stripe_form_account_options"]').click();
+		 }
 	});
 });
