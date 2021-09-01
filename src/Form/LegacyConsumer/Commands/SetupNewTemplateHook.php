940bf3794989c12d5c462057c0ff47ff5e842a05
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2021-09-01 17:56:57 +0530

    feature: add filter to register custom rendering logic

--- src/Form/LegacyConsumer/Commands/SetupNewTemplateHook.php
+++ src/Form/LegacyConsumer/Commands/SetupNewTemplateHook.php
@@ -3,6 +3,7 @@
 namespace Give\Form\LegacyConsumer\Commands;
 
 use Give\Form\LegacyConsumer\FieldView;
+use Give\Framework\FieldsAPI\Field;
 use Give\Framework\FieldsAPI\Group;
 
 /**
@@ -21,12 +22,27 @@ class SetupNewTemplateHook implements HookCommandInterface {
 		// On the old hook, run the new hook and render the fields.
 		add_action(
 			"give_$hook",
-			static function ( $formID ) use ( $hook ) {
+			static function ( $formId ) use ( $hook ) {
 				$collection = Group::make( $hook );
-				do_action( "give_fields_$hook", $collection, $formID );
+				do_action( "give_fields_$hook", $collection, $formId );
 				$collection->walk(
-					static function ( $node ) use ( $formID ) {
-						FieldView::render( $node, $formID );
+					static function ( $node ) use ( $formId ) {
+
+						/**
+						 * Use this filter to render custom field which does not exist in field api.
+						 *
+						 * @unreleased
+						 *
+						 * @param Field $field
+						 * @param int $formId
+						 */
+						$html = apply_filters( 'give_fields_field_view', '', $node, $formId );
+						if( $html ){
+							echo $html;
+							return;
+						}
+
+						FieldView::render( $node, $formId );
 					}
 				);
 			}
