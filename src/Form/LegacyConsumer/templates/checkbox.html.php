8e7845fef103ce5073b2455a0dc8c02f63e44fd3
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2021-09-02 23:16:30 +0530

    refactor: do not add id to checkbox label

--- src/Form/LegacyConsumer/templates/checkbox.html.php
+++ src/Form/LegacyConsumer/templates/checkbox.html.php
@@ -1,10 +1,8 @@
 <?php /** @var Give\Framework\FieldsAPI\Checkbox $field */ ?>
-<?php /** @var string $fieldIdAttribute */ ?>
-<label class="give-label" for="<?php echo $fieldIdAttribute; ?>">
+<label class="give-label">
 	<input
 		type="checkbox"
 		name="<?php echo $field->getName(); ?>"
-		id="<?php echo $fieldIdAttribute; ?>"
 		<?php echo $field->isRequired() ? 'required' : ''; ?>
 		<?php echo $field->isChecked() ? 'checked' : ''; ?>
 		<?php echo $field->isReadOnly() ? 'readonly' : ''; ?>
