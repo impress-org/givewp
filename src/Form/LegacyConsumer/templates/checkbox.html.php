a664fafacc0e850de0e4da2f0c4f0f5af302f82b
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2021-09-02 21:45:55 +0530

    refactor: define label explicity for radio and checkbox

--- src/Form/LegacyConsumer/templates/checkbox.html.php
+++ src/Form/LegacyConsumer/templates/checkbox.html.php
@@ -1,11 +1,13 @@
 <?php /** @var Give\Framework\FieldsAPI\Checkbox $field */ ?>
-<label class="give-label">
-	<input
-		type="checkbox"
-		name="<?php echo $field->getName(); ?>"
-		<?php echo $field->isRequired() ? 'required' : ''; ?>
-		<?php echo $field->isChecked() ? 'checked' : ''; ?>
-		<?php echo $field->isReadOnly() ? 'readonly' : ''; ?>
-	>
+<?php /** @var string $fieldIdAttribute */ ?>
+<input
+	type="checkbox"
+	id="<?php echo $fieldIdAttribute; ?>"
+	name="<?php echo $field->getName(); ?>"
+	<?php echo $field->isRequired() ? 'required' : ''; ?>
+	<?php echo $field->isChecked() ? 'checked' : ''; ?>
+	<?php echo $field->isReadOnly() ? 'readonly' : ''; ?>
+>
+<label class="give-label" for="<?php echo $fieldIdAttribute; ?>">
 	<?php echo $field->getLabel(); ?>
 </label>
