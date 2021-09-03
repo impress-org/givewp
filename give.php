5a309b382440454753697376e7ce745a4b1e5fab
Jason Adams <jason.adams@impress.org>
2021-09-03 09:55:01 -0700

    chore: prepare for 2.13.4 release

--- give.php
+++ give.php
@@ -5,7 +5,7 @@
  * Description: The most robust, flexible, and intuitive way to accept donations on WordPress.
  * Author: GiveWP
  * Author URI: https://givewp.com/
- * Version: 2.13.3
+ * Version: 2.13.4
  * Text Domain: give
  * Domain Path: /languages
  *
@@ -267,7 +267,7 @@ final class Give {
 	private function setup_constants() {
 		// Plugin version.
 		if ( ! defined( 'GIVE_VERSION' ) ) {
-			define( 'GIVE_VERSION', '2.13.3' );
+			define( 'GIVE_VERSION', '2.13.4' );
 		}
 
 		// Plugin Root File.
