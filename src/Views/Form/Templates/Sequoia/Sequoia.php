8d3d9928645d38be9667c2e57351f2abb842cf3d
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2021-09-03 18:14:06 +0530

    fix: remove radio and checkbox border styling

--- src/Views/Form/Templates/Sequoia/Sequoia.php
+++ src/Views/Form/Templates/Sequoia/Sequoia.php
@@ -150,9 +150,7 @@ class Sequoia extends Template implements Hookable, Scriptable {
 			#give-recurring-form .form-row select.required:focus,
 			form.give-form .form-row select.required:focus,
 			.give-select:focus,
-			.give-input-field-wrapper.has-focus,
-			[data-field-type="radio"] label.active::before,
-			[data-field-type="checkbox"] label.active::before + [data-field-type="checkbox"] label.active:focus{
+			.give-input-field-wrapper.has-focus{
 				border-color: %1$s !important;
 			}
 			',
