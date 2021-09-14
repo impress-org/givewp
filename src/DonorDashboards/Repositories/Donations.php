9bbde72b0575ef8795782798f51ffa0a2f9a6f76
alaca <ante.laca@gmail.com>
2021-09-14 13:07:18 +0200

    refactor: replace `<br>` tags with spaces

--- src/DonorDashboards/Repositories/Donations.php
+++ src/DonorDashboards/Repositories/Donations.php
@@ -245,7 +245,8 @@ class Donations {
 				}
 
 				$label = html_entity_decode( wp_strip_all_tags( $lineItem->label ) );
-				$value = $lineItem->id === 'paymentStatus' ? $this->getFormattedStatus( $payment->status ) : html_entity_decode( wp_strip_all_tags( $lineItem->value ) );
+				$value = str_replace( '<br>', ' ', $lineItem->value );
+				$value = $lineItem->id === 'paymentStatus' ? $this->getFormattedStatus( $payment->status ) : html_entity_decode( wp_strip_all_tags( $value ) );
 
 				$receiptArr[ $sectionIndex ]['lineItems'][] = [
 					'class' => $detailRowClass,
