cc529fad1834fcd331cdad8efaafbf8b41ccbc9a
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2021-09-14 22:17:03 +0530

    feature: implement conditional visibilty in legacy consumer

---
+++ src/Form/LegacyConsumer/resources/js/conditinal-fields.js
@@ -0,0 +1,52 @@
+window.addEventListener('load', () => {
+	/**
+	 * Handle fields visibility
+	 * @unreleased
+	 */
+	function handleVisibility(donationForm) {
+		donationForm.querySelectorAll('[data-field-visibility-conditions]').forEach(function (field) {
+			const fieldWrapper = field.closest('.form-row');
+			const visibilityConditions = JSON.parse(field.getAttribute('data-field-visibility-conditions'));
+			let visible = false;
+			for (const {type, field, value} of visibilityConditions) {
+				const inputs = donationForm.querySelectorAll(`[name="${field}"]`);
+
+				if (!inputs) {
+					return;
+				}
+
+				inputs.forEach((input) => {
+					const fieldType = input.getAttribute('type');
+
+					if (fieldType && (fieldType === 'radio' || fieldType === 'checkbox')) {
+						if (input.checked && input.value === value) {
+							visible = true;
+						}
+					} else if (input.value === value) {
+						visible = true;
+					}
+				});
+			}
+
+			visible ?
+				fieldWrapper.classList.remove('give-hidden') :
+				fieldWrapper.classList.add('give-hidden');
+		});
+	}
+
+	/**
+	 * @unreleased
+	 * @param event
+	 */
+	function handleVisibilityOnChangeHandler(event) {
+		handleVisibility(event.target.closest('.give-form') );
+	}
+
+
+	document.querySelectorAll('form.give-form')
+		.forEach(function (donationForm) {
+			handleVisibility(donationForm);
+			donationForm.querySelectorAll('input, select, textarea')
+				.forEach(field => field.addEventListener('change', handleVisibilityOnChangeHandler));
+		});
+});
