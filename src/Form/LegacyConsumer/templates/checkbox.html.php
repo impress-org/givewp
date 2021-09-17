01c10dfc8b2c55f1d6c13d119474723568908165
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2021-09-17 18:55:40 +0530

    fix: set checkbox value if defined

--- src/Form/LegacyConsumer/templates/checkbox.html.php
+++ src/Form/LegacyConsumer/templates/checkbox.html.php
@@ -6,6 +6,7 @@
 		<?php echo $field->isRequired() ? 'required' : ''; ?>
 		<?php echo $field->isChecked() ? 'checked' : ''; ?>
 		<?php echo $field->isReadOnly() ? 'readonly' : ''; ?>
+		<?php echo $field->getDefaultValue() ? "value=\"{$field->getDefaultValue()}\"" : ''; ?>
 		<?php include dirname( __FILE__ ) . '/conditional-visibility-attribute.html.php'; ?>
 	>
 	<?php echo $field->getLabel(); ?>
