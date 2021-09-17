92e484eb071081833383f0517d36916ba3650b86
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2021-09-17 19:37:38 +0530

    fix: check checkbox by default

--- src/Form/LegacyConsumer/templates/checkbox.html.php
+++ src/Form/LegacyConsumer/templates/checkbox.html.php
@@ -1,13 +1,40 @@
 <?php /** @var Give\Framework\FieldsAPI\Checkbox $field */ ?>
-<label class="give-label">
-	<input
-		type="checkbox"
-		name="<?php echo $field->getName(); ?>"
-		<?php echo $field->isRequired() ? 'required' : ''; ?>
-		<?php echo $field->isChecked() ? 'checked' : ''; ?>
-		<?php echo $field->isReadOnly() ? 'readonly' : ''; ?>
-		<?php echo $field->getDefaultValue() ? "value=\"{$field->getDefaultValue()}\"" : ''; ?>
-		<?php include dirname( __FILE__ ) . '/conditional-visibility-attribute.html.php'; ?>
-	>
-	<?php echo $field->getLabel(); ?>
-</label>
+<?php /** @var string $fieldIdAttribute */ ?>
+
+<?php if ( $options = $field->getOptions() ): ?>
+	<fieldset <?php include dirname( __FILE__ ) . '/conditional-visibility-attribute.html.php'; ?>>
+		<legend class="screen-reader-text">
+			<?php include plugin_dir_path( __FILE__ ) . '/label-content.html.php'; ?>
+		</legend>
+		<div class="give-label" aria-hidden="true">
+			<?php include plugin_dir_path( __FILE__ ) . '/label-content.html.php'; ?>
+		</div>
+		<?php foreach ( $options as $index => $option ) : ?>
+			<?php $id = $fieldIdAttribute . '-' . $index; ?>
+			<label class="give-label" for="<?php echo $id; ?>">
+				<input
+					type="checkbox"
+					name="<?php echo $field->getName(); ?>[]"
+					id="<?php echo $id; ?>"
+					<?php echo $field->isRequired() ? 'required' : ''; ?>
+					<?php echo in_array( $option->getValue(), $field->getDefaultValue() ) ? 'checked' : ''; ?>
+					value="<?php echo $option->getValue(); ?>"
+				>
+				<?php echo $option->getLabel() ?: $option->getValue(); ?>
+			</label>
+		<?php endforeach; ?>
+	</fieldset>
+<?php else: ?>
+	<label class="give-label">
+		<input
+			type="checkbox"
+			name="<?php echo $field->getName(); ?>"
+			<?php echo $field->isRequired() ? 'required' : ''; ?>
+			<?php echo $field->isChecked() ? 'checked' : ''; ?>
+			<?php echo $field->isReadOnly() ? 'readonly' : ''; ?>
+			<?php echo $field->getDefaultValue() ? "value=\"{$field->getDefaultValue()}\"" : ''; ?>
+			<?php include dirname( __FILE__ ) . '/conditional-visibility-attribute.html.php'; ?>
+		>
+		<?php echo $field->getLabel(); ?>
+	</label>
+<?php endif; ?>
