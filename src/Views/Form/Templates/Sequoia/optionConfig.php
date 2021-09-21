5c6ebf374ad7e7dacc977d4c4da53eebc22e442d
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2021-09-21 12:54:00 +0530

    format: use ?: operator

--- src/Views/Form/Templates/Sequoia/optionConfig.php
+++ src/Views/Form/Templates/Sequoia/optionConfig.php
@@ -8,7 +8,7 @@ $formInfo = get_post( FrontendFormTemplateUtils::getFormId() );
 
 // Setup dynamic defaults
 $introHeadline    = ( ! $formInfo->post_title || 'post-new.php' === $pagenow ) ? __( 'Support Our Cause', 'give' ) : $formInfo->post_title;
-$introDescription = $formInfo->post_excerpt ? $formInfo->post_excerpt : __( 'Help our organization by donating today! All donations go directly to making a difference for our cause.', 'give' );
+$introDescription = $formInfo->post_excerpt ?: esc_html__( 'Help our organization by donating today! All donations go directly to making a difference for our cause.', 'give' );
 
 return [
 	'introduction'        => [
