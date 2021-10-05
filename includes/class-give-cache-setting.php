e7d7cec30f55a4133fb1943e1eb3005836177191
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2021-10-05 11:46:09 +0530

    fix: do not edit label in Give_Cache_Setting class

--- includes/class-give-cache-setting.php
+++ includes/class-give-cache-setting.php
@@ -250,16 +250,7 @@ class Give_Cache_Setting {
 		 * @since 2.4.0
 		 */
 		$gateways = apply_filters( 'give_register_gateway', $gateways );
-
-		// Replace payment gateway checkout label with admin defined checkout label.
-		if ( ! empty( $this->settings['give_settings']['gateways_label'] ) ) {
-			foreach ( $this->settings['give_settings']['gateways_label'] as $gatewayId => $checkoutLabel ) {
-				if ( $checkoutLabel ) {
-					$gateways[ $gatewayId ]['checkout_label'] = $checkoutLabel;
-				}
-			}
-		}
-
+		
 		$this->settings['gateways'] = $gateways;
 	}
 
