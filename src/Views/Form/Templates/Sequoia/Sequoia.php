15fd5c6961baf922e6cf17db9afc4a08dd356c30
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2021-10-19 23:39:44 +0530

    fix: load only in use google font weights

--- src/Views/Form/Templates/Sequoia/Sequoia.php
+++ src/Views/Form/Templates/Sequoia/Sequoia.php
@@ -78,7 +78,7 @@ class Sequoia extends Template implements Hookable, Scriptable {
 		if ( $isGoogleFontEnabled ) {
 			wp_enqueue_style(
 				'give-google-font-montserrat',
-				'https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap',
+				'https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700&display=swap',
 				[],
 				GIVE_VERSION
 			);
