26ca8c7e9ca62ac22496863163b7ffe3e179e94b
Jon Waldstein <Jpwaldstein@gmail.com>
2021-10-25 10:17:29 -0400

    feature: add missing log statment in create payment

--- src/PaymentGateways/Actions/CreatePaymentAction.php
+++ src/PaymentGateways/Actions/CreatePaymentAction.php
@@ -2,6 +2,7 @@
 
 namespace Give\PaymentGateways\Actions;
 
+use Give\Log\Log;
 use Give\PaymentGateways\DataTransferObjects\FormData;
 
 /**
@@ -22,6 +23,7 @@ class CreatePaymentAction {
 
 		// If errors are present, send the user back to the donation page, so they can be corrected
 		if (! $payment ) {
+			Log::error( esc_html__( 'Payment Error', 'give' ), $formData->toPaymentArray() );
 			give_send_back_to_checkout( '?payment-mode=' . $formData->gateway );
 		}
 
