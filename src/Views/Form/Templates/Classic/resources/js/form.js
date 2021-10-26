ce3d668f879771410fad8f658cfd83d851a523a1
Nathan Knowler <nathan@givewp.com>
2021-10-26 14:40:51 -0500

    feature: style donation button currency symbol

--- src/Views/Form/Templates/Classic/resources/js/form.js
+++ src/Views/Form/Templates/Classic/resources/js/form.js
@@ -1,3 +1,6 @@
+/** @jsx h */
+/** @jsxFragment h */
+import h from 'vhtml';
 import {domIsReady, insertAfter, removeNode} from './not-jquery.js';
 
 // Transforms document for classic template
@@ -14,6 +17,15 @@ function transform() {
 	)
 
 	document.querySelector('.give-personal-info-section legend:first-of-type').textContent = classicTemplateOptions.donor_information.headline;
+
+	document.querySelectorAll('.give-donation-level-btn:not(.give-btn-level-custom)').forEach(node => {
+		node.innerHTML = (
+			<span class="give-formatted-currency">
+				<span class="give-currency-symbol">$</span>
+				<span>{node.getAttribute('value')}</span>
+			</span>
+		);
+	});
 }
 
 domIsReady(transform);
