20bf2428543e2b7600ee0f38f54e317fb0f536eb
Nathan Knowler <nathan@givewp.com>
2021-10-26 06:24:59 -0500

    feature: add borders + spacing in stats panel

--- src/Views/Form/Templates/Classic/resources/views/header.php
+++ src/Views/Form/Templates/Classic/resources/views/header.php
@@ -16,9 +16,15 @@
 	<?php endif; ?>
 	<aside class="give-form-stats-panel">
 		<ul class="give-form-stats-panel-list">
-			<li>$0 raised</li>
-			<li>50 donations</li>
-			<li>$10,000 goal</li>
+			<li class="give-form-stats-panel-stat">
+				<span class="give-form-stats-panel-stat-number">$0</span> raised
+			</li>
+			<li class="give-form-stats-panel-stat">
+				<span class="give-form-stats-panel-stat-number">50</span> donations
+			</li>
+			<li class="give-form-stats-panel-stat">
+				<span class="give-form-stats-panel-stat-number">$10,000</span> goal
+			</li>
 			<li class="give-form-goal-progress">
 				<meter class="give-form-goal-progress-meter" min="0" max="1000000" value="200000">$20,000 of $1,000,000 goal</meter>
 			</li>
