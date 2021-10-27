616a59651bcdf8a5bbbe8407c7871b4db254873d
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2021-10-27 10:26:41 +0530

    fix: use existing value objects

--- src/PaymentGateways/DataTransferObjects/FormData.php
+++ src/PaymentGateways/DataTransferObjects/FormData.php
@@ -4,6 +4,7 @@ namespace Give\PaymentGateways\DataTransferObjects;
 
 use Give\ValueObjects\Address;
 use Give\ValueObjects\CardInfo;
+use Give\ValueObjects\DonorInfo;
 
 /**
  * Class FormData
@@ -25,10 +26,6 @@ class FormData {
 	/**
 	 * @var string
 	 */
-	public $donorEmail;
-	/**
-	 * @var string
-	 */
 	public $purchaseKey;
 	/**
 	 * @var string
@@ -41,7 +38,7 @@ class FormData {
 	/**
 	 * @var string
 	 */
-	public $gateway;
+	public $paymentGateway;
 	/**
 	 * @var string
 	 */
@@ -102,6 +99,10 @@ class FormData {
 	 * @var Address
 	 */
 	public $billingAddress;
+	/**
+	 * @var DonorInfo
+	 */
+	public $donorInfo;
 
 	/**
 	 * Convert data from request into DTO
@@ -113,43 +114,52 @@ class FormData {
 	public static function fromRequest( array $request ) {
 		$self = new static();
 
-		$self->price = $request['price'];
-		$self->date = $request['date'];
-		$self->donorEmail = $request['user_email'];
-		$self->purchaseKey = $request['purchase_key'];
-		$self->currency = give_get_currency( $request['post_data']['give-form-id'], $request );
-		$self->userInfo = $request['user_info'];
-		$self->postData = $request['post_data'];
-		$self->honeypot = $request['post_data']['give-honeypot'];
-		$self->formTitle = $request['post_data']['give-form-title'];
-		$self->formId = (int) $request['post_data']['give-form-id'];
-		$self->priceId = isset( $request['post_data']['give-price-id'] ) ? $request['post_data']['give-price-id'] : '';
-		$self->formIdPrefix = $request['post_data']['give-form-id-prefix'];
-		$self->currentUrl = $request['post_data']['give-current-url'];
-		$self->formMinimum = $request['post_data']['give-form-minimum'];
-		$self->formMaximum = $request['post_data']['give-form-maximum'];
-		$self->formHash = $request['post_data']['give-form-hash'];
-		$self->loggedInOnly = $request['post_data']['give-logged-in-only'];
-		$self->amount = $request['post_data']['give-amount'];
-		$self->gateway = $request['post_data']['give-gateway'];
-		$self->gatewayNonce = $request['gateway_nonce'];
-		$self->cardInfo = CardInfo::fromArray( [
-			'name' => $request['card_info']['card_name'],
-			'cvc' => $request['card_info']['card_cvc'],
+		$billingAddress = [
+			'line1'      => $request['card_info']['card_address'],
+			'line2'      => $request['card_info']['card_address_2'],
+			'city'       => $request['card_info']['card_city'],
+			'state'      => $request['card_info']['card_state'],
+			'country'    => $request['card_info']['card_country'],
+			'postalCode' => $request['card_info']['card_zip'],
+		];
+
+		$self->price          = $request['price'];
+		$self->date           = $request['date'];
+		$self->purchaseKey    = $request['purchase_key'];
+		$self->currency       = give_get_currency( $request['post_data']['give-form-id'], $request );
+		$self->userInfo       = $request['user_info'];
+		$self->postData       = $request['post_data'];
+		$self->honeypot       = $request['post_data']['give-honeypot'];
+		$self->formTitle      = $request['post_data']['give-form-title'];
+		$self->formId         = (int) $request['post_data']['give-form-id'];
+		$self->priceId        = isset( $request['post_data']['give-price-id'] ) ? $request['post_data']['give-price-id'] : '';
+		$self->formIdPrefix   = $request['post_data']['give-form-id-prefix'];
+		$self->currentUrl     = $request['post_data']['give-current-url'];
+		$self->formMinimum    = $request['post_data']['give-form-minimum'];
+		$self->formMaximum    = $request['post_data']['give-form-maximum'];
+		$self->formHash       = $request['post_data']['give-form-hash'];
+		$self->loggedInOnly   = $request['post_data']['give-logged-in-only'];
+		$self->amount         = $request['post_data']['give-amount'];
+		$self->paymentGateway = $request['post_data']['give-gateway'];
+		$self->gatewayNonce   = $request['gateway_nonce'];
+		$self->donorInfo      = DonorInfo::fromArray( [
+			'wpUserId'  => $request['user_info']['id'],
+			'firstName' => $request['user_info']['first_name'],
+			'lastName'  => $request['user_info']['last_name'],
+			'email'     => $request['user_info']['email'],
+			'honorific' => ! empty( $payment_data['user_info']['title'] ) ? $payment_data['user_info']['title'] : '',
+			'address'   => $billingAddress
+		] );
+		$self->cardInfo       = CardInfo::fromArray( [
+			'name'     => $request['card_info']['card_name'],
+			'cvc'      => $request['card_info']['card_cvc'],
 			'expMonth' => $request['card_info']['card_exp_month'],
-			'expYear' => $request['card_info']['card_exp_year'],
-			'number' => $request['card_info']['card_number'],
-			'address' => $request['card_info']['card_address'],
+			'expYear'  => $request['card_info']['card_exp_year'],
+			'number'   => $request['card_info']['card_number'],
+			'address'  => $billingAddress,
 		] );
 
-		$self->billingAddress = Address::fromArray( [
-			'line1' => $request['card_info']['card_address'],
-			'line2' => $request['card_info']['card_address_2'],
-			'city' => $request['card_info']['card_city'],
-			'state' => $request['card_info']['card_state'],
-			'country' => $request['card_info']['card_country'],
-			'postalCode' => $request['card_info']['card_zip'],
-		] );
+		$self->billingAddress = Address::fromArray( $billingAddress );
 
 		return $self;
 	}
@@ -161,16 +171,16 @@ class FormData {
 	 */
 	public function toPaymentArray() {
 		return [
-			'price' => $this->price,
+			'price'           => $this->price,
 			'give_form_title' => $this->formTitle,
-			'give_form_id' => $this->formId,
-			'give_price_id' => $this->priceId,
-			'date' => $this->date,
-			'user_email' => $this->donorEmail,
-			'purchase_key' => $this->purchaseKey,
-			'currency' => $this->currency,
-			'user_info' => $this->userInfo,
-			'status' => 'pending',
+			'give_form_id'    => $this->formId,
+			'give_price_id'   => $this->priceId,
+			'date'            => $this->date,
+			'user_email'      => $this->donorInfo->email,
+			'purchase_key'    => $this->purchaseKey,
+			'currency'        => $this->currency,
+			'user_info'       => $this->userInfo,
+			'status'          => 'pending',
 		];
 	}
 }
