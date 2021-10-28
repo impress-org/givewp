28ba60e858ac18538cb172b68b064b8846ad2991
Kyle B. Johnson <kyle.johnson@impress.org>
2021-10-28 11:18:32 -0400

    feature: Restrict donation summary to Sequoia template.

--- src/DonationSummary/SummaryView.php
+++ src/DonationSummary/SummaryView.php
@@ -14,11 +14,27 @@ class SummaryView {
      */
     public function __invoke( $formID ) {
         $this->formID = $formID;
-        $this->render();
+
+        if( 'sequoia' === $this->getFormTemplate() ) {
+            if( $this->isDonationSummaryEnabled() ) {
+                $this->render();
+            }
+        }
     }
 
     public function render() {
         include 'resources/views/summary.php';
+    }
+
+    protected function getFormTemplate() {
+        return Give()->form_meta->get_meta( $this->formID, '_give_form_template', $single = true );
+    }
+
+    protected function isDonationSummaryEnabled() {
+        $templateSettings = Give()->form_meta->get_meta( $this->formID, '_give_sequoia_form_template_settings', $single = true );
+        return isset( $templateSettings[ 'donation_summary' ] )
+            && isset( $templateSettings[ 'donation_summary' ][ 'enabled' ] )
+            && give_is_setting_enabled( $templateSettings[ 'donation_summary' ][ 'enabled' ] );
     }
 
     protected function isFeeRecoveryEnabled() {
