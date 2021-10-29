59033e4ff8ba7ece84182f7139626e45755850a2
Kyle B. Johnson <kyle.johnson@impress.org>
2021-10-29 11:53:54 -0400

    chore: Restore removed test and remove unused test.

--- tests/unit/tests/DonationSummary/SummaryViewTest.php
+++ tests/unit/tests/DonationSummary/SummaryViewTest.php
@@ -4,13 +4,17 @@ use Give\DonationSummary\SummaryView;
 
 final class SummaryViewTest extends Give_Unit_Test_Case {
 
-    public function test_get_form_template() {
+    public function test_get_form_template_location() {
 
         $view = self::create_form_summary_view([
-            '_give_form_template' => 'sequoia',
+            '_give_sequoia_form_template_settings' => [
+                'donation_summary' => [
+                    'location' => 'give_donation_form_before_submit',
+                ],
+            ],
         ]);
 
-        $this->assertEquals( 'sequoia', $view->getFormTemplate() );
+        $this->assertEquals( 'give_donation_form_before_submit', $view->getFormTemplateLocation() );
     }
 
     public function test_is_donation_summary_enabled() {
