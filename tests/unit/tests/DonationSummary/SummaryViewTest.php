791324be2802214435dd0bbe421cab56ce7d7ad4
Kyle B. Johnson <kyle.johnson@impress.org>
2021-10-29 12:03:13 -0400

    chore: Update tests to account for function re-use.

--- tests/unit/tests/DonationSummary/SummaryViewTest.php
+++ tests/unit/tests/DonationSummary/SummaryViewTest.php
@@ -43,6 +43,18 @@ final class SummaryViewTest extends Give_Unit_Test_Case {
     }
 
     protected static function create_form_summary_view( $meta ) {
+        $meta[ '_give_form_template' ] = 'sequoia';
+
+        // A backwards compatibility process requies that these be set.
+        $meta[ '_give_sequoia_form_template_settings' ] = array_merge([
+            'introduction' => [],
+            'payment_amount' => [],
+            'visual_appearance' => [
+                'decimals_enabled' => 'disabled',
+                'primary_color' => '#000',
+            ]
+        ], $meta[ '_give_sequoia_form_template_settings' ] );
+
         $form = Give_Helper_Form::create_simple_form( compact( 'meta' ));
 
         $view = new SummaryView();
