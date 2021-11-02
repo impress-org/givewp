8ad1630b654245f3d29fc9c596dd2f4682b27c54
alaca <ante.laca@gmail.com>
2021-11-02 16:40:37 +0100

    refactor: update receipt template

--- src/Views/Form/Templates/Classic/resources/views/receipt.php
+++ src/Views/Form/Templates/Classic/resources/views/receipt.php
@@ -9,110 +9,126 @@ use Give\Views\IframeContentView;
 use Give\Helpers\Form\Template as FormTemplateUtils;
 use Give_Payment as Payment;
 
-$donationSessionAccessor = new DonationAccessor();
-$donation                = new Payment( $donationSessionAccessor->getDonationId() );
-$options                 = FormTemplateUtils::getOptions();
+$donation = new Payment( ( new DonationAccessor() )->getDonationId() );
+$options  = FormTemplateUtils::getOptions();
 
-/* @var Classic $sequoiaTemplate */
-$sequoiaTemplate = Give()->templates->getTemplate();
-$receipt         = $sequoiaTemplate->getReceiptDetails( $donation->ID );
+/* @var Classic $template */
+$template = Give()->templates->getTemplate();
+$receipt  = $template->getReceiptDetails( $donation->ID );
 
 /* @var LineItem|null $pdfReceiptDownloadLinkDetailItem */
 $pdfReceiptLinkDetailItem = null;
 
 ob_start();
 ?>
-<div class="give-receipt-wrap give-embed-receipt">
-	<div class="give-section receipt">
-		<h2 class="headline">
-			<?php echo $receipt->heading; ?>
-		</h2>
-		<p class="message">
-			<?php echo $receipt->message; ?>
-		</p>
-
-		<?php if ( isset( $options[ 'donation_receipt' ][ 'social_sharing' ] ) && $options[ 'donation_receipt' ][ 'social_sharing' ] === 'enabled' ) : ?>
-			<div class="social-sharing">
-				<p class="instruction">
-					<?php echo esc_html( $options[ 'donation_receipt' ][ 'sharing_instructions' ] ); ?>
-				</p>
-				<div class="btn-row">
-					<!-- Use inline onclick listener to avoid popup blockers -->
-					<button class="give-btn social-btn facebook-btn" onclick="GiveClassicTemplate.share(this);">
-						<?php esc_html_e( 'Share on Facebook', 'give' ); ?><i class="fab fa-facebook"></i>
-					</button>
-					<!-- Use inline onclick listener to avoid popup blockers -->
-					<button class="give-btn social-btn twitter-btn" onclick="GiveClassicTemplate.share(this);">
-						<?php esc_html_e( 'Share on Twitter', 'give' ); ?><i class="fab fa-twitter"></i>
-					</button>
+	<div class="give-receipt-wrap give-embed-receipt">
+		<div class="give-section receipt">
+			<div class="give-form-header">
+				<div class="give-form-header-top-wrap">
+					<aside class="give-form-secure-badge">
+						<svg class="give-form-secure-icon">
+							<use href="#give-icon-checkmark"/>
+						</svg>
+						<?php _e( 'Success', 'give' ); ?>!
+					</aside>
+					<h1 class="give-form-title">
+						<?= $options[ 'donation_receipt' ][ 'headline' ]; ?>
+					</h1>
+					<p class="give-form-description">
+						<?= $options[ 'donation_receipt' ][ 'description' ]; ?>
+					</p>
 				</div>
 			</div>
-		<?php endif; ?>
 
-		<?php
-		/* @var Section $section */
-		foreach ( $receipt as $section ) {
-			// Continue if section does not have line items.
-			if ( ! $section->getLineItems() ) {
-				continue;
-			}
+			<h2 class="headline">
+				<?php echo $receipt->heading; ?>
+			</h2>
+			<p class="message">
+				<?php echo $receipt->message; ?>
+			</p>
 
-			if ( 'PDFReceipt' === $section->id ) {
-				$pdfReceiptLinkDetailItem = $section[ 'receiptLink' ];
-				continue;
-			}
+			<?php if ( isset( $options[ 'donation_receipt' ][ 'social_sharing' ] ) && $options[ 'donation_receipt' ][ 'social_sharing' ] === 'enabled' ) : ?>
+				<div class="social-sharing">
+					<p class="instruction">
+						<?php echo esc_html( $options[ 'donation_receipt' ][ 'sharing_instructions' ] ); ?>
+					</p>
+					<div class="btn-row">
+						<!-- Use inline onclick listener to avoid popup blockers -->
+						<button class="give-btn social-btn facebook-btn" onclick="GiveClassicTemplate.share(this);">
+							<?php esc_html_e( 'Share on Facebook', 'give' ); ?><i class="fab fa-facebook"></i>
+						</button>
+						<!-- Use inline onclick listener to avoid popup blockers -->
+						<button class="give-btn social-btn twitter-btn" onclick="GiveClassicTemplate.share(this);">
+							<?php esc_html_e( 'Share on Twitter', 'give' ); ?><i class="fab fa-twitter"></i>
+						</button>
+					</div>
+				</div>
+			<?php endif; ?>
 
-			echo '<div class="details">';
-			if ( $section->label ) {
-				printf( '<h3 class="headline">%1$s</h3>', $section->label );
-			}
-			echo '<div class="details-table">';
-
-			/* @var LineItem $lineItem */
-			foreach ( $section as $lineItem ) {
-				// Continue if line item does not have value.
-				if ( ! $lineItem->value ) {
+			<?php
+			/* @var Section $section */
+			foreach ( $receipt as $section ) {
+				// Continue if section does not have line items.
+				if ( ! $section->getLineItems() ) {
 					continue;
 				}
 
-				// This class is required to highlight total donation amount in receipt.
-				$detailRowClass = '';
-				if ( DonationReceipt::DONATIONSECTIONID === $section->id ) {
-					$detailRowClass = 'totalAmount' === $lineItem->id ? ' total' : '';
+				if ( 'PDFReceipt' === $section->id ) {
+					$pdfReceiptLinkDetailItem = $section[ 'receiptLink' ];
+					continue;
 				}
 
-				printf(
-					'<div class="details-row%1$s">%2$s<div class="detail">%3$s</div><div class="value">%4$s</div></div>',
-					$detailRowClass,
-					$lineItem->icon,
-					$lineItem->label,
-					$lineItem->value
-				);
+				echo '<div class="details">';
+				if ( $section->label ) {
+					printf( '<h3 class="headline">%1$s</h3>', $section->label );
+				}
+				echo '<div class="details-table">';
+
+				/* @var LineItem $lineItem */
+				foreach ( $section as $lineItem ) {
+					// Continue if line item does not have value.
+					if ( ! $lineItem->value ) {
+						continue;
+					}
+
+					// This class is required to highlight total donation amount in receipt.
+					$detailRowClass = '';
+					if ( DonationReceipt::DONATIONSECTIONID === $section->id ) {
+						$detailRowClass = 'totalAmount' === $lineItem->id ? ' total' : '';
+					}
+
+					printf(
+						'<div class="details-row%1$s">%2$s<div class="detail">%3$s</div><div class="value">%4$s</div></div>',
+						$detailRowClass,
+						$lineItem->icon,
+						$lineItem->label,
+						$lineItem->value
+					);
+				}
+				echo '</div>';
+				echo '</div>';
 			}
-			echo '</div>';
-			echo '</div>';
-		}
-		?>
+			?>
 
-		<?php if ( $pdfReceiptLinkDetailItem ) : ?>
-			<div class="give-btn download-btn">
-				<?php echo $pdfReceiptLinkDetailItem->value; ?>
+			<?php if ( $pdfReceiptLinkDetailItem ) : ?>
+				<div class="give-btn download-btn">
+					<?php echo $pdfReceiptLinkDetailItem->value; ?>
+				</div>
+			<?php endif; ?>
+
+		</div>
+		<div class="form-footer">1
+			<div class="secure-notice">
+				<i class="fas fa-lock"></i>
+				<?php _e( 'Secure Donation', 'give' ); ?>
 			</div>
-		<?php endif; ?>
-
-	</div>
-	<div class="form-footer">1
-		<div class="secure-notice">
-			<i class="fas fa-lock"></i>
-			<?php _e( 'Secure Donation', 'give' ); ?>
 		</div>
 	</div>
-</div>
 
 
 <?php
 $iframeView = new IframeContentView();
 
 echo $iframeView->setTitle( esc_html__( 'Donation Receipt', 'give' ) )
-				->setBody( ob_get_clean() )
-				->renderBody();
+                ->setBody( ob_get_clean() )
+                ->renderBody();
