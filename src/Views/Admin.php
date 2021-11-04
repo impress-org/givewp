5e6c36b54ae43ad29e0131bb7cc83422d6ab8b1a
Nathan Knowler <nathan@givewp.com>
2021-11-04 13:58:35 -0500

    refactor: remove temporary code

--- src/Views/Admin.php
+++ src/Views/Admin.php
@@ -44,34 +44,6 @@ class Admin {
 		// Load Reports page
 		$reports = new Admin\Pages\Reports();
 		$reports->init();
-
-		// ATTENTION: This is not for real, just for working on frontend
-		add_action('admin_menu', function () {
-			add_submenu_page(
-				'edit.php?post_type=give_forms',
-				esc_html__( 'Add-ons', 'give' ),
-				esc_html__( 'Add-ons', 'give' ),
-				'view_give_reports',
-				'give-addons',
-				function () {
-					echo '<div id="root"></div>';
-				}
-			);
-		});
-
-		add_action('admin_enqueue_scripts', function ($base) {
-			if ($base !== "give_forms_page_give-addons") return;
-
-			wp_enqueue_style('give-admin-addon-gallery', 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap');
-
-			wp_enqueue_script(
-				'give-admin-addon-gallery',
-				GIVE_PLUGIN_URL . 'assets/dist/js/admin-addon-gallery.js',
-				['wp-element', 'wp-i18n', 'wp-hooks'],
-				'0.0.0',
-				true
-			);
-		});
 	}
 
 }
