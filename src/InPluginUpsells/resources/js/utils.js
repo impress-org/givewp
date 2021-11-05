b0f13e31d92b22fce64ca02db7f5aed871c17697
Nathan Knowler <nathan@givewp.com>
2021-11-04 23:06:38 -0500

    doc: document how the transformStrong function works

--- src/InPluginUpsells/resources/js/utils.js
+++ src/InPluginUpsells/resources/js/utils.js
@@ -1,11 +1,17 @@
 /**
+ * Replaces double asterisks with `<strong>` tags.
+ *
  * @param {string} text
+ * @returns {string}
  */
 export function transformStrong(text) {
+    // Keep track of whether we're inside a <strong> tag.
     let startingTag = false;
 
     return text.replaceAll('**', () => {
+        // Reverse the starting tag state to determine the current state.
         startingTag = !startingTag;
+        // Return the appropriate tag.
         return startingTag ? '<strong>' : '</strong>';
     });
 }
