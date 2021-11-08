7c1ab8c3a10a3c73b843308e01007e3e9a528560
Jon Waldstein <Jpwaldstein@gmail.com>
2021-11-08 11:10:45 -0500

    docs: update LegacySubscriber description

--- src/Subscriptions/Models/LegacySubscriber.php
+++ src/Subscriptions/Models/LegacySubscriber.php
@@ -7,6 +7,10 @@ use Give_Recurring_Subscriber;
 /**
  * Class LegacySubscriber
  *
+ * This is a temporary class that extends a give-recurring concept of Give_Recurring_Subscriber.
+ * To avoid any issue with someone trying to use this without having give-recurring installed,
+ * there is a conditional to make sure the extended class exists.
+ *
  * @unreleased
  */
 if (class_exists('Give_Recurring_Subscriber')){
