91c402819d0e197fdff5e76766dcf79093c776d8
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2021-11-08 11:57:17 +0530

    format: improve code formatting

--- src/Helpers/Form/Template.php
+++ src/Helpers/Form/Template.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Give\Helpers\Form;
 
 use Give\Form\Template\LegacyFormSettingCompatibility;
@@ -7,46 +8,52 @@ use Give\Helpers\Form\Template\Utils\Frontend;
 /**
  * @since 2.7.0
  */
-class Template {
-	/**
-	 * This function will return selected form template for a specific form.
-	 *
-	 * @param int $formId Form id. Default value: check explanation in ./Utils.php:103
-	 *
-	 * @return string
-	 * @since 2.7.0
-	 */
-	public static function getActiveID( $formId = null ) {
-		return Give()->form_meta->get_meta( $formId ?: Frontend::getFormId(), '_give_form_template', true );
-	}
+class Template
+{
+    /**
+     * This function will return selected form template for a specific form.
+     *
+     * @since 2.7.0
+     *
+     * @param int $formId Form id. Default value: check explanation in ./Utils.php:103
+     *
+     * @return string
+     */
+    public static function getActiveID($formId = null)
+    {
+        return Give()->form_meta->get_meta($formId ?: Frontend::getFormId(), '_give_form_template', true);
+    }
 
-	/**
+    /**
      * Return saved form template settings
      *
-     * @param int $formId
+     * @since 2.7.0
+     *
      * @param string $templateId
      *
+     * @param int $formId
+     *
      * @return array
-     * @since 2.7.0
      */
-	public static function getOptions( $formId = null, $templateId = '' ) {
-		$formId   = $formId ?: Frontend::getFormId();
-		$template = $templateId ?: Give()->form_meta->get_meta( $formId, '_give_form_template', true );
-		$settings = Give()->form_meta->get_meta( $formId, "_give_{$template}_form_template_settings", true );
+    public static function getOptions($formId = null, $templateId = '')
+    {
+        $formId   = $formId ?: Frontend::getFormId();
+        $template = $templateId ?: Give()->form_meta->get_meta($formId, '_give_form_template', true);
+        $settings = Give()->form_meta->get_meta($formId, "_give_{$template}_form_template_settings", true);
 
-		$settings = $settings ?: [];
+        $settings = $settings ?: [];
 
-		// Exit if admin did not have any settings.
-		// New donation form does not have any setting saved.
-		if ( ! $settings ) {
-			return $settings;
-		}
+        // Exit if admin did not have any settings.
+        // New donation form does not have any setting saved.
+        if ( ! $settings) {
+            return $settings;
+        }
 
-		// Backward compatibility for migrated settings.
-		// 1. "Introduction -> Primary Color" move to "Visual Appearance -> Primary Color"
-		// 2. "Payment Amount -> Decimal amounts" move to "Visual Appearance -> Decimal amounts"
-		return self::handleOptionsBackwardCompatibility($settings);
-	}
+        // Backward compatibility for migrated settings.
+        // 1. "Introduction -> Primary Color" move to "Visual Appearance -> Primary Color"
+        // 2. "Payment Amount -> Decimal amounts" move to "Visual Appearance -> Decimal amounts"
+        return self::handleOptionsBackwardCompatibility($settings);
+    }
 
     /**
      * Save settings
@@ -58,21 +65,22 @@ class Template {
      *
      * @return bool
      */
-	public static function saveOptions( $formId, $settings ) {
-		$templateId = Give()->form_meta->get_meta( $formId, '_give_form_template', true );
-		$template = Give()->templates->getTemplate( $templateId );
-		$isUpdated = Give()->form_meta->update_meta( $formId, "_give_{$templateId}_form_template_settings", $settings );
+    public static function saveOptions($formId, $settings)
+    {
+        $templateId = Give()->form_meta->get_meta($formId, '_give_form_template', true);
+        $template   = Give()->templates->getTemplate($templateId);
+        $isUpdated  = Give()->form_meta->update_meta($formId, "_give_{$templateId}_form_template_settings", $settings);
 
-		/*
-		 * Below code save legacy setting which connected/mapped to form template setting.
-		 * Existing form render on basis of these settings if missed then required output will not generate from give_form_shortcode -> give_get_donation_form function.
-		 *
-		 * Note: We can remove legacy setting compatibility by returning anything except LegacyFormSettingCompatibility class object.
-		 */
-		$legacySettingHandler = new LegacyFormSettingCompatibility( $template );
-		$legacySettingHandler->save( $formId, $settings );
+        /*
+         * Below code save legacy setting which connected/mapped to form template setting.
+         * Existing form render on basis of these settings if missed then required output will not generate from give_form_shortcode -> give_get_donation_form function.
+         *
+         * Note: We can remove legacy setting compatibility by returning anything except LegacyFormSettingCompatibility class object.
+         */
+        $legacySettingHandler = new LegacyFormSettingCompatibility($template);
+        $legacySettingHandler->save($formId, $settings);
 
-		return $isUpdated;
+        return $isUpdated;
     }
 
     /**
@@ -98,5 +106,5 @@ class Template {
         }
 
         return $settings;
-	}
+    }
 }
