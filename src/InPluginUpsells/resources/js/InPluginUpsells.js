d6927e0561a091b03cd4f4f583c784818c884ccb
Nathan Knowler <nathan@givewp.com>
2021-11-09 12:57:51 -0600

    format: use consistent spacing

--- src/InPluginUpsells/resources/js/InPluginUpsells.js
+++ src/InPluginUpsells/resources/js/InPluginUpsells.js
@@ -7,42 +7,42 @@ import {PricingPlans} from './PricingPlans';
 import styles from './InPluginUpsells.module.css';
 
 export const InPluginUpsells = () => (
-	<Tabs as="article" className={styles.root}>
-		<div className={styles.header}>
-			<div className={styles.container}>
-				<h1 className="screen-reader-text">
-					{__('Give Add-ons Gallery', 'give')}
-				</h1>
+    <Tabs as="article" className={styles.root}>
+        <div className={styles.header}>
+            <div className={styles.container}>
+                <h1 className="screen-reader-text">
+                    {__('Give Add-ons Gallery', 'give')}
+                </h1>
                 <img src="" alt={__('GiveWP', 'give')} />
                 <aside
                     className={styles.sourceNotice}
                     dangerouslySetInnerHTML={{
                         __html: __('This page is loaded from <a href="https://givewp.com">GiveWP.com</a>')
                     }}
-                />
-				<TabList className={styles.tabs}>
-					<Tab>
-						{__('Must Have Add-ons', 'give')}
-					</Tab>
-					<Tab>
-						{__('View Pricing Plans', 'give')}
-					</Tab>
-					<Tab>
-						{__('Additional Add-ons', 'give')}
-					</Tab>
-				</TabList>
-			</div>
-		</div>
-		<TabPanels className={styles.container}>
-			<TabPanel>
-				<MustHaveAddons />
-			</TabPanel>
-			<TabPanel>
-				<PricingPlans />
-			</TabPanel>
-			<TabPanel>
-				<AdditionalAddons />
-			</TabPanel>
-		</TabPanels>
-	</Tabs>
+                    />
+                <TabList className={styles.tabs}>
+                    <Tab>
+                        {__('Must Have Add-ons', 'give')}
+                    </Tab>
+                    <Tab>
+                        {__('View Pricing Plans', 'give')}
+                    </Tab>
+                    <Tab>
+                        {__('Additional Add-ons', 'give')}
+                    </Tab>
+                </TabList>
+            </div>
+        </div>
+        <TabPanels className={styles.container}>
+            <TabPanel>
+                <MustHaveAddons />
+            </TabPanel>
+            <TabPanel>
+                <PricingPlans />
+            </TabPanel>
+            <TabPanel>
+                <AdditionalAddons />
+            </TabPanel>
+        </TabPanels>
+    </Tabs>
 );
