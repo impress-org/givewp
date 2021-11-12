76a7db8a54bb8c90d529fa2f5e67f035173834d9
Jon Waldstein <Jpwaldstein@gmail.com>
2021-11-12 09:35:30 -0500

    feature: add response functions file to composer

--- src/Framework/Http/Response/functions.php
+++ src/Framework/Http/Response/functions.php
@@ -3,26 +3,24 @@
 namespace Give\Framework\Http\Response;
 
 
-if (! function_exists('response')) {
-  /**
-   * Return a new response from the application.
-   *
-   * @unreleased
-   *
-   * @param  string  $content
-   * @param  int  $status
-   * @param  array  $headers
-   * @return Response|ResponseFactory
-   */
-    function response($content = '', $status = 200, array $headers = [])
-    {
-        /** @var ResponseFactory $factory */
-        $factory = give(ResponseFactory::class);
+/**
+ * Return a new response from the application.
+ *
+ * @unreleased
+ *
+ * @param  string  $content
+ * @param  int  $status
+ * @param  array  $headers
+ * @return Response|ResponseFactory
+ */
+function response($content = '', $status = 200, array $headers = [])
+{
+    /** @var ResponseFactory $factory */
+    $factory = give(ResponseFactory::class);
 
-        if (func_num_args() === 0) {
-            return $factory;
-        }
-
-        return $factory->make($content, $status, $headers);
+    if (func_num_args() === 0) {
+        return $factory;
     }
+
+    return $factory->make($content, $status, $headers);
 }
