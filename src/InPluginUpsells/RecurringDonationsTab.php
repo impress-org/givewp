f7aec9b05469459e2ad36a2938a793eb144ee729
alaca <ante.laca@gmail.com>
2021-11-15 13:39:50 +0100

    refactor: move render logic into settings page class

--- src/InPluginUpsells/RecurringDonationsTab.php
+++ src/InPluginUpsells/RecurringDonationsTab.php
@@ -6,8 +6,6 @@ namespace Give\InPluginUpsells;
  * @unreleased
  */
 class RecurringDonationsTab {
-    protected $containerId = 'give-in-plugin-upsells';
-
     /**
      * Load scripts
      */
@@ -32,14 +30,6 @@ class RecurringDonationsTab {
 			'GiveRecurringDonations',
             ['assetsUrl' => GIVE_PLUGIN_URL . 'assets/dist/']
         );
-    }
-
-    /**
-     * Render the React container
-     */
-    public function render() {
-        echo '<svg style="display: none"><path id="give-in-plugin-upsells-checkmark" d="M5.595 11.373.72 6.498a.75.75 0 0 1 0-1.06l1.06-1.061a.75.75 0 0 1 1.061 0L6.125 7.66 13.159.627a.75.75 0 0 1 1.06 0l1.061 1.06a.75.75 0 0 1 0 1.061l-8.625 8.625a.75.75 0 0 1-1.06 0Z" fill="currentColor"/></svg>';
-		echo "<div id=\"{$this->containerId}\"></div>";
     }
 
     /**
