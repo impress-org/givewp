cee69511ad85dd33839c0e65d22e1ee24cea8349
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2021-11-16 18:32:48 +0530

    doc: update function description

--- includes/class-give-license-handler.php
+++ includes/class-give-license-handler.php
@@ -353,7 +353,7 @@ if ( ! class_exists('Give_License') ) :
          * Get license information.
          *
          * @since  1.8.9
-         * @access public
+         * @unreleased log failed license api request information
          *
          * @param bool $response_in_array
          *
