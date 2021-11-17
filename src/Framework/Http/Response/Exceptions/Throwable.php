13fd437a6baff619faaa93163d43badae4079705
Jon Waldstein <Jpwaldstein@gmail.com>
2021-11-17 10:47:56 -0500

    docs: update throwable docblock

--- src/Framework/Http/Response/Exceptions/Throwable.php
+++ src/Framework/Http/Response/Exceptions/Throwable.php
@@ -5,6 +5,10 @@ namespace Give\Framework\Http\Exceptions;
 use Give\Framework\Exceptions\Primitives\Exception;
 
 /**
+ * A local version for a php 7 exception
+ *
+ * @see https://www.php.net/manual/en/class.throwable.php
+ *
  * @unreleased
  */
 class Throwable extends Exception
