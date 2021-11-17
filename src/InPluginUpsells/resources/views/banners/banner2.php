983db34d122b701c4b2ca186c50a53fc372f1970
alaca <ante.laca@gmail.com>
2021-11-17 12:19:49 +0100

    feature: banner templates

---
+++ src/InPluginUpsells/resources/views/banners/banner2.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @var array $banner
+ */
+?>
+<div class="give-sale-banner">
+    Banner 2
+
+    <span class="give-sale-banner-dismiss" data-id="<?= $banner['id']; ?>"></span>
+</div>
---
+++ src/InPluginUpsells/resources/views/banners/giving-tuesday.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @var array $banner
+ */
+?>
+<div class="give-sale-banner">
+    Giving tuesday
+
+    <span class="give-sale-banner-dismiss" data-id="<?= $banner['id']; ?>"></span>
+</div>
