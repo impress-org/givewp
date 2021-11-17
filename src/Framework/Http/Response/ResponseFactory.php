766bd7a8e920d0c7f017e7d0cec201bda5216d0a
Jon Waldstein <Jpwaldstein@gmail.com>
2021-11-17 15:37:30 -0500

    refactor: remove unnecessary response factory interface

--- src/Framework/Http/Response/ResponseFactory.php
+++ src/Framework/Http/Response/ResponseFactory.php
@@ -2,14 +2,13 @@
 
 namespace Give\Framework\Http\Response;
 
-use Give\Framework\Http\Response\Contracts\ResponseFactoryInterface;
 use Give\Framework\Http\Response\Types\JsonResponse;
 use Give\Framework\Http\Response\Types\RedirectResponse;
 
 /**
  * @unreleased
  */
-class ResponseFactory implements ResponseFactoryInterface
+class ResponseFactory
 {
 
     /**
