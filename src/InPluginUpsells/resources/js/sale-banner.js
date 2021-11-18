eb5de4e0fce2041ec0f32ac90a29ca3bffb4fd08
Nathan Knowler <nathan@givewp.com>
2021-11-18 05:52:31 -0600

    feature: run script immediately

--- src/InPluginUpsells/resources/js/sale-banner.js
+++ src/InPluginUpsells/resources/js/sale-banner.js
@@ -1,36 +1,32 @@
-const bodyIsReady = onReady => window.requestAnimationFrame( document.body ? onReady : bodyIsReady );
+const bannersContainer = document.querySelector( '.give-sale-banners-container' );
+const dismissActions = document.querySelectorAll( '.give-sale-banner-dismiss' );
+const pageTitle = document.querySelector( '.page-title-action, .wp-heading-inline, #give-in-plugin-upsells h1' );
 
-bodyIsReady( () => {
-    const bannersContainer = document.querySelector( '.give-sale-banners-container' );
-    const dismissActions = document.querySelectorAll( '.give-sale-banner-dismiss' );
-    const pageTitle = document.querySelector( '.page-title-action, .wp-heading-inline, #give-in-plugin-upsells h1' );
+const hideBanner = ( { target: dismissAction } ) => {
+    const formData = new FormData();
+    formData.append( 'id', dismissAction.dataset.id );
 
-    const hideBanner = ( { target: dismissAction } ) => {
-        const formData = new FormData();
-        formData.append( 'id', dismissAction.dataset.id );
+    document.getElementById( dismissAction.getAttribute( 'aria-controls' ) ).remove();
 
-        document.getElementById( dismissAction.getAttribute( 'aria-controls' ) ).remove();
-
-        fetch( `${ window.GiveSaleBanners.apiRoot }/hide`, {
-            method: 'POST',
-            headers: {
-                'X-WP-Nonce': window.GiveSaleBanners.apiNonce,
-            },
-            body: formData,
-        } );
-
-        if ( bannersContainer.querySelectorAll( '.give-sale-banner' ).length === 0 ) {
-            bannersContainer.remove();
-        }
-    };
-
-    if ( pageTitle && bannersContainer ) {
-        pageTitle.parentNode.insertBefore( bannersContainer, pageTitle.nextSibling );
-
-        bannersContainer.style.display = null;
-    }
-
-    dismissActions.forEach( ( action ) => {
-        action.addEventListener( 'click', hideBanner );
+    fetch( `${ window.GiveSaleBanners.apiRoot }/hide`, {
+        method: 'POST',
+        headers: {
+            'X-WP-Nonce': window.GiveSaleBanners.apiNonce,
+        },
+        body: formData,
     } );
+
+    if ( bannersContainer.querySelectorAll( '.give-sale-banner' ).length === 0 ) {
+        bannersContainer.remove();
+    }
+};
+
+if ( pageTitle && bannersContainer ) {
+    pageTitle.parentNode.insertBefore( bannersContainer, pageTitle.nextSibling );
+
+    bannersContainer.style.display = null;
+}
+
+dismissActions.forEach( ( action ) => {
+    action.addEventListener( 'click', hideBanner );
 } );
