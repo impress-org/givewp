6c0f468a771dcebdd3e8f7bb85325e3a3f98dfbf
Nathan Knowler <nathan@givewp.com>
2021-11-18 23:36:53 -0600

    format: format select

--- src/FormAPI/Form/Select.php
+++ src/FormAPI/Form/Select.php
@@ -2,31 +2,34 @@
 
 namespace Give\FormAPI\Form;
 
-class Select extends Field {
+class Select extends Field
+{
 
-	/**
-	 * @var array
-	 */
-	public $options = [];
+    /**
+     * @var array
+     */
+    public $options = [];
 
-	/**
-	 * @inheritDoc
-	 */
-	public function parse( $array ) {
-		parent::parse( $array );
+    /**
+     * @inheritDoc
+     */
+    public function parse($array)
+    {
+        parent::parse($array);
 
-		$this->options = isset( $array[ 'options' ] ) ? $array[ 'options' ] : [];
-	}
+        $this->options = isset($array[ 'options' ]) ? $array[ 'options' ] : [];
+    }
 
-	/**
-	 * @inheritDoc
-	 */
-	public function toArray() {
-		return array_merge(
-			parent::toArray(),
-			[
-				'options' => $this->options,
-			]
-		);
-	}
+    /**
+     * @inheritDoc
+     */
+    public function toArray()
+    {
+        return array_merge(
+            parent::toArray(),
+            [
+                'options' => $this->options,
+            ]
+        );
+    }
 }
