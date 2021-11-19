307fdede9a96a418cbf4a6c0fc9e925506a9fec0
alaca <ante.laca@gmail.com>
2021-11-19 17:32:01 +0100

    feature: donation form goal stats show correct values

--- src/Views/Form/Templates/Classic/resources/views/header.php
+++ src/Views/Form/Templates/Classic/resources/views/header.php
@@ -2,42 +2,58 @@
 /**
  * @var string $title
  * @var string $description
- * @var bool   $isSecureBadgeEnabled
- * @var bool   $secureBadgeContent
+ * @var bool $isSecureBadgeEnabled
+ * @var bool $secureBadgeContent
+ * @var bool $hasGoal
+ * @var array $goalStats
  */
+
 ?>
 <div class="give-form-header">
     <div class="give-form-header-top-wrap">
         <h1 class="give-form-title"><?= $title ?></h1>
         <p class="give-form-description"><?= $description ?></p>
-        <?php if ($isSecureBadgeEnabled) : ?>
-        <aside class="give-form-secure-badge">
-            <svg class="give-form-secure-icon">
-                <use href="#give-icon-lock" />
-            </svg>
-            <?= $secureBadgeContent ?>
-        </aside>
-        <?php endif; ?>
+        <?php
+        if ($isSecureBadgeEnabled) : ?>
+            <aside class="give-form-secure-badge">
+                <svg class="give-form-secure-icon">
+                    <use href="#give-icon-lock"/>
+                </svg>
+                <?= $secureBadgeContent ?>
+            </aside>
+        <?php
+        endif; ?>
     </div>
-    <aside class="give-form-stats-panel">
-        <ul class="give-form-stats-panel-list">
-            <li class="give-form-stats-panel-stat">
-                <span class="give-form-stats-panel-stat-number">$0</span> raised
-            </li>
-            <li class="give-form-stats-panel-stat">
-                <span class="give-form-stats-panel-stat-number">50</span> donations
-            </li>
-            <li class="give-form-stats-panel-stat">
-                <span class="give-form-stats-panel-stat-number">$10,000</span> goal
-            </li>
-            <li class="give-form-goal-progress">
-                <div class="give-form-goal-progress-meter" role="meter" aria-labelledby="meter-label" aria-valuemin="0" aria-valuemax="1000000" aria-valuenow="200000" aria-valuetext="20%">
-                    <div id="meter-label" hidden>
-                        $20,000 of $1,000,000 goal
+    <?php
+    if ($hasGoal): ?>
+        <aside class="give-form-stats-panel">
+            <ul class="give-form-stats-panel-list">
+                <li class="give-form-stats-panel-stat">
+                    <span class="give-form-stats-panel-stat-number">
+                        <?= $goalStats[ 'raised' ]; ?>
+                    </span> <?= __('raised', 'give'); ?>
+                </li>
+                <li class="give-form-stats-panel-stat">
+                    <span class="give-form-stats-panel-stat-number">
+                         <?= $goalStats[ 'count' ]; ?>
+                    </span> <?= $goalStats[ 'countLabel' ]; ?>
+                </li>
+                <li class="give-form-stats-panel-stat">
+                    <span class="give-form-stats-panel-stat-number">
+                        <?= $goalStats[ 'goal' ]; ?>
+                    </span> <?= __('goal', 'give'); ?>
+                </li>
+                <li class="give-form-goal-progress">
+                    <div class="give-form-goal-progress-meter" role="meter" aria-labelledby="meter-label" aria-valuemin="0" aria-valuemax="1000000"
+                         aria-valuenow="<?= $goalStats[ 'progress' ]; ?>%" aria-valuetext="<?= $goalStats[ 'progress' ]; ?>%">
+                        <div id="meter-label" hidden>
+                            <?= sprintf(__('%s of %s goal', 'give'), $goalStats[ 'raised' ], $goalStats[ 'goal' ]); ?>
+                        </div>
+                        <div class="give-form-goal-progress-meter-bar" style="width: <?= $goalStats[ 'progress' ]; ?>%"></div>
                     </div>
-                    <div class="give-form-goal-progress-meter-bar" style="width: 20%"></div>
-                </div>
-            </li>
-        </ul>
-    </aside>
+                </li>
+            </ul>
+        </aside>
+    <?php
+    endif; ?>
 </div>
