9f7efc477cc70a3a224aa1760f66ee178c454d09
Jon Waldstein <Jpwaldstein@gmail.com>
2021-11-23 15:28:22 -0500

    feature: add return url generator

---
+++ src/Framework/PaymentGateways/Actions/GenerateReturnUrlFromRedirectOffsite.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Give\Framework\PaymentGateways\Actions;
+
+class GenerateReturnUrlFromRedirectOffsite {
+    /**
+     * @param string $gatewayId
+     * @param string $gatewayMethod
+     * @return string
+     */
+    public function __invoke($gatewayId, $gatewayMethod)
+    {
+        return add_query_arg(
+			[
+				'give-listener' => 'payment-gateway',
+				'give-gateway-id' => $gatewayId,
+                'give-gateway-method' => $gatewayMethod
+			],
+			home_url()
+		);
+    }
+}
