b8de944006a0295671bc54d9a1fca3d07febb333
Jon Waldstein <Jpwaldstein@gmail.com>
2021-11-24 14:32:15 -0500

    feature: throw route exception if not valid

--- src/Framework/PaymentGateways/Routes/GatewayRoute.php
+++ src/Framework/PaymentGateways/Routes/GatewayRoute.php
@@ -2,8 +2,8 @@
 
 namespace Give\Framework\PaymentGateways\Routes;
 
-use Exception;
 use Give\Framework\PaymentGateways\DataTransferObjects\GatewayOffsiteReturnData;
+use Give\Framework\PaymentGateways\Exceptions\PaymentGatewayException;
 use Give\Framework\PaymentGateways\PaymentGateway;
 use Give\Framework\PaymentGateways\PaymentGatewayRegister;
 
@@ -18,14 +18,20 @@ class GatewayRoute
     protected $gatewayMethod;
 
     /**
-     * @throws Exception
+     * @throws PaymentGatewayException
      */
     public function __invoke()
     {
-        $gateways = give(PaymentGatewayRegister::class)->getOffsitePaymentGateways();
+        /** @var PaymentGatewayRegister $paymentGatewayRegister */
+        $paymentGatewayRegister = give(PaymentGatewayRegister::class);
+        $gateways = $paymentGatewayRegister->getOffsitePaymentGateways();
         $gatewayIds = array_keys($gateways);
 
-        if ($this->isValid($gatewayIds)) {
+        if ($this->isValidListener()) {
+            if (!$this->isValidRequest($gatewayIds)) {
+                throw new PaymentGatewayException('This route is not valid.');
+            }
+
             $data = GatewayOffsiteReturnData::fromRequest($_GET);
 
             /** @var PaymentGateway $gateway */
@@ -34,9 +40,11 @@ class GatewayRoute
             $gatewayMethod = $data->gatewayMethod;
             $gateway->$gatewayMethod($data->paymentId);
         }
-	}
+    }
 
     /**
+     * Check if the request is valid
+     *
      * @unreleased
      *
      * @param  array  $gatewayIds
@@ -45,12 +53,24 @@ class GatewayRoute
      * @example ?give-listener=give-gateway&give-gateway-id=test-gateway&give-gateway-method=handleReturnFromRedirect
      *
      */
-	private function isValid($gatewayIds) {
-        $isset = isset($_GET['give-listener'], $_GET['give-gateway-id'], $_GET['give-gateway-method']);
-        $listenerValid = $_GET['give-listener'] === 'give-gateway';
+    private function isValidRequest($gatewayIds)
+    {
+        $isset = isset($_GET['give-gateway-id'], $_GET['give-gateway-method']);
         $idValid = in_array($_GET['give-gateway-id'], $gatewayIds, true);
         $methodValid = $_GET['give-gateway-method'] === $this->gatewayMethod;
 
-		return $isset && $listenerValid && $idValid && $methodValid;
-	}
+        return $isset && $idValid && $methodValid;
+    }
+
+    /**
+     * Check if the listener is valid
+     *
+     * @unreleased
+     *
+     * @return bool
+     */
+    private function isValidListener()
+    {
+        return isset($_GET['give-listener']) && $_GET['give-listener'] === 'give-gateway';
+    }
 }
