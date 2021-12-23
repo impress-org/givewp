d423d88b57e051bbaf0f90b8c9aa2d9e93c29519
Kyle B. Johnson <kyle.johnson@impress.org>
2021-12-23 13:34:44 -0500

    chore: Housekeeping.

--- src/Framework/PaymentGateways/CommandHandlers/PaymentCompleteHandler.php
+++ src/Framework/PaymentGateways/CommandHandlers/PaymentCompleteHandler.php
@@ -2,8 +2,6 @@
 
 namespace Give\Framework\PaymentGateways\CommandHandlers;
 
-use Give\Framework\PaymentGateways\Commands\PaymentComplete;
-
 class PaymentCompleteHandler extends PaymentHandler {
     protected function getPaymentStatus() { return 'complete'; }
 }
--- src/Framework/PaymentGateways/CommandHandlers/PaymentProcessingHandler.php
+++ src/Framework/PaymentGateways/CommandHandlers/PaymentProcessingHandler.php
@@ -2,8 +2,6 @@
 
 namespace Give\Framework\PaymentGateways\CommandHandlers;
 
-use Give\Framework\PaymentGateways\Commands\PaymentProcessing;
-
 class PaymentProcessingHandler extends PaymentHandler {
     protected function getPaymentStatus() { return 'processing'; }
 }
