7fa305e1705b9fbe52eb92f03d3b6ef3d88d3ff0
alaca <ante.laca@gmail.com>
2022-01-21 19:19:31 +0100

    refactor: update method name

--- src/Framework/QueryBuilder/Traits/Meta.php
+++ src/Framework/QueryBuilder/Traits/Meta.php
@@ -5,18 +5,20 @@ namespace Give\Framework\QueryBuilder\Traits;
 trait Meta
 {
     /**
+     * Select meta columns
+     *
      * @param  string  $table
      * @param  string  $foreignKey
      * @param  string  $primaryKey
+     * @param  array  $columns
      *
      * @return $this
      */
-    public function joinMeta($table, $foreignKey, $primaryKey, $columns = [])
+    public function attachMeta($table, $foreignKey, $primaryKey, ...$columns)
     {
-        $this->joins[] = [$this->alias($table), $foreignKey, $primaryKey, 'LEFT'];
-
         foreach ($columns as $i => $entry) {
             list ($column, $columnAlias) = $entry;
+
             $tableAlias      = sprintf('%s_%d', $table, $i);
             $this->joins[]   = [$this->alias($table) . ' ' . $tableAlias, $foreignKey, $primaryKey, 'LEFT'];
             $this->selects[] = [$tableAlias . '.meta_value', $columnAlias];
