c9b2c1e4078e5d23ef008320e46e79ff1fd02a55
Kyle B. Johnson <kyle.johnson@impress.org>
2022-01-23 21:31:22 -0500

    chore: Add custom exceptions.

---
+++ src/PaymentGateways/Gateways/Stripe/Exceptions/PaymentIntentException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Give\PaymentGateways\Gateways\Stripe\Exceptions;
+
+use Give\Framework\PaymentGateways\Exceptions\PaymentGatewayException;
+
+class PaymentIntentException extends PaymentGatewayException {}
