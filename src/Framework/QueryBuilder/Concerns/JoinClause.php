9d11938f75494b949a9bcae6eb7e2b21f980c387
alaca <ante.laca@gmail.com>
2022-01-25 14:13:48 +0100

    feature: add `joinOn`, `joinAnd`, `joinOr`, `getDefinedJoins` helper methods

--- src/Framework/QueryBuilder/Concerns/JoinClause.php
+++ src/Framework/QueryBuilder/Concerns/JoinClause.php
@@ -2,8 +2,12 @@
 
 namespace Give\Framework\QueryBuilder\Concerns;
 
+use Closure;
+use Give\Framework\Database\DB;
 use Give\Framework\QueryBuilder\Join;
+use Give\Framework\QueryBuilder\JoinCondition;
 use Give\Framework\QueryBuilder\Types\JoinType;
+use Give\Framework\QueryBuilder\Types\Operator;
 
 /**
  * @unreleased
@@ -17,33 +21,44 @@ trait JoinClause
     protected $joins = [];
 
     /**
+     * Helper method used to build complex JOIN queries, Check README.md for more info.
+     * If you need to perform only simple JOINs with one "simple" JOIN condition, then you don't need this method.
+     *
      * @param  string  $table
-     * @param  string  $foreignKey
-     * @param  string  $primaryKey
      * @param  string  $joinType  \Give\Framework\QueryBuilder\Types\JoinType
+     * @param  array|Closure  $condition
      * @param  string|null  $alias
      *
      * @return $this
      */
-    public function join($table, $foreignKey, $primaryKey, $joinType, $alias = null)
+    public function join($table, $joinType, $condition, $alias = null)
     {
-        $this->joins[] = new Join($table, $foreignKey, $primaryKey, $joinType, $alias);
+        $this->joins[] = new Join(
+            $table,
+            $joinType,
+            $condition,
+            $alias
+        );
 
         return $this;
     }
 
-
     /**
      * @param  string  $table
-     * @param  string  $foreignKey
-     * @param  string  $primaryKey
+     * @param $foreignKey
+     * @param $primaryKey
      * @param  string|null  $alias
      *
      * @return $this
      */
     public function leftJoin($table, $foreignKey, $primaryKey, $alias = null)
     {
-        $this->join($table, $foreignKey, $primaryKey, JoinType::LEFT, $alias);
+        $this->join(
+            $table,
+            JoinType::LEFT,
+            [$foreignKey, $primaryKey],
+            $alias
+        );
 
         return $this;
     }
@@ -54,11 +69,17 @@ trait JoinClause
      * @param  string  $primaryKey
      * @param  string|null  $alias
      *
+     *
      * @return $this
      */
     public function innerJoin($table, $foreignKey, $primaryKey, $alias = null)
     {
-        $this->join($table, $foreignKey, $primaryKey, JoinType::INNER, $alias);
+        $this->join(
+            $table,
+            JoinType::INNER,
+            [$foreignKey, $primaryKey],
+            $alias
+        );
 
         return $this;
     }
@@ -73,7 +94,87 @@ trait JoinClause
      */
     public function rightJoin($table, $foreignKey, $primaryKey, $alias = null)
     {
-        $this->join($table, $foreignKey, $primaryKey, JoinType::RIGHT, $alias);
+        $this->join(
+            $table,
+            JoinType::RIGHT,
+            [$foreignKey, $primaryKey],
+            $alias
+        );
+
+        return $this;
+    }
+
+    /**
+     * Add JoinCondition using Query Builder
+     *
+     * @note this is a special helper method for building complex JOIN queries. Check README.md for more info.
+     *
+     * @param  string  $column1
+     * @param  string  $comparisonOperator
+     * @param  string  $column2
+     * @param  bool  $quote
+     *
+     * @return $this
+     */
+    public function joinOn($column1, $comparisonOperator, $column2, $quote = false)
+    {
+        $this->joins[] = new JoinCondition(
+            Operator::ON,
+            $column1,
+            $comparisonOperator,
+            $column2,
+            $quote
+        );
+
+        return $this;
+    }
+
+    /**
+     * Add JoinCondition using Query Builder
+     *
+     * @note this is a special helper method for building complex JOIN queries. Check README.md for more info.
+     *
+     * @param  string  $column1
+     * @param  string  $comparisonOperator
+     * @param  string  $column2
+     * @param  bool  $quote
+     *
+     * @return $this
+     */
+    public function joinAnd($column1, $comparisonOperator, $column2, $quote = false)
+    {
+        $this->joins[] = new JoinCondition(
+            Operator::AND,
+            $column1,
+            $comparisonOperator,
+            $column2,
+            $quote
+        );
+
+        return $this;
+    }
+
+    /**
+     * Add JoinCondition using Query Builder
+     *
+     * @note this is a special helper method for building complex JOIN queries. Check README.md for more info.
+     *
+     * @param  string  $column1
+     * @param  string  $comparisonOperator
+     * @param  string  $column2
+     * @param  bool  $quote
+     *
+     * @return $this
+     */
+    public function joinOr($column1, $comparisonOperator, $column2, $quote = false)
+    {
+        $this->joins[] = new JoinCondition(
+            Operator::OR,
+            $column1,
+            $comparisonOperator,
+            $column2,
+            $quote
+        );
 
         return $this;
     }
@@ -84,11 +185,41 @@ trait JoinClause
     public function getJoinSQL()
     {
         return array_map(function (Join $joinTable) {
+            $conditions = array_map(function (JoinCondition $condition) use ($joinTable) {
+                return DB::prepare(
+                    $condition->quote
+                        ? '%1s %2s %3s %s'
+                        : '%1s %2s %3s %4s',
+                    $condition->logicalOperator,
+                    $condition->column1,
+                    $condition->comparisonOperator,
+                    $condition->column2
+                );
+            }, $joinTable->conditions);
+
+            // Join table is using an alias
             if ($joinTable->alias) {
-                return "{$joinTable->joinType} JOIN {$joinTable->table} {$joinTable->alias} ON {$joinTable->foreignKey} = {$joinTable->alias}.{$joinTable->primaryKey}";
+                return DB::prepare(
+                        ' %1s JOIN %2s %3s ',
+                        $joinTable->joinType,
+                        $joinTable->table,
+                        $joinTable->alias
+                    ) . implode(' ', $conditions);
             }
 
-            return "{$joinTable->joinType} JOIN {$joinTable->table} ON {$joinTable->foreignKey} = {$joinTable->table}.{$joinTable->primaryKey}";
+            return DB::prepare(
+                    ' %1s JOIN %2s ',
+                    $joinTable->joinType,
+                    $joinTable->table
+                ) . implode(' ', $conditions);
         }, $this->joins);
+    }
+
+    /**
+     * @return Join[]
+     */
+    public function getDefinedJoins()
+    {
+        return $this->joins;
     }
 }
