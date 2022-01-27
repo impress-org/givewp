1d3e1050e658ded409e2ad73c45f7aa4398f60fd
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2022-01-27 20:11:04 +0530

    doc: add comment about testing

--- src/PaymentGateways/Gateways/TestGateway/Commands/CreateTestGatewayOffsitePaymentUrlCommand.php
+++ src/PaymentGateways/Gateways/TestGateway/Commands/CreateTestGatewayOffsitePaymentUrlCommand.php
@@ -21,12 +21,29 @@ class CreateTestGatewayOffsitePaymentUrlCommand implements GatewayCommand
      *
      * @return string
      */
-    public function __invoke( OffsiteGatewayPaymentData $offsiteGatewayPaymentData )
+    public function __invoke(OffsiteGatewayPaymentData $offsiteGatewayPaymentData)
     {
+        // Test offsite payment flow:
+        // 1) Setup a second website (http://freshdb.test).
+        // 2) Add below code as in mu plugin.
+        // add_action( 'init', function(){
+        //     if( ! isset( $_GET['test-offsite-redirect'] ) ) {
+        //        return;
+        //     }
+        //
+        //     $redirect = urldecode($_GET['redirect']);
+        //
+        //     wp_redirect($redirect);
+        //     exit();
+        // });
+        // 3) Process payment with "Test Gateway Offsite"
+        //
+        // If you want to test failed or cancelled donation flow then change url to:
+        // $offsiteGatewayPaymentData->failedRedirectUrl or $offsiteGatewayPaymentData->cancelledRedirectUrl
         return sprintf(
             '%1$s?test-offsite-redirect=1&redirect=%2$s',
             'http://freshdb.test',
-            urlencode( $offsiteGatewayPaymentData->redirectUrl )
+            urlencode($offsiteGatewayPaymentData->failedRedirectUrl)
         );
     }
 }
