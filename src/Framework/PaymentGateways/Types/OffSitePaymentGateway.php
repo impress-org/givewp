8e668d2b4d18dffefbbb5b84c0ed55d23e957d61
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2022-01-27 16:54:16 +0530

    refactor: move function to parent class
    Move functions to OffSitePaymentGateway class.

--- src/Framework/PaymentGateways/Types/OffSitePaymentGateway.php
+++ src/Framework/PaymentGateways/Types/OffSitePaymentGateway.php
@@ -2,6 +2,10 @@
 
 namespace Give\Framework\PaymentGateways\Types;
 
+use Give\Framework\PaymentGateways\Commands\PaymentCancelled;
+use Give\Framework\PaymentGateways\Commands\PaymentCommand;
+use Give\Framework\PaymentGateways\Commands\PaymentFailed;
+use Give\Framework\PaymentGateways\Commands\PaymentProcessing;
 use Give\Framework\PaymentGateways\Contracts\OffsiteGatewayInterface;
 use Give\Framework\PaymentGateways\PaymentGateway;
 
@@ -10,4 +14,41 @@ use Give\Framework\PaymentGateways\PaymentGateway;
  */
 abstract class OffSitePaymentGateway extends PaymentGateway implements OffsiteGatewayInterface
 {
+    /**
+     * @unreleased
+     *
+     * @param int $donationId
+     *
+     * @return PaymentCommand
+     */
+    public function returnSuccessFromOffsiteRedirect($donationId)
+    {
+        return new PaymentProcessing();
+    }
+
+    /**
+     * Handle failed donation redirect.
+     *
+     * @unreleased
+     *
+     * @param int $donationId
+     *
+     * @return PaymentCommand
+     */
+    public function returnFailureFromOffsiteRedirect($donationId)
+    {
+        return new PaymentFailed();
+    }
+
+    /**
+     * Handle cancelled donation redirect.
+     *
+     * @param int $donationId
+     *
+     * @return PaymentCancelled
+     */
+    public function returnCancelFromOffsiteRedirect($donationId)
+    {
+        return new PaymentCancelled();
+    }
 }
