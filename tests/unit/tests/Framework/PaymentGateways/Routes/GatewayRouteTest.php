fe58e7456b1f97e1b83a1320bfe1b11dbeab1c60
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2022-01-27 15:33:56 +0530

    tests: add tests for  GatewayRoute class

---
+++ tests/unit/tests/Framework/PaymentGateways/Routes/GatewayRouteTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use Give\Framework\PaymentGateways\Actions\GenerateGatewayRouteUrl;
+use Give\Framework\PaymentGateways\DataTransferObjects\GatewayRouteData;
+use Give\Framework\PaymentGateways\Exceptions\PaymentGatewayException;
+use Give\Framework\PaymentGateways\Routes\GatewayRoute;
+use Give\Helpers\Call;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @unreleased
+ */
+class GatewayRouteTest extends TestCase
+{
+    public function testThrowExceptionOnInvalidGatewayId(){
+        $gatewayRoute = new GatewayRoute();
+
+        $gatewayRouteData = GatewayRouteData::fromRequest(
+            [
+                'give-gateway-id' => 'mock-payapl-offsite',
+                'give-gateway-method' => 'returnSuccessFromOffsiteRedirect',
+                'give-donation-id' => 123,
+                '_wpnonce' => ''
+            ]
+        );
+
+        $this->expectException( PaymentGatewayException::class );
+        $gatewayRoute->process( $gatewayRouteData );
+    }
+}
