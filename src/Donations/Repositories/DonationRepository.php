dc0f3422b49d086c4c62e46b38456aefb83c23d1
alaca <ante.laca@gmail.com>
2022-01-31 11:05:49 +0100

    refactor: `DonationRepository`

--- src/Donations/Repositories/DonationRepository.php
+++ src/Donations/Repositories/DonationRepository.php
@@ -3,10 +3,11 @@
 namespace Give\Donations\Repositories;
 
 use Exception;
-use Give\Donations\DataTransferObjects\DonationPostData;
+use Give\Donations\DataTransferObjects\DonationQueryData;
 use Give\Donations\Models\Donation;
 use Give\Framework\Database\DB;
 use Give\Framework\Database\Exceptions\DatabaseQueryException;
+use Give\Framework\QueryBuilder\QueryBuilder;
 use Give\Log\Log;
 
 class DonationRepository
@@ -17,93 +18,147 @@ class DonationRepository
      * @unreleased
      *
      * @param  int  $donationId
+     *
      * @return Donation
      */
     public function getById($donationId)
     {
-        $donationPost = get_post($donationId);
+        $donation = DB::table('posts')
+            ->select(
+                ['ID', 'id'],
+                ['post_date', 'createdAt'],
+                ['post_modified', 'updatedAt'],
+                ['post_status', 'status'],
+                ['post_parent', 'parentId']
+            )
+            ->attachMeta('give_donationmeta',
+                'ID',
+                'donation_id',
+                ['_give_payment_total', 'amount'],
+                ['_give_payment_currency', 'currency'],
+                ['_give_payment_gateway', 'gateway'],
+                ['_give_payment_donor_id', 'donorId'],
+                ['_give_donor_billing_first_name', 'firstName'],
+                ['_give_donor_billing_last_name', 'lastName'],
+                ['_give_payment_donor_email', 'email'],
+                ['subscription_id', 'subscriptionId']
+            )
+            ->where('ID', $donationId)
+            ->get();
 
-        return DonationPostData::fromPost($donationPost)->toDonation();
+        return DonationQueryData::fromObject($donation)->toDonation();
     }
 
     /**
      * @param  int  $subscriptionId
-     * @return array|Donation[]
+     *
+     * @return Donation[]
      */
     public function getBySubscriptionId($subscriptionId)
     {
-        global $wpdb;
+        $donations = DB::table('posts')
+            ->select(
+                ['ID', 'id'],
+                ['post_date', 'createdAt'],
+                ['post_modified', 'updatedAt'],
+                ['post_status', 'status'],
+                ['post_parent', 'parentId']
+            )
+            ->attachMeta(
+                'give_donationmeta',
+                'ID',
+                'donation_id',
+                ['_give_payment_total', 'amount'],
+                ['_give_payment_currency', 'currency'],
+                ['_give_payment_gateway', 'gateway'],
+                ['_give_payment_donor_id', 'donorId'],
+                ['_give_donor_billing_first_name', 'firstName'],
+                ['_give_donor_billing_last_name', 'lastName'],
+                ['_give_payment_donor_email', 'email'],
+                ['subscription_id', 'subscriptionId']
+            )
+            ->leftJoin('give_donationmeta', 'ID', 'donationMeta.donation_id', 'donationMeta')
+            ->where('post_type', 'give_payment')
+            ->where('post_status', 'give_subscription')
+            ->where('donationMeta.meta_key', 'subscription_id')
+            ->where('donationMeta.meta_value', $subscriptionId)
+            ->orderBy('post_date', 'DESC')
+            ->getAll();
 
-        $donationIds = DB::get_col(
-            "SELECT donation_id
-                    FROM {$wpdb->prefix}give_donationmeta
-                    WHERE meta_key = 'subscription_id'
-                    AND meta_value = '$subscriptionId'"
-        );
 
-        $posts = get_posts([
-            'include' => $donationIds,
-            'post_type' => 'give_payment',
-            'post_status' => 'give_subscription',
-            'orderby' => 'post_date',
-        ]);
-
-        return array_map(static function ($post) {
-            return DonationPostData::fromPost($post)->toDonation();
-        }, $posts);
+        return array_map(static function ($donation) {
+            return DonationQueryData::fromObject($donation)->toDonation();
+        }, $donations);
     }
 
     /**
      * @param  int donorId
-     * @return array|Donation[]
+     *
+     * @return Donation[]
      */
     public function getByDonorId($donorId)
     {
-        global $wpdb;
+        $donations = DB::table('posts')
+            ->select(
+                ['ID', 'id'],
+                ['post_date', 'createdAt'],
+                ['post_modified', 'updatedAt'],
+                ['post_status', 'status'],
+                ['post_parent', 'parentId']
+            )
+            ->attachMeta(
+                'give_donationmeta',
+                'ID',
+                'donation_id',
+                ['_give_payment_total', 'amount'],
+                ['_give_payment_currency', 'currency'],
+                ['_give_payment_gateway', 'gateway'],
+                ['_give_payment_donor_id', 'donorId'],
+                ['_give_donor_billing_first_name', 'firstName'],
+                ['_give_donor_billing_last_name', 'lastName'],
+                ['_give_payment_donor_email', 'email'],
+                ['subscription_id', 'subscriptionId']
+            )
+            ->where('post_type', 'give_payment')
+            ->whereIn('ID', function (WhereQueryBuilder $builder) use ($donorId) {
+                $builder
+                    ->select('donation_id')
+                    ->from('give_donationmeta')
+                    ->where('meta_key', '_give_payment_donor_id')
+                    ->where('meta_value', $donorId);
+            })
+            ->orderBy('post_date', 'DESC')
+            ->getAll();
 
-        $donationIds = DB::get_col(
-            "SELECT donation_id
-                    FROM {$wpdb->prefix}give_donationmeta
-                    WHERE meta_key = '_give_payment_donor_id'
-                    AND meta_value = '$donorId'"
-        );
-
-        $posts = get_posts([
-            'include' => $donationIds,
-            'post_type' => 'give_payment',
-            'orderby' => 'post_date',
-        ]);
-
-        return array_map(static function ($post) {
-            return DonationPostData::fromPost($post)->toDonation();
-        }, $posts);
+        return array_map(static function ($donation) {
+            return DonationQueryData::fromObject($donation)->toDonation();
+        }, $donations);
     }
 
     /**
      * @param  Donation  $donation
+     *
      * @return Donation
      * @throws Exception|DatabaseQueryException
      */
     public function insert(Donation $donation)
     {
-        global $wpdb;
-
         $date = current_datetime()->format('Y-m-d H:i:s');
 
         DB::query('START TRANSACTION');
 
         try {
-            DB::insert($wpdb->posts, [
+            DB::insert(DB::prefix('posts'), [
                 'post_date' => $date,
                 'post_date_gmt' => get_gmt_from_date($date),
-                'post_status' => $donation->status->getValue(),
+                'post_status' => $donation->status,
                 'post_type' => 'give_payment'
             ], null);
 
             $donationId = DB::last_insert_id();
 
             foreach ($donation->getMeta() as $metaKey => $metaValue) {
-                DB::insert($wpdb->donationmeta, [
+                DB::insert(DB::prefix('give_donationmeta'), [
                     'donation_id' => $donationId,
                     'meta_key' => $metaKey,
                     'meta_value' => $metaValue,
@@ -124,27 +179,26 @@ class DonationRepository
 
     /**
      * @param  Donation  $donation
+     *
      * @return Donation
      * @throws Exception|DatabaseQueryException
      */
     public function update(Donation $donation)
     {
-        global $wpdb;
-
         $date = current_datetime()->format('Y-m-d H:i:s');
 
         DB::query('START TRANSACTION');
 
         try {
-            DB::update($wpdb->posts, [
+            DB::update(DB::prefix('posts'), [
                 'post_modified' => $date,
                 'post_modified_gmt' => get_gmt_from_date($date),
-                'post_status' => $donation->status->getValue(),
-                'post_type' => $donation->status->getValue()
+                'post_status' => $donation->status,
+                'post_type' => $donation->status
             ], ['id' => $donation->id]);
 
             foreach ($donation->getMeta() as $metaKey => $metaValue) {
-                DB::update($wpdb->donationmeta, [
+                DB::update(DB::prefix('give_donationmeta'), [
                     'meta_value' => $metaValue,
                 ], [
                     'donation_id' => $donation->id,
@@ -166,6 +220,7 @@ class DonationRepository
 
     /**
      * @param  Donation  $donation
+     *
      * @return array
      */
     public function getMeta(Donation $donation)
