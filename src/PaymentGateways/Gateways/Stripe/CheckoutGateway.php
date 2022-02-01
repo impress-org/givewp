8526793fa3856f8c3131caf7e0e441e43b94bf22
Kyle B. Johnson <kyle.johnson@impress.org>
2022-01-31 19:31:00 -0500

    wip: Migrate Stripe Checkout to use the Gateway API.

--- src/PaymentGateways/Gateways/Stripe/CheckoutGateway.php
+++ src/PaymentGateways/Gateways/Stripe/CheckoutGateway.php
@@ -3,18 +3,20 @@
 namespace Give\PaymentGateways\Gateways\Stripe;
 
 use Give\Framework\PaymentGateways\Commands\GatewayCommand;
+use Give\Framework\PaymentGateways\Commands\RedirectOffsite;
 use Give\Framework\PaymentGateways\Exceptions\PaymentGatewayException;
 use Give\Framework\PaymentGateways\PaymentGateway;
 use Give\Helpers\Form\Utils as FormUtils;
+use Give\Helpers\Gateways\Stripe;
 use Give\PaymentGateways\DataTransferObjects\GatewayPaymentData;
+use Give\PaymentGateways\Gateways\Stripe\Exceptions\CheckoutException;
+use Give\PaymentGateways\Gateways\Stripe\ValueObjects\CheckoutSession;
 use Give\PaymentGateways\Gateways\Stripe\ValueObjects\PaymentIntent;
 
-/**
- * @unreleased
- */
-class CreditCardGateway extends PaymentGateway
+class CheckoutGateway extends PaymentGateway
 {
-    use Traits\CreditCardForm;
+    use Traits\CheckoutInstructions;
+    use Traits\CheckoutModal;
     use Traits\HandlePaymentIntentStatus;
 
     /**
@@ -31,11 +33,19 @@ class CreditCardGateway extends PaymentGateway
         $workflow->action( new Actions\GetPaymentMethodFromRequest );
         $workflow->action( new Actions\SaveDonationSummary );
         $workflow->action( new Actions\GetOrCreateStripeCustomer );
-        $workflow->action( new Actions\CreatePaymentIntent );
 
-        return $this->handlePaymentIntentStatus(
-            $workflow->resolve( PaymentIntent::class )
-        );
+        switch( give_stripe_get_checkout_type() ) {
+            case 'modal':
+                $workflow->action( new Actions\CreatePaymentIntent() );
+                $paymentIntent = $workflow->resolve( PaymentIntent::class );
+                return $this->handlePaymentIntentStatus( $paymentIntent );
+            case 'redirect':
+                $workflow->action( new Actions\CreateCheckoutSession() );
+                $session = $workflow->resolve( CheckoutSession::class );
+                return new RedirectOffsite( $session->url );
+            default:
+                throw new CheckoutException( 'Invalid Checkout Error' );
+        }
     }
 
     /**
@@ -43,7 +53,7 @@ class CreditCardGateway extends PaymentGateway
      */
     public static function id()
     {
-        return 'stripe';
+        return 'stripe-checkout';
     }
 
     /**
@@ -59,7 +69,7 @@ class CreditCardGateway extends PaymentGateway
      */
     public function getName()
     {
-        return __('Stripe - Credit Card', 'give');
+        return __('Stripe - Checkout', 'give');
     }
 
     /**
@@ -67,7 +77,7 @@ class CreditCardGateway extends PaymentGateway
      */
     public function getPaymentMethodLabel()
     {
-        return __('Stripe - Credit Card', 'give');
+        return __('Stripe - Checkout', 'give');
     }
 
     /**
@@ -75,10 +85,17 @@ class CreditCardGateway extends PaymentGateway
      */
     public function getLegacyFormFieldMarkup($formId, $args)
     {
+        Stripe::canShowBillingAddress( $formId, $args );
+
         if (FormUtils::isLegacyForm($formId)) {
             return false;
         }
 
-        return $this->getCreditCardFormHTML( $formId, $args );
+        switch( give_stripe_get_checkout_type() ) {
+            case 'modal':
+                return $this->getCheckoutModalHTML();
+            case 'redirect':
+                return $this->getCheckoutInstructions();
+        }
     }
 }
