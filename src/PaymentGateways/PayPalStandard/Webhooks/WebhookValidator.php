f961a2a7a6a2c2155a0b4b0147e6c38618fb2f26
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2022-02-04 08:45:15 +0530

    refactor: early return from function

--- src/PaymentGateways/PayPalStandard/Webhooks/WebhookValidator.php
+++ src/PaymentGateways/PayPalStandard/Webhooks/WebhookValidator.php
@@ -24,42 +24,44 @@ class WebhookValidator
         $eventData = array_merge( [ 'cmd' => '_notify-validate' ], $eventData );
 
         // Validate IPN request w/ PayPal if user hasn't disabled this security measure.
-        if (give_is_setting_enabled(give_get_option('paypal_verification', 'enabled'))) {
-            $requestArgs = [
-                'method' => 'POST',
-                'timeout' => 45,
-                'redirection' => 5,
-                'httpversion' => '1.1',
-                'blocking' => true,
-                'headers' => [
-                    'host' => give_is_test_mode() ? 'www.sandbox.paypal.com' : 'www.paypal.com',
-                    'connection' => 'close',
-                    'content-type' => 'application/x-www-form-urlencoded',
-                    'post' => '/cgi-bin/webscr HTTP/1.1',
-                ],
-                'sslverify' => false,
-                'body' => $eventData,
-            ];
+        if (! give_is_setting_enabled(give_get_option('paypal_verification', 'enabled'))) {
+            return true;
+        }
 
-            $apiResponse = wp_remote_post(give_get_paypal_redirect(), $requestArgs);
+        $requestArgs = [
+            'method' => 'POST',
+            'timeout' => 45,
+            'redirection' => 5,
+            'httpversion' => '1.1',
+            'blocking' => true,
+            'headers' => [
+                'host' => give_is_test_mode() ? 'www.sandbox.paypal.com' : 'www.paypal.com',
+                'connection' => 'close',
+                'content-type' => 'application/x-www-form-urlencoded',
+                'post' => '/cgi-bin/webscr HTTP/1.1',
+            ],
+            'sslverify' => false,
+            'body' => $eventData,
+        ];
 
-            if (is_wp_error($apiResponse)) {
-                Log::error(
-                    'PayPal Standard IPN Error',
-                    ['IPN Data' => $apiResponse]
-                );
+        $apiResponse = wp_remote_post(give_get_paypal_redirect(), $requestArgs);
 
-                return false;
-            }
+        if (is_wp_error($apiResponse)) {
+            Log::error(
+                'PayPal Standard IPN Error',
+                ['IPN Data' => $apiResponse]
+            );
 
-            if ('VERIFIED' !== $apiResponse['body']) {
-                Log::error(
-                    'PayPal Standard IPN Error',
-                    ['IPN Data' => $apiResponse]
-                );
+            return false;
+        }
 
-                return false;
-            }
+        if ('VERIFIED' !== $apiResponse['body']) {
+            Log::error(
+                'PayPal Standard IPN Error',
+                ['IPN Data' => $apiResponse]
+            );
+
+            return false;
         }
 
         return true;
