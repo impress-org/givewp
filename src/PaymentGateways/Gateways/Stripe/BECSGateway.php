fe2c2562343bfa685d6a2407c4fbde5bf2d7dc50
Kyle B. Johnson <kyle.johnson@impress.org>
2022-02-04 15:42:31 -0500

    chore: Add missing error message definitions.

--- src/PaymentGateways/Gateways/Stripe/BECSGateway.php
+++ src/PaymentGateways/Gateways/Stripe/BECSGateway.php
@@ -3,6 +3,7 @@
 namespace Give\PaymentGateways\Gateways\Stripe;
 
 use Give\Framework\PaymentGateways\Commands\GatewayCommand;
+use Give\Framework\PaymentGateways\Contracts\SubscriptionModuleInterface;
 use Give\Framework\PaymentGateways\Exceptions\PaymentGatewayException;
 use Give\Framework\PaymentGateways\PaymentGateway;
 use Give\Helpers\Form\Utils as FormUtils;
@@ -18,6 +19,18 @@ class BECSGateway extends PaymentGateway
 {
     use BECSMandateForm;
     use HandlePaymentIntentStatus;
+
+    /** @var array */
+    protected $errors = [];
+
+    public function __construct(SubscriptionModuleInterface $subscriptionModule = null)
+    {
+        parent::__construct($subscriptionModule);
+
+        $this->errorMessages['accountConfiguredNoSsl']    = esc_html__( 'Mandate form fields are disabled because your site is not running securely over HTTPS.', 'give' );
+        $this->errorMessages['accountNotConfiguredNoSsl'] = esc_html__( 'Mandate form fields are disabled because Stripe is not connected and your site is not running securely over HTTPS.', 'give' );
+        $this->errorMessages['accountNotConfigured']      = esc_html__( 'Mandate form fields are disabled. Please connect and configure your Stripe account to accept donations.', 'give' );
+    }
 
     /**
      * @inheritDoc
