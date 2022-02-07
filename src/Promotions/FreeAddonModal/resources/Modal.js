18559c8ce21d89d94de0df33654d81d3f6b8fbd5
Jason Adams <jason.adams@impress.org>
2022-02-07 12:46:32 -0800

    feature: add modal error and submission states

--- src/Promotions/FreeAddonModal/resources/Modal.js
+++ src/Promotions/FreeAddonModal/resources/Modal.js
@@ -44,31 +44,51 @@ const Modal = () => {
                     alt="Email Reports Icon"
                     className={styles.icon}
                 />
-                <p>
-                    As a special thanks, we’d like to offer you a <span>FREE</span> premium add-on, called Email
-                    Reports!
-                </p>
-                <form onSubmit={handleSubscribe} className={styles.form}>
-                    <div className={styles.fields}>
-                        <label>
-                            <span>First Name</span>
-                            <input type="text" name="firstName" required ref={firstNameInput} />
-                        </label>
-                        <label>
-                            <span>Email Address</span>
-                            <input type="email" name="email" required ref={emailInput} />
-                        </label>
-                    </div>
-                    <GreenButton as="input" text="👉 Get my Free Add-on" shadow />
-                    <em className={styles.disclosure}>
-                        * By submitting this form, you agree to be subscribed to our GiveWP Newsletter (you can
-                        unsubscribe at any time. The free Email Reports add-on is only the open source installable zip
-                        file. It does not include a license or access to priority support.
-                    </em>
-                </form>
-                <a onClick={handleDismiss} href="#">
-                    No thanks!
-                </a>
+                {
+                    userSubscribed ? (
+                        <>
+                            <p>Excellent! We have sent you an email to confirm your submission. Please confirm the email and we'll send you your free add-on!</p>
+                            <GreenButton onClick={() => setIsOpen(false)} text="Close" />
+                        </>
+                    ) : (
+                        <>
+                            {hasSubscriptionError && (
+                                <div className={styles.submissionError}>
+                                    There was an issue submitting your information. Please try again. If the problem
+                                    persists, try refreshing the page. If it still doesn't work, please{' '}
+                                    <a href="https://givewp.com/support" target="_blank">
+                                        contact support.
+                                    </a>
+                                </div>
+                            )}
+                            <p>
+                                As a special thanks, we’d like to offer you a <span>FREE</span> premium add-on, called Email
+                                Reports!
+                            </p>
+                            <form onSubmit={handleSubscribe} className={styles.form}>
+                                <div className={styles.fields}>
+                                    <label>
+                                        <span>First Name</span>
+                                        <input type="text" name="firstName" required ref={firstNameInput} />
+                                    </label>
+                                    <label>
+                                        <span>Email Address</span>
+                                        <input type="email" name="email" required ref={emailInput} />
+                                    </label>
+                                </div>
+                                <GreenButton as="input" text="👉 Get my Free Add-on" shadow />
+                                <em className={styles.disclosure}>
+                                    * By submitting this form, you agree to be subscribed to our GiveWP Newsletter (you can
+                                    unsubscribe at any time. The free Email Reports add-on is only the open source installable zip
+                                    file. It does not include a license or access to priority support.
+                                </em>
+                            </form>
+                            <a onClick={handleDismiss} href="#">
+                                No thanks!
+                            </a>
+                        </>
+                    )
+                }
             </div>
         </div>
     );
