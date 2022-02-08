1cb12ece9d04cc6dccdfe92f2303d973290e539c
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2022-02-08 21:58:06 +0530

    refactor: use if with early return instead of switch

--- src/PaymentGateways/Gateways/PayPalStandard/Webhooks/Listeners/PaymentUpdated.php
+++ src/PaymentGateways/Gateways/PayPalStandard/Webhooks/Listeners/PaymentUpdated.php
@@ -24,37 +24,34 @@ class PaymentUpdated implements EventListener
         $donation = new Give_Payment($eventData->custom);
         $donationStatus = strtolower($eventData->payment_status);
 
-        switch (true) {
-            // Process refunds & reversed.
-            case in_array($donationStatus, ['refunded', 'reversed']):
-                if ('refunded' !== $donation->status) {
-                    Call::invoke(ProcessIpnDonationRefund::class, $eventData, $donation->ID);
-                }
+        // Process refunds & reversed.
+        if (in_array($donationStatus, ['refunded', 'reversed'])) {
+            if ('refunded' !== $donation->status) {
+                Call::invoke(ProcessIpnDonationRefund::class, $eventData, $donation->ID);
+            }
 
-                return;
+            return;
+        }
 
-            // Process completed donations.
-            case 'completed' === $donationStatus:
-                if ('publish' !== $donation->status) {
-                    $donation->add_note(
-                        sprintf( /* translators: %s: Paypal transaction ID */
-                            __('PayPal Transaction ID: %s', 'give'),
-                            $eventData->txn_id
-                        )
-                    );
-                    $donation->transaction_id = $eventData->txn_id;
-                    $donation->status = 'publish';
+        // Process completed donations.
+        if ('completed' === $donationStatus && 'publish' !== $donation->status) {
+            $donation->add_note(
+                sprintf( /* translators: %s: Paypal transaction ID */
+                    __('PayPal Transaction ID: %s', 'give'),
+                    $eventData->txn_id
+                )
+            );
+            $donation->transaction_id = $eventData->txn_id;
+            $donation->status = 'publish';
 
-                    $donation->save();
-                }
-                break;
+            $donation->save();
+            return;
+        }
 
-            // Add note about pending payment.
-            case 'pending' === $donationStatus:
-                if (isset($eventData->pending_reason)) {
-                    $donation->add_note(give_paypal_get_pending_donation_note($eventData->pending_reason));
-                }
-                break;
+        // Add note about pending payment.
+        if ('pending' === $donationStatus && isset($eventData->pending_reason)) {
+            $donation->add_note(give_paypal_get_pending_donation_note($eventData->pending_reason));
+            return;
         }
     }
 }
