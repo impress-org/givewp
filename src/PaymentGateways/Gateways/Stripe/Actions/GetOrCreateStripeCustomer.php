9d35dbf22f7fb05412680bec24b31b7ffb996845
Jon Waldstein <jonwaldstein@Jons-Air.fios-router.home>
2022-02-09 13:53:39 -0500

    fix: undefined variables in local method

--- src/PaymentGateways/Gateways/Stripe/Actions/GetOrCreateStripeCustomer.php
+++ src/PaymentGateways/Gateways/Stripe/Actions/GetOrCreateStripeCustomer.php
@@ -4,7 +4,6 @@ namespace Give\PaymentGateways\Gateways\Stripe\Actions;
 
 use Give\PaymentGateways\DataTransferObjects\GatewayPaymentData;
 use Give\PaymentGateways\Gateways\Stripe\Exceptions\StripeCustomerException;
-use Give\PaymentGateways\Gateways\Stripe\ValueObjects\PaymentMethod;
 use Give\PaymentGateways\Gateways\Stripe\WorkflowAction;
 use Give_Stripe_Customer;
 
@@ -19,28 +18,35 @@ class GetOrCreateStripeCustomer extends WorkflowAction
     {
         $giveStripeCustomer = new Give_Stripe_Customer($paymentData->donorInfo->email);
 
-        if(!$giveStripeCustomer->get_id()) {
+        if (!$giveStripeCustomer->get_id()) {
             throw new StripeCustomerException(__('Unable to find or create stripe customer object.', 'give'));
         }
 
         $this->saveStripeCustomerId($paymentData->donationId, $giveStripeCustomer->get_id());
 
-        $this->bind( $giveStripeCustomer );
+        $this->bind($giveStripeCustomer);
     }
 
-    protected function saveStripeCustomerId( $paymentId, $stripeCustomerId )
+    /**
+     * @unreleased
+     *
+     * @param  int  $donationId
+     * @param  string  $stripeCustomerId
+     * @return void
+     */
+    protected function saveStripeCustomerId($donationId, $stripeCustomerId)
     {
         $donor = new \Give_Donor(
-            give_get_payment_donor_id( $paymentId )
+            give_get_payment_donor_id($donationId)
         );
-        $donor->update_meta( give_stripe_get_customer_key(), $stripeCustomerId );
+
+        $donor->update_meta(give_stripe_get_customer_key(), $stripeCustomerId);
 
         give_insert_payment_note(
-            $paymentData->donationId,
-            sprintf(__('Stripe Customer ID: %s', 'give'), $giveStripeCustomer->get_id())
+            $donationId,
+            sprintf(__('Stripe Customer ID: %s', 'give'), $stripeCustomerId)
         );
-        give_update_meta($paymentData->donationId, give_stripe_get_customer_key(), $giveStripeCustomer->get_id());
-
-        $this->bind($giveStripeCustomer);
+        
+        give_update_meta($donationId, give_stripe_get_customer_key(), $stripeCustomerId);
     }
 }
