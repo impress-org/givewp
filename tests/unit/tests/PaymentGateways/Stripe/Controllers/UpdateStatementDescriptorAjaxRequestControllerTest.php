5b480bb8694f00fe68e2359bfa6923725f6f17cd
Kyle B. Johnson <kyle.johnson@impress.org>
2022-02-17 14:32:13 -0500

    debug: Inspect current user for capabilities.

--- tests/unit/tests/PaymentGateways/Stripe/Controllers/UpdateStatementDescriptorAjaxRequestControllerTest.php
+++ tests/unit/tests/PaymentGateways/Stripe/Controllers/UpdateStatementDescriptorAjaxRequestControllerTest.php
@@ -26,6 +26,10 @@ class UpdateStatementDescriptorAjaxRequestControllerTest extends WP_Ajax_UnitTes
         global $wp_roles;
         $this->assertTrue( get_option( $wp_roles->role_key )[ 'administrator' ][ 'capabilities' ][ 'manage_give_settings' ] );
 
+        var_dump( wp_get_current_user()->data );
+        var_dump( wp_get_current_user()->caps );
+        $this->assertTrue( wp_get_current_user()->allcaps['manage_give_settings'] );
+
         try {
             $this->_handleAjax('edit_stripe_account_statement_descriptor');
         } catch (WPAjaxDieContinueException $e) {
