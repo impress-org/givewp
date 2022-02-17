7ef5f36ed9aee8cf1b3a52e5e7bd9be472f6939f
Jon Waldstein <jonwaldstein@jons-air.mynetworksettings.com>
2022-02-17 14:48:11 -0500

    test: add donation create tests for exceptions

--- tests/unit/tests/Donations/Models/TestDonation.php
+++ tests/unit/tests/Donations/Models/TestDonation.php
@@ -7,6 +7,7 @@ use Give\Donations\Models\Donation;
 use Give\Donations\Repositories\DonationRepository;
 use Give\Donations\ValueObjects\DonationStatus;
 use Give\Framework\Database\DB;
+use Give\Framework\Exceptions\Primitives\InvalidArgumentException;
 use Give\Framework\Models\Traits\InteractsWithTime;
 use Give\PaymentGateways\Gateways\TestGateway\TestGateway;
 
@@ -54,6 +55,51 @@ class TestDonation extends \Give_Unit_Test_Case
         $donationFromDatabase = $repository->getById($donation->id);
 
         $this->assertEquals($donation->getAttributes(), $donationFromDatabase->getAttributes());
+    }
+
+    /**
+     * @unreleased
+     *
+     * @return void
+     *
+     * @throws Exception
+     */
+    public function testCreateShouldThrowExceptionIfAttributesAreNotComplete()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        $donationMissingAmount = Donation::create([
+            'status' => DonationStatus::PENDING(),
+            'gateway' => TestGateway::id(),
+            'currency' => 'USD',
+            'donorId' => 1,
+            'firstName' => 'Bill',
+            'lastName' => 'Murray',
+            'email' => 'billMurray@givewp.com',
+        ]);
+    }
+
+    /**
+     * @unreleased
+     *
+     * @return void
+     *
+     * @throws Exception
+     */
+    public function testCreateShouldThrowExceptionIfAttributeIsInvalidType()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        $donationWithWrongAmountType = Donation::create([
+            'status' => DonationStatus::PENDING(),
+            'gateway' => TestGateway::id(),
+            'amount' => '50',
+            'currency' => 'USD',
+            'donorId' => 1,
+            'firstName' => 'Bill',
+            'lastName' => 'Murray',
+            'email' => 'billMurray@givewp.com',
+        ]);
     }
 
     /**
