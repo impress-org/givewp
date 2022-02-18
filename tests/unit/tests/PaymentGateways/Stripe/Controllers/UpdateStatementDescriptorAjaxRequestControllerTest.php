0f426f24e4bacadc00cb1dcced3a927ab9a31026
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2022-02-18 23:18:56 +0530

    debug: print global role_objects

--- tests/unit/tests/PaymentGateways/Stripe/Controllers/UpdateStatementDescriptorAjaxRequestControllerTest.php
+++ tests/unit/tests/PaymentGateways/Stripe/Controllers/UpdateStatementDescriptorAjaxRequestControllerTest.php
@@ -28,6 +28,7 @@ class UpdateStatementDescriptorAjaxRequestControllerTest extends WP_Ajax_UnitTes
         $this->assertTrue( get_option( $wp_roles->role_key )[ 'administrator' ][ 'capabilities' ][ 'manage_give_settings' ] );
 
         var_dump( $wp_user_roles );
+        var_dump( $wp_roles->role_objects );
         var_dump( $wp_roles->get_role('administrator') );
         var_dump( wp_get_current_user()->data );
         var_dump( wp_get_current_user()->caps );
