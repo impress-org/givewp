66d4e48c65b4ff7b7825510a2ca31023675f9f95
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2022-02-19 02:06:39 +0530

    debug: remove testing code

--- tests/unit/tests/PaymentGateways/Stripe/Traits/HasStripeStatementDescriptorTextTest.php
+++ tests/unit/tests/PaymentGateways/Stripe/Traits/HasStripeStatementDescriptorTextTest.php
@@ -13,13 +13,13 @@ class HasStripeStatementDescriptorTextTest extends TestCase
     public function testTextLessThenFiveLetter()
     {
         $this->expectExceptionMessage('Stripe statement descriptor text contain between 5 - 22 letters, inclusive.');
-        $this->filterStatementDescriptor('demo');
+        $this->validateStatementDescriptor('demo');
     }
 
     public function testTextGreaterThenTwentyTwoLetter()
     {
         $this->expectExceptionMessage('Stripe statement descriptor text contain between 5 - 22 letters, inclusive.');
-        $this->filterStatementDescriptor('This is a long stripe statement descriptor.');
+        $this->validateStatementDescriptor('This is a long stripe statement descriptor.');
     }
 
     public function testTextWithReserveWords()
@@ -27,11 +27,11 @@ class HasStripeStatementDescriptorTextTest extends TestCase
         $this->expectExceptionMessage(
             'Stripe statement descriptor text should not contain any of the special characters <code>< > \ \' " *</code>.'
         );
-        $this->filterStatementDescriptor('* < > " \' \\');
+        $this->validateStatementDescriptor('* < > " \' \\');
     }
 
     public function testTextWithText()
     {
-        $this->assertSame( get_bloginfo('name'), $this->filterStatementDescriptor(get_bloginfo('name')));
+        $this->assertSame( get_bloginfo('name'), $this->validateStatementDescriptor(get_bloginfo('name')));
     }
 }
