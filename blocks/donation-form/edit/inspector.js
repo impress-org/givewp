3c5b3bd5707204d2253bb52ee53a6cd05bbce60b
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2022-03-07 23:25:27 +0530

    fix: use correct package to import inspector controls

--- blocks/donation-form/edit/inspector.js
+++ blocks/donation-form/edit/inspector.js
@@ -2,7 +2,7 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n'
-import { InspectorControls } from '@wordpress/blocks';
+import { InspectorControls } from '@wordpress/block-editor';
 import { Dashicon, Button, PanelBody, SelectControl, ToggleControl, TextControl } from '@wordpress/components';
 import { Component } from '@wordpress/element';
 import {withSelect} from '@wordpress/data';
