6f29a6991be423825e9a212be7327fe33ce5041c
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2022-03-08 22:57:24 +0530

    chore: resolve only wordpress, lodash and jquery package
    externals also remove in favor of @wordpress/dependency-extraction-webpack-plugin packages because it will always resolve these libraries to WordPress globally available libraries.

--- webpack.mix.js
+++ webpack.mix.js
@@ -54,11 +54,6 @@ mix.setPublicPath('assets/dist')
     .copyDirectory('assets/src/fonts', 'assets/dist/fonts');
 
 mix.webpackConfig({
-    externals: {
-        $: 'jQuery',
-        jquery: 'jQuery',
-        lodash: 'lodash',
-    },
     resolve: {
         alias: {
             '@givewp/components': path.resolve(__dirname, 'src/Views/Components/'),
@@ -74,7 +69,42 @@ mix.webpackConfig({
          * - jquery
          * - lodash, lodash-es
          */
-        new DependencyExtractionWebpackPlugin()
+        new DependencyExtractionWebpackPlugin(
+            {
+                useDefaults: false,
+                requestToExternal: (request) => {
+                    const WORDPRESS_NAMESPACE = '@wordpress/';
+
+                    if (request.startsWith(WORDPRESS_NAMESPACE)) {
+                        return [
+                            'wp',
+
+                            /* Transform @wordpress dependencies:
+                            * - request @wordpress/api-fetch becomes [ 'wp', 'apiFetch' ]
+                            * - request @wordpress/i18n becomes [ 'wp', 'i18n' ]
+                            */
+                            request.substring(WORDPRESS_NAMESPACE.length)
+                                .replace(/-([a-z])/g, (_, letter) => letter.toUpperCase()),
+                        ];
+                    } else if (['lodash', 'lodash-es'].includes(request)) {
+                        return 'lodash';
+                    } else if (request === 'jquery') {
+                        return 'jQuery';
+                    }
+                },
+                requestToHandle: (request) => {
+                    const WORDPRESS_NAMESPACE = '@wordpress/';
+
+                    if (request === 'lodash-es') {
+                        return 'lodash';
+                    }
+
+                    if (request.startsWith(WORDPRESS_NAMESPACE)) {
+                        return 'wp-' + request.substring(WORDPRESS_NAMESPACE.length);
+                    }
+                }
+            }
+        )
     ]
 });
 
