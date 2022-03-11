074a46c99a8ff6345fb70335aee579eaedb0b889
Kyle B. Johnson <kyle.johnson@impress.org>
2022-03-11 10:20:28 -0500

    chore: Uncomment code - whoops!

--- src/Framework/PaymentGateways/Routes/RouteSignature.php
+++ src/Framework/PaymentGateways/Routes/RouteSignature.php
@@ -80,7 +80,7 @@ class RouteSignature
      */
     public function createExpirationTimestamp()
     {
-//        Shim::load( 'current_datetime' );
+        Shim::load( 'current_datetime' );
         return (string)current_datetime()->modify('+1 day')->getTimestamp();
     }
 
