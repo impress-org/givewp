57608ea60b751203f672c2c9badece5748ff8263
alaca <ante.laca@gmail.com>
2022-03-15 19:45:34 +0100

    feature: register list donors endpoint

--- src/ServiceProviders/RestAPI.php
+++ src/ServiceProviders/RestAPI.php
@@ -3,6 +3,7 @@
 namespace Give\ServiceProviders;
 
 use Give\DonationForms\Endpoints\ListForms;
+use Give\Donors\Endpoints\ListDonors;
 use Give\DonationForms\Endpoints\FormActions;
 use Give\API\Endpoints\Logs\FlushLogs;
 use Give\API\Endpoints\Logs\GetLogs;
@@ -41,6 +42,7 @@ class RestAPI implements ServiceProvider
         GetLogs::class,
         FlushLogs::class,
         ListForms::class,
+        ListDonors::class,
         FormActions::class,
         GetMigrations::class,
         RunMigration::class,
