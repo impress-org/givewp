83b3dbb01a8d01543ee2ba2dc180787f77cbc750
alaca <ante.laca@gmail.com>
2022-03-16 17:51:34 +0100

    feature: add getDateTime function

--- src/Donors/Endpoints/ListDonors.php
+++ src/Donors/Endpoints/ListDonors.php
@@ -84,7 +84,7 @@ class ListDonors extends Endpoint
                 'name' => $donor->name,
                 'titlePrefix' => $donor->titlePrefix,
                 'donationCount' => $donor->donationCount,
-                'dateCreated' => $donor->createdAt,
+                'dateCreated' => $this->getDateTime($donor->createdAt),
                 'donationRevenue' => $this->formatAmount($donor->donationRevenue),
                 'hasGravatar' => give_validate_gravatar($donor->email)
             ];
@@ -97,6 +97,42 @@ class ListDonors extends Endpoint
                 'pageCount' => $pageCount
             ]
         );
+    }
+
+
+    /**
+     * Returns human readable date.
+     *
+     * @param string $date Date YYYY-MM-DD
+     *
+     * @return string
+     */
+    private function getDateTime($date)
+    {
+        $dateTimestamp = strtotime($date);
+        $currentTimestamp = current_time('timestamp');
+        $todayTimestamp = strtotime('today', $currentTimestamp);
+        $yesterdayTimestamp = strtotime('yesterday', $currentTimestamp);
+
+        if ($dateTimestamp >= $todayTimestamp) {
+            return sprintf(
+                '%1$s %2$s %3$s',
+                esc_html__('Today', 'give'),
+                esc_html__('at', 'give'),
+                date_i18n(get_option('time_format'), $dateTimestamp)
+            );
+        }
+
+        if ($dateTimestamp >= $yesterdayTimestamp) {
+            return sprintf(
+                '%1$s %2$s %3$s',
+                esc_html__('Yesterday', 'give'),
+                esc_html__('at', 'give'),
+                date_i18n(get_option('time_format'), $dateTimestamp)
+            );
+        }
+
+        return date_i18n(get_option('date_format'), $dateTimestamp);
     }
 
     /**
