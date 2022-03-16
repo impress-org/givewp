c4fbc7e20329ab1d15cdc28311f472a61e8c8e3f
Jon Waldstein <jonwaldstein@jons-air.mynetworksettings.com>
2022-03-16 16:17:39 -0400

    docs: update since tag

--- src/Log/Log.php
+++ src/Log/Log.php
@@ -122,7 +122,7 @@ class Log
      * @param  string  $name
      * @param  array  $arguments
      *
-     * @unreleased add debug
+     * @unreleased added conditional for logging debug()
      * @since 2.18.0 - always log errors, warnings & only log all if WP_DEBUG_LOG is enabled
      * @since 2.11.1
      *
