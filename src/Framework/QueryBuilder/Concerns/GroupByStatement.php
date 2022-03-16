854fd497fad2021752cadf6c139c1016c6d3c6c2
alaca <ante.laca@gmail.com>
2022-03-16 06:54:37 +0100

    refactor: prevent duplicate group by columns in groupBy method

--- src/Framework/QueryBuilder/Concerns/GroupByStatement.php
+++ src/Framework/QueryBuilder/Concerns/GroupByStatement.php
@@ -20,15 +20,17 @@ trait GroupByStatement
      */
     public function groupBy($tableColumn)
     {
-        $this->groupByColumns[] = DB::prepare( '%1s', $tableColumn);
+        if (!in_array($tableColumn, $this->groupByColumns, true)) {
+            $this->groupByColumns[] = DB::prepare('%1s', $tableColumn);
+        }
 
         return $this;
     }
 
     protected function getGroupBySQL()
     {
-        return ! empty($this->groupByColumns)
-            ? ['GROUP BY ' . implode(',', array_unique($this->groupByColumns))]
+        return !empty($this->groupByColumns)
+            ? ['GROUP BY ' . implode(',', $this->groupByColumns)]
             : [];
     }
 }
