34e2b5919607f25c02b822cf1e8ff93ebda440b9
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2022-03-22 12:11:45 +0530

    feature: add trait for create stripe source

---
+++ src/PaymentGateways/Gateways/Stripe/Traits/StripeApi/CreateSource.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Give\PaymentGateways\Gateways\Stripe\Traits\StripeApi;
+
+use Exception;
+use Give\PaymentGateways\Gateways\Stripe\Exceptions\StripeApiRequestException;
+use Give\PaymentGateways\Stripe\ApplicationFee;
+use Stripe\Source;
+
+/**
+ * @unreleased
+ */
+class CreateSource
+{
+    /**
+     * @unreleased
+     *
+     * @param array $stripeSourceRequestArgs
+     * @param int $formId
+     *
+     * @return Source
+     * @throws StripeApiRequestException
+     */
+    public function createCharge($stripeSourceRequestArgs, $formId = 0)
+    {
+        give_stripe_set_app_info($formId);
+
+        try {
+            // Charge application fee, only if the Stripe premium add-on is not active.
+            if (ApplicationFee::canAddfee()) {
+                $stripeSourceRequestArgs['application_fee_amount'] = give_stripe_get_application_fee_amount(
+                    $stripeSourceRequestArgs['amount']
+                );
+            }
+
+            return Source::create(
+                $stripeSourceRequestArgs,
+                give_stripe_get_connected_account_options()
+            );
+        } catch (Exception $e) {
+            throw new StripeApiRequestException(
+                sprintf(
+                /* translators: 1: Exception Error Message */
+                    esc_html__('Unable to create a successful source. Details: %1$s', 'give'),
+                    $e->getMessage()
+                )
+            );
+        }
+    }
+}
