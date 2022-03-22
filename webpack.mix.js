29a61021ee3240072d9438842e29c20a602a3acd
Jon Waldstein <jonwaldstein@jons-air.mynetworksettings.com>
2022-03-22 17:57:40 -0400

    refactor: match package, mix, and babel closer to core for easier setup and merge later

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,6 @@
-const path = require('path');
 const mix = require('laravel-mix');
 const wpPot = require('wp-pot');
+const DependencyExtractionWebpackPlugin = require('@wordpress/dependency-extraction-webpack-plugin');
 
 mix.setPublicPath('public')
     .sourceMaps(false)
@@ -23,12 +23,51 @@ mix.setPublicPath('public')
     // images
     .copy('src/NextGen/resources/images/*.{jpg,jpeg,png,gif}', 'public/images');
 
-mix.webpackConfig( {
-	externals: {
-		$: 'jQuery',
-		jquery: 'jQuery',
-	},
-} );
+mix.webpackConfig({
+    plugins: [
+        /*
+         * Transform script dependencies only for following external libraries:
+         * - @wordpress/
+         * - jquery
+         * - lodash, lodash-es
+         */
+        new DependencyExtractionWebpackPlugin({
+            useDefaults: false,
+            requestToExternal: (request) => {
+                const WORDPRESS_NAMESPACE = '@wordpress/';
+
+                if (request.startsWith(WORDPRESS_NAMESPACE)) {
+                    return [
+                        'wp',
+
+                        /* Transform @wordpress dependencies:
+                         * - request @wordpress/api-fetch becomes [ 'wp', 'apiFetch' ]
+                         * - request @wordpress/i18n becomes [ 'wp', 'i18n' ]
+                         */
+                        request
+                            .substring(WORDPRESS_NAMESPACE.length)
+                            .replace(/-([a-z])/g, (_, letter) => letter.toUpperCase()),
+                    ];
+                } else if (['lodash', 'lodash-es'].includes(request)) {
+                    return 'lodash';
+                } else if (request === 'jquery') {
+                    return 'jQuery';
+                }
+            },
+            requestToHandle: (request) => {
+                const WORDPRESS_NAMESPACE = '@wordpress/';
+
+                if (request === 'lodash-es') {
+                    return 'lodash';
+                }
+
+                if (request.startsWith(WORDPRESS_NAMESPACE)) {
+                    return 'wp-' + request.substring(WORDPRESS_NAMESPACE.length);
+                }
+            },
+        }),
+    ],
+});
 
 if ( mix.inProduction() ) {
 	wpPot( {
