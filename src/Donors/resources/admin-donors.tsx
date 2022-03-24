4cb6668bde73d6f91f1b40267f4b413fd0d70fb4
Jessica Thomas <lidgrt@gmail.com>
2022-03-24 11:24:31 -0500

    feature: add root js for donors page

---
+++ src/Donors/resources/admin-donors.tsx
@@ -0,0 +1,10 @@
+import {StrictMode} from 'react';
+import ReactDOM from 'react-dom';
+import './admin-donors.module.scss';
+
+ReactDOM.render(
+    <StrictMode>
+        {/*<DonorsListTable />*/}
+    </StrictMode>,
+    document.getElementById('give-admin-donors-root')
+);
