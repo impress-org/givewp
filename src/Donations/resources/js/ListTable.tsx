cc635244760a5ddc58363bceec634c6280fe1af0
alaca <ante.laca@gmail.com>
2022-03-28 12:52:49 +0200

    feature: Donation ListTable

---
+++ src/Donations/resources/js/ListTable.tsx
@@ -0,0 +1,107 @@
+import {__} from '@wordpress/i18n';
+import {useSWRConfig} from 'swr';
+import {ListTableColumn, ListTablePage} from '@givewp/components';
+import RowAction from "@givewp/components/ListTable/RowAction";
+import ListTableApi from '@givewp/components/ListTable/api';
+
+declare global {
+    interface Window {
+        GiveDonations;
+    }
+}
+
+export default function () {
+
+    const API = new ListTableApi(window.GiveDonations);
+    const {mutate} = useSWRConfig();
+
+    const rowActions = ({item, removeRow, setUpdateErrors, parameters}) => {
+
+        const fetchAndUpdateErrors = async (parameters, endpoint, id, method) => {
+            const response = await API.fetchWithArgs(endpoint, {ids: [id]}, method);
+            setUpdateErrors(response);
+            await mutate(parameters);
+            return response;
+        }
+
+        return (
+            <>
+                <RowAction
+                    href={`/wp-admin/edit.php?post_type=give_forms&page=give-payment-history&view=view-payment-details&id=${item.id}`}
+                    displayText={__('Edit', 'give')}
+                />
+                <RowAction
+                    onClick={removeRow(async () => await fetchAndUpdateErrors(parameters, '/delete', item.id, 'DELETE'))}
+                    actionId={item.id}
+                    displayText={__('Delete', 'give')}
+                    hiddenText={item.name}
+                    highlight
+                />
+            </>
+        );
+    }
+
+    const columns: Array<ListTableColumn> = [
+        {
+            name: 'id',
+            text: __('ID', 'give'),
+            heading: true,
+            preset: 'idBadge'
+        },
+        {
+            name: 'amount',
+            text: __('Amount', 'give'),
+        },
+        {
+            name: 'createdAt',
+            text: __('Date', 'give'),
+        },
+        {
+            name: 'firstName',
+            text: __('Donor Name', 'give'),
+            render: (donation: { firstName, lastName }) => (
+                <>
+                    {donation.firstName}{' '}{donation.lastName}
+                </>
+            )
+        },
+        {
+            name: 'formTitle',
+            text: __('Donation Form', 'give')
+        },
+        {
+            name: 'status',
+            text: __('Status', 'give'),
+            preset: 'postStatus',
+        },
+
+    ];
+
+    const filters = [
+        {
+            name: 'search',
+            type: 'search',
+            text: __('Name, Email, or Donation ID', 'give'),
+            ariaLabel: __('Search donations', 'give')
+        },
+        // {
+        //     name: 'form',
+        //     type: 'formselect',
+        //     text: __('Select Form', 'give'),
+        //     ariaLabel: __('Filter donation forms by status', 'give'),
+        //     options: window.GiveDonations.forms
+        // }
+    ]
+
+    return (
+        <ListTablePage
+            title={__('Donations', 'give')}
+            singleName={__('Donation', 'give')}
+            pluralName={__('Donations', 'give')}
+            columns={columns}
+            rowActions={rowActions}
+            apiSettings={window.GiveDonations}
+            filterSettings={filters}
+        />
+    )
+}
