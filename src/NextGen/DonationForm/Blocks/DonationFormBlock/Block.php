4d14b88fc8d93431092ca49167aed6b8aefc9a11
Jon Waldstein <jonwaldstein@jons-air.mynetworksettings.com>
2022-03-28 18:51:13 -0400

    feature: scaffold donation form from fields api

--- src/NextGen/DonationForm/Blocks/DonationFormBlock/Block.php
+++ src/NextGen/DonationForm/Blocks/DonationFormBlock/Block.php
@@ -3,6 +3,11 @@
 namespace Give\NextGen\DonationForm\Blocks\DonationFormBlock;
 
 use Give\Framework\EnqueueScript;
+use Give\Framework\FieldsAPI\Email;
+use Give\Framework\FieldsAPI\Exceptions\EmptyNameException;
+use Give\Framework\FieldsAPI\Form;
+use Give\Framework\FieldsAPI\Hidden;
+use Give\Framework\FieldsAPI\Text;
 
 class Block
 {
@@ -25,9 +30,17 @@ class Block
      * @param  array  $attributes
      *
      * @return string
+     * @throws EmptyNameException
      */
     public function render($attributes)
     {
+        $donationForm = $this->createForm($attributes);
+
+        $exports = [
+            'attributes' => $attributes,
+            'form' => $donationForm->jsonSerialize()
+        ];
+
         // enqueue front-end scripts
         // since this is using render_callback viewScript in blocks.json will not work.
         $enqueueScripts = new EnqueueScript(
@@ -44,9 +57,46 @@ class Block
 
         <div id="root-give-next-gen-donation-form-block"></div>
 
-        <script>window.giveNextGenExports = <?= wp_json_encode(compact('attributes')) ?>;</script>
+        <script>window.giveNextGenExports = <?= wp_json_encode($exports) ?>;</script>
 
         <?php
         return ob_get_clean();
+    }
+
+    /**
+     * @unreleased
+     *
+     * @param  array  $attributes
+     * @return Form
+     * @throws EmptyNameException
+     */
+    private function createForm($attributes)
+    {
+        $donationForm = new Form('DonationForm');
+
+        $donationForm->append(
+            Text::make('firstName')
+                ->label(__('First Name', 'give'))
+                ->required(),
+
+            Text::make('lastName')
+                ->label(__('Last Name', 'give'))
+                ->required(),
+
+            Email::make('email')
+                ->label(__('Email', 'give'))
+                ->required()
+                ->emailTag('email'),
+
+            Text::make('donationAmount')
+                ->label(__('Donation Amount', 'give'))
+                ->defaultValue(50)
+                ->required(),
+
+            Hidden::make('formId')
+                ->defaultValue($attributes['formId'])
+        );
+
+        return $donationForm;
     }
 }
