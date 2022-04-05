f09b4582fa2e4c7274fbcac3d9a8476e0ee0cfca
Jessica Thomas <lidgrt@gmail.com>
2022-04-05 12:46:30 -0500

    refactor: rename View Donor row action to Edit

--- src/Donors/resources/components/DonorsRowActions.tsx
+++ src/Donors/resources/components/DonorsRowActions.tsx
@@ -19,7 +19,7 @@ export function DonorsRowActions({data, item, removeRow, addRow, setUpdateErrors
         <>
             <RowAction
                 href={`/wp-admin/edit.php?post_type=give_forms&page=give-donors&view=overview&id=${item.id}`}
-                displayText={__('View Donor', 'give')}
+                displayText={__('Edit', 'give')}
             />
             <RowAction
                 onClick={removeRow(async () => await fetchAndUpdateErrors(parameters, '/delete', item.id, 'DELETE'))}
