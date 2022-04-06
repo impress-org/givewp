c38e66ac3da6686158de1b9c7bc38fb9f5ae5fba
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2022-04-06 17:12:39 +0530

    feature: add new functions to subscriotion module contract

--- src/Framework/PaymentGateways/Contracts/SubscriptionModuleInterface.php
+++ src/Framework/PaymentGateways/Contracts/SubscriptionModuleInterface.php
@@ -5,6 +5,7 @@ namespace Give\Framework\PaymentGateways\Contracts;
 use Give\Framework\PaymentGateways\Commands\GatewayCommand;
 use Give\PaymentGateways\DataTransferObjects\GatewayPaymentData;
 use Give\PaymentGateways\DataTransferObjects\GatewaySubscriptionData;
+use Give\Subscriptions\Models\Subscription;
 
 interface SubscriptionModuleInterface
 {
@@ -13,10 +14,44 @@ interface SubscriptionModuleInterface
      *
      * @since 2.18.0
      *
-     * @param  GatewayPaymentData  $paymentData
-     * @param  GatewaySubscriptionData  $subscriptionData
+     * @param GatewayPaymentData $paymentData
+     * @param GatewaySubscriptionData $subscriptionData
      *
      * @return GatewayCommand
      */
     public function createSubscription(GatewayPaymentData $paymentData, GatewaySubscriptionData $subscriptionData);
+
+    /**
+     * Return flag whether subscription cancelable.
+     *
+     * @unreleased
+     *
+     * @param Subscription $subscriptionModel
+     *
+     * @return GatewayCommand
+     */
+    public function canCancelSubscription(Subscription $subscriptionModel);
+
+    /**
+     * Return flag whether subscription synchronizable.
+     *
+     * @unreleased
+     *
+     * @param Subscription $subscriptionModel
+     *
+     * @return GatewayCommand
+     */
+    public function canSyncSubscriptionWithPaymentGateway(Subscription $subscriptionModel);
+
+    /**
+     * Return flag whether subscription editable.
+     * This will cover subscription amount and subscription's payment method like donor credit card number.
+     *
+     * @unreleased
+     *
+     * @param Subscription $subscriptionModel
+     *
+     * @return GatewayCommand
+     */
+    public function canUpdateSubscription(Subscription $subscriptionModel);
 }
