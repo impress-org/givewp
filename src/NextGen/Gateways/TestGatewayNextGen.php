25c61081364d143fd08683be57ed3f6a68f3f5cf
Jon Waldstein <jonwaldstein@Jons-MacBook-Air.local>
2022-04-07 12:41:29 -0400

    feature: add a new gateway for testing next gen

---
+++ src/NextGen/Gateways/TestGatewayNextGen.php
@@ -0,0 +1,91 @@
+<?php
+namespace Give\NextGen\Gateways;
+
+use Give\Framework\FieldsAPI\Contracts\Node;
+use Give\Framework\FieldsAPI\Group;
+use Give\Framework\PaymentGateways\Commands\RespondToBrowser;
+use Give\Framework\PaymentGateways\PaymentGateway;
+use Give\Helpers\Form\Utils as FormUtils;
+use Give\PaymentGateways\DataTransferObjects\GatewayPaymentData;
+use Give\PaymentGateways\Gateways\TestGateway\Views\LegacyFormFieldMarkup;
+
+/**
+ * @unreleased
+ */
+class TestGatewayNextGen extends PaymentGateway
+{
+    /**
+     * @inheritDoc
+     */
+    public static function id()
+    {
+        return 'test-gateway-next-gen';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getId()
+    {
+        return self::id();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getName()
+    {
+        return __('Test Gateway Next Gen', 'give');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getPaymentMethodLabel()
+    {
+        return __('Test Gateway Next Gen', 'give');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getLegacyFormFieldMarkup($formId, $args)
+    {
+        if (FormUtils::isLegacyForm($formId)) {
+            return false;
+        }
+
+        /** @var LegacyFormFieldMarkup $legacyFormFieldMarkup */
+        $legacyFormFieldMarkup = give(LegacyFormFieldMarkup::class);
+
+        return $legacyFormFieldMarkup();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function createPayment(GatewayPaymentData $paymentData)
+    {
+        $transactionId = "test-gateway-transaction-id-{$paymentData->donationId}";
+
+         give_update_payment_status($paymentData->donationId);
+
+         give_set_payment_transaction_id($paymentData->donationId, $transactionId);
+
+         //return new PaymentComplete();
+
+        return new RespondToBrowser([
+            'donationId' => $paymentData->donationId,
+            'redirectUrl' => give_get_success_page_uri(),
+            'status' => "Complete"
+        ]);
+    }
+
+    /**
+     * @return Node
+     */
+    public function getPaymentFields()
+    {
+        return Group::make($this->getId());
+    }
+}
--- src/NextGen/ServiceProvider.php
+++ src/NextGen/ServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace Give\NextGen;
 
+use Give\Framework\PaymentGateways\PaymentGatewayRegister;
+use Give\NextGen\Gateways\TestGatewayNextGen;
 use Give\ServiceProviders\ServiceProvider as ServiceProviderInterface;
 
 /**
@@ -14,7 +16,6 @@ class ServiceProvider implements ServiceProviderInterface
      */
     public function register()
     {
-
     }
 
     /**
@@ -22,6 +23,8 @@ class ServiceProvider implements ServiceProviderInterface
      */
     public function boot()
     {
-
+        add_action('give_register_payment_gateway', function (PaymentGatewayRegister $registrar) {
+            $registrar->registerGateway(TestGatewayNextGen::class);
+        });
     }
 }
