4803f7d40f57099be173dc6738a49b2526c63534
Jessica Thomas <lidgrt@gmail.com>
2022-04-21 15:36:17 -0500

    fix: potential fix for bulk actions sometimes not opening modal

--- src/Views/Components/ListTable/BulkActionSelect.tsx
+++ src/Views/Components/ListTable/BulkActionSelect.tsx
@@ -2,15 +2,24 @@ import pageStyles from "@givewp/components/ListTable/ListTablePage.module.scss";
 import styles from './BulkActionSelect.module.scss';
 import {__} from "@wordpress/i18n";
 import Select from "@givewp/components/ListTable/Select";
+import {useState} from "react";
+
+let selected = '';
 
 export const BulkActionSelect = ({bulkActions = null, showModal, data, parameters}) => {
+    const [selectedState, setSelectedState] = useState(selected);
     if(!bulkActions){
         return null;
     }
 
+    const changeSelected = (event) => {
+        selected = event.target.value;
+        setSelectedState(event.target.value);
+    }
+
     return (
         <form id={styles.bulkActionsForm} onSubmit={showModal}>
-            <Select name='giveListTableBulkActions'>
+            <Select name='giveListTableBulkActions' value={selectedState} onChange={changeSelected}>
                 <option value=''>{__('Bulk Actions', 'give')}</option>
                 {bulkActions.map(action => {
                     if (typeof action?.isVisible == 'function' && !action.isVisible(data, parameters)) {
