0d5933c3f1c09518a50e10b72b5c20c25ce619b4
alaca <ante.laca@gmail.com>
2022-05-02 13:34:50 +0200

    refactor: use `DonationFormsResponseData` DTO

--- src/DonationForms/Endpoints/ListForms.php
+++ src/DonationForms/Endpoints/ListForms.php
@@ -3,7 +3,7 @@
 namespace Give\DonationForms\Endpoints;
 
 use Give\DonationForms\Controllers\DonationFormsRequestController;
-use Give\Helpers\Date;
+use Give\DonationForms\DataTransferObjects\DonationFormsResponseData;
 use WP_REST_Request;
 use WP_REST_Response;
 
@@ -82,19 +82,7 @@ class ListForms extends Endpoint
         $totalPages = (int)ceil($totalForms / $request->get_param('perPage'));
 
         foreach ($forms as $form) {
-            $data[] = [
-                'id' => $form->id,
-                'name' => $form->title,
-                'status' => $form->status,
-                'goal' => $form->goalEnabled === 'enabled' ? $this->getGoal($form->id) : false,
-                'donations' => give()->donationFormsRepository->getFormDonationsCount($form->id),
-                'amount' => $this->getFormAmount($form),
-                'revenue' => $this->formatAmount($form->revenue ?? ''),
-                'datetime' => Date::getDateTime($form->createdAt),
-                'shortcode' => sprintf('[give_form id="%d"]', $form->id),
-                'permalink' => html_entity_decode(get_permalink($form->id)),
-                'edit' => html_entity_decode(get_edit_post_link($form->id))
-            ];
+            $data[] = DonationFormsResponseData::fromObject($form)->toArray();
         }
 
         return new WP_REST_Response(
@@ -105,71 +93,5 @@ class ListForms extends Endpoint
                 'trash' => defined('EMPTY_TRASH_DAYS') && EMPTY_TRASH_DAYS > 0,
             ]
         );
-    }
-
-    /**
-     * @param int $formId
-     *
-     * @return array
-     */
-    private function getGoal(int $formId): array
-    {
-        $goal = give_goal_progress_stats($formId);
-
-        $getFormatFromGoal = static function ($goal) {
-            switch ($goal['format']) {
-                case 'donation':
-                    return _n('donation', 'donations', $goal['raw_goal'], 'give');
-
-                case 'donors':
-                    return _n('donor', 'donors', $goal['raw_goal'], 'give');
-
-                case 'amount':
-                    return __('amount', 'give');
-
-                case 'percentage':
-                    return __('percentage', 'give');
-
-                default:
-                    return '';
-            }
-        };
-
-        return [
-            'actual' => html_entity_decode($goal['actual']),
-            'goal' => html_entity_decode($goal['goal']),
-            'progress' => html_entity_decode($goal['progress']),
-            'format' => $getFormatFromGoal($goal)
-        ];
-    }
-
-    /**
-     * @param object $form
-     *
-     *
-     * @return string
-     */
-    private function getFormAmount($form): string
-    {
-        $donationLevels = unserialize($form->donationLevels, ['allowed_classes' => false]);
-
-        if (
-            is_array($donationLevels)
-            && $amount = array_column($donationLevels, '_give_amount')
-        ) {
-            return $this->formatAmount(min($amount)) . ' - ' . $this->formatAmount(max($amount));
-        }
-
-        return $this->formatAmount($form->setPrice ?? '');
-    }
-
-    /**
-     * @param string $amount
-     *
-     * @return string
-     */
-    private function formatAmount(string $amount): string
-    {
-        return html_entity_decode(give_currency_filter(give_format_amount($amount)));
     }
 }
