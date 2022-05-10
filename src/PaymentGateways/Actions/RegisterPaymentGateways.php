5b1bb4a896642c0197c4a703e36be12c4b463581
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2022-05-10 11:47:00 +0530

    chore: revert test code

--- src/PaymentGateways/Actions/RegisterPaymentGateways.php
+++ src/PaymentGateways/Actions/RegisterPaymentGateways.php
@@ -10,7 +10,6 @@ use Give\PaymentGateways\Gateways\Stripe\BECSGateway as StripeBECSGateway;
 use Give\PaymentGateways\Gateways\Stripe\CheckoutGateway as StripeCheckoutGateway;
 use Give\PaymentGateways\Gateways\Stripe\CreditCardGateway as StripeCreditCardGateway;
 use Give\PaymentGateways\Gateways\Stripe\SEPAGateway as StripeSEPAGateway;
-use Give\PaymentGateways\Gateways\TestGateway\TestGateway;
 use Give\PaymentGateways\PayPalCommerce\PayPalCommerce;
 
 class RegisterPaymentGateways
@@ -22,7 +21,7 @@ class RegisterPaymentGateways
      */
     public $gateways = [
         // When complete, the Test Gateway will eventually replace The legacy Manual Gateway.
-        TestGateway::class,
+        // TestGateway::class,
         //TestGatewayOffsite::class,
         StripeBECSGateway::class,
         StripeCheckoutGateway::class,
