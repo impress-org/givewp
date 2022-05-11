920cca3adb8c2bc013768cc44db390b2f0151608
alaca <ante.laca@gmail.com>
2022-05-11 18:03:39 +0200

    feature: add ColumnIdCollisionException exception

---
+++ src/Framework/ListTableAPI/Exceptions/ColumnIdCollisionException.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Give\Framework\ListTableAPI\Exceptions;
+
+use Give\Framework\Exceptions\Primitives\Exception;
+
+/**
+ * @unreleased
+ */
+class ColumnIdCollisionException extends Exception
+{
+    public function __construct($id, $code = 0, Exception $previous = null)
+    {
+        $message = "Column with id \"$id\" already exist";
+        parent::__construct($message, $code, $previous);
+    }
+}
