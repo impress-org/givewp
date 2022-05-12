62cea765bd459977e299d900ce6ff0b3aeb8d919
alaca <ante.laca@gmail.com>
2022-05-12 10:14:52 +0200

    feature: add concerns

---
+++ src/Framework/ListTable/Concerns/Items.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Give\Framework\ListTable\Concerns;
+
+/**
+ * @unreleased
+ */
+trait Items
+{
+    /**
+     * @var array
+     */
+    private $items = [];
+
+    /**
+     * Set table items
+     *
+     * @unreleased
+     */
+    public function items(array $items)
+    {
+        $data = [];
+
+        foreach ($items as $row) {
+            foreach ($row as $name => $value) {
+                if ($column = $this->getColumnById($name)) {
+                    $row[$name] = $column->applyFilters($value, $row);
+                }
+            }
+
+            $data[] = $row;
+        }
+
+        $this->items = $data;
+    }
+
+    /**
+     * Sort table items by column
+     *
+     * @unreleased
+     */
+    public function sortItems(string $column, string $direction = 'ASC')
+    {
+        $sortColumn = array_column($this->items, $column);
+        $sortDirection = ('DESC' === strtoupper($direction)) ? SORT_DESC : SORT_ASC;
+
+        array_multisort($sortColumn, $sortDirection, $this->items);
+    }
+
+    /**
+     * @unreleased
+     */
+    public function getItems(): array
+    {
+        return $this->items;
+    }
+}
