ea3f7d4e35869cc7be2178d0866d4dfd0164b945
alaca <ante.laca@gmail.com>
2022-05-12 10:13:43 +0200

    feature: add ListTable class

---
+++ src/Framework/ListTable/ListTable.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Give\Framework\ListTable;
+
+use Give\Framework\ListTable\Concerns\Columns;
+use Give\Framework\ListTable\Concerns\Items;
+
+/**
+ * @unreleased
+ */
+abstract class ListTable
+{
+    use Columns;
+    use Items;
+
+    /**
+     * @unreleased
+     * @throws Exceptions\ColumnIdCollisionException
+     */
+    public function __construct()
+    {
+        $this->addColumns(...$this->columns());
+    }
+
+    /**
+     * Get table ID
+     *
+     * @unreleased
+     */
+    abstract public function id(): string;
+
+    /**
+     * Register table columns
+     *
+     * @unreleased
+     * @return Column[]
+     */
+    abstract public function columns(): array;
+
+    /**
+     * Get table definitions
+     *
+     * @unreleased
+     */
+    public function getTable(): array
+    {
+        return [
+            'id' => $this->id(),
+            'columns' => $this->getColumns()
+        ];
+    }
+}
+
