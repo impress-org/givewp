9d3e45e13dd5168a4fdb29df1ad3b7ddd78b4e8c
alaca <ante.laca@gmail.com>
2022-05-16 17:42:20 +0200

    refactor: update tests

--- tests/unit/tests/Framework/ListTable/DonationsListTableColumnsTest.php
+++ tests/unit/tests/Framework/ListTable/DonationsListTableColumnsTest.php
@@ -171,33 +171,6 @@ final class DonationsListTableColumnsTest extends TestCase
     /**
      * @return void
      */
-    public function testItShouldSortItemsBasedOnColumnRowValue()
-    {
-        $items = [
-            [
-                'id' => 1,
-                'formTitle' => 'Donation Form 1',
-            ],
-            [
-                'id' => 2,
-                'formTitle' => 'Donation Form 2',
-            ]
-        ];
-
-        $listTable = give(DonationsListTable::class);
-
-        $listTable->items($items);
-        $listTable->sortItems('id', 'desc');
-
-        $listTableItems = $listTable->getItems();
-
-        $this->assertEquals('Donation Form 2', $listTableItems[0]['formTitle']);
-        $this->assertEquals('Donation Form 1', $listTableItems[1]['formTitle']);
-    }
-
-    /**
-     * @return void
-     */
     public function testItShouldFilterColumnRowValue()
     {
         $items = [
