0ca55a22214107d41fd033749898ba1743fec1dc
Joshua Dinh <josh@joshuadinh.tech>
2022-05-25 07:50:27 -0700

    feature: add conditional styling upon selection

--- blocks/components/toggle/index.js
+++ blocks/components/toggle/index.js
@@ -5,7 +5,12 @@ export default function ({options, onClick, selected}) {
     return (
         <div className="give-toggle">
             {options.map((value, i) => {
-                return  <div key={i} className="give-toggle__option"  onClick={onClick}>{value}</div>
+                return  (<div
+                            key={i}
+                            onClick={() => onClick(i)}
+                            className="give-toggle__option" style={ selected === i ? {background: '#007cba', color: 'white'} : {}}>
+                            {value}
+                        </div>)
             })}
         </div>
     );
