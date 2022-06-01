5daf0f5bc0cc460329e11f3b5628f60fb7ee85eb
Joshua Dinh <josh@joshuadinh.tech>
2022-06-01 09:15:32 -0700

    feature: update inspector controls

--- blocks/donation-form-grid/edit/inspector.js
+++ blocks/donation-form-grid/edit/inspector.js
@@ -6,7 +6,7 @@ import { select } from '@wordpress/data';
 import {InspectorControls, store } from '@wordpress/block-editor';
 import {
     ColorPalette,
-    FormTokenField,
+    FormTokenField, Panel,
     PanelBody,
     SelectControl,
     TextControl,
@@ -20,6 +20,8 @@ import ColumnSelector from '../../components/column-selector';
  */
 import giveFormOptions from '../data/options';
 import giveDonorWallOptions from "../../donor-wall/data/options";
+import ToggleOptions from "../../components/toggle";
+import Filter from "../../components/filter";
 
 /**
  * Render Inspector Controls
@@ -79,211 +81,134 @@ const Inspector = ({attributes, setAttributes}) => {
 
     return (
         <InspectorControls key="inspector">
-            <PanelBody title={__('Display Appearance', 'give')}>
-                <ColumnSelector
-                    label={__('Columns', 'give')}
-                    selected={columns}
-                    onClick={(value) => saveSetting('columns', value)}
-                    help={__('Controls how many columns of the Form Grid appear. "Best Fit" will adjust responsively to the space available.', 'give')}
-                />
-                <SelectControl
-                    label={ __( 'Columns', 'give' ) }
-                    name="columns"
-                    value={ columns }
-                    options={ giveFormOptions.columns }
-                    onChange={ ( value ) => saveSetting( 'columns', value ) } />
-                <ToggleControl
-                    name="showTitle"
-                    label={__('Show Title', 'give')}
-                    checked={!!showTitle}
-                    onChange={(value) => saveSetting('showTitle', value)}/>
-
-                <ToggleControl
-                    name="showExcerpt"
-                    label={__('Show Excerpt', 'give')}
-                    checked={!!showExcerpt}
-                    onChange={(value) => saveSetting('showExcerpt', value)}
-                />
-
-                <TextControl
-                    name="excerptLength"
-                    label={__('Excerpt Length', 'give')}
-                    value={excerptLength || ''}
-                    className={!showExcerpt && 'hidden'}
-                    onChange={(value) => saveSetting('excerptLength', Number.parseInt(value) || 0)}
-                />
-
-                <ToggleControl
-                    name="showGoal"
-                    label={__('Show Goal', 'give')}
-                    checked={!!showGoal}
-                    onChange={(value) => saveSetting('showGoal', value)}
-                />
-
-                {showGoal && (
-                    <ToggleControl
-                        name="showProgressBar"
-                        label={__('Show Progress Bar', 'give')}
-                        checked={!!showProgressBar}
-                        onChange={(value) => saveSetting('showProgressBar', value)}
+            <Panel>
+                <PanelBody title= {__('Layout', 'give')} initialOpen={ true }>
+                    <ColumnSelector
+                        label={__('Columns', 'give')}
+                        selected={columns}
+                        onClick={(value) => saveSetting('columns', value)}
+                        help={__('Controls how many columns of the Form Grid appear. "Best Fit" will adjust responsively to the space available.', 'give')}
                     />
-                )}
+                    <SelectControl
+                        label={ __( 'Columns', 'give' ) }
+                        name="columns"
+                        value={ columns }
+                        options={ giveFormOptions.columns }
+                        onChange={ ( value ) => saveSetting( 'columns', value ) } />
+                </PanelBody>
+            </Panel>
+            <Panel>
+                <PanelBody title= {__('Display Elements', 'give')} initialOpen={ true }>
+                    <ToggleControl
+                        name="showTitle"
+                        label={__('Show Title', 'give')}
+                        checked={!!showTitle}
+                        onChange={(value) => saveSetting('showTitle', value)}/>
+                    <ToggleControl
+                        name="showGoal"
+                        label={__('Show Goal', 'give')}
+                        checked={!!showGoal}
+                        onChange={(value) => saveSetting('showGoal', value)}
+                    />
+                    <ToggleControl
+                        name="showExcerpt"
+                        label={__('Show Excerpt', 'give')}
+                        checked={!!showExcerpt}
+                        onChange={(value) => saveSetting('showExcerpt', value)}
+                    />
+                    <ToggleControl
+                        align="right"
+                        name="showFeaturedImage"
+                        label={__('Show Featured Image', 'give')}
+                        checked={!!showFeaturedImage}
+                        onChange={(value) => saveSetting('showFeaturedImage', value)}
+                    />
+                </PanelBody>
+            </Panel>
+            <Panel>
+                <PanelBody title= {__('Grid Settings', 'give')} initialOpen={ true }>
+                    <SelectControl
+                        label={__('Order By', 'give')}
+                        name="orderBy"
+                        value={orderBy}
+                        options={giveFormOptions.orderBy}
+                        onChange={(value) => saveSetting('orderBy', value)}
+                        help={__('The order forms are displayed in.', 'give')}
+                    />
 
-                <ToggleControl
-                    align="right"
-                    name="showFeaturedImage"
-                    label={__('Show Featured Image', 'give')}
-                    checked={!!showFeaturedImage}
-                    onChange={(value) => saveSetting('showFeaturedImage', value)}
-                />
-
-                {showFeaturedImage && (
-                    <>
-                        <SelectControl
-                            name="imageSize"
-                            label={__('Image Size', 'give')}
-                            value={imageSize}
-                            options={getImageSizes()}
-                            onChange={(value) => saveSetting('imageSize', value)}
-                            help={__('Featured image size. Default "medium." Accepts WordPress image sizes, which by default are "thumbnail," "medium," "large," or "full." ', 'give')}
+                    <SelectControl
+                        label={__('Order', 'give')}
+                        name="order"
+                        value={order}
+                        options={giveFormOptions.order}
+                        onChange={(value) => saveSetting('order', value)}
+                        help={__('Whether the order ascends or descends.', 'give')}
+                    />
+                    <SelectControl className="" label={ __( 'Filter', 'give' ) } name="filter" value={ filter } options={ giveDonorWallOptions.filter } onChange={ ( value ) => saveSetting( 'filter', value ) } />
+                    <Filter
+                        filter={filter}
+                        TextControls ={[
+                            {name:"ids", value: ids,  onChange: ( value ) => saveSetting( 'ids', value ), filterValue: 'Donor ID', help: __('By Default, donations to all forms will display. Use this setting to restrict the donor to display only donations to certains forms. Use a comma-separated list of form IDs.', 'give') },
+                            {name:"formID", value: formID, onChange: ( value ) => saveSetting( 'formID',  value ), filterValue: 'Form ID', help: __('By default, all donors will display. Use this setting to restrict the donor wall to only display certain donors. Use a comma-separated list of donor IDs.', 'give') },
+                            {name:"categories", value: categories , onChange:  ( value ) => saveSetting( 'categories', value ), filterValue: 'Categories'},
+                            {name:"tags", value: tags , onChange: ( value ) => saveSetting( 'tags', value ), filterValue: 'Tags'},
+                            {name: "onlyComments", checked: !!onlyComments, onChange: (value) => saveSetting('onlyComments', value), filterValue: 'Donors with comments'}
+                        ]}
+                    />
+                </PanelBody>
+            </Panel>
+            <Panel>
+                <PanelBody title= {__('Grid Interaction', 'give')} initialOpen={ true }>
+                    <SelectControl
+                        label={__('Display Type', 'give')}
+                        name="displayType"
+                        value={displayType}
+                        options={giveFormOptions.displayType}
+                        onChange={(value) => saveSetting('displayType', value)}
+                        help={__('What should happen when a visitor clicks on a form within the grid? "Redirect" sends them to the individual form. "Modal" opens the form in a lightbox/popup on the same page.', 'give')}
+                    />
+                    <TextControl
+                        name="formsPerPage"
+                        label={__('Forms Per Page', 'give')}
+                        value={formsPerPage}
+                        onChange={(value) => saveSetting('formsPerPage', value)}
+                        help={__('How many forms should display on the first page load? To restrict display to only one page, disable pagination below.', 'give')}
+                    />
+                </PanelBody>
+            </Panel>
+            <Panel>
+                <PanelBody title= {__('Color Settings', 'give')} initialOpen={ true }>
+                        <>
+                            <p>
+                                {__('Donate Button Background Color', 'give')}
+                            </p>
+                            <ColorPalette
+                                clearable={false}
+                                onChange={(value) => saveSetting('donateButtonBackgroundColor', value)}
+                                value={donateButtonBackgroundColor}
+                            />
+                            <p>
+                                {__('Donate Button Text Color', 'give')}
+                            </p>
+                            <ColorPalette
+                                clearable={false}
+                                onChange={(value) => saveSetting('donateButtonTextColor', value)}
+                                value={donateButtonTextColor}
+                            />
+                        </>
+                        <ToggleControl
+                            name="showDonateButton"
+                            label={__('Show Donate Button', 'give')}
+                            checked={!!showDonateButton}
+                            onChange={(value) => saveSetting('showDonateButton', value)}
                         />
+                </PanelBody>
+            </Panel>
+            <Panel>
+                <PanelBody title= {__('Color Settings', 'give')} initialOpen={ true }>
 
-                        <TextControl
-                            name="imageHeight"
-                            label={__('Image Height', 'give')}
-                            value={imageHeight}
-                            onChange={(value) => saveSetting('imageHeight', value)}
-                            help={__('Featured image height. Default "auto". Accepts valid heights in px, em, or rem.', 'give')}
-                        />
-                    </>
-                )}
-
-                <ToggleControl
-                    name="showDonateButton"
-                    label={__('Show Donate Button', 'give')}
-                    checked={!!showDonateButton}
-                    onChange={(value) => saveSetting('showDonateButton', value)}
-                />
-                {showDonateButton && (
-                    <>
-                        <p>
-                            {__('Donate Button Background Color', 'give')}
-                        </p>
-                        <ColorPalette
-                            clearable={false}
-                            onChange={(value) => saveSetting('donateButtonBackgroundColor', value)}
-                            value={donateButtonBackgroundColor}
-                        />
-                        <p>
-                            {__('Donate Button Text Color', 'give')}
-                        </p>
-                        <ColorPalette
-                            clearable={false}
-                            onChange={(value) => saveSetting('donateButtonTextColor', value)}
-                            value={donateButtonTextColor}
-                        />
-                    </>
-                )}
-            </PanelBody>
-
-            <PanelBody title={__('Filters and Categories', 'give')}>
-
-                <SelectControl
-                    label={__('Order By', 'give')}
-                    name="orderBy"
-                    value={orderBy}
-                    options={giveFormOptions.orderBy}
-                    onChange={(value) => saveSetting('orderBy', value)}
-                    help={__('The order forms are displayed in.', 'give')}
-                />
-
-                <SelectControl
-                    label={__('Order', 'give')}
-                    name="order"
-                    value={order}
-                    options={giveFormOptions.order}
-                    onChange={(value) => saveSetting('order', value)}
-                    help={__('Whether the order ascends or descends.', 'give')}
-                />
-
-                <FormTokenField
-                    name="categories"
-                    label={__('Categories', 'give')}
-                    value={getAsArray(categories)}
-                    onChange={(value) => saveSetting('categories', value)}
-                />
-
-                <p className="components-form-token-field__help">
-                    {__('Type the name of your category to add it to the list. Only forms within the categories you choose will be displayed in this grid.', 'give')}
-                </p>
-
-                <FormTokenField
-                    name="tags"
-                    label={__('Tags', 'give')}
-                    value={getAsArray(tags)}
-                    onChange={(value) => saveSetting('tags', value)}
-                />
-
-                <p className="components-form-token-field__help">
-                    {__('Type the name of your tag to add it to the list. Only forms with these tags you choose will be displayed in this grid.', 'give')}
-                </p>
-
-                <FormTokenField
-                    name="formIDs"
-                    label={__('Form IDs', 'give')}
-                    value={getAsArray(formIDs)}
-                    onChange={(value) => saveSetting('formIDs', value)}
-                />
-
-                <p className="components-form-token-field__help">
-                    {__('Type the ID of your form to add it to the list. Only forms with these IDs you choose will be displayed in this grid.', 'give')}
-                </p>
-
-                <ToggleControl
-                    name="excludeForms"
-                    label={__('Exclude specific forms?', 'give')}
-                    checked={!!excludeForms}
-                    onChange={(value) => saveSetting('excludeForms', value)}
-                />
-
-                {excludeForms && (
-                    <>
-                        <FormTokenField
-                            name="excludedFormIDs"
-                            label={__('Excluded Form IDs', 'give')}
-                            value={getAsArray(excludedFormIDs)}
-                            onChange={(value) => saveSetting('excludedFormIDs', value)}
-                        />
-                        <p className="components-form-token-field__help">
-                            {__('Type the ID of your form to exclude it from the list. Forms with these IDs you choose will not be displayed in this grid.', 'give')}
-                        </p>
-                    </>
-                )}
-            </PanelBody>
-
-            <PanelBody title={__('Grid Behavior', 'give')}>
-                <SelectControl
-                    label={__('Display Type', 'give')}
-                    name="displayType"
-                    value={displayType}
-                    options={giveFormOptions.displayType}
-                    onChange={(value) => saveSetting('displayType', value)}
-                    help={__('What should happen when a visitor clicks on a form within the grid? "Redirect" sends them to the individual form. "Modal" opens the form in a lightbox/popup on the same page.', 'give')}
-                />
-                <TextControl
-                    name="formsPerPage"
-                    label={__('Forms Per Page', 'give')}
-                    value={formsPerPage}
-                    onChange={(value) => saveSetting('formsPerPage', value)}
-                    help={__('How many forms should display on the first page load? To restrict display to only one page, disable pagination below.', 'give')}
-                />
-                <ToggleControl
-                    name="paged"
-                    label={__('Show Pagination', 'give')}
-                    checked={!!paged}
-                    onChange={(value) => saveSetting('paged', value)}
-                />
-            </PanelBody>
+                </PanelBody>
+            </Panel>
         </InspectorControls>
     );
 };
