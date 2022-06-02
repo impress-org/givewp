71312bf3d7862cde8994ac5c38202a1d31379f0c
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2022-06-02 19:31:22 +0530

    feature: add second argument to function

--- src/Framework/PaymentGateways/Contracts/Subscription/SubscriptionAmountEditable.php
+++ src/Framework/PaymentGateways/Contracts/Subscription/SubscriptionAmountEditable.php
@@ -2,6 +2,7 @@
 
 namespace Give\Framework\PaymentGateways\Contracts\Subscription;
 
+use Give\Framework\Support\ValueObjects\Money;
 use Give\Subscriptions\Models\Subscription;
 
 /**
@@ -12,7 +13,8 @@ interface SubscriptionAmountEditable
     /**
      * Update subscription amount.
      *
+     * @unreleased Add second argument to specify the new amount.
      * @since 2.20.0
      */
-    public function updateSubscriptionAmount(Subscription $subscription);
+    public function updateSubscriptionAmount(Subscription $subscription, Money $newRenewalAmount);
 }
