7181f334b3a56645f131512e72ef69cae90d4ca9
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2022-06-16 09:55:58 +0530

    fix: use sanitize_file_name function

--- includes/admin/tools/export/export-functions.php
+++ includes/admin/tools/export/export-functions.php
@@ -48,7 +48,7 @@ function give_do_ajax_export() {
 
 	$step     = absint( $_POST['step'] );
 	$class    = sanitize_text_field( $form['give-export-class'] );
-	$filename = isset( $_POST['file_name'] ) ? sanitize_text_field( $_POST['file_name'] ) : null;
+	$filename = isset( $_POST['file_name'] ) ? sanitize_file_name( $_POST['file_name'] ) : null;
 
 	/* @var Give_Batch_Export $export */
 	$export = new $class( $step, $filename );
