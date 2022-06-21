1f063ab773439ab7410adc6a3a84f4838e4f3e2b
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2022-06-21 11:48:40 +0530

    refactor: revert changes in wp-textdomain.js

--- wp-textdomain.js
+++ wp-textdomain.js
@@ -2,8 +2,5 @@ const wpTextdomain = require('wp-textdomain');
 
 wpTextdomain(process.argv[2], {
     domain: 'give',
-    // fix: true,
-    'glob': {
-        "ignore": ['vendor', 'languages', 'node_modules', 'assets', 'tests', 'sample-data', '.git', '.github']
-    }
+    fix: true,
 });
