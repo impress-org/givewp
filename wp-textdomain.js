2154dc2f2c2795edcaff05162093da66613cb45f
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2022-06-21 11:31:09 +0530

    fix: make string translatable

--- wp-textdomain.js
+++ wp-textdomain.js
@@ -1,6 +1,9 @@
-const wpTextdomain = require( 'wp-textdomain' );
+const wpTextdomain = require('wp-textdomain');
 
-wpTextdomain( process.argv[ 2 ], {
-	domain: 'give',
-	fix: true,
-} );
+wpTextdomain(process.argv[2], {
+    domain: 'give',
+    // fix: true,
+    'glob': {
+        "ignore": ['vendor', 'languages', 'node_modules', 'assets', 'tests', 'sample-data', '.git', '.github']
+    }
+});
