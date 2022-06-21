413022be09cfd7eb21621a3b37dbd5ca71d2763d
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2022-06-21 19:44:29 +0530

    doc: update filter name

--- src/Framework/PaymentGateways/Contracts/PaymentGatewayInterface.php
+++ src/Framework/PaymentGateways/Contracts/PaymentGatewayInterface.php
@@ -60,7 +60,7 @@ interface PaymentGatewayInterface extends SubscriptionModuleInterface
 
     /**
      * Create a payment with gateway
-     * Note: You can use "givewp_new_payment_{$gatewayId}_gateway_data" filter hook to pass additional data for gateway which helps/require to process transaction.
+     * Note: You can use "givewp_create_payment_{$gatewayId}_gateway_data" filter hook to pass additional data for gateway which helps/require to process transaction.
      *       This filter will help to add additional arguments to this function which should be optional otherwise you will get PHP fatal error.
      *
      * @unreleased Add second param to function to pass gateway data to process transaction
