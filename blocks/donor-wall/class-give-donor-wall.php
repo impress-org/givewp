241601afa9f9c1a9972e7c78bdae9cb0c04d4d79
Joshua Dinh <josh@joshuadinh.tech>
2022-06-28 09:05:24 -0700

    fix: render block with original time property

--- blocks/donor-wall/class-give-donor-wall.php
+++ blocks/donor-wall/class-give-donor-wall.php
@@ -231,7 +231,7 @@ class Give_Donor_Wall_Block {
 			'loadmore_text'     => $attributes['loadMoreText'],
 			'avatar_size'       => $avatarSize ?: 75,
             'color'             => $attributes['color'],
-            'show_timestamp'    => $attributes['showTimestamp'],
+            'show_time'         => $attributes['showTimestamp'],
 		];
 
 		$html = Give_Donor_Wall::get_instance()->render_shortcode( $parameters );
