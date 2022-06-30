38f0269da291263274cee6ccab2282b2bf8d0206
Jon Waldstein <jonwaldstein@jons-air.mynetworksettings.com>
2022-06-30 16:18:24 -0400

    test: update fields api tests to make top level sections

--- tests/unit/tests/Framework/FieldsAPI/Concerns/MoveNodeTest.php
+++ tests/unit/tests/Framework/FieldsAPI/Concerns/MoveNodeTest.php
@@ -1,21 +1,28 @@
 <?php
 
 use Give\Framework\FieldsAPI\Form;
+use Give\Framework\FieldsAPI\Section;
 use Give\Framework\FieldsAPI\Text;
 use PHPUnit\Framework\TestCase;
 
-final class MoveNodeTest extends TestCase {
+final class MoveNodeTest extends TestCase
+{
 
-    public function testMoveAfter() {
-	    $form = Form::make( 'form' )
-		    ->append(
-			    Text::make( 'firstTextField' ),
-			    Text::make( 'secondTextField' )
-		    );
+    public function testMoveAfter()
+    {
+        $form = Form::make('form')
+            ->append(
+                Section::make('form-section')
+                    ->append(
+                        Text::make('firstTextField'),
+                        Text::make('secondTextField')
+                    )
+            );
 
-	    $form->move( 'firstTextField' )->after( 'secondTextField' );
+        $section = $form->getNodeByName('form-section');
+        $section->move('firstTextField')->after('secondTextField');
 
-	    $this->assertEquals( 1, $form->getNodeIndexByName( 'firstTextField' ) );
-	    $this->assertEquals( 0, $form->getNodeIndexByName( 'secondTextField' ) );
+        $this->assertEquals(1, $section->getNodeIndexByName('firstTextField'));
+        $this->assertEquals(0, $section->getNodeIndexByName('secondTextField'));
     }
 }
--- tests/unit/tests/Framework/FieldsAPI/Concerns/RemoveNodeTest.php
+++ tests/unit/tests/Framework/FieldsAPI/Concerns/RemoveNodeTest.php
@@ -1,19 +1,26 @@
 <?php
 
 use Give\Framework\FieldsAPI\Form;
+use Give\Framework\FieldsAPI\Section;
 use Give\Framework\FieldsAPI\Text;
 use PHPUnit\Framework\TestCase;
 
-final class RemoveNodeTest extends TestCase {
+final class RemoveNodeTest extends TestCase
+{
 
-    public function testRemoveNode() {
-        $form = Form::make( 'form' )->append(
-            Text::make( 'firstTextField' ),
-            Text::make( 'secondTextField' )
-        );
+    public function testRemoveNode()
+    {
+        $form = Form::make('form')
+            ->append(
+                Section::make('form-section')
+                    ->append(
+                        Text::make('firstTextField'),
+                        Text::make('secondTextField')
+                    )
+            );
 
-        $form->remove( 'secondTextField' );
+        $form->remove('secondTextField');
 
-        $this->assertEquals( 1, $form->count() );
+        $this->assertEquals(1, $form->getNodeByName('form-section')->count());
     }
 }
