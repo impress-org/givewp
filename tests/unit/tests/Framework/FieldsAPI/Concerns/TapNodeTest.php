11ecd77bd3c6a5a399bc4ec95d29ac1c895ed19a
Kyle B. Johnson <kyle.johnson@impress.org>
2022-08-16 16:25:44 -0400

    chore: Add TapNode tests

---
+++ tests/unit/tests/Framework/FieldsAPI/Concerns/TapNodeTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use Give\Framework\FieldsAPI\Concerns\TapNode;
+use PHPUnit\Framework\TestCase;
+
+final class TapNodeTest extends TestCase
+{
+
+    public function testTappedNodeReturnsOriginalNode()
+    {
+        $node = new class {
+            use TapNode;
+        };
+
+        $this->assertEquals($node, $node->tap(function ($tappedNode) {
+            // This section intentionally left blank.
+        }));
+    }
+
+    public function testTappedNodeIsUpdated()
+    {
+        $node = new class {
+            use TapNode;
+
+            public $updated = false;
+        };
+
+        $this->assertTrue($node->tap(function ($tappedNode) {
+            $tappedNode->updated = true;
+        })->updated);
+    }
+}
