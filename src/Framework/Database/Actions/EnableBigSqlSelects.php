2ce9f520eff436307e2fd77d3fcf1856743ca041
Jason Adams <jason.adams@impress.org>
2022-08-17 09:17:12 -0700

    doc: add additional context

--- src/Framework/Database/Actions/EnableBigSqlSelects.php
+++ src/Framework/Database/Actions/EnableBigSqlSelects.php
@@ -11,6 +11,10 @@ class EnableBigSqlSelects
      *
      * Enables mysql big selects for the session using a session system variable.
      *
+     * This is necessary for hosts that have an arbitrary MAX_JOIN_SIZE limit, which prevents more complex queries from
+     * running properly. Setting SQL_BIG_SELECTS ignores this limit. This is also done by WooCommerce, supporting the
+     * idea that this is a viable option. There also doesn't seem to be a way for hosts to prevent this.
+     *
      * @see https://dev.mysql.com/doc/refman/5.7/en/server-system-variables.html#sysvar_sql_big_selects
      * @see https://dev.mysql.com/doc/refman/5.7/en/system-variable-privileges.html
      *
