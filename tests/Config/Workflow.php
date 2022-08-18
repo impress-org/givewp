ee36bd4a59654fe6243800c6a8e5956be18f7046
Jon Waldstein <jonwaldstein@jons-air.mynetworksettings.com>
2022-08-18 08:58:01 -0400

    docs: update since tags

--- tests/Config/Workflow.php
+++ tests/Config/Workflow.php
@@ -6,18 +6,30 @@ namespace GiveTests\Config;
  * Runs the test suite for GitHub actions, which tests multiple
  * WordPress versions by leveraging wordpress-tests-lib.
  */
-class Workflow implements Config {
-
+class Workflow implements Config
+{
+    /**
+     * @inheritDoc
+     * @unreleased
+     */
     public function config(): string
     {
         return '/tmp/wordpress-tests-lib/wp-tests-config.php';
     }
 
+    /**
+     * @inheritDoc
+     * @unreleased
+     */
     public function bootstrap(): string
     {
         return '/tmp/wordpress-tests-lib/includes/bootstrap.php';
     }
 
+    /**
+     * @inheritDoc
+     * @unreleased
+     */
     public function functions(): string
     {
         return '/tmp/wordpress-tests-lib/includes/functions.php';
