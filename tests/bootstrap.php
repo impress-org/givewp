4c80f099172c2d42798bd0fbb47a7f51cd273f87
Jon Waldstein <jonwaldstein@jons-air.mynetworksettings.com>
2022-08-18 09:19:04 -0400

    Revert "docs: update comments in bootstrap"
    This reverts commit 1ae602530c92ec5d736bee0264c467f2ae18253b.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,12 @@
 <?php
-require_once __DIR__ . '/../../give/tests/bootstrap.php';
+if (file_exists(__DIR__ . '/../../give/vendor/wordpress/wordpress/tests/phpunit/includes/functions.php')) {
+    require_once __DIR__ . '/../../give/vendor/wordpress/wordpress/tests/phpunit/includes/functions.php';
+} elseif (file_exists('/tmp/wordpress-tests-lib/includes/functions.php')) {
+    require_once '/tmp/wordpress-tests-lib/includes/functions.php';
+}
 
-tests_add_filter('muplugins_loaded', static function () {
+tests_add_filter('muplugins_loaded', function () {
     require_once __DIR__ . '/../give-next-gen.php';
 });
+
+require_once __DIR__ . '/../../give/tests/unit/bootstrap.php';
