86fe7b8102c0a305618bc6e5f7d79073c20cf38c
Kyle B. Johnson <kyle.johnson@impress.org>
2022-08-29 09:09:38 -0400

    refactor: Enforce request type check with enum

---
+++ src/ServiceProviders/RequestType.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Give\ServiceProviders;
+
+/**
+ * @unreleased
+ */
+class RequestType
+{
+    const ADMIN = 'admin';
+    const AJAX = 'ajax';
+    const CRON = 'cron';
+    const FRONTEND = 'frontend';
+    const WPCLI = 'wpcli';
+}
