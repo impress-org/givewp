1d91786acde9074870d767a9eb9814beff45cb47
Kyle B. Johnson <kyle.johnson@hey.com>
2022-10-20 16:03:13 -0400

    chore: Add unreleased tags

--- templates/shortcode-form-grid.php
+++ templates/shortcode-form-grid.php
@@ -190,6 +190,9 @@ $renderTags = static function($wrapper_class, $apply_styles = true) use($form_id
                             ? $atts['donate_button_text']
                             : give_get_meta( $form_id, '_give_form_grid_donate_button_text', true );
 
+                         /**
+                          * @unreleased Updates the default text color for the donate button, see #6591.
+                          */
                         $button_text_color = ! empty( $atts['donate_button_text_color'] )
                             ? $atts['donate_button_text_color']
                             : 'inherit';
