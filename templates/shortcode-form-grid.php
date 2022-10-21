4a3a57103c1e84dcd229c653a4ebab60a98e2599
Kyle B. Johnson <kjohnson@users.noreply.github.com>
2022-10-21 12:45:12 -0400

    Form Grid donate button defaults are now updated (#6591)
    * fix: Change default text color to black
    
    * fix: Revert show_donate_button default value

--- templates/shortcode-form-grid.php
+++ templates/shortcode-form-grid.php
@@ -190,9 +190,12 @@ $renderTags = static function($wrapper_class, $apply_styles = true) use($form_id
                             ? $atts['donate_button_text']
                             : give_get_meta( $form_id, '_give_form_grid_donate_button_text', true );
 
+                         /**
+                          * @unreleased Updated the default text color for the donate button, see #6591.
+                          */
                         $button_text_color = ! empty( $atts['donate_button_text_color'] )
                             ? $atts['donate_button_text_color']
-                            : '#fff';
+                            : '#000000';
                         ?>
                         <button style="text-decoration-color: <?php echo $button_text_color; ?>">
                                     <span style="color: <?php echo $button_text_color; ?>">
