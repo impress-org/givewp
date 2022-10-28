bed35ac157d75d575c99c5ea990a924f44cbf166
Jon Waldstein <jonwaldstein@jons-air.mynetworksettings.com>
2022-10-28 12:17:59 -0400

    tests: fix and update DonationFormViewRouteData test

--- tests/Unit/DataTransferObjects/DonationFormViewRouteDataTest.php
+++ tests/Unit/DataTransferObjects/DonationFormViewRouteDataTest.php
@@ -3,6 +3,7 @@
 namespace TestsNextGen\Unit\DataTransferObjects;
 
 use Give\NextGen\DonationForm\DataTransferObjects\DonationFormViewRouteData;
+use Give\NextGen\DonationForm\Models\DonationForm;
 use GiveTests\TestCase;
 
 /**
@@ -22,7 +23,7 @@ class DonationFormViewRouteDataTest extends TestCase
             'form-template-id' => 'classic',
         ]);
 
-        $this->assertSame('1', $data->formId);
+        $this->assertSame(1, $data->formId);
     }
 
     /**
@@ -38,6 +39,22 @@ class DonationFormViewRouteDataTest extends TestCase
         ]);
 
         $this->assertSame('classic', $data->formTemplateId);
+    }
+
+    /**
+     * @unreleased
+     *
+     * @return void
+     */
+    public function testShouldReturnFormBlocks()
+    {
+        $data = DonationFormViewRouteData::fromRequest([
+            'form-id' => '1',
+            'form-template-id' => 'classic',
+            'form-blocks' => DonationForm::factory()->definition()['blocks']->toJson(),
+        ]);
+
+        $this->assertEquals(DonationForm::factory()->definition()['blocks'], $data->formBlocks);
     }
 
 }
