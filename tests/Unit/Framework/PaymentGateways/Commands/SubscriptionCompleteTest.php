af1187d89115a192dd7f9b0d1e63dd9101b6686d
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2022-11-05 17:15:17 +0530

    chore add incomplete test for SubscriptonComplete gateway command

---
+++ tests/Unit/Framework/PaymentGateways/Commands/SubscriptionCompleteTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace GiveTests\Unit\Framework\PaymentGateways\Commands;
+
+use GiveTests\TestCase;
+
+/**
+ * @unreleased
+ */
+class SubscriptionCompleteTest extends TestCase
+{
+    /**
+     * @unreleased
+     * @return void
+     */
+    public function testSubscriptionActiveAndDonationCompleted()
+    {
+        $this->markTestIncomplete();
+    }
+
+    /**
+     * @unreleased
+     * @return void
+     */
+    public function testSubscriptionPendingAndDonationProcessing()
+    {
+        $this->markTestIncomplete();
+    }
+}
