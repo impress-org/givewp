f7feb7d01569f657741658b4962cb3b61a4361ad
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2022-11-05 15:46:17 +0530

    Add status property to SubscriptionComplete gateway command

--- src/Framework/PaymentGateways/Commands/SubscriptionComplete.php
+++ src/Framework/PaymentGateways/Commands/SubscriptionComplete.php
@@ -2,6 +2,9 @@
 
 namespace Give\Framework\PaymentGateways\Commands;
 
+use Give\Donations\ValueObjects\DonationStatus;
+use Give\Subscriptions\ValueObjects\SubscriptionStatus;
+
 /***
  * @since 2.18.0
  */
@@ -19,13 +22,32 @@ class SubscriptionComplete implements GatewayCommand
      * @var string
      */
     public $gatewaySubscriptionId;
+    /**
+     * The subscription status.
+     *
+     * @var SubscriptionStatus
+     */
+    public $subscriptionStatus;
+    /**
+     * The donation status.
+     *
+     * @var DonationStatus
+     */
+    public $donationStatus;
 
     /**
+     * @unreleased Add support for donation and subscription status.
      * @since 2.18.0
      */
-    public function __construct(string $gatewayTransactionId, string $gatewaySubscriptionId)
-    {
-        $this->gatewayTransactionId = $gatewayTransactionId;
+    public function __construct(
+        string $gatewayTransactionId,
+        string $gatewaySubscriptionId,
+        SubscriptionStatus $subscriptionStatus = null,
+        DonationStatus $donationStatus = null
+    ) {
+        $this->gatewayTransactionId  = $gatewayTransactionId;
         $this->gatewaySubscriptionId = $gatewaySubscriptionId;
+        $this->subscriptionStatus    = $subscriptionStatus;
+        $this->donationStatus        = $donationStatus;
     }
 }
