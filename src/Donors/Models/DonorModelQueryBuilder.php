db4f9e274983b3d713cc3b0a66fc7cb127e39b6c
Paulo Iankoski <me@wpdev.com.br>
2022-11-11 15:57:46 -0300

    chore: attach additional emails to query results

--- src/Donors/Models/DonorModelQueryBuilder.php
+++ src/Donors/Models/DonorModelQueryBuilder.php
@@ -19,9 +19,11 @@ class DonorModelQueryBuilder extends ModelQueryBuilder
     {
         $row = DB::get_row($this->getSQL(), OBJECT);
 
-        if (!$row) {
+        if ( ! $row) {
             return null;
         }
+
+        $row = $this->attachAdditionalEmails($row);
 
         return $this->getRowAsModel($row);
     }
@@ -37,14 +39,77 @@ class DonorModelQueryBuilder extends ModelQueryBuilder
     {
         $results = DB::get_results($this->getSQL(), OBJECT);
 
-        if (!$results) {
+        if ( ! $results) {
             return null;
         }
+
+        $results = $this->attachAdditionalEmails($results);
 
         if (isset($this->model)) {
             return $this->getAllAsModel($results);
         }
 
         return $results;
+    }
+
+    /**
+     * @unreleased
+     *
+     * @param array|object $queryResults
+     *
+     * @return array|object
+     */
+    private function attachAdditionalEmails($queryResults)
+    {
+        if (is_array($queryResults)) {
+            $donorIds = wp_list_pluck($queryResults, 'id');
+            $additionalEmails = $this->getAdditionalEmails($donorIds);
+            $queryResults = array_map(function ($donor) use ($additionalEmails) {
+                $donor->additionalEmails = $additionalEmails[$donor->id] ?? '';
+
+                return $donor;
+            }, $queryResults);
+        } else {
+            $queryResults->additionalEmails = $this->getAdditionalEmails([$queryResults->id], true) ?? '';
+        }
+
+        return $queryResults;
+    }
+
+    /**
+     * @unreleased
+     *
+     * @param array $donorIds Array of donor ids
+     * @param bool  $single Return additional emails for the first donor id
+     *
+     * @return array
+     */
+    private function getAdditionalEmails(array $donorIds, bool $single = false): array
+    {
+        $results = DB::table('give_donormeta')
+            ->select(['donor_id', 'donorId'])
+            ->selectRaw(
+                "CONCAT('[',GROUP_CONCAT(DISTINCT CONCAT('\"',%1s,'\"')),']') AS %2s",
+                'meta_value',
+                'additionalEmails'
+            )
+            ->whereIn('donor_id', $donorIds)
+            ->where('meta_key', 'additional_emails')
+            ->getAll();
+
+        if ( ! $results) {
+            return [];
+        }
+
+        $additionalEmails = [];
+        foreach ($results as $result) {
+            $additionalEmails[(int)$result->donorId] = $result->additionalEmails;
+        }
+
+        if ($single) {
+            return $additionalEmails[$donorIds[0]] ?? [];
+        }
+
+        return $additionalEmails;
     }
 }
