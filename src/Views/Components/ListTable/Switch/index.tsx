0234bad0d443430e76d35294e87be034f9ff95ae
Joshua Dinh <josh@joshuadinh.tech>
2022-11-15 15:47:39 -0800

    fix: update styles

--- src/Views/Components/ListTable/Switch/index.tsx
+++ src/Views/Components/ListTable/Switch/index.tsx
@@ -1,9 +1,8 @@
-import {forwardRef, useCallback, createRef, useEffect, useState} from 'react';
 import styles from './style.module.scss';
 
 const Switch = ({label, toggle, checked}) => {
     return (
-        <label className={styles.label}>
+        <label className={styles.container}>
             <input type="checkbox" aria-label="switch" checked={checked} onChange={() => toggle(!checked)} />
             <span className={styles.switch} />
             <span>{label && label}</span>
