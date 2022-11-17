c89fd8d43ba9c03b3498a8d3fe003ff667969a91
Paulo Iankoski <me@wpdev.com.br>
2022-11-17 15:00:19 -0300

    feature: add initial DonationForms properties

--- src/DonationForms/Models/DonationForm.php
+++ src/DonationForms/Models/DonationForm.php
@@ -2,12 +2,53 @@
 
 namespace Give\DonationForms\Models;
 
+use DateTime;
 use Give\Framework\Models\Contracts\ModelHasFactory;
 use Give\Framework\Models\Contracts\ModelReadOnly;
 use Give\Framework\Models\Model;
+use Give\Framework\Models\ValueObjects\Relationship;
+use Give\Framework\Support\ValueObjects\Money;
 
+/**
+ * Class DonationForm
+ *
+ * @unreleased
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $donationLevels
+ * @property string $goal
+ * @property int $donationsCount
+ * @property Money $donationsRevenue
+ * @property string $shortcode
+ * @property DateTime $createdAt
+ * @property DateTime $updatedAt
+ * @property string $status // TODO: Implement DonationFormStatus class and replace this with DonationFormStatus
+ */
 class DonationForm extends Model implements ModelReadOnly, ModelHasFactory
 {
+    /**
+     * @inheritdoc
+     */
+    protected $properties = [
+        'id' => 'int',
+        'title' => 'string',
+        'donationLevels' => 'string',
+        'goal' => 'string',
+        'donationsCount' => 'int',
+        'donationsRevenue' => Money::class,
+        'shortcode' => 'string',
+        'createdAt' => DateTime::class,
+        'updatedAt' => DateTime::class,
+        'status' => 'string', // TODO: Implement DonationFormStatus class and replace this with DonationFormStatus::class
+    ];
+
+    /**
+     * @inheritdoc
+     */
+    protected $relationships = [
+        'donations' => Relationship::HAS_MANY,
+    ];
 
     public static function find($id)
     {
