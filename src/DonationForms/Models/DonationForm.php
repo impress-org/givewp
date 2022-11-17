c17abeead44c2fafa53659ab566899b2cb5ce815
Paulo Iankoski <me@wpdev.com.br>
2022-11-17 13:40:56 -0300

    feature: init Donation Form model scaffold

---
+++ src/DonationForms/Models/DonationForm.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Give\DonationForms\Models;
+
+use Give\Framework\Models\Contracts\ModelHasFactory;
+use Give\Framework\Models\Contracts\ModelReadOnly;
+use Give\Framework\Models\Model;
+
+class DonationForm extends Model implements ModelReadOnly, ModelHasFactory
+{
+
+    public static function find($id)
+    {
+        // TODO: Implement find() method.
+    }
+
+    public static function query()
+    {
+        // TODO: Implement query() method.
+    }
+
+    public static function fromQueryBuilderObject($object)
+    {
+        // TODO: Implement fromQueryBuilderObject() method.
+    }
+
+    public static function factory()
+    {
+        // TODO: Implement factory() method.
+    }
+}
