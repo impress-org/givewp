b39a3fa2bc779ab5f2c6c4a3113cf51404d2d5d5
Paulo Iankoski <me@wpdev.com.br>
2022-11-27 22:09:50 -0300

    feature: implement extra data for tooltip on StatusColumn

--- src/Subscriptions/ListTable/Columns/StatusColumn.php
+++ src/Subscriptions/ListTable/Columns/StatusColumn.php
@@ -45,10 +45,56 @@ class StatusColumn extends ModelColumn
      */
     public function getCellValue($model): string
     {
+        $template = '
+            <div class="subscriptionStatus">
+                <div class="statusBadge statusBadge--%1$s"><p>%2$s</p></div>
+                %3$s
+            </div>
+        ';
+
+        $extraTemplate = '
+            <div class="subscriptionStatus__container">
+                <img src="%1$s" alt="%2$s"/>
+                <div class="subscriptionStatus__message subscriptionStatus__message--%3$s">
+                    <img src="%1$s" alt="%2$s"/>%4$s</div>
+                </div>
+            </div>
+        ';
+
+        if ('failed' === $model->status->getvalue()) {
+            $extra = [
+                'label' => __('failed', 'give'),
+                'status' => 'failed',
+                'text' => __('This subscription has failed', 'give'),
+            ];
+        } elseif (0 === $model->installments) {
+            $extra = [
+                'label' => __('indefinite', 'give'),
+                'status' => 'indefinite',
+                'text' => __('This subscription continues <strong> indefinitely </strong>', 'give'),
+            ];
+        } elseif (count($model->donations) < $model->installments) {
+            $extra = [
+                'label' => __('limited', 'give'),
+                'status' => 'limited',
+                'text' => sprintf(
+                    __('This subscription has <strong> %d </strong> remaining donations', 'give'),
+                    $model->installments - count($model->donations)
+                )
+            ];
+        }
+
         return sprintf(
-        '<div class="statusBadge statusBadge--%1$s"><p>%2$s</p></div>',
+            $template,
             $model->status->getValue(),
-            $model->status->label()
+            $model->status->label(),
+            is_array( $extra ) ? sprintf(
+                $extraTemplate,
+                GIVE_PLUGIN_URL . 'assets/dist/images/list-table/' . $extra['status'] . '-subscription-icon.svg',
+                $extra['label'],
+                $extra['status'],
+                $extra['text']
+            ) : ''
         );
     }
 }
