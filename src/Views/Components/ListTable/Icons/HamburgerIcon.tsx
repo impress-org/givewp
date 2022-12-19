9965387a4c5614c85eac93f848684e1e2acb2cce
Joshua Dinh <josh@joshuadinh.tech>
2022-12-19 17:02:44 +0900

    fix: add svg icons for modal

---
+++ src/Views/Components/ListTable/Icons/HamburgerIcon.tsx
@@ -0,0 +1,10 @@
+export default function HamburgerIcon() {
+    return (
+        <svg width="24" height="17" viewBox="0 0 24 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="24" height="2" rx="1" fill="#C4C4C4" />
+            <rect y="5" width="24" height="2" rx="1" fill="#C4C4C4" />
+            <rect y="10" width="24" height="2" rx="1" fill="#C4C4C4" />
+            <rect y="15" width="24" height="2" rx="1" fill="#C4C4C4" />
+        </svg>
+    );
+}
