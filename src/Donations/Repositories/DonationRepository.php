4e5ec17c3be20cc20560f4746f5e788d1800fb8a
Glauber Silva <glauber.silva@givewp.com>
2022-12-23 16:10:12 -0300

    doc: add unreleased tag

--- src/Donations/Repositories/DonationRepository.php
+++ src/Donations/Repositories/DonationRepository.php
@@ -230,11 +230,12 @@ class DonationRepository
     }
 
     /**
-     * @since 2.23.1 Use give_update_meta() method to update entries on give_donationmeta table
-     * @since 2.23.0 retrieve the post_parent instead of relying on parentId property
-     * @since 2.21.0 replace actions with givewp_donation_updating and givewp_donation_updated
-     * @since 2.20.0 return void
-     * @since 2.19.6
+     * @unreleased Cleans the post cache after updating it
+     * @since      2.23.1 Use give_update_meta() method to update entries on give_donationmeta table
+     * @since      2.23.0 retrieve the post_parent instead of relying on parentId property
+     * @since      2.21.0 replace actions with givewp_donation_updating and givewp_donation_updated
+     * @since      2.20.0 return void
+     * @since      2.19.6
      *
      * @return void
      * @throws Exception|InvalidArgumentException
