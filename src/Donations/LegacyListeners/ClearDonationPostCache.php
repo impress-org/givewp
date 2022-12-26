479328749c9ceaad285162f53388d66fe72a8f1f
Glauber Silva <glauber.silva@givewp.com>
2022-12-26 13:03:57 -0300

    doc: add unreleased tag

--- src/Donations/LegacyListeners/ClearDonationPostCache.php
+++ src/Donations/LegacyListeners/ClearDonationPostCache.php
@@ -4,8 +4,14 @@ namespace Give\Donations\LegacyListeners;
 
 use Give\Donations\Models\Donation;
 
+/**
+ * @unreleased
+ */
 class ClearDonationPostCache
 {
+    /**
+     * @unreleased
+     */
     public function __invoke(Donation $donation)
     {
         clean_post_cache($donation->id);
