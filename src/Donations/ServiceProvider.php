2439b6f2d266d2649d29528ff99a9067d7575074
Jon Waldstein <jonwaldstein@macbook-air.mynetworksettings.com>
2022-12-29 16:01:16 -0500

    refactor: replace deprecate Call::invoke method for rest of legacy listener actions

--- src/Donations/ServiceProvider.php
+++ src/Donations/ServiceProvider.php
@@ -14,7 +14,6 @@ use Give\Donations\Models\Donation;
 use Give\Donations\Repositories\DonationNotesRepository;
 use Give\Donations\Repositories\DonationRepository;
 use Give\Framework\Migrations\MigrationsRegister;
-use Give\Helpers\Call;
 use Give\Helpers\Hooks;
 use Give\ServiceProviders\ServiceProvider as ServiceProviderInterface;
 
@@ -43,20 +42,20 @@ class ServiceProvider implements ServiceProviderInterface
     /**
      * Legacy Listeners
      *
-     * @unreleased Remove UpdateSequentialId from "givewp_donation_updated" aciton hook.
+     * @unreleased Remove UpdateSequentialId from "givewp_donation_updated" action hook.
      * @since 2.19.6
      */
     private function bootLegacyListeners()
     {
         Hooks::addAction('givewp_donation_creating', DispatchGivePreInsertPayment::class);
 
-        add_action('givewp_donation_created', function (Donation $donation) {
-            Call::invoke(InsertSequentialId::class, $donation);
-            Call::invoke(DispatchGiveInsertPayment::class, $donation);
-            Call::invoke(UpdateDonorPaymentIds::class, $donation);
+        add_action('givewp_donation_created', static function (Donation $donation) {
+            (new InsertSequentialId())($donation);
+            (new DispatchGiveInsertPayment())($donation);
+            (new UpdateDonorPaymentIds())($donation);
 
             if ($donation->subscriptionId && $donation->type->isRenewal()) {
-                Call::invoke(DispatchGiveRecurringAddSubscriptionPaymentAndRecordPayment::class, $donation);
+                (new DispatchGiveRecurringAddSubscriptionPaymentAndRecordPayment())($donation);
             }
 
             /**
@@ -70,7 +69,7 @@ class ServiceProvider implements ServiceProviderInterface
         });
 
         add_action('givewp_donation_updated', function (Donation $donation) {
-            Call::invoke(DispatchGiveUpdatePaymentStatus::class, $donation);
+            (new DispatchGiveUpdatePaymentStatus())($donation);
         });
 
         Hooks::addAction('givewp_donation_deleted', RemoveSequentialId::class);
@@ -86,8 +85,7 @@ class ServiceProvider implements ServiceProviderInterface
         $userId = get_current_user_id();
         $showLegacy = get_user_meta($userId, '_give_donations_archive_show_legacy', true);
         // only register new admin page if user hasn't chosen to use the old one
-        if(empty($showLegacy))
-        {
+        if (empty($showLegacy)) {
             Hooks::addAction('admin_menu', DonationsAdminPage::class, 'registerMenuItem');
 
             if (DonationsAdminPage::isShowing()) {
