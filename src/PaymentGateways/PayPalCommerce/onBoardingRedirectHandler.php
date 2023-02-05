cbb66eb72049ba014d499b9241f8f1d3d55ef0f9
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2023-02-05 21:27:57 +0530

    doc: edit comment

--- src/PaymentGateways/PayPalCommerce/onBoardingRedirectHandler.php
+++ src/PaymentGateways/PayPalCommerce/onBoardingRedirectHandler.php
@@ -96,7 +96,7 @@ class onBoardingRedirectHandler
     /**
      * Save PayPal merchant details
      *
-     * @unreleased x.x.x Handle exception and delete only partner link details.
+     * @unreleased x.x.x Handle exception.
      * @since 2.9.0
      *
      * @return MerchantDetail
