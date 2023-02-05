a7a425085ec13ef13fb14b4955efcd8325220393
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2023-02-05 17:37:35 +0530

    tests: add blank test

---
+++ tests/Unit/PaymentGateways/PayPalCommerce/Repositories/PayPalAuthTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace Give\Tests\Unit\PaymentGateways\PayPalCommerce\Repositories;
+use Give\Tests\TestCase;
+
+/**
+ * @unreleased x.x.x
+ */
+class PayPalAuthTest extends TestCase
+{
+    /**
+     * @unreleased x.x.x
+     * @return void
+     */
+    public function testGetTokenFromClientCredentials()
+    {
+        $this->markTestIncomplete();
+    }
+}
