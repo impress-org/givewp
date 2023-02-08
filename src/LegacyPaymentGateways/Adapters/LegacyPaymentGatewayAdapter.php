6377014459e540dafd9fd60989ffb4f11b4a3d26
Glauber Silva <glauber.silva@givewp.com>
2023-02-08 17:40:42 -0300

    fix: import donation model

--- src/LegacyPaymentGateways/Adapters/LegacyPaymentGatewayAdapter.php
+++ src/LegacyPaymentGateways/Adapters/LegacyPaymentGatewayAdapter.php
@@ -3,6 +3,7 @@
 namespace Give\LegacyPaymentGateways\Adapters;
 
 use Exception;
+use Give\Donations\Models\Donation;
 use Give\Donations\ValueObjects\DonationType;
 use Give\Donors\Models\Donor;
 use Give\Framework\PaymentGateways\Contracts\PaymentGatewayInterface;
