d5bed02d48ad47af7a0449c3c29264663ff93d6d
Glauber Silva <glauber.silva@givewp.com>
2023-02-09 13:44:25 -0300

    refactor: add properties

--- src/Framework/PaymentGateways/Commands/SubscriptionSynced.php
+++ src/Framework/PaymentGateways/Commands/SubscriptionSynced.php
@@ -4,12 +4,34 @@ namespace Give\Framework\PaymentGateways\Commands;
 
 use Give\Subscriptions\Models\Subscription;
 
+/**
+ * @unreleased
+ */
 class SubscriptionSynced implements GatewayCommand
 {
+    /**
+     * @var Subscription
+     */
+    public $subscription;
+
+    /**
+     * @var array
+     */
+    public $donations;
+
+    /**
+     * @var string
+     */
+    public $notice;
+
+    /**
+     * @unreleased
+     */
     public function __construct(Subscription $subscription, array $donations, string $notice = '')
     {
-        //$this->gatewayTransactionId = $gatewayTransactionId;
-        //$this->gatewaySubscriptionId = $gatewaySubscriptionId;
+        $this->subscription = $subscription;
+        $this->donations = $donations;
+        $this->notice = $notice;
     }
 
 }
