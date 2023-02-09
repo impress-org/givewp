8414062de07423b6c6ed9473d252c23988e5f382
Glauber Silva <glauber.silva@givewp.com>
2023-02-09 14:37:20 -0300

    refactor: change return

--- src/Framework/PaymentGateways/CommandHandlers/SubscriptionSyncedHandler.php
+++ src/Framework/PaymentGateways/CommandHandlers/SubscriptionSyncedHandler.php
@@ -17,8 +17,19 @@ class SubscriptionSyncedHandler
      */
     public function __invoke(SubscriptionSynced $subscriptionSynced): JsonResponse
     {
+        $subscriptionAttributesChanged = $subscriptionSynced->subscription->getDirty();
+        $subscriptionOldStatus = $subscriptionSynced->subscription->getOriginal('status');
+        $subscriptionNewStatus = $subscriptionSynced->subscription->status->getValue();
+
+        $subscriptionSynced->subscription->save();
+
         return response()->json([
-            'subscription' => $subscriptionSynced->subscription->getDirty(),
+            'subscription' => [
+                'attributes' => $subscriptionSynced->subscription->getAttributes(),
+                'attributes-changes' => $subscriptionAttributesChanged,
+                'old-status' => $subscriptionOldStatus,
+                'new-status' => $subscriptionNewStatus,
+            ],
             'donations' => $subscriptionSynced->donations,
             'notice' => $subscriptionSynced->notice,
         ]);
