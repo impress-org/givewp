45f2246b77c5d238e8aeb85a99c4604aa72eda2e
Jon Waldstein <jonwaldstein@macbook-air.mynetworksettings.com>
2023-02-10 12:15:46 -0500

    refactor: conditionally display section label and description

--- src/NextGen/DonationForm/resources/registrars/templates/layouts/Section.tsx
+++ src/NextGen/DonationForm/resources/registrars/templates/layouts/Section.tsx
@@ -1,14 +1,24 @@
 import type {SectionProps} from '@givewp/forms/propTypes';
 
-export default function Section({section: {name, label, description}, children}: SectionProps) {
-    return (
-        <fieldset aria-labelledby={name}>
-            <div>
+const SectionHeader = ({name, label, description}: { name: string; label?: string; description?: string }) => {
+    return (label.length > 0 || description.length > 0) ? (
+        <div>
+            {label.length > 0 && (
                 <legend id={name}>{label}</legend>
+            )}
+            {description.length > 0 && (
                 <p>
                     <em>{description}</em>
                 </p>
-            </div>
+            )}
+        </div>
+    ) : <></>;
+};
+
+export default function Section({section: {name, label, description}, children}: SectionProps) {
+    return (
+        <fieldset aria-labelledby={name}>
+            <SectionHeader name={name} label={label} description={description}/>
             <div className="givewp-section-nodes">{children}</div>
         </fieldset>
     );
