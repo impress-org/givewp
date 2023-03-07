b9e1a4ae257d0a5bb5da7615a4e8e82a5a78744a
Joshua Dinh <josh@joshuadinh.tech>
2023-03-07 13:46:45 -0800

    style: add Form component styles + update FormNavigation margin

--- src/Views/Components/AdminUI/FormElements/index.tsx
+++ src/Views/Components/AdminUI/FormElements/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
+import styles from './style.module.scss';
 import {Form} from '../types';
 
 const Form: React.FC<HTMLFormElement | Form> = ({children, id, onSubmit}) => (
-    <form id={id} onSubmit={onSubmit}>
+    <form className={styles.form} id={id} onSubmit={onSubmit}>
         {children}
     </form>
 );
