9afd7448e62d38d2c7c39e7671de39180997f63d
Joshua Dinh <josh@joshuadinh.tech>
2023-03-08 11:31:03 -0800

    feature: add MoreActionsMenu component

---
+++ src/Views/Components/AdminUI/MoreActionsMenu/index.tsx
@@ -0,0 +1,19 @@
+import styles from './style.module.scss';
+
+export default function MoreActionsMenu({actionConfig, toggle}) {
+    const handleClick = (action) => {
+        action();
+        toggle();
+    };
+    return (
+        <ul className={styles.actionMenu}>
+            {actionConfig.map((action) => {
+                return (
+                    <li>
+                        <button onClick={() => handleClick(action.action)}>{action.title}</button>
+                    </li>
+                );
+            })}
+        </ul>
+    );
+}
