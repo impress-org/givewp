ea5de5ea66685f24517c7d654b606978b71d4647
Joshua Dinh <josh@joshuadinh.tech>
2023-03-16 11:41:57 -0700

    fix: update

--- src/Donations/resources/DonationDetails/app/components/PaymentInformation/DonationMethod.tsx
+++ src/Donations/resources/DonationDetails/app/components/PaymentInformation/DonationMethod.tsx
@@ -1,5 +1,3 @@
-import PaypalIcon from '@givewp/components/AdminUI/Icons/PaypalIcon';
-
 import {DonationMethodProps} from './types';
 
 import styles from './style.module.scss';
@@ -8,30 +6,6 @@ import styles from './style.module.scss';
  *
  * @unreleased
  */
-function renderGatewayIcon(gatewayId) {
-    switch (gatewayId) {
-        case 'paypal':
-            return <PaypalIcon />;
-        case 'stripe':
-            return <span>stripe test</span>;
-        case 'test-gateway-next-gen':
-            return <span>next-gen test</span>;
-        case 'offline-donation':
-            return <span>offline-donation test</span>;
-        default:
-            return '';
-    }
-}
-
-/**
- *
- * @unreleased
- */
 export default function DonationMethod({gateway, gatewayId}: DonationMethodProps) {
-    return (
-        <div className={styles.paymentMethod}>
-            {renderGatewayIcon(gatewayId)}
-            {gateway}
-        </div>
-    );
+    return <div className={styles.paymentMethod}>{gateway}</div>;
 }
