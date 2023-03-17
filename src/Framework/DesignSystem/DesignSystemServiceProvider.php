651cf11a9f01860c2e521ac4206682b013e72d35
Jason Adams <jason.adams@givewp.com>
2023-03-17 14:40:34 -0700

    feature: add design system style registration

---
+++ src/Framework/DesignSystem/DesignSystemServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Give\Framework\DesignSystem;
+
+use Give\Framework\DesignSystem\Actions\RegisterDesignSystemStyles;
+use Give\Helpers\Hooks;
+use Give\ServiceProviders\ServiceProvider;
+
+class DesignSystemServiceProvider implements ServiceProvider
+{
+    public function register()
+    {
+    }
+
+    public function boot()
+    {
+        Hooks::addAction('wp_enqueue_scripts', RegisterDesignSystemStyles::class);
+        Hooks::addAction('admin_enqueue_scripts', RegisterDesignSystemStyles::class);
+    }
+}
