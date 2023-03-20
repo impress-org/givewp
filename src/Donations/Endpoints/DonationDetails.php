ebe2d7b4f7c89bcc4599cd0d91e3ed578d1bea21
Paulo Iankoski <me@wpdev.com.br>
2023-03-20 18:46:44 -0300

    refactor: split attributes array in functions

--- src/Donations/Endpoints/DonationDetails.php
+++ src/Donations/Endpoints/DonationDetails.php
@@ -22,12 +22,24 @@ class DonationDetails extends Endpoint
     protected $endpoint = 'admin/donation/(?P<id>[\d]+)';
 
     /**
+     * @var array
+     */
+    private $attributes = [];
+
+    /**
      * @unreleased
      *
      * @inheritDoc
      */
     public function registerRoute()
     {
+        $this->registerIdAttribute();
+        $this->registerTotalDonationAttribute();
+        $this->registerFeeRecoveredAttribute();
+        $this->registerCreatedAtAttribute();
+        $this->registerStatusAttribute();
+        $this->registerFormAttribute();
+
         register_rest_route(
             'give-api/v2',
             $this->endpoint,
@@ -37,91 +49,193 @@ class DonationDetails extends Endpoint
                     'callback' => [$this, 'handleRequest'],
                     'permission_callback' => [$this, 'permissionsCheck'],
                 ],
-                'args' => [
-                    'id' => [
-                        'type' => 'integer',
-                        'required' => true,
-                        'validate_callback' => function ($param) {
-                            if (give()->donations->getById($param) === null) {
-                                return new WP_Error(
-                                    'donation_not_found',
-                                    __('Donation not found.', 'give'),
-                                    ['status' => 404]
-                                );
-                            }
-
-                            return true;
-                        },
-                    ],
-                    'totalDonation' => [
-                        'type' => 'number',
-                        'required' => false,
-                        'validate_callback' => function ($param) {
-                            if ($param < 0) {
-                                return new WP_Error(
-                                    'invalid_total_donation',
-                                    __('Invalid total donation.', 'give'),
-                                    ['status' => 400]
-                                );
-                            }
-
-                            return true;
-                        },
-                    ],
-                    'feeRecovered' => [
-                        'type' => 'number',
-                        'required' => false,
-                        'validate_callback' => function ($param) {
-                            if ($param < 0) {
-                                return new WP_Error(
-                                    'invalid_fee_recovered',
-                                    __('Invalid fee recovered.', 'give'),
-                                    ['status' => 400]
-                                );
-                            }
-
-                            return true;
-                        },
-                    ],
-                    'createdAt' => [
-                        'type' => 'string',
-                        'required' => false,
-                        'validate_callback' => function ($param) {
-                            if ( ! DateTime::createFromFormat(Temporal::ISO8601_JS,
-                                    $param) && ! Temporal::toDateTime($param)) {
-                                return new WP_Error(
-                                    'invalid_date',
-                                    __('Invalid date.', 'give'),
-                                    ['status' => 400]
-                                );
-                            }
-
-                            return true;
-                        },
-                    ],
-                    'status' => [
-                        'type' => 'string',
-                        'required' => false,
-                        'enum' => array_values(DonationStatus::toArray()),
-                    ],
-                    'form' => [
-                        'type' => 'integer',
-                        'required' => false,
-                        'validate_callback' => function ($param) {
-                            if (give()->donationForms->getById($param) === null) {
-                                return new WP_Error(
-                                    'form_not_found',
-                                    __('Form not found.', 'give'),
-                                    ['status' => 404]
-                                );
-                            }
-
-                            return true;
-                        },
-                    ],
-                ],
+                'args' => $this->getRouteAttributes(),
             ]
         );
+    }
+
+    /**
+     * Register id attribute.
+     *
+     * @unreleased
+     *
+     * @return void
+     */
+    private function registerIdAttribute()
+    {
+        $this->attributes['id'] = [
+            'type' => 'integer',
+            'required' => true,
+            'validate_callback' => function ($param) {
+                if (give()->donations->getById($param) === null) {
+                    return new WP_Error(
+                        'donation_not_found',
+                        __('Donation not found.', 'give'),
+                        ['status' => 404]
+                    );
+                }
+
+                return true;
+            },
+        ];
+    }
+
+    /**
+     * Register totalDonation attribute.
+     *
+     * @unreleased
+     *
+     * @return void
+     */
+    private function registerTotalDonationAttribute()
+    {
+        $this->attributes['totalDonation'] = [
+            'type' => 'number',
+            'required' => false,
+            'validate_callback' => function ($param) {
+                if ($param < 0) {
+                    return new WP_Error(
+                        'invalid_total_donation',
+                        __('Invalid total donation.', 'give'),
+                        ['status' => 400]
+                    );
+                }
+
+                return true;
+            },
+            'update_callback' => function ($value, $donation) {
+                $donation->amount = Money::fromDecimal(
+                    $value,
+                    $donation->amount->getCurrency()
+                );
+
+                return $donation;
+            },
+        ];
+    }
+
+    /**
+     * Register feeRecovered attribute.
+     *
+     * @unreleased
+     *
+     * @return void
+     */
+    private function registerFeeRecoveredAttribute()
+    {
+        $this->attributes['feeRecovered'] = [
+            'type' => 'number',
+            'required' => false,
+            'validate_callback' => function ($param) {
+                if ($param < 0) {
+                    return new WP_Error(
+                        'invalid_fee_recovered',
+                        __('Invalid fee recovered.', 'give'),
+                        ['status' => 400]
+                    );
+                }
+
+                return true;
+            },
+            'update_callback' => function ($value, $donation) {
+                $donation->feeAmountRecovered = Money::fromDecimal(
+                    $value,
+                    $donation->feeAmountRecovered->getCurrency()
+                );
+
+                return $donation;
+            },
+        ];
+    }
+
+    /**
+     * Register createdAt attribute.
+     *
+     * @unreleased
+     *
+     * @return void
+     */
+    private function registerCreatedAtAttribute()
+    {
+        $this->attributes['createdAt'] = [
+            'type' => 'string',
+            'required' => false,
+            'validate_callback' => function ($param) {
+                if ( ! DateTime::createFromFormat(Temporal::ISO8601_JS,
+                        $param) && ! Temporal::toDateTime($param)) {
+                    return new WP_Error(
+                        'invalid_date',
+                        __('Invalid date.', 'give'),
+                        ['status' => 400]
+                    );
+                }
+
+                return true;
+            },
+            'update_callback' => function ($value, $donation) {
+                $date = DateTime::createFromFormat(Temporal::ISO8601_JS, $value);
+                if ( ! $date) {
+                    $date = Temporal::toDateTime($value);
+                }
+                $donation->createdAt = $date;
+
+                return $donation;
+            },
+        ];
+    }
+
+    /**
+     * Register status attribute.
+     *
+     * @unreleased
+     *
+     * @return void
+     */
+    private function registerStatusAttribute()
+    {
+        $this->attributes['status'] = [
+            'type' => 'string',
+            'required' => false,
+            'enum' => array_values(DonationStatus::toArray()),
+            'update_callback' => function ($value, $donation) {
+                $donation->status = $value;
+
+                return $donation;
+            },
+        ];
+    }
+
+    /**
+     * Register form attribute.
+     *
+     * @unreleased
+     *
+     * @return void
+     */
+    private function registerFormAttribute()
+    {
+        $this->attributes['form'] = [
+            'type' => 'integer',
+            'required' => false,
+            'validate_callback' => function ($param) {
+                if (give()->donationForms->getById($param) === null) {
+                    return new WP_Error(
+                        'form_not_found',
+                        __('Form not found.', 'give'),
+                        ['status' => 404]
+                    );
+                }
+
+                return true;
+            },
+            'update_callback' => function ($value, $donation) {
+                $form = give()->donationForms->getById($value);
+                $donation->formId = $value;
+                $donation->formTitle = $form->title;
+
+                return $donation;
+            },
+        ];
     }
 
     /**
@@ -157,42 +271,14 @@ class DonationDetails extends Endpoint
         $updatedFields = [];
 
         try {
-            $totalDonation = $request->get_param('totalDonation');
-            if ($totalDonation) {
-                $donation->amount = Money::fromDecimal(
-                    $totalDonation,
-                    $donation->amount->getCurrency()
-                );
-
-                $updatedFields[] = 'totalDonation';
-            }
-
-            $feeRecovered = $request->get_param('feeRecovered');
-            if ($feeRecovered) {
-                $donation->feeAmountRecovered = Money::fromDecimal(
-                    $feeRecovered,
-                    $donation->amount->getCurrency()
-                );
-
-                $updatedFields[] = 'feeRecovered';
-            }
-
-            $createdAt = $request->get_param('createdAt');
-            if ($createdAt) {
-                $formattedDate = DateTime::createFromFormat(Temporal::ISO8601_JS, $createdAt);
-                if ( ! $formattedDate) {
-                    $formattedDate = Temporal::toDateTime($createdAt);
+            foreach ($this->attributes as $attr => $attrArgs) {
+                if ( ! $request->has_param($attr) || ! array_key_exists('update_callback', $attrArgs)) {
+                    continue;
                 }
-                $donation->createdAt = $formattedDate;
 
-                $updatedFields[] = 'createdAt';
-            }
-
-            $status = $request->get_param('status');
-            if ($status) {
-                $donation->status = new DonationStatus($status);
-
-                $updatedFields[] = 'status';
+                $value = $request->get_param($attr);
+                $donation = $attrArgs['update_callback']($value, $donation);
+                $updatedFields[] = $attr;
             }
 
             $formId = $request->get_param('form');
@@ -218,6 +304,23 @@ class DonationDetails extends Endpoint
                 'success' => true,
                 'updatedFields' => $updatedFields,
             ]
+        );
+    }
+
+    /**
+     * @return array
+     */
+    private function getRouteAttributes(): array
+    {
+        return array_map(
+            function ($attribute) {
+                if (array_key_exists('update_callback', $attribute)) {
+                    unset($attribute['update_callback']);
+                }
+
+                return $attribute;
+            },
+            $this->attributes
         );
     }
 }
