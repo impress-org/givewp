dd3481849f9dabf5a52b76ec21fec5b1cea756c8
Joshua Dinh <josh@joshuadinh.tech>
2023-03-24 16:34:14 -0700

    feature: handle donation methods

--- src/Donations/resources/DonationDetails/app/components/PaymentInformation/DonationMethod/index.tsx
+++ src/Donations/resources/DonationDetails/app/components/PaymentInformation/DonationMethod/index.tsx
@@ -1,11 +1,26 @@
 import styles from './style.module.scss';
 import {DonationMethodProps} from './types';
+import {paymentMethodList} from './paymentMethodList';
 
 /**
  *
  * @unreleased
  */
 
+function GatewayLogo(gatewwayId: string) {
+    for (const prop in paymentMethodList) {
+        if (prop === gatewwayId) {
+            const matchingValue = paymentMethodList[prop];
+            return matchingValue;
+        }
+    }
+}
+
 export default function DonationMethod({gatewayLabel, gatewayId}: DonationMethodProps) {
-    return <div className={styles.paymentMethod}>{gatewayLabel}</div>;
+    return (
+        <div className={styles.paymentMethod}>
+            <span>{GatewayLogo(gatewayId)}</span>
+            <span>{gatewayLabel}</span>
+        </div>
+    );
 }
