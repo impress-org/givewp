e87ff596678b32715805bf18160cefced406f463
Jon Waldstein <jon@givewp.com>
2023-03-24 15:18:41 -0400

    feature: add processing actions to stripe webhook listeners (#6749)
    Co-authored-by: Jon Waldstein <jonwaldstein@jons-air.mynetworksettings.com>

--- src/PaymentGateways/Gateways/Stripe/Webhooks/Listeners/PaymentIntentSucceeded.php
+++ src/PaymentGateways/Gateways/Stripe/Webhooks/Listeners/PaymentIntentSucceeded.php
@@ -19,6 +19,11 @@ class PaymentIntentSucceeded extends StripeEventListener
      */
     public function processEvent(Event $event)
     {
+        /**
+         * @unreleased
+         */
+        do_action('give_stripe_processing_payment_intent_succeeded', $event);
+
         /* @var PaymentIntent $paymentIntent */
         $paymentIntent = $event->data->object;
 
