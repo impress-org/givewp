5c44e720eb4b45a886e4ac6e130d7a981f101fd3
Joshua Dinh <josh@joshuadinh.tech>
2023-03-28 10:51:58 -0700

    fix:remove gatway ID and icons

--- src/Donations/resources/DonationDetails/app/components/PaymentInformation/DonationMethod/index.tsx
+++ src/Donations/resources/DonationDetails/app/components/PaymentInformation/DonationMethod/index.tsx
@@ -1,28 +1,9 @@
 import {DonationMethodProps} from './types';
-import {paymentMethodList} from './paymentMethodList';
 
 /**
  *
  * @unreleased
  */
-
-function GatewayLogo({gatewayId}: {gatewayId: string}) {
-    for (const prop in paymentMethodList) {
-        if (prop === gatewayId) {
-            return paymentMethodList[prop];
-        }
-    }
-    return null; // Return null if the gatewayId is not found in the paymentMethodList
-}
-/**
- *
- * @unreleased
- */
-export default function DonationMethod({gatewayLabel, gatewayId}: DonationMethodProps) {
-    return (
-        <div>
-            <GatewayLogo gatewayId={gatewayId} />
-            <span>{gatewayLabel}</span>
-        </div>
-    );
+export default function DonationMethod({gatewayLabel}: DonationMethodProps) {
+    return <span>{gatewayLabel}</span>;
 }
--- src/Donations/resources/DonationDetails/app/components/PaymentInformation/DonationMethod/types.ts
+++ src/Donations/resources/DonationDetails/app/components/PaymentInformation/DonationMethod/types.ts
@@ -1,4 +1,3 @@
 export type DonationMethodProps = {
     gatewayLabel: string;
-    gatewayId: string
 }
