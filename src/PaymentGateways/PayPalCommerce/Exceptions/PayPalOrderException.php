46e1c4b22fddd7da515096911c026d8a057c31da
Kyle B. Johnson <kjohnson@users.noreply.github.com>
2023-03-31 09:35:02 -0400

    Add support for PayPal Order ID via gateway data (#6723)
    * refactor: Add support for orderID via gateway data
    
    * refactor: Consolidate hook callback and action

---
+++ src/PaymentGateways/PayPalCommerce/Exceptions/PayPalOrderException.php
@@ -0,0 +1,9 @@
+<?php
+namespace Give\PaymentGateways\PayPalCommerce\Exceptions;
+
+use Give\Framework\Exceptions\Primitives\Exception;
+
+/**
+ * @unreleased
+ */
+class PayPalOrderException extends Exception {}
