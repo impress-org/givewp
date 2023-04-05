9b2c574c0271e015508d0df4317da363a33fe62f
Joshua Dinh <josh@joshuadinh.tech>
2023-04-05 13:29:11 -0700

    Feature: update icons

---
+++ src/Donations/resources/DonationDetails/app/components/DonorComments/index.tsx
@@ -0,0 +1,122 @@
+import {Fragment, useRef, useState} from 'react';
+
+import {__} from '@wordpress/i18n';
+
+import ModalDialog from '@givewp/components/AdminUI/ModalDialog';
+import SectionHeader, {HeaderAction, Title} from '@givewp/components/AdminUI/SectionHeader';
+import Button from '@givewp/components/AdminUI/Button';
+import {TextAreaField} from '@givewp/components/AdminUI/FormElements';
+import {FieldsetContainer} from '@givewp/components/AdminUI/ContainerLayout';
+import EmptyState from '@givewp/components/AdminUI/EmptyState';
+
+import styles from './style.module.scss';
+import {apiNonce, apiRoot} from '../../../../window';
+import {usePostRequest} from '@givewp/components/AdminUI/api';
+import PenToPaperIcon from '@givewp/components/AdminUI/Icons/PenToPaperIcon';
+import AddIcon from '@givewp/components/AdminUI/Icons/AddIcon';
+import PlaceholderAvatar from '@givewp/components/AdminUI/Icons/PlaceholderAvatar';
+
+const {comment, id} = window.GiveDonations.donationDetails;
+const donorAvatar = '';
+
+export function DonorComments() {
+    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const hasComment = !!comment;
+
+    const endpoint = `${apiRoot}/${id}/comment`;
+
+    const successMessage = __('Your update to the comment section was a success.', 'give');
+    const errorMessage = __('There was an error while updating your comment. Please try again.', 'give');
+
+    const {postData} = usePostRequest(endpoint, apiNonce, successMessage, errorMessage);
+
+    const handlePostRequest = async (updatedComment) => {
+        const data = {content: updatedComment};
+
+        try {
+            await postData(data);
+        } catch (error) {
+            console.error(error);
+        }
+        setIsModalOpen(false);
+    };
+
+    return (
+        <section>
+            <SectionHeader>
+                <Title title={__('Donor comment', 'give')} />
+                <HeaderAction action={openModal}>
+                    {hasComment ? (
+                        <Fragment>
+                            <PenToPaperIcon />
+                            {__('Edit comment', 'give')}
+                        </Fragment>
+                    ) : (
+                        <Fragment>
+                            <AddIcon />
+                            {__('Add comment', 'give')}
+                        </Fragment>
+                    )}
+                </HeaderAction>
+            </SectionHeader>
+
+            <SectionContainer hasComment={hasComment} />
+
+            <ModalDialog
+                open={isModalOpen}
+                onClose={() => setIsModalOpen(false)}
+                handleClose={() => setIsModalOpen(false)}
+                title={__('Donor comment', 'give')}
+            >
+                <CommentDialog handlePostRequest={handlePostRequest} />
+            </ModalDialog>
+        </section>
+    );
+}
+
+function SectionContainer({hasComment}: {hasComment: boolean}) {
+    const Avatar = () => (donorAvatar ? <img src={donorAvatar} alt={'profile image'} /> : <PlaceholderAvatar />);
+
+    return (
+        <FieldsetContainer>
+            {hasComment ? (
+                <div className={styles.comments}>
+                    <Avatar />
+                    <span>{comment}</span>
+                </div>
+            ) : (
+                <EmptyState message={__('No comments.', 'give')} />
+            )}
+        </FieldsetContainer>
+    );
+}
+
+function CommentDialog({handlePostRequest}) {
+    const commentRef = useRef(null);
+
+    return (
+        <div>
+            <TextAreaField
+                defaultValue={comment}
+                ref={commentRef}
+                name={'comments'}
+                placeholder={__('Type in your comment', 'give')}
+                label={__('Comment', 'give')}
+                type={'text'}
+            />
+            <Button
+                variant={'primary'}
+                size={'large'}
+                onClick={() => handlePostRequest(commentRef.current.value)}
+                disabled={false}
+            >
+                {__('Save changes', 'give')}
+            </Button>
+        </div>
+    );
+}
---
+++ src/Views/Components/AdminUI/Icons/PlaceholderAvatar.tsx
@@ -0,0 +1,43 @@
+export default function PlaceholderAvatar() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="210" height="211" viewBox="0 0 210 211" fill="none">
+            <g filter="url(#filter0_d_1002_562)">
+                <circle cx="105" cy="104" r="100" fill="#E8E8E8" />
+                <circle cx="105" cy="104" r="98.1731" stroke="white" strokeWidth="3.65389" />
+            </g>
+            <mask id="mask0_1002_562" maskUnits="userSpaceOnUse" x="7" y="4" width="196" height="196">
+                <circle cx="105" cy="102" r="96" fill="white" stroke="white" strokeWidth="4" />
+            </mask>
+            <g mask="url(#mask0_1002_562)">
+                <path
+                    d="M106 115C129.704 115 148.916 95.7882 148.916 72.0834C148.916 48.3787 129.704 29.1667 106 29.1667C82.2949 29.1667 63.083 48.3787 63.083 72.0834C63.083 95.7882 82.2949 115 106 115ZM136.041 125.729H130.442C122.999 129.149 114.717 131.094 106 131.094C97.2822 131.094 89.0342 129.149 81.5573 125.729H75.958C51.0798 125.729 30.8955 145.913 30.8955 170.792V184.74C30.8955 193.625 38.1042 200.833 46.9893 200.833H165.01C173.895 200.833 181.104 193.625 181.104 184.74V170.792C181.104 145.913 160.92 125.729 136.041 125.729Z"
+                    fill="#B6B6B6"
+                />
+            </g>
+            <defs>
+                <filter
+                    id="filter0_d_1002_562"
+                    x="0.128151"
+                    y="0.346113"
+                    width="209.744"
+                    height="209.744"
+                    filterUnits="userSpaceOnUse"
+                    colorInterpolationFilters="sRGB"
+                >
+                    <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                    <feColorMatrix
+                        in="SourceAlpha"
+                        type="matrix"
+                        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                        result="hardAlpha"
+                    />
+                    <feOffset dy="1.21796" />
+                    <feGaussianBlur stdDeviation="2.43592" />
+                    <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1002_562" />
+                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1002_562" result="shape" />
+                </filter>
+            </defs>
+        </svg>
+    );
+}
