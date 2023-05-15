6ca948743cce1e544d4a4fcb9da7205f26b954b4
Joshua Dinh <75056371+JoshuaHungDinh@users.noreply.github.com>
2023-05-15 11:39:48 -0700

    feature: Add rotating "ProTip" message to Donation table page (#6797)
    * fix: Fix broken reference to tribute last name (#6721)
    
    Co-authored-by: David GABISON <david.gabison@outlook.com>
    
    * fix: prevent js error on subscription cancellation from donor dashboard (#6792)
    
    * feature: add product recommendation banner
    
    * refactor: format with prettier
    
    * Render component based on window
    
    * refactor: add comment, update fontsize
    
    * refactor: split recommendation component logic
    
    * refactor: update product recommendation component.
    
    * feature: add the necessary logic to handle rotating product recommendations
    
    * feature: add logic to handle dismissed product options
    
    * fix: add type to dismissedRecommendations
    
    * refactor: manage dismissedRecommendations with state
    
    * refactor: move component directory and update imports
    
    * fix: clears console warning for false and undefined classname values
    
    * feature: add permisions check
    
    * refactor: specify page for dismissed notice value
    
    * refactor: specify the page in the dismissed notice value
    
    * feature: add dismissal notice for active plugins
    
    * feature: span Protip to accomodate additional columns for addons
    
    * refactor: update dir name and doc tag
    
    * refactor: move endpoint into Promotions directory
    
    * refactor: return early
    
    * refactor: close recommmendation prior to request
    
    * fix: update createInterpolateElement
    
    * refactor: update directory name
    
    * refactor: pass recommendation as table prop
    
    * refactor: make ProductRecommendations more flexible
    
    * refactor: translate message
    
    * refactor: update route directory
    
    * refactor: update fee recovery link text
    
    * refactor: use lowercase convention
    
    * refactor: update fee recovery message
    
    * refactor: update and include routes in endpoint directory for inPluginUpsells
    
    * refactor: log error messages
    
    * fix: implement restroute. updates imports
    
    * feature: add auth status code
    
    * refactor: import log
    
    * feature: include utm links
    
    * refactor: simplify route
    
    * refactor: remove todo
    
    * refactor: remove try catch statment.
    
    ---------
    
    Co-authored-by: David GABISON <47400514+pulsovi@users.noreply.github.com>
    Co-authored-by: David GABISON <david.gabison@outlook.com>
    Co-authored-by: Ravinder Kumar <ravinder.kumar.apj@gmail.com>

---
+++ src/Views/Components/ListTable/ProductRecommendations/useRecommendations.ts
@@ -0,0 +1,55 @@
+import {useCallback, useState} from 'react';
+
+type EnumValues =
+    | 'givewp_donations_recurring_recommendation_dismissed'
+    | 'givewp_donations_fee_recovery_recommendation_dismissed'
+    | 'givewp_donations_designated_funds_recommendation_dismissed';
+
+export interface RecommendedProductData {
+    enum: EnumValues;
+    documentationPage: string;
+    message: string;
+    innerHtml: string;
+}
+
+/**
+ * @unreleased
+ */
+export function useRecommendations(apiSettings, options) {
+    const [dismissedRecommendations, setDismissedRecommendations] = useState<string[]>(
+        apiSettings.dismissedRecommendations
+    );
+
+    const getRecommendation = useCallback((): RecommendedProductData | null => {
+        const availableOptions = options.filter((option) => !dismissedRecommendations.includes(option.enum));
+
+        if (availableOptions.length === 0) {
+            return null;
+        }
+
+        const randomIndex = Math.floor(Math.random() * availableOptions.length);
+
+        return availableOptions[randomIndex];
+    }, [dismissedRecommendations]);
+
+    const removeRecommendation = async (data: {option: EnumValues}): Promise<void> => {
+        const url = `/wp-json/give-api/v2/admin/recommended-options`;
+
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-WP-Nonce': apiSettings.apiNonce,
+                },
+                body: JSON.stringify(data),
+            });
+
+            const result = await response.json();
+
+        if (result.success) {
+            setDismissedRecommendations((prev) => [...prev, data.option]);
+        }
+    };
+
+    return {getRecommendation, removeRecommendation};
+}
