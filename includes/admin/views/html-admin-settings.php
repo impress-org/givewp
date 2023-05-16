50ab907c9c7e8f671625a4eeb32e884b0b01610b
Joshua Dinh <josh@joshuadinh.tech>
2023-05-16 13:46:22 -0700

    refactor: use utils to check addon

--- includes/admin/views/html-admin-settings.php
+++ includes/admin/views/html-admin-settings.php
@@ -1,5 +1,7 @@
 <?php
 
+use Give\Helpers\Utils;
+
 /**
  * Admin View: Settings
  */
@@ -103,7 +105,7 @@ if ( ! empty($tabs) && array_key_exists(give_get_current_setting_tab(), $tabs)) 
              *
              * @unreleased
              */
-            if (class_exists('Give_Recurring')) {
+            if (Utils::isPluginActive('give-recurring/give-recurring.php')) {
                 echo '
                          <a class="give-nav-addons-tab" href="https://docs.givewp.com/settings-star" target="_blank">
                             <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
