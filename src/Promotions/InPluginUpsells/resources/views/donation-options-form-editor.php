b33aedf299a3431e80945c8481b1694737d92c97
Joshua Dinh <josh@joshuadinh.tech>
2023-05-17 16:04:32 -0700

    fix: remove esc_url

--- src/Promotions/InPluginUpsells/resources/views/donation-options-form-editor.php
+++ src/Promotions/InPluginUpsells/resources/views/donation-options-form-editor.php
@@ -2,7 +2,7 @@
 
 echo '<div class="givewp-donation-options inner-panel">
     <div>
-        <img src="' . esc_url(GIVE_PLUGIN_URL . 'assets/dist/images/list-table/light-bulb-icon.svg') . '" alt="light-bulb-icon" />
+        <img src="' . GIVE_PLUGIN_URL . 'assets/dist/images/list-table/light-bulb-icon.svg' . '" alt="light-bulb-icon" />
         <p>';
 _e(
     'Did you know you can provide donors with flexible recurring donation options to increase their annual giving by 30% or more.',
@@ -11,11 +11,11 @@ _e(
 echo '</p>
         <a href="https://docs.givewp.com/recurring-legacy" target="_blank" class="givewp-donation-options_external">';
 _e('Get more donations');
-echo '<img src="' . esc_url(GIVE_PLUGIN_URL . 'assets/dist/images/list-table/external-link-icon.svg') . '" alt="external-link" />
+echo '<img src="' . GIVE_PLUGIN_URL . 'assets/dist/images/list-table/external-link-icon.svg' . '" alt="external-link" />
         </a>
     </div>
     <button type="button" class="givewp-donation-options_close">
-        <img src="' . esc_url(GIVE_PLUGIN_URL . 'assets/dist/images/admin/close-icon.svg') . '" alt="close-message" />
+        <img src="' . GIVE_PLUGIN_URL . 'assets/dist/images/admin/close-icon.svg' . '" alt="close-message" />
     </button>
 </div>';
 
