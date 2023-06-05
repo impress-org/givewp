73013ea8c18c96283fe39ce483bf4aa76a9ca776
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2023-06-05 10:34:50 +0530

    feat: add request to generate client token for paypal js sdk

---
+++ src/PaymentGateways/PayPalCommerce/PayPalCheckoutSdk/Requests/GenerateClientToken.php
@@ -0,0 +1,19 @@
+<?php
+namespace Give\PaymentGateways\PayPalCommerce\PayPalCheckoutSdk\Requests;
+
+use PayPalHttp\HttpRequest;
+
+/**
+ * Class GenerateClientToken
+ *
+ * This class use to generate a client token for PayPal JS SDK.
+ *
+ * @unreleased
+ */
+class GenerateClientToken extends HttpRequest
+{
+    public function __construct()
+    {
+        parent::__construct('/v1/identity/generate-token', "POST");
+    }
+}
