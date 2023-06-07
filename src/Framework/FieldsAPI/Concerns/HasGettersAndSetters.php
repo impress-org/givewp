80dd27a346fb4a8cb534ca06bd8841f14a21ad6f
Paulo Iankoski <me@wpdev.com.br>
2023-06-07 15:36:38 -0300

    feature: add a HasGettersAndSetters trait

---
+++ src/Framework/FieldsAPI/Concerns/HasGettersAndSetters.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Give\Framework\FieldsAPI\Concerns;
+
+use BadMethodCallException;
+
+/**
+ * Trait HasGettersAndSetters
+ *
+ * @unreleased 
+ */
+trait HasGettersAndSetters
+{
+    public function __call($name, $arguments)
+    {
+        $prefix = substr($name, 0, 3);
+        $property = lcfirst(substr($name, 3));
+
+        if (!property_exists($this, $property)) {
+            throw new BadMethodCallException(sprintf(__('Property %s does not exist', 'givewp'), $name));
+        }
+
+        if ($prefix === 'get') {
+            return $this->$property;
+        }
+
+        if ($prefix === 'set') {
+            $this->$property = $arguments[0];
+
+            return $this;
+        }
+
+        throw new BadMethodCallException(sprintf(__('Method %s does not exist', 'givewp'), $name));
+    }
+}
