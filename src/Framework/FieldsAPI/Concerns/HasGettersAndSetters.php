b0cff683374ac88049653b031804f066a44dc47e
Paulo Iankoski <me@wpdev.com.br>
2023-06-07 19:29:05 -0300

    refactor: set public getters and setters from DocComments

--- src/Framework/FieldsAPI/Concerns/HasGettersAndSetters.php
+++ src/Framework/FieldsAPI/Concerns/HasGettersAndSetters.php
@@ -2,6 +2,9 @@
 
 namespace Give\Framework\FieldsAPI\Concerns;
 
+use BadMethodCallException;
+use ReflectionClass;
+
 /**
  * Trait HasSettersAndGetters
  *
@@ -9,19 +12,55 @@ namespace Give\Framework\FieldsAPI\Concerns;
  */
 trait HasGettersAndSetters
 {
-    use HasGetters {
-        HasGetters::__call as protected callGetters;
-    }
-    use HasSetters {
-        HasSetters::__call as protected callSetters;
-    }
-
     public function __call($name, $arguments = null)
     {
-        if ($arguments) {
-            return $this->callSetters($name, $arguments);
-        } else {
-            return $this->callGetters($name, $arguments);
+        $gettersAndSetters = $this->extractGettersAndSetters();
+
+        if (!array_key_exists($name, $gettersAndSetters)) {
+            throw new BadMethodCallException(sprintf(__('Method %s does not exist', 'givewp'), $name));
         }
+
+        if (strpos($name, 'get') === 0) {
+            $property = lcfirst(substr($name, 3));
+
+            if (!property_exists($this, $property)) {
+                throw new BadMethodCallException(sprintf(__('Property %s does not exist', 'givewp'), $name));
+            }
+
+            return $this->$property;
+        }
+
+        $this->$name = $arguments[0];
+
+        return $this;
+    }
+
+    /**
+     * @unreleased
+     */
+    private function extractGettersAndSetters(): array
+    {
+        $methods = [];
+        $class = new ReflectionClass($this);
+
+        // Extract @method annotations
+        $docComment = $class->getDocComment();
+        $docComment = explode("\n", $docComment);
+        $docComment = array_filter($docComment, function ($line) {
+            return strpos($line, '@method') !== false;
+        });
+
+        /** @var array $docComment */
+        foreach ($docComment as $line) {
+            $pattern = '/@method\s*(\w+)\((\w+\s+\$\w+)?\):?\s*(\w+)?/';
+            preg_match($pattern, $line, $matches);
+
+            $methods[$matches[1]] = array_combine(
+                ['method', 'params', 'return'],
+                array_slice(array_pad($matches, 4, ''), 1)
+            );
+        }
+
+        return $methods;
     }
 }
