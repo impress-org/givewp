6fd8293d4fe52bb32ba623094e3bb50afa938a6e
Paulo Iankoski <me@wpdev.com.br>
2023-06-13 17:28:43 -0300

    Refactor: replace ReactDOMServer function with temporary rendering (#193)
    * refactor: replace ReactDOM function with temporary rendering
    
    * refactor: remove unnecessary destructuring
    
    Co-authored-by: Jason Adams <jason.adams@givewp.com>
    
    ---------
    
    Co-authored-by: Jason Adams <jason.adams@givewp.com>

--- src/FormBuilder/resources/js/form-builder/src/components/onboarding/steps/withText.tsx
+++ src/FormBuilder/resources/js/form-builder/src/components/onboarding/steps/withText.tsx
@@ -1,48 +1,68 @@
-import {renderToStaticMarkup} from "react-dom/server";
-import Logo from "@givewp/form-builder/components/icons/logo";
-import {Button} from "@wordpress/components";
-import {__, sprintf} from "@wordpress/i18n";
-import {createInterpolateElement} from "@wordpress/element";
+import {render} from '@wordpress/element';
+import Logo from '@givewp/form-builder/components/icons/logo';
+import {Button} from '@wordpress/components';
+import {__, sprintf} from '@wordpress/i18n';
 
 const TextContent = ({title, description, stepNumber, stepCount, isFirst, isLast}) => {
     return (
         <div>
             {isFirst && (
-                <div style={{display: "flex", justifyContent: 'center', margin: '0 auto var(--givewp-spacing-4)'}}>
-                    <Logo/>
+                <div style={{display: 'flex', justifyContent: 'center', margin: '0 auto var(--givewp-spacing-4)'}}>
+                    <Logo />
                 </div>
             )}
-            {!isFirst && !isLast &&(
-                <div style={{display:'flex', backgroundColor: 'var(--givewp-blue-25)', fontSize: '12px',padding:'4px',borderRadius:'2px',justifyContent:'space-between', alignItems: 'center'}}>
+            {!isFirst && !isLast && (
+                <div
+                    style={{
+                        display: 'flex',
+                        backgroundColor: 'var(--givewp-blue-25)',
+                        fontSize: '12px',
+                        padding: '4px',
+                        borderRadius: '2px',
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
+                    }}
+                >
                     <div>{sprintf(__('Step %s of %s', 'give'), stepNumber, stepCount)}</div>
-                    <Button variant='tertiary' className={'js-exit-tour'}>{__('Exit tour', 'give')}</Button>
+                    <Button variant="tertiary" className={'js-exit-tour'}>
+                        {__('Exit tour', 'give')}
+                    </Button>
                 </div>
             )}
-            <h3 style={{
-                fontSize: isFirst || isLast ? '20px' : '16px',
-                margin: 'var(--givewp-spacing-3) 0',
-            }}>{title}</h3>
+            <h3
+                style={{
+                    fontSize: isFirst || isLast ? '20px' : '16px',
+                    margin: 'var(--givewp-spacing-3) 0',
+                }}
+            >
+                {title}
+            </h3>
             <p style={{fontSize: '14px'}}>{description}</p>
         </div>
-    )
-}
+    );
+};
 
 const withText = (steps) => {
-
     return steps.map((step, index) => {
         const stepCountAdjustedForBookends = steps.length - 2;
-        const textContent = <TextContent
-            title={step.title}
-            description={step.text}
-            stepNumber={index}
-            stepCount={stepCountAdjustedForBookends}
-            isFirst={index === 0}
-            isLast={index === steps.length - 1}
-        />;
-        return {...step, ...{
-                text: renderToStaticMarkup(textContent)
-            }}
-    })
-}
+        const textContent = (
+            <TextContent
+                title={step.title}
+                description={step.text}
+                stepNumber={index}
+                stepCount={stepCountAdjustedForBookends}
+                isFirst={index === 0}
+                isLast={index === steps.length - 1}
+            />
+        );
+        const tempContainer = document.createElement('div');
+        render(textContent, tempContainer);
 
-export default withText
+        return {
+            ...step,
+            text: tempContainer.innerHTML,
+        };
+    });
+};
+
+export default withText;
