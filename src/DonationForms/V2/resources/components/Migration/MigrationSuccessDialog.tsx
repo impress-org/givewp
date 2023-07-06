39b06c72476cb279bdae5930032af6d917eeefcf
alaca <ante.laca@gmail.com>
2023-07-06 18:05:33 +0200

    refactor: dialog styles

--- src/DonationForms/V2/resources/components/Migration/MigrationSuccessDialog.tsx
+++ src/DonationForms/V2/resources/components/Migration/MigrationSuccessDialog.tsx
@@ -1,8 +1,9 @@
+import cx from 'classnames';
 import {__} from '@wordpress/i18n';
 import ModalDialog from '@givewp/components/AdminUI/ModalDialog';
 import ButtonGroup from '@givewp/components/AdminUI/ButtonGroup';
 import Button from '@givewp/components/AdminUI/Button';
-import {AlertTriangle} from '@givewp/components/AdminUI/Icons';
+import {AlertTriangle, Award} from '@givewp/components/AdminUI/Icons';
 
 import styles from './style.module.scss';
 
@@ -10,16 +11,17 @@ export default function MigrationSuccessDialog({handleClose, formId}) {
     return (
         <ModalDialog
             isOpen={true}
-            icon={<AlertTriangle />}
+            icon={<Award />}
             showHeader={false}
+            showCloseIcon={false}
             title={__('Great! First step completed', 'give')}
             handleClose={handleClose}
         >
-            <div className={styles.title}>
+            <div className={cx(styles.title, styles.center)}>
                 {__('Great! First step completed', 'give')}
             </div>
 
-            <div>
+            <div className={styles.center}>
                 {__('You just made a copy of your v2 form to the v3 form. Test the v3 form out to make sure it works as expected.', 'give')}
             </div>
 
