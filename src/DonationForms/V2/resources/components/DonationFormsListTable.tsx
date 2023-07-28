541c33ba891627833be1e72094b5ca03c493d4df
Kyle B. Johnson <kyle.johnson@hey.com>
2023-07-28 10:48:43 -0400

    fix: Update v2form check

--- src/DonationForms/V2/resources/components/DonationFormsListTable.tsx
+++ src/DonationForms/V2/resources/components/DonationFormsListTable.tsx
@@ -92,7 +92,7 @@ const donationFormsFilters: Array<FilterConfig> = [
 ];
 
 const v2FormBadge = item => {
-    if (!item.v3form) {
+    if (item.v2form) {
         return <div className={styles.v2Badge}>
             <svg width="4" height="4" viewBox="0 0 4 4" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <circle cx="2" cy="2" r="2" fill="#2B13BF"/>
