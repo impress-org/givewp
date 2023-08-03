deaab4ddda5e27393ecfee49ba8267781b53d1ff
Jason Adams <jason.adams@givewp.com>
2023-08-03 10:58:37 -0700

    feature: add field scopes

---
+++ src/Framework/FieldsAPI/Concerns/HasPersistence.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Give\Framework\FieldsAPI\Concerns;
+
+use Give\Framework\FieldsAPI\ValueObjects\Scope;
+
+trait HasPersistence
+{
+    /**
+     * @unreleased
+     *
+     * @var Scope|null
+     */
+    protected $scope;
+
+    /**
+     * @unreleased
+     *
+     * @var string|null
+     */
+    protected $metaKey = null;
+
+    /**
+     * @unreleased
+     *
+     * @param string|Scope $scope
+     */
+    public function scope($scope): self
+    {
+        $this->scope = $scope instanceof Scope ? $scope : new Scope($scope);
+
+        return $this;
+    }
+
+    /**
+     * @unreleased
+     */
+    public function getScope(): Scope
+    {
+        return $this->scope;
+    }
+
+    /**
+     * @unreleased
+     */
+    public function getScopeValue(): string
+    {
+        return (string)$this->scope;
+    }
+
+    /**
+     * @unreleased
+     */
+    public function metaKey(string $metaKey): self
+    {
+        $this->metaKey = $metaKey;
+
+        return $this;
+    }
+
+    /**
+     * @unreleased
+     */
+    public function getMetaKey(): string
+    {
+        return $this->metaKey;
+    }
+
+    /**
+     * @unreleased updated to use scoping under the hood, no signature change
+     * @since 2.28.0 added types
+     * @since 2.10.2
+     */
+    public function storeAsDonorMeta(bool $storeAsDonorMeta = true): self
+    {
+        $this->scope = $storeAsDonorMeta
+            ? Scope::donor()
+            : Scope::donation();
+
+        return $this;
+    }
+
+    /**
+     * @unreleased updated to use scoping under the hood, no signature change
+     * @since 2.28.0 added types
+     * @since 2.10.2
+     */
+    public function shouldStoreAsDonorMeta(): bool
+    {
+        return $this->scope->isDonor();
+    }
+}
---
+++ src/Framework/FieldsAPI/ValueObjects/Scope.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Give\Framework\FieldsAPI\ValueObjects;
+
+/**
+ * The scope of the field for use in persistence. The two built-in scopes are donation and donor, but the scope may be
+ * any custom string. Using a custom scope allows for an add-on to either not store the field, or store it in a custom
+ * location.
+ *
+ * @unreleased
+ */
+class Scope
+{
+    const DONATION = 'donation';
+    const DONOR = 'donor';
+
+    /**
+     * @var string
+     */
+    private $scope;
+
+    /**
+     * @unreleased
+     */
+    public static function donation(): self
+    {
+        return new self(self::DONATION);
+    }
+
+    /**
+     * @unreleased
+     */
+    public static function donor(): self
+    {
+        return new self(self::DONOR);
+    }
+
+    /**
+     * @unreleased
+     */
+    public function __construct(string $scope)
+    {
+        $this->scope = $scope;
+    }
+
+    /**
+     * @unreleased
+     */
+    public function isDonation(): bool
+    {
+        return $this->scope === self::DONATION;
+    }
+
+    /**
+     * @unreleased
+     */
+    public function isDonor(): bool
+    {
+        return $this->scope === self::DONOR;
+    }
+
+    /**
+     * @param self|string $scope
+     */
+    public function is($scope): bool
+    {
+        if ( $scope instanceof self ) {
+            $scope = $scope->scope;
+        }
+
+        return $this->scope === $scope;
+    }
+
+    /**
+     * @unreleased
+     */
+    public function __toString()
+    {
+        return $this->scope;
+    }
+}
