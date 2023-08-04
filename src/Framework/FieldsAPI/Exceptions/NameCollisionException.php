0dd368cb7448d111197d3b9730f5ca495d43ba4c
Jon Waldstein <jonwaldstein@jons-air.mynetworksettings.com>
2023-08-04 12:45:39 -0400

    feature: add existing and incoming nodes to exception

--- src/Framework/FieldsAPI/Exceptions/NameCollisionException.php
+++ src/Framework/FieldsAPI/Exceptions/NameCollisionException.php
@@ -3,8 +3,10 @@
 namespace Give\Framework\FieldsAPI\Exceptions;
 
 use Give\Framework\Exceptions\Primitives\Exception;
+use Give\Framework\FieldsAPI\Contracts\Node;
 
 /**
+ * @unreleased add existing and incoming nodes to exception
  * @since 2.10.2
  */
 class NameCollisionException extends Exception
@@ -13,10 +15,26 @@ class NameCollisionException extends Exception
      * @var string
      */
     protected $nodeNameCollision;
+    /**
+     * @var Node
+     */
+    protected $existingNode;
+    /**
+     * @var Node
+     */
+    protected $incomingNode;
     
-    public function __construct($name, $code = 0, Exception $previous = null)
+    public function __construct(
+        string $name,
+        Node $existingNode,
+        Node $incomingNode,
+        int $code = 0,
+        Exception $previous = null
+    )
     {
         $this->nodeNameCollision = $name;
+        $this->existingNode = $existingNode;
+        $this->incomingNode = $incomingNode;
 
         $message = "Node name collision for $name";
         parent::__construct($message, $code, $previous);
@@ -28,5 +46,21 @@ class NameCollisionException extends Exception
     public function getNodeNameCollision(): string
     {
         return $this->nodeNameCollision;
+    }
+
+    /**
+     * @unreleased
+     */
+    public function getIncomingNode(): Node
+    {
+        return $this->incomingNode;
+    }
+
+    /**
+     * @unreleased
+     */
+    public function getExistingNode(): Node
+    {
+        return $this->existingNode;
     }
 }
