2d3ed2650881c0846fd86ac934cefb1648d70b9b
Jon Waldstein <jonwaldstein@jons-air.mynetworksettings.com>
2023-08-10 17:38:09 -0400

    tests: update confirmation page test

--- tests/Unit/Framework/Receipts/TestGenerateConfirmationPageReceipt.php
+++ tests/Unit/Framework/Receipts/TestGenerateConfirmationPageReceipt.php
@@ -74,6 +74,15 @@ class TestGenerateConfirmationPageReceipt extends TestCase
             );
         }
 
+        if ($donation->comment) {
+            $additionalDetails->addDetail(
+                new ReceiptDetail(
+                    __('Comment', 'give'),
+                    $receipt->donation->comment
+                )
+            );
+        }
+
         $heading = (new DonationTemplateTags($donation, $donationForm->settings->receiptHeading))->getContent();
         $description = (new DonationTemplateTags($donation, $donationForm->settings->receiptDescription))->getContent();
 
