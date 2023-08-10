74861aca5b0c4f6a2be55aa31ef832f966fcb36c
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2023-08-10 18:40:02 +0530

    feat: disable banner by user permanenetly

--- src/PaymentGateways/PayPalCommerce/Banners/PayPalStandardToDonationsMigrationGlobalBanner.php
+++ src/PaymentGateways/PayPalCommerce/Banners/PayPalStandardToDonationsMigrationGlobalBanner.php
@@ -7,10 +7,17 @@ use Give\PaymentGateways\Gateways\PayPalStandard\PayPalStandard;
 /**
  * Class PayPalStandardToDonationsMigrationGlobalBanner
  *
+ * Note: This class uses notice api to disable the banner for the user.
+ *
  * @unreleased
  */
 class PayPalStandardToDonationsMigrationGlobalBanner
 {
+    /**
+     * @unreleased
+     * @var string
+     */
+    private $bannerId = 'PayPalStandardToDonationsMigrationGlobalBanner';
 
     /**
      * @unreleased
@@ -18,7 +25,6 @@ class PayPalStandardToDonationsMigrationGlobalBanner
      */
     public function setHook()
     {
-
         add_action('admin_enqueue_scripts', function () {
             if (give_is_gateway_active(PayPalStandard::id())) {
                 wp_add_inline_script('give-admin-scripts', $this->getModalScript());
@@ -33,8 +39,11 @@ class PayPalStandardToDonationsMigrationGlobalBanner
      */
     public function getModalScript(): string
     {
-        $bannerId = 'PayPalStandardToDonationsMigrationGlobalBanner';
-        $nonce = wp_create_nonce("give_edit_{$bannerId}_notice");
+        if ($this->isBannerDisabledForUser()) {
+            return '';
+        }
+
+        $nonce = wp_create_nonce("give_edit_{$this->bannerId}_notice");
 
         $modalTitle = esc_html__(
             'PayPal Standard Deprecation',
@@ -63,9 +72,11 @@ class PayPalStandardToDonationsMigrationGlobalBanner
             'give'
         );
 
-        $linkToPayPalDonationsSettingPage = esc_url_raw(admin_url(
-            'edit.php?post_type=give_forms&page=give-settings&tab=gateways&section=paypal'
-        ));
+        $linkToPayPalDonationsSettingPage = esc_url_raw(
+            admin_url(
+                'edit.php?post_type=give_forms&page=give-settings&tab=gateways&section=paypal'
+            )
+        );
 
         return <<<EOT
             document.addEventListener("DOMContentLoaded", () => {
@@ -102,7 +113,7 @@ class PayPalStandardToDonationsMigrationGlobalBanner
 						close: () => {
 							wp.ajax.post({
 								'give-action': 'dismiss_notices',
-								'notice_id': '$bannerId',
+								'notice_id': '$this->bannerId',
 								'dismissible_type': 'user',
 								'dismiss_interval': 'permanent',
 								'_wpnonce': '$nonce'
@@ -123,5 +134,17 @@ EOT;
         return '<svg width="16" height="15" viewBox="0 0 16 15" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path fill-rule="evenodd" clip-rule="evenodd" d="M8.54163 1.11224C8.19676 0.958893 7.80304 0.958893 7.45817 1.11224C7.19182 1.23068 7.01879 1.43929 6.89839 1.60927C6.78002 1.7764 6.65328 1.99535 6.5139 2.23615L1.00246 11.7559C0.862519 11.9976 0.73535 12.2172 0.649123 12.4036C0.561489 12.593 0.46642 12.8476 0.496719 13.1382C0.535926 13.5143 0.732954 13.856 1.03876 14.0784C1.27509 14.2502 1.54309 14.2955 1.75093 14.3146C1.95543 14.3334 2.2092 14.3333 2.48846 14.3333H13.5113C13.7906 14.3333 14.0444 14.3334 14.2489 14.3146C14.4567 14.2955 14.7247 14.2502 14.961 14.0784C15.2668 13.856 15.4639 13.5143 15.5031 13.1382C15.5334 12.8476 15.4383 12.593 15.3507 12.4036C15.2645 12.2172 15.1373 11.9976 14.9974 11.756L9.48589 2.23612C9.34652 1.99534 9.21978 1.77639 9.10141 1.60927C8.98101 1.43929 8.80798 1.23068 8.54163 1.11224ZM8.66659 6C8.66659 5.63181 8.36811 5.33333 7.99992 5.33333C7.63173 5.33333 7.33325 5.63181 7.33325 6V8.66666C7.33325 9.03485 7.63173 9.33333 7.99992 9.33333C8.36811 9.33333 8.66659 9.03485 8.66659 8.66666V6ZM7.99992 10.6667C7.63173 10.6667 7.33325 10.9651 7.33325 11.3333C7.33325 11.7015 7.63173 12 7.99992 12H8.00659C8.37478 12 8.67325 11.7015 8.67325 11.3333C8.67325 10.9651 8.37478 10.6667 8.00659 10.6667H7.99992Z" fill="#F29718"/>
 </svg>';
+    }
+
+    /**
+     * @unreleased
+     * @return bool
+     */
+    private function isBannerDisabledForUser(): bool
+    {
+        $optionKey = give()->notices->get_notice_key($this->bannerId, 'permanent', get_current_user_id());
+        $optionData = \Give_Cache::get($optionKey, true);
+
+        return ! empty($optionData) && ! is_wp_error($optionData);
     }
 }
