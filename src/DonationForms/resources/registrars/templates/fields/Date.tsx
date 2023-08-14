6092df764656092476b6956d27c68dd8ccca7444
Paulo Iankoski <me@wpdev.com.br>
2023-08-14 16:24:46 -0300

    fix: format saved date as presented (#251)

--- src/DonationForms/resources/registrars/templates/fields/Date.tsx
+++ src/DonationForms/resources/registrars/templates/fields/Date.tsx
@@ -1,18 +1,18 @@
 import DatePicker from 'react-datepicker';
+import {format, parse} from 'date-fns';
 
 import {DateProps} from '@givewp/forms/propTypes';
 import 'react-datepicker/dist/react-datepicker.css';
 import styles from '../styles.module.scss';
 import {Controller} from 'react-hook-form';
 
-export default function Date({Label, ErrorMessage, description, dateFormat, inputProps}: DateProps) {
+export default function Date({Label, ErrorMessage, description, dateFormat = 'yyyy/mm/dd', inputProps}: DateProps) {
     const {useFormContext} = window.givewp.form.hooks;
     const FieldDescription = window.givewp.form.templates.layouts.fieldDescription;
     const {name} = inputProps;
     const {control} = useFormContext();
 
     dateFormat = dateFormat.replace('mm', 'MM');
-
 
     return (
         <label className={styles.dateField}>
@@ -21,19 +21,19 @@ export default function Date({Label, ErrorMessage, description, dateFormat, inpu
             <Controller
                 control={control}
                 name={name}
-                render={({
-                    field: {onChange, onBlur, value, ref},
-                }) =>
-                        <DatePicker
-                            ref={(element) => {
-                                element && ref(element.input)
-                            }}
-                            dateFormat={dateFormat}
-                            selected={value}
-                            onChange={onChange}
-                            onBlur={onBlur}
-                        />
-                }
+                render={({field: {onChange, onBlur, value, ref}}) => (
+                    <DatePicker
+                        ref={(element) => {
+                            element && ref(element.input);
+                        }}
+                        dateFormat={dateFormat}
+                        selected={value && parse(value, dateFormat, new window.Date())}
+                        onChange={(date) => {
+                            onChange(format(date, dateFormat));
+                        }}
+                        onBlur={onBlur}
+                    />
+                )}
             />
 
             <ErrorMessage />
