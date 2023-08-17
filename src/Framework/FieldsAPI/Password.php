2a0fafc1e19b124bd13159d79900014261009c64
Kyle B. Johnson <kyle.johnson@hey.com>
2023-08-17 15:03:27 -0400

    refactor: Update Password field to prevent storage

--- src/Framework/FieldsAPI/Password.php
+++ src/Framework/FieldsAPI/Password.php
@@ -16,14 +16,14 @@ class Password extends Field
     const TYPE = 'password';
 
     /**
-     * @param $storeAsDonorMeta
+     * @param bool $storeAsDonorMeta
      *
      * @return $this
      */
-    public function storeAsDonorMeta($storeAsDonorMeta = true): self
+    public function storeAsDonorMeta(bool $storeAsDonorMeta = true): self
     {
         // Do not allow password fields to be stored as donor meta.
-        $this->storeAsDonorMeta = false;
+        $this->scope = null;
 
         return $this;
     }
