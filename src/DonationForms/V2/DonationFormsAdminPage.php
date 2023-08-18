2a4755b4c6d1d3030c881690c17c537f95ab36c1
Ante Laca <ante.laca@get10up.com>
2023-08-18 13:18:13 +0200

    feature: add new props and methods

--- src/DonationForms/V2/DonationFormsAdminPage.php
+++ src/DonationForms/V2/DonationFormsAdminPage.php
@@ -83,16 +83,18 @@ class DonationFormsAdminPage
     {
         $data = [
             'apiRoot' => $this->apiRoot,
-            'onboardingApiRoot' =>$this->onboardingApiRoot,
+            'onboardingApiRoot' => $this->onboardingApiRoot,
             'apiNonce' => $this->apiNonce,
             'preload' => $this->preloadDonationForms(),
             'authors' => $this->getAuthors(),
             'table' => give(DonationFormsListTable::class)->toArray(),
             'adminUrl' => $this->adminUrl,
             'pluginUrl' => GIVE_PLUGIN_URL,
-            'showMigrationOnboarding' => defined('GIVE_NEXT_GEN_VERSION') && ! give_get_option('show_migration_onboarding'),
-            'migrationOnboardingCompleted' => defined('GIVE_NEXT_GEN_VERSION') && give_get_option('migration_onboarding_completed'),
-            'unsupportedAddons' => $this->getUnsupportedAddons(),
+            'showMigrationOnboarding' => ! give_get_option('show_migration_onboarding'),
+            'showBanner' => ! give_get_option('show_onboarding_banner'),
+            'migrationOnboardingCompleted' => give_get_option('migration_onboarding_completed'),
+            'supportedAddons' => $this->getSupportedAddons(),
+            'supportedGateways' => $this->getSupportedGateways(),
         ];
 
         EnqueueScript::make('give-admin-donation-forms', 'assets/dist/js/give-admin-donation-forms.js')
@@ -213,54 +215,67 @@ class DonationFormsAdminPage
      * @since 2.20.0
      * @return string
      */
-    public static function getUrl()
+    public static function getUrl(): string
     {
         return add_query_arg(['page' => 'give-forms'], admin_url('edit.php?post_type=give_forms'));
     }
 
     /**
-     * Get an array of unsupported addons for addons compatibility modal
+     * Get an array of supported addons
      *
      * @unreleased
      * @return array
      */
-    public function getUnsupportedAddons(): array
+    public function getSupportedAddons(): array
     {
-        $unsupportedAddons = [
-            [
-                'Tributes' => class_exists('Give_Tributes'),
-                'Funds' => defined('GIVE_FUNDS_ADDON_NAME'),
-                'Recurring Donation' => class_exists('Give_Recurring'),
-                'Currency Switcher' => class_exists('Give_Currency_Switcher'),
-                'Form Field Manager' => class_exists('Give_Form_Fields_Manager'),
-                'Fee Recovery' => class_exists('Give_Fee_Recovery'),
-                'Peer-to-Peer' => defined('GIVE_P2P_NAME'),
-                'PDF Receipts' => class_exists('Give_PDF_Receipts'),
-                'Manual Donations' => class_exists('Give_Manual_Donations'),
-                'Gift Aid' => class_exists('Give_Gift_Aid'),
-                'Annual Receipts' => class_exists('Give_Annual_Receipts'),
-                'MailChimp' => class_exists('Give_MailChimp'),
-                'Text-to-Give' => defined('GIVE_TEXT_TO_GIVE_ADDON_NAME'),
-                'Donation Block for Stripe' => defined('DONATION_BLOCK_FILE'),
-                'Double the Donation' => defined('GIVE_DTD_NAME'),
-                'Donation Upsells for WooCommerce' => class_exists('Give_WooCommerce'),
-                'Simple Social Shout' => class_exists('SIMPLE_SOCIAL_SHARE_4_GIVEWP'),
-                'Receipt Attachments' => defined('GIVERA_VERSION'),
-                'Per Form Gateways' => class_exists('Give_Per_Form_Gateways'),
-                'Per Form Confirmations' => class_exists('Per_Form_Confirmations_4_GIVEWP'),
-                'Form Countdown' => class_exists('Give_Form_Countdown'),
-                'ActiveCampaign' => class_exists('Give_ActiveCampaign'),
-                'Constant Contact' => class_exists('Give_Constant_Contact'),
-            ],
+        $supportedAddons = [
+            'Tributes' => class_exists('Give_Tributes'),
+            'Funds' => defined('GIVE_FUNDS_ADDON_NAME'),
+            'Recurring Donation' => class_exists('Give_Recurring'),
+            'Currency Switcher' => class_exists('Give_Currency_Switcher'),
+            'Form Field Manager' => class_exists('Give_Form_Fields_Manager'),
+            'Fee Recovery' => class_exists('Give_Fee_Recovery'),
+            'Peer-to-Peer' => defined('GIVE_P2P_NAME'),
+            'PDF Receipts' => class_exists('Give_PDF_Receipts'),
+            'Manual Donations' => class_exists('Give_Manual_Donations'),
+            'Gift Aid' => class_exists('Give_Gift_Aid'),
+            'Annual Receipts' => class_exists('Give_Annual_Receipts'),
+            'MailChimp' => class_exists('Give_MailChimp'),
+            'Text-to-Give' => defined('GIVE_TEXT_TO_GIVE_ADDON_NAME'),
+            'Donation Block for Stripe' => defined('DONATION_BLOCK_FILE'),
+            'Double the Donation' => defined('GIVE_DTD_NAME'),
+            'Donation Upsells for WooCommerce' => class_exists('Give_WooCommerce'),
+            'Simple Social Shout' => class_exists('SIMPLE_SOCIAL_SHARE_4_GIVEWP'),
+            'Receipt Attachments' => defined('GIVERA_VERSION'),
+            'Per Form Gateways' => class_exists('Give_Per_Form_Gateways'),
+            'Per Form Confirmations' => class_exists('Per_Form_Confirmations_4_GIVEWP'),
+            'Form Countdown' => class_exists('Give_Form_Countdown'),
+            'ActiveCampaign' => class_exists('Give_ActiveCampaign'),
+            'Constant Contact' => class_exists('Give_Constant_Contact'),
         ];
 
         $output = [];
 
-        foreach ($unsupportedAddons as $addon) {
-            foreach ($addon as $name => $isInstalled) {
-                if ($isInstalled) {
-                    $output[] = $name;
-                }
+        foreach ($supportedAddons as $name => $isInstalled) {
+            if ($isInstalled) {
+                $output[] = $name;
+            }
+        }
+
+        return $output;
+    }
+
+    public function getSupportedGateways(): array
+    {
+        $supportedGateways = [
+            // Todo: add list of supported gateways
+        ];
+
+        $output = [];
+
+        foreach ($supportedGateways as $name => $isInstalled) {
+            if ($isInstalled) {
+                $output[] = $name;
             }
         }
 
