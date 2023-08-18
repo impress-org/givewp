1059434094f068386a25d9323a0a669380227e4a
Ante Laca <ante.laca@get10up.com>
2023-08-18 15:29:53 +0200

    refactor: update feature notice dialog

--- src/DonationForms/V2/resources/components/Onboarding/FeatureNoticeDialog.tsx
+++ src/DonationForms/V2/resources/components/Onboarding/FeatureNoticeDialog.tsx
@@ -1,11 +1,13 @@
 import {__} from '@wordpress/i18n';
 import ModalDialog from '@givewp/components/AdminUI/ModalDialog';
-import {MinusIcon, CheckVerified, StarsIcon} from '@givewp/components/AdminUI/Icons';
+import {CheckVerified, StarsIcon} from '@givewp/components/AdminUI/Icons';
 import Button from '@givewp/components/AdminUI/Button';
 import styles from './style.module.scss';
 
 
 export default function FeatureNoticeDialog({handleClose}) {
+    const {supportedAddons, supportedGateways} = window.GiveDonationForms;
+
     return (
         <ModalDialog
             isOpen={true}
@@ -19,23 +21,47 @@ export default function FeatureNoticeDialog({handleClose}) {
 
                 {__('GiveWP 3.0 introduces an enhanced forms experience powered by the new Visual Donations Form Builder. The team is still working on add-on and gateway compatibility. If you need to use an add-on or gateway that isn\'t listed, use the "Add form" option for now', 'give')}
 
-                <div className={styles.title}>
-                    {__('Supported add-ons', 'give')}
-                </div>
+                {supportedAddons.length > 0 && (
+                    <>
+                        <div className={styles.title}>
+                            {__('Supported add-ons', 'give')}
+                        </div>
 
-                <div className={styles.addonsContainer}>
-                    {window.GiveDonationForms.supportedAddons.map(addon => <div className={styles.addon} key={addon}><CheckVerified />{addon}</div>)}
-                </div>
+                        <div className={styles.itemsContainer}>
+                            {supportedAddons.map(addon => (
+                                <div className={styles.item} key={addon}><CheckVerified />{addon}</div>
+                            ))}
+                        </div>
+                    </>
+                )}
 
-                <br />
+                {supportedGateways.length > 0 && (
+                    <>
+                        <div className={styles.title}>
+                            {__('Supported gateways', 'give')}
+                        </div>
+
+                        <div className={styles.itemsContainer}>
+                            {supportedGateways.map(gateway => (
+                                <div className={styles.item} key={gateway}><CheckVerified />{gateway}</div>
+                            ))}
+                        </div>
+                    </>
+                )}
 
                 <Button
                     size="large"
-                    onClick={handleClose}
+                    onClick={() => window.location.href = 'edit.php?post_type=give_forms&page=givewp-form-builder'}
                     style={{width: '100%'}}
                 >
-                    {__('Close', 'give')}
+                    {__('Proceed with the new form builder', 'give')}
                 </Button>
+
+                <br />
+
+                <a href="#">
+                    {__('Read more on Add-ons and Gateways compatibility', 'give')}
+                </a>
             </>
         </ModalDialog>
     )
