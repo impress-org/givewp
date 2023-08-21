2eebc9e9b3a3cb4692ce9449837673ee1d680783
Jason Adams <jason.adams@givewp.com>
2023-08-21 16:25:32 -0700

    fix: set the default value of field if missing

--- src/DonationForms/resources/app/fields/FieldNode.tsx
+++ src/DonationForms/resources/app/fields/FieldNode.tsx
@@ -3,17 +3,34 @@ import {useTemplateWrapper} from '../templates';
 import registerFieldAndBuildProps from '../utilities/registerFieldAndBuildProps';
 import type {FieldProps} from '@givewp/forms/propTypes';
 import memoNode from '@givewp/forms/app/utilities/memoNode';
+import {useEffect} from 'react';
 
 const formTemplates = window.givewp.form.templates;
 
 function FieldNode({node}: {node: Field}) {
-    const {register} = window.givewp.form.hooks.useFormContext();
+    const {register, getValues, setValue} = window.givewp.form.hooks.useFormContext();
     const {errors} = window.givewp.form.hooks.useFormState();
     const Field =
         node.type !== 'hidden'
             ? useTemplateWrapper<FieldProps>(formTemplates.fields[node.type], 'div', node.name)
             : formTemplates.fields[node.type];
     const fieldProps = registerFieldAndBuildProps(node, register, errors);
+
+    /**
+     * Set the default value for the field if it is not already set. This is necessary because the default value is not
+     * applied to the initial form render if it requires conditions to be visible.
+     */
+    useEffect(() => {
+        if (node.defaultValue === undefined) {
+            return;
+        }
+
+        const value = getValues(node.name);
+
+        if (value === undefined || value === null) {
+            setValue(node.name, node.defaultValue);
+        }
+    }, []);
 
     return <Field key={node.name} {...fieldProps} />;
 }
