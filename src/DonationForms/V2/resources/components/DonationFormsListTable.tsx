cba6514d60d75702ef897e7414d0e8220482504e
Ante Laca <ante.laca@get10up.com>
2023-08-22 13:16:30 +0200

    feature: add tooltip

--- src/DonationForms/V2/resources/components/DonationFormsListTable.tsx
+++ src/DonationForms/V2/resources/components/DonationFormsListTable.tsx
@@ -81,7 +81,12 @@ const columnFilters: Array<ColumnFilterConfig> = [
             if (!item?.migrated) {
                 return (
                     <div className={styles.migratedForm}>
-                        <CubeIcon />
+                        <div className={styles.tooltipContainer}>
+                            <CubeIcon />
+                            <div className={styles.tooltip}>
+                                {__('This icon indicates that this is an upgraded form, which uses the new form builder', 'give')}
+                            </div>
+                        </div>
                         <Interweave attributes={{className: 'interweave'}} content={item?.title} />
                     </div>
                 );
