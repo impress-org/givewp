ff8896e1cb42047aac1496f1a056e4b8f8e3b1b1
Paulo Iankoski <me@wpdev.com.br>
2023-08-25 20:57:12 -0300

    refactor: update stripe options on form update

---
+++ src/FormBuilder/Actions/UpdateStripeSettingsMeta.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Give\FormBuilder\Actions;
+
+use Give\DonationForms\Models\DonationForm;
+
+/**
+ * Update Stripe settings on backwards compatible form meta.
+ *
+ * @since 3.0.0
+ */
+class UpdateStripeSettingsMeta
+{
+    /**
+     * @since 3.0.0
+     * @param  DonationForm  $form
+     */
+    public function __invoke(DonationForm $form)
+    {
+        $paymentGatewaysBlock = $form->blocks->findByName('givewp/payment-gateways');
+        if (!$paymentGatewaysBlock) {
+            give()->form_meta->delete_meta($form->id, 'give_stripe_per_form_accounts');
+            give()->form_meta->delete_meta($form->id, '_give_stripe_default_account');
+
+            return;
+        }
+
+        [
+            'stripeUseGlobalDefault' => $stripeUseGlobalDefault,
+            'stripeAccountId' => $stripeAccountId
+        ] = $paymentGatewaysBlock->getAttributes() + ['stripeUseGlobalDefault' => true, 'stripeAccountId' => ''];
+
+        if (is_bool($stripeUseGlobalDefault)) {
+            give()->form_meta->update_meta($form->id, "give_stripe_per_form_accounts", $stripeUseGlobalDefault ? 'disabled' : 'enabled');
+        }
+
+        if (is_string($stripeAccountId)) {
+            give()->form_meta->update_meta($form->id, "_give_stripe_default_account", $stripeAccountId);
+        }
+    }
+}
