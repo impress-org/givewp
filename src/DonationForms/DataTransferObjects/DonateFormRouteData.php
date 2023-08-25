311fcddd39ada38d6253de0c81f27d179ab535c2
Jon Waldstein <jon@givewp.com>
2023-08-25 12:15:45 -0400

    Fix: `$gatewayData` request supports all native content types (#6904)
    * fix: update gateway data request to support all native content types
    
    * fix: filter boolean value for isEmbed since bools are sent as strings in multipart/form-data
    
    ---------
    
    Co-authored-by: Jon Waldstein <jonwaldstein@jons-air.mynetworksettings.com>

--- src/DonationForms/DataTransferObjects/DonateFormRouteData.php
+++ src/DonationForms/DataTransferObjects/DonateFormRouteData.php
@@ -5,6 +5,7 @@ namespace Give\DonationForms\DataTransferObjects;
 use Give\DonationForms\Exceptions\DonationFormFieldErrorsException;
 use Give\DonationForms\Models\DonationForm;
 use Give\Framework\FieldsAPI\Actions\CreateValidatorFromForm;
+use Give\Framework\FieldsAPI\Exceptions\NameCollisionException;
 use Give\Framework\Support\Contracts\Arrayable;
 use WP_Error;
 
@@ -49,7 +50,7 @@ class DonateFormRouteData implements Arrayable
         $self->formId = (int)$requestData['formId'];
         $self->gatewayId = $requestData['gatewayId'];
         $self->originUrl = $requestData['originUrl'];
-        $self->isEmbed = $requestData['isEmbed'];
+        $self->isEmbed = filter_var($requestData['isEmbed'], FILTER_VALIDATE_BOOLEAN);
         $self->embedId = $self->isEmbed ? $requestData['embedId'] : null;
         $self->requestData = $requestData;
 
@@ -63,7 +64,7 @@ class DonateFormRouteData implements Arrayable
      *
      * @since 3.0.0
      *
-     * @throws DonationFormFieldErrorsException
+     * @throws DonationFormFieldErrorsException|NameCollisionException
      */
     public function validated(): DonateControllerData
     {
