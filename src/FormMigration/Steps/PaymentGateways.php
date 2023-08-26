7cf51a4450d3b1356ff2ff731b77d75218030d3a
Paulo Iankoski <me@wpdev.com.br>
2023-08-25 22:25:41 -0300

    refactor: update migration step with new attributes

--- src/FormMigration/Steps/PaymentGateways.php
+++ src/FormMigration/Steps/PaymentGateways.php
@@ -12,11 +12,7 @@ class PaymentGateways extends FormMigrationStep
     public function process()
     {
         $paymentGatewaysBlock = $this->fieldBlocks->findByName('givewp/payment-gateways');
-        $paymentGatewaysBlock->setAttribute('gatewaysSettings', [
-            'stripe_payment_element' => [
-                'useGlobalDefault' => $this->formV2->getStripeUseGlobalDefault(),
-                'accountId' => $this->formV2->getStripeAccountId(),
-            ]
-        ]);
+        $paymentGatewaysBlock->setAttribute('stripeUseGlobalDefault', $this->formV2->getStripeUseGlobalDefault());
+        $paymentGatewaysBlock->setAttribute('accountId', $this->formV2->getStripeAccountId());
     }
 }
