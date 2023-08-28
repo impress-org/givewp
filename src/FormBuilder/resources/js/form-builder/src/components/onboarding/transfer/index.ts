d9b0ad19c89c3f933fc3e1c4df4dac07c1faf620
alaca <ante.laca@gmail.com>
2023-08-28 16:09:56 +0200

    feature: upgrade success dialog

---
+++ src/FormBuilder/resources/js/form-builder/src/components/onboarding/transfer/index.ts
@@ -0,0 +1,5 @@
+import TransferNotice from './TransferNotice'
+import TransferDialog from './TransferDialog'
+import UpgradeDialog from './UpgradeDialog'
+
+export {TransferNotice, UpgradeDialog, TransferDialog}
