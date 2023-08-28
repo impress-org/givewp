78b7794f6dd5fd64f78e8988c5a6262834abd221
Glauber Silva <glauber.silva@givewp.com>
2023-08-28 12:15:14 -0300

    Feature: adds a new SubscriptionSynced command class (#6697)
    * feature: add refund checkbox to the framework
    
    * feature: add supportsRefund method
    
    * doc: improve comment
    
    * doc: fix typo
    
    * refactor: replace gateway id by gateway name
    
    * doc: adjust unreleased tag
    
    * feature: add supportsRefund method
    
    * refactor: test gateway should return boolean in refundDonation
    
    * feature: add handleRefundDonation method
    
    * feature: add RefundComplete command
    
    * feature: Handle PaymentRefunded command
    
    * feature: Handle PaymentRefunded command
    
    * feature: add isFunctionReturningCommand method
    
    * fix: wrong return from isFunctionReturningCommand method
    
    * doc: change comments
    
    * fix: import donation model
    
    * fix: add return
    
    * doc: edit comment
    
    * refactor: remove supportsRefund method
    
    * feature: add SubscriptionSynced command
    
    * feature: add SubscriptionSyncedHandler class
    
    * refactor: add properties
    
    * doc: add comments
    
    * refactor: change return
    
    * feature: add handleSynchronizeSubscription method
    
    * refactor: change index name
    
    * refactor: simplify refund checkbox logic.
    
    * refactor: make the checkbox selector more specific
    
    * refactor: check if the refundDonation return is a command before using it as were
    
    * refactor: move refund method to controller
    
    * fix: rollback class into develop version
    
    * fix: add missing return deleted by mistake
    
    * refactor: remove not used classes
    
    * doc: remove commented line
    
    * feature: add testShouldCallGatewayRefundDonation method
    
    * fix: add missing return type
    
    * feature: add synchronizeSubscription method
    
    * feature: add return for SubscriptionSynced command
    
    * doc: add exception
    
    * refactor: change array keys
    
    * refactor: change SubscriptionSyncedHandler data return
    
    * refactor: return array instead of donation objects
    
    * refactor: add date and time format
    
    * refactor: add args for missing and present donations
    
    * refactor: change transaction data format
    
    * refactor: add unreleased tag
    
    ---------
    
    Co-authored-by: Jon Waldstein <jonwaldstein@macbook-air.mynetworksettings.com>

---
+++ src/Framework/PaymentGateways/Commands/SubscriptionSynced.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Give\Framework\PaymentGateways\Commands;
+
+use Give\Donations\Models\Donation;
+use Give\Subscriptions\Models\Subscription;
+
+/**
+ * @unreleased
+ */
+class SubscriptionSynced implements GatewayCommand
+{
+    /**
+     * @unreleased
+     *
+     * @var Subscription
+     */
+    public $subscription;
+
+    /**
+     * @unreleased
+     *
+     * @var array
+     */
+    public $missingDonations;
+
+    /**
+     * @unreleased
+     *
+     * @var array
+     */
+    public $presentDonations;
+
+    /**
+     * @unreleased
+     *
+     * @var string
+     */
+    public $notice;
+
+    /**
+     * @unreleased
+     *
+     * @param Subscription $subscription     Do not save the subscription, just return it so the API can see what's dirty
+     * @param Donation[]   $missingDonations The missing donations added to the subscription
+     * @param Donation[]   $presentDonations Optional. The already present donations of the subscription
+     * @param string       $notice           Optional. Use to notify users about some limitation or specificity of the gateway
+     */
+    public function __construct(
+        Subscription $subscription,
+        array $missingDonations,
+        array $presentDonations = [],
+        string $notice = ''
+    ) {
+        $this->subscription = $subscription;
+        $this->missingDonations = $missingDonations;
+        $this->presentDonations = $presentDonations;
+        $this->notice = $notice;
+    }
+}
