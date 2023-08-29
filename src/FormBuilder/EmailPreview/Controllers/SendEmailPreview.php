69bc2cc4c76caa1b06019f1b6f08a7d123a86dd4
Joshua Dinh <josh@joshuadinh.tech>
2023-08-29 15:45:47 -0700

    fix: set headers with Content-Type: text/html; in SendEmailPreview

--- src/FormBuilder/EmailPreview/Controllers/SendEmailPreview.php
+++ src/FormBuilder/EmailPreview/Controllers/SendEmailPreview.php
@@ -16,7 +16,7 @@ class SendEmailPreview
     /**
      * @since 3.0.0
      *
-     * @param  WP_REST_Request  $request
+     * @param WP_REST_Request $request
      *
      * @return WP_REST_Response
      */
@@ -25,10 +25,13 @@ class SendEmailPreview
         $email = $request->get_param('email_address');
         $subject = $request->get_param('email_subject');
         $message = give(BuildEmailPreview::class)->__invoke($request);
+        $headers = 'From:' . $email . "\r\n" .
+                   'Content-Type: text/html; charset=UTF-8' . "\r\n";
 
-        $sent = wp_mail( $email, $subject, $message );
+        $sent = wp_mail($email, $subject, $message, $headers);
 
         ob_clean();
+
         return new WP_REST_Response($sent, 200);
     }
 }
