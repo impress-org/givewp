9f35c87902664112bbd70a8c3bbfbb9bea2219ec
Joshua Dinh <josh@joshuadinh.tech>
2023-08-30 14:28:52 -0700

    refactor: update types name

--- src/Promotions/WelcomeBanner/resources/js/app/components/Row/index.tsx
+++ src/Promotions/WelcomeBanner/resources/js/app/components/Row/index.tsx
@@ -2,13 +2,13 @@ import {ReactNode} from 'react';
 
 import './styles.scss';
 
-type ColumnRowProps = {
+type RowProps = {
     children: ReactNode;
 };
 
 /**
  * @unreleased
  */
-export default function Row({children}: ColumnRowProps) {
+export default function Row({children}: RowProps) {
     return <div className={'givewp-welcome-banner-row'}>{children}</div>;
 }
