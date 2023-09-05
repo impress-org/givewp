cea5619adcd48321d725ee62e0fd96e93257e9a8
Jon Waldstein <jonwaldstein@jons-air.mynetworksettings.com>
2023-09-05 17:21:00 -0400

    feature: add form error styling

--- src/DonationForms/resources/registrars/templates/layouts/Form.tsx
+++ src/DonationForms/resources/registrars/templates/layouts/Form.tsx
@@ -7,13 +7,18 @@ export default function Form({children, formProps, formError, isSubmitting}: For
     return (
         <form {...formProps}>
             {children}
-            {formError && (
-                <div style={{textAlign: 'center'}}>
-                    <p>{__('The following error occurred when submitting the form:', 'give')}</p>
-                    <p>{formError}</p>
-                </div>
-            )}
+
             <section className="givewp-layouts givewp-layouts-section">
+                {formError && (
+                    <div className="givewp-donation-form__errors">
+                        <p className="givewp-donation-form__errors__description">
+                            {__('The following error occurred when submitting the form:', 'give')}
+                        </p>
+                        <ul className="givewp-donation-form__errors__messages">
+                            <li className="givewp-donation-form__errors__message">{formError}</li>
+                        </ul>
+                    </div>
+                )}
                 <button type="submit" disabled={isSubmitting} aria-busy={isSubmitting}>
                     {isSubmitting ? __('Submitting…', 'give') : donateButtonCaption}
                 </button>
