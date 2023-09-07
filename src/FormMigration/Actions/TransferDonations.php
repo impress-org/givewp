c6db203d44eb4d9129a7499b70e64a4a12fd22b6
Kyle B. Johnson <kyle.johnson@hey.com>
2023-09-07 15:22:33 -0400

    feature: Transfer calculated sales, earnings

--- src/FormMigration/Actions/TransferDonations.php
+++ src/FormMigration/Actions/TransferDonations.php
@@ -26,17 +26,34 @@ class TransferDonations
 
     public function __invoke($destinationId)
     {
-        DB::table('posts')
-            ->where('ID', $destinationId)
-            ->update(['post_status' => DonationFormStatus::UPGRADED]);
+        DB::transaction(function() use ($destinationId) {
+            DB::table('posts')
+                ->where('ID', $destinationId)
+                ->update(['post_status' => DonationFormStatus::UPGRADED]);
 
-        DB::table('give_donationmeta')
-            ->where('meta_key', '_give_payment_form_id')
-            ->where('meta_value', $this->sourceId)
-            ->update(['meta_value' => $destinationId]);
+            DB::table('give_donationmeta')
+                ->where('meta_key', '_give_payment_form_id')
+                ->where('meta_value', $this->sourceId)
+                ->update(['meta_value' => $destinationId]);
 
-        DB::table('give_revenue')
-            ->where('form_id', $this->sourceId)
-            ->update(['form_id' => $destinationId]);
+            DB::table('give_revenue')
+                ->where('form_id', $this->sourceId)
+                ->update(['form_id' => $destinationId]);
+
+            give_update_meta(
+                $destinationId,
+                '_give_form_sales',
+                give_get_meta($this->sourceId, '_give_form_sales', true)
+            );
+            give_update_meta($this->sourceId, '_give_form_sales', 0);
+
+            give_update_meta(
+                $destinationId,
+                '_give_form_earnings',
+                give_get_meta($this->sourceId, '_give_form_earnings', true)
+            );
+            give_update_meta($this->sourceId, '_give_form_earnings', 0);
+
+        });
     }
 }
