c713c9d4c952839afdd1b446d0070cd7af96fd33
Paulo Iankoski <me@wpdev.com.br>
2023-09-13 18:39:41 -0300

    Fix: Change the migration to update gateways settings only when data doesn't exist. (#6937)
    Co-authored-by: Jon Waldstein <Jpwaldstein@gmail.com>

--- src/PaymentGateways/Migrations/CopyV2GatewaysSettingsToV3.php
+++ src/PaymentGateways/Migrations/CopyV2GatewaysSettingsToV3.php
@@ -31,19 +31,27 @@ class CopyV2GatewaysSettingsToV3 extends Migration
      */
     public function run()
     {
-        $v2Gateways = give_get_option('gateways', []);
-        $v3Gateways = array_intersect_key($v2Gateways, give()->gateways->getPaymentGateways(3));
-        give_update_option('gateways_v3', $v3Gateways);
+        $v3Gateways = give_get_option('gateways_v3', null);
+        if (is_null($v3Gateways)) {
+            $v2Gateways = give_get_option('gateways', []);
+            $v3Gateways = array_intersect_key($v2Gateways, give()->gateways->getPaymentGateways(3));
+            give_update_option('gateways_v3', $v3Gateways);
+        }
 
-        $v2GatewaysLabels = give_get_option('gateways_label', []);
-        $v3GatewaysLabels = array_intersect_key($v2GatewaysLabels, $v3Gateways);
-        give_update_option('gateways_label_v3', $v3GatewaysLabels);
+        $v3GatewaysLabels = give_get_option('gateways_label_v3', null);
+        if (is_null($v3GatewaysLabels)) {
+            $v2GatewaysLabels = give_get_option('gateways_label', []);
+            $v3GatewaysLabels = array_intersect_key($v2GatewaysLabels, $v3Gateways);
+            give_update_option('gateways_label_v3', $v3GatewaysLabels);
+        }
 
-        $v2DefaultGateway = give_get_option('default_gateway', '');
-        $v3DefaultGateway = array_key_exists($v2DefaultGateway, $v3Gateways) ? $v2DefaultGateway : current(
-            array_keys($v3Gateways)
-        );
-        give_update_option('default_gateway_v3', $v3DefaultGateway);
+        if (is_null(give_get_option('default_gateway_v3', null))) {
+            $v2DefaultGateway = give_get_option('default_gateway', '');
+            $v3DefaultGateway = array_key_exists($v2DefaultGateway, $v3Gateways) ? $v2DefaultGateway : current(
+                array_keys($v3Gateways)
+            );
+            give_update_option('default_gateway_v3', $v3DefaultGateway);
+        }
     }
 
     /**
