08d1b942ea9e6801b3dd473c272f28daf8cbc4d9
Paulo Iankoski <me@wpdev.com.br>
2023-09-13 14:45:47 -0300

    refactor: rewrite conditionals using is_null

--- src/PaymentGateways/Migrations/CopyV2GatewaysSettingsToV3.php
+++ src/PaymentGateways/Migrations/CopyV2GatewaysSettingsToV3.php
@@ -32,20 +32,20 @@ class CopyV2GatewaysSettingsToV3 extends Migration
     public function run()
     {
         $v3Gateways = give_get_option('gateways_v3', null);
-        if ($v3Gateways === null) {
+        if (is_null($v3Gateways)) {
             $v2Gateways = give_get_option('gateways', []);
             $v3Gateways = array_intersect_key($v2Gateways, give()->gateways->getPaymentGateways(3));
             give_update_option('gateways_v3', $v3Gateways);
         }
 
         $v3GatewaysLabels = give_get_option('gateways_label_v3', null);
-        if ($v3GatewaysLabels === null) {
+        if (is_null($v3GatewaysLabels)) {
             $v2GatewaysLabels = give_get_option('gateways_label', []);
             $v3GatewaysLabels = array_intersect_key($v2GatewaysLabels, $v3Gateways);
             give_update_option('gateways_label_v3', $v3GatewaysLabels);
         }
 
-        if (give_get_option('default_gateway_v3', null) === null) {
+        if (is_null(give_get_option('default_gateway_v3', null))) {
             $v2DefaultGateway = give_get_option('default_gateway', '');
             $v3DefaultGateway = array_key_exists($v2DefaultGateway, $v3Gateways) ? $v2DefaultGateway : current(
                 array_keys($v3Gateways)
