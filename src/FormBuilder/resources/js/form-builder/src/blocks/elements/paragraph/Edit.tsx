cfc2c0b07b4ba2a51fcc666610956ce9f2f98125
Kyle B. Johnson <kjohnson@users.noreply.github.com>
2023-09-13 10:28:27 -0400

    Fix: Restore focusability of paragraph block/element (#6931)

--- src/FormBuilder/resources/js/form-builder/src/blocks/elements/paragraph/Edit.tsx
+++ src/FormBuilder/resources/js/form-builder/src/blocks/elements/paragraph/Edit.tsx
@@ -1,19 +1,31 @@
 import {BlockEditProps} from '@wordpress/blocks';
-import {RichText} from '@wordpress/block-editor';
+import {RichText, InspectorControls} from '@wordpress/block-editor';
 import {__} from '@wordpress/i18n';
+import {PanelBody, PanelRow, TextareaControl, TextControl} from "@wordpress/components";
 
 export default function Edit({attributes, setAttributes}: BlockEditProps<any>) {
     const {content} = attributes;
 
     return (
-        <div>
+        <>
             <RichText
                 tagName="p"
                 value={content}
                 allowedFormats={['core/bold', 'core/italic', 'core/link']}
-                onChange={(value) => setAttributes({content: value})}
+                onChange={(content) => setAttributes({content})}
                 placeholder={__('Enter some text', 'custom-block-editor')}
             />
-        </div>
+            <InspectorControls>
+                <PanelBody title={__('Attributes', 'give')} initialOpen={true}>
+                    <PanelRow>
+                        <TextareaControl
+                            label={__('Content', 'give')}
+                            value={content}
+                            onChange={(content) => setAttributes({content})}
+                        />
+                    </PanelRow>
+                </PanelBody>
+            </InspectorControls>
+        </>
     );
 }
