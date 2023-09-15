c6de24ab3ae5276d1504c9a82bb90fe06ae00c6d
alaca <ante.laca@gmail.com>
2023-09-16 00:37:39 +0200

    refactor: use snackbar component

--- src/FormBuilder/resources/js/form-builder/src/components/onboarding/transfer/ReturnButton.tsx
+++ src/FormBuilder/resources/js/form-builder/src/components/onboarding/transfer/ReturnButton.tsx
@@ -1,45 +1,14 @@
-import {useState, useEffect, CSSProperties} from 'react';
+import {useEffect, useState} from 'react';
 import {__} from '@wordpress/i18n';
-import {ArrowUpLeft, ExitIcon} from '@givewp/components/AdminUI/Icons'
-
-const containerStyles = {
-    zIndex: 99999999,
-    position: 'fixed',
-    left: '10%',
-    bottom: 30,
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'var(--givewp-grey-900)',
-    color: 'var(--givewp-shades-white)',
-    borderRadius: 2,
-    boxShadow: '0 2px 6px 0 rgba(0, 0, 0, 0.3)',
-    fontSize: '0.875rem',
-    cursor: 'pointer'
-} as CSSProperties;
-
-const redirectContainerStyles = {
-    display: 'flex',
-    alignItems: 'center',
-    gap: 5,
-    padding: '10px 15px 8px 15px',
-    borderRight: '1px solid var(--givewp-grey-700)',
-
-} as CSSProperties;
-
-const closeIconStyles = {
-    fill: 'var(--givewp-shades-white)',
-    width: '18px',
-    height: '18px',
-} as CSSProperties;
+import {ArrowUpLeft} from '@givewp/components/AdminUI/Icons'
+import {Snackbar} from "@wordpress/components";
 
 export default function ReturnButton() {
     const [hidden, setHidden] = useState(false);
 
     useEffect(() => {
         setHidden(!sessionStorage.getItem('givewp-show-return-btn'));
-        sessionStorage.removeItem('givewp-show-return-btn')
+        sessionStorage.removeItem('givewp-show-return-btn');
     }, []);
 
     if (hidden) {
@@ -47,21 +16,14 @@ export default function ReturnButton() {
     }
 
     return (
-        <div style={containerStyles}>
-            <div
-                style={redirectContainerStyles}
-                onClick={() => history.back()}
+        <div className="components-snackbar-list components-editor-notices__snackbar givewp-return-btn">
+            <Snackbar
+                icon={<ArrowUpLeft />}
+                explicitDismiss={true}
+                onDismiss={() => setHidden(true)}
             >
-                <span><ArrowUpLeft /></span>
-                <span style={{marginTop: '-5px'}}>{__('Return to editing form', 'give')}</span>
-            </div>
-            <div
-                onClick={() => setHidden(true)}
-                style={{padding: '10px 15px 8px 15px'}}
-            >
-                <ExitIcon style={closeIconStyles} />
-            </div>
+                <span onClick={() => history.back()}>{__('Return to editing form', 'give')}</span>
+            </Snackbar>
         </div>
     )
-
 }
