98010e461f3dbac23665e51f8776f170c31f3fe0
Joshua Dinh <75056371+JoshuaHungDinh@users.noreply.github.com>
2023-09-20 16:04:30 -0700

    Fix: update anonymous donation block placeholder UI/UX. (#6956)

--- src/FormBuilder/resources/js/form-builder/src/blocks/fields/anonymous/Edit.tsx
+++ src/FormBuilder/resources/js/form-builder/src/blocks/fields/anonymous/Edit.tsx
@@ -1,14 +1,14 @@
 import {CheckboxControl} from '@wordpress/components';
 import {BlockEditProps} from '@wordpress/blocks';
 
+import './styles.scss';
+
 export default function Edit({attributes}: BlockEditProps<any>) {
     const {label, description} = attributes;
 
     return (
-        <>
-            <div>
-                <CheckboxControl label={label} readOnly onChange={null} help={description} />
-            </div>
-        </>
+        <div className={'givewp-fields-anonymous-donations'}>
+            <CheckboxControl checked={false} label={label} readOnly onChange={null} help={description} />
+        </div>
     );
 }
