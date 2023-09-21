2ee813c6aafd517fe454911d6e51f7b3d229f50d
alaca <ante.laca@gmail.com>
2023-09-21 22:58:18 +0200

    feature: multistep placeholder component

---
+++ src/FormBuilder/resources/js/form-builder/src/components/canvas/DesignPreviewLoading/placeholders/MultiStep.tsx
@@ -0,0 +1,28 @@
+export default function MultiStep() {
+    return (
+        <div className="givewp__component-DesignPreview-container-multistep">
+            <div className="givewp__component-DesignPreview-container-multistep-header"></div>
+            <div className="givewp__component-DesignPreview-container-multistep-image"></div>
+            <div className="givewp__component-DesignPreview-container-multistep-donation-levels">
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+            </div>
+            <div className="givewp__component-DesignPreview-container-multistep-progressbar">
+                <div></div>
+            </div>
+            <div className="givewp__component-DesignPreview-container-multistep-button">
+                <div></div>
+            </div>
+            <div className="givewp__component-DesignPreview-container-multistep-footer">
+                <div></div>
+            </div>
+        </div>
+    )
+}
