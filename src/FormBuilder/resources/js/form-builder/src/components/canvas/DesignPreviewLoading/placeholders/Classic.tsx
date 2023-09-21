6ed4d75d503a5053363f49b83b47b891675be247
alaca <ante.laca@gmail.com>
2023-09-21 22:58:31 +0200

    feature: classic placeholder component

---
+++ src/FormBuilder/resources/js/form-builder/src/components/canvas/DesignPreviewLoading/placeholders/Classic.tsx
@@ -0,0 +1,9 @@
+export default function Classic() {
+    return (
+        <div className="givewp__component-DesignPreview-container-classic">
+            <div className="givewp__component-DesignPreview-container-classic-header">
+
+            </div>
+        </div>
+    )
+}
