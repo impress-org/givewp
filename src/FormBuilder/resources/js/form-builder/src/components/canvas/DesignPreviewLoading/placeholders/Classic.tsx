de9e1c8097ac304e16f497fa2965d2b34d20dbf6
alaca <ante.laca@gmail.com>
2023-09-22 09:42:34 +0200

    feature: update template

--- src/FormBuilder/resources/js/form-builder/src/components/canvas/DesignPreviewLoading/placeholders/Classic.tsx
+++ src/FormBuilder/resources/js/form-builder/src/components/canvas/DesignPreviewLoading/placeholders/Classic.tsx
@@ -1,9 +1,34 @@
 export default function Classic() {
     return (
         <div className="givewp__component-DesignPreview-container-classic">
-            <div className="givewp__component-DesignPreview-container-classic-header">
-
+            <div className="givewp__component-DesignPreview-container-classic-headerwrap">
+                <div className="givewp__component-DesignPreview-container-classic-header"></div>
+                <div className="givewp__component-DesignPreview-container-classic-title">
+                    <div></div>
+                </div>
+                <div className="givewp__component-DesignPreview-container-classic-donation-levels">
+                    <div>
+                        <div></div>
+                    </div>
+                    <div>
+                        <div></div>
+                    </div>
+                    <div>
+                        <div></div>
+                    </div>
+                </div>
+                <div className="givewp__component-DesignPreview-container-classic-progressbar">
+                    <div></div>
+                </div>
             </div>
+            <div className="givewp__component-DesignPreview-container-classic-wrap">
+                <div className="givewp__component-DesignPreview-container-classic-bar"></div>
+                <div className="givewp__component-DesignPreview-container-classic-doublebar">
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+
         </div>
     )
 }
