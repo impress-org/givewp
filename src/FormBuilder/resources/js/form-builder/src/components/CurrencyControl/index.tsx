186e3fa21c80d9d5b76d3b86eb52cdfc037e1a7a
Paulo Iankoski <me@wpdev.com.br>
2023-09-26 19:30:29 -0300

    Refactor: normalize currency component (#6957)

--- src/FormBuilder/resources/js/form-builder/src/components/CurrencyControl/index.tsx
+++ src/FormBuilder/resources/js/form-builder/src/components/CurrencyControl/index.tsx
@@ -1,6 +1,7 @@
 import CurrencyInput, {CurrencyInputProps, formatValue} from 'react-currency-input-field';
 import {BaseControl} from '@wordpress/components';
 import {useInstanceId} from '@wordpress/compose';
+import {useState} from '@wordpress/element';
 import {getFormBuilderWindowData} from '@givewp/form-builder/common/getWindowData';
 
 const formatCurrencyAmount = (amount: string) => {
@@ -12,6 +13,28 @@ const formatCurrencyAmount = (amount: string) => {
     });
 };
 
+function parseValueFromLocale(amount: string): string {
+    if (!amount) {
+        return amount;
+    }
+
+    const numberFormat = new Intl.NumberFormat(window.navigator.language);
+    const parts = numberFormat.formatToParts(1234.56);
+
+    let groupSeparator: string;
+    let decimalSeparator: string;
+
+    for (const part of parts) {
+        if (part.type === 'group') {
+            groupSeparator = part.value;
+        } else if (part.type === 'decimal') {
+            decimalSeparator = part.value;
+        }
+    }
+
+    return amount.replaceAll(groupSeparator, '').replace(decimalSeparator, '.');
+}
+
 const Currency = ({amount}: {amount: string}) => {
     return formatCurrencyAmount(amount);
 };
@@ -22,28 +45,33 @@ interface CurrencyControlProps extends CurrencyInputProps {
     help?: string;
 }
 
-const CurrencyControl = ({label, help, hideLabelFromVision, ...rest}: CurrencyControlProps) => {
+const CurrencyControl = ({label, help, hideLabelFromVision, value, onValueChange, ...rest}: CurrencyControlProps) => {
+    const [localizedValue, setLocalizedValue] = useState(value);
+
     const {currency = 'USD'} = getFormBuilderWindowData();
     // simplified implementation of useBaseControlProps()
     const uniqueId = useInstanceId(BaseControl, 'wp-components-base-control');
 
+    const updateValue = (value: string) => {
+        setLocalizedValue(value);
+        onValueChange(parseValueFromLocale(value));
+    };
+
     return (
         <BaseControl label={label} help={help} id={uniqueId} hideLabelFromVision={hideLabelFromVision}>
             <CurrencyInput
-                {...rest}
                 id={uniqueId}
+                value={localizedValue}
+                onValueChange={updateValue}
                 className={'components-text-control__input'}
                 allowDecimals={true}
                 allowNegativeValue={false}
                 maxLength={9}
                 intlConfig={{locale: window.navigator.language, currency}}
+                {...rest}
             />
         </BaseControl>
     );
 };
 
-export {
-    formatCurrencyAmount,
-    Currency,
-    CurrencyControl,
-};
+export {formatCurrencyAmount, Currency, CurrencyControl};
