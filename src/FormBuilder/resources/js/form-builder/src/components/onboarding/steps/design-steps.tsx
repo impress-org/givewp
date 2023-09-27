bfeb45d7ef4a9037f371153cb55859f66859af80
Kyle B. Johnson <kyle.johnson@hey.com>
2023-09-27 16:08:27 -0400

    wip

--- src/FormBuilder/resources/js/form-builder/src/components/onboarding/steps/design-steps.tsx
+++ src/FormBuilder/resources/js/form-builder/src/components/onboarding/steps/design-steps.tsx
@@ -4,7 +4,7 @@ import DesignWelcome from "./components/DesignWelcome";
 
 export default [
     {
-        id: 'welcome',
+        id: 'design-welcome',
         title: __('Choose your form design', 'give'),
         text: __('Select one that suits your taste and requirements for your cause.', 'give'),
         component: <DesignWelcome />,
@@ -17,13 +17,13 @@ export default [
         },
     },
     {
-        id: 'edit-design',
+        id: 'design-edit-settings',
         attachTo: { element: '#sidebar-primary', on: 'left-start' as Placement },
         title: __('Editing a form design', 'give'),
         text: __('This is where you can customize the appearance (i.e. colors and features) of your form based on the selected form design.', 'give'),
     },
     {
-        id: 'edit-form',
+        id: 'design-edit-form',
         attachTo: { element: '#editor-state-toggle', on: 'bottom' as Placement },
         title: __('Edit form', 'give'),
         text: __('This is where you add and edit various blocks and sections to make up your form.', 'give'),
