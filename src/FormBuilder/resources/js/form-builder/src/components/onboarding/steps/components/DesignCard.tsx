473508bc6f2913d8ab619ec7e522f4dde461934b
Kyle B. Johnson <kyle.johnson@hey.com>
2023-09-27 16:05:03 -0400

    wip

--- src/FormBuilder/resources/js/form-builder/src/components/onboarding/steps/components/DesignCard.tsx
+++ src/FormBuilder/resources/js/form-builder/src/components/onboarding/steps/components/DesignCard.tsx
@@ -1,8 +1,8 @@
 import CheckCircleIcon from "./icons/check-circle"
 
-const DesignCard = ({title, description}) => {
+const DesignCard = ({title, description, image}) => {
     return <div style={{display: 'flex', flexDirection: 'column', gap: 'var(--givewp-spacing-1)', padding: 'var(--givewp-spacing-4) var(--givewp-spacing-5)'}}>
-        {/*<img src="#" alt="" style={{height: '12.5rem', width: '17.25rem'}} />*/}
+        <img src={image} alt="" style={{height: '12.5rem', width: '17.25rem', marginBottom: 'var(--givewp-spacing-4)'}} />
         <strong>{title}</strong>
         <p>{description}</p>
         <CheckCircleIcon />
