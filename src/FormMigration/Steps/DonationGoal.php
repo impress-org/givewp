1710e89232e3960e1a9f140c2de35e7e2d332580
Jon Waldstein <jonwaldstein@jons-air.mynetworksettings.com>
2023-10-03 11:10:21 -0400

    chore: revert goal step deleted

--- src/FormMigration/Steps/DonationGoal.php
+++ src/FormMigration/Steps/DonationGoal.php
@@ -12,6 +12,7 @@ class DonationGoal extends FormMigrationStep
         $this->formV3->settings->goalType = $this->formV2->getDonationGoalType();
         $this->formV3->settings->goalAmount = $this->formV2->getDonationGoalAmount();
         $this->formV3->settings->enableAutoClose = $this->formV2->isAutoClosedEnabled();
+        $this->formV3->settings->goalAchievedMessage = $this->formV2->getGoalAchievedMessage();
         // @note `_give_goal_color` is not supported in v3 forms (defers to the Form Design).
     }
 }
