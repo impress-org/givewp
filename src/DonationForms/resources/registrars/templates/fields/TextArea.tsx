5befee296593711bf51e7b5a858853739dabc07a
Paulo Iankoski <me@wpdev.com.br>
2023-10-04 17:40:29 -0300

    refactor: add rows attribute to Textarea field and update its template

--- src/DonationForms/resources/registrars/templates/fields/TextArea.tsx
+++ src/DonationForms/resources/registrars/templates/fields/TextArea.tsx
@@ -1,15 +1,26 @@
 import type {TextareaProps} from '@givewp/forms/propTypes';
 
-export default function TextArea({Label, ErrorMessage, placeholder, fieldError, inputProps, helpText}: TextareaProps) {
+export default function TextArea({
+    Label,
+    ErrorMessage,
+    placeholder,
+    fieldError,
+    inputProps,
+    description,
+    rows,
+}: TextareaProps) {
+    const FieldDescription = window.givewp.form.templates.layouts.fieldDescription;
+
     return (
         <label>
             <Label />
-            {helpText && (
-                <div className="givewp-fields-textarea__description">
-                    <small>{helpText}</small>
-                </div>
-            )}
-            <textarea aria-invalid={fieldError ? 'true' : 'false'} {...inputProps} placeholder={placeholder} />
+            {description && <FieldDescription description={description} />}
+            <textarea
+                aria-invalid={fieldError ? 'true' : 'false'}
+                {...inputProps}
+                placeholder={placeholder}
+                rows={rows}
+            />
             <ErrorMessage />
         </label>
     );
