b2ab1131a4a8c2b30f07553647a723778b147774
Jon Waldstein <jonwaldstein@jons-air.mynetworksettings.com>
2023-10-12 18:27:23 -0400

    refactor: remove fragment and format file

--- src/FormBuilder/resources/js/form-builder/src/blocks/fields/text/Edit.tsx
+++ src/FormBuilder/resources/js/form-builder/src/blocks/fields/text/Edit.tsx
@@ -5,16 +5,10 @@ export default function Edit({attributes}: BlockEditProps<any>) {
     const {label, isRequired, description, placeholder, defaultValue} = attributes;
 
     return (
-        <>
-            <div className={classnames({'give-is-required': isRequired})}>
-                <span
-                    className='components-input-control__label give-text-block__label'
-                >
-                    {label}
-                </span>
-                {description && <p className="give-text-block__description">{description}</p>}
-                <input type="text" placeholder={placeholder} readOnly onChange={null} value={defaultValue} />
-            </div>
-        </>
+        <div className={classnames({'give-is-required': isRequired})}>
+            <span className="components-input-control__label give-text-block__label">{label}</span>
+            {description && <p className="give-text-block__description">{description}</p>}
+            <input type="text" placeholder={placeholder} readOnly onChange={null} value={defaultValue} />
+        </div>
     );
 }
