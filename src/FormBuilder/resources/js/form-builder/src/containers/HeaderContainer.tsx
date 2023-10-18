5b7987992afbcbaf090ee576cf4baf2b822eace7
Joshua Dinh <josh@joshuadinh.tech>
2023-10-18 15:57:26 -0700

    refactor: handle pageslug on save

--- src/FormBuilder/resources/js/form-builder/src/containers/HeaderContainer.tsx
+++ src/FormBuilder/resources/js/form-builder/src/containers/HeaderContainer.tsx
@@ -1,8 +1,8 @@
 import React, {useState} from 'react';
 import {EditIcon, GiveIcon} from '../components/icons';
-import {drawerRight, moreVertical, external} from '@wordpress/icons';
+import {drawerRight, external, moreVertical} from '@wordpress/icons';
 import {setFormSettings, setTransferState, useFormState, useFormStateDispatch} from '../stores/form-state';
-import {Button, Dropdown, ExternalLink, Icon, MenuGroup, MenuItem, TextControl} from '@wordpress/components';
+import {Button, Dropdown, ExternalLink, MenuGroup, MenuItem, TextControl} from '@wordpress/components';
 import {__} from '@wordpress/i18n';
 import {Header} from '../components';
 import {getWindowData, Storage} from '../common';
@@ -11,9 +11,10 @@ import {setIsDirty} from '@givewp/form-builder/stores/form-state/reducer';
 import revertMissingBlocks from '@givewp/form-builder/common/revertMissingBlocks';
 import {Markup} from 'interweave';
 import {InfoModal, ModalType} from '../components/modal';
-import {setEditorMode, useEditorState, useEditorStateDispatch} from "@givewp/form-builder/stores/editor-state";
-import EditorMode from "@givewp/form-builder/types/editorMode";
-import {useDispatch} from "@wordpress/data";
+import {setEditorMode, useEditorState, useEditorStateDispatch} from '@givewp/form-builder/stores/editor-state';
+import EditorMode from '@givewp/form-builder/types/editorMode';
+import {useDispatch} from '@wordpress/data';
+import {cleanForSlug} from '@wordpress/url';
 
 const Logo = () => (
     <div
@@ -37,11 +38,7 @@ const Logo = () => (
     </div>
 );
 
-const HeaderContainer = ({
-                             SecondarySidebarButtons = null,
-                             showSidebar,
-                             toggleShowSidebar,
-                         }) => {
+const HeaderContainer = ({SecondarySidebarButtons = null, showSidebar, toggleShowSidebar}) => {
     const {blocks, settings: formSettings, isDirty, transfer} = useFormState();
 
     const {formTitle} = formSettings;
@@ -72,7 +69,7 @@ const HeaderContainer = ({
                 setErrorMessage(error.message);
             })
             .then(({formTitle, pageSlug}: FormSettings) => {
-                dispatch(setFormSettings({formTitle, pageSlug}));
+                dispatch(setFormSettings({formTitle, pageSlug: isPublished ? pageSlug : cleanForSlug(formTitle)}));
                 dispatch(setIsDirty(false));
                 setSaving(null);
                 showOnSaveNotice(formStatus);
@@ -82,22 +79,25 @@ const HeaderContainer = ({
     const showOnSaveNotice = (formStatus: string) => {
         if ('draft' === formStatus) {
             createSuccessNotice(__('Draft saved.', 'give'), {
-                type: 'snackbar'
+                type: 'snackbar',
             });
         } else {
-            const notice = 'publish' === formStatus && formSettings.formStatus !== 'draft'
-                ? __('Form updated.', 'give')
-                : __('Form published.', 'give')
+            const notice =
+                'publish' === formStatus && formSettings.formStatus !== 'draft'
+                    ? __('Form updated.', 'give')
+                    : __('Form published.', 'give');
 
             createSuccessNotice(notice, {
                 type: 'snackbar',
-                actions: [{
-                    label: __('View form', 'give'),
-                    url: permalink
-                }]
+                actions: [
+                    {
+                        label: __('View form', 'give'),
+                        url: permalink,
+                    },
+                ],
             });
         }
-    }
+    };
 
     const {mode} = useEditorState();
     const dispatchEditorState = useEditorStateDispatch();
@@ -108,7 +108,7 @@ const HeaderContainer = ({
         if (EditorMode.design === mode) {
             dispatchEditorState(setEditorMode(EditorMode.schema));
         }
-    }
+    };
 
     // @ts-ignore
     return (
@@ -126,7 +126,7 @@ const HeaderContainer = ({
                                 borderRadius: '4px',
                                 display: 'flex',
                                 gap: 'var(--givewp-spacing-2)',
-                                padding: 'var(--givewp-spacing-3) var(--givewp-spacing-4)'
+                                padding: 'var(--givewp-spacing-3) var(--givewp-spacing-4)',
                             }}
                             onClick={() => toggleEditorMode()}
                             icon={EditIcon}
@@ -154,16 +154,11 @@ const HeaderContainer = ({
                             {isSaving && 'draft' === isSaving
                                 ? __('Saving...', 'give')
                                 : 'draft' === formSettings.formStatus
-                                    ? __('Save as Draft', 'give')
-                                    : __('Switch to Draft', 'give')}
+                                ? __('Save as Draft', 'give')
+                                : __('Switch to Draft', 'give')}
                         </Button>
                         {isPublished && (
-                            <Button
-                                label={__('View form', 'give')}
-                                href={permalink}
-                                target="_blank"
-                                icon={external}
-                            />
+                            <Button label={__('View form', 'give')} href={permalink} target="_blank" icon={external} />
                         )}
                         <Button
                             onClick={() => onSave('publish')}
@@ -174,8 +169,8 @@ const HeaderContainer = ({
                             {isSaving && 'publish' === isSaving
                                 ? __('Updating...', 'give')
                                 : 'publish' === formSettings.formStatus
-                                    ? __('Update', 'give')
-                                    : __('Publish', 'give')}
+                                ? __('Update', 'give')
+                                : __('Publish', 'give')}
                         </Button>
                         <Button onClick={toggleShowSidebar} isPressed={showSidebar} icon={drawerRight} />
                         <Dropdown
@@ -189,12 +184,12 @@ const HeaderContainer = ({
                                         icon={moreVertical}
                                         onClick={() => {
                                             if (transfer.showTooltip) {
-                                                dispatch(setTransferState({showTooltip: false}))
+                                                dispatch(setTransferState({showTooltip: false}));
                                             }
                                             onToggle();
                                         }}
                                     />
-                                )
+                                );
                             }}
                             renderContent={({onClose}) => (
                                 <div style={{minWidth: '280px', maxWidth: '400px'}}>
@@ -214,7 +209,9 @@ const HeaderContainer = ({
                                         {isMigratedForm && !isTransferredForm && !transfer.showNotice && (
                                             <>
                                                 <MenuItem
-                                                    className={transfer.showTooltip && 'givewp-transfer-selected-menuitem'}
+                                                    className={
+                                                        transfer.showTooltip && 'givewp-transfer-selected-menuitem'
+                                                    }
                                                     onClick={() => {
                                                         dispatch(setTransferState({showTransferModal: true}));
                                                         onClose();
@@ -225,7 +222,10 @@ const HeaderContainer = ({
 
                                                 {transfer.showTooltip && (
                                                     <div className="givewp-transfer-tooltip">
-                                                        {__('Want to transfer donation data later? Access this option in the three dots menu above at any time.', 'give')}
+                                                        {__(
+                                                            'Want to transfer donation data later? Access this option in the three dots menu above at any time.',
+                                                            'give'
+                                                        )}
                                                     </div>
                                                 )}
                                             </>
@@ -236,9 +236,7 @@ const HeaderContainer = ({
                                         href="https://docs.givewp.com/nextgenfeedback"
                                         rel="noopener noreferrer"
                                     >
-                                        <MenuItem icon={external}>
-                                                {__('Submit Feedback', 'give')}
-                                        </MenuItem>
+                                        <MenuItem icon={external}>{__('Submit Feedback', 'give')}</MenuItem>
                                     </ExternalLink>
                                 </div>
                             )}
