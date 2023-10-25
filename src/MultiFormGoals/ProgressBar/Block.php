2d7cb359e8b397a85280cf27a0d0ae6b276efe0b
Glauber Silva <glauber.silva@givewp.com>
2023-10-25 13:37:17 -0300

    fix: respect ids param

--- src/MultiFormGoals/ProgressBar/Block.php
+++ src/MultiFormGoals/ProgressBar/Block.php
@@ -62,6 +62,8 @@ class Block
                 'ids' => array_map(
                     static function ($id) {
                         _give_redirect_form_id($id);
+
+                        return $id;
                     },
                     $attributes['ids']
                 ),
