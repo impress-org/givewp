4eb75562b5eec7c2e7383df659759f08ec8f85d5
Kyle B. Johnson <kyle.johnson@hey.com>
2023-11-01 09:36:41 -0400

    refactor: Update field name support check

--- src/FormBuilder/resources/js/form-builder/src/middleware/duplicatedFields.ts
+++ src/FormBuilder/resources/js/form-builder/src/middleware/duplicatedFields.ts
@@ -7,21 +7,23 @@ export default (blocks) => {
             ...section,
             innerBlocks: section.innerBlocks.map((block) => {
 
-                // Only Text Field blocks, for now.
-                if(block.name !== 'givewp/text') return block;
-
                 const isNewField = typeof block.attributes.metaUUID === 'undefined';
                 const isDuplicatedField = !isNewField && block.attributes.metaUUID !== block.clientId;
                 if(!isDuplicatedField) return block;
 
-                const fieldNames = getBlockNames(blocks);
-                const validateFieldName = getFieldNameValidator(fieldNames);
+                // If the duplicated block has a field name it needs to be updated for uniqueness.
+                // This is important to do here while we know which block is the duplicate.
+                let fieldName = block.attributes.fieldName;
+                if(fieldName) {
+                    const fieldNames = getBlockNames(blocks);
+                    const validateFieldName = getFieldNameValidator(fieldNames);
 
-                const slugifiedLabel = slugifyMeta(block.attributes.label);
-                let slugifiedName = block.attributes.fieldName ?? slugifiedLabel;
+                    const slugifiedLabel = slugifyMeta(block.attributes.label);
+                    let slugifiedName = block.attributes.fieldName ?? slugifiedLabel;
 
-                const [isUnique, suggestedName] = validateFieldName(slugifiedName, false);
-                const fieldName = isUnique ? slugifiedName : suggestedName;
+                    const [isUnique, suggestedName] = validateFieldName(slugifiedName, false);
+                    fieldName = isUnique ? slugifiedName : suggestedName;
+                }
 
                 return {
                     ...block,
--- src/FormBuilder/resources/js/form-builder/src/middleware/uniqueFieldNames.ts
+++ src/FormBuilder/resources/js/form-builder/src/middleware/uniqueFieldNames.ts
@@ -1,5 +1,9 @@
 import {slugifyMeta} from "@givewp/form-builder/supports/field-settings/MetaKeyTextControl";
 import {flattenBlocks, getFieldNameValidator} from "@givewp/form-builder/hooks/useFieldNameValidator";
+import {FieldSettings} from "@givewp/form-builder/supports/field-settings/types";
+import {getBlockSupport} from "@wordpress/blocks";
+import {GiveWPSupports} from "@givewp/form-builder/supports/types";
+import normalizeFieldSettings from "@givewp/form-builder/supports/field-settings/normalizeFieldSettings";
 
 const hasLabelChanged = (slugifiedName, slugifiedLabel) => {
     return slugifiedName !== slugifiedLabel &&
@@ -8,22 +12,34 @@ const hasLabelChanged = (slugifiedName, slugifiedLabel) => {
         !slugifiedName.startsWith(slugifiedLabel + '_' );
 }
 
+const doesBlockSupportFieldNames = (block) => {
+    // @ts-ignore
+    const giveSupports = getBlockSupport(block.name, 'givewp') as GiveWPSupports;
+
+    const fieldSettings: FieldSettings = normalizeFieldSettings(giveSupports?.fieldSettings);
+
+    return fieldSettings?.metaKey;
+}
+
+const getBlockNamesWithoutBlock = (blocks, block) => {
+    return blocks
+        .flatMap(flattenBlocks)
+        .filter(b => b.clientId !== block.clientId)
+        .map((block) => block.attributes.fieldName)
+        .filter((name) => name)
+}
+
 export default (blocks) => {
     return blocks.map((section) => {
         return {
             ...section,
             innerBlocks: section.innerBlocks.map((block) => {
 
-                // Only Text Field blocks, for now.
-                if(block.name !== 'givewp/text') return block;
+                if(!doesBlockSupportFieldNames(block)) return block;
 
-                const fieldNames = blocks
-                    .flatMap(flattenBlocks)
-                    .filter(b => b.clientId !== block.clientId)
-                    .map((block) => block.attributes.fieldName)
-                    .filter((name) => name)
-
-                const validateFieldName = getFieldNameValidator(fieldNames);
+                const validateFieldName = getFieldNameValidator(
+                    getBlockNamesWithoutBlock(blocks, block)
+                );
 
                 const slugifiedLabel = slugifyMeta(block.attributes.label);
                 let slugifiedName = block.attributes.fieldName || slugifiedLabel;
