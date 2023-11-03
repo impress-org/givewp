92da34b5b64bbcba7d31991849ff9691dddc460d
Angela Blake <35241639+angelablake@users.noreply.github.com>
2023-11-03 14:13:56 -0500

    chore: prepare for 3.1.1 release

--- src/DonationForms/Shortcodes/GiveFormShortcode.php
+++ src/DonationForms/Shortcodes/GiveFormShortcode.php
@@ -13,7 +13,7 @@ class GiveFormShortcode
     public static $instance = 0;
 
     /**
-     * @unreleased use static instance ID to simulate blockId attribute
+     * @since 3.1.1 use static instance ID to simulate blockId attribute
      * @since 3.0.0
      */
     public function __invoke(string $output, array $atts): string
