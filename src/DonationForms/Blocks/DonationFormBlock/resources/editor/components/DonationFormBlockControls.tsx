abf51e18deb49c99dfedafaa59816943289fe8f7
Joshua Dinh <josh@joshuadinh.tech>
2023-11-09 14:55:42 -0800

    refactor: combine inspector controls

--- src/DonationForms/Blocks/DonationFormBlock/resources/editor/components/DonationFormBlockControls.tsx
+++ src/DonationForms/Blocks/DonationFormBlock/resources/editor/components/DonationFormBlockControls.tsx
@@ -1,16 +1,33 @@
-import {ExternalLink, PanelBody, PanelRow, SelectControl, TextControl} from '@wordpress/components';
+import {ExternalLink, PanelBody, PanelRow, SelectControl, TextControl, ToggleControl} from '@wordpress/components';
 import {__} from '@wordpress/i18n';
 import {InspectorControls} from '@wordpress/block-editor';
+import {Option} from '../types';
+
+interface DonationFormBlockControls {
+    attributes;
+    setAttributes: (newAttributes: Record<string, any>) => void;
+    formOptions: Option[];
+    isResolving: boolean;
+    isLegacyForm: boolean;
+}
 
 export default function DonationFormBlockControls({
-    isResolving,
-    formOptions,
-    formId,
-    formFormat,
+    attributes,
     setAttributes,
-    openFormButton,
-    showOpenFormButton,
-}) {
+    formOptions,
+    isResolving,
+    isLegacyForm,
+}: DonationFormBlockControls) {
+    const {id, displayStyle, continueButtonTitle, showTitle, contentDisplay, showGoal, showContent} = attributes;
+    const showOpenFormButton = displayStyle === ('newTab' || 'modal' || 'reveal');
+
+    const displayStyleOptions = (
+        options: {label: string; value: string}[],
+        legacyOptions: {label: string; value: string}[]
+    ) => {
+        return isLegacyForm ? options.concat(legacyOptions) : options;
+    };
+
     return (
         <InspectorControls>
             <PanelBody title={__('Form Settings', 'give')} initialOpen={true}>
@@ -20,7 +37,7 @@ export default function DonationFormBlockControls({
                     ) : (
                         <SelectControl
                             label={__('Choose a donation form', 'give')}
-                            value={formId ?? ''}
+                            value={id ?? ''}
                             options={[
                                 // add a disabled selector manually
                                 ...[{value: '', label: __('Select...', 'give'), disabled: true}],
@@ -35,47 +52,110 @@ export default function DonationFormBlockControls({
                 <PanelRow>
                     <SelectControl
                         label={__('Form Format', 'give')}
-                        value={formFormat}
-                        options={[
-                            {
-                                label: __('Full Form', 'give'),
-                                value: 'fullForm',
-                            },
-                            {
-                                label: __('New Tab', 'give'),
-                                value: 'newTab',
-                            },
-                            {
-                                label: __('Modal', 'give'),
-                                value: 'modal',
-                            },
-                        ]}
+                        value={displayStyle}
+                        options={displayStyleOptions(
+                            [
+                                {
+                                    label: __('Full Form', 'give'),
+                                    value: 'fullForm',
+                                },
+                                {
+                                    label: __('New Tab', 'give'),
+                                    value: 'newTab',
+                                },
+                                {
+                                    label: __('Modal', 'give'),
+                                    value: 'modal',
+                                },
+                            ],
+                            [
+                                {
+                                    label: __('Reveal', 'give'),
+                                    value: 'reveal',
+                                },
+                            ]
+                        )}
                         onChange={(value) => {
                             setAttributes({displayStyle: value});
                         }}
                     />
                 </PanelRow>
-
                 {showOpenFormButton && (
                     <PanelRow>
                         <TextControl
                             label={__('Open Form Button', 'give')}
-                            value={openFormButton}
+                            value={continueButtonTitle}
                             onChange={(value) => {
                                 setAttributes({continueButtonTitle: value});
                             }}
                         />
                     </PanelRow>
                 )}
-                <PanelRow>
-                    {formId && (
+
+                {isLegacyForm && (
+                    <>
+                        <PanelRow>
+                            <ToggleControl
+                                label={__('Title', 'give')}
+                                name="showTitle"
+                                checked={!!showTitle}
+                                onChange={(value) => {
+                                    setAttributes({showTitle: value});
+                                }}
+                            />
+                        </PanelRow>
+                        <PanelRow>
+                            <ToggleControl
+                                label={__('Goal', 'give')}
+                                name="showGoal"
+                                checked={!!showGoal}
+                                onChange={(value) => {
+                                    setAttributes({showGoal: value});
+                                }}
+                            />
+                        </PanelRow>
+                        <PanelRow>
+                            <ToggleControl
+                                label={__('Content', 'give')}
+                                name="contentDisplay"
+                                checked={!!contentDisplay}
+                                onChange={(value) => {
+                                    setAttributes({contentDisplay: value});
+                                }}
+                            />
+                        </PanelRow>
+                        {contentDisplay && (
+                            <PanelRow>
+                                <SelectControl
+                                    label={__('Content Position', 'give')}
+                                    name="showContent"
+                                    value={showContent}
+                                    options={[
+                                        {value: 'above', label: __('Above', 'give')},
+                                        {value: 'below', label: __('Below', 'give')},
+                                    ]}
+                                    onChange={(value) => {
+                                        setAttributes({showContent: value});
+                                    }}
+                                />
+                            </PanelRow>
+                        )}
+                    </>
+                )}
+
+                {id && (
+                    <PanelRow>
                         <ExternalLink
-                            href={`/wp-admin/edit.php?post_type=give_forms&page=givewp-form-builder&donationFormID=${formId}`}
+                            href={
+                                isLegacyForm
+                                    ? `/wp-admin/post.php?post=${id}&action=edit`
+                                    : `/wp-admin/edit.php?post_type=give_forms&page=givewp-form-builder&donationFormID=${id}`
+                            }
                         >
                             {__('Edit donation form', 'give')}
                         </ExternalLink>
-                    )}
-                </PanelRow>
+                    </PanelRow>
+                )}
             </PanelBody>
         </InspectorControls>
     );
