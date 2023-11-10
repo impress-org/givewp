70febf6ccd9081939b05f6009f32061a367b3c06
Joshua Dinh <josh@joshuadinh.tech>
2023-11-10 10:01:31 -0800

    refactor: remove utiils - begin reconstructing edit function. wip.

--- src/DonationForms/Blocks/DonationFormBlock/resources/editor/Edit.tsx
+++ src/DonationForms/Blocks/DonationFormBlock/resources/editor/Edit.tsx
@@ -1,12 +1,9 @@
 import {useCallback, useEffect, useState} from '@wordpress/element';
-import DonationFormBlockPreview from './components/DonationFormBlockPreview';
+import ServerSideRender from '@wordpress/server-side-render';
 import DonationFormSelector from './components/DonationFormSelector';
 import useFormOptions from './hooks/useFormOptions';
 import DonationFormBlockControls from './components/DonationFormBlockControls';
-
-import ServerSideRender from '@wordpress/server-side-render';
-import {isLegacyForm, isTemplateForm} from '../../../../../../blocks/utils';
-import Inspector from '../../../../../../blocks/donation-form/edit/inspector';
+import DonationFormBlockPreview from './components/DonationFormBlockPreview';
 
 import './styles/index.scss';
 
@@ -17,7 +14,7 @@ import './styles/index.scss';
 export default function Edit(props) {
     const {attributes, isSelected, setAttributes, className, clientId} = props;
     const {id, blockId, displayStyle, continueButtonTitle} = attributes;
-    const {formOptions, isResolving, forms} = useFormOptions();
+    const {formOptions, isResolving} = useFormOptions();
     const [showPreview, setShowPreview] = useState<boolean>(!!id);
 
     useEffect(() => {
@@ -32,36 +29,58 @@ export default function Edit(props) {
         }
     }, [isResolving, id, JSON.stringify(formOptions)]);
 
-    const showOpenFormButton = displayStyle === 'newTab' || displayStyle === 'modal';
-    const isv2Form = forms && id && isTemplateForm(forms, id);
-    const isv3Form = forms && id && !isTemplateForm(forms, id) && !isLegacyForm(forms, id);
-    if (isv2Form) {
-        return (
-            <div className={!!isSelected ? `${className} isSelected` : className}>
-                <Inspector {...{...props}} />
-                <ServerSideRender block="give/donation-form" attributes={attributes} />
-            </div>
-        );
-    }
+    // Todo: combine & return form.template / use pageSlug for new tab linke
+    const isLegacyForm = () => {
+        if (!isResolving && formOptions.length > 0) {
+            const data = forms.find((form) => parseInt(form.id) === parseInt(id));
 
-    if (isv3Form) {
+            return (
+                data &&
+                data.excerpt.rendered !== '<p>[]</p>\n' &&
+                (!data.formTemplate || data.formTemplate === 'legacy')
+            );
+        }
+    };
+
+    const isTemplateForm = (forms, id) => {
+        if (forms) {
+            const data = forms.find((form) => parseInt(form.id) === parseInt(id));
+
+            return data && data.formTemplate !== '';
+        }
+
+        return false;
+    };
+    console.error(forms);
+    const isv3Form = formOptions && id && !isTemplateForm(forms, id) && !isLegacyForm(forms, id);
+    const isv2Form = formOptions && id && isTemplateForm(forms, id);
+
+    if (id) {
         return (
             <>
                 <DonationFormBlockControls
-                    isResolving={isResolving}
-                    formOptions={formOptions}
-                    formId={id}
-                    formFormat={displayStyle}
+                    attributes={attributes}
                     setAttributes={setAttributes}
-                    openFormButton={continueButtonTitle}
-                    showOpenFormButton={showOpenFormButton}
+                    formOptions={formOptions}
+                    isResolving={isResolving}
+                    isLegacyForm={isLegacyForm(forms, id)}
                 />
-                <DonationFormBlockPreview
-                    clientId={clientId}
-                    formId={id}
-                    formFormat={displayStyle}
-                    openFormButton={continueButtonTitle}
-                />
+
+                {isv2Form && (
+                    <div className={!!isSelected ? `${className} isSelected` : className}>
+                        <ServerSideRender block="give/donation-form" attributes={attributes} />
+                    </div>
+                )}
+
+                {isv3Form && (
+                    <DonationFormBlockPreview
+                        clientId={clientId}
+                        formId={id}
+                        formFormat={displayStyle}
+                        openFormButton={continueButtonTitle}
+                        pageSlug={formOptions.pageSlug}
+                    />
+                )}
             </>
         );
     }
