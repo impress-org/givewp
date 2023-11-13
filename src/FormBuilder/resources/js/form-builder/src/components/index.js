7f7d9c3748472bc8b873c759bb70911e8490690e
Paulo Iankoski <me@wpdev.com.br>
2023-11-13 12:07:24 -0300

    feature: make settings components available for add-ons

--- src/FormBuilder/resources/js/form-builder/src/components/index.js
+++ src/FormBuilder/resources/js/form-builder/src/components/index.js
@@ -1,5 +1,7 @@
 import {CurrencyControl} from '@givewp/form-builder/components/CurrencyControl';
 import ClassicEditor from '@givewp/form-builder/components/ClassicEditor';
+import SettingsGroup from '@givewp/form-builder/components/canvas/FormSettingsContainer/components/SettingsGroup';
+import SettingsSection from '@givewp/form-builder/components/canvas/FormSettingsContainer/components/SettingsSection';
 
 export {default as Header} from './header';
 export {SecondarySidebar, Sidebar} from './sidebar';
@@ -8,4 +10,6 @@ export default function registerComponents() {
     window.givewp.form.components = window.givewp.form.components || {};
     window.givewp.form.components.CurrencyControl = CurrencyControl;
     window.givewp.form.components.ClassicEditor = ClassicEditor;
+    window.givewp.form.components.SettingsGroup = SettingsGroup;
+    window.givewp.form.components.SettingsSection = SettingsSection;
 }
