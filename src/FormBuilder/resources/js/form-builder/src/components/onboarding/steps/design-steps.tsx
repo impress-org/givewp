5f08e9f71588e600edf3d6ee3707e5e9233fe88d
Paulo Iankoski <me@wpdev.com.br>
2023-11-20 09:51:42 -0300

    refactor: update tour step pointing to new tabs

--- src/FormBuilder/resources/js/form-builder/src/components/onboarding/steps/design-steps.tsx
+++ src/FormBuilder/resources/js/form-builder/src/components/onboarding/steps/design-steps.tsx
@@ -1,17 +1,20 @@
-import {__} from "@wordpress/i18n";
-import Placement from "./types/placement";
+import {__} from '@wordpress/i18n';
+import Placement from './types/placement';
 
 export default [
     {
         id: 'design-edit-settings',
-        attachTo: { element: '#sidebar-primary', on: 'left-start' as Placement },
+        attachTo: {element: '#sidebar-primary', on: 'left-start' as Placement},
         title: __('Editing a form design', 'give'),
-        text: __('This is where you can customize the appearance (i.e. colors and features) of your form based on the selected form design.', 'give'),
+        text: __(
+            'This is where you can customize the appearance (i.e. colors and features) of your form based on the selected form design.',
+            'give'
+        ),
     },
     {
         id: 'design-edit-form',
-        attachTo: { element: '#editor-state-toggle', on: 'bottom-end' as Placement },
-        title: __('Edit form', 'give'),
+        attachTo: {element: '#editor-state-switch-schema', on: 'bottom-center' as Placement},
+        title: __('Build', 'give'),
         text: __('This is where you add and edit various blocks and sections to make up your form.', 'give'),
     },
-]
+];
