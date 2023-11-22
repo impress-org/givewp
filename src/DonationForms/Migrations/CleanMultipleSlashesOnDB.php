44b32dc263b529eb3901ba0d6d086ea363babc29
Glauber Silva <glauber.silva@givewp.com>
2023-11-22 09:14:28 -0300

    refactor: remove formBuilderFields from migration

--- src/DonationForms/Migrations/CleanMultipleSlashesOnDB.php
+++ src/DonationForms/Migrations/CleanMultipleSlashesOnDB.php
@@ -23,38 +23,19 @@ class CleanMultipleSlashesOnDB extends Migration
          * and became 6 when it runs on the database, which represents a sequence of only 3 backslashes as we need 2 of
          * them to escape just 1 as you can check here: https://dev.mysql.com/doc/refman/8.0/en/string-literals.html
          */
-        $sql = DB::prepare("SELECT form_id FROM $formMetaTable as fm WHERE
-        (fm.meta_key = 'formBuilderSettings' AND fm.meta_value LIKE '%\\\\\\\\\\\\\\\\\\\\\\\\%')
-        OR
-        (fm.meta_key = 'formBuilderFields' AND fm.meta_value LIKE '%\\\\\\\\\\\\\\\\\\\\\\\\%')");
+        $sql = DB::prepare("SELECT * FROM $formMetaTable as fm
+         WHERE fm.meta_key = 'formBuilderSettings' AND fm.meta_value LIKE '%\\\\\\\\\\\\\\\\\\\\\\\\%'");
 
-        $formIds = DB::get_results($sql, ARRAY_A);
+        $items = DB::get_results($sql);
 
-        foreach ($formIds as $formId) {
-            $formId = (int)$formId['form_id'];
-
-            $settings = Give()->form_meta->get_meta($formId, 'formBuilderSettings', true);
-            $blocks = Give()->form_meta->get_meta($formId, 'formBuilderFields', true);
-
-            $settings = json_decode($settings, true, JSON_UNESCAPED_SLASHES);
-            $blocks = json_decode($blocks, true, JSON_UNESCAPED_SLASHES);
-
-            $settings = $this->cleanMultipleSlashes($settings);
-            $blocks = $this->cleanMultipleSlashes($blocks);
-
+        foreach ($items as $item) {
+            $settings = $this->cleanMultipleSlashes(json_decode($item->meta_value, true, JSON_UNESCAPED_SLASHES));
 
             DB::table('give_formmeta')
-                ->where('form_id', $formId)
+                ->where('form_id', $item->form_id)
                 ->where('meta_key', DonationFormMetaKeys::SETTINGS()->getValue())
                 ->update([
                     'meta_value' => json_encode($settings),
-                ]);
-
-            DB::table('give_formmeta')
-                ->where('form_id', $formId)
-                ->where('meta_key', DonationFormMetaKeys::FIELDS()->getValue())
-                ->update([
-                    'meta_value' => json_encode($blocks),
                 ]);
         }
     }
