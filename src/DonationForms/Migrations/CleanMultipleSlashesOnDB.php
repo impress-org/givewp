85e822c2bfdc2ed2ba18023fab65d29f824b0658
Glauber Silva <glauber.silva@givewp.com>
2023-11-22 08:51:39 -0300

    refactor: remove production code from migration logic

--- src/DonationForms/Migrations/CleanMultipleSlashesOnDB.php
+++ src/DonationForms/Migrations/CleanMultipleSlashesOnDB.php
@@ -2,11 +2,8 @@
 
 namespace Give\DonationForms\Migrations;
 
-use Give\DonationForms\Actions\SanitizeDonationFormPreviewRequest;
-use Give\DonationForms\Models\DonationForm;
-use Give\DonationForms\Properties\FormSettings;
-use Give\Framework\Blocks\BlockCollection;
-use Give\Framework\Exceptions\Primitives\Exception;
+use Give\DonationForms\ValueObjects\DonationFormMetaKeys;
+use Give\Framework\Database\DB;
 use Give\Framework\Migrations\Contracts\Migration;
 
 /**
@@ -14,32 +11,51 @@ use Give\Framework\Migrations\Contracts\Migration;
  */
 class CleanMultipleSlashesOnDB extends Migration
 {
-
     /**
      * @unreleased
-     *
-     * @throws Exception
      */
     public function run()
     {
-        global $wpdb;
+        $formMetaTable = DB::prefix('give_formmeta');
 
-        $formMetaTable = $wpdb->prefix . 'give_formmeta';
-
-        $sql = $wpdb->prepare("SELECT form_id FROM $formMetaTable as fm WHERE
+        /**
+         * For some reason, these 24 backslashes used in the SQL instruction became 12 when we passed it to the $sql var,
+         * and became 6 when it runs on the database, which represents a sequence of only 3 backslashes as we need 2 of
+         * them to escape just 1 as you can check here: https://dev.mysql.com/doc/refman/8.0/en/string-literals.html
+         */
+        $sql = DB::prepare("SELECT form_id FROM $formMetaTable as fm WHERE
         (fm.meta_key = 'formBuilderSettings' AND fm.meta_value LIKE '%\\\\\\\\\\\\\\\\\\\\\\\\%')
         OR
         (fm.meta_key = 'formBuilderFields' AND fm.meta_value LIKE '%\\\\\\\\\\\\\\\\\\\\\\\\%')");
 
-        $formIds = $wpdb->get_results($sql, ARRAY_A);
+        $formIds = DB::get_results($sql, ARRAY_A);
 
         foreach ($formIds as $formId) {
-            $form = DonationForm::find((int)$formId['form_id']);
-            $settings = (new SanitizeDonationFormPreviewRequest())($form->settings->toArray());
-            $blocks = (new SanitizeDonationFormPreviewRequest())($form->blocks->toArray());
-            $form->settings = FormSettings::fromArray($settings);
-            $form->blocks = BlockCollection::fromArray($blocks);
-            $form->save();
+            $formId = (int)$formId['form_id'];
+
+            $settings = Give()->form_meta->get_meta($formId, 'formBuilderSettings', true);
+            $blocks = Give()->form_meta->get_meta($formId, 'formBuilderFields', true);
+
+            $settings = json_decode($settings, true, JSON_UNESCAPED_SLASHES);
+            $blocks = json_decode($blocks, true, JSON_UNESCAPED_SLASHES);
+
+            $settings = $this->cleanMultipleSlashes($settings);
+            $blocks = $this->cleanMultipleSlashes($blocks);
+
+
+            DB::table('give_formmeta')
+                ->where('form_id', $formId)
+                ->where('meta_key', DonationFormMetaKeys::SETTINGS()->getValue())
+                ->update([
+                    'meta_value' => json_encode($settings),
+                ]);
+
+            DB::table('give_formmeta')
+                ->where('form_id', $formId)
+                ->where('meta_key', DonationFormMetaKeys::FIELDS()->getValue())
+                ->update([
+                    'meta_value' => json_encode($blocks),
+                ]);
         }
     }
 
@@ -65,5 +81,17 @@ class CleanMultipleSlashesOnDB extends Migration
     public static function timestamp()
     {
         return strtotime('2023-20-11');
+    }
+
+    /**
+     * @unreleased
+     */
+    public function cleanMultipleSlashes($var)
+    {
+        if (is_array($var)) {
+            return array_map([$this, 'cleanMultipleSlashes'], $var);
+        } else {
+            return is_string($var) ? deslash($var) : $var;
+        }
     }
 }
