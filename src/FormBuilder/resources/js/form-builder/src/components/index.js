2c12f3538742783b83fb92f8db93042e51eaf76f
Glauber Silva <glauber.silva@givewp.com>
2023-11-27 18:37:00 -0300

    feature: mount OptionsPanel to window global object

--- src/FormBuilder/resources/js/form-builder/src/components/index.js
+++ src/FormBuilder/resources/js/form-builder/src/components/index.js
@@ -1,5 +1,6 @@
 import {CurrencyControl} from '@givewp/form-builder/components/CurrencyControl';
 import ClassicEditor from '@givewp/form-builder/components/ClassicEditor';
+import OptionsPanel from '@givewp/form-builder/components/OptionsPanel';
 
 export {default as Header} from './header';
 export {SecondarySidebar, Sidebar} from './sidebar';
@@ -8,4 +9,5 @@ export default function registerComponents() {
     window.givewp.form.components = window.givewp.form.components || {};
     window.givewp.form.components.CurrencyControl = CurrencyControl;
     window.givewp.form.components.ClassicEditor = ClassicEditor;
+    window.givewp.form.components.OptionsPanel = OptionsPanel;
 }
