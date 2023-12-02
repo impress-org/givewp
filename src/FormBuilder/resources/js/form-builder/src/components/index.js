00ae82a429cdf0d6b34d26c86dff6d210e92aac3
Paulo Iankoski <me@wpdev.com.br>
2023-12-01 21:54:27 -0300

    refactor: move SettingsSection component to library repo

--- src/FormBuilder/resources/js/form-builder/src/components/index.js
+++ src/FormBuilder/resources/js/form-builder/src/components/index.js
@@ -1,6 +1,5 @@
 import {CurrencyControl} from '@givewp/form-builder/components/CurrencyControl';
 import ClassicEditor from '@givewp/form-builder/components/ClassicEditor';
-import SettingsSection from '@givewp/form-builder/components/canvas/FormSettingsContainer/components/SettingsSection';
 
 export {default as Header} from './header';
 export {SecondarySidebar, Sidebar} from './sidebar';
@@ -9,5 +8,4 @@ export default function registerComponents() {
     window.givewp.form.components = window.givewp.form.components || {};
     window.givewp.form.components.CurrencyControl = CurrencyControl;
     window.givewp.form.components.ClassicEditor = ClassicEditor;
-    window.givewp.form.components.SettingsSection = SettingsSection;
 }
