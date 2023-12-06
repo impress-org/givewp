076aeb4d0d502b9274d12fcc267d5a6b4f96218c
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2023-12-06 21:44:02 +0530

    Hotfix: PayPal Donations payment methods should render in donation form (#7121)

--- src/PaymentGateways/PayPalCommerce/ScriptLoader.php
+++ src/PaymentGateways/PayPalCommerce/ScriptLoader.php
@@ -188,6 +188,7 @@ EOT;
     /**
      * Load public assets.
      *
+     * @unreleased Get form id from post content if form id is not available.
      * @unreleased Use EnqueueScript to register and enqueue script.
      * @since 2.32.0 Handle exception if client token is not generated.
      * @since 2.9.0
@@ -195,6 +196,10 @@ EOT;
     public function loadPublicAssets()
     {
         $formId = FrontendFormTemplateUtils::getFormId();
+
+        if (! $formId) {
+            $formId = $this->getFormIdFromPostContent();
+        }
 
         if (
             ! $formId
@@ -281,5 +286,57 @@ EOT;
     private function getPartnerJsUrl()
     {
         return 'https://www.paypal.com/webapps/merchantboarding/js/lib/lightbox/partner.js';
+    }
+
+    /**
+     * This function should return form id from page, post content.
+     *
+     *
+     * @unreleased
+     */
+    private function getFormIdFromPostContent(): ?int
+    {
+        global $post;
+        $formId = null;
+
+        if (!$post) {
+            return null;
+        }
+
+
+        // Donation form can be in page, post content as shortcode.
+        $has_shortcode = $post && has_shortcode($post->post_content, 'give_form');
+        if ($has_shortcode) {
+            $pattern = get_shortcode_regex(['give_form']);
+
+            if (
+                preg_match('/' . $pattern . '/s', $post->post_content, $matches)
+                && array_key_exists(2, $matches)
+                && 'give_form' === $matches[2]
+            ) {
+                $attributes = shortcode_parse_atts($matches[3]);
+
+                if (array_key_exists('id', $attributes)) {
+                    $formId = $attributes['id'];
+                }
+            }
+        }
+
+        // Donation form can be in page, post content as Donation Form block.
+        if (!$formId) {
+            $blocks = parse_blocks($post->post_content);
+
+            foreach ($blocks as $block) {
+                if (
+                    $block['blockName'] === 'give/donation-form'
+                    && array_key_exists('id', $block['attrs'])
+                ) {
+                    $formId = $block['attrs']['id'];
+                    break;
+                }
+            }
+        }
+
+        return absint($formId);
     }
 }
