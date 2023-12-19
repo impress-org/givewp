c99e07a84ef766f41f831adf7b930b60e75e893b
Glauber Silva <glauber.silva@givewp.com>
2023-12-19 11:29:09 -0300

    refactor: move migration logic to the PerFormGateways class

--- src/FormMigration/Steps/PaymentGateways.php
+++ src/FormMigration/Steps/PaymentGateways.php
@@ -10,7 +10,6 @@ use Give\FormMigration\Contracts\FormMigrationStep;
 class PaymentGateways extends FormMigrationStep
 {
     /**
-     * @unreleased Add Per Form Gateways settings to this step
      * @since 3.0.0
      */
     public function process()
@@ -27,24 +26,5 @@ class PaymentGateways extends FormMigrationStep
         foreach ($attributes as $key => $value) {
             $paymentGatewaysBlock->setAttribute($key, $value);
         }
-
-        $perFormGateways = give()->form_meta->get_meta($this->formV2->id, '_give_per_form_gateways', true);
-        $compatibleGateways = array_keys(give_get_enabled_payment_gateways(0, 3));
-        $gateways = [];
-        if (is_array($perFormGateways) && count($perFormGateways) > 0) {
-
-            foreach ($perFormGateways as $key => $checked) {
-                if (in_array($key, $compatibleGateways)) {
-                $gateways[] = [
-                    'key' => $key,
-                    'label' => give_get_gateway_checkout_label($key),
-                    'checked' => (bool)$checked,
-                ];
-                }
-            }
-        }
-
-        $paymentGatewaysBlock->setAttribute('useDefaultGateways', count($gateways) === 0);
-        $paymentGatewaysBlock->setAttribute('perFormGateways', $gateways);
     }
 }
