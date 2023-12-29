45d550879264e374b6a13b9909ca962bf4289575
Paulo Iankoski <me@wpdev.com.br>
2023-12-28 22:35:39 -0300

    feature: clear action scheduler upon deletion

--- src/Multisite/Actions/DeleteSiteTables.php
+++ src/Multisite/Actions/DeleteSiteTables.php
@@ -15,5 +15,10 @@ class DeleteSiteTables
         $giveTables = DB::get_col("SHOW TABLES LIKE '{$prefix}'");
 
         DB::query('DROP TABLE ' . implode(',', $giveTables));
+
+        wp_clear_scheduled_hook('give_daily_scheduled_events');
+        wp_clear_scheduled_hook('give_weekly_scheduled_events');
+        wp_clear_scheduled_hook('give_daily_cron');
+        wp_clear_scheduled_hook('give_weekly_cron');
     }
 }
