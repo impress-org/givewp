78be4079f1976a168446de7d5364219f2f655c7a
Glauber Silva <glauber.silva@givewp.com>
2024-01-02 09:39:45 -0300

    refactor: use FormMetaDecorator instead of local methods

--- src/FormMigration/Steps/FundsAndDesignations.php
+++ src/FormMigration/Steps/FundsAndDesignations.php
@@ -15,10 +15,7 @@ class FundsAndDesignations extends FormMigrationStep
      */
     public function canHandle(): bool
     {
-        $fundsAndDesignationsAttributes = $this->getFundsAndDesignationsAttributes($this->formV2->id);
-
-        return count($fundsAndDesignationsAttributes['fund']) !== 0 ||
-               count($fundsAndDesignationsAttributes['options']) !== 0;
+        return $this->formV2->hasFunds() || $this->formV2->hasFundOptions();
     }
 
     /**
@@ -26,7 +23,7 @@ class FundsAndDesignations extends FormMigrationStep
      */
     public function process()
     {
-        $fundsAndDesignationsAttributes = $this->getFundsAndDesignationsAttributes($this->formV2->id);
+        $fundsAndDesignationsAttributes = $this->formV2->getFundsAndDesignationsAttributes();
 
         if (count($fundsAndDesignationsAttributes['fund']) === 0 &&
             count($fundsAndDesignationsAttributes['options']) === 0) {
@@ -40,70 +37,5 @@ class FundsAndDesignations extends FormMigrationStep
         $this->fieldBlocks->insertAfter('givewp/donation-amount', $fundsAndDesignationsBlock);
     }
 
-    /**
-     * @unreleased
-     */
-    private function getFundsAndDesignationsAttributes(int $formId): array
-    {
-        $label = give_get_meta($formId, 'give_funds_label', true);
-        $isAdminChoice = 'admin_choice' === give_get_meta($formId, 'give_funds_form_choice', true);
-        $adminChoice = give_get_meta($formId, 'give_funds_admin_choice', true);
-        $donorOptions = give_get_meta($formId, 'give_funds_donor_choice', true);
 
-
-        $options = [];
-        foreach ($donorOptions as $fundId) {
-            $options[] = [
-                'value' => $fundId,
-                'label' => $this->getFundLabel($fundId),
-                'checked' => $isAdminChoice ? $fundId === $adminChoice : true,
-                'isDefault' => $this->isDefaultFund($fundId),
-            ];
-        }
-
-        return [
-            'label' => $label,
-            'fund' => $isAdminChoice ? [
-                'value' => $adminChoice,
-                'label' => $this->getFundLabel($adminChoice),
-                'checked' => true,
-                'isDefault' => $this->isDefaultFund($adminChoice),
-            ] : $options[0],
-            'options' => $options,
-        ];
-    }
-
-    /**
-     * @unreleased
-     */
-    private function getFundLabel(int $fundId): string
-    {
-        global $wpdb;
-
-        $fund = $wpdb->get_row(
-            $wpdb->prepare("SELECT * FROM {$wpdb->give_funds} WHERE id = %d", $fundId)
-        );
-
-        if ( ! $fund) {
-            return '';
-        }
-
-        return $fund->title;
-    }
-
-    /**
-     * @unreleased
-     */
-    private function isDefaultFund(int $fundId): bool
-    {
-        global $wpdb;
-
-        $fund = $wpdb->get_row("SELECT id FROM {$wpdb->give_funds} WHERE is_default = 1");
-
-        if ( ! $fund) {
-            return false;
-        }
-
-        return $fund->id === $fundId;
-    }
 }
