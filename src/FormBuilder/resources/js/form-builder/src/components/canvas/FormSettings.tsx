e44f5683ac19baf68dbe9e8d9f044ae976a49eac
Paulo Iankoski <me@wpdev.com.br>
2024-01-09 12:31:10 -0300

    refactor: extract routes object and validate for duplicated paths

--- src/FormBuilder/resources/js/form-builder/src/components/canvas/FormSettings.tsx
+++ src/FormBuilder/resources/js/form-builder/src/components/canvas/FormSettings.tsx
@@ -1,57 +1,63 @@
-import React, {useMemo} from 'react';
-import FormSettingsContainer from '@givewp/form-builder/components/canvas/FormSettingsContainer';
-import FormGeneralSettingsGroup from '@givewp/form-builder/settings/group-general';
-import FormDonationConfirmationSettingsGroup from '@givewp/form-builder/settings/group-donation-confirmation';
-import {__} from '@wordpress/i18n';
-import EmailGeneralSettings from '@givewp/form-builder/settings/group-email-settings/general';
-import getEmailSettings from '@givewp/form-builder/settings/group-email-settings';
+import React from "react";
+import FormSettingsContainer from "@givewp/form-builder/components/canvas/FormSettingsContainer";
+import FormGeneralSettingsGroup from "@givewp/form-builder/settings/group-general";
+import FormDonationConfirmationSettingsGroup from "@givewp/form-builder/settings/group-donation-confirmation";
+import { __ } from "@wordpress/i18n";
+import EmailGeneralSettings from "@givewp/form-builder/settings/group-email-settings/general";
+import getEmailSettings from "@givewp/form-builder/settings/group-email-settings";
+
+/**
+ * @unreleased
+ */
+const routes: Route[] = [
+    {
+        name: __('General', 'give'),
+        path: 'general',
+        element: FormGeneralSettingsGroup,
+    },
+    {
+        name: __('Donation Confirmation', 'give'),
+        path: 'donation-confirmation',
+        element: FormDonationConfirmationSettingsGroup,
+    },
+    {
+        name: __('Email Settings', 'give'),
+        path: 'email-settings',
+        element: null,
+        childRoutes: [
+            {
+                name: __('General', 'give'),
+                path: 'email-settings/general',
+                element: EmailGeneralSettings,
+            },
+            ...getEmailSettings(),
+        ],
+    },
+    ...wp.hooks.applyFilters('givewp_form_builder_settings_additional_routes', []),
+];
 
 /**
  * @unreleased
  */
 export default function FormSettings() {
-    const routes: Route[] = useMemo(() => {
-        const additionalSettings = validateRoutes(
-            wp.hooks.applyFilters('givewp_form_builder_settings_additional_routes', [])
-        );
-
-        return [
-            {
-                name: __('General', 'give'),
-                path: 'general',
-                element: FormGeneralSettingsGroup,
-            },
-            {
-                name: __('Donation Confirmation', 'give'),
-                path: 'donation-confirmation',
-                element: FormDonationConfirmationSettingsGroup,
-            },
-            {
-                name: __('Email Settings', 'give'),
-                path: 'email-settings',
-                element: null,
-                childRoutes: [
-                    {
-                        name: __('General', 'give'),
-                        path: 'email-settings/general',
-                        element: EmailGeneralSettings,
-                    },
-                    ...getEmailSettings(),
-                ],
-            },
-            ...additionalSettings,
-        ];
-    }, []);
-
-    return <FormSettingsContainer routes={routes} />;
+    return <FormSettingsContainer routes={validateRoutes(routes)} />;
 }
 
 /**
  * @unreleased
  */
 function validateRoutes(routes: Route[]): Route[] {
+    const paths = [];
+
     return routes.filter((route) => {
         let isValid = isValidRoute(route);
+
+        if (paths.includes(route.path)) {
+            console.error(`${__('Duplicate path found in FormSettings routes:', 'give')} ${route.path}`);
+            isValid = false;
+        } else {
+            paths.push(route.path);
+        }
 
         if (route.childRoutes) {
             if (Array.isArray(route.childRoutes)) {
