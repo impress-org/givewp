2d5f2eb8b1c9bc068ce72ba2d78d7f17c20214a4
Paulo Iankoski <me@wpdev.com.br>
2024-01-18 19:02:30 -0300

    fix: confirm $gateways is an array before searching on it

--- src/PaymentGateways/Gateways/PayPalStandard/Migrations/SetPayPalStandardGatewayId.php
+++ src/PaymentGateways/Gateways/PayPalStandard/Migrations/SetPayPalStandardGatewayId.php
@@ -26,32 +26,30 @@ class SetPayPalStandardGatewayId extends Migration
         $give_settings = give_get_settings();
         $gateways = $give_settings['gateways'];
         $updateSettings = false;
-        if ( $gateways ) {
 
-            if (array_key_exists('paypal-standard', $gateways)) {
-                unset($gateways['paypal-standard']);
-                $gateways['paypal'] = '1';
-                $give_settings['gateways'] = $gateways;
-    
+        if (is_array($gateways) && array_key_exists('paypal-standard', $gateways)) {
+            unset($gateways['paypal-standard']);
+            $gateways['paypal'] = '1';
+            $give_settings['gateways'] = $gateways;
+
+            $updateSettings = true;
+        }
+
+        // Reset paypal gateway custom label.
+        if (isset($give_settings['gateways_label'])) {
+            $gateways_label = $give_settings['gateways_label'];
+            if (array_key_exists('paypal-standard', $gateways_label)) {
+                $gateways_label['paypal'] = $gateways_label['paypal-standard'];
+                unset($gateways_label['paypal-standard']);
+                $give_settings['gateways_label'] = $gateways_label;
                 $updateSettings = true;
             }
-    
-            // Reset paypal gateway custom label.
-            if (isset($give_settings['gateways_label'])) {
-                $gateways_label = $give_settings['gateways_label'];
-                if (array_key_exists('paypal-standard', $gateways_label)) {
-                    $gateways_label['paypal'] = $gateways_label['paypal-standard'];
-                    unset($gateways_label['paypal-standard']);
-                    $give_settings['gateways_label'] = $gateways_label;
-                    $updateSettings = true;
-                }
-            }
-    
-            // Set paypal standard as default payment gateway.
-            if ('paypal-standard' === $give_settings['default_gateway']) {
-                $give_settings['default_gateway'] = 'paypal';
-                $updateSettings = true;
-            }
+        }
+
+        // Set paypal standard as default payment gateway.
+        if ('paypal-standard' === $give_settings['default_gateway']) {
+            $give_settings['default_gateway'] = 'paypal';
+            $updateSettings = true;
         }
 
         if ($updateSettings) {
