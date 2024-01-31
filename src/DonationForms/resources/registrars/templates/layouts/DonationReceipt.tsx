2b8a6e9b038d23d519f382a4e63acc1fbc59a2ea
Jon Waldstein <jonwaldstein@jons-air.mynetworksettings.com>
2024-01-31 16:53:15 -0500

    docs: update since tag

--- src/DonationForms/resources/registrars/templates/layouts/DonationReceipt.tsx
+++ src/DonationForms/resources/registrars/templates/layouts/DonationReceipt.tsx
@@ -17,6 +17,7 @@ const SecureBadge = () => {
 
 /**
  *
+ * @unreleased updated to render value using Interweave
  * @since 3.0.0
  */
 const Details = ({id, heading, details}: {id: string; heading: string; details: ReceiptDetail[]}) =>
