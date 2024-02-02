35b1286ffa65f926b2729b9bcc607dfd1e4eff3d
Kyle B. Johnson <kjohnson@users.noreply.github.com>
2024-02-02 21:15:30 +0000

    wip

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 REPO_FOLDER="/workspaces/$RepositoryName"
 
+echo $PWD
 echo $REPO_FOLDER
 echo $CODESPACE_NAME
 
@@ -11,15 +12,15 @@ echo $CODESPACE_NAME
 # service apache2 start
 
 # WordPress Core install
-cd /var/www/html
-wp core download
-wp config create --dbname=wordpress --dbuser=wordpress --dbpass=wordpress --dbhost=db
-LINE_NUMBER=`grep -n -o 'stop editing!' wp-config.php | cut -d ':' -f 1`
-sed -i "${LINE_NUMBER}r ../.devcontainer/wp-config-addendum.txt" wp-config.php && sed -i -e "s/CODESPACE_NAME/$CODESPACE_NAME/g"  wp-config.php
-wp core install --url=https://$(CODESPACE_NAME) --title=WordPress --admin_user=admin --admin_password=admin --admin_email=mail@example.com
+# cd /var/www/html
+# wp core download
+# wp config create --dbname=wordpress --dbuser=wordpress --dbpass=wordpress --dbhost=db
+# LINE_NUMBER=`grep -n -o 'stop editing!' wp-config.php | cut -d ':' -f 1`
+# sed -i "${LINE_NUMBER}r ../.devcontainer/wp-config-addendum.txt" wp-config.php && sed -i -e "s/CODESPACE_NAME/$CODESPACE_NAME/g"  wp-config.php
+# wp core install --url=https://$(CODESPACE_NAME) --title=WordPress --admin_user=admin --admin_password=admin --admin_email=mail@example.com
 
 # Selected plugins
-wp plugin delete akismet
+# wp plugin delete akismet
 # wp plugin install show-current-template --activate
 # wp plugin activate wp-codespace
 
@@ -27,8 +28,8 @@ wp plugin delete akismet
 # echo xdebug.log_level=0 | sudo tee -a /usr/local/etc/php/conf.d/xdebug.ini
 
 # install dependencies
-cd $REPO_FOLDER
-npm install
+# cd $REPO_FOLDER
+# npm install
 # composer install
 
 # Setup local plugin
@@ -39,6 +40,3 @@ npm install
 # echo export PATH=\"\$PATH:$REPO_FOLDER/vendor/bin:$REPO_FOLDER/node_modules/.bin/\" >> ~/.bashrc
 # echo "cd $REPO_FOLDER/wordpress" >> ~/.bashrc
 # source ~/.bashrc
-
-# VIM
-apt install vim
