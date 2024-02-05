641e4bd4fe5e989dcea897549d9ec392b98d32b6
Kyle B. Johnson <kyle.johnson@hey.com>
2024-02-05 13:18:02 -0500

    chore: Update Event model factory

--- src/EventTickets/Factories/EventFactory.php
+++ src/EventTickets/Factories/EventFactory.php
@@ -2,6 +2,7 @@
 
 namespace Give\EventTickets\Factories;
 
+use DateTime;
 use Give\DonationForms\Models\DonationForm;
 use Give\Framework\Models\Factories\ModelFactory;
 
@@ -12,23 +13,16 @@ class EventFactory extends ModelFactory
      */
     public function definition(): array
     {
-        $title = $this->faker->words(3, true);
-        $description = $this->faker->paragraph();
         $start_datetime = $this->faker->dateTimeThisYear('+6 months');
 
         return [
-            'id' => wp_insert_post([
-                'post_type' => 'give_event',
-                'post_title' => $title,
-                'post_content' => '[]', // Empty block editor content for potential event page
-                'post_excerpt' => $description,
-                'post_status' => 'publish',
-            ]),
-            'title' => $title,
-            'description' => $description,
+            'title' => $this->faker->words(3, true),
+            'description' => $this->faker->paragraph(),
             'start_datetime' => $start_datetime,
             'end_datetime' => $start_datetime->modify('+1 hour'),
             'ticket_close_datetime' => $start_datetime->modify('-1 day'),
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
         ];
     }
 }
