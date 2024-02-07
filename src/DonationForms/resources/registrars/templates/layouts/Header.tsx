bb6b8f6e1e1d9ca272b4bae3755f00599781e897
Joshua Dinh <josh@joshuadinh.tech>
2024-02-07 10:36:03 -0800

    Refactor: remove header bkg image wrapper

--- src/DonationForms/resources/registrars/templates/layouts/Header.tsx
+++ src/DonationForms/resources/registrars/templates/layouts/Header.tsx
@@ -1,24 +1,9 @@
 import type {HeaderProps} from '@givewp/forms/propTypes';
 
 /**
- * @unreleased add background style wrapper
  * @since 3.0.0
  */
 export default function Header({HeaderImage, Title, Description, Goal}: HeaderProps) {
-    const {designSettingsImageStyle} = window.givewp.form.hooks.useDonationFormSettings();
-
-    if (designSettingsImageStyle === 'background') {
-        return (
-            <>
-                <div className={'givewp-layouts givewp-design-settings-image--background'}>
-                    <Title />
-                    <Description />
-                </div>
-                <Goal />
-            </>
-        );
-    }
-
     return (
         <>
             <Title />
