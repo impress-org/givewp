03888c1af519dc022dea7bdfaaa55faec9bc8095
Joshua Dinh <josh@joshuadinh.tech>
2024-02-07 16:23:17 -0800

    refactor: update header template

--- src/DonationForms/resources/registrars/templates/layouts/Header.tsx
+++ src/DonationForms/resources/registrars/templates/layouts/Header.tsx
@@ -4,6 +4,20 @@ import type {HeaderProps} from '@givewp/forms/propTypes';
  * @since 3.0.0
  */
 export default function Header({HeaderImage, Title, Description, Goal}: HeaderProps) {
+    const {designSettingsImageStyle} = window.givewp.form.hooks.useDonationFormSettings();
+
+    if (designSettingsImageStyle === 'background') {
+        return (
+            <>
+                <div className={'givewp-multistep-header-background'}>
+                    <Title />
+                    <Description />
+                </div>
+                <Goal />
+            </>
+        );
+    }
+
     return (
         <>
             <Title />
