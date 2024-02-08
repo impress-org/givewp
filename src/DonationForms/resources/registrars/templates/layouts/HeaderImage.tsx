e92c2d1d68d9ec8e7f7ad8761bd9f0f96a335195
Joshua Dinh <josh@joshuadinh.tech>
2024-02-07 16:25:33 -0800

    fix: add HeaderImageprops.

--- src/DonationForms/resources/registrars/templates/layouts/HeaderImage.tsx
+++ src/DonationForms/resources/registrars/templates/layouts/HeaderImage.tsx
@@ -1,7 +1,9 @@
+import {HeaderImageProps} from '@givewp/forms/propTypes';
+
 /**
  * @unreleased
  */
-export default function HeaderImage({style, image, alt}) {
+export default function HeaderImage({style, image, alt}: HeaderImageProps) {
     return (
         <div className={`givewp-layouts givewp-design-settings-image--${style}`}>
             <img src={image} alt={alt} />
