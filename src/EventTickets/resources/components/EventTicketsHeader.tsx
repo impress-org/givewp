79777484eda4078e0ca1a092780c80b6f3f91e17
Paulo Iankoski <me@wpdev.com.br>
2024-02-09 13:58:32 -0300

    refactor: replace string parsing with separated assignments

--- src/EventTickets/resources/components/EventTicketsHeader.tsx
+++ src/EventTickets/resources/components/EventTicketsHeader.tsx
@@ -2,7 +2,8 @@ import moment from 'moment';
 
 export default function EventTicketsHeader({title, date}) {
     const fullDate = moment(date).format('dddd, MMMM Do, h:mma z');
-    const [day, month] = moment(date).format('DD MMM').split(' ');
+    const day = moment(date).format('DD');
+    const month = moment(date).format('MMM');
 
     return (
         <div className={'givewp-event-tickets__header'}>
