f1e088e7f5ad7b3d0db01a1745f22a249aa6d156
Joshua Dinh <josh@joshuadinh.tech>
2024-02-17 16:35:10 -0800

    feature: pass HeaderImageProp types

--- src/DonationForms/resources/registrars/templates/layouts/HeaderImage.tsx
+++ src/DonationForms/resources/registrars/templates/layouts/HeaderImage.tsx
@@ -1,6 +1,8 @@
+import {HeaderImageProps} from '@givewp/forms/propTypes';
+
 /**
  * @unreleased
  */
-export default function HeaderImage({url, alt}) {
+export default function HeaderImage({url, alt}: HeaderImageProps) {
     return <img src={url} alt={alt} />;
 }
