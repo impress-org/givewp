1a7039a98f81bdaa59a90e8457901ae77a328de3
Paulo Iankoski <me@wpdev.com.br>
2024-02-19 19:04:46 -0300

    tests: add tests to EventsListTable endpoint

---
+++ tests/Unit/EventTickets/Endpoints/TestListEvents.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Give\Tests\Unit\EventTickets\Endpoints;
+
+use Exception;
+use Give\EventTickets\Endpoints\ListEvents;
+use Give\EventTickets\ListTable\EventTicketsListTable;
+use Give\EventTickets\Models\Event;
+use Give\Tests\TestCase;
+use Give\Tests\TestTraits\RefreshDatabase;
+use WP_REST_Request;
+use WP_REST_Server;
+
+class TestListEvents extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @unreleased
+     *
+     * @throws Exception
+     */
+    public function testShouldReturnListWithSameSize(): void
+    {
+        $events = Event::factory()->count(5)->create();
+
+        $mockRequest = $this->getMockRequest();
+        // set_params
+        $mockRequest->set_param('page', 1);
+        $mockRequest->set_param('perPage', 30);
+        $mockRequest->set_param('locale', 'us-US');
+
+        $listDonations = give(ListEvents::class);
+
+        $response = $listDonations->handleRequest($mockRequest);
+
+        $this->assertSameSize($events, $response->data['items']);
+    }
+
+    /**
+     * @unreleased
+     *
+     * @throws Exception
+     */
+    public function testShouldReturnListWithSameData(): void
+    {
+        $events = Event::factory()->count(5)->create();
+        $sortDirection = ['asc', 'desc'][round(rand(0, 1))];
+        $mockRequest = $this->getMockRequest();
+        // set_params
+        $mockRequest->set_param('page', 1);
+        $mockRequest->set_param('perPage', 30);
+        $mockRequest->set_param('locale', 'us-US');
+        $mockRequest->set_param('sortColumn', 'id');
+        $mockRequest->set_param('sortDirection', $sortDirection);
+
+        $expectedItems = $this->getMockColumns($events, $sortDirection);
+
+        $listDonations = give(ListEvents::class);
+
+        $response = $listDonations->handleRequest($mockRequest);
+
+        $this->assertSame($expectedItems, $response->data['items']);
+    }
+
+    /**
+     * @unreleased
+     */
+    public function getMockRequest(): WP_REST_Request
+    {
+        return new WP_REST_Request(
+            WP_REST_Server::READABLE,
+            '/wp/v2/admin/events'
+        );
+    }
+
+    /**
+     * @unreleased
+     */
+    public function getMockColumns(array $events, string $sortDirection = 'desc'): array
+    {
+        $listTable = new EventTicketsListTable();
+        $columns = $listTable->getColumns();
+
+        $expectedItems = [];
+        foreach ($events as $event) {
+            $expectedItem = [];
+            foreach ($columns as $column) {
+                $expectedItem[$column::getId()] = $column->getCellValue($event);
+            }
+            $expectedItems[] = $expectedItem;
+        }
+
+        if ($sortDirection === 'desc') {
+            $expectedItems = array_reverse($expectedItems);
+        }
+
+        return $expectedItems;
+    }
+}
+
