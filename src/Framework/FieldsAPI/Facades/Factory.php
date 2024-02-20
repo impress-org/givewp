781f779739a9e9066e8368e7c00c63db370f539a
Kyle B. Johnson <kyle.johnson@hey.com>
2024-02-20 16:55:52 -0500

    fix: Restore deleted class

---
+++ src/Framework/FieldsAPI/Facades/Factory.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Give\Framework\FieldsAPI\Facades;
+
+use Give\Framework\Support\Facades\Facade;
+
+class Factory extends Facade
+{
+    protected function getFacadeAccessor()
+    {
+        return \Give\Framework\FieldsAPI\Factory::class;
+    }
+}
