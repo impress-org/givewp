d9ae2215f3c930daebc48c6634017c2292dff35e
Joshua Dinh <josh@joshuadinh.tech>
2024-02-20 13:46:51 -0800

    feature: add HeaderImage templates to MultiStep Header layout

--- src/DonationForms/resources/registrars/templates/layouts/Header.tsx
+++ src/DonationForms/resources/registrars/templates/layouts/Header.tsx
@@ -1,14 +1,79 @@
 import type {HeaderProps} from '@givewp/forms/propTypes';
 
 /**
+ * @unreleased add HeaderImage
  * @since 3.0.0
  */
 export default function Header({HeaderImage, Title, Description, Goal}: HeaderProps) {
+    const {designSettingsImageStyle, designSettingsImageUrl} = window.givewp.form.hooks.useDonationFormSettings();
+
+    if (!designSettingsImageUrl) {
+        return (
+            <div className={`givewp-layouts-header__templates-ms`}>
+                <Title />
+                <Description />
+                <Goal />
+            </div>
+        );
+    }
+
     return (
-        <>
-            <Title />
-            <Description />
-            <Goal />
-        </>
+        <div
+            className={`givewp-layouts-header__templates-ms givewp-layouts-header__templates-ms--${designSettingsImageStyle}`}
+        >
+            <HeaderImageTemplates
+                imagePosition={designSettingsImageStyle}
+                HeaderImage={HeaderImage}
+                Title={Title}
+                Description={Description}
+                Goal={Goal}
+            />
+        </div>
     );
+}
+
+function HeaderImageTemplates({imagePosition, HeaderImage, Title, Description, Goal}) {
+    switch (imagePosition) {
+        case 'background':
+            return (
+                <>
+                    <div className={'givewp-layouts-header__content'}>
+                        <HeaderImage />
+                        <Title />
+                        <Description />
+                    </div>
+                    <div className={'givewp-layouts-header__goal'}>
+                        <Goal />
+                    </div>
+                </>
+            );
+        case 'above':
+            return (
+                <>
+                    <HeaderImage />
+                    <div className={'givewp-layouts-header__content'}>
+                        <Title />
+                        <Description />
+                        <Goal />
+                    </div>
+                </>
+            );
+        case 'center':
+            return (
+                <>
+                    <Title />
+                    <Description />
+                    <HeaderImage />
+                    <Goal />
+                </>
+            );
+        default:
+            return (
+                <>
+                    <Title />
+                    <Description />
+                    <Goal />
+                </>
+            );
+    }
 }
