4e9438fa338aef5de38046e2da161ece1a74a25c
Joshua Dinh <75056371+JoshuaHungDinh@users.noreply.github.com>
2024-02-21 04:41:08 +0900

    Refactor: add HeaderImage to classic template (#7238)

--- src/DonationForms/FormDesigns/ClassicFormDesign/js/layouts/Header.tsx
+++ src/DonationForms/FormDesigns/ClassicFormDesign/js/layouts/Header.tsx
@@ -1,4 +1,5 @@
 import {__} from '@wordpress/i18n';
+import {HeaderProps} from '@givewp/forms/propTypes';
 
 /**
  * @since 3.0.0
@@ -13,19 +14,80 @@ const SecureBadge = () => {
 };
 
 /**
+ * @unreleased add HeaderImage
  * @since 3.0.0
  */
-export default function Header({TextWrapper, Title, Description, Goal}) {
-    return (
-        <>
-            <TextWrapper>
+export default function Header({HeaderImage, Title, Description, Goal}: HeaderProps) {
+    const {designSettingsImageStyle, designSettingsImageUrl} = window.givewp.form.hooks.useDonationFormSettings();
+
+    if (!designSettingsImageUrl) {
+        return (
+            <div className={`givewp-layouts-header__templates`}>
                 <Title />
                 <Description />
-            </TextWrapper>
-            <SecureBadge />
-            <Goal />
-        </>
+                <SecureBadge />
+                <Goal />
+            </div>
+        );
+    }
+
+    return (
+        <div
+            className={`givewp-layouts-header__templates givewp-layouts-header__templates--${designSettingsImageStyle}`}
+        >
+            <HeaderImageTemplates
+                imagePosition={designSettingsImageStyle}
+                HeaderImage={HeaderImage}
+                Title={Title}
+                Description={Description}
+                Goal={Goal}
+            />
+        </div>
     );
 }
 
-
+function HeaderImageTemplates({imagePosition, HeaderImage, Title, Description, Goal}) {
+    switch (imagePosition) {
+        case 'background':
+            return (
+                <>
+                    <HeaderImage />
+                    <Title />
+                    <Description />
+                    <SecureBadge />
+                    <Goal />
+                </>
+            );
+        case 'above':
+            return (
+                <>
+                    <HeaderImage />
+                    <div className={'givewp-layouts-header__content'}>
+                        <Title />
+                        <Description />
+                        <SecureBadge />
+                        <Goal />
+                    </div>
+                </>
+            );
+        case 'center':
+            return (
+                <>
+                    <Title />
+                    <Description />
+                    <SecureBadge />
+                    <HeaderImage />
+                    <Goal />
+                </>
+            );
+        default:
+            return (
+                <>
+                    <Title />
+                    <Description />
+                    <SecureBadge />
+                    <Goal />
+                </>
+            );
+    }
+}
