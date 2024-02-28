bfb45835807c1b12c27a1eb12789c3e65afcc2c1
Glauber Silva <glauber.silva@givewp.com>
2024-02-28 11:16:00 -0300

    Revert "refactor: deactivate legacy version"
    This reverts commit e888595e84c6069ebc23404b9440ac9aa4b54b82.

--- src/Framework/PaymentGateways/Actions/GenerateGatewayRouteUrl.php
+++ src/Framework/PaymentGateways/Actions/GenerateGatewayRouteUrl.php
@@ -28,7 +28,7 @@ class GenerateGatewayRouteUrl
 
         return esc_url_raw(add_query_arg(
             $queryArgs,
-            'https://egoeidq1zb.sharedwithexpose.com/'//home_url()
+            home_url()
         ));
     }
 }
