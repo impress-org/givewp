6f5bb0ab282702150959de3b35ec0b31774a7f49
Paulo Iankoski <me@wpdev.com.br>
2024-02-29 17:17:09 -0300

    refactor: set title for other sections

--- src/EventTickets/resources/admin/components/EventDetailsPage/DonationFormsSection/index.tsx
+++ src/EventTickets/resources/admin/components/EventDetailsPage/DonationFormsSection/index.tsx
@@ -1,7 +1,9 @@
+import {__} from '@wordpress/i18n';
+
 export default function DonationFormsSection() {
     return (
         <section>
-            <h1>Donation Forms Section</h1>
+            <h2>{__('Donation Forms', 'give')}</h2>
             <p>Donation Forms List Table</p>
         </section>
     );
--- src/EventTickets/resources/admin/components/EventDetailsPage/TicketTypesSection/index.tsx
+++ src/EventTickets/resources/admin/components/EventDetailsPage/TicketTypesSection/index.tsx
@@ -1,7 +1,9 @@
+import {__} from '@wordpress/i18n';
+
 export default function TicketTypesSection() {
     return (
         <section>
-            <h1>Ticket Types Section</h1>
+            <h2>{__('Tickets', 'give')}</h2>
             <p>Ticket Types List Table</p>
         </section>
     );
