6522920cf30f49cf164768c60c32cf20c8eaaf0f
Paulo Iankoski <me@wpdev.com.br>
2024-03-02 20:08:05 -0300

    Feature: Add Edit Event Modal to Event section

---
+++ src/EventTickets/resources/admin/components/EventDetailsPage/EventSection/EventSectionRowActions.tsx
@@ -0,0 +1,43 @@
+import {__, sprintf} from '@wordpress/i18n';
+import RowAction from '@givewp/components/ListTable/RowAction';
+import ListTableApi from '@givewp/components/ListTable/api';
+
+const apiSettings = window.GiveEventTicketsDetails;
+const eventTicketsApi = new ListTableApi(apiSettings);
+
+/**
+ * @unreleased
+ */
+export function EventSectionRowActions({item, openEditModal, setUpdateErrors}) {
+    const fetchAndUpdateErrors = async (endpoint, id, method) => {
+        const response = await eventTicketsApi.fetchWithArgs(endpoint, {ids: [id]}, method);
+        setUpdateErrors(response);
+        return response;
+    };
+
+    const deleteItem = async (itemId) => await fetchAndUpdateErrors('/events/list-table', itemId, 'DELETE');
+
+    const confirmModal = async () => {
+        const confirmDelete = confirm(sprintf(__('Really delete event #%d?', 'give'), item.id));
+
+        if (confirmDelete) {
+            if (await deleteItem(item.id)) {
+                window.location.href =
+                    window.GiveEventTicketsDetails.adminUrl + 'edit.php?post_type=give_forms&page=give-event-tickets';
+            }
+        }
+    };
+
+    return (
+        <>
+            <RowAction onClick={openEditModal} displayText={__('Edit', 'give')} />
+            <RowAction
+                onClick={confirmModal}
+                actionId={item.id}
+                displayText={__('Delete', 'give')}
+                hiddenText={item.name}
+                highlight
+            />
+        </>
+    );
+}
--- src/EventTickets/resources/admin/components/EventDetailsPage/EventSection/index.tsx
+++ src/EventTickets/resources/admin/components/EventDetailsPage/EventSection/index.tsx
@@ -1,7 +1,11 @@
 import {__, _x} from '@wordpress/i18n';
 import {format} from 'date-fns';
+import {useState} from 'react';
+import EventFormModal from '../../EventFormModal';
 import locale from '../../../../date-fns-locale';
 import SectionTable from '../SectionTable';
+
+const apiSettings = window.GiveEventTicketsDetails;
 
 /**
  * @unreleased
@@ -12,6 +16,15 @@ export default function EventSection({setUpdateErrors}) {
     const endDateTime = new Date(event.endDateTime.date);
     const dateFormat = _x("MM/dd/yyyy 'at' h:mmaaa", 'Date format for event details page', 'give');
 
+    const [isOpen, setOpen] = useState<boolean>(false);
+    const openModal = () => setOpen(true);
+    const closeModal = (response: ResponseProps | null) => {
+        setOpen(false);
+
+        if (response?.id) {
+            // Update the event details
+        }
+    };
     const tableHeaders = {
         title: __('Event', 'give'),
         description: __('Description', 'give'),
@@ -31,6 +44,25 @@ export default function EventSection({setUpdateErrors}) {
         <section>
             <h2>{__('Event', 'give')}</h2>
             <SectionTable tableHeaders={tableHeaders} data={data} />
+            <EventFormModal
+                isOpen={isOpen}
+                handleClose={closeModal}
+                title={__('Edit your event', 'give')}
+                apiSettings={apiSettings}
+                event={data}
+            />
         </section>
     );
+}
+
+interface ResponseProps {
+    id: string;
+    title: string;
+    description: string;
+    startDateTime: {
+        date: string;
+    };
+    endDateTime: {
+        date: string;
+    };
 }
