c6196408fc7d3a96a9e8bd489dc3d9fbad18aff9
Jon Waldstein <jonwaldstein@jwaldstein-mbp.mynetworksettings.com>
2024-03-04 17:57:39 -0500

    chore: reformat

--- src/DonationForms/resources/registrars/templates/layouts/Header.tsx
+++ src/DonationForms/resources/registrars/templates/layouts/Header.tsx
@@ -6,85 +6,85 @@ import cx from 'classnames';
  * @since 3.0.0
  */
 export default function Header({ HeaderImage, Title, Description, Goal, isMultiStep }: HeaderProps) {
-  const { designSettingsImageStyle, designSettingsImageUrl } = window.givewp.form.hooks.useDonationFormSettings();
+    const { designSettingsImageStyle, designSettingsImageUrl } = window.givewp.form.hooks.useDonationFormSettings();
 
-  if (!designSettingsImageUrl) {
+    if (!designSettingsImageUrl) {
+        return (
+            <div className={
+                cx({
+                        'givewp-layouts-header__templates': !isMultiStep,
+                        'givewp-layouts-header__templates-ms': isMultiStep
+                    }
+                )}>
+                <Title />
+                <Description />
+                <Goal />
+            </div>
+        );
+    }
+
     return (
-      <div className={
-        cx({
-            'givewp-layouts-header__templates': !isMultiStep,
-            'givewp-layouts-header__templates-ms': isMultiStep
-          }
-        )}>
-        <Title />
-        <Description />
-        <Goal />
-      </div>
+        <div
+            className={cx({
+                'givewp-layouts-header__templates': !isMultiStep,
+                [`givewp-layouts-header__templates--${designSettingsImageStyle}`]: !isMultiStep,
+                'givewp-layouts-header__templates-ms': isMultiStep,
+                [`givewp-layouts-header__templates-ms--${designSettingsImageStyle}`]: isMultiStep
+            })}
+        >
+            <HeaderImageTemplates
+                imagePosition={designSettingsImageStyle}
+                HeaderImage={HeaderImage}
+                Title={Title}
+                Description={Description}
+                Goal={Goal}
+            />
+        </div>
     );
-  }
-
-  return (
-    <div
-      className={cx({
-        'givewp-layouts-header__templates': !isMultiStep,
-        [`givewp-layouts-header__templates--${designSettingsImageStyle}`]: !isMultiStep,
-        'givewp-layouts-header__templates-ms': isMultiStep,
-        [`givewp-layouts-header__templates-ms--${designSettingsImageStyle}`]: isMultiStep
-      })}
-    >
-      <HeaderImageTemplates
-        imagePosition={designSettingsImageStyle}
-        HeaderImage={HeaderImage}
-        Title={Title}
-        Description={Description}
-        Goal={Goal}
-      />
-    </div>
-  );
 }
 
 function HeaderImageTemplates({ imagePosition, HeaderImage, Title, Description, Goal }) {
-  switch (imagePosition) {
-    case 'background':
-      return (
-        <>
-          <div className={'givewp-layouts-header__content'}>
-            <HeaderImage />
-            <Title />
-            <Description />
-          </div>
-          <div className={'givewp-layouts-header__goal'}>
-            <Goal />
-          </div>
-        </>
-      );
-    case 'above':
-      return (
-        <>
-          <HeaderImage />
-          <div className={'givewp-layouts-header__content'}>
-            <Title />
-            <Description />
-            <Goal />
-          </div>
-        </>
-      );
-    case 'center':
-      return (
-        <>
-          <Title />
-          <Description />
-          <HeaderImage />
-          <Goal />
-        </>
-      );
-    default:
-      return (
-        <>
-          <Title />
-          <Description />
-          <Goal />
-        </>
-      );
-  }
+    switch (imagePosition) {
+        case 'background':
+            return (
+                <>
+                    <div className={'givewp-layouts-header__content'}>
+                        <HeaderImage />
+                        <Title />
+                        <Description />
+                    </div>
+                    <div className={'givewp-layouts-header__goal'}>
+                        <Goal />
+                    </div>
+                </>
+            );
+        case 'above':
+            return (
+                <>
+                    <HeaderImage />
+                    <div className={'givewp-layouts-header__content'}>
+                        <Title />
+                        <Description />
+                        <Goal />
+                    </div>
+                </>
+            );
+        case 'center':
+            return (
+                <>
+                    <Title />
+                    <Description />
+                    <HeaderImage />
+                    <Goal />
+                </>
+            );
+        default:
+            return (
+                <>
+                    <Title />
+                    <Description />
+                    <Goal />
+                </>
+            );
+    }
 }
