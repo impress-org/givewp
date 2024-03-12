b8307084a091ddbde783b674686ca46ed34e6c1d
Paulo Iankoski <me@wpdev.com.br>
2024-03-12 15:19:29 -0300

    chore: optimize imports

--- src/EventTickets/resources/admin/components/EventDetailsPage/EventSection/EventSectionRowActions.tsx
+++ src/EventTickets/resources/admin/components/EventDetailsPage/EventSection/EventSectionRowActions.tsx
@@ -1,9 +1,5 @@
 import {__} from '@wordpress/i18n';
 import RowAction from '@givewp/components/ListTable/RowAction';
-import EventTicketsApi from '../../api';
-
-const apiSettings = window.GiveEventTicketsDetails;
-const eventTicketsApi = new EventTicketsApi(apiSettings);
 
 /**
  * @unreleased
