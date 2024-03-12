e824b882a823c24e8a9b2bf1804f4c66f31c8e8a
Paulo Iankoski <me@wpdev.com.br>
2024-03-12 15:15:31 -0300

    refactor: remove ability to delete an event from details page

--- src/EventTickets/resources/admin/components/EventDetailsPage/EventSection/EventSectionRowActions.tsx
+++ src/EventTickets/resources/admin/components/EventDetailsPage/EventSection/EventSectionRowActions.tsx
@@ -1,4 +1,4 @@
-import {__, sprintf} from '@wordpress/i18n';
+import {__} from '@wordpress/i18n';
 import RowAction from '@givewp/components/ListTable/RowAction';
 import EventTicketsApi from '../../api';
 
@@ -10,31 +10,8 @@ const eventTicketsApi = new EventTicketsApi(apiSettings);
  */
 export function EventSectionRowActions({event, openEditModal}) {
     return () => {
-        const deleteItem = async (itemId) =>
-            await eventTicketsApi.fetchWithArgs('/events/list-table', {ids: [itemId]}, 'DELETE');
-
-        const confirmModal = async () => {
-            const confirmDelete = confirm(sprintf(__('Really delete event #%d?', 'give'), event.id));
-
-            if (confirmDelete) {
-                if (await deleteItem(event.id)) {
-                    window.location.href =
-                        apiSettings.adminUrl + 'edit.php?post_type=give_forms&page=give-event-tickets';
-                }
-            }
-        };
-
         return (
-            <>
                 <RowAction onClick={openEditModal} displayText={__('Edit', 'give')} />
-                <RowAction
-                    onClick={confirmModal}
-                    actionId={event.id}
-                    displayText={__('Delete', 'give')}
-                    hiddenText={event.title}
-                    highlight
-                />
-            </>
         );
     };
 }
