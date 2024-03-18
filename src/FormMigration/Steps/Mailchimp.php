12eb229518e32009410969da0fd82aef287bb103
Joshua Dinh <josh@joshuadinh.tech>
2024-03-18 13:03:49 -0700

    Refactor: update arrays to include empty strings

--- src/FormMigration/Steps/Mailchimp.php
+++ src/FormMigration/Steps/Mailchimp.php
@@ -25,7 +25,7 @@ class Mailchimp extends FormMigrationStep
     {
         $block = BlockModel::make([
             'name'       => 'givewp/mailchimp',
-            'attributes' => $this->getAttributes()
+            'attributes' => $this->getAttributes(),
         ]);
 
         $this->fieldBlocks->insertAfter('givewp/email', $block);
@@ -36,7 +36,7 @@ class Mailchimp extends FormMigrationStep
      */
     private function getAttributes(): array
     {
-        return   [
+        return [
             'label'            => $this->formV2->getMailchimpLabel() ??
                                   give_get_option('give_mailchimp_label', __('Subscribe to newsletter?')),
             'checked'          => $this->formV2->getMailchimpDefaultChecked() ??
@@ -44,13 +44,13 @@ class Mailchimp extends FormMigrationStep
             'doubleOptIn'      => $this->formV2->getMailchimpDoubleOptIn() ??
                                   give_get_option('give_mailchimp_double_opt_in'),
             'subscriberTags'   => $this->formV2->getMailchimpSubscriberTags() ??
-                                  [],
+                                  [''],
             'sendDonationData' => $this->formV2->getMailchimpSendDonationData() ??
                                   give_get_option('give_mailchimp_donation_data', true),
             'sendFFMData'      => $this->formV2->getMailchimpSendFFMData() ??
                                   give_get_option('give_mailchimp_ffm_pass_field'),
             'defaultAudiences' => $this->formV2->getMailchimpDefaultAudiences() ??
-                                  give_get_option('give_mailchimp_list', []),
+                                  give_get_option('give_mailchimp_list', ['']),
         ];
     }
 }
