80498118d7d78f4c902653f36c024659265fd669
Paulo Iankoski <me@wpdev.com.br>
2024-03-22 18:06:20 -0300

    test: configure Jest

---
+++ tests/resources/jest.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    rootDir: '../../',
+    preset: '@wordpress/jest-preset-default',
+    moduleNameMapper: {
+        '^@givewp/form-builder(.*)$': '<rootDir>/src/FormBuilder/resources/js/form-builder/src$1',
+        '^@givewp/forms/app(.*)$': '<rootDir>/src/DonationForms/resources/app$1',
+    },
+    setupFilesAfterEnv: ['<rootDir>/tests/resources/config/testing-library.js'],
+    testEnvironmentOptions: {
+        url: 'http://localhost/',
+    },
+    testPathIgnorePatterns: [
+        '/.git/',
+        '/node_modules/',
+        '/vendor/',
+        '<rootDir>/.*/assets/dist/',
+        '<rootDir>/.*/build/',
+        '<rootDir>/.+.d.ts$',
+    ],
+    resolver: '<rootDir>/tests/resources/scripts/resolver.js',
+    transform: {
+        '^.+\\.[jt]sx?$': '<rootDir>/tests/resources/scripts/babel-transformer.js',
+    },
+    transformIgnorePatterns: ['/node_modules/(?!(docker-compose|yaml)/)', '\\.pnp\\.[^\\/]+$'],
+    watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
+    reporters: ['default'],
+};
