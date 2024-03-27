c942413a70d68d2e2c84697b16a49b3f590e1a4d
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2024-03-27 22:36:32 +0530

    add: use version command to get latest version of strauss phar

--- bin/strauss-installar.sh
+++ bin/strauss-installar.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
-# Function to check if the .phar file or .txt file does not exist, or if the latest release version is not installed
+# Function to get the installed version of strauss
+get_installed_version() {
+    local version_output
+    version_output=$(php ./bin/strauss.phar --version)
+    echo "$version_output" | sed -n -e 's/^.*strauss //p'
+}
+
+# Function to check if the latest release version is not installed
 is_update_needed() {
     local latest_release=$1
-    [[ ! -f ./bin/strauss.phar || ! -f ./bin/strauss-version.txt || $(cat ./bin/strauss-version.txt) != "$latest_release" ]]
+    local current_version
+
+    if [[ ! -f ./bin/strauss.phar ]]; then
+        return 0
+    fi
+
+    current_version=$(get_installed_version)
+    [[ "$current_version" != "$latest_release" ]]
 }
 
 # Function to download and install the latest release
 download_and_install() {
     local latest_release=$1
+    rm -f ./bin/strauss.phar
     curl -o bin/strauss.phar -L -C - https://github.com/BrianHenryIE/strauss/releases/download/"$latest_release"/strauss.phar
     echo "$latest_release" > ./bin/strauss-version.txt
 }
@@ -16,8 +31,9 @@ download_and_install() {
 # Main script execution
 main() {
     local latest_release
-    latest_release="0.16.0" # strauss release version
+    latest_release="0.17.0" # strauss release version
     if is_update_needed "$latest_release"; then
+        echo "Updating strauss to $latest_release ..."
         download_and_install "$latest_release"
     fi
 }
