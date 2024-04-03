3bd84b6f71e6d5384d7be463bd8c903a159fb344
Glauber Silva <glauber.silva@givewp.com>
2024-04-03 14:44:51 -0300

    refactor: display dial code and use strict mode

--- src/FormBuilder/resources/js/form-builder/src/blocks/fields/phone/Edit.tsx
+++ src/FormBuilder/resources/js/form-builder/src/blocks/fields/phone/Edit.tsx
@@ -20,11 +20,11 @@ export default function Edit({attributes}: BlockEditProps<any>) {
         script.async = true;
         document.body.appendChild(script);
 
-        console.log(IntlTelInput.initialCountry);
-
         const interval = setTimeout(() => {
             // @ts-ignore
             window.intlTelInput(document.querySelector('#' + inputId), {
+                showSelectedDialCode: true,
+                strictMode: true,
                 utilsScript: IntlTelInput.utilsScriptUrl,
                 initialCountry: IntlTelInput.initialCountry,
                 i18n: JSON.parse(IntlTelInput.i18n),
