277a9aa03c2cda8d705005a04fe85d149723bf86
Joshua Dinh <josh@joshuadinh.tech>
2024-04-05 11:48:15 -0700

    feature: add okay button to withButtons

--- src/FormBuilder/resources/js/form-builder/src/components/onboarding/steps/filters/withButtons.tsx
+++ src/FormBuilder/resources/js/form-builder/src/components/onboarding/steps/filters/withButtons.tsx
@@ -4,23 +4,41 @@ const withButtons = (steps) => {
         text: 'Previous',
         type: 'back'
     };
+
     const next = {
         classes: 'shepherd-button-primary',
         text: 'Next',
         type: 'next'
     }
+
     const complete = {
         classes: 'shepherd-button-primary',
         text: 'Got it',
         type: 'complete'
     }
-    return steps.map((step, index) => {
 
+    const Okay = {
+        classes: 'shepherd-button-primary shepherd-button-primary--tools',
+        text: 'Okay',
+        type: 'complete',
+    };
+
+    return steps.map((step, index) => {
         if(index === 0) {
             return {...step, ...{
                 buttons: [next]
             }}
         }
+
+        if (step.id === 'schema-find-tour') {
+            return {
+                ...step,
+                ...{
+                    buttons: [Okay],
+                },
+            };
+        }
+
 
         if(index === steps.length - 1) {
             return {...step, ...{
