5bdb74b7c06a8edfaf33512bdb0aca43698b7b45
Joshua Dinh <josh@joshuadinh.tech>
2024-04-09 13:55:40 -0700

    refactor: remove import

--- src/FormMigration/FormMetaDecorator.php
+++ src/FormMigration/FormMetaDecorator.php
@@ -5,7 +5,6 @@ namespace Give\FormMigration;
 use Give\DonationForms\V2\Models\DonationForm;
 use Give\DonationForms\ValueObjects\GoalType;
 use Give\FormMigration\Contracts\FormModelDecorator;
-use Give\Log\Log;
 use Give\PaymentGateways\Gateways\Stripe\StripePaymentElementGateway\StripePaymentElementGateway;
 use Give_Email_Notification_Util;
 
