00f12b6d1d0bc88c2c765e849283e330d50552e9
Joshua Dinh <josh@joshuadinh.tech>
2024-04-17 15:06:25 -0700

    feature: add descriptions ui

--- src/DonationForms/resources/registrars/templates/fields/Amount/DonationAmountLevels.tsx
+++ src/DonationForms/resources/registrars/templates/fields/Amount/DonationAmountLevels.tsx
@@ -8,9 +8,12 @@ type DonationAmountLevelsProps = {
     currency: string;
     levels: number[];
     onLevelClick?: (amount: number) => void;
+    descriptions: string[];
+    descriptionsEnabled: boolean;
 };
 
 /**
+ * @unreleased add level descriptions.
  * @since 3.0.0
  */
 export default function DonationAmountLevels({
@@ -18,29 +21,43 @@ export default function DonationAmountLevels({
     currency,
     levels,
     onLevelClick,
+    descriptions,
+    descriptionsEnabled,
 }: DonationAmountLevelsProps) {
     const {useWatch, useCurrencyFormatter} = window.givewp.form.hooks;
     const amount = useWatch({name});
     const formatter = useCurrencyFormatter(currency);
 
     return (
-        <div className="givewp-fields-amount__levels-container">
+        <div className={'givewp-fields-amount__levels-container'}>
             {levels.map((levelAmount, index) => {
                 const label = formatter.format(levelAmount);
                 const selected = levelAmount === amount;
+                const hasDescription = descriptionsEnabled && descriptions[index] !== '';
+
                 return (
-                    <button
-                        className={classNames('givewp-fields-amount__level', {
-                            'givewp-fields-amount__level--selected': selected,
+                    <div
+                        className={classNames('givewp-fields-amount__level-container', {
+                            'givewp-fields-amount__level-container--col': hasDescription,
                         })}
-                        type="button"
-                        onClick={() => {
-                            onLevelClick(levelAmount);
-                        }}
-                        key={index}
                     >
-                        {label}
-                    </button>
+                        <button
+                            className={classNames('givewp-fields-amount__level', {
+                                'givewp-fields-amount__level--selected': selected,
+                                'givewp-fields-amount__level--description': !hasDescription,
+                            })}
+                            type="button"
+                            onClick={() => {
+                                onLevelClick(levelAmount);
+                            }}
+                            key={index}
+                        >
+                            {label}
+                        </button>
+                        {hasDescription && (
+                            <span className={'givewp-fields-amount__level__description'}>{descriptions[index]}</span>
+                        )}
+                    </div>
                 );
             })}
         </div>
