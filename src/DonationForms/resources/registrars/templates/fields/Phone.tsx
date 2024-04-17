ed12a8edef73e664e3e2842abf7a7cda3502e754
Glauber Silva <glauber.silva@givewp.com>
2024-04-17 12:18:49 -0300

    refactor: use React component

--- src/DonationForms/resources/registrars/templates/fields/Phone.tsx
+++ src/DonationForms/resources/registrars/templates/fields/Phone.tsx
@@ -1,6 +1,6 @@
 import {PhoneProps} from '@givewp/forms/propTypes';
-import {useEffect, useRef} from 'react';
-import intlTelInput from 'intl-tel-input';
+import {useEffect, useState} from 'react';
+import IntlTelInput from 'intl-tel-input/react';
 import 'intl-tel-input/build/css/intlTelInput.css';
 import InputMask from 'react-input-mask';
 
@@ -21,50 +21,38 @@ export default function Phone({
     const {useFormContext} = window.givewp.form.hooks;
     const {setValue, setError, trigger} = useFormContext();
 
-    let iti = null;
-    const intlTelInputRef = useRef(null);
     const isIntlTelInput = intlTelInputSettings.initialCountry && intlTelInputSettings.utilsScriptUrl;
+    const isFormBuilderPreview = window.location.href.indexOf('about:srcdoc') > -1;
+
+    const intlInitOptions = {
+        initialCountry: intlTelInputSettings.initialCountry,
+        showSelectedDialCode: intlTelInputSettings.showSelectedDialCode,
+        strictMode: intlTelInputSettings.strictMode,
+        i18n: intlTelInputSettings.i18n,
+    };
+
+    /**
+     * Control when the country list appears as a fullscreen popup vs an inline dropdown. By default, it will
+     * appear as a fullscreen popup on mobile devices. However, in the form builder preview it always load the
+     * country list in fullscreen mode, so we need set it to "false" to prevent this behaviour.
+     */
+    if (isFormBuilderPreview) {
+        intlInitOptions['useFullscreenPopup'] = false;
+    }
 
     useEffect(() => {
-        if (!isIntlTelInput) {
-            return;
-        }
-        const isFormBuilderPreview = window.location.href.indexOf('about:srcdoc') > -1;
         const interval = setTimeout(
             () => {
-                const options = {
-                    utilsScript: intlTelInputSettings.utilsScriptUrl,
-                    initialCountry: intlTelInputSettings.initialCountry,
-                    showSelectedDialCode: intlTelInputSettings.showSelectedDialCode,
-                    strictMode: intlTelInputSettings.strictMode,
-                    i18n: intlTelInputSettings.i18n,
-                };
+                window.intlTelInputGlobals.loadUtils(intlTelInputSettings.utilsScriptUrl);
 
-                /**
-                 * Control when the country list appears as a fullscreen popup vs an inline dropdown. By default, it will
-                 * appear as a fullscreen popup on mobile devices. However, in the form builder preview it always load the
-                 * country list in fullscreen mode, so we need set it to "false" to prevent this behaviour.
-                 */
+                // It's necessary to fix a missing padding in the form builder preview
                 if (isFormBuilderPreview) {
-                    options['useFullscreenPopup'] = false;
+                    document.querySelectorAll('.iti__tel-input').forEach(function (input: HTMLInputElement) {
+                        // @ts-ignore
+                        const countryContainerWidth = document.querySelector('.iti__country-container').offsetWidth;
+                        input.style.paddingLeft = String(countryContainerWidth + 4) + 'px';
+                    });
                 }
-
-                iti = intlTelInput(intlTelInputRef.current, options);
-
-                const handleIntlTelInputChange = (event) => {
-                    const number = iti.getNumber();
-                    if (number && !iti.isValidNumber()) {
-                        const errorCode = iti.getValidationError();
-                        setValue(inputProps.name, errorCode);
-                        setError(inputProps.name, {type: 'custom', message: intlTelInputSettings.errorMap[errorCode]});
-                    } else {
-                        setValue(inputProps.name, number);
-                        trigger(inputProps.name, {shouldFocus: false});
-                    }
-                };
-
-                intlTelInputRef.current.addEventListener('change', handleIntlTelInputChange);
-                intlTelInputRef.current.addEventListener('keyup', handleIntlTelInputChange);
             },
             isFormBuilderPreview ? 100 : 0 // It's necessary to properly load the utilsScript in the form builder preview
         );
@@ -74,6 +62,19 @@ export default function Phone({
         };
     }, []);
 
+    const [country, setCountry] = useState<string>(intlTelInputSettings.initialCountry);
+
+    const onChangeNumber = (number: string) => {
+        if (number && !window.intlTelInputUtils.isValidNumber(number, country)) {
+            const errorCode = window.intlTelInputUtils.getValidationError(number, country);
+            setValue(inputProps.name, errorCode);
+            setError(inputProps.name, {type: 'custom', message: intlTelInputSettings.errorMap[errorCode]});
+        } else {
+            setValue(inputProps.name, number);
+            trigger(inputProps.name, {shouldFocus: false});
+        }
+    };
+
     return (
         <label>
             <Label />
@@ -82,7 +83,14 @@ export default function Phone({
             {isIntlTelInput ? (
                 <>
                     <input type={'hidden'} placeholder={placeholder} {...inputProps} />
-                    <input type={'text'} aria-invalid={fieldError ? 'true' : 'false'} ref={intlTelInputRef} />
+                    <IntlTelInput
+                        onChangeCountry={setCountry}
+                        onChangeNumber={onChangeNumber}
+                        initOptions={intlInitOptions}
+                        inputProps={{
+                            'aria-invalid': fieldError ? 'true' : 'false',
+                        }}
+                    />
                 </>
             ) : phoneFormat === 'domestic' ? (
                 <InputMask type={'phone'} {...inputProps} mask={'(999) 999-9999'} placeholder={placeholder} />
