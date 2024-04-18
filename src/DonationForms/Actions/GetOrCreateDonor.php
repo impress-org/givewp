f849eeaa35026ee00193d504fa316e36c03d3972
Glauber Silva <glauber.silva@givewp.com>
2024-04-18 15:23:24 -0300

    fix: missing phone number for existing donors not logged in

--- src/DonationForms/Actions/GetOrCreateDonor.php
+++ src/DonationForms/Actions/GetOrCreateDonor.php
@@ -29,20 +29,21 @@ class GetOrCreateDonor
         // first check if donor exists as a user
         $donor = $userId ? Donor::whereUserId($userId) : null;
 
-        // If they exist as a donor & user then make sure they don't already own this email before adding to their additional emails list..
+        // if they exist as a donor & user then make sure they don't already own this email before adding to their additional emails list..
         if ($donor && !$donor->hasEmail($donorEmail) && !Donor::whereEmail($donorEmail)) {
             $donor->additionalEmails = array_merge($donor->additionalEmails ?? [], [$donorEmail]);
-            $donor->save();
-        }
-
-        if ($donor && empty($donor->phone)) {
-            $donor->phone = $donorPhone;
             $donor->save();
         }
 
         // if donor is not a user than check for any donor matching this email
         if (!$donor) {
             $donor = Donor::whereEmail($donorEmail);
+        }
+
+        // if they exist as a donor & user but don't have a phone number then add it to their profile.
+        if ($donor && empty($donor->phone)) {
+            $donor->phone = $donorPhone;
+            $donor->save();
         }
 
         // if no donor exists then create a new one using their personal information from the form.
