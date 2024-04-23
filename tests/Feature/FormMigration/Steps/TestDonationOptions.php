5162654ba38bc995bdc4a441f0d322f770b81bc2
Paulo Iankoski <me@wpdev.com.br>
2024-04-23 18:20:00 -0300

    Refactor: Change v2 to v3 migration to include level descriptions (#7362)

--- tests/Feature/FormMigration/Steps/TestDonationOptions.php
+++ tests/Feature/FormMigration/Steps/TestDonationOptions.php
@@ -47,6 +47,7 @@ class TestDonationOptions extends TestCase {
     }
 
     /**
+     * @unreleased Updated test to include donation levels with descriptions
      * @since 3.4.0
      */
     public function testProcessShouldUpdateDonationAmountBlockAttributesWithDonationLevels(): void
@@ -67,7 +68,29 @@ class TestDonationOptions extends TestCase {
 
         $block = $payload->formV3->blocks->findByName('givewp/donation-amount');
 
-        $this->assertSame([10.00, 25.00, 50.00, 100.00], $block->getAttribute('levels'));
+        $expectedLevels = [
+            [
+                'value' => 10.00,
+                'label' => 'Small Gift',
+                'checked' => false,
+            ],
+            [
+                'value' => 25.00,
+                'label' => 'Mid-size Gift',
+                'checked' => true,
+            ],
+            [
+                'value' => 50.00,
+                'label' => 'Large Gift',
+                'checked' => false,
+            ],
+            [
+                'value' => 100.00,
+                'label' => 'Big Gift',
+                'checked' => false,
+            ],
+        ];
+        $this->assertSame($expectedLevels, $block->getAttribute('levels'));
         $this->assertTrue($block->getAttribute('customAmount'));
         $this->assertSame((float)$meta['_give_custom_amount_range_minimum'], $block->getAttribute('customAmountMin'));
         $this->assertSame((float)$meta['_give_custom_amount_range_maximum'], $block->getAttribute('customAmountMax'));
