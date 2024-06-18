8a95606a5875c2378e1fff6c84db2a703a1341d4
Glauber Silva <glauber.silva@givewp.com>
2024-06-18 10:44:08 -0300

    refactor: update font size

--- assets/src/js/admin/paypal-commerce/index.js
+++ assets/src/js/admin/paypal-commerce/index.js
@@ -340,7 +340,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
                 // Show confirmation modal.
                 let modalBody =
-                    '<div><input type="checkbox" id="keep_webhooks" name="keep_webhooks" checked /><label for="keep_webhooks">' +
+                    '<div><input type="checkbox" id="keep_webhooks" name="keep_webhooks" checked /><label style="font-size: 14px;" for="keep_webhooks">' +
                     givePayPalCommerce.translations.keepWebhooksAfterDisconnect +
                     '</label></div><br/>' +
                     '<a style="font-size: 13px;font-style: italic;" href="https://docs.givewp.com/pp-donations-webhooks" target="_blank">' +
