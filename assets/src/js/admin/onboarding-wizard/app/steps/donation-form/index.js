293fa5f410d872da39e99ac0952b4ce5590dbf11
Paulo Iankoski <me@wpdev.com.br>
2024-07-15 15:24:27 -0300

    Refactor: Replace carets with bullets in the onboarding wizard (#7445)

--- assets/src/js/admin/onboarding-wizard/app/steps/donation-form/index.js
+++ assets/src/js/admin/onboarding-wizard/app/steps/donation-form/index.js
@@ -1,51 +1,49 @@
 // Import vendor dependencies
-import { __ } from '@wordpress/i18n'
+import {__} from '@wordpress/i18n';
 
 // Import components
 import ContinueButton from '../../../components/continue-button';
 import DonationFormComponent from '../../../components/donation-form';
-import GradientChevronIcon from '../../../components/icons/gradient-chevron';
+import Bullet from '../../../components/icons/bullet';
 
 // Import styles
 import './style.scss';
 
 const DonationForm = () => {
-	return (
-		<div className="give-obw-donation-form">
-			<div className="give-obw-donation-form__preview">
-				<DonationFormComponent />
-			</div>
-			<div className="give-obw-donation-form__content">
-				<div className="give-obw-donation-form__fixed">
-					<h1>{ __( '🎉 Congrats! Check out your first donation form.', 'give' ) }</h1>
-					<p>
-						{ __( 'This form is customized based on your responses.', 'give' ) }
-					</p>
+    return (
+        <div className="give-obw-donation-form">
+            <div className="give-obw-donation-form__preview">
+                <DonationFormComponent />
+            </div>
+            <div className="give-obw-donation-form__content">
+                <div className="give-obw-donation-form__fixed">
+                    <h1>{__('🎉 Congrats! Check out your first donation form.', 'give')}</h1>
+                    <p>{__('This form is customized based on your responses.', 'give')}</p>
 
-					<h2>{ __( 'After setup you can:', 'give' ) }</h2>
-					<ul>
-						<li>
-							<GradientChevronIcon index={ 1 } />
-							{ __( 'Customize the text, color and image', 'give' ) }
-						</li>
-						<li>
-							<GradientChevronIcon index={ 2 } />
-							{ __( 'Modify donation amounts and add a fundraising goal', 'give' ) }
-						</li>
-						<li>
-							<GradientChevronIcon index={ 3 } />
-							{ __( 'Add or remove payment options', 'give' ) }
-						</li>
-						<li>
-							<GradientChevronIcon index={ 4 } />
-							{ __( 'Extend functionality with add-ons and more', 'give' ) }
-						</li>
-					</ul>
-					<ContinueButton testId="preview-continue-button" />
-				</div>
-			</div>
-		</div>
-	);
+                    <h2>{__('After setup you can:', 'give')}</h2>
+                    <ul>
+                        <li>
+                            <Bullet />
+                            {__('Customize the text, color and image', 'give')}
+                        </li>
+                        <li>
+                            <Bullet />
+                            {__('Modify donation amounts and add a fundraising goal', 'give')}
+                        </li>
+                        <li>
+                            <Bullet />
+                            {__('Add or remove payment options', 'give')}
+                        </li>
+                        <li>
+                            <Bullet />
+                            {__('Extend functionality with add-ons and more', 'give')}
+                        </li>
+                    </ul>
+                    <ContinueButton testId="preview-continue-button" />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default DonationForm;
