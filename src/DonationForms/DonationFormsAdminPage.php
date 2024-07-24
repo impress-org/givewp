04265c3aeb44e0bb9a45ebab8aee2b688470efd6
Glauber Silva <glauber.silva@givewp.com>
2024-07-24 09:30:49 -0300

    refactor: simplify logic to replace menu link

--- src/DonationForms/DonationFormsAdminPage.php
+++ src/DonationForms/DonationFormsAdminPage.php
@@ -12,29 +12,9 @@ class DonationFormsAdminPage
      */
     public function addFormSubmenuLink()
     {
-        global $submenu;
-
-        $giveParentPageKey = 'edit.php?post_type=give_forms';
-
-        if ( ! isset($submenu[$giveParentPageKey])) {
-            return;
-        }
-
-        /** @var $giveSubmenu array [0: "Add Form", 1: "edit_give_forms", 2: "post-new.php?post_type=give_forms", ...] */
-        foreach ($submenu[$giveParentPageKey] as $giveSubmenuKey => $giveSubmenu) {
-            if ($this->isLegacyAddFormSubmenuLink($giveSubmenu)) {
-                $submenu[$giveParentPageKey][$giveSubmenuKey][2] = 'edit.php?post_type=give_forms&page=givewp-form-builder';
-            }
-        }
-    }
-
-    /**
-     * @unreleased
-     *
-     * @param $giveSubmenu array [0: "Add Form", 1: "edit_give_forms", 2: "post-new.php?post_type=give_forms", ...]
-     */
-    private function isLegacyAddFormSubmenuLink(array $giveSubmenu): bool
-    {
-        return isset($giveSubmenu[2]) && 'post-new.php?post_type=give_forms' === $giveSubmenu[2];
+        remove_submenu_page('edit.php?post_type=give_forms', 'post-new.php?post_type=give_forms');
+        add_submenu_page('edit.php?post_type=give_forms', __('Add Form', 'give'), __('Add Form', 'give'),
+            'edit_give_forms',
+            'edit.php?post_type=give_forms&page=givewp-form-builder', '', 1);
     }
 }
