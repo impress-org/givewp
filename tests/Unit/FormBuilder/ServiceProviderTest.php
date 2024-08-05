3963501a1ca204015c8bee802a4357b659202638
Kyle B. Johnson <kyle.johnson@hey.com>
2024-08-05 13:48:15 -0400

    test: Add test for dismissable notice

---
+++ tests/Unit/FormBuilder/ServiceProviderTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Give\Tests\Unit\FormBuilder;
+
+use Give\Tests\TestCase;
+use Give\Tests\TestTraits\RefreshDatabase;
+
+class ServiceProviderTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testItDismissesTheAdditionalPaymentGatewaysNotice() {
+
+        $userId = $this->factory()->user->create();
+        wp_set_current_user($userId);
+
+        do_action('wp_ajax_givewp_additional_payment_gateways_hide_notice');
+
+        $this->assertTrue(
+            (bool) get_user_meta($userId, 'givewp-additional-payment-gateways-notice-dismissed', true)
+        );
+    }
+}
