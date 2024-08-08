307b12f4ca3cf82e149cad130dbd06cfc2eceb15
Jon Waldstein <jonwaldstein@jwaldstein-mbp.mynetworksettings.com>
2024-08-08 11:42:24 -0400

    refactor: more updates to use createRoot

--- src/Subscriptions/resources/admin-subscriptions.tsx
+++ src/Subscriptions/resources/admin-subscriptions.tsx
@@ -2,10 +2,12 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import SubscriptionsListTable from './components/SubscriptionsListTable';
 
-const root = createRoot(document.getElementById('give-admin-subscriptions-root'));
+const element = document.getElementById('give-admin-subscriptions-root');
 
-root.render(
-    <StrictMode>
-        <SubscriptionsListTable />
-    </StrictMode>
-);
+if (element) {
+    createRoot(element).render(
+        <StrictMode>
+            <SubscriptionsListTable />
+        </StrictMode>
+    );
+}
