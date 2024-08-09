eeac8232916d12c0532bfc9ac1681307a311c869
David GABISON <david.gabison@outlook.com>
2024-08-09 13:52:52 +0200

    test: add test for ListDonations custom filter

--- tests/Unit/Donations/Endpoints/TestListDonations.php
+++ tests/Unit/Donations/Endpoints/TestListDonations.php
@@ -101,6 +101,63 @@ class TestListDonations extends TestCase
     }
 
     /**
+     * @unreleased
+     *
+     * @return void
+     * @throws Exception
+     */
+    public function testShouldAllowAddingFilters()
+    {
+        $donations = Donation::factory()->count(5)->create();
+
+        $expectedItems = array_slice($donations, 0, 2);
+        foreach ($expectedItems as $item) {
+            give_update_payment_meta($item->id, 'my_key', 'on');
+        }
+        $expectedItems = $this->getMockColumns($expectedItems);
+
+        add_filter('give_list-donation_api_args', function ($args) {
+            $args['my_param'] = [
+                'type' => 'string',
+                'required' => false,
+                'sanitize_callback' => 'sanitize_text_field',
+            ];
+            return $args;
+        });
+
+        add_filter('give_list-donation_where_conditions', function ($value, $endpoint) {
+            list($query, $dependencies) = $value;
+
+            $paramValue = $endpoint->request->get_param('my_param');
+            if (!empty($paramValue)) {
+                $query->attachMeta(
+                    'give_donationmeta',
+                    'ID',
+                    'donation_id',
+                    ['my_key', 'myKey']
+                );
+                $query->where('give_donationmeta_attach_meta_myKey.meta_value', $paramValue);
+            }
+
+            return [$query, $dependencies];
+        }, 10, 2);
+
+        $mockRequest = $this->getMockRequest();
+        // set_params
+        $mockRequest->set_param('page', 1);
+        $mockRequest->set_param('perPage', 30);
+        $mockRequest->set_param('locale', 'us-US');
+        $mockRequest->set_param('testMode', give_is_test_mode());
+        $mockRequest->set_param('my_param', 'on');
+
+        $listDonations = give(ListDonations::class);
+
+        $response = $listDonations->handleRequest($mockRequest);
+
+        $this->assertSame($expectedItems, $response->data['items']);
+    }
+
+    /**
      *
      * @since 2.25.0
      */
