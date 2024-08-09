22ad1b97d3217f94fbe4a7d4817e7171ec570f91
Jon Waldstein <jonwaldstein@jwaldstein-mbp.mynetworksettings.com>
2024-08-09 12:39:40 -0400

    refactor: rename and use filter

--- src/DonationForms/Actions/AddHoneyPotFieldToDonationForms.php
+++ src/DonationForms/Actions/AddHoneyPotFieldToDonationForms.php
@@ -3,32 +3,36 @@
 namespace Give\DonationForms\Actions;
 
 use Give\DonationForms\Rules\HoneyPotRule;
-use Give\FormAPI\Form\Text;
-use Give\Framework\FieldsAPI\Date;
 use Give\Framework\FieldsAPI\DonationForm;
 use Give\Framework\FieldsAPI\Exceptions\EmptyNameException;
+use Give\Framework\FieldsAPI\Text;
 
 /**
  * @unreleased
  */
-class AddHoneyPotFieldToDonationForms {
+class AddHoneyPotFieldToDonationForms
+{
     /**
      * @unreleased
      * @throws EmptyNameException
      */
     public function __invoke(DonationForm $form)
     {
+        if (!apply_filters('givewp_donation_forms_honeypot_enabled', true)) {
+            return;
+        }
+
         $formNodes = $form->all();
         $lastSection = $form->count() ? $formNodes[$form->count() - 1] : null;
 
-        $field = Date::make('birthday')
-            ->label('Birthday')
-            ->scope('honeypot')
-            ->showInAdmin(false)
-            ->showInReceipt(false)
-            ->rules(new HoneyPotRule());
-
         if ($lastSection) {
+            $field = Text::make('givewp-birthday')
+                ->label('Birthday')
+                ->scope('honeypot')
+                ->showInAdmin(false)
+                ->showInReceipt(false)
+                ->rules(new HoneyPotRule());
+
             $lastSection->append($field);
         }
     }
--- tests/Unit/DonationForms/Actions/TestAddHoneyPotFieldToDonationForms.php
+++ tests/Unit/DonationForms/Actions/TestAddHoneyPotFieldToDonationForms.php
@@ -3,7 +3,7 @@
 namespace Give\Tests\Unit\DonationForms\Actions;
 
 use Give\DonationForms\Actions\AddHoneyPotFieldToDonationForms;
-use Give\Framework\FieldsAPI\Date;
+use Give\Framework\FieldsAPI\Text;
 use Give\Framework\FieldsAPI\DonationForm;
 use Give\Framework\FieldsAPI\Exceptions\EmptyNameException;
 use Give\Framework\FieldsAPI\Exceptions\NameCollisionException;
@@ -19,7 +19,8 @@ class TestAddHoneyPotFieldToDonationForms extends TestCase {
      * @unreleased
      * @throws NameCollisionException|EmptyNameException|TypeNotSupported
      */
-    public function testShouldAddHoneyPotFieldToDonationForms() {
+    public function testShouldAddHoneyPotFieldToDonationForms(): void
+    {
         $formNode = new DonationForm('donation-form');
         $formNode->append(Section::make('section-1'), Section::make('section-2'), Section::make('section-3'));
         $action = new AddHoneyPotFieldToDonationForms();
@@ -28,7 +29,23 @@ class TestAddHoneyPotFieldToDonationForms extends TestCase {
 
         /** @var Section $lastSection */
         $lastSection = $formNode->getNodeByName('section-3');
-        $this->assertNotNull($lastSection->getNodeByName('birthday'));
-        $this->assertInstanceOf(Date::class, $lastSection->getNodeByName('birthday'));
+        $this->assertNotNull($lastSection->getNodeByName('givewp-birthday'));
+        $this->assertInstanceOf(Text::class, $lastSection->getNodeByName('givewp-birthday'));
+    }
+
+    /**
+     * @unreleased
+     * @throws NameCollisionException|EmptyNameException|TypeNotSupported
+     */
+    public function testShouldNotAddHoneyPotFieldToDonationFormsIfUsingFilter(): void
+    {
+        add_filter('givewp_donation_forms_honeypot_enabled', '__return_false');
+
+        $formNode = new DonationForm('donation-form');
+        $formNode->append(Section::make('section-1'), Section::make('section-2'), Section::make('section-3'));
+        $action = new AddHoneyPotFieldToDonationForms();
+        $action($formNode);
+
+        $this->assertNull($formNode->getNodeByName('givewp-birthday'));
     }
 }
