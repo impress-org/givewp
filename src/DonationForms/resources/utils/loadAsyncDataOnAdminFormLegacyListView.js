4eeed32822f3f16916a1de8402040ac8ae0ee26c
Glauber Silva <glauber.silva@givewp.com>
2024-08-12 14:35:21 -0300

    feature: abort requests before unload page

--- src/DonationForms/resources/utils/loadAsyncDataOnAdminFormLegacyListView.js
+++ src/DonationForms/resources/utils/loadAsyncDataOnAdminFormLegacyListView.js
@@ -1,16 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    // Determine if an element is in the visible viewport
-    function isInViewport(element) {
-        /*const rect = element.getBoundingClientRect();
-        const html = document.documentElement;
-        return (
-            rect.top >= 0 &&
-            rect.left >= 0 &&
-            rect.bottom <= (window.innerHeight || html.clientHeight) &&
-            rect.right <= (window.innerWidth || html.clientWidth)
-        );*/
+    let abortLoadAsyncData = false;
 
+    function isInViewport(element) {
         const { top, bottom } = element.getBoundingClientRect();
         const vHeight = (window.innerHeight || document.documentElement.clientHeight);
 
@@ -20,20 +12,14 @@ document.addEventListener('DOMContentLoaded', () => {
         );
     }
 
-    const loadFormData = async (formId) => {
-        const response = await fetch(ajaxurl + '?action=givewp_get_form_async_data_for_list_view&formId=' + formId);
-        return await response.json()
-    }
+    const fetchFormData = ( formId, listViewItemElement, donationsElement, earningsElement, amountRaisedElement = null, progressBarElement = null) => {
 
-    const fetchFormData = (item) => {
-        item.classList.add('list-view-async-data');
+        listViewItemElement.classList.add('list-view-async-data-loaded');
 
-        const formId = item.id.split('post-')[1];
-        const columnGoal = item.querySelector('.column-goal');
-        const columnDonations = item.querySelector('.column-donations');
-        const columnEarnings = item.querySelector('.column-earnings');
+        const controller = new AbortController();
+        const signal = controller.signal;
 
-        fetch(ajaxurl + '?action=givewp_get_form_async_data_for_list_view&formId=' + formId).
+        fetch(ajaxurl + '?action=givewp_get_form_async_data_for_list_view&formId=' + formId, { signal }).
         then(function(response){
             return response.json();
         })
@@ -41,95 +27,59 @@ document.addEventListener('DOMContentLoaded', () => {
             console.log('response: ', response)
 
             if (response.success) {
-                const giveGoalTextElement = columnGoal.querySelector(".give-goal-text");
-                const progressBarElement = columnGoal.querySelector(".give-admin-progress-bar");
-                if (!!giveGoalTextElement && !!progressBarElement){
+                donationsElement.querySelector('a').innerHTML = response.data.donationsCount;
+                earningsElement.querySelector('a').innerHTML = response.data.earnings;
+
+                if (!!amountRaisedElement && !!progressBarElement){
+                    amountRaisedElement.querySelector('span').innerHTML = response.data.amountRaised;
                     progressBarElement.querySelector('span').style.width = response.data.percentComplete + '%';
-                    giveGoalTextElement.querySelector('span').innerHTML = response.data.amountRaised;
                 }
-                columnDonations.querySelector('a').innerHTML = response.data.donationsCount;
-                columnEarnings.querySelector('a').innerHTML = response.data.earnings;
             }
+        }) .catch(error => {
+            console.log('error: ', error);
+        }).finally(() => {
+            console.log('finally!');
+        });
+
+        addEventListener("beforeunload", (event) => {
+            abortLoadAsyncData = true;
+            controller.abort('Async request aborted due to exit page.');
         });
     }
 
     const maybeLoadAsyncData = () => {
-        const legacyAdminAllFormsListViewItems = document.querySelectorAll('.type-give_forms:not(.list-view-async-data)');
-        console.log('legacyAdminAllFormsListViewItems.length: ', legacyAdminAllFormsListViewItems.length);
-        if (legacyAdminAllFormsListViewItems.length > 0) {
-            legacyAdminAllFormsListViewItems.forEach((item) => {
 
-                if (!item.hasAttribute('id') || !item.id.includes('post-')) {
+        if (abortLoadAsyncData) {
+            console.log('abortLoadAsyncData');
+            return;
+        }
+
+        const legacyAdminAllFormsListViewItems = document.querySelectorAll('.type-give_forms:not(.list-view-async-data-loaded)');
+        if (legacyAdminAllFormsListViewItems.length > 0) {
+            legacyAdminAllFormsListViewItems.forEach((listViewItemElement) => {
+
+                if (!listViewItemElement.hasAttribute('id') || !listViewItemElement.id.includes('post-')) {
                     return;
                 }
 
-                //const formId = item.id.split('post-')[1];
-                //const columnGoal = item.querySelector('.column-goal');
-                //const columnDonations = item.querySelector('.column-donations');
-                //const columnEarnings = item.querySelector('.column-earnings')
+                const formId = listViewItemElement.id.split('post-')[1];
+                const donationsElement = listViewItemElement.querySelector('.column-donations');
+                const earningsElement = listViewItemElement.querySelector('.column-earnings');
 
-                if (isInViewport(item)) {
-                    console.log('item: ', item);
-                    fetchFormData(item);
-                    //fetchFormData(formId, columnGoal, columnDonations, columnEarnings);
+                const goalElement = listViewItemElement.querySelector('.column-goal');
+                const amountRaisedElement = goalElement.querySelector(".give-goal-text");
+                const progressBarElement = goalElement.querySelector(".give-admin-progress-bar");
+
+                if (isInViewport(listViewItemElement)) {
+                    console.log('item: ', listViewItemElement);
+                    fetchFormData(formId, listViewItemElement, donationsElement, earningsElement, amountRaisedElement, progressBarElement);
                 }
-
-                /*loadFormData(formId).then(function(response){
-                    console.log('response: ', response)
-                    if (response.success) {
-                        const giveGoalTextElement = columnGoal.querySelector(".give-goal-text");
-                        const progressBarElement = columnGoal.querySelector(".give-admin-progress-bar");
-                        if (!!giveGoalTextElement && !!progressBarElement){
-                            progressBarElement.querySelector('span').style.width = response.data.percentComplete + '%';
-                            giveGoalTextElement.querySelector('span').innerHTML = response.data.amountRaised;
-                        }
-
-                        columnDonations.querySelector('a').innerHTML = response.data.donationsCount;
-                        columnEarnings.querySelector('a').innerHTML = response.data.earnings;
-                    }
-                });*/
-
-
-
-                //if (!!giveGoalTextElement && !!progressBarElement){
-
-                    //const formId = giveGoalTextElement.getAttribute("data-form-id");
-                    //console.log('formId: ', formId);
-
-                    //progressBarElement.querySelector('span').style.width = '100%';
-                    //giveGoalTextElement.querySelector('span').innerHTML = formId;
-
-                    /*loadFormData(formId).then(function(response){
-                        console.log('response: ', response)
-                        if (response.success) {
-                            //alert('before update');
-                            progressBar.querySelector('span').style.width = response.data.percentComplete + '%';
-                            giveGoalTextElement.querySelector('span').innerHTML = response.data.amountRaised;
-                        }
-                    });*/
-
-
-                   /* fetch(ajaxurl + '?action=givewp_get_form_async_data_for_list_view&formId=' + formId).
-                    then(function(response){
-                        return response.json();
-                    })
-                    .then(function(response){
-                        console.log('response: ', response)
-
-                        if (response.success) {
-                            //alert('before update');
-                            progressBar.querySelector('span').style.width = response.data.percentComplete + '%';
-                            giveGoalTextElement.querySelector('span').innerHTML = response.data.amountRaised;
-                        }
-                    });*/
-                //}
             });
         }
     }
 
     maybeLoadAsyncData();
 
-    // If scrolling near bottom of page, load more async data
     window.addEventListener('scroll', () => {
         if (
             window.scrollY + window.innerHeight >= document.body.offsetHeight - 1000
