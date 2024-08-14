89f4f62f81e7348adcafaa07de714bea47ab07eb
Kyle B. Johnson <kjohnson@users.noreply.github.com>
2024-08-14 12:11:09 -0400

    Feature: Add akismet support to v3 forms (#7474)

---
+++ src/DonationSpam/Exceptions/SpamDonationException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Give\DonationSpam\Exceptions;
+
+use Give\Framework\Exceptions\Primitives\Exception;
+
+/**
+ * @unreleased
+ */
+class SpamDonationException extends Exception {}
