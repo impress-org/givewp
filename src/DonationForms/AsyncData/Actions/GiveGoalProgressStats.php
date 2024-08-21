882da69f1cce0ad6562ab4d31aa409bd1ad2b85f
Glauber Silva <glauber.silva@givewp.com>
2024-08-21 13:53:57 -0300

    refactor: remove unused function

--- src/DonationForms/AsyncData/Actions/GiveGoalProgressStats.php
+++ src/DonationForms/AsyncData/Actions/GiveGoalProgressStats.php
@@ -13,16 +13,6 @@ class GiveGoalProgressStats
     /**
      * @unreleased
      */
-    public function maybeChangeAmountRaisedOutput($amountRaisedCachedValue, $formId)
-    {
-        // Only use cached values on form list views
-        if ( ! $this->isSingleForm() && ! wp_doing_ajax() && AdminFormListViewOptions::useCachedMetaKeys()) {
-            return $amountRaisedCachedValue;
-        }
-
-        return AsyncDataHelpers::getFormRevenueValue($formId);
-    }
-
     public function maybeChangeGoalProgressStatsActualValue($stats)
     {
         if (false !== strpos($stats['actual'], 'give-skeleton')) {
