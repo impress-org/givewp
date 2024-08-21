3a9a50293b236d843bf538d18bea3921b179e6ad
Kyle B. Johnson <kyle.johnson@hey.com>
2024-08-21 13:33:09 -0400

    chore: Add Campaigns service provider

---
+++ src/Campaigns/ServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Give\Campaigns;
+
+use Give\Helpers\Hooks;
+use Give\ServiceProviders\ServiceProvider as ServiceProviderInterface;
+
+/**
+ * @unreleased
+ */
+class ServiceProvider implements ServiceProviderInterface
+{
+    /**
+     * @unreleased
+     * @inheritDoc
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * @unreleased
+     * @inheritDoc
+     */
+    public function boot(): void
+    {
+        // Hooks::addAction('init', Actions\MyAction::class);
+        // Hooks::addAction('rest_api_init', Controllers\MyEndpoint::class);
+    }
+}
