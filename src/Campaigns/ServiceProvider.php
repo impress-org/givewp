a56ea7b597466aa24b9fe6c49d5ab4e82e68c67a
Glauber Silva <glauber.silva@givewp.com>
2024-08-22 17:00:28 -0300

    Feature: add campaigns menu item and initial empty page (#7511)

--- src/Campaigns/ServiceProvider.php
+++ src/Campaigns/ServiceProvider.php
@@ -27,5 +27,15 @@ class ServiceProvider implements ServiceProviderInterface
     {
         // Hooks::addAction('init', Actions\MyAction::class);
         // Hooks::addAction('rest_api_init', Controllers\MyEndpoint::class);
+
+        $this->registerMenus();
+    }
+
+    /**
+     * @unreleased
+     */
+    private function registerMenus()
+    {
+        Hooks::addAction('admin_menu', CampaignsAdminPage::class, 'addCampaignsSubmenuPage', 999);
     }
 }
