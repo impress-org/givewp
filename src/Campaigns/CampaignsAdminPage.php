5b4fc4a6c6372048f452d49d99729d4d25425640
Glauber Silva <glauber.silva@givewp.com>
2024-08-22 14:46:55 -0300

    feature: add campaigns menu and initial empty page

---
+++ src/Campaigns/CampaignsAdminPage.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Give\Campaigns;
+
+/**
+ * @unreleased
+ */
+class CampaignsAdminPage
+{
+    /**
+     * @unreleased
+     */
+    public function addCampaignsSubmenuPage()
+    {
+        add_submenu_page(
+            'edit.php?post_type=give_forms',
+            esc_html__('Campaigns', 'give'),
+            esc_html__('Campaigns', 'give'),
+            'edit_give_forms',
+            'campaigns',
+            [$this, 'renderCampaignsPage'],
+            0
+        );
+    }
+
+    /**
+     * @unreleased
+     */
+    public function renderCampaignsPage()
+    {
+        echo '<div id="give-admin-campaigns-root"><p style="padding: 200px 30px">The campaigns list table will be loaded here...</p></div>';
+    }
+}
