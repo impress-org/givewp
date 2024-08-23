d19b894f73123267f1f8a69b88cb8b6f1bd18c71
Jon Waldstein <jon@givewp.com>
2024-08-23 14:55:59 -0400

    Feature: donation confirmation page redirect (#7439)
    Co-authored-by: Jon Waldstein <jonwaldstein@jwaldstein-mbp.mynetworksettings.com>

--- src/DonationForms/Listeners/TemporarilyReplaceLegacySuccessPageUri.php
+++ src/DonationForms/Listeners/TemporarilyReplaceLegacySuccessPageUri.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Give\DonationForms\Listeners;
 
 use Give\DonationForms\DataTransferObjects\DonateControllerData;
@@ -15,6 +16,7 @@ class TemporarilyReplaceLegacySuccessPageUri
      *
      * This is a temporary solution until we can update the gateway api to support the new receipt urls.
      *
+     * @unreleased Added "givewp_enable_donation_confirmation_page_redirect" filter
      * @since 3.0.0
      *
      * @return void
@@ -22,6 +24,12 @@ class TemporarilyReplaceLegacySuccessPageUri
     public function __invoke(DonateControllerData $formData, Donation $donation)
     {
         $filteredUrl = $formData->getDonationConfirmationReceiptViewRouteUrl($donation);
+        $redirectUrl = $formData->getDonationConfirmationPageFromSettings($donation);
+        $form = $formData->getDonationForm();
+
+        if (apply_filters('givewp_donation_confirmation_page_redirect_enabled', $form->settings->enableReceiptConfirmationPage, $donation->formId)) {
+            $filteredUrl = $redirectUrl;
+        }
 
         add_filter('give_get_success_page_uri', static function ($url) use ($filteredUrl) {
             return $filteredUrl;
