efe696de5c52ffbdc3c819ad4cd5612574316099
Kyle B. Johnson <kyle.johnson@hey.com>
2024-08-23 14:17:25 -0400

    chore: Add docblocks

--- src/Campaigns/Models/Campaign.php
+++ src/Campaigns/Models/Campaign.php
@@ -5,15 +5,22 @@ namespace Give\Campaigns\Models;
 use Give\Framework\Models\Contracts\ModelCrud;
 use Give\Framework\Models\Model;
 
+/**
+ * @unreleased
+ */
 class Campaign extends Model implements ModelCrud
 {
     /**
+     * @unreleased
      * @inheritdoc
      */
     protected $properties = [
         'title' => 'string',
     ];
 
+    /**
+     * @unreleased
+     */
     public static function find($id)
     {
         // TODO: Implement find() method.
@@ -31,21 +38,33 @@ class Campaign extends Model implements ModelCrud
         return $campaign;
     }
 
+    /**
+     * @unreleased
+     */
     public function save()
     {
         // TODO: Implement save() method.
     }
 
+    /**
+     * @unreleased
+     */
     public function delete()
     {
         // TODO: Implement delete() method.
     }
 
+    /**
+     * @unreleased
+     */
     public static function query()
     {
         // TODO: Implement query() method.
     }
 
+    /**
+     * @unreleased
+     */
     public static function fromQueryBuilderObject($object)
     {
         // TODO: Implement fromQueryBuilderObject() method.
