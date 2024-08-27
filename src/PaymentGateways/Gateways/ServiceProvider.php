725ec34f3f847750e15490a0d128145f6cb0f41a
Kyle B. Johnson <kjohnson@users.noreply.github.com>
2024-08-27 16:07:30 -0400

    Chore: Disable Stripe legacy spam validation

--- src/PaymentGateways/Gateways/ServiceProvider.php
+++ src/PaymentGateways/Gateways/ServiceProvider.php
@@ -120,7 +120,6 @@ class ServiceProvider implements ServiceProviderInterface
 
     /**
      * @since 3.0.0
-     * @since 3.16.0 Add validation of card info.
      */
     private function addLegacyStripeAdapter()
     {
@@ -129,7 +128,6 @@ class ServiceProvider implements ServiceProviderInterface
 
         $legacyStripeAdapter->addDonationDetails();
         $legacyStripeAdapter->loadLegacyStripeWebhooksAndFilters();
-        $legacyStripeAdapter->validateCardInformation();
     }
 
     /**
