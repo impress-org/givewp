949a4b4c76fbf7f5cd32aa36c61df0c80df7e5af
alaca <ante.laca@gmail.com>
2024-08-27 17:35:22 +0200

    feature: add campaign model test

---
+++ tests/Unit/Campaigns/Models/CampaignModelTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Give\Tests\Unit\Campaigns\Models;
+
+use Give\Campaigns\Models\Campaign;
+use Give\Tests\TestCase;
+use Give\Tests\TestTraits\RefreshDatabase;
+
+/**
+ * @unreleased
+ */
+final class CampaignModelTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @unreleased
+     */
+    public function testFindShouldReturnCampaign()
+    {
+        $mockCampaign = Campaign::factory()->create();
+
+        $campaign = Campaign::find($mockCampaign->id);
+
+        $this->assertInstanceOf(Campaign::class, $campaign);
+        $this->assertSame($mockCampaign->id, $campaign->id);
+        $this->assertEquals($campaign, $mockCampaign);
+    }
+}
