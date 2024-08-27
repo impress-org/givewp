051613398fd6116025de71ebaa5a6cd7f0b26443
alaca <ante.laca@gmail.com>
2024-08-27 12:35:07 +0200

    feature: register table names

--- src/Campaigns/ServiceProvider.php
+++ src/Campaigns/ServiceProvider.php
@@ -19,7 +19,7 @@ class ServiceProvider implements ServiceProviderInterface
      */
     public function register(): void
     {
-        //
+        $this->registerTableNames();
     }
 
     /**
@@ -47,6 +47,18 @@ class ServiceProvider implements ServiceProviderInterface
                 CreateCampaignFormsTable::class,
             ]
         );
+    }
+
+
+    /**
+     * @unreleased
+     */
+    private function registerTableNames(): void
+    {
+        global $wpdb;
+
+        $wpdb->give_campaigns = $wpdb->prefix . 'give_campaigns';
+        $wpdb->give_campaign_forms = $wpdb->prefix . 'give_campaign_forms';
     }
 
     /**
