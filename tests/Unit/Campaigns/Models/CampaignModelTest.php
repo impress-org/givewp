571a6e2b40c6dbaf21098c8f90e185a547255561
Kyle B. Johnson <kyle.johnson@hey.com>
2024-09-03 12:22:03 -0400

    test: Add test for one-to-many campaign forms

--- tests/Unit/Campaigns/Models/CampaignModelTest.php
+++ tests/Unit/Campaigns/Models/CampaignModelTest.php
@@ -3,6 +3,8 @@
 namespace Give\Tests\Unit\Campaigns\Models;
 
 use Give\Campaigns\Models\Campaign;
+use Give\DonationForms\Models\DonationForm;
+use Give\Framework\Database\DB;
 use Give\Tests\TestCase;
 use Give\Tests\TestTraits\RefreshDatabase;
 
@@ -23,5 +25,21 @@ final class CampaignModelTest extends TestCase
 
         $this->assertInstanceOf(Campaign::class, $campaign);
         $this->assertEquals($mockCampaign->toArray(), $campaign->toArray());
+    }
+
+    /**
+     * @unreleased
+     */
+    public function testCampaignHasManyForms()
+    {
+        $campaign = Campaign::factory()->create();
+        $form1 = DonationForm::factory()->create();
+        $form2 = DonationForm::factory()->create();
+
+        $db = DB::table('give_campaign_forms');
+        $db->insert(['form_id' => $form1->id, 'campaign_id' => $campaign->id]);
+        $db->insert(['form_id' => $form2->id, 'campaign_id' => $campaign->id]);
+
+        $this->assertEquals(2, $campaign->forms()->count());
     }
 }
