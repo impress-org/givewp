a1e038f26d15f1a9c5fcae07950cdd317569f18e
Jon Waldstein <jonwaldstein@jwaldstein-mbp.mynetworksettings.com>
2024-09-13 11:26:21 -0400

    docs: add since tag

--- src/DonationForms/resources/registrars/templates/elements/Html.tsx
+++ src/DonationForms/resources/registrars/templates/elements/Html.tsx
@@ -1,7 +1,10 @@
 import type {HtmlProps} from '@givewp/forms/propTypes';
 import {Interweave} from 'interweave';
 
+/**
+ * @unreleased updated to use interweave
+ * @since 3.0.0
+ */
 export default function Html({html}: HtmlProps) {
     return <Interweave tagName="div" content={html} />;
-
 }
