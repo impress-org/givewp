d362f6f69afe0305a59fe6da75ee94c004dfda86
Glauber Silva <glauber.silva@givewp.com>
2024-09-20 17:41:55 -0300

    refactor: simplify UpdateCampaign logic

--- src/Campaigns/Routes/UpdateCampaign.php
+++ src/Campaigns/Routes/UpdateCampaign.php
@@ -2,10 +2,10 @@
 
 namespace Give\Campaigns\Routes;
 
+use Exception;
 use Give\API\RestRoute;
 use Give\Campaigns\Models\Campaign;
 use Give\Campaigns\Routes\Traits\RestResponses;
-use Give\Framework\Exceptions\Primitives\Exception;
 use WP_REST_Request;
 use WP_REST_Response;
 
@@ -18,6 +18,9 @@ class UpdateCampaign implements RestRoute
 
     /** @var string */
     protected $endpoint = 'campaigns/(?P<id>[0-9]+)';
+
+    /** @var Campaign */
+    protected $campaign;
 
     public function registerRoute()
     {
@@ -37,6 +40,9 @@ class UpdateCampaign implements RestRoute
                         'type' => 'string',
                         'required' => true,
                         'sanitize_callback' => 'sanitize_text_field',
+                        'validate_callback' => function ($id) {
+                            return $this->campaign = Campaign::find($id);
+                        },
                     ],
                     'title' => [
                         'type' => 'string',
@@ -50,26 +56,22 @@ class UpdateCampaign implements RestRoute
 
     /**
      * @unreleased
+     *
+     * @throws Exception
      */
     public function handleRequest(WP_REST_Request $request): WP_REST_Response
     {
-        $campaignId = $request->get_param('id');
-        $campaign = Campaign::find($campaignId);
-
-        if ( ! $campaign) {
-            return $this->notFoundResponse(esc_html__(sprintf('Campaign %s not found.', $campaignId), 'give'));
+        foreach ($request->get_params() as $key => $value) {
+            if ('id' !== $key) {
+                $this->campaign->setAttribute($key, $value);
+            }
         }
 
-        try {
-            $title = $request->get_param('title');
-            $campaign->title = $title ? $request->get_param('title') : $campaign->title;
-
-            $campaign->save();
-
-            $response = json_encode($campaign->toArray());
-        } catch (Exception $e) {
-            return $this->badRequestResponse($e);
+        if ($this->campaign->isDirty()) {
+            $this->campaign->save();
         }
+
+        $response = json_encode($this->campaign->toArray());
 
         return new WP_REST_Response($response);
     }
