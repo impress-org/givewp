d2ac14cd9d42a961b0868f23f4436d118309242d
alaca <ante.laca@gmail.com>
2024-09-23 16:15:48 +0200

    refactor: remove console log

--- src/Campaigns/resources/admin/components/CampaignDetailsPage/index.tsx
+++ src/Campaigns/resources/admin/components/CampaignDetailsPage/index.tsx
@@ -36,8 +36,6 @@ export default function CampaignsDetailsPage({campaignId}) {
         }
     }, [hasResolved]);
 
-    console.log(formState.isDirty);
-
     const onSubmit: SubmitHandler<Campaign> = async (data, e) => {
         e.preventDefault();
 
