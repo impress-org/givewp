5e37ca936625761bb8ad4f16cb9679d769b78600
alaca <ante.laca@gmail.com>
2024-09-23 13:58:58 +0200

    refactor: use react hook form

--- src/Campaigns/resources/admin/components/CampaignDetailsPage/Tabs/Settings.tsx
+++ src/Campaigns/resources/admin/components/CampaignDetailsPage/Tabs/Settings.tsx
@@ -1,46 +1,33 @@
 import {__} from '@wordpress/i18n';
+import {useFormContext} from 'react-hook-form';
+
 import styles from '../style.module.scss';
-import {useEntityRecord} from '@wordpress/core-data';
-import {useEffect} from 'react';
-
-
-type FormInputs = {
-    title: string;
-    longDescription: string;
-    image: string;
-    goalType: string;
-    goal: number;
-};
 
 /**
  * @unreleased
  */
-export default ({campaignId}) => {
+export default () => {
 
-    const {record, hasResolved, edit} = useEntityRecord('givewp', 'campaign', campaignId);
-
-
-    useEffect(() => {
-        edit({
-            title: 'ajme'
-        })
-    }, []);
+    const {register, formState: {errors}} = useFormContext();
 
     return (
-        <form>
-            <div className={styles.sections}>
-                <div className={styles.section}>
-                    <div>
-                        <h2>
-                            {__('Campaign Details', 'give')}
-                        </h2>
-                        {__('This includes the campaign title, description, and the cover of your campaign.', 'give')}
-                    </div>
-                    <div>
-                        R
-                    </div>
+        <div className={styles.sections}>
+            <div className={styles.section}>
+                <div>
+                    <h2>
+                        {__('Campaign Details', 'give')}
+                    </h2>
+                    {__('This includes the campaign title, description, and the cover of your campaign.', 'give')}
+                </div>
+                <div>
+                    <input {...register('title')} />
+
+                    {errors.title && (
+                        // @ts-ignore
+                        <div className="error-message">{errors.title.message}</div>
+                    )}
                 </div>
             </div>
-        </form>
+        </div>
     );
 }
