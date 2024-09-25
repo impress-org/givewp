155924a532ecd3fb53af0963a83a119c81012b2c
Kyle B. Johnson <kjohnson@users.noreply.github.com>
2024-09-25 11:42:35 -0400

    Feature: Add endpoint for Campaign Overview Statistics (#7545)

---
+++ src/Campaigns/Routes/CampaignOverviewStatistics.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Give\Campaigns\Routes;
+
+use DateInterval;
+use DatePeriod;
+use DateTimeImmutable;
+use Exception;
+use Give\API\RestRoute;
+use Give\Campaigns\CampaignDonationQuery;
+use Give\Campaigns\Models\Campaign;
+use Give\Framework\Support\Facades\DateTime\Temporal;
+use WP_REST_Response;
+use WP_REST_Server;
+
+/**
+ * @unreleased
+ */
+class CampaignOverviewStatistics implements RestRoute
+{
+    /** @var string */
+    protected $endpoint = 'campaign-overview-statistics';
+
+    /**
+     * @unreleased
+     */
+    public function registerRoute()
+    {
+        register_rest_route(
+            'give-api/v2',
+            $this->endpoint,
+            [
+                [
+                    'methods' => WP_REST_Server::READABLE,
+                    'callback' => [$this, 'handleRequest'],
+                    'permission_callback' => function () {
+                        return current_user_can('manage_options');
+                    },
+                ],
+                'args' => [
+                    'campaignId' => [
+                        'type' => 'integer',
+                        'required' => true,
+                        'sanitize_callback' => 'absint',
+                        'validate_callback' => 'is_numeric',
+                    ],
+                    'rangeInDays' => [
+                        'type' => 'integer',
+                        'required' => false,
+                        'sanitize_callback' => 'absint',
+                        'default' => 0, // Zero to mean "all time".
+                        'validate_callback' => 'is_numeric',
+                    ],
+                ],
+            ]
+        );
+    }
+
+    /**
+     * @unreleased
+     *
+     * @throws Exception
+     */
+    public function handleRequest($request): WP_REST_Response
+    {
+        $campaign = Campaign::find($request->get_param('campaignId'));
+
+        $query = new CampaignDonationQuery($campaign);
+
+        if(!$request->get_param('rangeInDays')) {
+            return new WP_REST_Response([[
+                'amountRaised' => $query->sumIntendedAmount(),
+                'donationCount' => $query->countDonations(),
+                'donorCount' => $query->countDonors(),
+            ]]);
+        }
+
+        $days = $request->get_param('rangeInDays');
+        $date = new DateTimeImmutable('now', wp_timezone());
+        $interval = DateInterval::createFromDateString("-$days days");
+        $period = new DatePeriod($date, $interval, 1);
+
+        return new WP_REST_Response(array_map(function($targetDate) use ($query, $interval) {
+
+            $query = $query->between(
+                Temporal::withStartOfDay($targetDate->add($interval)),
+                Temporal::withEndOfDay($targetDate)
+            );
+
+            return [
+                'amountRaised' => $query->sumIntendedAmount(),
+                'donationCount' => $query->countDonations(),
+                'donorCount' => $query->countDonors(),
+            ];
+        }, iterator_to_array($period) ));
+    }
+}
---
+++ tests/Unit/Campaigns/Routes/CampaignOverviewStatisticsTest.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace Give\Tests\Unit\Campaigns\Routes;
+
+use DateTime;
+use Give\Campaigns\Models\Campaign;
+use Give\Campaigns\Routes\CampaignOverviewStatistics;
+use Give\DonationForms\Models\DonationForm;
+use Give\Donations\Models\Donation;
+use Give\Donations\ValueObjects\DonationStatus;
+use Give\Framework\Database\DB;
+use Give\Framework\Support\ValueObjects\Money;
+use Give\Tests\TestCase;
+use Give\Tests\TestTraits\RefreshDatabase;
+use WP_REST_Request;
+
+/**
+ * @unreleased
+ */
+final class CampaignOverviewStatisticsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @unreleased
+     */
+    public function testReturnsAllTimeDonationsStatistics()
+    {
+        $campaign = Campaign::factory()->create();
+        $form = DonationForm::factory()->create();
+
+        $db = DB::table('give_campaign_forms');
+        $db->insert(['form_id' => $form->id, 'campaign_id' => $campaign->id]);
+
+        $donation1 = Donation::factory()->create([
+            'formId' => $form->id,
+            'status' => DonationStatus::COMPLETE(),
+            'amount' => new Money(1000, 'USD'),
+            'createdAt' => new DateTime('-35 days'),
+        ]);
+        give_update_meta($donation1->id, '_give_completed_date', $donation1->createdAt->format('Y-m-d H:i:s'));
+
+        $donation2 = Donation::factory()->create([
+            'formId' => $form->id,
+            'status' => DonationStatus::COMPLETE(),
+            'amount' => new Money(1000, 'USD'),
+            'createdAt' => new DateTime('-5 days'),
+        ]);
+        give_update_meta($donation2->id, '_give_completed_date', $donation2->createdAt->format('Y-m-d H:i:s'));
+
+        $donation3 = Donation::factory()->create([
+            'formId' => $form->id,
+            'status' => DonationStatus::COMPLETE(),
+            'amount' => new Money(1000, 'USD'),
+            'createdAt' => new DateTime('now'),
+        ]);
+        give_update_meta($donation3->id, '_give_completed_date', $donation3->createdAt->format('Y-m-d H:i:s'));
+
+        $request = new WP_REST_Request('GET', '/give-api/v2/campaign-overview-statistics');
+        $request->set_param('campaignId', $campaign->id);
+
+        $route = new CampaignOverviewStatistics;
+        $response = $route->handleRequest($request);
+
+        $this->assertEquals(3, $response->data[0]['donorCount']);
+        $this->assertEquals(3, $response->data[0]['donationCount']);
+        $this->assertEquals(30, $response->data[0]['amountRaised']);
+    }
+
+    /**
+     * @unreleased
+     */
+    public function testReturnsPeriodStatisticsWithPreviousPeriod()
+    {
+        $campaign = Campaign::factory()->create();
+        $form = DonationForm::factory()->create();
+
+        $db = DB::table('give_campaign_forms');
+        $db->insert(['form_id' => $form->id, 'campaign_id' => $campaign->id]);
+
+        $donation1 = Donation::factory()->create([
+            'formId' => $form->id,
+            'status' => DonationStatus::COMPLETE(),
+            'amount' => new Money(1000, 'USD'),
+            'createdAt' => new DateTime('-35 days'),
+        ]);
+        give_update_meta($donation1->id, '_give_completed_date', $donation1->createdAt->format('Y-m-d H:i:s'));
+
+        $donation2 = Donation::factory()->create([
+            'formId' => $form->id,
+            'status' => DonationStatus::COMPLETE(),
+            'amount' => new Money(1000, 'USD'),
+            'createdAt' => new DateTime('-5 days'),
+        ]);
+        give_update_meta($donation2->id, '_give_completed_date', $donation2->createdAt->format('Y-m-d H:i:s'));
+
+        $donation3 = Donation::factory()->create([
+            'formId' => $form->id,
+            'status' => DonationStatus::COMPLETE(),
+            'amount' => new Money(1000, 'USD'),
+            'createdAt' => new DateTime('now'),
+        ]);
+        give_update_meta($donation3->id, '_give_completed_date', $donation3->createdAt->format('Y-m-d H:i:s'));
+
+        $request = new WP_REST_Request('GET', '/give-api/v2/campaign-overview-statistics');
+        $request->set_param('campaignId', $campaign->id);
+        $request->set_param('rangeInDays', 30);
+
+        $route = new CampaignOverviewStatistics;
+        $response = $route->handleRequest($request);
+
+        $this->assertEquals(2, $response->data[0]['donorCount']);
+        $this->assertEquals(2, $response->data[0]['donationCount']);
+        $this->assertEquals(20, $response->data[0]['amountRaised']);
+
+        $this->assertEquals(1, $response->data[1]['donorCount']);
+        $this->assertEquals(1, $response->data[1]['donationCount']);
+        $this->assertEquals(10, $response->data[1]['amountRaised']);
+    }
+}
