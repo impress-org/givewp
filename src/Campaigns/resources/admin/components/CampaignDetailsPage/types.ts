5d6b98ada3fdf58ab2ccd3218dbc56d45482652c
Glauber Silva <glauber.silva@givewp.com>
2024-10-01 16:23:51 -0300

    refactor: remove duplicated campaign type

--- src/Campaigns/resources/admin/components/CampaignDetailsPage/types.ts
+++ src/Campaigns/resources/admin/components/CampaignDetailsPage/types.ts
@@ -1,18 +1,5 @@
 import {FC} from 'react';
 
-
-export interface Campaign {
-    id: number;
-    title: string;
-    type: string;
-    status: string;
-    shortDescription: string;
-    longDescription: string;
-    logo: string;
-    image: string;
-    goal: number;
-}
-
 export interface GiveCampaignDetails {
     adminUrl: string;
     currency: string;
