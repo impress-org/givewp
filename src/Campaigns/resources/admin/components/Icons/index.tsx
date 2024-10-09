f835e5ecfab0db5f2b7956489503f6509f4a338d
alaca <ante.laca@gmail.com>
2024-10-09 12:53:59 +0200

    feature: use current color

--- src/Campaigns/resources/admin/components/Icons/index.tsx
+++ src/Campaigns/resources/admin/components/Icons/index.tsx
@@ -50,8 +50,8 @@ export const ArrowReverse = () => (
 
 
 export const CloseIcon = () => (
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M17 7 7 17M7 7l10 10" stroke="#F9FAFB" strokeWidth="2" strokeLinecap="round"
+    <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path d="M17 7 7 17M7 7l10 10" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
               strokeLinejoin="round" />
     </svg>
 
