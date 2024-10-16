8dc5789789d70ed1b1a59114129d6a3e82a14889
Jon Waldstein <jon@givewp.com>
2024-10-16 15:30:06 -0400

    Feature: Add support for pausing/resuming subscriptions (#7551)
    Co-authored-by: Paulo Iankoski <me@wpdev.com.br>
    Co-authored-by: Jon Waldstein <jonwaldstein@jwaldstein-mbp.mynetworksettings.com>
    Co-authored-by: Joshua Dinh <75056371+JoshuaHungDinh@users.noreply.github.com>

---
+++ src/Framework/PaymentGateways/Contracts/Subscription/SubscriptionPausable.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Give\Framework\PaymentGateways\Contracts\Subscription;
+
+use Give\Subscriptions\Models\Subscription;
+
+/**
+ * @unreleased
+ */
+interface SubscriptionPausable
+{
+    /**
+     * Pause subscription.
+     *
+     * @unreleased
+     */
+    public function pauseSubscription(Subscription $subscription, array $data): void;
+
+    /**
+     * Resume subscription.
+     *
+     * @unreleased
+     */
+    public function resumeSubscription(Subscription $subscription): void;
+
+    /**
+     * Check if subscription can be paused.
+     *
+     * @unreleased
+     */
+    public function canPauseSubscription(): bool;
+}
