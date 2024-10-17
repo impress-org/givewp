d5aaffc11616da680ba02523eecc58234518852a
Glauber Silva <glauber.silva@givewp.com>
2024-10-17 12:48:42 -0300

    feature: add styles for add campaign form modal

--- src/DonationForms/V2/resources/components/AddCampaignFormModal/index.tsx
+++ src/DonationForms/V2/resources/components/AddCampaignFormModal/index.tsx
@@ -14,32 +14,51 @@ export default function AddCampaignFormModal({isOpen, handleClose, title, campai
             showHeader={true}
             handleClose={handleClose}
             title={title}
-            wrapperClassName={styles.formModal}
+            wrapperClassName={styles.addFormModal}
         >
-            {/*<div className={`givewp-campaigns__form`}>
-                <p>Options Goes Here...</p>
-            </div>*/}
-
-            <div>
-                <>
+            <div className={'givewp-editor-options'}>
+                <div className={'givewp-editor-options__card'}>
+                    <img
+                        src={`${window.GiveDonationForms.pluginUrl}/assets/dist/images/admin/give-settings-gateways-v3.jpg`}
+                        alt={__('Visual Form Builder', 'give')}
+                    />
+                    <span>{__('Recommended', 'give')}</span>
+                    <label>{__('Visual Form Builder', 'give')}</label>
+                    <p>
+                        {__(
+                            'Uses the blocks-based visual form builder for creating and customizing a donation form.',
+                            'give'
+                        )}
+                    </p>
                     <a
                         href={
                             'edit.php?post_type=give_forms&page=givewp-form-builder&donationFormID=new&campaignId=' +
                             campaignId
                         }
-                        className={styles.addCampaignFormButton}
+                        className={styles.addFormButton}
                     >
                         {__('Use Visual Form Builder', 'give')}
                     </a>
-                    <br />
-                    <br />
+                </div>
+                <div className={'givewp-editor-options__card'}>
+                    <img
+                        src={`${window.GiveDonationForms.pluginUrl}/assets/dist/images/admin/give-settings-gateways-v2.jpg`}
+                        alt={__('Option-Based Form Editor', 'give')}
+                    />
+                    <label>{__('Option-Based Form Editor', 'give')}</label>
+                    <p>
+                        {__(
+                            'Uses the traditional settings options for creating and customizing a donation form.',
+                            'give'
+                        )}
+                    </p>
                     <a
                         href={'post-new.php?post_type=give_forms&campaignId=' + campaignId}
-                        className={styles.addCampaignFormButton}
+                        className={styles.addFormButton}
                     >
-                        {__('Option-Based Form Editor', 'give')}
+                        {__('Use Option-Based Form Editor', 'give')}
                     </a>
-                </>
+                </div>
             </div>
         </ModalDialog>
     );
