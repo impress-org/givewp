52863c96a8e7f10102ac5cc2bda14b59ba45ec7c
alaca <ante.laca@gmail.com>
2024-11-26 17:21:01 +0100

    fix: test

--- tests/Unit/Framework/QueryBuilder/InsertManyTest.php
+++ tests/Unit/Framework/QueryBuilder/InsertManyTest.php
@@ -34,7 +34,7 @@ final class InsertManyTest extends TestCase
         }
 
         $this->assertContains(
-            "INSERT INTO '" . DB::prefix('posts') . "' (post_title, post_type, post_content) VALUES ('Query Builder CRUD test 1', 'crud_test', 'Hello World 1!'),('Query Builder CRUD test 2', 'crud_test', 'Hello World 2!')",
+            "INSERT INTO '" . DB::prefix('posts') . "' (post_title, post_type, post_content) VALUES ('Query Builder CRUD test 1','crud_test','Hello World 1!'),('Query Builder CRUD test 2','crud_test','Hello World 2!')",
             $QB->getSQL()
         );
     }
