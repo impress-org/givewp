d03fbfa873b4eacd4b065348969a597d0e5048eb
Joshua Dinh <josh@joshuadinh.tech>
2024-12-17 11:50:02 +0900

    refactor: replace & update campaign overview inline styles

--- src/Campaigns/resources/admin/components/CampaignDetailsPage/Components/CampaignStats/index.tsx
+++ src/Campaigns/resources/admin/components/CampaignDetailsPage/Components/CampaignStats/index.tsx
@@ -1,14 +1,16 @@
 import {__} from '@wordpress/i18n';
 import {useEffect, useState} from "react";
-import RevenueChart from "./RevenueChart";
-import GoalProgressChart from "./GoalProgressChart";
+import RevenueChart from "../RevenueChart";
+import GoalProgressChart from "../GoalProgressChart";
 import apiFetch from '@wordpress/api-fetch';
 import { addQueryArgs } from '@wordpress/url';
-import HeaderText from './HeaderText';
-import HeaderSubText from './HeaderSubText';
-import DefaultFormWidget from "./DefaultForm";
+import HeaderText from '../HeaderText';
+import HeaderSubText from '../HeaderSubText';
+import DefaultFormWidget from "../DefaultForm";
 import {GiveCampaignDetails} from "@givewp/campaigns/admin/components/CampaignDetailsPage/types";
 import {useCampaignEntityRecord} from '@givewp/campaigns/utils';
+
+import styles from "./styles.module.scss"
 
 const campaignId = new URLSearchParams(window.location.search).get('id');
 
@@ -53,18 +55,16 @@ const CampaignStats = () => {
     return (
         <>
             <DateRangeFilters selected={dayRange} options={filterOptions} onSelect={onDayRangeChange} />
-
             <Row>
-                <StatWidget label={__('Amount Raised')} values={pluck(stats, 'amountRaised')} description={widgetDescription} formatter={currency} />
-                <StatWidget label={__('Donation Count')} values={pluck(stats, 'donationCount')} description={widgetDescription} />
-                <StatWidget label={__('Donor Count')} values={pluck(stats, 'donorCount')} description={widgetDescription} />
-            </Row>
-
-            <Row>
-                <Column flex={2}>
+                <Column>
+                    <Row>
+                        <StatWidget label={__('Amount Raised')} values={pluck(stats, 'amountRaised')} description={widgetDescription} formatter={currency} />
+                        <StatWidget label={__('Donation Count')} values={pluck(stats, 'donationCount')} description={widgetDescription} />
+                    </Row>
                     <RevenueWidget />
                 </Column>
-                <Column flex={1}>
+                <Column>
+                    <StatWidget label={__('Donor Count')} values={pluck(stats, 'donorCount')} description={widgetDescription} />
                     <GoalProgressWidget />
                     <DefaultFormWidget defaultForm={campaign.defaultFormTitle} />
                 </Column>
@@ -75,12 +75,7 @@ const CampaignStats = () => {
 
 const FooterText = ({children}) => {
     return (
-        <div style={{
-            fontSize: '12px',
-            fontWeight: 400,
-            lineHeight: '18px',
-            color: '#1F2937',
-        }}>
+        <div className={styles.footerText}>
             {children}
         </div>
     )
@@ -88,12 +83,7 @@ const FooterText = ({children}) => {
 
 const DisplayText = ({children}) => {
     return (
-        <div style={{
-            fontSize: '36px',
-            fontWeight: 600,
-            lineHeight: '43.57px',
-            color: '#060C1A',
-        }}>
+        <div className={styles.statWidgetDisplay}>
             {children}
         </div>
     )
@@ -101,27 +91,11 @@ const DisplayText = ({children}) => {
 
 const StatWidget = ({label, values, description, formatter = null}) => {
     return (
-        <div style={{
-            flex: 1,
-            padding: '24px',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-between',
-            backgroundColor: 'white',
-            borderRadius: '8px',
-            gap: '10px',
-        }}>
-            <header style={{flex: '1'}}>
+        <div className={styles.statWidget}>
+            <header>
                 <HeaderText>{label}</HeaderText>
             </header>
-            <div style={{
-                flex: '1',
-                padding: '5px',
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-                alignItems: 'baseline'
-            }}>
+            <div className={styles.statWidgetAmount}>
                 <DisplayText>
                     {formatter?.format(values[0]) ?? values[0]}
                 </DisplayText>
@@ -129,7 +103,7 @@ const StatWidget = ({label, values, description, formatter = null}) => {
                     <PercentChangePill value={values[0]} comparison={values[1]} />
                 )}
             </div>
-            <footer style={{flex: '1'}}>
+            <footer>
                 <FooterText>{description}</FooterText>
             </footer>
         </div>
@@ -147,13 +121,10 @@ const PercentChangePill = ({value, comparison}) => {
             : ['#e35f45', '#fff4f2', '⯆']
 
     return (
-        <span style={{
-            padding: '0.5rem',
-            fontSize: '.8rem',
-            borderRadius: '1rem',
-            backgroundColor: backgroundColor,
-            color: color,
-        }}>
+        <span
+            className={styles.percentChangePill}
+            style={{backgroundColor: backgroundColor, color: color,}}
+        >
             <small>{symbol}</small> {Math.abs(change)}%
         </span>
     )
@@ -162,12 +133,7 @@ const PercentChangePill = ({value, comparison}) => {
 
 const RevenueWidget = () => {
     return (
-        <div style={{
-            flex: 2,
-            backgroundColor: 'white',
-            padding: '20px',
-            borderRadius: '8px',
-        }}>
+        <div className={styles.revenueWidget}>
             <header>
                 <HeaderText>Revenue</HeaderText>
                 <HeaderSubText>{__('Show your revenue over time')}</HeaderSubText>
@@ -182,12 +148,7 @@ const GoalProgressWidget = () => {
     const {campaign} = useCampaignEntityRecord();
 
     return (
-        <div style={{
-            flex: 1,
-            backgroundColor: 'white',
-            padding: '20px',
-            borderRadius: '8px',
-        }}>
+        <div className={styles.progressWidget}>
             <HeaderText>{__('Goal Progress')}</HeaderText>
             <HeaderSubText>{__('Show your campaign performance')}</HeaderSubText>
             <GoalProgressChart value={campaign.goalProgress} goal={campaign.goal} />
@@ -197,24 +158,11 @@ const GoalProgressWidget = () => {
 
 const DateRangeFilters = ({options, onSelect, selected}) => {
     return (
-        <div style={{
-            padding: '1rem',
-            backgroundColor: '#f3f4f6',
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'end',
-            gap: '1rem',
-            borderRadius: '8px',
-        }}>
+        <div className={styles.dateRangeFilter}>
             {options.map((option, index) => (
                 <button
+                    className={selected === option.value && styles.selectedDateRange}
                     key={index}
-                    style={{
-                        border: 0,
-                        cursor: 'pointer',
-                        padding: '0.5rem 1rem',
-                        backgroundColor: selected === option.value ? 'white' : 'transparent',
-                    }}
                     onClick={() => onSelect(option.value)}
                 >
                     {option.label}
@@ -224,25 +172,15 @@ const DateRangeFilters = ({options, onSelect, selected}) => {
     )
 }
 
+
 const Row = ({children}) => (
-    <div style={{
-        display: 'flex',
-        flexDirection: 'row',
-        gap: '20px',
-        marginBottom: '1rem',
-    }}>
+    <div className={styles.row}>
         {children}
     </div>
 )
 
-const Column = ({children, flex = 1}) => (
-    <div style={{
-        flex: flex,
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-        gap: '20px',
-    }}>
+const Column = ({children}) => (
+    <div className={styles.column}>
         {children}
     </div>
 )
