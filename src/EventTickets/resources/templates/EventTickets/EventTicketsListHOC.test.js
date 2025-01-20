3c076afd7c99c4d4b4797911a833b5648b2c9a23
Paulo Iankoski <me@wpdev.com.br>
2025-01-20 13:52:26 -0300

    chore: update import

--- src/EventTickets/resources/templates/EventTickets/EventTicketsListHOC.test.js
+++ src/EventTickets/resources/templates/EventTickets/EventTicketsListHOC.test.js
@@ -1,5 +1,5 @@
 import {fireEvent, render} from '@testing-library/react';
-import {act} from 'react-dom/test-utils';
+import {act} from 'react';
 import EventTicketsListHOC from './EventTicketsListHOC';
 
 /**
