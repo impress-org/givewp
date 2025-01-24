a617c1a5723535bb06aa98f8d81759f2cbf88e72
Glauber Silva <glauber.silva@givewp.com>
2025-01-24 18:13:38 -0300

    tests: add WebhookTest class

--- src/PaymentGateways/Gateways/TestGateway/TestGateway.php
+++ src/PaymentGateways/Gateways/TestGateway/TestGateway.php
@@ -2,10 +2,13 @@
 
 namespace Give\PaymentGateways\Gateways\TestGateway;
 
+use Exception;
 use Give\Donations\Models\Donation;
 use Give\Framework\PaymentGateways\Commands\GatewayCommand;
 use Give\Framework\PaymentGateways\Commands\PaymentComplete;
 use Give\Framework\PaymentGateways\Commands\PaymentRefunded;
+use Give\Framework\PaymentGateways\Contracts\WebhookNotificationsListener;
+use Give\Framework\PaymentGateways\Log\PaymentGatewayLog;
 use Give\Framework\PaymentGateways\PaymentGateway;
 use Give\Framework\Support\Facades\Scripts\ScriptAsset;
 use Give\Helpers\Form\Utils as FormUtils;
@@ -15,10 +18,11 @@ use Give\PaymentGateways\Gateways\TestGateway\Views\LegacyFormFieldMarkup;
 /**
  * A gateway for testing the donation process. No actual payment is processed and only form validation is performed.
  *
+ * @unreleased Implements the WebhookNotificationsListener interface
  * @since 3.0.0 change to Test Donations and manual id to replace legacy gateway
  * @since 2.18.0
  */
-class TestGateway extends PaymentGateway
+class TestGateway extends PaymentGateway implements WebhookNotificationsListener
 {
     /**
      * @inheritDoc
@@ -104,5 +108,59 @@ class TestGateway extends PaymentGateway
     public function refundDonation(Donation $donation): PaymentRefunded
     {
         return new PaymentRefunded();
+    }
+
+    /**
+     * This method implementation is a sample that demonstrate how we can handle webhook notifications
+     *
+     * @unreleased
+     */
+    public function webhookNotificationsListener()
+    {
+        try {
+            $webhookNotification = give_clean($_REQUEST);
+
+            /**
+             * Allow developers to handle the webhook notification.
+             *
+             * @unreleased
+             *
+             * @param array $webhookNotification
+             */
+            do_action('givewp_' . $this::id() . '_webhook_notification_handler', $webhookNotification);
+
+            // We will handle recurring donations in a separate submodule.
+            if (isset($webhookNotification['gatewayRecurringPayment']) && $webhookNotification['gatewayRecurringPayment']) {
+                return;
+            }
+
+            if ( ! isset($webhookNotification['gatewayPaymentStatus']) && ! isset($webhookNotification['gatewayPaymentId'])) {
+                return;
+            }
+
+            switch (strtolower($webhookNotification['gatewayPaymentStatus'])) {
+                case 'complete':
+                    $this->webhook->events->paymentCompleted($webhookNotification['gatewayPaymentId']);
+                    break;
+                case 'failed':
+                    $this->webhook->events->paymentFailed($webhookNotification['gatewayPaymentId']);
+                    break;
+                case 'cancelled':
+                    $this->webhook->events->paymentCancelled($webhookNotification['gatewayPaymentId']);
+                    break;
+                case 'refunded':
+                    $this->webhook->events->paymentRefunded($webhookNotification['gatewayPaymentId']);
+                    break;
+                default:
+                    break;
+            }
+        } catch (Exception $e) {
+            esc_html_e('Webhook Notification failed.', 'give');
+            PaymentGatewayLog::error(
+                'Webhook Notification failed. Error: ' . $e->getMessage()
+            );
+        }
+
+        exit();
     }
 }
