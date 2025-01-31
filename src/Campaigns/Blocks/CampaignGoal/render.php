069c39d230bff31d2d24571c2c49c7ee13e99e15
alaca <ante.laca@gmail.com>
2025-01-31 12:23:37 +0100

    feature: render block

--- src/Campaigns/Blocks/CampaignGoal/render.php
+++ src/Campaigns/Blocks/CampaignGoal/render.php
@@ -1,8 +1,10 @@
 <?php
 
+use Give\Campaigns\DataTransferObjects\CampaignGoalData;
 use Give\Campaigns\Models\Campaign;
 use Give\Campaigns\Repositories\CampaignRepository;
-use Give\DonationForms\Blocks\DonationFormBlock\Controllers\BlockRenderController;
+use Give\Campaigns\ValueObjects\CampaignGoalType;
+use Give\Framework\Support\ValueObjects\Money;
 
 /**
  * @var array    $attributes
@@ -16,12 +18,53 @@ if (
     return;
 }
 
-$params = [
-    'formId' => $attributes['useDefaultForm']
-        ? $campaign->defaultFormId
-        : $attributes['selectedForm'],
-    'openFormButton' => $attributes['buttonText'],
-    'formFormat' => 'modal',
-];
+$stats = (new CampaignGoalData($campaign))->toArray();
 
-echo (new BlockRenderController())->render($params);
+$getGoalDescription = function(CampaignGoalType $goalType) {
+    $data = [
+        'amount' => __('Amount raised', 'give'),
+        'donations' => __('Number of donations', 'give'),
+        'donors' => __('Number of donors', 'give'),
+        'amountFromSubscriptions' => __('Recurring amount raised', 'give'),
+        'subscriptions' => __('Number of recurring donations', 'give'),
+        'donorsFromSubscriptions' => __('Number of recurring donors', 'give'),
+    ];
+
+    return $data[$goalType->getvalue()];
+};
+
+$getGoalFormattedValue = function($goalType, $value) {
+    switch ($goalType) {
+        case 'amount':
+        case 'amountFromSubscriptions':
+            $amount = Money::fromDecimal($value, give_get_currency());
+            return $amount->formatToLocale();
+        default:
+            return $value;
+    }
+};
+
+?>
+
+<div class="give-campaign-goal">
+    <div class="give-campaign-goal__container">
+        <div class="give-campaign-goal__container-item">
+            <span><?= $getGoalDescription($campaign->goalType); ?></span>
+            <strong>
+                <?= $getGoalFormattedValue($campaign->goalType, $stats['actual']); ?>
+            </strong>
+        </div>
+        <div class="give-campaign-goal__container-item">
+            <span><?= esc_html__('Our goal', 'give'); ?></span>
+            <strong><?= $getGoalFormattedValue($campaign->goalType, $campaign->goal); ?></strong>
+        </div>
+    </div>
+    <div class="give-campaign-goal__progress-bar">
+        <div class="give-campaign-goal__progress-bar-container">
+            <div class="give-campaign-goal__progress-bar-progress"
+                 style="width: <?= $stats['percentage']; ?>%">
+            </div>
+        </div>
+    </div>
+</div>
+
