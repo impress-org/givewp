740d7ac838900f4095176bd5baa091f376c49856
Jason Adams <jason.adams@stellarwp.com>
2025-02-06 09:45:23 -0700

    chore: adds missing inline docs

--- src/VendorOverrides/AdminNotices/AdminNoticesServiceProvider.php
+++ src/VendorOverrides/AdminNotices/AdminNoticesServiceProvider.php
@@ -7,12 +7,29 @@ namespace Give\VendorOverrides\AdminNotices;
 use Give\ServiceProviders\ServiceProvider;
 use Give\Vendors\StellarWP\AdminNotices\AdminNotices;
 
+/**
+ * Registers and boots the Admin Notices library
+ *
+ * @unreleased
+ *
+ * @see https://github.com/stellarwp/admin-notices
+ */
 class AdminNoticesServiceProvider implements ServiceProvider {
+    /**
+     * {@inheritDoc}
+     *
+     * @unreleased
+     */
     public function register()
     {
         AdminNotices::initialize('givewp', GIVE_PLUGIN_URL . 'vendor/vendor-prefixed/stellarwp/admin-notices/');
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @unreleased
+     */
     public function boot()
     {
     }
