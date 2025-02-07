0b6a957ab26ab3bc2b312e2c3dc02cc4be9ee177
alaca <ante.laca@gmail.com>
2025-02-07 15:44:34 +0100

    feature: add parameters to useCampaigns hook

--- src/Campaigns/Blocks/shared/hooks/useCampaigns.ts
+++ src/Campaigns/Blocks/shared/hooks/useCampaigns.ts
@@ -1,8 +1,14 @@
 import {useEntityRecords} from '@wordpress/core-data';
 import {Campaign} from '@givewp/campaigns/admin/components/types';
 
-export default function useCampaigns() {
-    const data = useEntityRecords('givewp', 'campaign');
+type useCampaignsParams = {
+    ids?: number[],
+    page?: number,
+    per_page?: number;
+}
+
+export default function useCampaigns({ids = [], page = 1, per_page = 30}: useCampaignsParams = {}) {
+    const data = useEntityRecords('givewp', 'campaign', {ids, page, per_page});
 
     return {
         campaigns: data?.records as Campaign[],
