ef9a78172f833d990a93c0360533300b856022b8
Jon Waldstein <jonwaldstein@mac.mynetworksettings.com>
2025-02-10 14:55:04 -0500

    refactor: only seperate alias

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
-const DependencyExtractionWebpackPlugin = require('@wordpress/dependency-extraction-webpack-plugin');
 
 /**
- * This is a custom webpack configuration, consumed by Laravel Mix.
+ * This is a custom webpack configuration, intended to be consumed by Laravel Mix specifically.
+ * This is important so the IDE can understand the aliases and resolve paths correctly.
+ *
+ * Note: @wordpress/scripts is set up to use its own webpack configuration.
  */
 module.exports = {
     resolve: {
@@ -13,47 +15,4 @@ module.exports = {
             '@givewp/src': path.resolve(__dirname, 'src/'),
         },
     },
-    plugins: [
-        /*
-         * Transform script dependencies only for following external libraries:
-         * - @wordpress/
-         * - jquery
-         * - lodash, lodash-es
-         */
-        new DependencyExtractionWebpackPlugin({
-            useDefaults: false,
-            requestToExternal: (request) => {
-                const WORDPRESS_NAMESPACE = '@wordpress/';
-
-                if (request.startsWith(WORDPRESS_NAMESPACE)) {
-                    return [
-                        'wp',
-
-                        /* Transform @wordpress dependencies:
-                         * - request @wordpress/api-fetch becomes [ 'wp', 'apiFetch' ]
-                         * - request @wordpress/i18n becomes [ 'wp', 'i18n' ]
-                         */
-                        request
-                            .substring(WORDPRESS_NAMESPACE.length)
-                            .replace(/-([a-z])/g, (_, letter) => letter.toUpperCase()),
-                    ];
-                } else if (['lodash', 'lodash-es'].includes(request)) {
-                    return 'lodash';
-                } else if (request === 'jquery') {
-                    return 'jQuery';
-                }
-            },
-            requestToHandle: (request) => {
-                const WORDPRESS_NAMESPACE = '@wordpress/';
-
-                if (request === 'lodash-es') {
-                    return 'lodash';
-                }
-
-                if (request.startsWith(WORDPRESS_NAMESPACE)) {
-                    return 'wp-' + request.substring(WORDPRESS_NAMESPACE.length);
-                }
-            },
-        }),
-    ],
-}
+};
--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const mix = require('laravel-mix');
 const WebpackRTLPlugin = require('webpack-rtl-plugin');
 const webpackConfig = require('./webpack.config');
+const DependencyExtractionWebpackPlugin = require('@wordpress/dependency-extraction-webpack-plugin');
 
 mix.setPublicPath('assets/dist')
     .sass('assets/src/css/frontend/give-frontend.scss', 'css/give.css')
@@ -80,6 +81,54 @@ mix.setPublicPath('assets/dist')
     .copyDirectory('assets/src/fonts', 'assets/dist/fonts');
 
 mix.webpackConfig(webpackConfig);
+mix.webpackConfig((webpack, config) => {
+    return {
+        plugins: [
+        /*
+         * Transform script dependencies only for following external libraries:
+         * - @wordpress/
+         * - jquery
+         * - lodash, lodash-es
+         */
+        new DependencyExtractionWebpackPlugin({
+            useDefaults: false,
+            requestToExternal: (request) => {
+                const WORDPRESS_NAMESPACE = '@wordpress/';
+
+                if (request.startsWith(WORDPRESS_NAMESPACE)) {
+                    return [
+                        'wp',
+
+                        /* Transform @wordpress dependencies:
+                         * - request @wordpress/api-fetch becomes [ 'wp', 'apiFetch' ]
+                         * - request @wordpress/i18n becomes [ 'wp', 'i18n' ]
+                         */
+                        request
+                            .substring(WORDPRESS_NAMESPACE.length)
+                            .replace(/-([a-z])/g, (_, letter) => letter.toUpperCase()),
+                    ];
+                } else if (['lodash', 'lodash-es'].includes(request)) {
+                    return 'lodash';
+                } else if (request === 'jquery') {
+                    return 'jQuery';
+                }
+            },
+            requestToHandle: (request) => {
+                const WORDPRESS_NAMESPACE = '@wordpress/';
+
+                if (request === 'lodash-es') {
+                    return 'lodash';
+                }
+
+                if (request.startsWith(WORDPRESS_NAMESPACE)) {
+                    return 'wp-' + request.substring(WORDPRESS_NAMESPACE.length);
+                }
+            },
+        }),
+        ...config.plugins,
+    ],
+    }
+})
 
 mix.options({
     // Don't perform any css url rewriting by default
