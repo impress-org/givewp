ac052cf2671b09709075749fa71cccf2f93a5a6a
alaca <ante.laca@gmail.com>
2025-02-10 17:20:13 +0100

    feature: add campaign list app

--- src/Campaigns/Blocks/CampaignList/app/index.tsx
+++ src/Campaigns/Blocks/CampaignList/app/index.tsx
@@ -1,17 +1,75 @@
+import {useState} from '@wordpress/element';
 import useCampaigns from '../../shared/hooks/useCampaigns';
+import {CampaignListType} from '../types';
 
-export default ({attributes}) => {
-    const campaigns = useCampaigns({
-        ids: attributes?.filterBy?.map((item: { value: number }) => item.value),
-        page: attributes?.page,
-        per_page: attributes?.per_page,
+import './styles.scss';
+
+const getGridSettings = (layout: string) => {
+    switch (layout) {
+        case 'double':
+            return 2;
+        case 'triple':
+            return 3;
+        default:
+            return 1;
+    }
+}
+
+export default ({attributes}: { attributes: CampaignListType }) => {
+    const [page, setPage] = useState(1);
+
+    const {campaigns, hasResolved} = useCampaigns({
+        ids: attributes?.filterBy?.map((item: { value: string }) => Number(item.value)),
+        per_page: attributes?.perPage,
+        page,
     });
 
-    console.log(campaigns);
+
+    if (!hasResolved) {
+        return (
+            <div>loading</div>
+        )
+    }
 
     return (
-        <div>
+        <>
+            <div
+                className="give-campaigns-campaignListBlock-grid"
+                style={{gridTemplateColumns: `repeat(${getGridSettings(attributes.layout)}, 1fr)`}}
+            >
+                {campaigns?.map((campaign) => (
+                    <div
+                        className="give-campaigns-campaignListBlock-grid-item"
+                    >
+                        {attributes.showImage && campaign.image && (
+                            <div
+                                style={{backgroundImage: `url(${campaign.image})`}}
+                                className="give-campaigns-campaignListBlock-grid-item-image">
+                            </div>
+                        )}
+                        <div className="give-campaigns-campaignListBlock-grid-item-title">
+                            {campaign.title}
+                        </div>
+                        {attributes.showDescription && (
+                            <div className="give-campaigns-campaignListBlock-grid-item-description">
+                                {campaign.shortDescription}
+                            </div>
+                        )}
 
-        </div>
+                        {attributes.showGoal && (
+                            <div>
+
+                            </div>
+                        )}
+                    </div>
+                ))}
+            </div>
+
+            {campaigns.length > 0 && attributes.showPagination && (
+                <div>
+                    Pagination
+                </div>
+            )}
+        </>
     )
 }
