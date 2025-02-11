c25deb0596eec952cc26d6afa75e3e7bd35655b4
alaca <ante.laca@gmail.com>
2025-02-11 16:00:08 +0100

    refactor: remove fragments

--- src/Campaigns/Blocks/shared/components/Pagination/index.tsx
+++ src/Campaigns/Blocks/shared/components/Pagination/index.tsx
@@ -17,19 +17,17 @@ export default ({currentPage, totalPages, setPage}: PaginationProps) => {
             <div className="give-campaign-components-pagination__pages">
                 <div className="give-campaign-components-pagination__pages-links">
                     {previousPage > 0 ? (
-                        <>
-                            <a
-                                href="#"
-                                title={__('Previous page', 'give')}
-                                className="give-campaign-components-pagination__pages-links-arrow"
-                                onClick={(e) => {
-                                    e.preventDefault();
-                                    setPage(previousPage);
-                                }}
-                            >
-                                <ChevronLeft />
-                            </a>
-                        </>
+                        <a
+                            href="#"
+                            title={__('Previous page', 'give')}
+                            className="give-campaign-components-pagination__pages-links-arrow"
+                            onClick={(e) => {
+                                e.preventDefault();
+                                setPage(previousPage);
+                            }}
+                        >
+                            <ChevronLeft />
+                        </a>
                     ) : (
                         <a className="give-campaign-components-pagination__pages-links-arrow-disabled">
                             <ChevronLeft />
@@ -54,19 +52,17 @@ export default ({currentPage, totalPages, setPage}: PaginationProps) => {
                     })}
 
                     {nextPage <= totalPages ? (
-                        <>
-                            <a
-                                href="#"
-                                title={__('Next page', 'give')}
-                                className="give-campaign-components-pagination__pages-links-arrow"
-                                onClick={(e) => {
-                                    e.preventDefault();
-                                    setPage(nextPage);
-                                }}
-                            >
-                                <ChevronRight />
-                            </a>
-                        </>
+                        <a
+                            href="#"
+                            title={__('Next page', 'give')}
+                            className="give-campaign-components-pagination__pages-links-arrow"
+                            onClick={(e) => {
+                                e.preventDefault();
+                                setPage(nextPage);
+                            }}
+                        >
+                            <ChevronRight />
+                        </a>
                     ) : (
                         <a className="give-campaign-components-pagination__pages-links-arrow-disabled">
                             <ChevronRight />
