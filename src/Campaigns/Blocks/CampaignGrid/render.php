7b266c96d5aac3d553a39c2fd70f07112ee70766
Ante Laca <ante.laca@gmail.com>
2025-02-13 18:45:31 +0100

    Update render.php
    Co-authored-by: Jon Waldstein <jon@givewp.com>

--- src/Campaigns/Blocks/CampaignGrid/render.php
+++ src/Campaigns/Blocks/CampaignGrid/render.php
@@ -3,4 +3,4 @@
  * @var array $attributes
  */
 ?>
-<div data-givewp-campaign-gird data-attributes=<?= json_encode($attributes) ?>></div>
+<div data-givewp-campaign-grid data-attributes=<?= json_encode($attributes) ?>></div>
