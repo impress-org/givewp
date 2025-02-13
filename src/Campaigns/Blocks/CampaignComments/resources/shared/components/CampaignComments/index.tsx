c7fec71b30f29cffa87329aa8c6d888dfac25b74
Joshua Dinh <josh@joshuadinh.tech>
2025-02-13 09:23:17 +0900

    refactor: update comments per page

--- src/Campaigns/Blocks/CampaignComments/resources/shared/components/CampaignComments/index.tsx
+++ src/Campaigns/Blocks/CampaignComments/resources/shared/components/CampaignComments/index.tsx
@@ -10,15 +10,13 @@ type CampaignCommentsProps = {
 export default function CampaignComments({attributes}: CampaignCommentsProps) {
     const comments = getGiveCampaignCommentsBlockWindowData();
 
-    const commentsPerPage = comments.slice(0, attributes.commentsPerPage);
-
     return (
         <div className={'givewp-campaign-comment-block'}>
             <h4 className={'givewp-campaign-comment-block__title'}>{attributes.title}</h4>
             <p className={'givewp-campaign-comment-block__cta'}>
                 {__('Leave a supportive message by donating to the campaign.', 'give')}
             </p>
-            {commentsPerPage?.map((comment: commentData, index: number) => (
+            {comments && comments?.slice(0, attributes.commentsPerPage)?.map((comment: commentData, index: number) => (
                 <CampaignCommentCard key={`givewp-campaign-comment-${index}`} attributes={attributes} data={comment} />
             ))}
         </div>
