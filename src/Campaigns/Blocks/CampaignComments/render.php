146d512ffb9ab03a5d28dc9c66339c1b87977fe5
Joshua Dinh <josh@joshuadinh.tech>
2025-02-13 11:24:31 +0900

    refactor: cleanup render file

--- src/Campaigns/Blocks/CampaignComments/render.php
+++ src/Campaigns/Blocks/CampaignComments/render.php
@@ -4,18 +4,15 @@ use Give\Campaigns\Blocks\CampaignComments\Controller\BlockRenderController;
 use Give\Campaigns\Models\Campaign;
 use Give\Campaigns\Repositories\CampaignRepository;
 
-$attributes = $attributes ?? [];
+/**
+ * @var array    $attributes
+ * @var Campaign $campaign
+ */
 
-if (!isset($attributes['campaignId'])) {
-    return;
-}
-
-/** @var Campaign $campaign */
-$campaign = give(CampaignRepository::class)->getById($attributes['campaignId']);
-
-if (!$campaign) {
+if (! isset($attributes['campaignId'])
+    || ! $campaign = give(CampaignRepository::class)->getById($attributes['campaignId'])
+) {
     return;
 }
 
 echo (new BlockRenderController())->render($attributes);
-?>
