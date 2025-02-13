d1455e6402da98bb67e24920e92f2e589d30a97a
Joshua Dinh <josh@joshuadinh.tech>
2025-02-13 15:45:14 +0900

    refactor: cleanup root render

--- src/Campaigns/Blocks/CampaignComments/resources/app.tsx
+++ src/Campaigns/Blocks/CampaignComments/resources/app.tsx
@@ -1,14 +1,14 @@
 import {createRoot} from '@wordpress/element';
 import CampaignComments from './shared/components/CampaignComments';
-import getGiveCampaignCommentsBlockWindowData from "./shared/window";
+import getGiveCampaignCommentsBlockWindowData from './shared/window';
 
 const roots = document.querySelectorAll('.givewp-campaign-comment-block');
 
-(function render() {
-    return roots.forEach((root) => {
+if (roots) {
+    roots.forEach((root) => {
         const attributes = root.getAttribute('data-attributes');
         const comments = getGiveCampaignCommentsBlockWindowData();
 
-        createRoot(root).render(<CampaignComments attributes={JSON.parse(attributes)} comments={comments} />);
+        return createRoot(root).render(<CampaignComments attributes={JSON.parse(attributes)} comments={comments} />);
     });
-})();
+}
