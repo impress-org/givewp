9a408b7900dd5c17220fa227c1163874167b8490
Paulo Iankoski <me@wpdev.com.br>
2025-02-12 21:50:27 -0300

    feature: implement color palette selector

--- src/Campaigns/resources/admin/components/CampaignDetailsPage/Components/ColorControl/index.tsx
+++ src/Campaigns/resources/admin/components/CampaignDetailsPage/Components/ColorControl/index.tsx
@@ -1,4 +1,4 @@
-import {ColorIndicator, Popover} from '@wordpress/components';
+import {ColorIndicator, ColorPalette, Popover} from '@wordpress/components';
 import {__} from '@wordpress/i18n';
 import {useState} from 'react';
 import {Controller, useFormContext} from 'react-hook-form';
@@ -7,6 +7,16 @@ import classnames from 'classnames';
 import EditIcon from './Icons/EditIcon';
 import CheckIcon from './Icons/CheckIcon';
 import './styles.scss';
+
+const defaultColors = [
+    {name: 'Blue', slug: 'blue', color: '#0b72d9'},
+    {name: 'Green', slug: 'green', color: '#27ae60'},
+    {name: 'Purple', slug: 'purple', color: '#19078c'},
+    {name: 'Orange', slug: 'orange', color: '#f29718'},
+    {name: 'Lavender', slug: 'lavender', color: '#9b51e0'},
+    {name: 'Terracotta', slug: 'terracotta', color: '#e26f56'},
+    {name: 'Red', slug: 'red', color: '#cc1818'},
+];
 
 const ColorControl = ({name, disabled, className}) => {
     const [popoverIsVisible, setPopoverIsVisible] = useState<boolean>(false);
@@ -21,7 +31,6 @@ const ColorControl = ({name, disabled, className}) => {
             name={name}
             control={control}
             render={({field}) => {
-                console.log('field', field);
                 return (
                     <div className={classnames('givewp-color-control', className)}>
                         <div className="givewp-color-control__indicator">
@@ -31,11 +40,35 @@ const ColorControl = ({name, disabled, className}) => {
 
                         {!disabled && (
                             <div className="givewp-color-control__popover">
-                                <button className="givewp-color-control__edit-button" onClick={toggleVisible}>
+                                <button
+                                    className={classnames('givewp-color-control__edit-button', {
+                                        'givewp-color-control__edit-button--active': popoverIsVisible,
+                                    })}
+                                    onClick={toggleVisible}
+                                >
                                     <EditIcon />
                                     {__('Edit', 'give')}
                                 </button>
-                                {popoverIsVisible && <Popover>This is the content inside the popover</Popover>}
+                                {popoverIsVisible && (
+                                    <Popover
+                                        className="givewp-color-control__popover-content"
+                                        offset={8}
+                                        onClose={toggleVisible}
+                                        placement="right"
+                                    >
+                                        <ColorPalette
+                                            clearable={false}
+                                            colors={[
+                                                {
+                                                    colors: defaultColors,
+                                                    name: __('Theme', 'give'),
+                                                },
+                                            ]}
+                                            value={field.value}
+                                            onChange={(color) => field.onChange(color)}
+                                        />
+                                    </Popover>
+                                )}
                             </div>
                         )}
                     </div>
