376eba218ed324e6623d439bfda1034404b20f21
alaca <ante.laca@gmail.com>
2025-02-14 06:56:27 +0100

    refactor: use givewp instead of give

--- src/Campaigns/Blocks/CampaignGoal/app/index.tsx
+++ src/Campaigns/Blocks/CampaignGoal/app/index.tsx
@@ -6,25 +6,25 @@ import './styles.scss';
 
 export default ({campaign}: { campaign: Campaign }) => {
     return (
-        <div className="give-campaign-goal">
-            <div className="give-campaign-goal__container">
-                <div className="give-campaign-goal__container-item">
+        <div className="givewp-campaign-goal">
+            <div className="givewp-campaign-goal__container">
+                <div className="givewp-campaign-goal__container-item">
                     <span>{getGoalDescription(campaign.goalType)}</span>
                     <strong>
                         {getGoalFormattedValue(campaign.goalType, campaign.goalStats.actual)}
                     </strong>
                 </div>
-                <div className="give-campaign-goal__container-item">
+                <div className="givewp-campaign-goal__container-item">
                     <span>{__('Our goal', 'give')}</span>
                     <strong>
                         {getGoalFormattedValue(campaign.goalType, campaign.goal)}
                     </strong>
                 </div>
             </div>
-            <div className="give-campaign-goal__progress-bar">
-                <div className="give-campaign-goal__progress-bar-container">
+            <div className="givewp-campaign-goal__progress-bar">
+                <div className="givewp-campaign-goal__progress-bar-container">
                     <div
-                        className="give-campaign-goal__progress-bar-progress"
+                        className="givewp-campaign-goal__progress-bar-progress"
                         style={{width: `${campaign.goalStats.percentage}%`}}>
                     </div>
                 </div>
