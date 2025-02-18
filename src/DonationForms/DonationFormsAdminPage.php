a832c022c259f72c4530e30c5528d8e46668bd96
Jon Waldstein <jonwaldstein@mac.mynetworksettings.com>
2025-02-18 14:20:40 -0500

    fix: correct merge for removing add form options

--- src/DonationForms/DonationFormsAdminPage.php
+++ src/DonationForms/DonationFormsAdminPage.php
@@ -17,8 +17,5 @@ class DonationFormsAdminPage
     public function addFormSubmenuLink()
     {
         remove_submenu_page('edit.php?post_type=give_forms', 'post-new.php?post_type=give_forms');
-        add_submenu_page('edit.php?post_type=give_forms', __('Add Form', 'give'), __('Add Form', 'give'),
-            'edit_give_forms',
-            'edit.php?post_type=give_forms&page=givewp-form-builder&locale=' . Language::getLocale(), '', 1);
     }
 }
