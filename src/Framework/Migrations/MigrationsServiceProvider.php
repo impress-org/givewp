9d498abe424ba02fdfd1b22b8a1b62bd441b0c1d
alaca <ante.laca@gmail.com>
2025-02-21 13:05:26 +0100

    refactor: migration run hook

--- src/Framework/Migrations/MigrationsServiceProvider.php
+++ src/Framework/Migrations/MigrationsServiceProvider.php
@@ -30,6 +30,8 @@ class MigrationsServiceProvider implements ServiceProvider
     {
         Hooks::addAction('admin_init', ManualMigration::class, '__invoke', 0);
         Hooks::addAction('admin_init', MigrationsRunner::class, 'run', 0);
-        Hooks::addAction('give_upgrades', MigrationsRunner::class, 'run', 0);
+        //Hooks::addAction('give_upgrades', MigrationsRunner::class, 'run', 0);
+        // running batch actions via cron doesn't trigger give_upgrades and all registered actions fail
+        Hooks::addAction('action_scheduler_init', MigrationsRunner::class, 'run');
     }
 }
