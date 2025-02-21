f04c773e88dc7de21cbd54307d3f9449a48db7b4
Kyle B. Johnson <kjohnson@users.noreply.github.com>
2025-02-20 22:07:37 -0500

    Update src/Campaigns/Actions/RedirectDisabledCampaignPage.php
    Co-authored-by: Jon Waldstein <jon@givewp.com>

--- src/Campaigns/Actions/RedirectDisabledCampaignPage.php
+++ src/Campaigns/Actions/RedirectDisabledCampaignPage.php
@@ -24,8 +24,6 @@ class RedirectDisabledCampaignPage
             global $wp_query;
             $wp_query->set_404();
             status_header(404);
-            get_template_part(404);
-            exit;
         }
     }
 }
