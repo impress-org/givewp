b1001271482cd8f41341d978978bdf31cc6e5b65
alaca <ante.laca@gmail.com>
2025-02-24 08:55:24 +0100

    chore: update docblock

--- src/Framework/Support/Facades/ActionScheduler/AsBackgroundJobs.php
+++ src/Framework/Support/Facades/ActionScheduler/AsBackgroundJobs.php
@@ -7,12 +7,9 @@ use Give\Framework\Support\Facades\Facade;
 /**
  * @since 3.6.0
  *
- * @unreleased
- *  - added enqueueAction method
- *  - switch parameter $status position with $returnFormat position
+ * @unreleased getActionsByGroup - switch parameter $status position with $returnFormat position
  *
  * @method static int enqueueAsyncAction(string $hook, array $args, string $group, bool $unique = false, int $priority = 10)
- * @method static int enqueueAction(int $timestamp, string $hook, array $args, string $group, bool $unique = false, int $priority = 10)
  * @method static array getActionByHookArgsGroup(string $hook, array $args, string $group, string $returnFormat = OBJECT, string $status = '')
  * @method static array getActionsByGroup(string $group, string $status = '', string $returnFormat = OBJECT)
  * @method static int deleteActionsByGroup(string $group, string $status = '')
