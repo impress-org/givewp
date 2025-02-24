9d07d6ae221a0db660bcab995ebadbec6b800276
alaca <ante.laca@gmail.com>
2025-02-24 08:13:55 +0100

    refactor: extend BaseMigration class

--- src/Framework/Migrations/Contracts/Migration.php
+++ src/Framework/Migrations/Contracts/Migration.php
@@ -2,75 +2,20 @@
 
 namespace Give\Framework\Migrations\Contracts;
 
-use Give\Framework\Exceptions\Primitives\RuntimeException;
-
 /**
  * Class Migration
  *
  * Extend this class when create database migration. up and timestamp are required member functions
  *
+ * @unreleased extend BaseMigration class
  * @since 2.9.0
  */
-abstract class Migration
+abstract class Migration extends BaseMigration
 {
     /**
      * Bootstrap migration logic.
      *
-     * @unreleased this method is now optional if migration class implements BatchMigration interface
      * @since 2.9.0
      */
-    public function run()
-    {
-        if ( ! is_subclass_of($this, BatchMigration::class)) {
-            throw new RuntimeException('run method is not defined.');
-        }
-    }
-
-    /**
-     * Return a unique identifier for the migration
-     *
-     * @return string
-     */
-    public static function id()
-    {
-        throw new RuntimeException('A unique ID must be provided for the migration');
-    }
-
-    /**
-     * Return a Unix Timestamp for when the migration was created
-     *
-     * Example: strtotime( '2020-09-16 12:30:00')
-     *
-     * @since 2.9.0
-     *
-     * @return int Unix timestamp for when the migration was created
-     */
-    public static function timestamp()
-    {
-        throw new RuntimeException('This method must be overridden to return a valid unix timestamp');
-    }
-
-    /**
-     * Return migration title
-     *
-     * @since 2.10.0
-     *
-     * @return string
-     */
-    public static function title()
-    {
-        return static::id();
-    }
-
-    /**
-     * Return migration source
-     *
-     * @since 2.10.0
-     *
-     * @return string
-     */
-    public static function source()
-    {
-        return esc_html__('GiveWP Core', 'give');
-    }
+    abstract public function run();
 }
