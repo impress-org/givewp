dc5bf12545ee68491a3d9f65ace9c52079ab8c8e
alaca <ante.laca@gmail.com>
2025-02-25 14:23:43 +0100

    feature: add new methods

--- src/Framework/Migrations/Contracts/BatchMigration.php
+++ src/Framework/Migrations/Contracts/BatchMigration.php
@@ -24,10 +24,37 @@ abstract class BatchMigration extends BaseMigration
      */
     abstract public function getItemsCount(): int;
 
+
+    /**
+     *
+     * Get the first and the last item ID for a batch
+     *
+     * @unreleased
+     *
+     * @return array<int, int>
+     */
+    abstract public function getBatchItemsAfter($lastId): ?array;
+
     /**
      * Run batch
      *
+     * @param $firstId - first item ID in batch
+     * @param $lastId  - last item ID in batch
+     *
      * @unreleased
      */
-    abstract public function runBatch($batchNumber);
+    abstract public function runBatch($firstId, $lastId);
+
+    /**
+     * Last step of the migration process
+     *
+     * The purpose of this method is to check if we have new items that came during the migration.
+     *
+     * @unreleased
+     *
+     * @param $lastProcessedId
+     *
+     * @return bool - true if there are new items, otherwise false
+     */
+    abstract public function hasIncomingData($lastProcessedId): ?bool;
 }
