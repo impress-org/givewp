d709c03fec858ead32aeaba64c65f591a28f7306
Jon Waldstein <jonwaldstein@mac.mynetworksettings.com>
2025-03-06 13:59:44 -0500

    feature: add new menu badge

--- src/Campaigns/ServiceProvider.php
+++ src/Campaigns/ServiceProvider.php
@@ -56,6 +56,7 @@ class ServiceProvider implements ServiceProviderInterface
         $this->registerCampaignBlocks();
         $this->setupCampaignForms();
         $this->loadCampaignOptions();
+        $this->addNewBadgeToMenu();
     }
 
     /**
@@ -193,5 +194,39 @@ class ServiceProvider implements ServiceProviderInterface
     private function loadCampaignOptions()
     {
         Hooks::addAction('init', LoadCampaignOptions::class);
+    }
+
+    /**
+     * @unreleased
+     *
+     * @return void
+     */
+    private function addNewBadgeToMenu(): void
+    {
+        // only show bade for existing users who have upgraded from a version prior to 4.0.0
+        if (version_compare((string)get_option('give_version_upgraded_from', ''), '4.0.0', '>=')) {
+            return;
+        }
+
+        // only show badge if not dismissed
+        if (get_option('givewp_new_notification_campaigns_dismissed', false) !== false) {
+            return;
+        }
+
+        // add "NEW" badge to the GiveWP menu item
+         add_action( 'admin_menu', function() {
+            global $menu;
+            array_walk($menu, static function (&$item) {
+                if ($item[0] === 'GiveWP') {
+                    $title = $item[0];
+                    $item[0] = sprintf('<span>%s </span><span class="update-plugins">%s</span>', $title, __('NEW', 'give'));
+                }
+            });
+        });
+
+         // dismiss the notice when visiting the campaigns list page
+         if (isset($_GET['page']) && $_GET['page'] === 'give-campaigns') {
+            update_option('givewp_new_notification_campaigns_dismissed', true);
+         }
     }
 }
