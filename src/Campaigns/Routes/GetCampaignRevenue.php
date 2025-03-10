5762cbf30cf85dc08068934f29ef0c493a03e791
Jon Waldstein <jonwaldstein@mac.mynetworksettings.com>
2025-03-10 17:49:02 -0400

    refactor: update revenue request to backfill each day

--- src/Campaigns/Routes/GetCampaignRevenue.php
+++ src/Campaigns/Routes/GetCampaignRevenue.php
@@ -2,6 +2,11 @@
 
 namespace Give\Campaigns\Routes;
 
+use DateInterval;
+use DateMalformedPeriodStringException;
+use \DatePeriod;
+use DateTime;
+use \DateTimeInterface;
 use Exception;
 use Give\API\RestRoute;
 use Give\Campaigns\CampaignDonationQuery;
@@ -43,6 +48,19 @@ class GetCampaignRevenue implements RestRoute
     }
 
     /**
+     *
+     * This request returns revenue for a campaign by day.
+     * The result will include all days between either the (campaign start date or oldest result) and the current date.
+     *
+     * @example
+     *
+     * $data = [
+     * ['date' => '2025-01-01', 'amount' => 100],
+     * ['date' => '2025-01-02', 'amount' => 0],
+     * ['date' => '2025-01-03', 'amount' => 200],
+     * ['date' => '2025-01-04', 'amount' => 0],
+     * ];
+     *
      * @unreleased
      *
      * @throws Exception
@@ -58,16 +76,57 @@ class GetCampaignRevenue implements RestRoute
         $query = new CampaignDonationQuery($campaign);
         $results = $query->getDonationsByDay();
 
+        if (empty($results)){
+            return new WP_REST_Response([], 200);
+        }
+
+        $resultMap = [];
+        foreach ($results as $result) {
+            $resultMap[$result->date] = $result->amount;
+        }
+
+        $firstResultDate = new DateTime($results[0]->date);
+        $lastResultDate = new DateTime($results[count($results) - 1]->date);
+
+        $queryStartDate = ($firstResultDate < $campaign->startDate) ? $firstResultDate : $campaign->startDate;
+        $campaignEndDate = current_datetime();
+        $queryEndDate = ($lastResultDate > $campaignEndDate) ? $lastResultDate : $campaignEndDate;
+
+        $dates = $this->getDatesFromRange($queryStartDate, $queryEndDate);
+
         $data = [];
-        if (!empty($results)) {
-            foreach ($results as $result) {
-                $data[] = [
-                    'date' => $result->date,
-                    'amount' => $result->amount
-                ];
-            }
+        foreach($dates as $date) {
+            $data[] = [
+                'date' => $date,
+                'amount' => $resultMap[$date] ?? 0
+            ];
         }
 
         return new WP_REST_Response($data, 200);
+    }
+
+    /**
+     * @unreleased
+     * @throws DateMalformedPeriodStringException
+     */
+    public function getDatesFromRange(DateTimeInterface $startDate, DateTimeInterface $endDate): array
+    {
+        // Include the end date
+        $endDate->modify('+1 day');
+
+        $startDateInterval = $startDate->diff($endDate);
+        if ($startDateInterval->days < 7) {
+            $startDate->modify('-7 days');
+        }
+
+        $interval = new DateInterval('P1D');
+        $dateRange = new DatePeriod($startDate, $interval, $endDate);
+
+        $days = [];
+        foreach ($dateRange as $date) {
+            $days[] = $date->format('Y-m-d');
+        }
+
+        return $days;
     }
 }
