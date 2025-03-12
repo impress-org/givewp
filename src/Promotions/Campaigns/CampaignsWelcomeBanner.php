042d3b975c3ef89bbeb9724cfda166b30ea28b6d
Joshua Dinh <josh@joshuadinh.tech>
2025-03-11 22:47:03 -0700

    fix: translate text

--- src/Promotions/Campaigns/CampaignsWelcomeBanner.php
+++ src/Promotions/Campaigns/CampaignsWelcomeBanner.php
@@ -44,44 +44,60 @@ class CampaignsWelcomeBanner
         $campaignsPageUrl = admin_url('admin.php?page=give-campaigns');
 
         return "
-            <div {$elements->customWrapperAttributes} class='givewp-campaign-welcome-banner-background' style='background-image: url(\"$backgroundUrl\") no-repeat right;'>
-               <div class='givewp-campaign-welcome-banner'>
-                <div class='givewp-campaign-welcome-banner__actions'>
-                    <div class='givewp-campaign-welcome-banner__actions__badge'><img src='$badgeIconUrl' alt='badge'/> NEW</div>
-                    <button type='button' class='givewp-campaign-welcome-banner__actions__dismiss' {$elements->closeAttributes()}>
-                    <img src='$exitIconUrl' alt='heart'>
-                    </button>
+    <div {$elements->customWrapperAttributes} class='givewp-campaign-welcome-banner-background'
+         style='background-image: url(\"$backgroundUrl\"); no-repeat right;'>
+
+        <div class='givewp-campaign-welcome-banner'>
+            <div class='givewp-campaign-welcome-banner__actions'>
+                <div class='givewp-campaign-welcome-banner__actions__badge'>
+                    <img src='$badgeIconUrl' alt='badge'/> " . esc_html__('NEW', 'give') . "
                 </div>
-                <h2 class='givewp-campaign-welcome-banner__title'>Introducing Campaigns in Give 4.0 ! 🎉</h2>
-                <p class='givewp-campaign-welcome-banner__description'>Say hello to a whole new way to supercharge your fundraising! 🚀</p>
-                <div  class='givewp-campaign-welcome-banner__features'>
-                    <div class='givewp-campaign-welcome-banner__features__group'>
-                         <div class='givewp-campaign-welcome-banner__features__group__item'>
-                            <img src='$heartIconUrl' alt='heart'>
-                            <span>Easily create, manage, and level up your campaigns.</span>
-                         </div>
-                         <div class='givewp-campaign-welcome-banner__features__group__item'>
-                            <img src='$heartIconUrl' alt='heart'>
-                            <span>Enjoy tailored reports—all in one awesome place!</span>
-                         </div>
+                <button type='button' class='givewp-campaign-welcome-banner__actions__dismiss'
+                        {$elements->closeAttributes()}>
+                    <img src='$exitIconUrl' alt='heart'>
+                </button>
+            </div> <!-- End actions -->
+
+            <h2 class='givewp-campaign-welcome-banner__title'>
+                " . esc_html__('Introducing Campaigns in Give 4.0 ! 🎉', 'give') . "
+            </h2>
+            <p class='givewp-campaign-welcome-banner__description'>
+                " . esc_html__('Say hello to a whole new way to supercharge your fundraising! 🚀', 'give') . "
+            </p>
+
+            <div class='givewp-campaign-welcome-banner__features'>
+                <div class='givewp-campaign-welcome-banner__features__group'>
+                    <div class='givewp-campaign-welcome-banner__features__group__item'>
+                        <img src='$heartIconUrl' alt='heart'>
+                        <span>" . esc_html__('Easily create, manage, and level up your campaigns.', 'give') . "</span>
                     </div>
 
-                    <div class='givewp-campaign-welcome-banner__features__group'>
-                         <div class='givewp-campaign-welcome-banner__features__group__item'>
-                            <img src='$heartIconUrl' alt='heart'>
-                            <span>Add as many donation forms as you want.</span>
-                         </div>
-                        <div class='givewp-campaign-welcome-banner__features__group__item'>
-                            <img src='$heartIconUrl' alt='heart'>
-                            <span>Update your campaigns on the fly.</span>
-                        </div>
+                    <div class='givewp-campaign-welcome-banner__features__group__item'>
+                        <img src='$heartIconUrl' alt='heart'>
+                        <span>" . esc_html__('Enjoy tailored reports—all in one awesome place!', 'give') . "</span>
                     </div>
-                </div>
-                <a class='givewp-campaign-welcome-banner__cta-button' href='$campaignsPageUrl' >
-                    <span class='givewp-campaign-welcome-banner'>Explore campaigns</span>
-                </a>
-              </div>
-            </div>
-    ";
+                </div> <!-- End features group -->
+
+                <div class='givewp-campaign-welcome-banner__features__group'>
+                    <div class='givewp-campaign-welcome-banner__features__group__item'>
+                        <img src='$heartIconUrl' alt='heart'>
+                        <span>" . esc_html__('Add as many donation forms as you want.', 'give') . "</span>
+                    </div>
+
+                    <div class='givewp-campaign-welcome-banner__features__group__item'>
+                        <img src='$heartIconUrl' alt='heart'>
+                        <span>" . esc_html__('Update your campaigns on the fly.', 'give') . "</span>
+                    </div>
+                </div> <!-- End features group -->
+            </div> <!-- End features -->
+
+            <a class='givewp-campaign-welcome-banner__cta-button' href='$campaignsPageUrl'>
+                <span class='givewp-campaign-welcome-banner'>
+                    " . esc_html__('Explore campaigns', 'give') . "
+                </span>
+            </a>
+        </div> <!-- End campaign welcome banner -->
+    </div> <!-- End campaign welcome banner background -->
+";
     }
 }
