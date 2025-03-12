9d0821e05dc3e97e04a5872c85f65c259fad5a4f
Joshua Dinh <josh@joshuadinh.tech>
2025-03-12 11:39:51 -0700

    refactor: load as template with view class

--- src/Promotions/Campaigns/CampaignsWelcomeBanner.php
+++ src/Promotions/Campaigns/CampaignsWelcomeBanner.php
@@ -4,6 +4,7 @@ namespace Give\Promotions\Campaigns;
 
 namespace Give\Promotions\Campaigns;
 
+use Give\Framework\Views\View;
 use Give\Vendors\StellarWP\AdminNotices\AdminNotice;
 use Give\Vendors\StellarWP\AdminNotices\AdminNotices;
 use Give\Vendors\StellarWP\AdminNotices\DataTransferObjects\NoticeElementProperties;
@@ -43,61 +44,17 @@ class CampaignsWelcomeBanner
         $exitIconUrl = GIVE_PLUGIN_URL . 'assets/src/images/admin/promotions/campaigns/dismiss-icon.svg';
         $campaignsPageUrl = admin_url('admin.php?page=give-campaigns');
 
-        return "
-    <div {$elements->customWrapperAttributes} class='givewp-campaign-welcome-banner-background'
-         style='background-image: url(\"$backgroundUrl\"); no-repeat right;'>
-
-        <div class='givewp-campaign-welcome-banner'>
-            <div class='givewp-campaign-welcome-banner__actions'>
-                <div class='givewp-campaign-welcome-banner__actions__badge'>
-                    <img src='$badgeIconUrl' alt='badge'/> " . esc_html__('NEW', 'give') . "
-                </div>
-                <button type='button' class='givewp-campaign-welcome-banner__actions__dismiss'
-                        {$elements->closeAttributes()}>
-                    <img src='$exitIconUrl' alt='heart'>
-                </button>
-            </div> <!-- End actions -->
-
-            <h2 class='givewp-campaign-welcome-banner__title'>
-                " . esc_html__('Introducing Campaigns in Give 4.0 ! 🎉', 'give') . "
-            </h2>
-            <p class='givewp-campaign-welcome-banner__description'>
-                " . esc_html__('Say hello to a whole new way to supercharge your fundraising! 🚀', 'give') . "
-            </p>
-
-            <div class='givewp-campaign-welcome-banner__features'>
-                <div class='givewp-campaign-welcome-banner__features__group'>
-                    <div class='givewp-campaign-welcome-banner__features__group__item'>
-                        <img src='$heartIconUrl' alt='heart'>
-                        <span>" . esc_html__('Easily create, manage, and level up your campaigns.', 'give') . "</span>
-                    </div>
-
-                    <div class='givewp-campaign-welcome-banner__features__group__item'>
-                        <img src='$heartIconUrl' alt='heart'>
-                        <span>" . esc_html__('Enjoy tailored reports—all in one awesome place!', 'give') . "</span>
-                    </div>
-                </div> <!-- End features group -->
-
-                <div class='givewp-campaign-welcome-banner__features__group'>
-                    <div class='givewp-campaign-welcome-banner__features__group__item'>
-                        <img src='$heartIconUrl' alt='heart'>
-                        <span>" . esc_html__('Add as many donation forms as you want.', 'give') . "</span>
-                    </div>
-
-                    <div class='givewp-campaign-welcome-banner__features__group__item'>
-                        <img src='$heartIconUrl' alt='heart'>
-                        <span>" . esc_html__('Update your campaigns on the fly.', 'give') . "</span>
-                    </div>
-                </div> <!-- End features group -->
-            </div> <!-- End features -->
-
-            <a class='givewp-campaign-welcome-banner__cta-button' href='$campaignsPageUrl'>
-                <span class='givewp-campaign-welcome-banner'>
-                    " . esc_html__('Explore campaigns', 'give') . "
-                </span>
-            </a>
-        </div> <!-- End campaign welcome banner -->
-    </div> <!-- End campaign welcome banner background -->
-";
+        return View::load(
+            'Promotions.CampaignWelcomeBanner',
+            [
+                'elements'         => $elements,
+                'backgroundUrl'    => $backgroundUrl,
+                'badgeIconUrl'     => $badgeIconUrl,
+                'heartIconUrl'     => $heartIconUrl,
+                'exitIconUrl'      => $exitIconUrl,
+                'campaignsPageUrl' => $campaignsPageUrl,
+            ],
+            false
+        );
     }
 }
