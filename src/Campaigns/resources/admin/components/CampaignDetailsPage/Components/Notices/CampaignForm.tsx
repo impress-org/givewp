058dc7236c961d1c447da8f33e8d30dc1a368870
alaca <ante.laca@gmail.com>
2025-03-19 16:57:51 +0100

    feature: update notice

--- src/Campaigns/resources/admin/components/CampaignDetailsPage/Components/Notices/CampaignForm.tsx
+++ src/Campaigns/resources/admin/components/CampaignDetailsPage/Components/Notices/CampaignForm.tsx
@@ -1,25 +1,22 @@
 import {__} from '@wordpress/i18n';
+import cx from 'classnames';
 import {ExternalLink} from '@wordpress/components';
 import {CloseIcon} from '@givewp/campaigns/admin/components/Icons';
 
-import styles from './styles.module.scss';
+import styles from './Notices.module.scss';
 
 export default ({handleClick}) => (
-    <div className={styles.tooltip} style={{
-        position: 'absolute',
-        left: 250,
-        top: 150,
-    }}>
-        <div className={styles.close} onClick={handleClick}>
+    <div className={cx(styles['tooltip'], styles['campaignForm'])}>
+        <div className={styles['close']} onClick={handleClick}>
             <CloseIcon />
         </div>
         <h3>
             {__('Campaign Form', 'give')}
         </h3>
-        <div className={styles.content}>
+        <div className={styles['content']}>
             {__('Get a quick view of all the forms associated with your campaign in the forms page. You can edit and add multiple forms to your campaign.', 'give')}
         </div>
-        <div className={styles.content}>
+        <div className={styles['content']}>
             <ExternalLink
                 href="#" //todo: add link
                 onClick={handleClick}
--- src/Campaigns/resources/admin/components/CampaignDetailsPage/Components/Notices/CampaignList.tsx
+++ src/Campaigns/resources/admin/components/CampaignDetailsPage/Components/Notices/CampaignList.tsx
@@ -1,25 +1,22 @@
 import {__} from '@wordpress/i18n';
+import cx from 'classnames';
 import {ExternalLink} from '@wordpress/components';
 import {CloseIcon} from '@givewp/campaigns/admin/components/Icons';
 
-import styles from './styles.module.scss';
+import styles from './Notices.module.scss';
 
 export default ({handleClick}) => (
-    <div className={styles.tooltip} style={{
-        position: 'absolute',
-        left: 20,
-        top: 90,
-    }}>
-        <div className={styles.close} onClick={handleClick}>
+    <div className={cx(styles['tooltip'], styles['campaignList'])}>
+        <div className={styles['close']} onClick={handleClick}>
             <CloseIcon />
         </div>
         <h3>
             {__('Campaign List', 'give')}
         </h3>
-        <div className={styles.content}>
+        <div className={styles['content']}>
             {__('We\'ve created a campaign from each of your donation forms. Your forms still work as before, but now with the added power of campaign management! Select a campaign to see how you can seamlessly manage your online fundraising.', 'give')}
         </div>
-        <div className={styles.content}>
+        <div className={styles['content']}>
             <ExternalLink
                 href="#" //todo: add link
                 onClick={handleClick}
