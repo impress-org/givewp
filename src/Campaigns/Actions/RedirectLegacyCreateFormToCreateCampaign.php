d94d4d3f3a96984de815216759cc300cf5f5c5dc
alaca <ante.laca@gmail.com>
2025-03-28 11:47:27 +0100

    initial commit

--- src/Campaigns/Actions/RedirectLegacyCreateFormToCreateCampaign.php
+++ src/Campaigns/Actions/RedirectLegacyCreateFormToCreateCampaign.php
@@ -14,49 +14,17 @@ class RedirectLegacyCreateFormToCreateCampaign
      */
     public function __invoke()
     {
-        if ( ! $this->isAddingNewForm()) {
-            return;
+        if (
+            $GLOBALS['pagenow'] === 'post-new.php'
+            && isset($_GET['post_type']) && $_GET['post_type'] === 'give_forms'
+        ) {
+            if (
+                ! isset($_GET['campaignId'])
+                || ! (Campaign::find(absint($_GET['campaignId'])))
+            ) {
+                wp_safe_redirect(admin_url('edit.php?post_type=give_forms&page=give-campaigns&new=campaign'));
+                exit;
+            }
         }
-
-        if ($this->isEditingCampaignForm()) {
-            return;
-        }
-
-        if ( ! isset($_GET['campaignId']) || ! (Campaign::find(absint($_GET['campaignId'])))) {
-            wp_safe_redirect(admin_url('edit.php?post_type=give_forms&page=give-campaigns&new=campaign'));
-            exit;
-        }
-    }
-
-    /**
-     * @unreleased
-     */
-    private function isAddingNewForm(): bool
-    {
-        global $pagenow;
-
-        $isOptionBasedFormEditorPage = $pagenow === 'post-new.php';
-        $isVisualFormBuilderPage = $pagenow === 'edit.php' && isset($_GET['page']) && 'givewp-form-builder' === $_GET['page'];
-        $isGiveFormsCpt = isset($_GET['post_type']) && $_GET['post_type'] === 'give_forms';
-
-        return ($isOptionBasedFormEditorPage || $isVisualFormBuilderPage) && $isGiveFormsCpt;
-    }
-
-    /**
-     * @unreleased
-     */
-    private function isEditingCampaignForm(): bool
-    {
-        global $pagenow;
-
-        $formId = $pagenow === 'post.php' && isset($_GET['post']) ? absint($_GET['post']) : 0;
-        $formId = $pagenow === 'edit.php' && isset($_GET['donationFormID'], $_GET['page']) && 'givewp-form-builder' === $_GET['page'] ? absint($_GET['donationFormID']) : $formId;
-        $isGiveFormsCpt = (isset($_GET['post_type']) && $_GET['post_type'] === 'give_forms') || (get_post_type($formId) === 'give_forms');
-
-        if ($formId && $isGiveFormsCpt) {
-            return (bool)Campaign::findByFormId($formId);
-        }
-
-        return false;
     }
 }
