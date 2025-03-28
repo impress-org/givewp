94c2490a77871226fc6993fa3010ddb0f0d0d579
alaca <ante.laca@gmail.com>
2025-03-28 12:48:09 +0100

    refactor: p2p part

--- src/Campaigns/Actions/RedirectLegacyCreateFormToCreateCampaign.php
+++ src/Campaigns/Actions/RedirectLegacyCreateFormToCreateCampaign.php
@@ -3,7 +3,6 @@
 namespace Give\Campaigns\Actions;
 
 use Give\Campaigns\Models\Campaign;
-use GiveP2P\P2P\Repositories\CampaignRepository;
 
 /**
  * @unreleased
@@ -35,10 +34,10 @@ class RedirectLegacyCreateFormToCreateCampaign
             && isset($_GET['post_type']) && $_GET['post_type'] === 'give_forms'
             && isset($_GET['page']) && $_GET['page'] === 'givewp-form-builder'
         ) {
-            if (isset($_GET['p2p'])) {
+            if (defined('GIVE_P2P_VERSION') && isset($_GET['p2p'])) {
                 if (
                     ! isset($_GET['donationFormID'])
-                    || ! give(CampaignRepository::class)->getByFormId($_GET['donationFormID'])
+                    || ! give(\GiveP2P\P2P\Repositories\CampaignRepository::class)->getByFormId($_GET['donationFormID'])
                 ) {
                     wp_safe_redirect(admin_url('edit.php?post_type=give_forms&page=p2p-add-campaign'));
                     exit;
