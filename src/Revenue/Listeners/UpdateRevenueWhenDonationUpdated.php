c2f30a9eee9083f1f8cfd1220709b355cf67c15f
Jon Waldstein <jonwaldstein@mac.mynetworksettings.com>
2025-04-08 13:56:39 -0400

    chore: merge develop and update changelog

--- src/Revenue/Listeners/UpdateRevenueWhenDonationUpdated.php
+++ src/Revenue/Listeners/UpdateRevenueWhenDonationUpdated.php
@@ -6,13 +6,13 @@ use Give\Donations\Models\Donation;
 use Give\Revenue\Repositories\Revenue;
 
 /**
- * @unreleased Updated class to support update other properties
+ * @since 4.1.0 Updated class to support update other properties
  * @since 2.22.1
  */
 class UpdateRevenueWhenDonationUpdated
 {
     /**
-     * @unreleased Added support to update revenue campaignId
+     * @since 4.1.0 Added support to update revenue campaignId
      * @since 3.3.0 updated to accept Donation model
      * @since 2.22.1
      */
