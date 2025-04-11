24779ec19eedd2e2294cbef8f962b793358a4cfb
Joshua Dinh <josh@joshuadinh.tech>
2025-04-11 11:21:06 -0700

    Feature: register shortcodes

---
+++ src/Campaigns/Actions/RegisterCampaignShortcodes.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Give\Campaigns\Actions;
+
+use Give\Campaigns\Blocks\Campaign\CampaignShortcode;
+use Give\Campaigns\Blocks\CampaignGrid\CampaignGridShortcode;
+
+/**
+ * @unreleased
+ */
+class RegisterCampaignShortcodes
+{
+    /**
+     * @unreleased
+     */
+    public function __invoke()
+    {
+        add_shortcode('givewp_campaign', [new CampaignShortcode(), 'renderShortcode']);
+        add_shortcode('givewp_campaign_grid', [new CampaignGridShortcode(), 'renderShortcode']);
+    }
+}
