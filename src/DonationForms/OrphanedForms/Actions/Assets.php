4c1938e9b66fa3729fb159fe7b0f8a81035967b6
alaca <ante.laca@gmail.com>
2025-04-15 17:33:57 +0200

    refactor: enqueue scripts

--- src/DonationForms/OrphanedForms/Actions/Assets.php
+++ src/DonationForms/OrphanedForms/Actions/Assets.php
@@ -2,7 +2,7 @@
 
 namespace Give\DonationForms\OrphanedForms\Actions;
 
-use Give\Framework\Support\Facades\Scripts\ScriptAsset;
+use Give\Helpers\EnqueueScript;
 
 /**
  * @unreleased
@@ -14,18 +14,16 @@ class Assets
      */
     public function __invoke()
     {
-        if ( 'enabled' !== give_get_option('show_orphaned_forms_table')) {
+        if ('enabled' !== give_get_option('show_orphaned_forms_table')) {
             return;
         }
 
-        $scriptAsset = ScriptAsset::get(GIVE_PLUGIN_DIR . 'build/orphanedFormsListTable.asset.php');
-
-        wp_enqueue_script(
-            'givewp-orphaned-forms-list-table',
-            GIVE_PLUGIN_URL . 'build/orphanedFormsListTable.js',
-            $scriptAsset['dependencies'],
-            $scriptAsset['version'],
-            true
-        );
+        EnqueueScript::make(
+            'orphaned-forms-list-table',
+            'build/assets/dist/js/orphaned-forms-list-table.js'
+        )
+            ->loadInFooter()
+            ->registerTranslations()
+            ->enqueue();
     }
 }
