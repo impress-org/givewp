498e79222a1c4f130850b9dbca373e0b6d7619c9
Joshua Dinh <josh@joshuadinh.tech>
2025-04-23 04:23:05 -0700

    feature: render v2 forms on the frontend

--- src/Campaigns/Blocks/CampaignForm/render.php
+++ src/Campaigns/Blocks/CampaignForm/render.php
@@ -2,7 +2,11 @@
 
 use Give\Campaigns\Models\Campaign;
 use Give\Campaigns\Repositories\CampaignRepository;
+use Give\DonationForms\Actions\GenerateDonationFormViewRouteUrl;
 use Give\DonationForms\Blocks\DonationFormBlock\Controllers\BlockRenderController;
+use Give\Helpers\Frontend\ConfirmDonation;
+use Give\Views\IframeView;
+use Give\Helpers\Form\Utils as FormUtils;
 
 /**
  * @var array    $attributes
@@ -11,18 +15,33 @@ use Give\DonationForms\Blocks\DonationFormBlock\Controllers\BlockRenderControlle
  * @unrelesed
  */
 
-if (
-    ! isset($attributes['campaignId']) ||
+if (! isset($attributes['campaignId']) ||
     ! ($campaign = give(CampaignRepository::class)->getById($attributes['campaignId']))
 ) {
     return;
 }
 
-echo (new BlockRenderController())->render([
-    'formId'           => $attributes['id'] ?? null,
-    'blockId'          => $attributes['blockId'] ?? null,
-    'openFormButton'   => esc_html($attributes['continueButtonTitle'] ?? __('Donate Now', 'give')),
-    'formFormat'       => $attributes['displayStyle'] ?? 'onpage',
-]);
+if (FormUtils::isV3Form($attributes['id'])) {
+    (new BlockRenderController())->render([
+        'formId'           => $attributes['id'] ?? null,
+        'blockId'          => $attributes['blockId'] ?? null,
+        'openFormButton'   => esc_html($attributes['continueButtonTitle'] ?? __('Donate Now', 'give')),
+        'formFormat'       => $attributes['displayStyle'] ?? 'onpage',
+    ]);
+} else {
+    ob_start();
 
-?>
+    if (!FormUtils::isLegacyForm($attributes['id'])) {
+        $showIframeInModal = 'button' === $attributes['displayStyle'];
+        $iframeView        = new IframeView();
+
+        ConfirmDonation::storePostedDataInDonationSession();
+
+        echo $iframeView->setFormId($attributes['id'])
+            ->showInModal($showIframeInModal)
+            ->setButtonTitle($attributes['continueButtonTitle'])
+            ->render();
+    } else {
+        echo give_get_donation_form($attributes);
+    }
+}
