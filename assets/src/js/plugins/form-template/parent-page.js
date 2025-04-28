63cbe14d48fc44040da5d1ced0bd8cb6f05c0158
Joshua Dinh <josh@joshuadinh.tech>
2025-04-28 12:55:00 -0700

    doc: add unreleased tag to modal buttons

--- assets/src/js/plugins/form-template/parent-page.js
+++ assets/src/js/plugins/form-template/parent-page.js
@@ -1,121 +1,125 @@
-import { initializeIframeResize } from './utils';
+import {initializeIframeResize} from './utils';
 
-jQuery( function() {
-	// This script is only for parent page.
-	if ( document.querySelector( 'body.give-form-templates' ) ) {
-		return false;
-	}
+jQuery(function () {
+    // This script is only for parent page.
+    if (document.querySelector('body.give-form-templates')) {
+        return false;
+    }
 
-	/**
-	 * Show hide iframe modal.
-	 *
-	 * @since 2.7.0
-	 */
-	document.querySelectorAll( '.js-give-embed-form-modal-opener' ).forEach( function( button ) {
-		button.addEventListener( 'click', function() {
-			const iframeContainer = document.getElementById( button.getAttribute( 'data-form-id' ) ),
-				iframe = iframeContainer.querySelector( 'iframe[name="give-embed-form"]' ),
-				iframeURL = iframe.getAttribute( 'data-src' );
+    /**
+     * Show hide iframe modal.
+     *
+     * @since 2.7.0
+     */
+    document.querySelectorAll('.js-give-embed-form-modal-opener').forEach(function (button) {
+        button.addEventListener('click', function () {
+            const iframeContainer = document.getElementById(button.getAttribute('data-form-id')),
+                iframe = iframeContainer.querySelector('iframe[name="give-embed-form"]'),
+                iframeURL = iframe.getAttribute('data-src');
 
-			// Load iframe.
-			if ( iframeURL ) {
-				iframe.setAttribute( 'src', iframeURL );
-				iframe.setAttribute( 'data-src', '' );
+            // Load iframe.
+            if (iframeURL) {
+                iframe.setAttribute('src', iframeURL);
+                iframe.setAttribute('data-src', '');
 
-				initializeIframeResize( iframe );
-			}
+                initializeIframeResize(iframe);
+            }
 
-			document.documentElement.style.overflow = 'hidden';
+            document.documentElement.style.overflow = 'hidden';
 
-			iframeContainer.classList.add( 'modal' );
-			iframeContainer.classList.remove( 'is-hide' );
-		} );
-	} );
+            iframeContainer.classList.add('modal');
+            iframeContainer.classList.remove('is-hide');
+        });
+    });
 
-	document.querySelectorAll( '.js-give-embed-form-modal-closer' ).forEach( function( button ) {
-		button.addEventListener( 'click', function( evt ) {
-			evt.preventDefault();
+    document.querySelectorAll('.js-give-embed-form-modal-closer').forEach(function (button) {
+        button.addEventListener('click', function (evt) {
+            evt.preventDefault();
 
-			const iframeContainer = document.getElementById( button.getAttribute( 'data-form-id' ) );
+            const iframeContainer = document.getElementById(button.getAttribute('data-form-id'));
 
-			document.documentElement.style.overflow = '';
+            document.documentElement.style.overflow = '';
 
-			iframeContainer.classList.remove( 'modal' );
-			iframeContainer.classList.add( 'is-hide' );
-		} );
-	} );
+            iframeContainer.classList.remove('modal');
+            iframeContainer.classList.add('is-hide');
+        });
+    });
 
-	/**
-	 * Trigger click on embedded form modal launcher when click on grid item form modal launcher.
-	 *
-	 * Note: This code with make form template (other then legacy form template) compatible with form grid.
-	 */
-	document.querySelectorAll( '.js-give-grid-modal-launcher' ).forEach( function( $formModalLauncher ) {
-		$formModalLauncher.addEventListener( 'click', function() {
-			const $embedFormLauncher = $formModalLauncher.nextElementSibling.firstElementChild,
-				$magnificPopContainer = document.querySelector( '.mfp-wrap.give-modal' );
+    /**
+     * Trigger click on embedded form modal launcher when click on grid item form modal launcher.
+     *
+     * Note: This code with make form template (other then legacy form template) compatible with form grid.
+     */
+    document.querySelectorAll('.js-give-grid-modal-launcher').forEach(function ($formModalLauncher) {
+        $formModalLauncher.addEventListener('click', function () {
+            const $embedFormLauncher = $formModalLauncher.nextElementSibling.firstElementChild,
+                $magnificPopContainer = document.querySelector('.mfp-wrap.give-modal');
 
-			$magnificPopContainer && $magnificPopContainer.classList.add( 'mfp-hide' );
+            $magnificPopContainer && $magnificPopContainer.classList.add('mfp-hide');
 
-			// Exit if form has legacy form template.
-			if ( ! $embedFormLauncher ) {
-				$magnificPopContainer && $magnificPopContainer.classList.remove( 'mfp-hide' );
-				return;
-			}
+            // Exit if form has legacy form template.
+            if (!$embedFormLauncher) {
+                $magnificPopContainer && $magnificPopContainer.classList.remove('mfp-hide');
+                return;
+            }
 
-			// Do not open magnific poppup.
-			jQuery.magnificPopup.close();
+            // Do not open magnific poppup.
+            jQuery.magnificPopup.close();
 
-			$embedFormLauncher.click();
-		} );
-	} );
+            $embedFormLauncher.click();
+        });
+    });
 
     /*
      * Close form modal by clicking on the background
      * @since 2.19.6
      */
-    document.addEventListener('click', function(e){
+    document.addEventListener('click', function (e) {
         if (e.target.matches('.modal-inner-wrap') || e.target.matches('.give-embed-form-wrapper.modal')) {
-           e.target.querySelector('.js-give-embed-form-modal-closer').click();
+            e.target.querySelector('.js-give-embed-form-modal-closer').click();
         }
     });
 
+    /**
+     * Close embed form modal when press "esc" key.
+     */
+    document.addEventListener('keydown', (event) => {
+        // Exit if pressed keycode is not 27. Only listen for "esc" key
+        if (event.isComposing || event.keyCode !== 27) {
+            return;
+        }
 
-	/**
-	 * Close embed form modal when press "esc" key.
-	 */
-	document.addEventListener( 'keydown', event => {
-		// Exit if pressed keycode is not 27. Only listen for "esc" key
-		if ( event.isComposing || event.keyCode !== 27 ) {
-			return;
-		}
+        // Close modal if open.
+        const $modal = document.querySelector('.give-embed-form-wrapper.modal');
+        if ($modal) {
+            const containerId = $modal.getAttribute('id'),
+                $button = document.querySelector(`.js-give-embed-form-modal-closer[data-form-id="${containerId}"]`);
 
-		// Close modal if open.
-		const $modal = document.querySelector( '.give-embed-form-wrapper.modal' );
-		if ( $modal ) {
-			const containerId = $modal.getAttribute( 'id' ),
-				$button = document.querySelector( `.js-give-embed-form-modal-closer[data-form-id="${ containerId }"]` );
+            $button && $button.click();
+        }
+    });
 
-			$button && $button.click();
-		}
-	} );
+    window.addEventListener('load', function () {
+        /**
+         * Automatically open form if it is in modal.
+         */
+        const $iframe = document.querySelector('.modal-content iframe[data-autoScroll="1"]');
+        if ($iframe) {
+            const containerId = $iframe.parentElement.parentElement.parentElement.getAttribute('id'),
+                $button = document.querySelector(`.js-give-embed-form-modal-opener[data-form-id="${containerId}"]`);
 
-	window.addEventListener( 'load', function() {
-		/**
-		 * Automatically open form if it is in modal.
-		 */
-		const $iframe = document.querySelector( '.modal-content iframe[data-autoScroll="1"]' );
-		if ( $iframe ) {
-			const containerId = $iframe.parentElement.parentElement.parentElement.getAttribute( 'id' ),
-				$button = document.querySelector( `.js-give-embed-form-modal-opener[data-form-id="${ containerId }"]` );
+            if ($button) {
+                $button.click();
+            }
+        }
+    });
+});
 
-			if ( $button ) {
-				$button.click();
-			}
-		}
-	} );
-} );
-
+/**
+ * Initialize Give Modal buttons in the block editor.
+ *
+ * @unreleased
+ */
 function initializeGiveModalButtons() {
     document.querySelectorAll('.js-give-embed-form-modal-opener').forEach(function (button) {
         if (!button.dataset.bound) {
@@ -166,4 +170,3 @@ jQuery(function () {
 
     observer.observe(document.body, {childList: true, subtree: true});
 });
-
