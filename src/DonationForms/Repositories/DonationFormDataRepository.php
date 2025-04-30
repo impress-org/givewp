0eed7473f5920c4c21afc1d94c24f5c981c4e51d
alaca <ante.laca@gmail.com>
2025-04-30 10:39:22 +0200

    feature: add donation form data repo

---
+++ src/DonationForms/Repositories/DonationFormDataRepository.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace Give\DonationForms\Repositories;
+
+
+use Give\DonationForms\DonationDataQuery;
+use Give\DonationForms\Models\DonationForm as V3FORM;
+use Give\DonationForms\V2\Models\DonationForm;
+use Give\DonationForms\ValueObjects\GoalType;
+
+class DonationFormDataRepository
+{
+    private array $amounts;
+    private array $subscriptionAmounts = [];
+    private array $donationsCount;
+    private array $subscriptionDonationsCount = [];
+    private array $donorsCount;
+    private array $subscriptionDonorsCount = [];
+
+    /**
+     * @param int[] $ids
+     *
+     * @return DonationFormDataRepository
+     */
+    public static function forms(array $ids): DonationFormDataRepository
+    {
+        $self = new self();
+
+        $donations = DonationDataQuery::donations($ids);
+
+        $self->amounts = $donations->collectIntendedAmounts();
+        $self->donationsCount = $donations->collectDonations();
+        $self->donorsCount = $donations->collectDonors();
+
+        // Set subscriptions data
+        if (defined('GIVE_RECURRING_VERSION')) {
+            $subscriptions = DonationDataQuery::subscriptions($ids);
+
+            $self->subscriptionAmounts = $subscriptions->collectInitialAmounts();
+            $self->subscriptionDonationsCount = $subscriptions->collectDonations();
+            $self->subscriptionDonorsCount = $subscriptions->collectDonors();
+        }
+
+        return $self;
+    }
+
+    /**
+     * @unreleased
+     *
+     * Get revenue for form
+     *
+     * @param DonationForm $form
+     *
+     * @return int
+     */
+    public function getRevenue(DonationForm $form): int
+    {
+        $data = $form->settings->goalType->isSubscriptions()
+            ? $this->subscriptionAmounts
+            : $this->amounts;
+
+        foreach ($data as $row) {
+            if (isset($row['id']) && $row['id'] == $form->id) {
+                return (int)$row['sum'];
+            }
+        }
+
+        return 0;
+    }
+
+    /**
+     * @unreleased
+     *
+     * Get donations count for form
+     *
+     * @param DonationForm $form
+     *
+     * @return int
+     */
+    public function getDonationsCount(DonationForm $form): int
+    {
+        $data = $form->settings->goalType->isSubscriptions()
+            ? $this->subscriptionDonationsCount
+            : $this->donationsCount;
+
+        foreach ($data as $row) {
+            if (isset($row['id']) && $row['id'] == $form->id) {
+                return (int)$row['count'];
+            }
+        }
+
+        return 0;
+    }
+
+    /**
+     * @unreleased
+     *
+     * Get donors count for form
+     *
+     * @param DonationForm $form
+     *
+     * @return int
+     */
+    public function getDonorsCount(DonationForm $form): int
+    {
+        $data = $form->settings->goalType->isSubscriptions()
+            ? $this->subscriptionDonorsCount
+            : $this->donorsCount;
+
+        foreach ($data as $row) {
+            if (isset($row['id']) && $row['id'] == $form->id) {
+                return (int)$row['count'];
+            }
+        }
+
+        return 0;
+    }
+
+
+    /**
+     * @unreleased
+     *
+     * Get goal data for form
+     *
+     * @param DonationForm $form
+     *
+     * @return array{actual: int, goal: int, actualFormatted: string, goalFormatted:string, percentage:float}
+     */
+    public function getGoalData(DonationForm $form): array
+    {
+        $actual = $this->getActualGoal($form);
+        $percentage = $form->settings->goalAmount
+            ? $actual / $form->settings->goalAmount
+            : 0;
+
+        return [
+            'actual' => $actual,
+            'goal' => $form->settings->goalAmount,
+            'actualFormatted' => $form->settings->goalType == GoalType::AMOUNT
+                ? give_currency_filter(give_format_amount($actual))
+                : $actual,
+            'goalFormatted' => $form->settings->goalType == GoalType::AMOUNT
+                ? give_currency_filter(give_format_amount($form->settings->goalAmount))
+                : $form->settings->goalAmount,
+            'percentage' => round($percentage * 100, 2),
+        ];
+    }
+
+    /**
+     * @unreleased
+     *
+     * @param DonationForm $form
+     *
+     * @return int
+     */
+    private function getActualGoal(DonationForm $form): int
+    {
+        switch ($form->settings->goalType->getValue()) {
+            case GoalType::DONATIONS():
+            case GoalType::SUBSCRIPTIONS():
+                return $this->getDonationsCount($form);
+            case GoalType::DONORS():
+            case GoalType::DONORS_FROM_SUBSCRIPTIONS():
+                return $this->getDonorsCount($form);
+            default:
+                return $this->getRevenue($form);
+        }
+    }
+}
