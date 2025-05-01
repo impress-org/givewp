71bd581af258274313b6a0308d3d67594db0adef
Joshua Dinh <75056371+JoshuaHungDinh@users.noreply.github.com>
2025-05-01 09:57:48 -0700

    Feature: add campaign form block and shortcode (#7887)

--- src/Campaigns/Actions/RegisterCampaignShortcodes.php
+++ src/Campaigns/Actions/RegisterCampaignShortcodes.php
@@ -3,6 +3,7 @@
 namespace Give\Campaigns\Actions;
 
 use Give\Campaigns\Blocks\Campaign\CampaignShortcode;
+use Give\Campaigns\Blocks\CampaignForm\CampaignFormShortcode;
 use Give\Campaigns\Blocks\CampaignGrid\CampaignGridShortcode;
 
 /**
@@ -17,5 +18,6 @@ class RegisterCampaignShortcodes
     {
         add_shortcode('givewp_campaign', [new CampaignShortcode(), 'renderShortcode']);
         add_shortcode('givewp_campaign_grid', [new CampaignGridShortcode(), 'renderShortcode']);
+        add_shortcode('givewp_campaign_form', [new CampaignFormShortcode(), 'renderShortcode']);
     }
 }
