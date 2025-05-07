ab7d03dee7c3f4fd56e7311ee2d01b231c299cd3
Jon Waldstein <jonwaldstein@mac.mynetworksettings.com>
2025-05-07 11:09:24 -0400

    docs: add since tag

--- src/PaymentGateways/PayPalCommerce/PayPalCommerce.php
+++ src/PaymentGateways/PayPalCommerce/PayPalCommerce.php
@@ -78,6 +78,7 @@ class PayPalCommerce extends PaymentGateway
     }
 
     /**
+     * @unreleased updated to use updateOrderFromDonation
      * @since 4.1.0 updated to include 3D Secure validation
      * @since 4.0.0 updated to update and capture payment
      * @since 2.19.0
