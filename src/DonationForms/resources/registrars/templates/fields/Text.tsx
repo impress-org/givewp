fb363c53c1013ae7e6ec9f1e90977009bbe66ab5
Paulo Iankoski <me@wpdev.com.br>
2025-05-16 06:26:57 -0300

    fix: set input id to text field so it reads the label content

--- src/DonationForms/resources/registrars/templates/fields/Text.tsx
+++ src/DonationForms/resources/registrars/templates/fields/Text.tsx
@@ -14,12 +14,14 @@ export default function Text({
 }: FieldHasDescriptionProps) {
     const autoComplete = autoCompleteAttr(inputProps?.name);
     const FieldDescription = window.givewp.form.templates.layouts.fieldDescription;
+    const inputId = `givewp-fields-text-${inputProps.name}-input`;
 
     return (
-        <label className={fieldError && 'givewp-field-error-label'}>
+        <label className={fieldError && 'givewp-field-error-label'} htmlFor={inputId}>
             <Label />
             {description && <FieldDescription description={description} />}
             <input
+                id={inputId}
                 type="text"
                 aria-invalid={fieldError ? 'true' : 'false'}
                 placeholder={placeholder}
