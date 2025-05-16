9d04c0d427536e0ff20f0a675be5a01614f9be04
Paulo Iankoski <me@wpdev.com.br>
2025-05-16 06:30:19 -0300

    refactor: show error when selecting file bigger than maxUploadSize

--- src/DonationForms/resources/registrars/templates/fields/File.tsx
+++ src/DonationForms/resources/registrars/templates/fields/File.tsx
@@ -1,11 +1,12 @@
 import {FileProps} from '@givewp/forms/propTypes';
+import {__, sprintf} from '@wordpress/i18n';
 
 /**
- * @unreleased add aria-required attribute.
+ * @unreleased Add aria-required attribute and file size validation.
  */
-export default function File({Label, allowedMimeTypes, ErrorMessage, fieldError, description, inputProps}: FileProps) {
+export default function File({Label, allowedMimeTypes, maxUploadSize, ErrorMessage, fieldError, description, inputProps}: FileProps) {
     const FieldDescription = window.givewp.form.templates.layouts.fieldDescription;
-    const {setValue} = window.givewp.form.hooks.useFormContext();
+    const {setValue, setError} = window.givewp.form.hooks.useFormContext();
     const {name} = inputProps;
 
     return (
@@ -21,7 +22,14 @@ export default function File({Label, allowedMimeTypes, ErrorMessage, fieldError,
                 aria-invalid={fieldError ? 'true' : 'false'}
                 accept={allowedMimeTypes.join(',')}
                 onChange={(e) => {
-                    setValue(name, e.target.files[0]);
+                    const file = e.target.files[0];
+
+                    if (file.size > maxUploadSize) {
+                        setError(name, {message: sprintf(__('The selected file must be less than or equal to %d bytes.', 'give'), maxUploadSize)});
+                    } else {
+                        setError(name, undefined);
+                        setValue(name, file);
+                    }
                 }}
                 aria-required={inputProps['aria-required']}
             />
