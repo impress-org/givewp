e45bd97a22d1ae806b515f739b14a92f00e3eb30
Your Name <your.email@example.com>
2025-05-29 14:42:43 -0700

    doc: add unreleased tag

--- includes/donors/class-give-donor-wall.php
+++ includes/donors/class-give-donor-wall.php
@@ -82,6 +82,7 @@ class Give_Donor_Wall {
 	/**
 	 * Displays donors in a grid layout.
 	 *
+     * @unreleased remove redundant _give_redirect_form_id() function.
      * @since 3.7.0 Sanitize attributes
      * @since 2.27.0 Moved AJAX nonce verification to ajax_handler method.
 	 * @since  2.2.0
@@ -366,6 +367,7 @@ class Give_Donor_Wall {
 		$query_atts['tags']          = $atts['tags'];
 		$query_atts['only_comments'] = ( true === $atts['only_comments'] );
 		$query_atts['anonymous']     = ( true === $atts['anonymous'] );
+
 
 
 		return $query_atts;
