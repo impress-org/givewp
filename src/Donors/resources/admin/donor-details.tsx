0a7d2a0adb5ab36f68f9b42faa739490cf9df06e
Your Name <your.email@example.com>
2025-06-03 12:44:58 -0700

    fix: pass donor id

--- src/Donors/resources/admin/donor-details.tsx
+++ src/Donors/resources/admin/donor-details.tsx
@@ -3,8 +3,9 @@ import DonorDetailsPage from './components/DonorDetailsPage';
 
 const container = document.getElementById('give-admin-donors-root');
 const urlParams = new URLSearchParams(window.location.search);
+const donorId = parseInt(urlParams.get('id') ?? '0');
 
 if (container) {
     const root = createRoot(container);
-    root.render(<DonorDetailsPage />);
+    root.render(<DonorDetailsPage donorId={donorId} />);
 }
