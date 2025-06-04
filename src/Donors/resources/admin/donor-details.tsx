226eb27d5dade7dde311a19b36232e82701bbe67
Your Name <joshuahungdinh@gmail.com>
2025-06-04 13:32:09 -0700

    refactor: remove donorId

--- src/Donors/resources/admin/donor-details.tsx
+++ src/Donors/resources/admin/donor-details.tsx
@@ -2,10 +2,8 @@ import {createRoot} from '@wordpress/element';
 import DonorDetailsPage from './components/DonorDetailsPage';
 
 const container = document.getElementById('give-admin-donors-root');
-const urlParams = new URLSearchParams(window.location.search);
-const donorId = parseInt(urlParams.get('id') ?? '0');
 
 if (container) {
     const root = createRoot(container);
-    root.render(<DonorDetailsPage donorId={donorId} />);
+    root.render(<DonorDetailsPage />);
 }
