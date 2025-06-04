3600ae2dedb3d6ff4e0650d16a2bb9b157c42597
Your Name <joshuahungdinh@gmail.com>
2025-06-04 09:51:48 -0700

    feature: include status in donations endpoint

--- src/Donors/resources/hooks/useDonorDonations.ts
+++ src/Donors/resources/hooks/useDonorDonations.ts
@@ -30,14 +30,19 @@ interface DonationsQueryParams {
     page?: number;
     perPage?: number;
     mode?: 'test' | 'live';
+    status?: 'any' | 'publish' | 'give_subscription' | 'pending' | 'processing' | 'refunded' | 'revoked' | 'failed' | 'cancelled' | 'abandoned' | 'preapproval';
 }
 
-export function useDonorDonations({donorId, page = 1, perPage = 5, mode = 'live'}: DonationsQueryParams) {
+/**
+ * @unreleased
+ */
+export function useDonorDonations({donorId, page = 1, perPage = 5, mode = 'live', status = 'publish'}: DonationsQueryParams) {
     const query = {
         page,
         per_page: perPage,
         mode,
         donor_id: donorId,
+        status,
     };
 
     const {records, hasResolved, isResolving} = useEntityRecords<DonationResponse>('givewp/v3', 'donations', query);
