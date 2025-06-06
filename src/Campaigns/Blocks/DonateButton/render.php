5cc8c0798a2411fcb80e093231598cccf8b35fc7
Jon Waldstein <jonwaldstein@mac.mynetworksettings.com>
2025-06-06 14:53:21 -0400

    fix: check if inside editor first

--- src/Campaigns/Blocks/DonateButton/render.php
+++ src/Campaigns/Blocks/DonateButton/render.php
@@ -22,19 +22,28 @@ $hasSelectedForm = isset($attributes['selectedForm']);
 $selectedFormId = $hasSelectedForm ? (int)$attributes['selectedForm'] : null;
 $formId = $useDefaultForm || ! $hasSelectedForm ? $campaign->defaultFormId : $selectedFormId;
 $buttonText = esc_html($attributes['buttonText'] ?? __('Donate', 'give'));
+$isEditor = defined('REST_REQUEST') && REST_REQUEST;
 ?>
 
 <div <?php echo wp_kses_data(get_block_wrapper_attributes(['class' => 'givewp-campaign-donate-button-block', 'style' => esc_attr($blockInlineStyles)])); ?>>
     <?php
     ob_start();
-    echo give_form_shortcode([
+    if ($isEditor) {
+        echo sprintf(
+            '<button type="button" class="givewp-donation-form-modal__open">%s</button>',
+            esc_html($buttonText)
+        );
+    } else {
+        echo give_form_shortcode([
         'id' => $formId,
         'campaign_id' => $campaign->id,
         'display_style' => 'modal',
         'continue_button_title' => $buttonText,
         'use_default_form' => $useDefaultForm,
         'button_color' => $campaign->primaryColor,
-    ]);
+        'block_id' => $attributes['blockId'] ?? '',
+        ]);
+    }
 
     $final_output = ob_get_clean();
     echo $final_output;
