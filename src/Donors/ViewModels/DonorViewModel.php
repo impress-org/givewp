2c7058f3d3276eef11e49bf1cce27d57ccb8c36b
Paulo Iankoski <me@wpdev.com.br>
2025-06-07 20:47:32 -0300

    feature: add CustomFields to the DonorViewModel

--- src/Donors/ViewModels/DonorViewModel.php
+++ src/Donors/ViewModels/DonorViewModel.php
@@ -3,7 +3,12 @@
 namespace Give\Donors\ViewModels;
 
 use Give\API\REST\V3\Routes\Donors\ValueObjects\DonorAnonymousMode;
+use Give\DonationForms\Models\DonationForm;
+use Give\DonationForms\Repositories\DonationFormRepository;
+use Give\Donations\Models\Donation;
 use Give\Donors\Models\Donor;
+use Give\Framework\FieldsAPI\Field;
+use Give\Framework\FieldsAPI\Types;
 
 /**
  * @unreleased
@@ -54,6 +59,7 @@ class DonorViewModel
                 'addresses' => array_map(function($address) { return $address->toArray(); }, $this->donor->addresses),
                 'avatarUrl' => $this->getAvatarUrl(),
                 'wpUserPermalink' => $this->donor->userId ? get_edit_user_link($this->donor->userId) : null,
+                'customFields' => $this->getCustomFields(),
             ],
         );
 
@@ -67,12 +73,21 @@ class DonorViewModel
                 'avatarUrl',
                 'company',
                 'addresses',
-                'wpUserPermalink'
+                'wpUserPermalink',
+                'customFields'
             ];
 
             foreach ($sensitiveDataExcluded as $propertyName) {
                 if (isset($data[$propertyName])) {
-                    $data[$propertyName] = '';
+                    switch ($propertyName) {
+                        case 'additionalEmails':
+                        case 'customFields':
+                            $data[$propertyName] = [];
+                            break;
+                        default:
+                            $data[$propertyName] = '';
+                            break;
+                    }
                 }
             }
         }
@@ -86,7 +101,12 @@ class DonorViewModel
                 'lastName',
                 'prefix',
                 'avatarUrl',
-                'company'
+                'company',
+                'email',
+                'phone',
+                'additionalEmails',
+                'wpUserPermalink',
+                'customFields'
             ];
 
             foreach ($anonymousDataRedacted as $propertyName) {
@@ -94,8 +114,13 @@ class DonorViewModel
                     case 'id':
                         $data[$propertyName] = 0;
                         break;
+                    case 'wpUserPermalink':
                     case 'avatarUrl':
                         $data[$propertyName] = '';
+                        break;
+                    case 'additionalEmails':
+                    case 'customFields':
+                        $data[$propertyName] = [];
                         break;
                     default:
                         $data[$propertyName] = __('anonymous', 'give');
@@ -121,5 +146,91 @@ class DonorViewModel
         } else {
             return give_validate_gravatar($this->donor->email) ? get_avatar_url($this->donor->email, ['size' => 80]) : null;
         }
+    }
+
+    /**
+     * Get custom fields for the donor
+     *
+     * @unreleased
+     */
+    private function getCustomFields(): array
+    {
+        $forms = $this->getUniqueDonationFormsForDonor();
+
+        if (!$forms) {
+            return [];
+        }
+
+        $fields = array_reduce($forms, function ($fields, DonationForm $form) {
+            return $fields + $this->getDisplayedDonorMetaFieldsForForm($form);
+        }, []);
+
+        return array_reduce($fields, function($customFields, Field $field) {
+            $value = $this->getFieldValue($field);
+            $label = method_exists($field, 'getLabel') ? $field->getLabel() : $field->getName();
+
+            if (empty($value)) {
+                return $customFields;
+            }
+
+            $customFields[] = [
+                'label' => $label,
+                'value' => $value,
+            ];
+
+            return $customFields;
+        }, []);
+    }
+
+    /**
+     * Get unique donation forms for the donor
+     *
+     * @unreleased
+     */
+    private function getUniqueDonationFormsForDonor(): array
+    {
+        $formIds = array_map(static function (Donation $donation) {
+            return $donation->formId;
+        }, $this->donor->donations()->getAll() ?? []);
+
+        $formIds = array_filter($formIds, static function ($formId) {
+            return !give(DonationFormRepository::class)->isLegacyForm($formId);
+        });
+
+        return array_filter(array_map(static function ($formId) {
+            return DonationForm::find($formId);
+        }, array_unique($formIds)));
+    }
+
+    /**
+     * Get displayed donor meta fields for a form
+     *
+     * @unreleased
+     */
+    private function getDisplayedDonorMetaFieldsForForm(DonationForm $form): array
+    {
+        return array_filter($form->schema()->getFields(), static function ($field) {
+            return $field->shouldShowInAdmin() && $field->shouldStoreAsDonorMeta();
+        });
+    }
+
+    /**
+     * Get field value for a custom field
+     *
+     * @unreleased
+     */
+    private function getFieldValue(Field $field)
+    {
+        $metaValue = give()->donor_meta->get_meta($this->donor->id, $field->getName(), true);
+
+        if (empty($metaValue)) {
+            return '';
+        }
+
+        if ($field->getType() === Types::FILE) {
+            return wp_get_attachment_link($metaValue);
+        }
+
+        return $metaValue;
     }
 }
