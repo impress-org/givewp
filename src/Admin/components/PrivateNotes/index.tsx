98a26ebdf92c0fb9e5d1aada266c6270bafec390
alaca <ante.laca@gmail.com>
2025-06-09 13:47:12 +0200

    feature: add notifications

--- src/Admin/components/PrivateNotes/index.tsx
+++ src/Admin/components/PrivateNotes/index.tsx
@@ -2,7 +2,9 @@ import {__} from '@wordpress/i18n';
 import useSWR from 'swr';
 import {Dispatch, SetStateAction, useState} from 'react';
 import apiFetch from '@wordpress/api-fetch';
+import {useDispatch} from '@wordpress/data';
 import {ConfirmationDialogIcon, DeleteIcon, DotsMenuIcon, EditIcon, NotesIcon} from './Icons';
+import NotificationPlaceholder from '@givewp/components/AdminDetailsPage/Notifications';
 import Spinner from '../Spinner';
 import ModalDialog from '@givewp/components/AdminUI/ModalDialog';
 import style from './style.module.scss';
@@ -19,11 +21,12 @@ export default function PrivateNotes({donorId, context}: {
     const endpoint = `/givewp/v3/donors/${donorId}/notes`;
     const [state, setNoteState] = useState({
         isSavingNote: false,
-        currentlyEditing: null,
         note: '',
     });
 
     const [isAddingNote, setIsAddingNote] = context;
+
+    const dispatch = useDispatch('givewp/admin-details-page-notifications');
 
     const {
         data,
@@ -38,6 +41,10 @@ export default function PrivateNotes({donorId, context}: {
             .then((response) => {
                 mutate(response).then(() => {
                     setIsAddingNote(false);
+                    dispatch.addSnackbarNotice({
+                        id: 'add-note',
+                        content: __('You added a private note', 'give'),
+                    });
                 });
             });
     };
@@ -46,6 +53,10 @@ export default function PrivateNotes({donorId, context}: {
         apiFetch({path: `/givewp/v3/donors/${donorId}/notes/${id}`, method: 'DELETE', data: {id}})
             .then(async (response) => {
                 await mutate(response);
+                dispatch.addSnackbarNotice({
+                    id: 'delete-note',
+                    content: __('Private note deleted successfully', 'give'),
+                });
             });
     };
 
@@ -53,6 +64,10 @@ export default function PrivateNotes({donorId, context}: {
         apiFetch({path: `/givewp/v3/donors/${donorId}/notes/${id}`, method: 'POST', data: {id, content}})
             .then(async (response) => {
                 await mutate(response);
+                dispatch.addSnackbarNotice({
+                    id: 'delete-note',
+                    content: __('Private note edited', 'give'),
+                });
             });
     };
 
@@ -73,7 +88,6 @@ export default function PrivateNotes({donorId, context}: {
         );
     }
 
-    console.log(isAddingNote);
     return (
         <div className={style.notesContainer}>
             {isAddingNote && (
@@ -125,8 +139,7 @@ export default function PrivateNotes({donorId, context}: {
                 </>
             )}
         </div>
-    )
-        ;
+    );
 }
 
 
@@ -141,17 +154,18 @@ const Note = ({note, onDelete, onEdit}) => {
     const [showDeleteDialog, setShowDeleteDialog] = useState(false);
 
     return (
-        <div
-            className={style.noteContainer}
-            onMouseEnter={() => setShowMenuIcon(true)}
-            onMouseLeave={() => {
-                setShowMenuIcon(false);
-                setShowContextMenu(false);
-            }}
-        >
-            {currentlyEditing ? (
-                <>
-                    <div className={style.addNoteContainer}>
+        <>
+            <div
+                className={style.noteContainer}
+                onMouseEnter={() => setShowMenuIcon(true)}
+                onMouseLeave={() => {
+                    setShowMenuIcon(false);
+                    setShowContextMenu(false);
+                }}
+            >
+                {currentlyEditing ? (
+                    <>
+                        <div className={style.addNoteContainer}>
                         <textarea
                             className={style.textarea}
                             onChange={(e) => setContent(e.target.value)}
@@ -159,81 +173,87 @@ const Note = ({note, onDelete, onEdit}) => {
                             {note.content}
                         </textarea>
 
-                        <div className={style.textAreaButtons}>
-                            <button
-                                className={cx(style.button, style.cancelBtn)}
-                                onClick={() => {
-                                    setCurrentlyEditing(null);
-                                }}
-                            >
-                                {__('Cancel', 'give')}
-                            </button>
-                            <button
-                                className={cx(style.button, style.saveBtn)}
-                                onClick={(e) => {
-                                    e.preventDefault();
-                                    onEdit(note.id, content);
-                                }}
-                            >
-                                {__('Save', 'give')}
-                            </button>
-                        </div>
-                    </div>
-                </>
-            ) : (
-                <>
-                    <div className={style.note}>
-                        <div className={style.title}>
-                            {note.content}
-                        </div>
-
-                        {showMenuIcon && (
-                            <div
-                                className={style.dotsMenu}
-                                onClick={() => setShowContextMenu(true)}
-                            >
-                                <DotsMenuIcon />
-                                {showContextMenu && (
-                                    <div className={style.menu}>
-                                        <a
-                                            href="#"
-                                            className={style.menuItem}
-                                            onClick={(e) => {
-                                                e.preventDefault();
-                                                setCurrentlyEditing(note.id);
-                                            }}
-                                        >
-                                            <EditIcon /> {__('Edit', 'give')}
-                                        </a>
-                                        <a
-                                            href="#"
-                                            className={cx(style.menuItem, style.delete)}
-                                            onClick={(e) => {
-                                                e.preventDefault();
-                                                setShowDeleteDialog(true);
-                                            }}
-                                        >
-                                            <DeleteIcon /> {__('Delete', 'give')}
-                                        </a>
-                                    </div>
-                                )}
+                            <div className={style.textAreaButtons}>
+                                <button
+                                    className={cx(style.button, style.cancelBtn)}
+                                    onClick={() => {
+                                        setCurrentlyEditing(null);
+                                        setShowContextMenu(false);
+                                    }}
+                                >
+                                    {__('Cancel', 'give')}
+                                </button>
+                                <button
+                                    className={cx(style.button, style.saveBtn)}
+                                    onClick={(e) => {
+                                        e.preventDefault();
+                                        setShowContextMenu(false);
+                                        onEdit(note.id, content);
+                                    }}
+                                >
+                                    {__('Save', 'give')}
+                                </button>
                             </div>
-                        )}
-                    </div>
-                    <div className={style.date}>
-                        {formatTimestamp(note.createdAt.date)}
-                    </div>
-                </>
-            )}
-            <ConfirmationDialog
-                title={__('Delete Note', 'give')}
-                isOpen={showDeleteDialog}
-                handleClose={() => setShowDeleteDialog(false)}
-                handleConfirm={() => {
-                    onDelete(note.id);
-                }}
-            />
-        </div>
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        <div className={style.note}>
+                            <div className={style.title}>
+                                {note.content}
+                            </div>
+
+                            {showMenuIcon && (
+                                <div
+                                    className={style.dotsMenu}
+                                    onClick={() => setShowContextMenu(true)}
+                                >
+                                    <DotsMenuIcon />
+                                    {showContextMenu && (
+                                        <div className={style.menu}>
+                                            <a
+                                                href="#"
+                                                className={style.menuItem}
+                                                onClick={(e) => {
+                                                    e.preventDefault();
+                                                    setShowContextMenu(false);
+                                                    setCurrentlyEditing(note.id);
+                                                }}
+                                            >
+                                                <EditIcon /> {__('Edit', 'give')}
+                                            </a>
+                                            <a
+                                                href="#"
+                                                className={cx(style.menuItem, style.delete)}
+                                                onClick={(e) => {
+                                                    e.preventDefault();
+                                                    setShowContextMenu(false);
+                                                    setShowDeleteDialog(true);
+                                                }}
+                                            >
+                                                <DeleteIcon /> {__('Delete', 'give')}
+                                            </a>
+                                        </div>
+                                    )}
+                                </div>
+                            )}
+                        </div>
+                        <div className={style.date}>
+                            {formatTimestamp(note.createdAt.date)}
+                        </div>
+                    </>
+                )}
+                <ConfirmationDialog
+                    title={__('Delete Note', 'give')}
+                    isOpen={showDeleteDialog}
+                    handleClose={() => setShowDeleteDialog(false)}
+                    handleConfirm={() => {
+                        onDelete(note.id);
+                    }}
+                />
+            </div>
+            <NotificationPlaceholder type="snackbar" />
+        </>
     );
 };
 
