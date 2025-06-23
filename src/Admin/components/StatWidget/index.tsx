097b0faefba5964a74e6ad5ab9c68ab43a39e5a2
Joshua Dinh <joshuahungdinh@gmail.com>
2025-06-23 14:35:18 -0700

    feature: update stat widget

--- src/Admin/components/StatWidget/index.tsx
+++ src/Admin/components/StatWidget/index.tsx
@@ -2,7 +2,9 @@ import {Spinner} from '@givewp/components';
 import {HeaderText} from '../Header';
 import PercentChangePill
     from '@givewp/campaigns/admin/components/CampaignDetailsPage/Components/CampaignStats/PercentChangePill';
+import classnames from 'classnames';
 import styles from './styles.module.scss';
+import { __ } from '@wordpress/i18n';
 
 /**
  * @unreleased
@@ -14,6 +16,8 @@ export type StatWidgetProps = {
     formatter: Intl.NumberFormat;
     loading?: boolean;
     previousValue?: number;
+    inActive?: boolean;
+    href?: string;
 };
 
 /**
@@ -23,17 +27,19 @@ export default function StatWidget({
     label,
     value,
     description,
+    href,
     formatter = null,
     loading = false,
     previousValue = null,
+    inActive = false,
 }: StatWidgetProps) {
     return (
-        <div className={styles.statWidget}>
+        <div className={classnames(styles.statWidget)}>
             <header>
                 <HeaderText>{label}</HeaderText>
             </header>
             <div className={styles.statWidgetAmount}>
-                <div className={styles.statWidgetDisplay}>
+                <div className={classnames(styles.statWidgetDisplay, {[styles.inActive]: inActive})}>
                     {!loading ? (
                         formatter?.format(value) ?? value
                     ) : (
@@ -41,6 +47,7 @@ export default function StatWidget({
                             <Spinner size="small" />
                         </span>
                     )}
+                {inActive && (<a className={styles.upgradeLink} href={href}>{__('Upgrade', 'give')}</a>)}
                 </div>
                 {previousValue !== null && <PercentChangePill value={value} comparison={previousValue} />}
             </div>
@@ -49,6 +56,7 @@ export default function StatWidget({
                     <div>{description}</div>
                 </footer>
             )}
+        
         </div>
     );
 }
