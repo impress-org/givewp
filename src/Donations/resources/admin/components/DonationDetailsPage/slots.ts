74b75d45db0bd99a9eda48632bc88f147ad9c486
Paulo Iankoski <me@wpdev.com.br>
2025-06-23 10:28:31 -0300

    feature: add custom fields section to additional info tab and register slots

---
+++ src/Donations/resources/admin/components/DonationDetailsPage/slots.ts
@@ -0,0 +1,11 @@
+import {createSlotFill} from '@wordpress/components';
+
+/**
+ *
+ */
+const {Slot: AdditionalInfoSlot, Fill: AdditionalInfoFill} = createSlotFill('GiveWP/DonationDetails/Records/AdditionalInfo');
+
+export {
+    AdditionalInfoSlot,
+    AdditionalInfoFill,
+};
