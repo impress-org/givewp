39b768e6679e9aee7b58328194f8e122f4bf16bf
Joshua Dinh <joshuahungdinh@gmail.com>
2025-06-23 14:34:39 -0700

    feature: add overview panel

---
+++ src/Admin/components/OverviewPanel/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
+interface OverviewPanelProps {
+    children: React.ReactNode;
+    className?: string;
+}
+
+/**
+ * @unreleased
+ */
+export default function OverviewPanel({ children, className = '' }: OverviewPanelProps) {
+    return (
+        <section className={`${styles.panel} ${className}`}>
+            {children}
+        </section>
+    );
+} 
