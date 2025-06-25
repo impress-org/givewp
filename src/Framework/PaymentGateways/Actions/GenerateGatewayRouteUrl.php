eb4ff83aa149fd8b729d7d613042887df22141ce
Glauber Silva <glauber.silva@givewp.com>
2025-06-25 18:07:40 -0300

    fix: remove temp url

--- src/Framework/PaymentGateways/Actions/GenerateGatewayRouteUrl.php
+++ src/Framework/PaymentGateways/Actions/GenerateGatewayRouteUrl.php
@@ -28,7 +28,7 @@ class GenerateGatewayRouteUrl
 
         return esc_url_raw(add_query_arg(
             $queryArgs,
-            'https://kubkavqjkm.sharedwithexpose.com/' //home_url()
+            home_url()
         ));
     }
 }
