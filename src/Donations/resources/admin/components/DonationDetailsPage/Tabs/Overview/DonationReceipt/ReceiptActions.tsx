476c329a01a92ab01aa29a5cc1a74b2aaafa72f4
Joshua Dinh <joshuahungdinh@gmail.com>
2025-06-26 13:52:05 -0700

    refactor: improve receipt widget styles

---
+++ src/Donations/resources/admin/components/DonationDetailsPage/Tabs/Overview/DonationReceipt/ReceiptActions.tsx
@@ -0,0 +1,18 @@
+import { __ } from "@wordpress/i18n";
+import styles from "./styles.module.scss";
+
+    /**
+     * @unreleased
+     */
+export default function ReceiptActions() {
+    return (
+        <>
+         <button type="button" aria-label={__('Download donation receipt', 'give')}>
+            {__('Download receipt', 'give')}
+          </button>
+          <button type="button" aria-label={__('Resend donation receipt to email', 'give')}>
+            {__('Resend receipt', 'give')}
+          </button>
+        </>
+    );
+}
