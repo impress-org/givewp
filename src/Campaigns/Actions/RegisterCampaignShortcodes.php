bd16f5964964cc2ed6ebf326dd21cae81228cd0b
Joshua Dinh <joshuahungdinh@gmail.com>
2025-06-27 13:18:33 -0700

    refactor: use new namespace imports

--- src/Campaigns/Actions/RegisterCampaignShortcodes.php
+++ src/Campaigns/Actions/RegisterCampaignShortcodes.php
@@ -2,9 +2,10 @@
 
 namespace Give\Campaigns\Actions;
 
-use Give\Campaigns\Blocks\Campaign\CampaignShortcode;
-use Give\Campaigns\Blocks\CampaignForm\CampaignFormShortcode;
-use Give\Campaigns\Blocks\CampaignGrid\CampaignGridShortcode;
+use Give\Campaigns\Shortcodes\CampaignShortcode;
+use Give\Campaigns\Shortcodes\CampaignFormShortcode;
+use Give\Campaigns\Shortcodes\CampaignGridShortcode;
+use Give\Campaigns\Shortcodes\CampaignCommentsShortcode;
 
 /**
  * @since 4.2.0
@@ -19,5 +20,6 @@ class RegisterCampaignShortcodes
         add_shortcode('givewp_campaign', [new CampaignShortcode(), 'renderShortcode']);
         add_shortcode('givewp_campaign_grid', [new CampaignGridShortcode(), 'renderShortcode']);
         add_shortcode('givewp_campaign_form', [new CampaignFormShortcode(), 'renderShortcode']);
+        add_shortcode('givewp_campaign_comments', [new CampaignCommentsShortcode(), 'renderShortcode']);
     }
 }
