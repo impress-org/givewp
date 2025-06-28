9e751bd61be3a872fe89ea47593b5c6b1092bebd
Ante Laća <ante.laca@gmail.com>
2025-06-28 05:06:03 +0200

    Fix: Divi compatibility issues (#7995)

---
+++ src/ThirdPartyCompatibility/Divi/DeregisterEntityScripts.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Give\ThirdPartyCompatibility\Divi;
+
+/**
+ * @unreleased
+ */
+class DeregisterEntityScripts
+{
+    public function __invoke()
+    {
+        if ( ! isset($_GET['page']) || $_GET['page'] !== 'et_theme_builder') {
+            return;
+        }
+
+        global $wp_scripts;
+
+        $registered_scripts = $wp_scripts->registered;
+
+        foreach ($registered_scripts as $handle => $script) {
+            if (preg_match('/^givewp-.*-entity$/', $handle)) {
+                wp_dequeue_script($handle);
+                wp_deregister_script($handle);
+            }
+        }
+    }
+}
