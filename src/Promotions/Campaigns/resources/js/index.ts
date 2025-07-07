fd2266b2c2be168c88a10ff82da53aae24338b4e
Joshua Dinh <joshuahungdinh@gmail.com>
2025-07-07 11:38:20 -0700

    fix: create container to render welcomebanner

--- src/Promotions/Campaigns/resources/js/index.ts
+++ src/Promotions/Campaigns/resources/js/index.ts
@@ -1,4 +1,15 @@
-const pluginHeader = document.querySelector('.wp-header-end');
-// Place banner underneath Plugin header & add new button
-const element = document.querySelector('[data-stellarwp-givewp-notice-id="givewp-campaigns-welcome-banner-2025"]');
-pluginHeader.insertAdjacentElement('afterend', element);
+document.addEventListener('DOMContentLoaded', () => {
+  const pluginHeader = document.querySelector('.wp-header-end');
+  if (pluginHeader) {
+    const container = document.createElement('div');
+    container.id = 'givewp-campaigns-welcome-banner';
+    pluginHeader.insertAdjacentElement('afterend', container);
+
+    const banner = document.querySelector('[data-stellarwp-givewp-notice-id="givewp-campaigns-welcome-banner-2025"]');
+
+    if (banner) {
+      container.appendChild(banner);
+    }
+  }
+});
+  
