0d5a4e347a59d8e84c26257ae444c4d49e3fb639
Joshua Dinh <joshuahungdinh@gmail.com>
2025-07-09 15:15:07 -0700

    feature: update receipt action buttons

--- src/Donations/resources/admin/components/DonationDetailsPage/Tabs/Overview/DonationReceipt/ReceiptActions.tsx
+++ src/Donations/resources/admin/components/DonationDetailsPage/Tabs/Overview/DonationReceipt/ReceiptActions.tsx
@@ -1,18 +1,43 @@
 import { __ } from "@wordpress/i18n";
+import { useState } from "react";
+import useResendReceipt from '../../../../../../hooks/useResendReceipt';
+import ConfirmationDialog from '@givewp/components/AdminDetailsPage/ConfirmationDialog';
 import styles from "./styles.module.scss";
+import classnames from "classnames";
 
-/**
+
+ /**
  * @unreleased
  */
-export default function ReceiptActions() {
+export default function ReceiptActions({pdfReceiptUrl}: {pdfReceiptUrl: string}) {
+  const [showConfirmationDialog, setShowConfirmationDialog] = useState(false);
+  const { loading, message, handleResendReceipt } = useResendReceipt();
+
+  const handleConfirmationModal = () => {
+    setShowConfirmationDialog(true);
+  }
+ 
+
     return (
         <>
-         <button className={styles.actionButton} type="button" aria-label={__('Download donation receipt', 'give')}>
+          <a className={classnames(styles.action, styles.actionLink, {
+            [styles.actionDisabled]: true,
+          })} href={pdfReceiptUrl} aria-label={__('Download donation receipt', 'give')}>
             {__('Download receipt', 'give')}
+          </a>
+          <button className={styles.action} type="button" aria-label={__('Resend donation receipt to email', 'give')} onClick={handleConfirmationModal} disabled={loading}>
+            {loading ? __('Resending...', 'give') : message}
           </button>
-          <button className={styles.actionButton} type="button" aria-label={__('Resend donation receipt to email', 'give')}>
-            {__('Resend receipt', 'give')}
-          </button>
+          <ConfirmationDialog
+                variant="regular"
+                title={__('Resend Receipt ', 'give')}
+                actionLabel={__('Resend Receipt', 'give')}
+                isOpen={showConfirmationDialog}
+                handleClose={() => setShowConfirmationDialog(null)}
+                handleConfirm={()=>{handleResendReceipt(); setShowConfirmationDialog(false)}}
+            >
+                {__('Are you sure you want to resend the donation receipt?', 'give')}
+            </ConfirmationDialog>
         </>
     );
 }
