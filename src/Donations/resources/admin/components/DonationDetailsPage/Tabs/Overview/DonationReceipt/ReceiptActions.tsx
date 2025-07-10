173055d9c8a6776141d08c5ca72c415d6d36ddd5
Joshua Dinh <joshuahungdinh@gmail.com>
2025-07-10 16:14:02 -0700

    fix: messaging

--- src/Donations/resources/admin/components/DonationDetailsPage/Tabs/Overview/DonationReceipt/ReceiptActions.tsx
+++ src/Donations/resources/admin/components/DonationDetailsPage/Tabs/Overview/DonationReceipt/ReceiptActions.tsx
@@ -1,16 +1,23 @@
 import { __ } from "@wordpress/i18n";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import useResendReceipt from '../../../../../../hooks/useResendReceipt';
 import ConfirmationDialog from '@givewp/components/AdminDetailsPage/ConfirmationDialog';
 import styles from "./styles.module.scss";
 
 
- /**
+/**
  * @unreleased
  */
 export default function ReceiptActions() {
   const [showConfirmationDialog, setShowConfirmationDialog] = useState(false);
-  const { loading, message, handleResendReceipt } = useResendReceipt();
+  const { loading, hasResolved, message, handleResendReceipt } = useResendReceipt();
+
+  useEffect(() => {
+    if (hasResolved) {
+      setShowConfirmationDialog(false);
+    }
+  }, [hasResolved]);
+
 
   const handleConfirmationModal = () => {
     setShowConfirmationDialog(true);
@@ -19,15 +26,15 @@ export default function ReceiptActions() {
     return (
         <>
           <button className={styles.action} type="button" aria-label={__('Resend donation receipt to email', 'give')} onClick={handleConfirmationModal} disabled={loading}>
-            {loading ? __('Resending...', 'give') : message}
+            {__('Resend receipt', 'give')}
           </button>
           <ConfirmationDialog
                 variant="regular"
                 title={__('Resend Receipt', 'give')}
-                actionLabel={__('Resend Receipt', 'give')}
+                actionLabel={message}
                 isOpen={showConfirmationDialog}
                 handleClose={() => setShowConfirmationDialog(null)}
-                handleConfirm={()=>{handleResendReceipt(); setShowConfirmationDialog(false)}}
+                handleConfirm={()=>{handleResendReceipt()}}
             >
                 {__('Are you sure you want to resend the donation receipt?', 'give')}
             </ConfirmationDialog>
--- src/Donations/resources/hooks/useResendReceipt.tsx
+++ src/Donations/resources/hooks/useResendReceipt.tsx
@@ -7,6 +7,7 @@ import { __ } from '@wordpress/i18n';
 export default function useResendReceipt() {
     const [loading, setLoading] = useState(false);
     const [message, setMessage] = useState<string | null>(__('Resend Receipt', 'give'));
+    const [hasResolved, setHasResolved] = useState(false);
 
     const urlParams = new URLSearchParams(window.location.search);
     const donationId = urlParams.get('id');
@@ -14,10 +15,14 @@ export default function useResendReceipt() {
     const handleResendReceipt = async () => {
         if (!donationId) {
             setMessage(__('Donation ID not found.', 'give'));
+            setHasResolved(true);
             return;
         }
+
         setLoading(true);
-        setMessage(null);
+        setMessage(__('Resending', 'give'));
+        setHasResolved(false);
+
         try {
             const response = await fetch('/wp-json/give-api/v2/admin/donations/resendEmailReceipt', {
                 method: 'POST',
@@ -27,18 +32,17 @@ export default function useResendReceipt() {
                 },
                 body: JSON.stringify({ ids: donationId }),
             });
+
             if (!response.ok) {
                 throw new Error(await response.text());
             }
-            setMessage(__('Receipt resent successfully.', 'give'));
         } catch (error: any) {
             setMessage(__('Failed to resend receipt: ', 'give') + error.message);
         } finally {
             setLoading(false);
+            setHasResolved(true);
         }
     };
 
-    
-
-    return { loading, message, handleResendReceipt };
+    return { loading, message, hasResolved, handleResendReceipt };
 }
