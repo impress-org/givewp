38bf151e1f3e696e11dfe272ffd8d8db0838fbfc
Joshua Dinh <joshuahungdinh@gmail.com>
2025-07-21 14:10:35 -0700

    feature: add static SubscriptionStats

--- src/Subscriptions/resources/admin/components/SubscriptionDetailsPage/Tabs/Overview/index.tsx
+++ src/Subscriptions/resources/admin/components/SubscriptionDetailsPage/Tabs/Overview/index.tsx
@@ -1,10 +1,24 @@
+import { useSubscriptionEntityRecord } from "@givewp/subscriptions/utils";
+import SubscriptionStats from "./SubscriptionStats";
+import styles from "./styles.module.scss";
+
 /**
  * @unreleased
  */
 export default function SubscriptionDetailsPageOverviewTab() {
+    const {record: subscription, hasResolved, isResolving } = useSubscriptionEntityRecord();
+
     return (
-        <div>
-            <h2>Overview</h2>
+        <div className={styles.overview}>
+            <SubscriptionStats totalContributions={350} paymentsCompleted={3} totalInstallments={subscription.installments} loading={isResolving || !hasResolved} />
+
+            <div className={styles.left}>
+
+            </div>
+
+            <div className={styles.right}>
+
+            </div>
         </div>
     );
 }
