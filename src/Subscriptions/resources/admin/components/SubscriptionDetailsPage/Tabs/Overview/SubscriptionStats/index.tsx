7eb5305f9d29faa9c3533059ba3cff0e80b39c70
Joshua Dinh <joshuahungdinh@gmail.com>
2025-07-21 14:10:35 -0700

    feature: add static SubscriptionStats

---
+++ src/Subscriptions/resources/admin/components/SubscriptionDetailsPage/Tabs/Overview/SubscriptionStats/index.tsx
@@ -0,0 +1,47 @@
+import StatWidget from '@givewp/admin/components/StatWidget';
+import {__} from '@wordpress/i18n';
+import {amountFormatter} from '@givewp/admin/utils';
+import {getSubscriptionOptionsWindowData} from '@givewp/subscriptions/utils';
+import styles from './styles.module.scss';
+
+/**
+ * @unreleased
+ */
+type SubscriptionStatsProps = {
+    totalContributions: number;
+    paymentsCompleted: number;
+    totalInstallments: number;
+    loading: boolean;
+}
+
+/**
+ * @unreleased
+ */
+export default function SubscriptionStats({totalContributions, paymentsCompleted, totalInstallments, loading}: SubscriptionStatsProps) {
+    const {currency} = getSubscriptionOptionsWindowData();
+
+    const ongoingInstallments = totalInstallments === 0;
+
+    const paymentProgress = (
+        <div className={styles.paymentProgress}>
+          {paymentsCompleted} / <span className={styles.installments}>{ongoingInstallments ? '∞' : totalInstallments}</span>
+        </div>
+      );
+
+    return (
+        <div className={styles.container}>
+            <StatWidget
+                label={__('Total contributions so far', 'give')}
+                value={totalContributions}
+                formatter={amountFormatter(currency)}
+                loading={loading}
+            />
+            <StatWidget
+                label={__('Payments completed', 'give')}
+                value={paymentProgress}
+                formatter={null}
+                loading={loading}
+            />
+        </div>
+    );
+}
