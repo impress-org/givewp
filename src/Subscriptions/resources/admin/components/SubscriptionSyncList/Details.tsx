d9c8d6fc1a99fe3e3a379cf89b3ad31fb31ba6a6
Joshua Dinh <joshuahungdinh@gmail.com>
2025-07-24 11:30:48 -0700

    feature: add subscription sync list UI

---
+++ src/Subscriptions/resources/admin/components/SubscriptionSyncList/Details.tsx
@@ -0,0 +1,103 @@
+import { __ } from "@wordpress/i18n";
+import { createInterpolateElement } from '@wordpress/element';
+import classnames from 'classnames';
+import { CheckCircleIcon, HandHeartIcon, XCircleIcon } from "./icons";
+import styles from "./styles.module.scss";
+
+/**
+ * @unreleased
+ */
+type SyncDetailsProps = {
+    isUpdated: boolean;
+  }
+  
+/**
+ * @unreleased
+ */
+export default function SyncDetails({ isUpdated }: SyncDetailsProps) {
+  return (
+    <div className={styles.syncDetails}>
+      {isUpdated ? (
+        <div className={styles.detail}>
+          <div className={styles.detailContainer}>
+            <p className={styles.detailTitle}>{__('OLD', 'give')}</p>
+            <div className={styles.detailWrapper}>
+              <div className={styles.detailItem}>
+                  <p className={styles.detailLabel}>{__('Platform', 'give')}</p>
+                  <p className={styles.detailStatus}>{__('Active', 'give')}</p>
+                </div>
+                <XCircleIcon />
+                <div className={styles.detailItem}>
+                  <p className={styles.detailLabel}>{__('Gateway', 'give')}</p>
+                  <p className={styles.detailStatus}>{__('Completed', 'give')}</p>
+              </div>
+            </div>
+
+          </div>
+
+          <div className={styles.detailContainer}>
+            <p className={styles.detailTitle}>{__('NEW', 'give')}</p>
+            <div className={styles.detailWrapper}>
+                <div className={styles.detailItem}>
+                  <p className={styles.detailLabel}>{__('Platform', 'give')}</p>
+                  <p className={styles.detailStatus}>{__('Completed', 'give')}</p>
+                </div>
+                <CheckCircleIcon />
+                <div className={styles.detailItem}>
+                  <p className={styles.detailLabel}>{__('Gateway', 'give')}</p>
+                  <p className={styles.detailStatus}>{__('Completed', 'give')}</p>
+                </div>
+              </div>
+            </div>
+        </div>
+      ) : (
+        <div className={styles.accurateDetailWrapper}>
+          <div className={styles.detailItem}>
+            <p className={styles.detailLabel}>{__('Platform', 'give')}</p>
+            <p className={styles.detailStatus}>{__('Completed', 'give')}</p>
+          </div>
+          <CheckCircleIcon />
+          <div className={styles.detailItem}>
+            <p className={styles.detailLabel}>{__('Gateway', 'give')}</p>
+            <p className={styles.detailStatus}>{__('Completed', 'give')}</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+
+
+/**
+ * @unreleased
+ */
+type PaymentDetailsProps = {
+  payment: any;
+}
+
+/**
+ * @unreleased
+ */
+export function PaymentDetails({ payment }: PaymentDetailsProps) {
+    const PaymentAmount = () => (
+        <strong className={styles.paymentDescriptionAmount}>$100.00</strong>
+    );
+
+    const description = createInterpolateElement(
+        __('A donation of <PaymentAmount/> has been added.', 'give'),
+        { PaymentAmount: <PaymentAmount /> }
+    );
+
+    return (
+        <div className={classnames(styles.paymentDetails)}>
+            <div className={styles.paymentIcon}>
+                <HandHeartIcon />
+            </div>
+            <div className={styles.paymentsContent}>
+                <p className={styles.paymentDescription}>{description}</p>
+                <p className={styles.paymentDate}>14th May, 2025 10:30 AM</p>
+            </div>
+        </div>
+    );
+}
