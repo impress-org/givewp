f06f25d3b30fdfdd81f3f63c524303cfe2a2b301
Joshua Dinh <joshuahungdinh@gmail.com>
2025-07-25 11:00:28 -0700

    feature: add initial SubscriptionPaymentDetails markup

---
+++ src/Subscriptions/resources/admin/components/SubscriptionDetailsPage/Tabs/Overview/SubscriptionPaymentDetails/index.tsx
@@ -0,0 +1,37 @@
+import { __ } from '@wordpress/i18n';
+import OverviewPanel from "@givewp/src/Admin/components/OverviewPanel";
+import PaymentDetails from '@givewp/admin/components/PaymentDetails';
+import { HourGlassIcon, ClockIcon } from './Icons';
+import { Subscription } from '@givewp/subscriptions/admin/components/types';
+import { Donation } from '@givewp/donations/admin/components/types';
+import styles from './styles.module.scss';
+
+
+type SubscriptionDetailsProps = {
+    subscription: Subscription;
+    donation?: Donation;
+}
+
+/**
+ * @unreleased
+ */
+export default function SubscriptionPaymentDetails({subscription}: SubscriptionDetailsProps) {    
+    const isOngoing = subscription?.installments === 0;
+    const infoCardBadgeLabel = isOngoing ? <><ClockIcon />{__('Unlimited', 'give')}</> : <><HourGlassIcon />{__('Limited', 'give')}</>;
+  
+
+    return (
+        <OverviewPanel>
+            <h2 id="subscription-details-grid-title" className={'sr-only'}>
+                {__('Subscription Details', 'give')}
+            </h2>
+            <PaymentDetails
+                record={subscription}
+                gatewayLinkLabel={__('View subscription on gateway', 'give')}
+                infoCardTitle={__('Subscription info', 'give')}
+                infoCardBadgeLabel={infoCardBadgeLabel}
+                infoCardClassName={isOngoing ? styles.unlimited : styles.limited}
+            />   
+        </OverviewPanel>
+    );
+}
--- src/Subscriptions/resources/admin/components/SubscriptionDetailsPage/Tabs/Overview/index.tsx
+++ src/Subscriptions/resources/admin/components/SubscriptionDetailsPage/Tabs/Overview/index.tsx
@@ -1,10 +1,22 @@
+import { useSubscriptionEntityRecord } from "@givewp/subscriptions/utils";
+import SubscriptionPaymentDetails from "./SubscriptionPaymentDetails";
+import styles from "./styles.module.scss";
+
 /**
  * @unreleased
  */
 export default function SubscriptionDetailsPageOverviewTab() {
+    const {record: subscription, hasResolved, isResolving } = useSubscriptionEntityRecord();
+
     return (
-        <div>
-            <h2>Overview</h2>
+        <div className={styles.overview}>
+
+            <div className={styles.left}>
+                <SubscriptionPaymentDetails subscription={subscription} />
+            </div>
+
+            <div className={styles.right}>
+            </div>
         </div>
     );
 }
