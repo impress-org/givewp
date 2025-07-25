529a52786f60210846280df33e17123804786126
Joshua Dinh <joshuahungdinh@gmail.com>
2025-07-24 20:42:17 -0700

    feature: add inital SubscriptionSummary markup

--- src/Subscriptions/resources/admin/components/SubscriptionDetailsPage/Tabs/Overview/index.tsx
+++ src/Subscriptions/resources/admin/components/SubscriptionDetailsPage/Tabs/Overview/index.tsx
@@ -1,10 +1,25 @@
+import { useSubscriptionEntityRecord } from "@givewp/subscriptions/utils";
+import { useSubscriptionAmounts } from "@givewp/subscriptions/hooks";
+import styles from "./styles.module.scss";
+import SubscriptionSummary from "./SubscriptionSummary";
+
 /**
  * @unreleased
  */
 export default function SubscriptionDetailsPageOverviewTab() {
+    const {record: subscription, hasResolved, isResolving } = useSubscriptionEntityRecord();
+    const {intendedAmount} = useSubscriptionAmounts(subscription);
+
     return (
-        <div>
-            <h2>Overview</h2>
+        <div className={styles.overview}>
+
+            <div className={styles.left}>
+
+            </div>
+
+            <div className={styles.right}>
+                <SubscriptionSummary subscription={subscription} intendedAmount={intendedAmount} />
+            </div>
         </div>
-    );
+    )
 }
