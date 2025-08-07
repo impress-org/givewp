4c9b5a1b2c705c02516149bac797b7978a52fa65
Joshua Dinh <joshuahungdinh@gmail.com>
2025-08-06 22:31:24 -0700

    fix: update SubscriptionSummaryGrid

--- src/Subscriptions/resources/admin/components/SubscriptionDetailsPage/Tabs/Overview/SubscriptionSummaryGrid/index.tsx
+++ src/Subscriptions/resources/admin/components/SubscriptionDetailsPage/Tabs/Overview/SubscriptionSummaryGrid/index.tsx
@@ -1,14 +1,14 @@
 import { __ } from '@wordpress/i18n';
-import OverviewPanel from "@givewp/src/Admin/components/OverviewPanel";
-import Grid, { GridCard } from '@givewp/src/Admin/components/Grid';
+import OverviewPanel from "@givewp/admin/components/OverviewPanel";
+import Grid, { GridCard } from '@givewp/admin/components/Grid';
 import { CampaignCard, DonorCard } from '@givewp/donations/admin/components/DonationDetailsPage/Tabs/Overview/DonationSummaryGrid';
-import Spinner from '@givewp/src/Admin/components/Spinner';
+import Spinner from '@givewp/admin/components/Spinner';
 import PaymentMethodIcon from '@givewp/donations/admin/components/DonationDetailsPage/Tabs/Overview/DonationSummaryGrid/PaymentMethodIcon';
 import ExternalLinkIcon from '@givewp/donations/admin/components/DonationDetailsPage/Tabs/Overview/DonationSummaryGrid/icon';
 import { HourGlassIcon, ClockIcon } from './Icons';
 import { Subscription } from '@givewp/subscriptions/admin/components/types';
 import { Donation } from '@givewp/donations/admin/components/types';
-import { formatTimestamp } from '@givewp/src/Admin/utils';
+import { formatTimestamp } from '@givewp/admin/utils';
 import classnames from 'classnames';
 
 import styles from './styles.module.scss';
@@ -16,16 +16,16 @@ import styles from './styles.module.scss';
 type SubscriptionDetailsProps = {
     subscription: Subscription;
     donation?: Donation;
-    isResolving: boolean;
+    isLoading: boolean;
 }
 
 /**
  * @unreleased
  */
-export default function SubscriptionSummaryGrid({subscription, donation, isResolving}: SubscriptionDetailsProps) {    
+export default function SubscriptionSummaryGrid({subscription, donation, isLoading}: SubscriptionDetailsProps) {    
     const isOngoing = subscription?.installments === 0;
     const badgeLabel = isOngoing ? <><ClockIcon />{__('Unlimited', 'give')}</> : <><HourGlassIcon />{__('Limited', 'give')}</>;
-  
+
     return (
         <OverviewPanel className={styles.overviewPanel}>
             <h2 id="subscription-details-grid-title" className={'sr-only'}>
@@ -37,8 +37,8 @@ export default function SubscriptionSummaryGrid({subscription, donation, isResol
 
                 {/* Next Payment */}
                 <GridCard className={styles.card} heading={__('Next payment', 'give')} headingId="next-payment">
-                    {isResolving && (<Spinner />)}
-                    {!isResolving && donation && (
+                    {isLoading && <Spinner />}
+                    {!isLoading && (
                         <>
                             <time className={styles.date} dateTime={donation?.createdAt?.date}>
                                 {formatTimestamp(donation?.createdAt?.date, true)}
@@ -53,18 +53,15 @@ export default function SubscriptionSummaryGrid({subscription, donation, isResol
                             </div>                  
                         </>
                     )}
-             
-
                 </GridCard>
 
                 {/* Associated Donor */}
                 <DonorCard donation={donation} />
 
-
                 {/* Gateway Info */}
-                <GridCard className={styles.card} heading={__('Gateway', 'give')} headingId="gateway">
-                    {isResolving && (<Spinner />)}
-                    {!isResolving && (
+                <GridCard className={classnames(styles.card)} heading={__('Gateway', 'give')} headingId="gateway">
+                    {isLoading && <Spinner />}
+                    {!isLoading && (
                         <>
                             <strong className={styles.paymentMethod}>
                                 <PaymentMethodIcon paymentMethod={donation?.gateway?.id} />
