7ce5341e47102f4120ba98b788aefa310651214d
Joshua Dinh <joshuahungdinh@gmail.com>
2025-08-06 22:29:33 -0700

    feature: update GridCard styles & classnames

--- src/Admin/components/Grid/index.tsx
+++ src/Admin/components/Grid/index.tsx
@@ -1,10 +1,5 @@
 import { __ } from '@wordpress/i18n';
 import classnames from 'classnames';
-import { formatTimestamp } from '@givewp/src/Admin/utils';
-import type {Donation} from '@givewp/donations/admin/components/types';
-import { useDonorEntityRecord } from '@givewp/donors/utils';
-import { useCampaignEntityRecord } from '@givewp/campaigns/utils';
-import Spinner from '@givewp/src/Admin/components/Spinner';
 import styles from './styles.module.scss';
 
 
@@ -44,7 +39,7 @@ type GridCardProps = {
 export function GridCard({children, heading, headingId, className}: GridCardProps) {
     return (
         <div className={classnames(styles.card, className)} role="region" aria-labelledby={`${headingId}-label`}>
-            <h3 id={headingId}>{heading}</h3>
+            <h3 id={headingId} className={styles.heading}>{heading}</h3>
             {children}
         </div>
     );
