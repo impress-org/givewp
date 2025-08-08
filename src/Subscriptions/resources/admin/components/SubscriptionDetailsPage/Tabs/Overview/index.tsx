050965de76eab5c837578472c35d5e33809d26bc
Paulo Iankoski <me@wpdev.com.br>
2025-08-07 23:08:35 -0300

    chore: remove duplicated import

--- src/Subscriptions/resources/admin/components/SubscriptionDetailsPage/Tabs/Overview/index.tsx
+++ src/Subscriptions/resources/admin/components/SubscriptionDetailsPage/Tabs/Overview/index.tsx
@@ -6,7 +6,6 @@ import Header from "@givewp/admin/components/Header";
 import { SubscriptionNotes } from "@givewp/admin/components/PrivateNotes";
 import { getSubscriptionOptionsWindowData, useSubscriptionEntityRecord } from "@givewp/subscriptions/utils";
 import { useDonationsBySubscription } from "@givewp/subscriptions/hooks";
-import { useSubscriptionEntityRecord } from "@givewp/subscriptions/utils";
 import styles from "./styles.module.scss";
 
 /**
@@ -16,7 +15,7 @@ export default function SubscriptionDetailsPageOverviewTab() {
     const {mode, adminUrl} = getSubscriptionOptionsWindowData();
     const {record: subscription, hasResolved } = useSubscriptionEntityRecord();
     const {records: donations, hasResolved: donationsResolved, isResolving: donationsLoading} = useDonationsBySubscription(subscription?.id, mode);
-    
+
     return (
         <div className={styles.overview}>
             <SubscriptionStats donations={donations} currency={subscription?.amount?.currency} totalInstallments={subscription?.installments} loading={!hasResolved || donationsLoading || !donationsResolved} />
