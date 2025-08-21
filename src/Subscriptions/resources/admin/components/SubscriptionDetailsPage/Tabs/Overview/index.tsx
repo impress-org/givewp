72f415e13397317efbddd8d435e947711f490c1c
Joshua Dinh <joshuahungdinh@gmail.com>
2025-08-21 12:41:25 -0700

    refactor: get currency from parent component

--- src/Subscriptions/resources/admin/components/SubscriptionDetailsPage/Tabs/Overview/index.tsx
+++ src/Subscriptions/resources/admin/components/SubscriptionDetailsPage/Tabs/Overview/index.tsx
@@ -31,7 +31,7 @@ export default function SubscriptionDetailsPageOverviewTab() {
             </div>
 
             <div className={styles.right}>
-                <GoalProgressChart value={intendedAmount} subscription={subscription} />
+                <GoalProgressChart value={intendedAmount} currency={subscription?.amount?.currency} subscription={subscription} />
                 <SubscriptionSummary subscription={subscription} donation={donations?.[0]} adminUrl={adminUrl} intendedAmount={intendedAmount} isLoading={subscriptionLoading || !subscriptionsResolved || donationsLoading || !donationsResolved} />
             </div>
         </div>
