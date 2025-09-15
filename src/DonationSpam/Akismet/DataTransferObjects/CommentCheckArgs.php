55264b7c2dd75c7c0cb3d0c7477ed39628a3ce54
alaca <ante.laca@gmail.com>
2025-09-15 18:01:10 +0200

    fix: add AllowDynamicProperties comment

--- src/DonationSpam/Akismet/DataTransferObjects/CommentCheckArgs.php
+++ src/DonationSpam/Akismet/DataTransferObjects/CommentCheckArgs.php
@@ -3,8 +3,10 @@
 namespace Give\DonationSpam\Akismet\DataTransferObjects;
 
 /**
+ * @unreleased add #[AllowDynamicProperties]
  * @since 3.15.0
  */
+#[\AllowDynamicProperties]
 class CommentCheckArgs
 {
     public $blog;
