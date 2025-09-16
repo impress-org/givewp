98362f98c4d93b1f5b60bd533eb710abbd4d6c3e
Joshua Dinh <joshuahungdinh@gmail.com>
2025-09-16 14:29:21 -0700

    fix: remnove changes to campaign column

--- src/Donations/ListTable/Columns/CampaignColumn.php
+++ src/Donations/ListTable/Columns/CampaignColumn.php
@@ -14,8 +14,6 @@ use Give\Framework\ListTable\ModelColumn;
  */
 class CampaignColumn extends ModelColumn
 {
-    protected $sortColumn = null;
-
     /**
      * @since 4.3.0
      *
@@ -37,7 +35,6 @@ class CampaignColumn extends ModelColumn
     }
 
     /**
-     * @unreleased
      * @since 4.8.0 Added class to link
      * @since 4.3.0
      *
@@ -47,13 +44,11 @@ class CampaignColumn extends ModelColumn
      */
     public function getCellValue($model): string
     {
-        $campaignTitle = $model->campaign->title;
-        
         return sprintf(
             '<a href="%s" aria-label="%s" class="campaignLink">%s</a>',
             admin_url("edit.php?post_type=give_forms&page=give-campaigns&id={$model->campaign->id}&tab=overview&action=edit"),
             __('Visit campaign page', 'give'),
-            $campaignTitle
+            $model->campaign->title
         );
     }
 }
