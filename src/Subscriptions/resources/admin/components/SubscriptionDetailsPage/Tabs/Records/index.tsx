3d8d24e95b4bee23c8ab841265d80274d757ba22
Paulo Iankoski <me@wpdev.com.br>
2025-09-23 14:49:52 -0300

    refactor: add separated notices based on what is being changed

--- src/Subscriptions/resources/admin/components/SubscriptionDetailsPage/Tabs/Records/index.tsx
+++ src/Subscriptions/resources/admin/components/SubscriptionDetailsPage/Tabs/Records/index.tsx
@@ -6,24 +6,41 @@ import { RecordsSlot } from '../../slots';
 import SubscriptionDetails from './SubscriptionDetails';
 
 /**
- * @unreleased add SubscriptionDetails 
+ * @unreleased
+ */
+const AdminNotice = ({ isVisible, children }: {isVisible: boolean, children: React.ReactNode}) => {
+    if (!isVisible) return null;
+
+    return (
+        <div style={{ marginBottom: 'var(--givewp-spacing-4)' }}>
+            <Notice type="info">{children}</Notice>
+        </div>
+    );
+};
+
+/**
+ * @unreleased add SubscriptionDetails
  * @since 4.8.0
  */
 export default function SubscriptionDetailsPageRecordsTab() {
-    const {isDirty} = useFormState();
+    const { isDirty, dirtyFields } = useFormState();
+
+    const isSubscriptionStatusModified = isDirty && Boolean(dirtyFields?.status);
+    const totalDirtyFieldsCount = Object.keys(dirtyFields || {}).length;
+    const hasNonStatusFieldChanges = isDirty && totalDirtyFieldsCount > (isSubscriptionStatusModified ? 1 : 0);
 
     return (
         <>
-            {isDirty && (
-                <div style={{ marginBottom: 'var(--givewp-spacing-4)' }}>
-                    <Notice type="info">
-                        {__('Some changes made to this subscription will only affect future renewals.', 'give')}
-                    </Notice>
-                </div>
-            )}
+            <AdminNotice isVisible={isSubscriptionStatusModified}>
+                {__('Changing the status here will not update the status at the payment gateway.', 'give')}
+            </AdminNotice>
+            <AdminNotice isVisible={hasNonStatusFieldChanges}>
+                {__('Some changes made to this subscription will only affect future renewals.', 'give')}
+            </AdminNotice>
+
             <AdminSectionsWrapper>
                 <SubscriptionDetails />
-                <RecordsSlot/>
+                <RecordsSlot />
             </AdminSectionsWrapper>
         </>
     );
