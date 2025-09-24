dc2dfcc4bbf39c0c6e3d338b84d9e00e8e31377d
Joshua Dinh <joshuahungdinh@gmail.com>
2025-09-24 13:36:12 -0700

    refactor: use css align

--- src/Views/Components/ListTable/RowAction/index.tsx
+++ src/Views/Components/ListTable/RowAction/index.tsx
@@ -14,16 +14,13 @@ export default function RowAction({
 }) {
     if (href) {
         return (
-            <>
-                <a
-                    href={href}
-                    className={cx(styles.action, {[styles.delete]: highlight}, className)}
-                    aria-label={ariaLabel || displayText}
-                >
-                    {displayText} {hiddenText && <span className="give-visually-hidden">{hiddenText}</span>}
-                </a>
-                <span className={styles.separator}>|</span>
-             </>
+            <a
+                href={href}
+                className={cx(styles.action, {[styles.delete]: highlight}, className)}
+                aria-label={ariaLabel || displayText}
+            >
+                {displayText} {hiddenText && <span className="give-visually-hidden">{hiddenText}</span>}
+            </a>
         );
     }
 
@@ -32,18 +29,15 @@ export default function RowAction({
     }
 
     return (
-        <>
-            <button
-                type="button"
-                onClick={onClick}
-                data-actionid={actionId}
-                className={cx(styles.action, {[styles.delete]: highlight}, className)}
-                disabled={disabled}
-                aria-label={ariaLabel || displayText}
-            >
-                {displayText} {hiddenText && <span className="give-visually-hidden">{hiddenText}</span>}
-            </button>
-            <span className={styles.separator}>|</span>
-        </>
+        <button
+            type="button"
+            onClick={onClick}
+            data-actionid={actionId}
+            className={cx(styles.action, {[styles.delete]: highlight}, className)}
+            disabled={disabled}
+            aria-label={ariaLabel || displayText}
+        >
+            {displayText} {hiddenText && <span className="give-visually-hidden">{hiddenText}</span>}
+        </button>
     );
 }
