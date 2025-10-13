15054fa544b6609c562cba7c55511780822a83fe
Glauber Silva <glauber.silva@givewp.com>
2025-10-13 17:01:18 -0300

    Feature: add sort feature to the amount and name columns for the subscription list table (#8137)

--- src/Subscriptions/ListTable/Columns/AmountColumn.php
+++ src/Subscriptions/ListTable/Columns/AmountColumn.php
@@ -4,16 +4,18 @@ declare(strict_types=1);
 
 namespace Give\Subscriptions\ListTable\Columns;
 
-use Give\Subscriptions\Models\Subscription;
 use Give\Framework\ListTable\ModelColumn;
+use Give\Subscriptions\Models\Subscription;
 
 /**
+ * @unreleased add sort column
  * @since 2.24.0
  *
  * @extends ModelColumn<Subscription>
  */
 class AmountColumn extends ModelColumn
 {
+    protected $sortColumn = 'amount';
 
     /**
      * @since 2.24.0
