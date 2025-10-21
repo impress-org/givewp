231e6756a69230a76442c4c3cf2965fb49f3adfa
Paulo Iankoski <me@wpdev.com.br>
2025-10-21 15:24:44 -0300

    tests: remove tests for removed method

--- tests/Unit/Donations/Endpoints/TestListDonations.php
+++ tests/Unit/Donations/Endpoints/TestListDonations.php
@@ -92,45 +92,6 @@ class TestListDonations extends TestCase
     }
 
     /**
-     * @since 4.0.0
-     *
-     * @return void
-     * @throws Exception
-     */
-    public function testShouldFilterInvalidDateArgument_missingMonthDay()
-    {
-        $listDonation = give(ListDonations::class);
-        $key = 'start';
-        $value = '2020';
-
-        $mockRequest = $this->getMockRequest();
-        $mockRequest->set_param($key, $value);
-
-        $response = $listDonation->validateDate($value, $mockRequest, $key);
-        $this->assertFalse($response);
-    }
-
-    /**
-     * @since 4.0.0
-     *
-     * @return void
-     * @throws Exception
-     */
-    public function testShouldFilterInvalidDateArgument_InvalidChar()
-    {
-        $listDonation = give(ListDonations::class);
-        $key = 'start';
-        $value = '2020-mar-02';
-
-        $mockRequest = $this->getMockRequest();
-        $mockRequest->set_param($key, $value);
-
-        $response = $listDonation->validateDate($value, $mockRequest, $key);
-        $this->assertFalse($response);
-    }
-
-
-    /**
      * @since 4.10.0
      */
     public function testShouldFilterDonationsByDateRange()
