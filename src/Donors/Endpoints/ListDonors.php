4a5623de6ffb936a40fc044e38bd89544784ea14
Jon Waldstein <Jpwaldstein@gmail.com>
2025-12-19 10:58:53 -0500

    chore: remove unused since tag

--- src/Donors/Endpoints/ListDonors.php
+++ src/Donors/Endpoints/ListDonors.php
@@ -29,8 +29,6 @@ class ListDonors extends Endpoint
     /**
      * @inheritDoc
      *
-     * @unreleased replace permission callback logic with UserPermissions facade
-     *
      * @since 4.12.0 Add format parameter to start and end dates, replacing custom validation callback
      */
     public function registerRoute()
