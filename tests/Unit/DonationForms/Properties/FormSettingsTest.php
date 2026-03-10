e12fb066462d7722b9e53478d914eff08733008e
Rahula Palu Caleffi <rahula_dd@hotmail.com>
2026-03-10 13:43:32 -0300

    fix: Sanitize designId and escape HTML for classNames during the form preview

--- tests/Unit/DonationForms/Properties/FormSettingsTest.php
+++ tests/Unit/DonationForms/Properties/FormSettingsTest.php
@@ -27,4 +27,40 @@ class FormSettingsTest extends TestCase
 
         $this->assertSame('.test { color: green; }', $formSettings->customCss);
     }
+
+    /**
+     * @since TBD
+     */
+    public function testDesignIdStripsHtmlBreakingCharacters()
+    {
+        $formSettings = FormSettings::fromArray([
+            'designId' => 'x"><img src=x onerror=alert(document.domain)><div class="x',
+        ]);
+
+        $this->assertStringNotContainsString('"', $formSettings->designId);
+        $this->assertStringNotContainsString('>', $formSettings->designId);
+        $this->assertStringNotContainsString('<', $formSettings->designId);
+    }
+
+    /**
+     * @since TBD
+     */
+    public function testDesignIdPreservesValidSlug()
+    {
+        $formSettings = FormSettings::fromArray([
+            'designId' => 'multi-step-form-design',
+        ]);
+
+        $this->assertSame('multi-step-form-design', $formSettings->designId);
+    }
+
+    /**
+     * @since TBD
+     */
+    public function testDesignIdIsNullWhenNotProvided()
+    {
+        $formSettings = FormSettings::fromArray([]);
+
+        $this->assertNull($formSettings->designId);
+    }
 }
