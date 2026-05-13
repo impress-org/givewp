5ace98c22a4b12c02178e30f631a91be79296b05
Paulo Iankoski <me@wpdev.com.br>
2026-05-13 19:05:03 -0300

    feature: register lw_harbor/premium_plugin_exists filter in HarborServiceProvider

--- src/VendorOverrides/Harbor/HarborServiceProvider.php
+++ src/VendorOverrides/Harbor/HarborServiceProvider.php
@@ -6,6 +6,7 @@ namespace Give\VendorOverrides\Harbor;
 
 use Give\Helpers\Hooks;
 use Give\ServiceProviders\ServiceProvider as ServiceProviderContract;
+use Give\VendorOverrides\Harbor\Actions\HasActivePremiumAddons;
 use Give\VendorOverrides\Harbor\Actions\ReportLegacyLicences;
 use Give\Vendors\LiquidWeb\Harbor\Config;
 use Give\Vendors\LiquidWeb\Harbor\Harbor;
@@ -29,6 +30,7 @@ class HarborServiceProvider implements ServiceProviderContract
     }
 
     /**
+     * @unreleased Register lw_harbor/premium_plugin_exists filter
      * @since 4.15.0
      *
      * @inheritDoc
@@ -37,6 +39,9 @@ class HarborServiceProvider implements ServiceProviderContract
     {
         // reports legacy licenses to Harbor
         Hooks::addFilter('stellarwp/harbor/legacy_licenses', ReportLegacyLicences::class);
+
+        // reports whether any GiveWP premium add-on is active to Harbor
+        Hooks::addFilter('lw_harbor/premium_plugin_exists', HasActivePremiumAddons::class);
 
         // adds a "licensing" submenu to Give
         lw_harbor_register_submenu('edit.php?post_type=give_forms');
