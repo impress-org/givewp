79a5b68f4a4e89ec316ee2b2fd1bfe786de1b028
Paulo Iankoski <me@wpdev.com.br>
2026-05-13 19:05:01 -0300

    feature: add HasActivePremiumAddons action for Harbor premium detection

---
+++ src/VendorOverrides/Harbor/Actions/HasActivePremiumAddons.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Give\VendorOverrides\Harbor\Actions;
+
+/**
+ * Return true if another brand already has premium presence or if any GiveWP premium add-on is active.
+ *
+ * Hooked into the `lw_harbor/premium_plugin_exists` filter.
+ *
+ * @unreleased
+ */
+class HasActivePremiumAddons
+{
+    /**
+     * @unreleased
+     */
+    public function __invoke(?bool $otherBrandsExistence): bool
+    {
+        if ($otherBrandsExistence) {
+            return true;
+        }
+
+        $premiumAddons = give_get_plugins(['only_premium_add_ons' => true]);
+
+        foreach ($premiumAddons as $addon) {
+            if ($addon['Status'] === 'active') {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
