6caa89e36e8e5fdce9602458b9987745e6a3c5a8
Jon Waldstein <Jpwaldstein@gmail.com>
2026-06-16 14:54:01 -0400

    refactor: boyscout updates to clean up

--- src/DonationSpam/EmailAddressWhiteList.php
+++ src/DonationSpam/EmailAddressWhiteList.php
@@ -13,19 +13,29 @@ class EmailAddressWhiteList
     protected $whitelistEmails;
 
     /**
+     * @since TBD Normalize whitelisted emails so comparisons are case- and whitespace-insensitive.
      * @since 3.15.1 Add array type to enforce type.
      * @since 3.15.0
      */
     public function __construct(array $whitelistEmails = [])
     {
-        $this->whitelistEmails = $whitelistEmails;
+        $this->whitelistEmails = array_map([$this, 'normalize'], $whitelistEmails);
     }
 
     /**
+     * @since TBD Compare against the normalized whitelist so casing/whitespace don't cause a miss.
      * @since 3.15.0
      */
     public function validate($email): bool
     {
-        return in_array($email, $this->whitelistEmails, true);
+        return in_array($this->normalize($email), $this->whitelistEmails, true);
+    }
+
+    /**
+     * @since TBD
+     */
+    private function normalize($email): string
+    {
+        return strtolower(trim((string)$email));
     }
 }
