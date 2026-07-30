ecbbdd3f6cf243d2cdc4f533b21cd31f97677ff7
WayneRocha <dev.waynerocha@gmail.com>
2026-07-30 12:45:58 -0300

    feat: add password validation on backend

--- src/DonorDashboards/Tabs/EditProfileTab/PasswordRoute.php
+++ src/DonorDashboards/Tabs/EditProfileTab/PasswordRoute.php
@@ -6,6 +6,7 @@ use Give\DonorDashboards\Helpers\SanitizeProfileData as SanitizeHelper;
 use Give\DonorDashboards\Profile as Profile;
 use Give\DonorDashboards\Tabs\Contracts\Route as RouteAbstract;
 use WP_REST_Request;
+use WP_REST_Response;
 
 /**
  * @since 2.10.0
@@ -38,18 +39,32 @@ class PasswordRoute extends RouteAbstract
     /**
      * Handles password update.
      *
+     * @since TBD added password validation
      * @since 3.3.0
      *
      * @param WP_REST_Request $request
      *
-     * @return array
-     *
+     * @return array|WP_REST_Response
      */
     public function handleRequest(WP_REST_Request $request)
     {
+        $newPassword = trim($request->get_param('newPassword'));
+
+        if (empty($newPassword)) {
+            return new WP_REST_Response(
+                [
+                    'status' => 400,
+                    'response' => 'invalid_password',
+                    'body_response' => [
+                        'message' => esc_html__('Please enter a valid password.', 'give'),
+                    ],
+                ]
+            );
+        }
+
         wp_update_user([
             'ID' => wp_get_current_user()->ID,
-            'user_pass' => $request->get_param('newPassword'),
+            'user_pass' => $newPassword,
         ]);
 
         return [
