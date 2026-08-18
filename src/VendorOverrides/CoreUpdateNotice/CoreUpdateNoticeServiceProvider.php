717ce73866ae4dec718524c8b5f42536c39cd970
WayneRocha <dev.waynerocha@gmail.com>
2026-08-18 17:24:57 -0300

    refactor: update Core Update Notice integration and improve registration logic

--- src/VendorOverrides/CoreUpdateNotice/CoreUpdateNoticeServiceProvider.php
+++ src/VendorOverrides/CoreUpdateNotice/CoreUpdateNoticeServiceProvider.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Give\VendorOverrides\CoreUpdateNotice;
 
 use Give\ServiceProviders\ServiceProvider;
-use Give\Vendors\StellarWP\CoreUpdateNotice\Config;
+use Give\Vendors\StellarWP\CoreUpdateNotice\CoreUpdateNotice;
 use Give\Vendors\StellarWP\CoreUpdateNotice\Register;
 
 /**
@@ -24,7 +24,7 @@ class CoreUpdateNoticeServiceProvider implements ServiceProvider
      */
     public function register()
     {
-        Config::setContainer(give()->getContainer());
+        /* The notice is registered on init in boot(), once the text domain has loaded. */
     }
 
     /**
@@ -34,16 +34,29 @@ class CoreUpdateNoticeServiceProvider implements ServiceProvider
      */
     public function boot()
     {
-        /* Deferred to init because the copy below is only translatable once the text domain loads. */
         add_action('init', static function () {
-            Register::notice([
-                'heading' => __('Keep your site protected. Update to the latest version of WordPress.', 'give'),
-                'body' => __(
-                    'Your site is running on an outdated version of WordPress, which can leave it vulnerable to security issues. To decrease your risk of exposure, please update your WordPress install to the latest version.',
-                    'give'
-                ),
-                'dismiss' => __('Dismiss this notice.', 'give'),
-            ]);
+            Register::notice(
+                new CoreUpdateNotice([
+                    'heading' => __(
+                        'Keep your site protected. Update to the latest version of WordPress.',
+                        'give'
+                    ),
+                    'body' => __(
+                        'Your site is running on an outdated version of WordPress, which can leave it vulnerable to security issues. To decrease your risk of exposure, please update your WordPress install to the latest version.',
+                        'give'
+                    ),
+                    'dismiss' => __('Dismiss this notice.', 'give'),
+                ]),
+                static function (): bool {
+                    $screen = get_current_screen();
+
+                    return $screen !== null && (
+                        $screen->post_type === 'give_forms'
+                        || strpos($screen->id, 'give_forms_page_') === 0
+                        || strpos($screen->id, 'admin_page_give-') === 0
+                    );
+                }
+            );
         });
     }
 }
