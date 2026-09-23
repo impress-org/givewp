fcc119e6525ede5fe4e7937af0c24ab010796fa8
Paulo Iankoski <me@wpdev.com.br>
2026-09-23 20:58:06 -0300

    Fix: escape output in the default email header template

--- templates/emails/header-default.php
+++ templates/emails/header-default.php
@@ -4,6 +4,7 @@
  *
  * @package     Give/Templates/Emails
  * @version     1.0
+ * @since TBD Escape output.
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -65,22 +66,22 @@ $header_content_h1  = "
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-		<title><?php echo get_bloginfo( 'name' ); ?></title>
+		<title><?php echo esc_html( get_bloginfo( 'name' ) ); ?></title>
 	</head>
-	<body leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" offset="0" style="<?php echo $body; ?>">
-		<div style="<?php echo $wrapper; ?>">
+	<body leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" offset="0" style="<?php echo esc_attr( $body ); ?>">
+		<div style="<?php echo esc_attr( $wrapper ); ?>">
 			<table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%">
 				<tr>
 					<td align="center" valign="top">
 						{email_logo}
-						<table border="0" cellpadding="0" cellspacing="0" width="600" id="template_container" style="<?php echo $template_container; ?>">
+						<table border="0" cellpadding="0" cellspacing="0" width="600" id="template_container" style="<?php echo esc_attr( $template_container ); ?>">
 							<tr>
 								<td align="center" valign="top">
 									<!-- Header -->
-									<table border="0" cellpadding="0" cellspacing="0" width="600" id="template_header" style="<?php echo $template_header; ?>" bgcolor="#ffffff">
+									<table border="0" cellpadding="0" cellspacing="0" width="600" id="template_header" style="<?php echo esc_attr( $template_header ); ?>" bgcolor="#ffffff">
 										<tr>
 											<td>
-												<h1 style="<?php echo $header_content_h1; ?>"><?php echo Give()->emails->get_heading(); ?></h1>
+												<h1 style="<?php echo esc_attr( $header_content_h1 ); ?>"><?php echo esc_html( Give()->emails->get_heading() ); ?></h1>
 											</td>
 										</tr>
 									</table>
@@ -92,9 +93,9 @@ $header_content_h1  = "
 									<!-- Body -->
 									<table border="0" cellpadding="0" cellspacing="0" width="600" id="template_body">
 										<tr>
-											<td valign="top" style="<?php echo $body_content; ?>">
+											<td valign="top" style="<?php echo esc_attr( $body_content ); ?>">
 												<!-- Content -->
 												<table border="0" cellpadding="20" cellspacing="0" width="100%">
 													<tr>
 														<td valign="top">
-															<div style="<?php echo $body_content_inner; ?>">
+															<div style="<?php echo esc_attr( $body_content_inner ); ?>">
