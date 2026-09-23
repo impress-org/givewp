51333770843cb0a2bbcdf87de60d6cfc9c2bd978
Paulo Iankoski <me@wpdev.com.br>
2026-09-23 15:37:20 -0300

    Fix: escape output in the classic editor shortcode button

--- includes/admin/shortcodes/class-shortcode-button.php
+++ includes/admin/shortcodes/class-shortcode-button.php
@@ -136,6 +136,7 @@ final class Give_Shortcode_Button {
 	 *
 	 * @return string|bool
 	 *
+	 * @since TBD Escape output.
 	 * @since 1.0
 	 */
 	public function shortcode_button() {
@@ -153,9 +154,9 @@ final class Give_Shortcode_Button {
 
 				$shortcodes[ $shortcode ] = sprintf(
 					'<div class="sc-shortcode mce-menu-item give-shortcode-item-%1$s" data-shortcode="%2$s">%3$s</div>',
-					$shortcode,
-					$shortcode,
-					$values['label']
+					esc_attr( $shortcode ),
+					esc_attr( $shortcode ),
+					esc_html( $values['label'] )
 				);
 			}
 		}
@@ -175,13 +176,13 @@ final class Give_Shortcode_Button {
 
 				printf(
 					'<button type="button" class="button sc-shortcode" data-shortcode="%s">%s</button>',
-					$shortcode,
-					sprintf(
+					esc_attr( $shortcode ),
+					wp_kses_post( sprintf(
 						'%s %s %s',
 						$img,
-						__( 'Insert', 'give' ),
-						self::$shortcodes[ $shortcode ]['label']
-					)
+						esc_html__( 'Insert', 'give' ),
+						esc_html( self::$shortcodes[ $shortcode ]['label'] )
+					) )
 				);
 			} else {
 				printf(
@@ -189,8 +190,9 @@ final class Give_Shortcode_Button {
 					'<button class="button sc-button" type="button">%s %s</button>' .
 					'<div class="sc-menu mce-menu">%s</div>' .
 					'</div>',
-					$img,
-					__( 'GiveWP Shortcodes', 'give' ),
+					wp_kses_post( $img ),
+					esc_html__( 'GiveWP Shortcodes', 'give' ),
+					// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- each menu item is built by shortcode_button() with esc_attr()/esc_html() on its own values.
 					implode( '', array_values( $shortcodes ) )
 				);
 			}
