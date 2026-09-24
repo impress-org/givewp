6006bd21c91c728f3ff0b4daae2da0e0fa34ebae
Paulo Iankoski <me@wpdev.com.br>
2026-09-23 21:02:11 -0300

    Fix: escape output in the default email footer template

--- templates/emails/footer-default.php
+++ templates/emails/footer-default.php
@@ -4,6 +4,7 @@
  *
  * @package     Give/Templates/Emails
  * @version     1.0
+ * @since TBD Escape output.
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -40,13 +41,13 @@ $credit = "
 							<tr>
 								<td align="center" valign="top">
 									<!-- Footer -->
-									<table border="0" cellpadding="10" cellspacing="0" width="600" id="template_footer" style="<?php echo $template_footer; ?>">
+									<table border="0" cellpadding="10" cellspacing="0" width="600" id="template_footer" style="<?php echo esc_attr( $template_footer ); ?>">
 										<tr>
 											<td valign="top">
 												<table border="0" cellpadding="10" cellspacing="0" width="100%">
 													<tr>
-														<td colspan="2" valign="middle" id="credit" style="<?php echo $credit; ?>">
-															<?php echo wpautop( wp_kses_post( wptexturize( apply_filters( 'give_email_footer_text', '<a href="' . esc_url( home_url() ) . '">' . get_bloginfo( 'name' ) . '</a>' ) ) ) ); ?>
+														<td colspan="2" valign="middle" id="credit" style="<?php echo esc_attr( $credit ); ?>">
+															<?php echo wp_kses_post( wpautop( wptexturize( apply_filters( 'give_email_footer_text', '<a href="' . esc_url( home_url() ) . '">' . esc_html( get_bloginfo( 'name' ) ) . '</a>' ) ) ) ); ?>
 														</td>
 													</tr>
 												</table>
