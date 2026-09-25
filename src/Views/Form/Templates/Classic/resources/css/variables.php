c07705af1869d9181033dbdac632108314607dde
Paulo Iankoski <me@wpdev.com.br>
2026-09-25 16:23:08 -0300

    Fix: use esc_url_raw() instead of esc_url() for the CSS header background image, esc_url() breaks the URL passed to wp_add_inline_style()

--- src/Views/Form/Templates/Classic/resources/css/variables.php
+++ src/Views/Form/Templates/Classic/resources/css/variables.php
@@ -1,7 +1,7 @@
 <?php /** @since TBD Escape output. */ ?>
 :root {
     --give-primary-color: <?= esc_attr($primaryColor) ?>;
-    --give-header-background-image: url("<?= esc_url($headerBackgroundImage) ?>");
+    --give-header-background-image: url("<?= esc_url_raw($headerBackgroundImage) ?>");
     <?php if (!empty($headerBackgroundColor)) : ?>
     --give-header-background-color--for-rgb: <?= (int) hexdec(substr($headerBackgroundColor, 1, 2)) ?>, <?= (int) hexdec(substr($headerBackgroundColor, 3, 2)) ?>, <?= (int) hexdec(substr($headerBackgroundColor, 5, 2)) ?>;
     <?php endif; ?>
